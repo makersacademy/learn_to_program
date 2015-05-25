2c7d118035a8b9927167fa775955f38dc94df6f4
Natalie Akam <natalie.akam@gmail.com>
2015-05-25 08:50:04 +0000

    Added billion seconds old program

---
+++ ch09-writing-your-own-methods/billion-seconds.rb
@@ -0,0 +1,11 @@
+# y2k = Time.mktime(2000, 1, 1) 
+
+birth_time = Time.mktime(1984, 12, 8, 4, 23, 4) 
+
+# number of seconds after 1/1/1970 I was born
+birth_seconds = birth_time.to_i
+billion_seconds_old = birth_seconds + 1000000000
+time_billion_seconds_old = Time.at(billion_seconds_old)
+
+  
+puts "I was born at #{birth_time} #{birth_time.zone}. I will be a billion seconds old at #{time_billion_seconds_old} #{time_billion_seconds_old.zone}"

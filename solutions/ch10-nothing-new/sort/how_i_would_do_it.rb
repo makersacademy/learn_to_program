8ced508aa51598b2ebd757311b58db1970def5db
Sam Joseph <tansaku@gmail.com>
2015-05-25 17:08:51 +0100

    adds more solutions and fixes some file names

---
+++ solutions/ch10-nothing-new/sort/how_i_would_do_it.rb
@@ -0,0 +1,12 @@
+# The well-known quicksort algorithm.
+def sort arr
+  return arr if arr.length <= 1
+  
+  middle = arr.pop
+  less = arr.select{|x| x < middle}
+  more = arr.select{|x| x >= middle}
+
+  sort(less) + [middle] + sort(more)
+end
+
+# p(sort(['can','feel','singing','like','a','can']))

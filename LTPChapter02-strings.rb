f79dcd1ffa3fd12032a0ff04040f3244bbda43a7
KateWilkinson <kate.wilkinson@hotmail.com>
2015-05-25 12:07:10 +0100

    Added LTP Chapter02 - strings

---
+++ LTPChapter02-strings.rb
@@ -0,0 +1,19 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts ''
+puts 'I like' + ' apple pie.'
+puts ''
+puts 'blink ' * 4
+puts ''
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5 
+puts '2' * 5
+puts '2 * 5'
+puts ''
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'

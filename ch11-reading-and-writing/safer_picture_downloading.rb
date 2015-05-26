76f571960777827f32888b50b435ecd1bf7a5d16
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-26 22:03:31 +0100

    Completing safe picture downloading exercise of Chapter 11.

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,35 @@
-# your code here
+# /usr/bin/env ruby
+
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox'
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files:  "
+
+pic_number = 1
+pic_names.each do |name|
+
+	print '.'
+
+	new_name = if pic_number < 10 
+		"#{batch_name}0#{pic_number}.jpg"
+	else
+		"#{batch_name}#{pic_number}.jpg" 
+	end
+
+	if new_name.exist?
+		puts 'Uh oh, looks like you already have a picture with that name'
+		exit
+	end
+
+	File.rename name, new_name
+
+	pic_number = pic_number + 1
+end
+
+puts 
+puts 'Done!'

5da9d5d522fcc31dc1017dd2af8f7c956dddcdcd
KateWilkinson <kate.wilkinson@hotmail.com>
2015-05-26 11:30:32 +0100

    Added LTP Chapter 06 - toc program

---
+++ LTPChap06-toc.rb
@@ -0,0 +1,9 @@
+line_width = 60
+
+puts ('Table of Contents'.center(line_width))
+
+puts ('Chapter 1: Getting Started'.ljust(line_width/2)) + ('page 1'.rjust(line_width/2))
+
+puts ('Chapter 2: Numbers'.ljust(line_width/2)) + ('page 9'.rjust(line_width/2))
+
+puts ('Chapter 3: Letters'.ljust(line_width/2)) + ('page 13'.rjust(line_width/2))

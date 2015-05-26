b057553e8d88ad76130b6484d746f5b86600ccf5
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-26 23:03:30 +0100

    Shuffling playlist in exercise of Chapter 11.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,15 +1,15 @@
 # /usr/bin/env ruby
 
-Dir.chdir "/Users/nhughes/Music/"
+Dir.chdir "/Users/nhughes/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album"
 
 puts "What would you like to call this playlist"
 playlist_name = gets.chomp
 
+all_sounds = Dir["*.mp3"].shuffle
+
 File.open(playlist_name + ".m3u", "w+") do |f|
-	Dir.foreach("iTunes/iTunes Media/Music/Unknown Artist/Unknown Album") do |sound|
-		if sound.end_with? ".mp3"
-			f.write("music/sound-effects/Miscellaneous/#{sound}\n")
-		end
+	all_sounds.each do |sound|
+		f.write("music/sound-effects/Miscellaneous/#{sound}\n")
 	end
 end
 

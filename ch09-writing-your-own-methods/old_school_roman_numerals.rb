f005d5411e4bd1fe71fce89b849d2de51645a0c4
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-26 18:23:19 +0100

    Completing old Roman numeral exercise of Chapter 9.

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,20 @@
 def old_roman_numeral num
   # your code here
-end
+
+  numeral = ""
+
+  numeral << "M" * (num/1000)
+  numeral << "D" * (num % 1000 / 500)
+  numeral << "C" * (num % 500 / 100)
+  numeral << "L" * (num % 100 / 50)
+  numeral << "X" * (num % 50 / 10)
+  numeral << "V" * (num % 10 / 5)
+  numeral << "I" * (num % 5 / 1)
+
+  numeral
+
+ end
+
+puts old_roman_numeral(3000)
+puts old_roman_numeral(599)
+puts old_roman_numeral(14)

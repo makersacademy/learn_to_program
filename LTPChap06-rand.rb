92b45105b99db3dffa9a8443031e55a51630132e
KateWilkinson <kate.wilkinson@hotmail.com>
2015-05-26 11:51:30 +0100

    Added LTP Chapter 06 rand

---
+++ LTPChap06-rand.rb
@@ -0,0 +1,38 @@
+puts rand
+puts rand
+puts rand 
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts (rand(1))
+puts (rand(1))
+puts (rand(1))
+puts (rand(999999999999999999999))
+puts ('The weatherman said there is a')
+puts (rand(101).to_s + '% chance of rain,')
+puts ('but you can never trust a weatherman.')
+
+puts ''
+srand 1976
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts ''
+srand 1976
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts ''
+srand 2121
+puts (rand(51))
+puts (rand(51))
+puts (rand(51))
+puts (rand(51))
+puts ''
+srand 2121
+puts (rand(51))
+puts (rand(51))
+puts (rand(51))
+puts (rand(51))

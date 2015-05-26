05a11e35c11275910aff4f91de3590f88258a8b2
Natalie Akam <natalie.akam@gmail.com>
2015-05-26 22:00:58 +0000

    Added logic for tens and ones

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -12,13 +12,29 @@ def old_roman_numeral num
     roman_num << 'CD' 
   elsif (no_hundreds == 9)
     roman_num << 'CM'
-  elsif (no_hundreds == 5)
-   
   else
-    roman_num << 'D' * (num%1000 / 500) 
-    roman_num << 'C' * (num/1000)
+    roman_num << 'D' * (num % 1000 / 500)
+    roman_num << 'C' * (num % 500 / 100) 
   end
 
+  if (no_tens == 4)
+    roman_num << 'XL' 
+  elsif (no_tens == 9)
+    roman_num << 'XC'
+  else
+    roman_num << 'L' * (num % 100 / 50)
+    roman_num << 'X' * (num % 50 / 10) 
+  end
+  
+  if (no_ones == 4)
+    roman_num << 'IV' 
+  elsif (no_ones == 9)
+    roman_num << 'IX'
+  else
+    roman_num << 'V' * (num%10 / 5) 
+    roman_num << 'I' * (num%5)
+  end
+  
 #  roman_num << 'M' * (num/1000)
 #  roman_num << 'D' * (num%1000 / 500) 
 #  roman_num << 'C' * (num%500 / 100) 

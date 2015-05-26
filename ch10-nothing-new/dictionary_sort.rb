fd6516061413ca061806f83b745d388d6b624b92
Jonathan Sayer <jon.sayer1@gmail.com>
2015-05-26 22:34:06 +0100

    Chapter 10 codes

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,32 @@
-def dictionary_sort arr
-  # your code here
+def Dictionary_sort array
+
+curr_index=0
+new_array=[]
+
+
+while curr_index < array.length
+	new_array.push array[curr_index].downcase
+	curr_index+=1
 end
+
+new_array.sort! 
+
+n=0
+#Go through all the elements in array, if they were capitalised, find that word in new array and capitalise it. 
+array.each do |word|
+	if word == word.capitalize
+		while n != new_array.length
+			if word.downcase==new_array[n]
+				new_array[n].capitalize!
+				break
+			else
+				n+=1
+			end
+		end
+	end
+end
+		
+
+return new_array
+end
+
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,22 @@
 def shuffle arr
-  # your code here
+	shuffled=[]
+	
+	while arr.length >0
+		rand_index=rand(arr.length)
+		
+		curr_index=0
+		new_arr=[]
+		
+		arr.each do |item|
+			if curr_index==rand_index
+				shuffled.push item
+			else 
+				new_arr.push item
+			end
+			
+			curr_index+=1
+			end
+	arr=new_arr
+	end
+	shuffled
 end

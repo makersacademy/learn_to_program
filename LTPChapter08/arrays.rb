97a5e5d2885ba21531c36cac4d77544d18c3e781
KateWilkinson <kate.wilkinson@hotmail.com>
2015-05-26 18:30:53 +0100

    updated files

---
+++ LTPChapter08/arrays.rb
@@ -0,0 +1,16 @@
+names= ['Ada', 'Belle', 'Chris']
+
+puts names
+puts 
+puts names [0]
+puts names [1]
+puts names [2]
+puts names [3]
+
+other_peeps = []
+other_peeps [3] = 'beebee Meaner'
+other_peeps [0] = 'Ah-ha'
+other_peeps [1] = 'Seedee'
+other_peeps [0] = 'beebee Ah-ha'
+puts other_peeps
+

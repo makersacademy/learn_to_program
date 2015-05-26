cb7fa70e4e4c8747ebc957d7833e5f2c34322ef2
Natalie Akam <natalie.akam@gmail.com>
2015-05-26 07:12:15 +0000

    Updated old_school_roman_numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
 def old_roman_numeral num
-  # your code here
+  roman_num = ''
+  roman_num << 'M' * (num/1000)
+  roman_num << 'D' * (num%1000 / 500) 
+  roman_num << 'C' * (num%500 / 100) 
+  roman_num << 'X' * (num%100 / 50) 
+  roman_num << 'L' * (num%50 / 10) 
+  roman_num << 'V' * (num%10 / 5)   
+  roman_num << 'I' * (num%5)
+ # I * num/1
+ # if num%5 == 0 puts 'V' 
+ # if num%5 ==1 puts 'I'
+  return roman_num
 end
+
+puts old_roman_numeral 1111
+puts old_roman_numeral 2554

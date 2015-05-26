0eee719864cd7797cf8e306a391ea9fecb061dc9
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-26 22:52:11 +0100

    Completing playlist exercise of Chapter 11.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,15 @@
-# your code here
+# /usr/bin/env ruby
+
+Dir.chdir "/Users/nhughes/Music/"
+
+puts "What would you like to call this playlist"
+playlist_name = gets.chomp
+
+File.open(playlist_name + ".m3u", "w+") do |f|
+	Dir.foreach("iTunes/iTunes Media/Music/Unknown Artist/Unknown Album") do |sound|
+		if sound.end_with? ".mp3"
+			f.write("music/sound-effects/Miscellaneous/#{sound}\n")
+		end
+	end
+end
+

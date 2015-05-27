298c8c1d12608d125ea3a926909536ad0a8b5cbf
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 10:46:43 +0100

    Completing one billion exercise of Chapter 12.

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,7 @@
-# your code here
+# /usr/bin/env ruby
+
+birthday = Time.local(1984, 12, 14)
+
+one_billion_seconds = birthday + 1000000000
+
+puts "On #{one_billion_seconds} you'll be a billion seconds old"

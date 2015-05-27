5fab4079d87195fd322a16dacc891b46c48834f6
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 11:52:31 +0100

    Completing birthday helper exercise of Chapter 12

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,18 @@
-# your code here
+# /usr/bin/env ruby
+
+birthdays = Hash.new
+
+f = File.open("ch12-new-classes-of-objects/birth_dates.txt", "r")
+
+f.each_line do |line|
+	name = line.split("\t")[0]
+	birthdate = line.split("\t")[1].chomp
+	birthdays[name] = birthdate
+end
+
+f.close
+
+puts "Who's birthday do you need to know?"
+want = gets.chomp
+
+puts "It's #{birthdays[want]}"

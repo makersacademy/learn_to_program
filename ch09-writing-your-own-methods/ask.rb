9f1810cb998e0fee8823d03590ca5f6053c08de8
Gerard Morera <grdmorera@gmail.com>
2015-05-27 23:21:44 +0200

    working on old_school_romans

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,13 +1,15 @@
+
 def ask question
   while true
     puts question
     reply = gets.chomp.downcase
+
     if ( reply == 'yes' || reply == 'no')
-      return true  if reply
-      return false if !!reply
+      return true  if reply == 'yes'
+      return false 
     else
       puts 'Please answer "yes" or "no".'
     end
+    
   end
-
 end
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,21 @@
-def old_roman_numeral num
-  # your code here
-end
+Class ToRomans
+
+  VALUES = { I:  1 
+             V:  5
+             X:  10
+             L:  50
+             C:  100
+             D:  500
+             M:  1000
+  }
+
+  def old_roman_numeral number
+
+  end
+
+  def parser number
+    VALUES.each_with_object [] do |( roman, decimal ), array|
+      
+
+
+  end

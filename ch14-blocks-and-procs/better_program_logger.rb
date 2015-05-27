cf1d45105b54dd68323c4b9a4822fe6ed070be02
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 15:16:19 +0100

    Completing better program logger exercise of Chapter 14.

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,1 +1,26 @@
-# your code here
+# /usr/bin/env ruby
+
+$nested_depth = 0
+
+def log description, &block
+	indent = " " * $nested_depth
+	puts indent + "Starting " + description + " ..."
+	$nested_depth += 1
+	returning = block.call
+	$nested_depth -= 1
+	puts indent + '..."' + description + '" finished, returning: ' + returning.to_s
+end
+
+log "outer block" do
+  log "some little block" do
+  	log "teeny-tiny block" do 
+  		rand()
+  	end
+    (40 + 80) / 15
+  end
+
+  log "yet another block" do
+    "I like Thai food!".upcase
+  end
+  "This is the end"
+end

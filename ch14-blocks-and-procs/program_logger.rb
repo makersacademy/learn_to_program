9b08b6e6b0ff363bd9c97427aac38b92b0bff367
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 15:09:20 +0100

    Completing program logger exercise of Chapter 14.

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,1 +1,18 @@
-# your code here
+# /usr/bin/env ruby
+
+def log description, &block
+	puts "Starting " + description + " ..."
+	returning = block.call
+	puts '..."' + description + '" finished, returning: ' + returning.to_s
+end
+
+log "outer block" do
+  log "some little block" do
+    (40 + 80) / 15
+  end
+
+  log "yet another block" do
+    "I like Thai food!".upcase
+  end
+  "This is the end"
+end

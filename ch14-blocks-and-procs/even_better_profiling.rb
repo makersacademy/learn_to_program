05e76965da095af8aa0e4fd5dfe7cb9abbead211
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 14:43:15 +0100

    Completing better profiling exercise of Chapter 14.

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,1 +1,14 @@
-# your code here
+# /usr/bin/env ruby
+
+def profile descriptionOfBlock, &block, on
+
+	if on
+		startTime = Time.now
+		block.call
+		duration = Time.now - startTime
+		puts descriptionOfBlock+':  '+duration.to_s+' seconds'
+	else
+		block.call
+	end
+
+end

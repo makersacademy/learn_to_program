366dbbd4127bdeac52c474a2ed21f82645d7f828
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 12:20:52 +0100

    Completing extending built in classes exercise of Chapter 13.

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,1 +1,18 @@
-# your code here
+# /usr/bin/env ruby
+class Array
+
+	def three_set_shuffle 
+		# Slice the array into three sets i.e. [[],[],[]]
+		three_sets = self.each_slice(3).to_a
+		# Let's shuffle the outer array
+		three_sets.shuffle!
+		# Now let's suffle the inner arrays
+		three_sets.each {|set| set.shuffle!}
+		# Now let's get back to having just one array of file names
+		three_sets.flatten!
+	end
+
+end
+
+my_set = [1,2,3,4,5]
+puts my_set.three_set_shuffle.inspect

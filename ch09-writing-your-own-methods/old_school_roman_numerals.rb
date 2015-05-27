ae665722500198c4066a6dbc41e7ef23cef8678f
Gerard Morera <grdmorera@gmail.com>
2015-05-28 00:38:08 +0200

    old_school_roman_numerals finished

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,21 +1,32 @@
-Class ToRomans
 
-  VALUES = { I:  1 
-             V:  5
-             X:  10
-             L:  50
-             C:  100
-             D:  500
-             M:  1000
+class ToRomans
+
+  VALUES = { "I" => 1, 
+             "V" => 5,
+             "X" => 10,
+             "L" => 50,
+             "C" => 100,
+             "D" => 500,
+             "M" => 1000
   }
 
   def old_roman_numeral number
-
+    calculator number
   end
 
-  def parser number
-    VALUES.each_with_object [] do |( roman, decimal ), array|
-      
+  def calculator number, result = ""
+    if number == 0
+      result
+    else
+      calculator number - elements( number )[ 1 ], result << elements( number )[ 0 ]
+    end
+  end 
 
-
+  def elements number
+    VALUES.select do |roman, decimal|
+      decimal <= number
+    end.max_by { |roman,decimal| decimal }
   end
+
+end
+

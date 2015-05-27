2763c27876792a325785e13c8a72f7626d1e3312
Michael Arnott <arnottmj@aol.com>
2015-05-27 16:12:45 +0100

    modidied ask solution

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -5,15 +5,18 @@ def ask question
 	
 		if (reply == 'yes' || reply == 'no') 
 			if reply == 'yes'
-				return true 
+				return true #replaced with return
 			else
-				return false 
+				return false #replaced with return
 			end 
 			#removed break command since now using return statements
 		else
 			puts 'Please answer "yes" or "no".' 
 		end
 	end
+
+#removed statement here since now using return statements
 end
 
-ask 'Do you like chocolate?'
+
+

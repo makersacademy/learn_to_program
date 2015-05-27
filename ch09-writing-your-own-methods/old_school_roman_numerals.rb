e9bcf4a143fc86873fbe1e125f5a8564305a753d
TheoLeanse <theoleanse@gmail.com>
2015-05-27 15:52:58 +0100

    adds the old-school Roman numeral method from chapter 9 exercises

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,22 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(supplied_num)
+  @result = ""
+  @remainder = supplied_num
+
+  def check_numeral(roman, decimal)
+    @result << roman * (@remainder/decimal)
+    @remainder %= decimal
+  end
+
+  numerals = {
+    "M" => 1000,
+    "D" => 500,
+    "C" => 100,
+    "L" => 50,
+    "X" => 10,
+    "V" => 5,
+    "I" => 1
+  }
+
+  numerals.each { |roman, decimal| check_numeral(roman, decimal)}
+  @result
 end

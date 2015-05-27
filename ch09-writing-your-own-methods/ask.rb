28ecb4bac4aa1ca4f1f8984508bbccbe564b79f0
Michael Arnott <arnottmj@aol.com>
2015-05-27 16:03:04 +0100

    updated ask file with solution

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,19 @@
 def ask question
-  # your code here
+  	while true
+		puts question
+		reply = gets.chomp.downcase
+	
+		if (reply == 'yes' || reply == 'no') 
+			if reply == 'yes'
+				return true 
+			else
+				return false 
+			end 
+			#removed break command since now using return statements
+		else
+			puts 'Please answer "yes" or "no".' 
+		end
+	end
 end
+
+ask 'Do you like chocolate?'

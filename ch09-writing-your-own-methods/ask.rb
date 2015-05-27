e493004723b45f4d49bbd60a5b5dc828087711d9
Paul Harker <harkerp@gmail.com>
2015-05-27 23:19:14 +0100

    first two exercises answered

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
-def ask question
-  # your code here
+def ask question 
+    while true
+    puts question
+    reply = gets.chomp.downcase
+
+if (question == "Do you wet the bed?" && reply == 'yes')
+	return true
+break 
 end
+if reply == 'no' || 'yes'
+	break
+else
+      puts 'Please answer "yes" or "no".'
+end 
+end
+end
+
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,32 @@
 def old_roman_numeral num
-  # your code here
+  roman_string = ''
+ 	while num > 0
+ 		if num >= 1000
+ 			roman_string << 'M'
+ 			num = num - 1000
+ 		elsif num >= 500
+ 			roman_string << 'D'
+ 			num = num - 500
+ 		elsif num >= 100
+ 			roman_string << 'C'
+ 			num = num - 100
+ 		elsif num >= 50
+ 			roman_string << 'L'
+ 			num = num - 50
+ 		elsif num >= 10
+ 			roman_string << 'X'
+ 			num = num - 10
+ 		elsif num >= 5
+ 			roman_string << 'V'
+ 			num = num - 5
+ 		elsif num >= 1
+ 			roman_string << 'I'
+ 			num = num - 1
+ 		end
+
+ 	end
+ 	puts roman_string
 end
+puts "What number?"
+entry = gets.to_i
+old_roman_numeral entry

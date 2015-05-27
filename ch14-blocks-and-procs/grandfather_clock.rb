7f75418feb9ef46c3d62d3be6988f0a8b7db45ce
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 14:51:11 +0100

    Completing grandfather clock exercise of Chapter 14.

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,1 +1,16 @@
-# your code here
+# /usr/bin/env ruby
+
+def hours proc
+
+	hours_passed = Time.new.hour % 12
+
+	hours_passed.times do 
+		proc.call
+	end
+end
+
+chime = Proc.new do
+	puts "DONG!"
+end
+
+puts hours(chime)

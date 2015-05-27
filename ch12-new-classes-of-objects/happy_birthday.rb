8ebadf08c80f6f601c492d56b14fc99be10e843d
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 10:54:43 +0100

    Completing happy birthday exercise of Chapter 12.

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+# /usr/bin/env ruby
+
+puts "What year were you born in?"
+year = gets.chomp.to_i
+
+puts "What month were you born in?"
+month = gets.chomp.to_i
+
+puts "What day of the month were you born?"
+day = gets.chomp.to_i
+
+birthday = Time.local(year, month, day)
+
+num_birthdays = (Time.now - birthday) / 31536000
+
+num_birthdays.to_i.times do 
+	puts "SPANK!"
+end

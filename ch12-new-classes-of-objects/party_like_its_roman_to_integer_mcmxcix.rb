9434eef0c9f0d2b2d899cc9e5cfae6b198a7a2d3
Nicola Hughes <nicolarhughes@gmail.com>
2015-05-27 11:32:18 +0100

    Completing roman numeral exercise of Chapter 12.

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,1 +1,31 @@
-# your code here
+# /usr/bin/env ruby
+
+def roman_to_int numeral
+
+	roman = {"i"=>1, "v"=>5, "x"=>10, "l"=>50, "c"=>100, "d"=>500, "m"=>1000}
+
+	digit = 0
+	prev = 0
+	len = numeral.length - 1
+
+	while len >= 0
+		letter = numeral[len].downcase
+		val = roman[letter]
+		len = len - 1
+		if !val
+			puts "Not a Roman numeral"
+			return
+		else
+			if val < prev
+      			val = val * -1
+    		else
+      			prev = val
+    		end
+    	digit = digit + val
+		end
+	end
+	digit
+end
+
+puts roman_to_int('mcmxcix')
+puts roman_to_int('CCCLXV')

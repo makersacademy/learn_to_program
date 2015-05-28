85b6245a6a9408bc36dbf995aba69a5358671e2b
Michael Arnott <arnottmj@aol.com>
2015-05-28 02:35:52 +0100

    added solution for sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,40 @@
 def sort arr
-  # your code here
+	recursive_sort(arr, [])
+  
 end
+
+def recursive_sort unsorted_array, sorted_array
+	
+	a = 0
+	b = 0
+
+	while b < unsorted_array.length
+		if unsorted_array[a] < unsorted_array[b]
+			b = b + 1
+		elsif a = b
+			b = b + 1
+		else
+			a = a + 1
+			b = 0
+		end
+	end
+
+	sorted_array.push unsorted_array[a]
+	unsorted_array.delete_at(a)
+
+	if unsorted_array.length > 0
+
+		recursive_sort(unsorted_array, sorted_array)
+	end
+
+	sorted_array
+
+end
+
+
+
+
+
+
+
+	

b9fe0d1312d76dacfe6be3502040d74c6f1dba28
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-05-28 23:00:21 +0100

    Add chapter9, ex1

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,16 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' || reply == 'no')
+      return true if reply == 'yes'
+      return false if reply == 'no'
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+  return
 end
+
+ask 'Do you like tacos?'

f8e986e55a1a401353a584cbd34eeaa1533c2181
MassimilianoMura <massimilianomura1983@gmail.com>
2015-05-28 17:04:53 +0100

    Old roman first version

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,21 @@
 def old_roman_numeral num
-  # your code here
+  thous = (num / 1000)
+  print 'M' * thous
+  fivhund = (num - (thous * 1000)) / 500
+  print  'D' * fivhund
+  hundred = (num - ((thous * 1000) + (fivhund * 500))) / 100
+  print 'C' * hundred
+  fift = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100))) / 50
+  print 'L' * fift
+  ten = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100) + (fift * 50))) / 10
+  print 'X' * ten
+  five = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100) + (fift * 50) + (ten * 10))) / 5
+  print 'V' * five
+  one = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100) + (fift * 50) + (ten * 10) + (five * 5))) / 1
+  print 'I' * one
 end
+
+puts old_roman_numeral (4837)
+puts old_roman_numeral (1887)
+puts old_roman_numeral (625)
+puts old_roman_numeral (12)

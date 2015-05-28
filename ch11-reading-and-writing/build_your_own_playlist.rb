da62b59e72c2292caac1d30302f4195fec172671
Gerard Morera <grdmorera@gmail.com>
2015-05-28 21:32:00 +0200

    build your on playlist done

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,15 @@
-# your code here
+class Playlist
+
+  def tracks
+      Dir['**/*.mp3'].shuffle
+  end
+
+  def reproduce 
+    File.open 'playlist.mp3', 'w' do |f|
+      tracks.each do |track|
+        f.write track
+      end
+    end
+  end
+
+end

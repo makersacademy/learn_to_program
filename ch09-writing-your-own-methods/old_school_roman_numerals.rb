c67c08fa6776b27325098d0633ebed1acffb012b
MassimilianoMura <massimilianomura1983@gmail.com>
2015-05-28 17:32:50 +0100

    Shorter version, modulus method added

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,21 +1,24 @@
 def old_roman_numeral num
   thous = (num / 1000)
   print 'M' * thous
-  fivhund = (num - (thous * 1000)) / 500
+  fivhund = (num % 1000) / 500
   print  'D' * fivhund
-  hundred = (num - ((thous * 1000) + (fivhund * 500))) / 100
+  hundred = (num % 500) / 100
   print 'C' * hundred
-  fift = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100))) / 50
+  fift = (num % 100) / 50
   print 'L' * fift
-  ten = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100) + (fift * 50))) / 10
+  ten = (num % 50) / 10
   print 'X' * ten
-  five = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100) + (fift * 50) + (ten * 10))) / 5
+  five = (num % 10) / 5
   print 'V' * five
-  one = (num - ((thous * 1000) + (fivhund * 500) + (hundred * 100) + (fift * 50) + (ten * 10) + (five * 5))) / 1
+  one = (num % 5) / 1
   print 'I' * one
 end
 
-puts old_roman_numeral (4837)
-puts old_roman_numeral (1887)
-puts old_roman_numeral (625)
+puts old_roman_numeral (2500)
+puts old_roman_numeral (1725)
+puts old_roman_numeral (1628)
+puts old_roman_numeral (751)
+puts old_roman_numeral (51)
 puts old_roman_numeral (12)
+puts old_roman_numeral (3)

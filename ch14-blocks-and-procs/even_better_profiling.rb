7dc446234f60535cad7fd1f3c3e59c8133a05a93
Gerard Morera <grdmorera@gmail.com>
2015-05-29 17:51:56 +0200

    better_profiling done

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,1 +1,12 @@
-# your code here
+TO_PROFILE = true
+
+def profile block_description, &block
+  if TO_PROFILE
+    start_time = Time.new
+    block
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block
+  end
+end

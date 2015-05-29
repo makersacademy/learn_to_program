6c1f43f140ea6bc01855e68bd727e52803139fd1
Gerard Morera <grdmorera@gmail.com>
2015-05-29 15:47:52 +0200

    starting eith orange tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,1 +1,19 @@
-# your code here
+class OrangeTree
+
+  attr_reader :orange_count, :age, :heigh
+
+  def initialize
+    @age          = age
+    @orange_count = orange_count
+    @heigh        = heigh
+  end
+
+  def one_year_passes
+    
+  end
+
+  
+  def pick_an_orange
+  end
+
+end

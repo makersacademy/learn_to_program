29670356f1d8cd2ad18f497b042f2a08340748ce
TheoLeanse <theoleanse@gmail.com>
2015-05-29 14:36:58 +0100

    adds build_your_own_playlist.rb (ch 11)

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,8 @@
-# your code here
+playlist = Dir["/Users/Theo/Music/*You.mp3"]
+playlist.sort_by!{rand}
+
+File.open "You playlist.m3u", "w" do |f|
+  playlist.each do |track|
+    f.write track + "\n"
+  end
+end

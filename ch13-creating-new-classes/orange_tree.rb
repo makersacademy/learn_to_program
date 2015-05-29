07a61cd26a07c8515eb615a5efcba64272d4b74c
Gerard Morera <grdmorera@gmail.com>
2015-05-29 16:45:32 +0200

    Orange tree done

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,19 +1,35 @@
 class OrangeTree
 
-  attr_reader :orange_count, :age, :heigh
+  attr_reader :age, :heigh, :orange_count
 
   def initialize
-    @age          = age
-    @orange_count = orange_count
-    @heigh        = heigh
+    @age           = 0
+    @heigh         = 0
+    @orange_count  = 0
   end
 
   def one_year_passes
-    
+    self.age += 1
+    self.orange_count = ( Math.log( age * 100 ) ).ceil if age > 2
+     death if age == 3 
+     
+    age
   end
 
+  def count_the_oranges
+    orange_count 
+  end
   
   def pick_an_orange
+    self.orange_count -= 1
   end
+
+  def death
+    self.nil
+  end
+
+private
+
+  attr_writer :age, :heigh, :orange_count
 
 end

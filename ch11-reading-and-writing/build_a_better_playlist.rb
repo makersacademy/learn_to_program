8fad9f93c8ec483a89d290d3fd5bf5dbda897439
Gerard Morera <grdmorera@gmail.com>
2015-05-29 19:45:57 +0200

    build a better playlist done

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,15 @@
-def music_shuffle filenames
-  # your code here
+class Playlist
+
+  def tracks
+      Dir['**/*.mp3'].sort_by{ rand }
+  end
+
+  def reproduce 
+    File.open 'playlist.mp3', 'w' do |f|
+      tracks.each do |track|
+        f.write track
+      end
+    end
+  end
+
 end

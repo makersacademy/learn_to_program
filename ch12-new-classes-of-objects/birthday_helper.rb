f529df915ff77d78741b09e7199b2110c5e79ee0
TheoLeanse <theoleanse@gmail.com>
2015-05-29 18:46:13 +0100

    adds chapter 12 exercises

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,11 @@
-# your code here
+birthday_book = {}
+
+File.readlines("birthdays.txt").each do |line|
+  name, date, year = line.split(",")
+  birthday_book[name] = date
+end
+
+puts "Who?"
+name = gets.chomp
+
+p "#{birthday_book[name]}'s birthday will be on #{date}!"
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,11 @@
-# your code here
+p "Which year were you born in?"
+year = gets.chomp
+p "...and which month?"
+month = gets.chomp
+p "...and which day?"
+day = gets.chomp
+
+birthday = Time.local(year, month, day)
+years = (Time.now - birthday) / 3.15569e7
+age = years.to_i
+age.times { p "SPANK!"}

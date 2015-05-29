155e6beee95c0ff1f0f1b27c51506db0ac62383a
Gerard Morera <grdmorera@gmail.com>
2015-05-29 19:20:36 +0200

    program logger finished

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,1 +1,17 @@
-# your code here
+def log string_description, &block
+  puts string_description
+  block.call
+  puts "End of the method"
+end
+
+log "Inside the method" do 
+  puts "Inside first block"
+  log "In the method again" do
+    "hello"
+  end
+end
+
+log "To the method again" do
+  puts "Inside the inner block"
+end
+

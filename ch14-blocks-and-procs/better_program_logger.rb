58f2a7d207d815092e90cf0ab4cfe281dbf76d93
Gerard Morera <grdmorera@gmail.com>
2015-05-29 19:29:16 +0200

    better program logger finished

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,1 +1,24 @@
-# your code here
+$logger_depth = 0
+
+def log string_description, &block
+  indent = ' ' * $logger_depth
+  puts indent + string_description
+
+  $logger_depth = $logger_depth + 4
+
+  block.call
+  
+  $logger_depth = $logger_depth - 4
+  puts indent + "End of the method"
+end
+
+log "Inside the method" do 
+  puts "Inside first block"
+  log "In the method again" do
+    "hello"
+  end
+end
+
+log "To the method again" do
+  puts "Inside the inner block"
+end

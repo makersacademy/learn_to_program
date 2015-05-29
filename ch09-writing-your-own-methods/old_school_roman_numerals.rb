feb126a784654813281a9f9463e63e7dc05f9c57
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-05-29 15:49:35 +0100

    Add Chapter 9, ex2 answer

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,23 @@
 def old_roman_numeral num
-  # your code here
+
+  numeral_M = ""
+  numeral_D = ""
+  numeral_C = ""
+  numeral_L = ""
+  numeral_X = ""
+  numeral_V = ""
+  numeral_I = ""
+
+  numeral_M = num / 1000
+  numeral_D = num % 1000 / 500
+  numeral_C = num % 500 / 100
+  numeral_L = num % 100 / 50
+  numeral_X = num % 50 / 10
+  numeral_V = num % 10 / 5
+  numeral_I = num % 5 / 1
+
+  puts "M"*numeral_M + "D"*numeral_D + "C"*numeral_C + "L"*numeral_L + "X"*numeral_X + "V"*numeral_V + "I"*numeral_I
+
 end
+
+puts(old_roman_numeral(1501))

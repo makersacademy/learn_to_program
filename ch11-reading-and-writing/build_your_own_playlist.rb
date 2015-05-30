a103ddc61c9c7a55c05abb25cc766355ec2d8f4d
Michael Arnott <arnottmj@aol.com>
2015-05-30 02:52:16 +0100

    added playlist and picture downloading

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,29 @@
-# your code here
+def shuffle arr
+
+  shuf = []
+
+  x = ""
+
+  while arr.length > 0
+
+    x = arr.sample
+    arr.delete(x)
+    shuf.push(x)
+
+  end
+
+  shuf
+
+end
+
+filename = 'playlist.m3u'
+
+music_list = Dir["/Users/michaelarnott/Music/iTunes/**/*.{aac,mp3}"]
+
+shuffled_music = shuffle(music_list)
+
+shuffled_music.each do |iterator|
+  File.open filename, 'a' do |f|
+    f.write iterator + "\n" #could have used f.puts and then wouldnt need to create space
+  end
+end

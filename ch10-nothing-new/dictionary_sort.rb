b4de1479fc3114263fb50512d152738b98a5b5fc
Natalia Antonova <5555482@gmail.com>
2015-05-30 21:58:58 +0100

    sort arr

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,9 +1,22 @@
-def sort arr
-return arr 
-if arr.length <= 1
-  middle = arr.pop
-  less   = arr.select{|x| x <  middle}
-  more   = arr.select{|x| x >= middle}
-  sort(less) + [middle] + sort(more)
+def sort arr 
+	rec_sort arr, []
 end
-puts(sort(['can','feel','singing','like','a','can']).join(' '))
+def rec_sort unsorted, sorted 
+	if unsorted.length <= 0
+return sorted 
+end
+# So if we got here, then it means we still # have work to do.
+smallest = unsorted.pop
+still_unsorted = []
+unsorted.each do |tested_object| 
+	if tested_object < smallest
+            still_unsorted.push smallest
+            smallest = tested_object
+else
+            still_unsorted.push tested_object
+end 
+end
+sorted.push smallest
+  rec_sort still_unsorted, sorted
+end
+puts(sort(['can','feel','singing','like','a','can']))

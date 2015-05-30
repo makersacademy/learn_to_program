7ad449e8ad657e60661c466584e615b82739114a
Michael Arnott <arnottmj@aol.com>
2015-05-30 04:24:41 +0100

    added better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,54 @@
 def music_shuffle filenames
-  # your code here
+
+  split_names = []
+  x = ""
+  shuf = []
+  counter = 1
+  flag = 0
+  final_shuf = []
+
+  filenames.each do |first|
+    split_names << first.split('/')
+  end
+
+  x = split_names.sample
+  shuf.push x
+  split_names.delete(x)
+
+  while split_names.length > 0
+
+    x = split_names.sample
+
+    if x[-2] != shuf[(counter-1),-2]
+      shuf.push x
+      flag = 0
+      counter = counter + 1
+      split_names.delete(x)
+    elsif flag == 3
+      shuf.push.x
+      flag = 0
+      coutner = counter + 1
+      split_names.delete(x)
+    else
+      flag = flag + 1
+    end
+
+  end
+
+  shuf.each do |second|
+    final_shuf.push(second.join("/"))
+  end
+  final_shuf
+end
+
+filename = 'playlist.m3u'
+
+music_list = Dir["/Users/michaelarnott/Music/iTunes/**/*.{aac,mp3}"]
+
+shuffled_music = music_shuffle(music_list)
+
+shuffled_music.each do |iterator|
+  File.open filename, 'a' do |f|
+    f.write iterator + "\n"
+  end
 end

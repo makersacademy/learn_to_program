c0eab126bd65c338d1eacaafb31783f1940596d3
Natalie Akam <natalie.akam@gmail.com>
2015-05-30 10:35:35 +0000

    Added sort program

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,23 @@
 def sort arr
-  # your code here
+  sorted = []
+
+  for counter in (1..arr.length) do
+    array_last = arr.length - 1
+    smallest = arr[0]
+    smallest_pos = 0
+    for num in (1..array_last) do 
+      if (smallest > arr[num])
+        smallest = arr[num]
+        smallest_pos = num
+      end 
+    end
+    arr.delete_at(smallest_pos)
+    sorted << smallest
+    end
+  return sorted
 end
+
+
+
+
+puts sort ["nat", "zoo", "ball", "apple", "win", "lose", "bee", "stu", "alpha", "beta", "nat"]

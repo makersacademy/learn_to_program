ff47197a1db4f042d39b25078a27b434dc34078a
Gerard Morera <grdmorera@gmail.com>
2015-05-31 17:21:23 +0200

    interactive baby dragon done

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,17 @@
-# your code here
+class Dragon
+
+  def self.reproduce
+    puts "How will be baby dragon called?"
+    name = gets.chomp
+    Dragon.new name
+  end
+
+  def action action_name
+    object = Object.new
+    if respond_to? action_name and not object.respond_to? action_name
+      send :action_name
+    else
+      puts "#{@name} can not do this!"
+    end
+
+  end

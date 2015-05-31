dbdbf679edfe5f39aea94bfdefdbebd94489ac76
Gerard Morera <grdmorera@gmail.com>
2015-05-31 13:15:43 +0200

    birth_helepr improved

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -3,16 +3,26 @@ class BirthdayHelper
   def get_birth_date 
     puts "This is a birthday helper."
     puts "Give me a name, baby."
-    parser person_name = gets.chomp
+    birth_date_striped person_name = gets.chomp
   end
 
-  def parser person_name, file_name = "./BirthDates.rb"
+private
+
+  def birth_date_striped person_name, file_name = "./BirthDates.rb"
     File.open file_name do |file|
-      file.each do |line| 
-        name, month_day, year = line.split "," 
-        return [month_day, year].join if name == person_name
-      end
+      parser file, person_name
     end
+  end
+
+  def parser file, person_name
+    date = []
+
+    file.each do |line| 
+      name, month_day, year = line.split "," 
+      date = [month_day, year] if name == person_name
+    end
+
+    date.join " ,"
   end
 
 end

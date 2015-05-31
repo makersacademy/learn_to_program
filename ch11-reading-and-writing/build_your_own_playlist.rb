da5388c15cb0d0f62dfc8f85d0f5311b9982c3d7
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-05-31 16:54:07 +0100

    Add Chapter 11, update to ex2 and add ex3

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,4 +1,4 @@
-Dir.chdir '/Users/loliphant/Documents/Projects/playlist'
+Dir.chdir '/Users/loliphant/Documents/Projects/playlist2'
 
 # find all music files
 # audio_files = Dir['*.{mp3}']
@@ -6,11 +6,11 @@ Dir.chdir '/Users/loliphant/Documents/Projects/playlist'
 # audio_string1 = "/Users/loliphant/Documents/Projects/playlist/Introduction.mp3\n"
 # audio_string2 = "/Users/loliphant/Documents/Projects/playlist/MindfulnessOfBreathing.mp3\n"
 # audio_string3 = "/Users/loliphant/Documents/Projects/playlist/JustBeing.mp3\n"
+# audio_string4 = "/Users/loliphant/Documents/Projects/playlist/mozart-clarinet-concerto-2-piano-and-clarinet.mp3\n"
+# audio_string5 = "/Users/loliphant/Documents/Projects/playlist/edvard-grieg-peer-gynt1-morning-mood-piano.mp3\n"
 #
 # File.open filename, 'w' do |f|
-#   f.write audio_string1
-#   f.write audio_string2
-#   f.write audio_string3
+#   f.write audio_string1 + audio_string2 + audio_string3 + audio_string4 + audio_string5
 # end
 
 
@@ -37,7 +37,7 @@ def shuffle(arr)
   shuf
 end
 
-filename = 'Playlist.m3u'
+filename = 'Playlist2.m3u'
 all_audio = shuffle(Dir['*.mp3'])
 
 File.open filename, 'w' do |f|

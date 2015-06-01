3d86e8b4c9cf52a5d6fe9701f1cf622796f5eca1
Alex <aleksandernasr@gmail.com>
2015-06-01 19:11:16 +0100

    Psych questionnaire

---
+++ ch09-writing-your-own-methods/Methods.rb
@@ -0,0 +1,12 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    return true if reply == 'yes'
+    return false if reply == 'no'
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+puts(ask('Do you like eating tacos?'))

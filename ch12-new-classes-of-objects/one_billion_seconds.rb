c90da87005f00abaf1975babb98177495b86df52
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-01 22:23:39 +0100

    Update to Chapter 12, ex1

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,6 +1,6 @@
 # find out the exact second you were born and find out when you'll turn 1 billion seconds old
 
-
-sec_til_bil = Time.new(1983, 05, 21, 23, 32, 8) + 10**9
+born_at = Time.local(1983, 05, 21, 23, 32, 8)
+sec_til_bil = born_at + 1000000000
 puts "You will be one billion seconds old on #{sec_til_bil}"
 puts "Scratch that sorry, you were one billion seconds old on #{sec_til_bil}"

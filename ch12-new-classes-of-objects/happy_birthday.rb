85e1b51097294bc3430d55bf10f4e206feb42c23
Elin <elinnet@gmail.com>
2015-06-01 21:47:54 +0100

    Three completed exercises for Chpt 12.  One oustanding.

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,24 @@
-# your code here
+
+# #Happy Birthday
+
+puts "What year were you born?"
+
+birthYear = gets.chomp.to_i
+
+puts "What month were you born? (1-12)"
+
+birthMonth = gets.chomp.to_i
+
+puts "What date were you born?"
+
+birthDate = gets.chomp.to_i
+
+t = Time.new
+
+date_string = Time.local(birthYear,birthMonth,birthDate)
+puts date_string
+
+
+years_passed = (t - date_string).to_i/60/60/24/365
+
+puts "SPANK\n" * years_passed
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,1 +1,43 @@
-# your code here
+
+
+def roman_to_integer roman
+
+  #create has so that roman numbers can be referenced to figures
+  digit_vals = {'i' => 1,
+                'v' => 5,
+                'x' => 10,
+                'l' => 50,
+                'c' => 100,
+                'd' => 500,
+                'm' => 1000,}
+
+  total = 0
+  prev = 0
+  index = roman.length - 1 #guides us to look at each roman no.
+
+  while index >= 0 #for each roman no., do the following
+    c = roman[index].downcase #look at the roman number, change downcase
+    val = digit_vals[c] #look up the hash to find its value
+    index = index - 1 #reduce index counter by one
+
+    if !val
+      puts 'This is not a valid roman number!'
+      return
+    end
+
+    if val < prev
+      val = val * -1 #if the number currently working with is less than the previous one added to 'total' then deduct this from 'total'
+    else
+      prev = val #number currently looking at is not smaller than the previous one added to total
+    end
+
+    total = total + val
+  end
+
+  total
+
+end
+
+puts roman_to_integer 'MCMLXIV'
+puts roman_to_integer 'MMMCCXLI'
+puts roman_to_integer 'LI'

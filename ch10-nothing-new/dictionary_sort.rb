08166f5ced6014e679825ebec13443be897c3e94
Robert Morgan <robbie.morgan@gmail.com>
2015-06-02 18:34:31 +0100

    Chapter 10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+
+if array.length <=1
+	return array
+else
+
+	test = array.pop
+	less=[]
+	greater=[]
+
+	array.each do |x|
+		if x.downcase<=test.downcase
+			less<<x
+		else
+			greater<<x
+		end
+	end
+
+	return dictionary_sort(less) + [test] + dictionary_sort(greater)
 end
+end
+
+puts dictionary_sort(['this','is','just','A','Test'])
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,23 @@
-def sort arr
-  # your code here
+def sort array
+
+if array.length <=1
+	return array
+else
+
+	test = array.pop
+	less=[]
+	greater=[]
+
+	array.each do |x|
+		if x <=test
+			less<<x
+		else
+			greater<<x
+		end
+	end
+
+	return sort(less) + [test] + sort(greater)
 end
+end
+
+puts sort(['this','is','just','a','test'])

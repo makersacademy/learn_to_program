0f8e7ad1b0cf79990d6d2563f0630e72ea7b5f5f
Simon Legg <leggsimon@gmail.com>
2015-06-02 22:17:03 +0100

    Added solutions to Chapter 10 - english_number.rb & Chapter 11 - safer_picture_downloading.rb

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,67 @@
-def english_number number
-  # your code here
+def english_number(number)
+  if number == 0
+    return "zero"
+  end
+
+  if number < 0
+    puts "Positive numbers only at the moment please. Maybe in the future."
+  end
+
+  three_number = number.to_s.reverse.each_char.each_slice(3).map{|x| x.reverse.join.to_i}
+
+  names = [nil,'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion', 'quindecillion']
+
+  three_number.map.with_index do |x, i|
+    if x != 0
+      [other(x), names[i]].compact.join(" ")
+    end
+  end.reverse.compact.join(" ")
+
+
 end
+
+def other(number)
+
+  eng_no = ""
+
+  singlePlace = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine",]
+  teenagers = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  tenPlace = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+
+  if number >= 100
+    hundreds = number / 100
+    number = number - (hundreds * 100)
+    eng_no = singlePlace[hundreds - 1] + " hundred"
+    if
+      number > 0
+      eng_no = eng_no + " "
+    end
+  end
+
+  if (number == 10) or (number >= 20)
+    tens = number / 10
+    number = number - (tens * 10)
+    eng_no = eng_no + tenPlace[tens - 1].to_s
+    if number > 0
+      eng_no = eng_no + "-"
+    end
+  end
+
+  if (number > 10) && (number < 20)
+    teens = number - 10
+    number = number - (teens + 10)
+    eng_no = eng_no + teenagers[teens - 1]
+  end
+
+
+  if number > 0
+    eng_no = eng_no + singlePlace[number - 1].to_s
+  end
+
+
+  eng_no
+end
+
+puts english_number(1000)
+puts english_number(10000000000)
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,27 @@
-# your code here
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox'
+
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files:  "
+
+pic_number = 1
+pic_names.each do |name|
+  print '.'
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  if new_name.exist?
+    puts "This file name already exists!"
+    exit
+  end
+
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end
+puts
+puts "Done!"

a3022bb962709ef4551c03dca15e7ce9c6779c26
Robert Morgan <robbie.morgan@gmail.com>
2015-06-02 18:05:43 +0100

    Chapter 9 Exercises

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral (n)
+ 
+ thousand = 'M' * (n/1000)
+ five_h = 'D' *(n%1000/500)
+ hundred = 'C' *(n%500/100)
+ fifty = 'L' *(n%100/50)
+ ten = 'X' *(n%50/10)
+ five = 'V' *(n%10/5)
+ one = 'I' *(n%5/1)
+
+answer = thousand+five_h+hundred+fifty+ten+five+one
+
+answer
 end
+
+puts old_roman_numeral(1999)
+
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,44 @@
-def roman_numeral num
-  # your code here
+def roman_numeral n 
+  thous = (n / 1000)
+  hunds = (n % 1000 / 100)
+  tens  = (n % 100 / 10)
+  ones  = (n % 10)
+
+  roman = 'M' * thous
+
+  if hunds == 9
+    roman = roman + 'CM'
+  elsif hunds == 4 
+    roman = roman + 'CD'
+  else
+    roman = roman + 'D' * (n % 1000 / 500)
+    roman = roman + 'C' * (n % 500 / 100) 
+  end
+
+  if tens == 9 
+    roman = roman + 'XC'
+  elsif tens == 4 
+    roman = roman + 'XL'
+  else
+    roman = roman + 'L' * (n % 100 / 50)
+    roman = roman + 'X' * (n % 50 / 10) 
+  end
+
+  if ones == 9 
+    roman = roman + 'IX'
+  elsif ones == 4 
+    roman = roman + 'IV'
+  else
+    roman = roman + 'V' * (n % 10 / 5)
+    roman = roman + 'I' * (n % 5 / 1) 
+  end
+  roman
 end
+
+puts roman_numeral(1999)
+
+puts roman_numeral(1444)
+
+puts roman_numeral(6)
+
+puts roman_numeral(1777) 

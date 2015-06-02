60cc851947dde9c96567b3749328515882b93d63
DuskyShelf <dupsdale@gmail.com>
2015-06-02 17:52:23 +0100

    added dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,18 @@
-def dictionary_sort arr
-  # your code here
+def sort array
+	puts recursive_sort array, []
 end
+
+def recursive_sort unsorted, sorted
+while true
+	if unsorted.empty?
+		return sorted
+	end
+
+	downcase = unsorted.map(&:downcase)
+	sorted << unsorted.delete_at(downcase.index(downcase.min))
+	recursive_sort unsorted, sorted
+end
+end
+
+list = ['Hello', 'hella', 'ABCD', 'aaaa', 'aBaa', 'Abaa']
+sort list

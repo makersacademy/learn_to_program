3b66996b0487f1b37a01683c4cf7b451d5af3c21
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-02 11:18:32 +0100

    Added code for build a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,37 @@
 def music_shuffle filenames
-  # your code here
+  filenames = filenames.sort
+  length    = filenames.length
+
+  2.times do |variable|
+    l_idx = 0
+    r_idx = length/2
+    shuffled = []
+
+    while shuffled.length < length
+      if shuffled.length%2 == 0
+        shuffled.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        shuffled.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+
+    filenames = shuffled
+  end
+
+  array = []
+  cut = rand(length)
+  idx = 0
+
+  while idx < length
+    array.push(filenames[(idx+cut)%length])
+    idx = idx + 1
+  end
+  
+  array  
 end
+
+songs = Dir['/Users/Kate/desktop/somemusicfiles/*.m4a']
+
+puts(music_shuffle(songs))

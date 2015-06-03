0ab5baa1ab7024c2baef7eee26044a8d24151b62
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-03 10:27:02 +0100

    Added code for program logger & better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,1 +1,20 @@
-# your code here
+$log_depth = 0
+
+def log description, &block
+  prefix = '  '*$log_depth
+  puts prefix + 'beginning "' + description + ' "...'
+  $log_depth = $log_depth + 1
+  result = block.call
+  $log_depth = $log_depth - 1
+  puts prefix + '..."' + description + '" finished, returning: ' + result.to_s
+end 
+
+log 'first block' do 
+  log 'second block' do
+    'I\'m the result of the second block'
+  end
+  log 'third block' do
+    'Hi, I\'m the result of the third block'
+  end
+  'I\'m the result of the first block'
+end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,1 +1,15 @@
-# your code here
+def log description, &block
+  puts 'beginning "' + description + ' "...'
+  result = block.call
+  puts '..."' + description + '" finished, returning: ' + result.to_s
+end 
+
+log 'first block' do 
+  log 'second block' do
+    'I\'m the result of the second block'
+  end
+  log 'third block' do
+    'Hi, I\'m the result of the third block'
+  end
+  'I\'m the result of the first block'
+end

7a5572f133faaa74b32873eca1d3fd50e3d335df
Elin <elinnet@gmail.com>
2015-06-03 16:14:15 +0100

    Commit of remaining Chpt 12 exercise - birthday_helper.rb

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,22 @@
-# your code here
+#BIRTHDAYHELPER
+hash = {
+
+"Christopher Alexander" => ["Oct", 4, 1936],
+"Christopher Lambert" => ["Mar", 29, 1957],
+"Christopher Lee" => ["May", 27, 1922],
+"Christopher Lloyd" => ["Oct", 22, 1938],
+"Christopher Pine" => ["Aug", 3, 1976],
+"Christopher Plummer" => ["Dec", 13, 1927],
+"Christopher Walken" => ["Mar", 31, 1943],
+"The King of Spain" => ["Jan", 5, 1938]
+}
+
+puts "Whose birthday are you interested in?"
+name = gets.chomp
+
+bdate = hash[name]
+year = bdate[2].to_i
+year_now = (Time.now).year
+age_now = year_now -year
+
+puts "He is #{age_now} years old"

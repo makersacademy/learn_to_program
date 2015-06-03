a4902b86ccab1ec0cbb835c43cf7161ad5c52cc3
DuskyShelf <dupsdale@gmail.com>
2015-06-03 14:38:35 +0100

    Completed happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,11 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp
+puts "What month were you born?"
+month = gets.chomp
+puts "What day were you born?"
+day = gets.chomp
+
+Age = Time.now - Time.new(year, month, day)
+
+puts "You are " + (Age / 31536000).floor.to_s + " years old."
+puts "SPANK! " * (Age / 31536000).floor

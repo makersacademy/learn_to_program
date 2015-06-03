2f2b6d13b0053c3e6aed225eea269ba2896c09b1
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-03 09:41:24 +0100

    Added code for grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,1 +1,22 @@
-# your code here
+def grandfather_clock block_description, &block
+  hour_now = Time.new.hour
+    if hour_now >= 13
+      hour_now = hour_now - 12
+      hour_now.times do
+        block.call
+      end
+    elsif hour_now == 0
+      hour_now = 12
+      hour_now.times do
+        block.call
+      end
+    else
+      hour_now.times do
+        block.call
+      end
+    end
+end
+
+grandfather_clock 'Chime' do
+  puts 'DONG!'
+end

3d438449d0415803e1a5f0151a552249342fe3b3
Robert Morgan <robbie.morgan@gmail.com>
2015-06-03 18:20:24 +0100

    Chapter 14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,27 @@
+$logger_depth =0
+
 def log desc, &block
-  # your code here
+  indent = ' '*$logger_depth
+  puts indent + 'Beginning "' + desc + '"...'
+  $logger_depth+=1
+  result = block.call
+  puts indent + '..."' + desc + '" finished, returning: ' + "\n" + indent + result.to_s
 end
+
+log 'outer block' do
+  log 'some little block' do
+  	log 'teeny-tiny block' do
+  		"LOTS OF LOVE".downcase
+  	end
+    6*7
+  end
+
+  log 'yet another block' do
+    'I love Indian food!'
+  end
+
+  5 == 5
+end
+
+
+
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,20 @@
 def log desc, &block
-  # your code here
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + "\n" + result.to_s
 end
+
+log 'outer block' do
+  log 'some little block' do
+    3+2
+  end
+
+  log 'yet another block' do
+    'I like Thai food!'
+  end
+
+  5 == 0
+end
+
+
+

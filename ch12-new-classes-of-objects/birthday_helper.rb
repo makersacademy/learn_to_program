7905cd1e7b887aa3af9ca25cacfb2b0cf263dd0c
DuskyShelf <dupsdale@gmail.com>
2015-06-03 16:02:53 +0100

    Added birthday_list.txt and completed birthday_helper.rb

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,19 @@
-# your code here
+# Have used a local file for this example. Not sure how to broaden
+
+Dir.chdir '/Users/DuskyShelf/projects/learn_to_program/ch12-new-classes-of-objects'
+
+contents = File.read('birthday_list.txt')
+
+hashed_birthdays = {}
+
+contents.each_line do |i|
+  separator = i.index(',')
+  name = i[0, (separator)]
+  birthday = i[((separator + 2) - i.length) .. - 1]
+hashed_birthdays[name] = birthday
+end
+
+puts "Whose birthday do you want to know?"
+name = gets.chomp
+
+puts hashed_birthdays[name]

a5e393c8596ee7598d10d5f775eafecb4b36684f
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-03 22:29:27 +0100

    Add chapter 12, ex3 & 4

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,39 @@
-# your code here
+# require 'yaml'
+
+# filename = 'birthday.txt'
+#
+# File.open filename, 'w' do |f|
+#   f.write test_string
+# end
+#
+# read_string = File.read filename
+#
+# puts(read_string == test_string)
+
+# load in birthdays.txt file
+birthdays = {}
+File.read('birthdays.txt').each_line do |line|
+    line = line.chomp
+    # find the index of the first comma, to establish where the name ends
+    first_comma = 0
+    while line[first_comma] != ',' && first_comma < line.length
+      first_comma += 1
+    end
+
+    name = line[0..(first_comma -1)]
+    date = line[-12..-1]
+
+    birthdays[name] = date
+end
+
+# Ask user whose birthday they want to know
+
+puts "Whose birthday would you like to know?"
+name = gets.chomp
+date = birthdays[name]
+
+if date == nil
+  puts "we don't have a record of that person"
+else
+  puts date[1..5]
+end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,37 @@
 def roman_to_integer roman
-  # your code here
+  # creates hash with numeric values for each numeral
+  digit_vals = {'i' => 1, 'v' => 5, 'x' => 10, 'l' => 50, 'c' => 100, 'd' => 500, 'm' => 1000}
+
+  # sets 2 new variables to point to 0
+  total = 0
+  prev = 0
+
+  # passes each character in str roman (reversed), into the block - so last character evaluated first
+  roman.reverse.each_char do |c_or_C|
+    # call downcase method on object 'c_or_C' variable is pointing to on each iteration, and pass into variable 'c'
+    c = c_or_C.downcase
+    # accesses integer value of item in hash, by passing in the the key
+    val = digit_vals[c]
+    # if val is false
+    if !val
+      puts 'This is not a valid roman numeral!'
+      # return statement needed here to stop the rest of the code from executing and output the error msg
+      return
+    end
+    # if 'val' is less than 'prev' * -1 to 'val'
+    if val < prev
+      val *= -1
+    else
+      # if 'val' is not less than 'prev', set 'prev' to value of 'val'
+      prev = val
+    end
+    # adds current 'val' value to 'total'
+    total += val
+    # continue w iteration for next character
+  end
+  # returns total
+  total
 end
+
+puts roman_to_integer('12MCMXCIX')
+puts roman_to_integer('MCMXCIX')

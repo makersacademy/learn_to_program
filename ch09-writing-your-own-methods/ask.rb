a7add7555d3de4b52a190421f39702283b6e7e25
kirsten jones <katsuraku@users.noreply.github.com>
2015-06-03 15:36:54 +0100

    Update ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,27 @@
 def ask question
-  # your code here
-end
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    return true if reply == 'yes' # these return values go nowhere until
+    return false if reply == 'no' # line 16 where a variable saves the return value
+    puts 'Please answer "yes" or "no"'
+    end
+ end
+ puts 'Hello and thank you for taking part in this questionnaire.'
+ puts 
+
+ask 'Do you like eating tacos?'  # So this return value gets ignored
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?' # using variable= saves the return value
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...!'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+
+puts 
+puts 'DEBRIEFING:'
+puts 'Thank you for answering these questions.'
+puts
+puts wets_bed

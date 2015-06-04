4478da68feecb2cd9cfe783e77fa044187408d34
Natalie Akam <natalie.akam@gmail.com>
2015-06-04 16:44:27 +0000

    Updated shuffle.rb but it is failing to pass rspec test

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,8 @@
 def shuffle arr
-  # your code here
+  shuffled_arr = []
+  arr.each do |x|
+    pos = rand(0..shuffled_arr.length) 
+    shuffled_arr.insert(pos, x)
+  end
+  return shuffled_arr
 end

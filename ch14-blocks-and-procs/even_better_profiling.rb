607cbd900592ee5ee5bf44c000e649956c23b48e
Elin <elinnet@gmail.com>
2015-06-04 20:09:30 +0100

    Commit two Chpt 14 exercises

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,1 +1,17 @@
-# your code here
+#Even better profiling - turn on/off
+
+def profile block_description, &block
+  #set profiling to true/false
+
+  profiling_on = false
+
+  if profiling_on
+    start_time = Time.new
+    block.call
+
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
+end
--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,1 +1,21 @@
-# your code here
+# Grandfather clock
+
+def grandfather_clock &block
+  hour = Time.new.hour
+
+  if hour >= 13
+    hour = hour -  12
+  end
+
+  if hour == 0
+    hour = 12
+  end
+
+  hour.times do
+    block.call
+  end
+end
+
+grandfather_clock do
+  puts 'DONG!'
+end

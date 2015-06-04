dd4ba8790a6c2bb725915757c854faf7d27da011
kirsten jones <katsuraku@users.noreply.github.com>
2015-06-04 13:13:15 +0100

    Update one_billion_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,7 @@
-# your code here
+
+puts "The date I turned one billion seconds old:"
+puts Time.local(1982, 2, 10) + 1000000000
+
+puts 
+puts "...and that date in GMT/UTC:"
+puts Time.gm(1982, 2, 10) + 1000000000

c659a2fde0e5db26e6e824a619f5756c634e8ecb
DuskyShelf <dupsdale@gmail.com>
2015-06-04 17:50:56 +0100

    shuffle.rb completed

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,12 @@
 def shuffle arr
-  # your code here
+  shuffled_arr = []
+
+  while arr.length > 0
+  shuffled_arr << arr.delete_at(rand(arr.length))
+  end
+
+  shuffled_arr
+
 end
+
+puts shuffle [1,2,3,4,5,6,7,8]

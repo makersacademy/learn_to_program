ed959404cdf1f08790c6bf24f61d55a868e26713
kirsten jones <katsuraku@users.noreply.github.com>
2015-06-04 14:15:56 +0100

    Update better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,28 @@
+$logger_depth = 0
+
 def log desc, &block
-  # your code here
+  prefix = '  '*$logger_depth
+
+  puts prefix + "I'm about to call" + desc + " now..."
+  
+  $logger_depth = $logger_depth + 1
+  result = block.call
+  
+  $logger_depth = $logger_depth - 1
+  puts prefix + "..." + desc + " finished, returning: " + result.to_s
+ 
+  puts "This line runs whenever the method ends." 
+  # I added this because I don't get the way the method works. 
+end
+
+log "block1" do
+  log " a block within a block" do
+	2 + 2
+  end
+
+  log "another tiny block" do
+  	"This program is weird"
+  end
+
+  "0" == 0
 end

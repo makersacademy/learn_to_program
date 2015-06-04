bc41938cb6d51d64c1c67802784f738fbdc422ac
Natalie Akam <natalie.akam@gmail.com>
2015-06-04 15:42:13 +0000

    Updated old_school_roman_numerals.rb so it passes rspec test

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -3,8 +3,8 @@ def old_roman_numeral num
   roman_num << 'M' * (num/1000)
   roman_num << 'D' * (num%1000 / 500) 
   roman_num << 'C' * (num%500 / 100) 
-  roman_num << 'X' * (num%100 / 50) 
-  roman_num << 'L' * (num%50 / 10) 
+  roman_num << 'L' * (num%100 / 50) 
+  roman_num << 'X' * (num%50 / 10) 
   roman_num << 'V' * (num%10 / 5)   
   roman_num << 'I' * (num%5)
  # I * num/1

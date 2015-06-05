ed27ffe1bb1357c2dbbd7593525d4d9dd155e27d
Edwardyao123 <zhigang_edward@hotmail.com>
2015-06-05 05:47:47 +0100

    push chapter 14 exercise

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
-def log desc, &block
-  # your code here
+def log description, &block
+    puts 'beginning "' + description + '"...'
+    result = block.call
+    puts '..."' + description + '"finished, returning: ' + result.to_s
+end
+log 'outer block' do
+    log 'some little block' do
+        1**1 + 2**2
+    end
+    
+    log 'yet another block' do
+        '!doof iahT ekil I'.reverse
+    end
+    '0' == 0
 end

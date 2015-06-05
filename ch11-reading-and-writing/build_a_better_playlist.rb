302a7282c6d382e001f176abdd22bcd77e606bb0
Edwardyao123 <zhigang_edward@hotmail.com>
2015-06-05 05:34:57 +0100

    push chapter 11 exercise

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,36 @@
 def music_shuffle filenames
-  # your code here
+    filenames = filenames.sort
+    fileLength = filenames.length
+    
+    2.times do
+        left = 0
+        right = fileLength / 2
+        shuf = []
+        
+        while shuf.length < fileLength
+            if shuf.length % 2 == 0
+                shuf.push(filenames[right])
+                right = right + 1
+                else
+                shuf.push(filenames[left])
+                left = left + 1
+            end
+        end
+        
+        file = shuf
+    end
+    
+    arr = []
+    cut = rand(fileLength)
+    index = 0
+    
+    while index < fileLength
+        arr.push(filenames[(index + cut ) % fileLength])
+        index += 1
+    end
+    arr
 end
+
+songs = ['with me','Hi','goodbye','kiss me','never','go away']
+
+puts music_shuffle(songs)

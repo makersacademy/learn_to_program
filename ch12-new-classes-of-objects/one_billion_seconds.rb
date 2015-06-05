28d76c10b838deedb011516945012c027a89f82e
Edwardyao123 <zhigang_edward@hotmail.com>
2015-06-05 05:40:09 +0100

    push chapter 12 exercise

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,19 @@
-# your code here
+puts Time.gm(1990, 3, 8) + 10**9
+
+puts "which year did you born?"
+bornYear = gets.chomp.to_i
+
+puts "which month did you born?"
+bornMonth = gets.chomp.to_i
+
+puts "which day did you born?"
+bornDay  = gets.chomp.to_i
+
+age = 1
+while Time.local(bornYear + age,bornMonth,bornDay) <= Time.new
+    puts 'SPANK'
+    age = age + 1
+end
+
+
+
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,25 @@
 def roman_to_integer roman
-  # your code here
+    romanToInt = {'i' => 1, 'v' => 5, 'x' => 10, 'l' => 50, 'c' => 100, 'd' => 500, 'm' => 1000}
+    
+    write = 0
+    last = 0
+    
+    roman.reverse.each_char do |alpha|
+        validity =  romanToInt[alpha.downcase]
+        if !validity
+            puts 'This is not a valid roman numeral!'
+            return
+        end
+        if validity < last
+            validity *= -1
+            else
+            last = validity
+        end
+        write += validity
+    end
+    write
 end
+
+puts roman_to_integer('mcmxcix')
+puts roman_to_integer('CCCLXV')
+puts roman_to_integer('fscl')

6904f466c389237231fb445ed87523718fe6cab1
Edwardyao123 <zhigang_edward@hotmail.com>
2015-06-05 05:43:55 +0100

    push chapter 13 exercise

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,21 @@
-# your code here
+puts "What is the name for your dragon?"
+name = gets.chomp
+dragon = Dragon.new name
+while true
+    puts "Key in: /'feed/',/'toss/',/'walk/',/'rock/',/'sleep/',/'exit/'"
+    keyEntered = gets.chomp
+    if keyEntered == 'feed'
+        dragon.feed
+        elsif keyEntered == 'toss'
+        dragon.toss
+        elsif keyEntered == 'walk'
+        dragon.walk
+        elsif keyEntered == 'rock'
+        dragon.rock
+        elsif keyEntered == 'sleep'
+        dragon.put_to_bed
+        else
+        puts "Key in a command please!"
+    end
+end
+

521dbe27ad1e4a159e858eb07a8f1e047985a0e5
Edwardyao123 <zhigang_edward@hotmail.com>
2015-06-05 04:51:32 +0100

    push chapter 9 exercise

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
 def old_roman_numeral num
-  # your code here
+  # your codef old_roman_numeral number
+  	oldRoman = ''
+
+  	oldRoman += 'M' * (num / 1000)
+  	oldRoman += 'D' * (num % 1000 / 500)
+  	oldRoman += 'C' * (num % 500 / 100)
+  	oldRoman += 'L' * (num % 100 / 50)
+  	oldRoman += 'X' * (num % 50 / 10)
+  	oldRoman += 'V' * (num % 10 / 5 )
+  	oldRoman += 'I' * (num % 5 / 1 )
+
+  	return oldRoman
 end
+
+puts old_roman_numeral 1999
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,42 @@
 def roman_numeral num
-  # your code here
-end
+
+  	newRoman = ''
+
+  	newRoman << 'M' * (num / 1000)
+
+  	hundredth  = num % 1000 / 100
+  	tenth 	   = num % 100 / 10
+  	oneth 	   = num % 10
+
+  	if hundredth == 9
+  		newRoman << 'CM'
+  	elsif hundredth == 4
+  		newRoman << 'CD'
+  	else
+  		newRoman << 'D' * (num % 1000 / 500)
+  		newRoman << 'C' * (num % 500 / 100)
+  	end
+
+  	if tenth == 9
+  		newRoman << 'XC'
+  	elsif tenth == 4
+  		newRoman << 'XL'
+  	else
+  		newRoman << 'L' * (num % 100 / 50)
+  		newRoman << 'X' * (num % 50 / 10)
+  	end
+
+  	if oneth == 9
+  		newRoman << 'IX'
+  	elsif oneth == 4
+  		newRoman << 'IV'
+  	else
+  		newRoman << 'V' * (num % 10 / 5)
+  		newRoman << 'I' * (num % 5 / 1)
+  	end
+
+  	return newRoman
+  	
+    end
+
+  puts roman_numeral 2015

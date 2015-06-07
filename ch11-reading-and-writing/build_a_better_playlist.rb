502748b22f610805a52111e44cd01c59af5e9ee6
Natalie Akam <natalie.akam@gmail.com>
2015-06-07 14:29:03 +0000

    Updated playlist programs

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,33 @@
 def music_shuffle filenames
-  # your code here
+
+  filenames = filenames.sort
+  
+  len = filenames.length
+
+  2.times do 
+    shuffled_arr = []
+    left_index = 0
+    right_index = len/2
+
+    while shuffled_arr.length < len
+      if shuffled_arr.length%2 == 0 #even indices
+        shuffled_arr.push(filenames[right_index])
+        right_index += 1
+      else
+        shuffled_arr.push(filenames[left_index])
+        left_index += 1
+      end
+    end
+    filenames = shuffled_arr
+  end
+
+  new_array = []
+  cut = rand(len)
+  ind = 0
+  
+  while ind < len
+    new_array.push(filenames[(ind+cut)% len])
+    ind += 1
+  end
+  new_array
 end
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,4 +1,15 @@
-track_names = Dir['Music/**/*.{mp3,wma}']
+def shuffle arr
+  shuffled_arr = []
+  arr.each do |x|
+    pos = rand(0..shuffled_arr.length) 
+    shuffled_arr.insert(pos, x)
+  end
+  return shuffled_arr
+end
+
+track_names = shuffle Dir['Music/**/*.{mp3,wma}']
+
+puts track_names
 
 filename = 'my_playlist.m3u'
 

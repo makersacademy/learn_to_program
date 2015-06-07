49e788a2fc3c52a79eba9523d47a0d8b38de7d2a
Simon Legg <leggsimon@gmail.com>
2015-06-07 18:12:41 +0100

    Added answers for Chapter 12 happy_birthday.rb & one_billion_seconds.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp
+puts "And month? (in numbers please)"
+month = gets.chomp
+puts "And finally the day?"
+day = gets.chomp
+
+birthday = Time.local(year, month, day)
+age = ((Time.new - birthday) / 31556900).to_i
+age.times do
+  puts "SPANK!"
+end

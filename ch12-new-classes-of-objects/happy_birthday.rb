b64ef5043d5945977f26a4960b1bf8ee13d35a0f
Jonathan Ng <jonngkh@hotmail.com>
2015-06-07 08:57:35 +0100

    completed happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,17 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "Month?"
+month = gets.chomp
+
+puts "Day?"
+day = gets.chomp
+
+b = Time.gm(year, month, day)
+t = Time.new
+
+age = (t - b)/31536000
+
+puts "You are #{age.to_i}-years-old! Here's a spank for every year you've wasted on this world!"
+
+puts ("SPANK! " * age)

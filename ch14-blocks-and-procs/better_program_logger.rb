5f97456a2093ab2154d8e7a406a276e7b62cabca
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-07 22:24:45 +0100

    Add chapter 14, ex4

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,29 @@
+# setting global variables - these you can see from anywhere in your code
+$logger_depth = 0
+
+
 def log desc, &block
-  # your code here
+  prefix = '  '*$logger_depth
+
+  puts "#{prefix}Beginning \"#{desc}\"..."
+
+  $logger_depth += 1
+  result = block.call
+
+  $logger_depth += 1
+  puts "#{prefix}...\"#{desc}\" finished, returning: #{result.to_s}"
+
+end
+
+log 'outer block' do
+
+  log 'some little block' do
+    5
+  end
+
+  log 'yet another block' do
+    "I like Thai food"
+  end
+
+  false
 end

e8df8a09ce3ffa6c00494ad9c3b50262dcfc5f7b
Natalie Akam <natalie.akam@gmail.com>
2015-06-07 20:18:49 +0000

    Updated birthday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,30 @@
-# your code here
+dates = File.read 'dates.txt'
+date_hash = {}
+dates.each_line do |x|
+  name, day, year = x.split(', ')
+  date_hash[name] = Time.gm(year, *(day.split))
+end
+puts date_hash['Christopher Lee']
+
+puts 'Whose birthday would you like to know?'
+name = gets.chomp
+
+bday = date_hash[name]
+
+puts "Oooh, I don't know that one..." if bday == nil
+
+now = Time.new
+age = now.year - bday.year
+
+  if now.month > bday.month || (now.month == bday.month && now.day > bday.day)
+    age += 1
+  end
+  
+  if now.month == bday.month && now.day == bday.day
+    puts "#{name} turns #{age} TODAY!!"
+  else
+    date = bday.strftime "%b %d"
+    puts "#{name} will be #{age} on #{date}."
+  end
+
+  

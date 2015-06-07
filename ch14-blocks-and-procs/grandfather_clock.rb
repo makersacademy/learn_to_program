0efcbda6f2a7d66ec1a2678c72c06b94852164fb
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-07 21:24:01 +0100

    Add chatper 14, ex2

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,19 @@
 def grandfather_clock &block
-  # your code here
+
+    hrs_passed = Time.new.hour
+
+    if hrs_passed <= 12
+      hrs_passed.times do
+        block.call
+      end
+    else
+      clock_face_nums = hrs_passed - 12
+      clock_face_nums.times do
+        block.call
+      end
+    end
+end
+
+grandfather_clock do
+  puts 'DONG'
 end

0a9e74a1216e63e50b52bb036dc7630144b78635
Sam Joseph <tansaku@gmail.com>
2015-06-08 07:20:40 +0100

    makes ask spec resilient to extra code calling the ask method

--- spec/ch09/ask_spec.rb
+++ spec/ch09/ask_spec.rb
@@ -1,16 +1,16 @@
-require './ch09-writing-your-own-methods/ask.rb'
-
 describe 'ask' do
-  it 'returns true when you say yes' do
+  before do
     stub(:puts).with('anything')
     stub(:puts).with('Please answer "yes" or "no".')
     stub(:gets).and_return 'yes'
+    allow_any_instance_of(Kernel).to receive(:gets).and_return 'yes'
+    require './ch09-writing-your-own-methods/ask.rb'
+  end
+  it 'returns true when you say yes' do
     expect(ask('anything')).to be true
   end
   it 'returns false when you say no' do
-    stub(:puts).with('anything')
-    stub(:puts).with('Please answer "yes" or "no".')
-    stub(:gets).and_return 'no'
+    allow_any_instance_of(Kernel).to receive(:gets).and_return 'no'
     expect(ask('anything')).to be false
   end
 end

a96698c448588ef72ab5c8d5794cb1ab9b655eb2
UpstatePedro <piers.m.holden@gmail.com>
2015-07-01 09:57:53 +0100

    Chapter 9 solutions - 1

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,26 @@
 def roman_numeral num
-  # your code here
+  answer=""
+  numerals = ["M", "D", "C", "L", "X", "V", "I"]
+  numbers = [1000, 500, 100, 50, 10, 5, 1]
+  i=0
+  numbers.each do |number|
+    factor = num/number
+    if factor>0
+      if factor <=3
+        #add that many to answer
+        answer+=numerals[i]*factor
+        num-=numbers[i]*factor
+      else
+        answer+=numerals[i]+numerals[i-1]
+        num-=(numbers[i-1]-numbers[i])
+
+      end
+    end
+
+    i+=1
+  end
+  answer
 end
+
+# Test case:
+#roman_numeral 4

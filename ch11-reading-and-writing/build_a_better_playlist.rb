d5a63766fe8d4dc26742bf7bfafcad8713a32b1d
UpstatePedro <piers.m.holden@gmail.com>
2015-07-03 11:11:30 +0100

    Completed Ch11 exercises

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,46 @@
-def music_shuffle filenames
-  # your code here
+#works fine so long as no single album accounts for more than half of the tracks: get lost in recursion trying
+#to find a different album for the next track if this is the case...
+
+trackNames = ["music/Jazz/Monk--Nutty/track08.ogg",
+              "music/Jazz/Monk--London_Collection_1/track05.ogg",
+              "music/Jazz/Monk--Nutty/track13.ogg",
+              "music/Jazz/Monk--Round_Midnight/track02.ogg",
+              "music/Jazz/Monk--Round_Midnight/track14.ogg",
+              "music/Jazz/Monk--Round_Midnight/track15.ogg",
+              "music/Jazz/Monk--Round_Midnight/track08.ogg",
+              "music/Rock/FNM--Who_Cares_A_Lot_2/track02.ogg",
+              "music/Rock/FNM--Who_Cares_A_Lot_2/track08.ogg",
+              "music/Rock/FNM--Who_Cares_A_Lot_1/track02.ogg",
+              "music/Rock/FNM--Who_Cares_A_Lot_2/track01.ogg"]
+
+def selectNext arr, prevString
+  index = rand(arr.length)
+  item = arr[index]
+  if prevString==""
+    return item
+  else
+    itemSplit = item.split '/'
+    prevStringSplit = prevString.split '/'
+    if itemSplit[2]==prevStringSplit[2]
+      selectNext arr, prevString
+    else
+      return item
+    end
+  end
 end
+
+def music_shuffle filenames
+  playList=[]
+  lastItem = ""
+  (filenames.length-1).times do
+    track = selectNext filenames, lastItem
+    playList.push(track)
+    lastItem=track
+  end
+  #then add the last item
+  puts ""
+  puts "The final playList:"
+  puts playList
+end
+
+music_shuffle trackNames
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,28 @@
-# your code here
+trackNames = ["music/Jazz/Monk--Nutty/track08.ogg",
+              "music/Jazz/Monk--London_Collection_1/track05.ogg",
+              "music/Jazz/Monk--Nutty/track13.ogg",
+              "music/Rock/FNM--Who_Cares_A_Lot_2/track01.ogg"]
+
+playListPath = '/Users/UpstatePedro/pmh_gd/Projects/learn_to_program/ch11-reading-and-writing/playList.m3u'
+playList = []
+numTracks = trackNames.length
+
+numTracks.times do
+  if trackNames.length>1
+    index = rand(trackNames.length)
+    #copy the track into the playlist file
+    track = trackNames[index]
+    playList.push(track)
+    # delete the track from the array
+    trackNames.delete(track)
+  else
+    #what to do if there's only one track left (don't want random number!)
+    #copy the remaining track into the playlist file
+    track = trackNames[0]
+    playList.push(track)
+  end
+end
+puts "The final playlist: #{playList}"
+File.open playListPath, 'w' do |f|
+  f.write playList
+end

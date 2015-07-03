f90bcf0accbed0cf2ad8064e53ccd6f025323efe
UpstatePedro <piers.m.holden@gmail.com>
2015-07-04 00:28:03 +0100

    Chapter 12 Exercises - 1

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,31 @@
-# your code here
+birthdays=File.read './birthdays.txt'
+
+def loadBirthdays file
+  dataHash=Hash.new
+  file.each_line {|line|
+       (name, date)=line.chomp.split(',')
+       dataHash[name] = date}
+  dataHash
+end
+
+def askQuestion hash
+  puts "Whose birthday would you like?"
+  search=gets.chomp
+  if hash[search]!=nil
+    response=hash[search]
+    puts "#{search}'s birthday is on #{response}"
+  elsif search=='n'
+    puts "Ok, closing down now!"
+    exit
+  else
+    puts "\nSorry, that name isn't in the database, please try again or enter \"n\" to quit the program"
+    askQuestion hash
+  end
+end
+
+def main file
+  dataHash=loadBirthdays file
+  askQuestion dataHash
+end
+
+main birthdays

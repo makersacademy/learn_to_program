984064338da92b3c6fc1233059da7d6023aa02e1
UpstatePedro <piers.m.holden@gmail.com>
2015-07-04 11:28:56 +0100

    Chapter 13 - 1

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,137 @@
-# your code here
+class Dragon
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 10 # He's full.
+    @stuff_in_intestine = 0 # He doesn't need to go.
+    puts "#{@name} is born."
+  end
+  def feed
+    puts "You feed #{@name}."
+    @stuff_in_belly = 10
+    passage_of_time
+  end
+  def walk
+    puts "You walk #{@name}."
+    @stuff_in_intestine = 0
+    passage_of_time
+  end
+  def put_to_bed
+    puts "You put #{@name} to bed."
+    @asleep = true
+    3.times do
+      if @asleep
+        passage_of_time
+      end
+      if @asleep
+        puts "#{@name} snores, filling the room with smoke."
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts "#{@name} wakes up slowly."
+    end
+  end
+  def toss
+    puts "You toss #{@name} up into the air."
+    puts 'He giggles, which singes your eyebrows.'
+    passage_of_time
+  end
+  def rock
+    puts "You rock #{@name} gently."
+    @asleep = true
+    puts 'He briefly dozes off...'
+    passage_of_time
+    if @asleep
+      @asleep = false
+      puts '...but wakes when you stop.'
+    end
+  end
+
+  private
+# "private" means that the methods defined here are
+# methods internal to the object. (You can feed your
+# dragon, but you can't ask him whether he's hungry.)
+  def hungry?
+# Method names can end with "?".
+# Usually, we do this only if the method
+# returns true or false, like this:
+    @stuff_in_belly <= 2
+  end
+  def poopy?
+    @stuff_in_intestine >= 8
+  end
+  def passage_of_time
+    if @stuff_in_belly > 0
+# Move food from belly to intestine.
+      @stuff_in_belly = @stuff_in_belly - 1
+      @stuff_in_intestine = @stuff_in_intestine + 1
+    else # Our dragon is starving!
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name} is starving! In desperation, he ate YOU!"
+      exit # This quits the program.
+    end
+    if @stuff_in_intestine >= 10
+      @stuff_in_intestine = 0
+      puts "Whoops! #{@name} had an accident..."
+    end
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name}'s stomach grumbles..."
+    end
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name} does the potty dance..."
+    end
+  end
+end
+
+def interaction dragon
+  puts ""
+  puts "what do you want to do with your dragon??"
+  puts "1) feed him"
+  puts "2) walk him"
+  puts "3) rock him"
+  puts "4) give him a toss......."
+  puts "5) put him to bed"
+  puts "Just enter one of the numbers :-)"
+  response = gets.chomp
+  if (response=="5"||response=="2"||response=="3"||response=="4"||response=="1")
+    case response
+      when "1"
+        dragon.feed
+        interaction dragon
+      when "2"
+        dragon.walk
+        interaction dragon
+      when "3"
+        dragon.rock
+        interaction dragon
+      when "4"
+        dragon.toss
+        interaction dragon
+      when "5"
+        dragon.put_to_bed
+        interaction dragon
+    end
+  else
+    puts "That wasn't an option! please enter a valid number."
+    interaction dragon
+  end
+end
+
+def main
+  pet = Dragon.new 'Norbert'
+  interaction pet
+end
+
+main
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,109 @@
+=begin
+Orange tree. Make an OrangeTree class that has a height method that returns
+its height and a one_year_passes method that, when called, ages the tree one
+year. Each year the tree grows taller (however much you think an orange
+tree should grow in a year), and after some number of years (again, your
+call) the tree should die. For the first few years, it should not produce
+fruit, but after a while it should, and I guess that older trees produce
+more each year than younger trees…whatever you think makes the most
+sense. And, of course, you should be able to count_the_oranges (which returns
+the number of oranges on the tree) and pick_an_orange (which reduces the
+@orange_count by 1 and returns a string telling you how delicious the orange
+was, or else it just tells you that there are no more oranges to pick this
+year). Make sure any oranges you don’t pick one year fall off before the
+next year.
+=end
+require 'Time'
+
 class OrangeTree
-  # your code here
+
+  def initialize
+    @age = 0
+    @height = 30
+    @alive = true
+    @orange_count = 0
+  end
+
+  def birthday
+    @age+=1
+    if @age<10
+      puts "Another year has passed, your orange tree is now #{@age} years old!"
+      @height+=15
+      if @age >3
+      # lose all old oranges & grow new crop
+      @orange_count=@age*2
+      end
+    else
+      # still alive until 10, then dies & exit prog
+      puts "your orange tree has reached the ripe old age of 10 & passed on..."
+      @alive = false
+      exit
+    end
+  end
+
+  def pick_an_orange
+    #reduce orange count by one, return deliciousness string
+    # don't go below 0 oranges!
+    if @orange_count>0
+      puts "mmmmm, that was bloody delicious!"
+      @orange_count-=1
+      puts "you've got #{@orange_count} left"
+    else
+      puts "that's a shame, there aren't any oranges left from this crop!"
+    end
+  end
+
+  def count_the_oranges
+    puts ""
+    puts "there are #{@orange_count} oranges left on the tree"
+  end
+
+  def measure_the_tree
+    puts ""
+    puts "Your tree is now #{@height}cm tall!"
+
+  end
+
+  def alive?
+    @alive
+  end
+
+  def interact
+    # opportunity to query the tree
+    puts ""
+    puts "would you like to do any of the following?:"
+    puts "1) find out how many oranges are on the tree this year"
+    puts "2) taste one of the oranges"
+    puts "3) measure how tall your tree is"
+    puts "4) Give the tree a break! Leave it to hunker down for the winter..."
+    puts "(Please just enter the number)"
+    response=gets.chomp
+    #Act on the request
+    case response
+      when "1"
+        self.count_the_oranges
+        interact
+      when "2"
+        self.pick_an_orange
+        interact
+      when "3"
+        self.measure_the_tree
+        interact
+      when "4"
+        puts"ok, good idea."
+    end
+  end
+
 end
+
+def main
+  tree = OrangeTree.new
+  while tree.alive?
+      tree.birthday
+      tree.interact
+  end
+
+end
+
+main
+

46eb95a6424b19f5265c7b43469a3cfa23055d7f
UpstatePedro <piers.m.holden@gmail.com>
2015-07-04 13:40:37 +0100

    Chapter 14 Exercises - 1

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
 def log desc, &block
-  # your code here
+  puts ("  "*$nesting_depth)+"starting #{desc}..."
+  output = block.call
+  puts ("  "*$nesting_depth)+"#{desc} finished, returning:"
+  puts ("  "*$nesting_depth)+output.to_s
+end
+
+$nesting_depth=0
+log "outer block" do
+  $nesting_depth=2
+  log "some little block" do
+    $nesting_depth=3
+    log "teeny-tiny block" do
+      "lots of love"
+    end
+    $nesting_depth=2
+    5
+  end
+  $nesting_depth=2
+  log "yet another block" do
+    "I like Thai food!"
+  end
+  $nesting_depth=0
+  false
 end

7218abe966fc4a1c10bdec5857287a37b20fd926
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-05 17:37:32 +0100

    Added solution

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,18 @@
 def old_roman_numeral num
-  # your code here
+  numerals = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I"}
+  roman_num = ""
+  numerals.each_key do |k|
+    x = num / k
+    if x == 4
+      roman_num +=
+    elsif x == 9
+      roman_num +=
+    else
+      roman_num += "#{numerals[k]}" * x
+    end
+    rem = num % k.to_i
+    num = rem
+  end
+  roman_num
 end
+

acfd36e9406b130cc90ec3a3b84febe122e95bb8
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-06 19:01:06 +0100

    Solution to sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,19 @@
-def sort arr
-  # your code here
+def sort(arr)
+  recursive_sort(arr, [])
+end
+
+def recursive_sort(unsorted_arr, sorted_arr)
+  while unsorted_arr.length > 0
+    smallest = unsorted_arr[0]
+    unsorted_arr.each do |word|
+      if word < smallest
+        smallest = word
+      end
+    end
+    sorted_arr << smallest
+    i = unsorted_arr.index(smallest)
+    unsorted_arr.delete_at(i)
+    recursive_sort(unsorted_arr, sorted_arr)
+  end
+  return sorted_arr
 end

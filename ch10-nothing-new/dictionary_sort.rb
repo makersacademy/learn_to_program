338db4dcdf0389fee696f5ef7b32d6545c543689
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-06 19:41:18 +0100

    Dictionary sort solution

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,16 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arr)
+  recursive_sort(arr, [])
+end
+
+def recursive_sort(unsorted_arr, sorted_arr)
+  while unsorted_arr.length > 0
+    smallest = unsorted_arr[0]
+    unsorted_arr.each{ |word| smallest = word if word.downcase < smallest.downcase }
+
+    sorted_arr << smallest
+    i = unsorted_arr.index(smallest)
+    unsorted_arr.delete_at(i)
+    recursive_sort(unsorted_arr, sorted_arr)
+  end
+  return sorted_arr
 end

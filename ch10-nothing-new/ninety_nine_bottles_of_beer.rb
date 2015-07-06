ec59c71d744bed09671144511eb8235a2ccb26b0
Lewis Jones <ljones140@gmail.com>
2015-07-06 21:03:15 +0100

    ninety nine bottles ready

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,109 @@
-# your code here
+def english_number number
+  if number < 0  
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+
+  numString = ''  
+
+  onesPlace = ['one',     'two',       'three',    'four',     'five',
+               'six',     'seven',     'eight',    'nine']
+  tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+               'sixty',   'seventy',   'eighty',   'ninety']
+  teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+               'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  big_numbers = [
+                 ["quindecillion"     ,10**48],
+                 ["quattuordecillion" ,10**45],
+                 ["tredecillion"      ,10**42],
+                 ["duodecillion"      ,10**39],
+                 ["undecillion"       ,10**36],
+                 ["decillion"         ,10**33],
+                 ["nonillion"         ,10**30],
+                 ["octillion"         ,10**27],
+                 ["septillion"        ,10**24],
+                 ["sextillion"        ,10**21],
+                 ["quintillion"       ,10**18],
+                 ["quadrillion"       ,10**15],
+                 ["trillion"          ,10**12],
+                 ["billion"           ,10**9],
+                 ["million"           ,10**6],
+                 ["thousand"          ,1000]]
+
+  left  = number
+
+  big_numbers.each do | name, bignum |
+  
+    write = left / bignum          
+    left  = left - write * bignum  
+
+  
+    if write > 0
+      current  = english_number write
+      numString = numString + current + " #{name}" 
+      if left > 0
+        numString = numString + ' '
+      end
+    end
+
+  end
+
+  write = left/100          
+  left  = left - write*100  
+
+  if (write > 0) 
+    hundreds  = english_number write
+    numString = numString + hundreds + ' hundred'
+
+    if left > 0
+      numString = numString + ' '
+    end
+  end
+
+  write = left/10          
+  left  = left - write*10 
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      numString = numString + teenagers[left-1]
+
+      left = 0
+    else
+      numString = numString + tensPlace[write-1]
+    end
+
+    if left > 0
+      numString = numString + '-'
+    end
+  end
+
+  write = left 
+  left  = 0     
+
+  if write > 0
+    numString = numString + onesPlace[write-1]
+  end
+
+  numString
+end
+
+def ninety_nine_bottles_of_beer bottles
+  chorus =  'You Break one down turn it around'
+  while true
+    verse      = english_number(bottles)  + ' Bottles of Beer'
+    puts verse + ' on the wall'
+    puts verse
+    puts chorus
+    puts verse
+    bottles    = bottles -1
+  if bottles == -1
+    break
+  end
+ end 
+end
+
+puts ninety_nine_bottles_of_beer 1000

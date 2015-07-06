e73ef49530f6a16ebc5350685eca92858d8b5137
Winifred Au <winifredau1990@gmail.com>
2015-07-06 16:55:11 -0400

    added chp 10 file for shuffle.rb changes

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,16 @@
-def shuffle arr
-  # your code here
+puts 'List some things here to shuffle:'
+array = []
+
+while true
+  response = gets.chomp.to_s
+  if response == ''
+    break
+  else
+  array.push response
+  end
 end
+
+def shuffle arr
+puts arr.shuffle
+end
+puts shuffle array

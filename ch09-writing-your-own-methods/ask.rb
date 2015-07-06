fdb68a5227c3cef60a9eb1340f15f42636e9515d
Lewis Jones <ljones140@gmail.com>
2015-07-06 14:00:37 +0100

    ask passing test but want to improve

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,15 @@
-def ask question
-  # your code here
+ def ask question
+   while true
+      puts question
+      reply = gets.chomp.downcase
+   if (reply == 'yes' || reply == 'no')
+     if reply == 'yes'
+       return true
+     end
+     if reply == 'no'
+       return false
+     end
+    end
+      puts 'please answer yes or no?'	
+  end    
 end

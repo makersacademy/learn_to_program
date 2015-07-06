d28ea1a982de3b9e3b455251bc70505c07a18b1b
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-07-06 22:09:36 +0800

    Added happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "Which year were you born in?"
+ans_y = gets.chomp.to_i
+puts "Which month were you born in?"
+ans_m = gets.chomp.to_i
+puts "Which day were born on?"
+ans_d = gets.chomp.to_i
+
+now = Time.new
+
+age = 0
+while Time.gm(ans_y + age + 1, ans_m, ans_d) <= now
+  age += 1
+end
+
+puts "You're #{age} years old! Here you go!"
+age.times {|x| puts 'SPANK!'}

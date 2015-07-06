dde120a486ef4cda0192ec7f7aa6b2e204e99bf6
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-06 18:14:14 +0100

    Refactored roman numerals solution a bit - still not optimal.

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,15 +1,15 @@
 def old_roman_numeral num
-  numerals = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I"}
+  numerals = {1000 => "M",
+              500 => "D",
+              100 => "C",
+              50 => "L",
+              10 => "X",
+              5 => "V",
+              1 => "I"}
   roman_num = ""
   numerals.each_key do |k|
     x = num / k
-    if x == 4
-      roman_num +=
-    elsif x == 9
-      roman_num +=
-    else
-      roman_num += "#{numerals[k]}" * x
-    end
+    roman_num += "#{numerals[k]}" * x
     rem = num % k.to_i
     num = rem
   end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,42 +1,43 @@
 def roman_numeral num
-    roman_num = ""
+  roman_num = ""
+
   thous = num / 1000
   roman_num += "M" * thous
   rem = num % 1000
 
-  if rem / 900 > 0
+  hund = rem / 100
+  if hund == 9
     roman_num += "CM"
-    rem = rem % 900
+    rem = rem % 100
+  elsif hund == 4
+    roman_num += "CD"
+    rem = rem % 100
   else
     five_hund = rem / 500
     roman_num += "D" * five_hund
     rem = rem % 500
-  end
 
-  hund = rem / 100
-  if hund == 4
-    roman_num += "CD"
-  else
+    hund = rem / 100
     roman_num += "C" * hund
+    rem = rem % 100
   end
-  rem = rem % 100
 
-  if rem / 90 > 0
+  ten = rem / 10
+  if ten == 9
     roman_num += "XC"
-    rem = rem % 90
+    rem = rem % 10
+  elsif ten == 4
+    roman_num += "XL"
+    rem = rem % 10
   else
     fifty = rem / 50
     roman_num += "L" * fifty
     rem = rem % 50
-  end
 
-  ten = rem / 10
-  if ten == 4
-    roman_num += "XL"
-  else
+    ten = rem / 10
     roman_num += "X" * ten
+    rem = rem % 10
   end
-  rem = rem % 10
 
   if rem == 9
     roman_num += "IX"
@@ -46,11 +47,8 @@ def roman_numeral num
     five = rem / 5
     roman_num += "V" * five
     rem = rem % 5
-
-    one = rem
     roman_num += "I" * rem
   end
   return roman_num
 end
 
-puts (roman_numeral(1))

5e05e8b47bd286a4b55a1536c117c29e259f2822
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-06 19:35:14 +0100

    Solution to shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,13 @@
 def shuffle arr
-  # your code here
+  shuffled_arr = []
+
+  while arr.length > 0
+    rand_num = rand(arr.length)
+    el = arr[rand_num]
+    shuffled_arr << el
+    i = arr.index(el)
+    arr.delete_at(i)
+  end
+  return shuffled_arr
 end
+

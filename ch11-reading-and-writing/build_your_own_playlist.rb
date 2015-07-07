45c5394f54a671647bf2157b78ecc5957912e1a9
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-07 20:07:56 +0100

    Solution for build a playlist.  Creates a playlist, but not sure if it has tracks in it yet.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,16 @@
-# your code here
+def create_playlist
+  music_dir = Dir["/Users/rebeccaappleyard1984/Desktop/Music"]
+
+  puts "What would you like to call your playlist?"
+  playlist_name = gets.chomp
+
+  filename = "/Users/rebeccaappleyard1984/Desktop/Music/#{playlist_name}.m3u"
+
+  File.open filename, 'w' do |f|
+    music_dir.each do |track|
+      f.write track
+    end
+  end
+end
+
+create_playlist()

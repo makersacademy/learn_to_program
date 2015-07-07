75f0fbe6d09811fa7aca2196c32eef8455b657d1
Lewis Jones <ljones140@gmail.com>
2015-07-07 10:13:21 +0100

    build own playlist complete

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,23 @@
-# your code here
+#This Program creates a random playlist based on a music libary
+#directory that holds music Music/Artist/Album/track
+#Dir.chdir '/Users/lewis/music/iTunes/iTunes Media/'
+#File type is hardcoded as MP3 but can be changed
+#amout of tracks controlled by the times.do
+
+#tracks = Dir['music/*/*/*.{MP3,mp3}']
+tracks = Dir['/Users/lewis/music/iTunes/iTunes Media/music/*/*/*.{MP3,mp3}']
+
+playlist = ''
+
+10.times do
+  playlist << "#{tracks.sample}\n"
+end
+
+puts playlist
+
+Dir.chdir '/users/lewis/rubyprograms/playlist'
+filename = "playlist#{Time.now}.m3u"
+
+File.open filename, 'w' do |f|
+  f.write playlist
+end

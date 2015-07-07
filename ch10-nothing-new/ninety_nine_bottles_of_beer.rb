d7069a2a1c1ac25c62c7c9900a64a88eaee2c211
Richard Charman <richardcharman@live.com>
2015-07-07 20:58:18 +0000

    Chapter 10 solutions

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,91 @@
-# your code here
+def english_number number
+  if number < 0 # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+# No more special cases! No more returns!
+num_string = '' # This is the string we will return.
+ones_place = ['one', 'two', 'three',
+              'four', 'five', 'six',
+              'seven', 'eight', 'nine']
+tens_place = ['ten', 'twenty', 'thirty',
+              'forty', 'fifty', 'sixty',
+              'seventy', 'eighty', 'ninety']
+teenagers = ['eleven', 'twelve', 'thirteen',
+             'fourteen', 'fifteen', 'sixteen',
+             'seventeen', 'eighteen', 'nineteen']
+expanded_numbers = [['hundred', 2], ['thousand',3], ['million', 6],
+                    ['billion',9], ['trillion',12],
+                    ['quadrillion',15], ['quintillion', 18]]
+# "left" is how much of the number
+# we still have left to write out.
+# "write" is the part we are
+# writing out right now.
+# write and left...get it? :)
+left = number
+  
+write = left/100 # How many hundreds left?
+left = left - write*100 # Subtract off those hundreds.
+  if write > 0
+  # Now here's the recursion:
+  hundreds = english_number write
+  num_string = num_string + hundreds + ' hundred'
+    if left > 0
+    # So we don't write 'two hundredfifty-one'...
+    num_string = num_string + ' '
+    end
+  end
+  
+write = left/10 # How many tens left?
+left = left - write*10 # Subtract off those tens.
+  if write > 0
+    if ((write == 1) and (left > 0))
+    # Since we can't write "tenty-two" instead of
+    # "twelve", we have to make a special exception
+    # for these.
+    num_string = num_string + teenagers[left-1]
+    # The "-1" is because teenagers[3] is
+    # 'fourteen', not 'thirteen'.
+    # Since we took care of the digit in the
+    # ones place already, we have nothing left to write.
+    left = 0
+    else
+    num_string = num_string + tens_place[write-1]
+    # The "-1" is because tens_place[3] is
+    # 'forty', not 'thirty'.
+    end
+      if left > 0
+      # So we don't write 'sixtyfour'...
+      num_string = num_string + '-'
+      end
+  end
+write = left # How many ones left to write out?
+left = 0 # Subtract off those ones.
+  if write > 0
+  num_string = num_string + ones_place[write-1]
+  # The "-1" is because ones_place[3] is
+  # 'four', not 'three'.
+  end
+# Now we just return "num_string"...
+num_string
+end
+beer = 9999
+while beer > 0
+  written = english_number beer
+  if beer > 1
+    puts written.capitalize + " bottles of beer on the wall, "
+    puts written.capitalize + " bottles of beer."
+    puts "You take one down, pass it around,"  
+  else
+    puts written.capitalize + " bottle of beer on the wall,"
+    puts "just " + written + " bottle of beer."
+    puts "You take it down, pass it around,"
+  end
+  beer = beer-1
+    written = english_number beer
+  puts written.capitalize + " bottles of beer on the wall."
+  puts
+end
+puts "BUUUUURRRRRPPPP!!!!!"
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,13 @@
 def shuffle arr
-  # your code here
+  array.shuffle!
 end
+
+puts "Enter word (or press return to shuffle)"
+array = []
+while true
+  userWord = gets.chomp.capitalize
+  break if userWord.empty?
+  array << userWord
+end
+puts "Here are you words all shuffled up -"
+puts array.shuffle!

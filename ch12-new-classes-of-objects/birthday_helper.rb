0cb5a9df6c6d86f05879e74e95abfb5c29a29fa8
Lewis Jones <ljones140@gmail.com>
2015-07-07 12:58:12 +0100

    commited birthday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,29 @@
-# your code here
+filename  = 'Birthdays.txt'
+birthdays = {}
+
+File.read(filename).each_line do |line|
+    birthdays[line.split(",")[0].gsub(/\A\p{Space}*/, '')] = (line.split(",")[1].gsub(/\A\p{Space}*/, '') + line.split(",")[2].chomp) 
+end
+
+
+birthdays.each do |name, date|
+ puts " #{name}  #{date}"
+end
+
+puts " give us a name?"
+entry = gets.chomp
+
+if birthdays[entry]
+  birthday = birthdays[entry]
+  month    = birthday.split(" ")[0]
+  day      = birthday.split(" ")[1]
+  year     = birthday.split(" ")[2]
+
+  nextage = ((Time.now - Time.local(year, month, day)) / 3153600 / 10 + 1).to_i.to_s
+
+  puts "Their birth date is on the #{day} of #{month} and they will be  #{nextage} "
+
+else
+  puts "name not in my list"
+end
+

aeb7de6e558172871cfadbe858a5ef95870fffbe
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-07-07 11:38:48 +0100

    Added party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,30 @@
 def roman_to_integer roman
-  # your code here
+  conversion = {'i' =>    1,
+                'v' =>    5,
+                'x' =>   10,
+                'l' =>   50,
+                'c' =>  100,
+                'd' =>  500,
+                'm' => 1000}
+  total = 0
+  value = 0
+  n = roman.length-1
+
+  while n >= 0
+    numeral = roman.downcase[n]
+    dummy = conversion[numeral]
+    return 'This is not in Roman numerals' if dummy == nil
+
+    if dummy >= value
+      value = dummy
+    else
+      value = dummy*-1
+    end
+
+    n -= 1
+    total += value
+  end
+  total
 end
+
+puts(roman_to_integer('mcmxcix'))

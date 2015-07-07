36804ff18f36d0a19ae095a74ceea1f484cd3511
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-07 18:44:27 +0100

    Added file exists check to picture downloader.

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,35 @@
-# your code here
+Dir.chdir '/Users/rebeccaappleyard1984/Desktop/Makers/PreCourse/Week_3/learn_to_program'
+
+pic_names = Dir['/Users/rebeccaappleyard1984/Desktop/Pics/*.jpg']
+
+puts "What would you like to call this batch?"
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files:  "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print "."
+
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  if File.exists?(new_name)
+    puts
+    puts "That file already exists."
+    puts "Process aborted."
+    exit
+  else
+  File.rename name, new_name
+  end
+
+  pic_number += 1
+end
+
+puts
+puts "All done!"

d868d2ac1af82f85249f459a1630e2d56b7ff52f
Lewis Jones <ljones140@gmail.com>
2015-07-07 12:09:24 +0100

    one billion secs working

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,9 @@
-# your code here
+birth = Time.local(1980, 10, 9, 6, 50)
+current = Time.now
+seconds_old = (current - birth)
+billion = 10**9
+#years_old =  seconds_old /  31557600 
+bill_diff = seconds_old - billion
+billion_date = current - bill_diff
+puts billion_date 
+

156734a8aecf03419d9db42cb37335ee45c4e998
Lewis Jones <ljones140@gmail.com>
2015-07-07 14:18:48 +0100

    to_roman working test passing

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -11,7 +11,32 @@ class Integer
     else
       num * (num-1).factorial
     end
-
   end
+
+  def to_roman
+    num = self
+    roman = {one: "I", five: "V", ten: "X", fifty: "L", hunds: "C", fivehund: "D", thous: "M"}
+    result = ''
+ 
+    thous = (num / 1000)
+    fivehund = (num % 1000 / 500)
+    hunds = (num % 500  / 100)
+    fifty = (num % 100   / 50)
+    ten =   (num % 50    / 10)
+    five =  (num % 10    / 5)
+    one =   (num % 5     / 1)
+
+    result << roman[:thous]    * thous 
+    result << roman[:fivehund] * fivehund  
+    result << roman[:hunds]    * hunds 
+    result << roman[:fifty]    * fifty  
+    result << roman[:ten]      * ten  
+    result << roman[:five]     * five  
+    result <<  roman[:one]      * one
+    result
+
+
+  end  
 end
+
 

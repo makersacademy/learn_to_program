8c46e4edc7ba860419c6a40e273147919ed48172
ojlamb <owen.lamb@gmail.com>
2015-07-08 09:56:02 +0000

    Add solution to dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,32 @@
 def dictionary_sort arr
-  # your code here
+   recursive_sort arr, []
 end
+
+def recursive_sort unsorted_arr, sorted_arr 
+    #Check to see if there are elements in the array that need sorting
+    if unsorted_arr.length <= 0 
+        return sorted_arr
+    end
+    
+    not_sorted = []
+    #Get a value to use for comparison
+    sorter = unsorted_arr.pop
+    unsorted_arr.each {|x|
+        #Capitalize to get dictionary sort
+        if x.capitalize < sorter.capitalize
+            not_sorted << sorter
+            sorter = x
+        else
+            not_sorted << x
+        end
+    }
+    
+    sorted_arr << sorter
+    
+    #Call recursive sort again to get next lowest string
+    recursive_sort not_sorted, sorted_arr
+end
+
+test_arr = ['Owen', 'alex', 'joe', 'Sam', 'nick']
+
+puts dictionary_sort test_arr

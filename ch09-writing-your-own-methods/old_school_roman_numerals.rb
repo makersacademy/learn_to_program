6a8e0207a957b6c1b7ec451ad8ce99e420cbc22f
Lewis Jones <ljones140@gmail.com>
2015-07-08 17:21:23 +0100

    Old Roman Numerals rewritten

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,50 +1,22 @@
-def old_roman_numeral num
+def old_roman_numeral number
 
-  roman = {one: "I", five: "V", ten: "X", fifty: "L", hunds: "C", fivehund: "D", thous: "M"}
-             
+  numerals = {ones: "I", fives: "V", tens: "X", fifties: "L", hundreds: "C", five_hundreds: "D", thousands: "M"}
+
   result = ''
- 
-  thous = (num / 1000)
-  fivehund = (num % 1000 / 500)
-  hunds = (num % 500  / 100)
-  fifty = (num % 100   / 50)
-  ten =   (num % 50    / 10)
-  five =  (num % 10    / 5)
-  one =   (num % 5     / 1)
 
-=begin
-   units = {
-  "thous" => (num / 1000), 
-  "fivehund" => (num % 1000 / 500),
-  "hunds" => (num % 500  / 100),
-  "fifty" => (num % 100   / 50),
-  "ten" =>   (num % 50    / 10),
-  "five" =>  (num % 10    / 5),
-  "one" =>   (num % 5     / 1)
+  units = {
+    thousands: (number / 1000),
+    five_hundreds: (number % 1000 / 500),
+    hundreds: (number % 500 / 100),
+    fifties: (number % 100 / 50),
+    tens: (number % 50 / 10),
+    fives: (number % 10 / 5),
+    ones: (number % 5 / 1)
   }
-=end   
-
-
-# this code here works
-  result << roman[:thous]    * thous 
-  result << roman[:fivehund] * fivehund  
-  result << roman[:hunds]    * hunds 
-  result << roman[:fifty]    * fifty  
-  result << roman[:ten]      * ten  
-  result << roman[:five]     * five  
-  result <<  roman[:one]      * one
+   
+  units.each do |key, value|
+    result << numerals[key] * value
+  end
   result
 
- #I am trying to be a bit clever and rather that havign to write the 7 lines above I want to iterate through 
- # the two hashes using the code below.
- 
-
-=begin
-  units.each do | key, unit |
-    unit.times  do result << roman[key] end
-  end
-=end 
-
-# result
 end
-

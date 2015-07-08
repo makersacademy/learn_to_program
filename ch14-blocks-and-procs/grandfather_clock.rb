717a1a995495262acfa43b2ba053146fb5a02f21
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-07-08 12:50:11 +0100

    Added grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,6 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock(&block)
+  hours = (Time.new.hour + 11) % 12 + 1
+  hours.times(&block)
 end
+
+grandfather_clock { puts 'DONG!' }

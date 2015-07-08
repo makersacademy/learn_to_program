c4937df2d50f3315ee217d3e2d323c147916eed3
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-08 17:45:34 +0100

    Added shuffle function - made the playlist a class.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,17 +1,37 @@
-def create_playlist
-  music_dir = Dir["/Users/rebeccaappleyard1984/Desktop/Music/*.m4a"]
+class Playlist
+  attr_accessor :playlist_name, :filename
+  def initialize(playlist_name)
+    @playlist_name = playlist_name
+    @filename = "/Users/rebeccaappleyard1984/Desktop/Music/#{playlist_name}.m3u"
+    create_playlist
+  end
 
-  puts "What would you like to call your playlist?"
-  playlist_name = gets.chomp
+  def create_playlist
+    music_dir = Dir["/Users/rebeccaappleyard1984/Desktop/Music/*.m4a"]
 
-  filename = "/Users/rebeccaappleyard1984/Desktop/Music/#{playlist_name}.m3u"
-
-  File.open filename, 'w' do |f|
-    music_dir.each do |track|
-      f.write track
-      f.write "\n"
+    File.open filename, 'w' do |f|
+      music_dir.each do |track|
+        f.write track
+        f.write "\n"
+      end
     end
+  end
+
+  def shuffle
+    playlist_str = File.read filename
+    playlist_arr = playlist_str.split("\n")
+    shuffled_arr = []
+    while playlist_arr.length > 0
+      rand_num = rand(playlist_arr.length)
+      shuffled_arr << playlist_arr[rand_num]
+      i = playlist_arr.index(playlist_arr[rand_num])
+      playlist_arr.delete_at(i)
+    end
+    puts shuffled_arr
   end
 end
 
-create_playlist()
+
+playlist = Playlist.new("My Songs")
+playlist.shuffle
+

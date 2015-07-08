9b6137699595322640745ba6175c990966098eea
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-08 20:41:27 +0100

    Solution to party like its mcmxcix

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,32 @@
 def roman_to_integer roman
-  # your code here
+  numerals = {1000 => "M",
+              500 => "D",
+              100 => "C",
+              50 => "L",
+              10 => "X",
+              5 => "V",
+              1 => "I"}
+  roman_arr = roman.upcase.split("")
+
+  roman_arr.map!{ |num| numerals.key(num) }
+
+  puts roman_arr
+  sum = 0
+  n = 0
+  while n < roman_arr.length
+    if n == roman_arr.length - 1
+      sum += roman_arr[n]
+      n += 1
+    elsif roman_arr[n] < roman_arr[n+1]
+      sum += (roman_arr[n+1] - roman_arr[n])
+      n += 2
+    else
+      sum += roman_arr[n]
+      n += 1
+    end
+  end
+  return sum
 end
+
+
+

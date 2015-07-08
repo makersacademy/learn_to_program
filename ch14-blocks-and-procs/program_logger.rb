fb60271200314240cc711cb80fc054ee1615b5d7
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-07-08 13:16:01 +0100

    Added program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,11 @@
-def log desc, &block
-  # your code here
+def log(desc, &block)
+  puts "Beginning '#{desc}'..."
+  result = block.call
+  puts "...'#{desc}' finished, returning: #{result}"
+end
+
+log 'outer block' do
+  log 'some little block' do 1 + 1 + 1 + 1 + 1 end
+  log 'yet another block' do 'I like Thai food!' end
+  false
 end

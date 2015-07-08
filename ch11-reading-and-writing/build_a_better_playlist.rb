a0c7bb3209797d73f6ab6cc926895904973986ae
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-08 18:31:08 +0100

    Not really sure about this solution - it's not passing the rspec test, but I can't see how to shuffle more right now

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,19 @@
 def music_shuffle filenames
-  # your code here
-end
+    filenames.collect!{ |file| file.split("/") }
+    shuffled_arr = []
+    while filenames.length > 0
+      rand_num = rand(filenames.length)
+      shuffled_arr << filenames[rand_num]
+      i = filenames.index(filenames[rand_num])
+      filenames.delete_at(i)
+    end
+    (0..shuffled_arr.length - 2).each do |n|
+    if shuffled_arr[n][-2] == shuffled_arr[n + 1][-2]
+      track = shuffled_arr.delete_at(n)
+      shuffled_arr.insert(rand(shuffled_arr.length - 1), track)
+    end
+  end
+  shuffled_arr.collect!{ |file| file.join("/") }
+  puts shuffled_arr
+  end
+

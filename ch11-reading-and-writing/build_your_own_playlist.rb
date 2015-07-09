77c13949fe50e81585d1843c2ca88b677896175e
Josh <Tattsy@hotmail.co.uk>
2015-07-09 19:56:56 +0100

    finished tasks CH11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,27 @@
-# your code here
+def shuffle ar
+ 	shuf_ar = [ ]
+	x = ar.length
+
+	until ar == []
+
+		y = rand(x)
+		if shuf_ar[y] == nil	
+			shuf_ar[y] = ar.pop
+		end
+
+	end
+
+	return shuf_ar
+
+end
+
+
+all_mp3s = shuffle (Dir['**/*.mp3'])
+
+File.open 'playlist.m3u', 'w' do |f| all_mp3s.each do |mp3|
+
+f.write mp3+"\n" end
+
+end
+
+puts 'Done!'

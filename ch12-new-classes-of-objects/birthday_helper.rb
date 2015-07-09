39d8caf2c139e33fe7bd52ea4936087bd9c10d98
Josh <Tattsy@hotmail.co.uk>
2015-07-09 21:38:34 +0100

    finished ch12 tasks

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,30 @@
-# your code here
+birth_dates = {}
+
+File.read("birthdates.txt").each_line do |line|
+	line = line.chomp
+
+	first_comma = 0
+	while line[first_comma].chr != "," && first_comma<line.length
+		first_comma += 1
+	end
+
+	name = line[0..(first_comma - 1)]
+	date = line[-10..-1]
+
+	birth_dates[name] = date
+end
+
+puts "Who's birthday do you want to know?"
+person = gets.chomp
+if birth_dates[person] == nil
+	puts "Sorry but I don't know that person"
+else
+	dates = birth_dates[person].split("/")
+	puts "#{person}'s birthday is the #{dates[0]}/#{dates[1]}"
+	now = Time.new
+	DOB = Time.local(dates[2],dates[1],dates[0])
+	age_s = now - DOB
+	StoY = 60 * 60 * 24 * 365.5
+	age = ( age_s / StoY ).floor
+	puts "and they are #{age} years old"
+end
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,5 @@
-# your code here
+DOB = Time.local(1992,7,13,20,17)
+
+BOld = DOB + (10 ** 9)
+
+puts "You will turn 1 Billion Seconds old on #{BOld}"

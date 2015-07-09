74358fffb7ed6a85e863ae667649e28b5f11d166
Lewis Jones <ljones140@gmail.com>
2015-07-09 21:55:53 +0100

    better logging completed issue with rspec

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
-def log desc, &block
-  # your code here
+$log_count = 0
+
+def log desc, &block 
+  $log_count += 1
+  indent = " " * ($log_count -1) 
+  puts "#{indent}Beginning \"#{desc}\"..."
+  puts "#{indent}...\"#{desc}\" finished, returning: #{block.call}"
 end
+
+#=begin
+    log 'outer block' do
+      log 'some little block' do
+        log 'teeny-tiny block' do
+          'lOtS oF lOVe'.downcase
+        end
+        7 * 3 * 2
+      end
+
+      log 'yet another block' do
+        '!doof naidnI evol I'.reverse
+      end
+      '0' == "0"
+    end
+#=end
+

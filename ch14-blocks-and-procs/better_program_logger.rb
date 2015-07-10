5d56c2d9934a2c787d44d17060fc11301527fa3d
Lewis Jones <ljones140@gmail.com>
2015-07-10 10:06:12 +0100

    better_program_logger passing tests

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -5,6 +5,22 @@ def log desc, &block
   indent = " " * ($log_count -1) 
   puts "#{indent}Beginning \"#{desc}\"..."
   puts "#{indent}...\"#{desc}\" finished, returning: #{block.call}"
+  $log_count -= 1
 end
 
 
+
+    log 'outer block' do
+      log 'some little block' do
+        log 'teeny-tiny block' do
+          'lOtS oF lOVe'.downcase
+        end
+        7 * 3 * 2
+      end
+
+      log 'yet another block' do
+        '!doof naidnI evol I'.reverse
+      end
+      '0' == "0"
+    end
+  

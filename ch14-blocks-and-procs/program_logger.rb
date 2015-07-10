db3a1d3c948359cf30ef629e9d24b68a406ac14f
Josh <Tattsy@hotmail.co.uk>
2015-07-10 11:39:43 +0100

    complete ch14 tasks

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
-  # your code here
+
+def log description, &block
+	puts "Beginning \"#{description}\"..."
+	result  = block.call
+	puts "...\"#{description}\" finished, returning: #{result}"
+end 
+
+log "outer block" do 
+	log "some little block" do 
+		1 ** 2 + 2 ** 2
+	end
+	log "yet another block" do 
+		"I like Thai food!"
+	end
+	"0" == 0
 end

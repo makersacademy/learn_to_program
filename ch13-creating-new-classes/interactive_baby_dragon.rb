14a744d06ee774d7c411c68684a74e4138f8b6be
Josh <Tattsy@hotmail.co.uk>
2015-07-10 09:07:03 +0100

    Completed ch13 tasks

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,123 @@
-# your code here
+class Dragon
+
+	def initialize name
+		@name = name
+		@stuff_in_belly = 10
+		@stuff_in_gut = 0
+		puts "#{@name} was born\n"
+	end
+
+	def feed
+		puts "You feed #{@name}"
+		@stuff_in_belly = 10
+		passage_of_time
+	end
+
+	def walk
+		puts "You take #{@name} for a walk"
+		@stuff_in_gut = 0
+		passage_of_time
+	end
+
+	def sleep
+		puts "you put #{@name} to bed"
+		@asleep = true
+		if @asleep == true
+			3.times do
+				if @asleep
+					passage_of_time
+				end
+				if @asleep
+					puts "#{@name} snores, breathing smoke everywhere"
+				end
+			end
+			if @asleep
+				@asleep = false
+				puts "#{@name} wakes up slowly"
+			end
+		end
+	end
+
+	def hug
+		puts "You gently rock while cudling #{@name}"
+		@asleep = true
+		puts "#{@name} briefly dozes off..."
+		passage_of_time
+		if @asleep
+			puts "... but wakes up when you stop"
+		end
+	end
+
+	def play
+		game = ["You toss #{@name} in the air, \n#{@name} giggles spitting fire and singes your eyebrows", "You play catch until #{@name} laughs, melting the ball", "You watch puff the magic dragon on TV \n#{@name} lets out a cute ROAR"]
+		puts game[rand(3)]
+		passage_of_time
+	end
+
+	private
+
+	def hungry?
+		@stuff_in_belly <=2
+	end
+
+	def toilet?
+		@stuff_in_gut >= 8
+	end
+
+	def passage_of_time
+		if @stuff_in_belly > 0
+			@stuff_in_belly -= 1
+			@stuff_in_gut += 1
+		else
+			if @asleep
+				asleep = false
+				puts "#{@name} wakes up suddenly!"
+			end
+			puts "#{@name} is starving! In desperation, #{@name} ate you!"
+			exit
+		end
+
+		if @stuff_in_gut >= 10
+			@stuff_in_gut = 0
+			puts "Whoops! #{@name} had a brown accident"
+		end
+
+		if hungry?
+			if @asleep
+				@asleep = false
+				puts "#{@name} wakes up suddenly!"
+			end
+			puts "#{@name}'s stomach rumbles"
+		end
+
+		if toilet?
+			if @asleep
+				@asleep = false
+				puts "#{@name} wakes up suddenly!"
+			end
+			puts "#{@name} does a potty dance..."
+		end
+	end
+end
+
+puts "Your egg hatches into baby dragon!!\nWhat do you want to call your pet dragon??"
+@name = gets.chomp
+
+pet = Dragon.new @name
+
+while true
+	command = ["hug", "play", "feed", "walk", "sleep"]
+	puts "What do you want to do with #{@name}"
+	action = gets.chomp
+	action.downcase!
+	command.each do |x|
+		if x == action
+			pet.send x
+			action = true
+		end
+	end
+	if action != true
+		puts "Please try and do one of the following activities with #{@name}\n#{command.join(", ")}"
+	end
+	puts " "
+end
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,128 @@
 class OrangeTree
-  # your code here
+#   I did my code originally which followed the rules from the book but was different
+#   as the book continually said whatever you think
+
+#   After looking at the rspec for this i realised that it wanted the output from the 
+#   exact orange tree from the book so i just copied that one. 
+
+#	def initialise
+#		@height = 0
+#		@orange_count = 0
+#		puts "You plant an orange tree"
+#	end
+#
+#	def height
+#		puts "The orange tree is #{@height}m tall"
+#		return @height
+#	end
+#
+#	def count_the_oranges
+#		if @orange_count == 1
+#			puts "There is 1 orange on the tree"
+#		else
+#			puts "There are #{@orange} oranges on the tree"
+#		end
+#		return @orange_count
+#	end
+#
+#	def pick_an_orange
+#		if @orange_count > 0
+#			puts "You pick an orange from the tree \n It is delicious"
+#			@orange_count -= 1
+#		else
+#			puts "There are no oranges on the tree for you to pick"
+#		end
+#	end
+#
+#	def one_year_passes
+#		puts "one year passes and the tree grows"
+#		@height += 1
+#		if @tall >= 10 && rand(3) > 1
+#			puts "The orange tree grows too old and died"
+#			exit
+#		end
+#		@orange_count = (@height - 2) * 2
+#		if @orange_count < 0
+#			@orange_count = 0
+#		end
+#	end
+
+	def initialize
+	@height = 0
+	@orange_count = 0
+	@alive = true
+	end
+
+	def height 
+		if @alive 
+			@height
+		else
+			'A dead tree is not very tall. :(' 
+		end
+	end
+
+	def count_the_oranges 
+		if @alive
+			@orange_count
+		else
+			'A dead tree has no oranges. :(' 
+		end
+	end
+
+	def one_year_passes 
+		if @alive
+
+			@height = (@height + 0.4).round(1)
+			@orange_count = 0 
+
+			if @height > 10 && rand(2) > 0
+				@alive = false
+				'Oh, no! The tree is too old, and has died. :('
+			elsif @height > 2
+				@orange_count = (@height * 15 - 25).to_i
+				"This year your tree grew to #{@height}m tall," + " and produced #{@orange_count} oranges."
+			else
+				"This year your tree grew to #{@height}m tall," +" but is still too young to bear fruit." 
+			end
+		else
+			'A year later, the tree is still dead. :(' 
+		end
+	end
+
+	def pick_an_orange 
+		if @alive
+			if @orange_count > 0
+				@orange_count -= 1 
+				'You pick a juicy, delicious orange!'
+			else
+			'You search every branch, but find no oranges.' 
+			end
+		else
+		'A dead tree has nothing to pick. :(' 
+		end
+	end
 end
+
+
+
+#tree = OrangeTree.new
+#tree.one_year_passes
+#tree.height
+#tree.orange_count
+
+ot = OrangeTree.new 
+23.times do
+ot.one_year_passes
+end
+
+puts(ot.one_year_passes) 
+puts(ot.count_the_oranges) 
+puts(ot.height) 
+puts(ot.one_year_passes) 
+puts(ot.one_year_passes) 
+puts(ot.one_year_passes) 
+puts(ot.one_year_passes) 
+puts(ot.one_year_passes) 
+puts(ot.height) 
+puts(ot.count_the_oranges) 
+puts(ot.pick_an_orange)

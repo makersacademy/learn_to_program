3d4ae7aeb44d8dfd0e322dc3ae1cae0f4f25e110
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-11 13:40:05 +0200

    ch09-writing-your-own-methods

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,40 @@
 def roman_numeral num
-  # your code here
-end
+    
+    hundreds = ((num % 1000) / 100)
+    tens = ((num % 100) / 10)
+    ones = (num % 10)
+    
+    roman = ''
+    roman = roman + 'M' * (num/1000)
+    
+    if hundreds == 9
+        roman = roman + 'CM'
+        elsif hundreds == 4
+        roman = roman + 'DC'
+        else
+        roman = roman + 'D' * ((num%1000) / 500)
+        roman = roman + 'C' * ((num%500) / 100)
+    end
+    
+    if tens == 9
+        roman = roman + 'XC'
+        elsif tens == 4
+        roman = roman + 'XL'
+        else
+        roman = roman + 'L' * ((num % 100) / 50)
+        roman = roman + 'X' * ((num % 50) / 10)
+    end
+    
+    if ones == 9
+        roman = roman + 'IX'
+        elsif ones == 4
+        roman = roman + 'IV'
+        else
+        roman = roman + 'V' * ((num%10)/5)
+        roman = roman + 'I' * (num%5)
+    end  
+    roman 
+end 
+
+puts old_school_roman num
+

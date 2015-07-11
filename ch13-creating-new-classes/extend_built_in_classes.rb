585b48d75947dce5728b3c52b1410466ed28b9e4
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-11 13:48:27 +0200

    ch13-creating-new-classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,14 @@
 class Integer
-  # your code here
+  def factorial
+  	num = self
+  	if num < 0
+		return 'You can\'t take the factorial of a negative number!'
+	elsif num <= 1 
+		1
+	else
+		num * (num-1).factorial
+	end 
 end
+end
+ 
+puts 10.factorial

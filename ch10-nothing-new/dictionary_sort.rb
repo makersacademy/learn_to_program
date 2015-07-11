f72e91d26b291724f7e6fd120ff27b6856f12ddf
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-11 13:45:40 +0200

    ch10-nothing-new

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,11 @@
 def dictionary_sort arr
-  # your code here
-end
+  new_arr = []
+
+  arr.each do |item|
+  	item == item.capitalize
+  	new_arr.push item 
+  end 
+  puts new_arr.sort
+end 
+
+dictionary_sort(['Can','feel','singing.','like','A','cin'])
--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,100 @@
 def english_number number
-  # your code here
-end
+
+  if number < 0 
+  	return 'Please enter a number that isn\'t negative.' 
+  end
+
+  if number == 0 
+  	return 'zero'
+  end 
+
+  num_string = ''
+
+  ones_place=['one',       'two',      'three',
+              'four',      'five',     'six',
+              'seven',     'eight',    'nine']
+  tens_place = ['ten',       'twenty',   'thirty',
+                'forty',     'fifty',    'sixty',
+                'seventy',   'eighty',   'ninety']
+  teenagers  = ['eleven',    'twelve',   'thirteen',
+                'fourteen',  'fifteen',  'sixteen',
+                'seventeen', 'eighteen', 'nineteen']
+
+  left = number
+
+  write = left/1000000000 
+  left = left - write*1000000000
+
+  if write > 0
+  	billions = english_number write
+  	num_string = num_string + billions + ' billion ' 
+  	if left > 0
+  		num_string = num_string + ''
+  	end
+  end
+
+  write = left/1000000 
+  left = left - write*1000000
+
+  if write > 0
+  	millions = english_number write
+  	num_string = num_string + millions + ' million ' 
+  	if left > 0
+  		num_string = num_string + ''
+  	end
+  end 
+
+
+  write = left/1000 
+  left = left - write*1000
+
+  if write > 0
+  	thousands = english_number write
+  	num_string = num_string + thousands + ' thousand ' 
+  	if left > 0
+  		num_string = num_string + ''
+  	end
+  end 
+
+  write = left/100 
+  left = left - write*100
+
+  if write > 0
+  	hundreds = english_number write
+  	num_string = num_string + hundreds + ' hundred ' 
+  	if left > 0
+  		num_string = num_string + ''
+  	end
+  end 
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+  	if ((write == 1) and (left > 0))
+  		num_string = num_string + teenagers[left-1]
+  		left = 0
+  	else 
+  		num_string = num_string + tens_place[write-1]
+  	end 
+
+  	if left > 0 
+  		num_string = num_string + '-'
+  	end 
+  end 
+
+  write = left
+  left = 0
+
+  if write > 0 
+  	num_string = num_string + ones_place[write-1]
+  end 
+
+  num_string
+end 
+
+puts english_number(1030555222)
+
+
+
+

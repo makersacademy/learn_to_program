5bd3545ea608540cbd638c67cd474b739b3570e7
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-11 13:49:40 +0200

    ch14-blocks-and-procs

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
-  # your code here
-end
+def log desc, &block 
+	puts "Beginning #{desc.inspect}..."
+	result = block.call
+	puts "... #{desc.inspect} finished, returning:" + result.to_s
+end 
+
+
+log 'outer block' do 
+	log ' some little block' do 1+2*5
+	end 
+
+	log 'yet another block' do 
+		33**2
+	end 
+	'0' == 0
+end 

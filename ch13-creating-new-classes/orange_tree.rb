0326d13b28b3af713986239549a231b0cf4ad789
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-12 16:06:41 +0100

    Finally got orange tree passing all tests - yay

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -3,54 +3,66 @@ class OrangeTree
   def initialize
     @orange_count = 0
     @height = 0
-    @age = 0
+    @alive = true
 
-    one_year_passes
+    "You plant your tree."
   end
 
   def height
-    @height
-  end
-
-  def age
-    @age
+    if @alive
+      @height
+    else
+      "A dead tree is not very tall. :("
+    end
   end
 
   def one_year_passes
-    if @age < 15
-      @age = @age + 1
-      @height = @height + 1
-      fruit
-      puts "This year your tree grew to #{@height}m tall, and produced #{count_the_oranges} oranges."
-    elsif @age < 50
-      @age = @age + 1
-      fruit
-      puts "This year your tree grew to #{@height}m tall, and produced #{count_the_oranges} oranges."
+    if @alive
+      if @height < 10
+        @height = (@height + 0.4).round(1)
+        fruit
+        "This year your tree grew to #{@height}m tall, and produced #{@orange_count} oranges."
+      else
+        death
+      end
     else
-      death
+      "A year later, the tree is still dead. :("
     end
-
   end
 
   def count_the_oranges
-    @orange_count
+    if @alive
+      @orange_count
+    else
+      "A dead tree has no oranges. :("
+    end
   end
 
   def pick_an_orange
-    puts "That is one delicious orange!"
-    @orange_count = @orange_count - 1
+    if @alive
+      if @orange_count > 0
+        "That is one delicious orange!"
+        @orange_count = @orange_count - 1
+      else
+        "There are no oranges to pick."
+      end
+    else
+      "A dead tree has nothing to pick. :("
+    end
   end
 
-  private
   def death
-    puts "The tree is too old and dies."
-    exit(0)
+    @alive = false
+    "Oh, no! The tree is too old, and has died. :("
   end
 
   def fruit
-    if @age > 3
-      @orange_count = 5 * @age
+    if @height > 1
+      @orange_count = (15 * @height - 25).to_i
     end
   end
 end
+
+
+
 

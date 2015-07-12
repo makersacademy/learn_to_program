cd2da6c680809afe83f375a17a93aed153c36d19
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-12 14:33:25 +0100

    Solution to program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
 def log desc, &block
-  # your code here
+  puts "Beginning \"#{desc}\"..."
+  result = block.call
+  puts "...\"#{desc}\" finished, returning: #{result}"
+end
+
+log "outer block" do
+  log "some little block" do
+    "#{4 + 1}"
+  end
+  log "yet another block" do
+    "I like Thai food!"
+  end
+  false
 end

3f43b32dfe0930fca704640e2d989a39c72cee62
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-12 14:54:13 +0100

    Solution to better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
+$depth = -1
 def log desc, &block
-  # your code here
+  $depth += 1
+  indent = " " * $depth
+  puts "#{indent}Beginning \"#{desc}\"..."
+  result = block.call
+  puts "#{indent}...\"#{desc}\" finished, returning: #{result}"
+  $depth -= 1
+end
+
+log "outer block" do
+  log "some little block" do
+    log "teeny-tiny block" do
+      "lots of love"
+    end
+    "#{6 * 7}"
+  end
+  log "yet another block" do
+    "I like Indian food!"
+  end
+  true
 end

8176d5ef6bf247d9aa0db51d35f328b1cdf2a8d6
Balint Nemes <squarebe03@gmail.com>
2015-07-13 19:48:51 +0100

    prg-logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,19 @@
 def log desc, &block
-  # your code here
+	puts 'Beginning "'+block_description+'" . . .' 
+	value_returned = block.call
+	puts '. . . "'+block_description+'" finished, returning:'
+	puts value_returned
+end
+
+log 'outer block' do
+
+log 'some little block' do
+	5
+end
+
+log 'yet another block' do
+	'I like Thai food!'
+end
+
+false
 end

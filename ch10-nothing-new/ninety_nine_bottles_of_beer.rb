6c1512ee6a2f5b880b3a813c9d7956d20c803528
Balint Nemes <squarebe03@gmail.com>
2015-07-13 18:22:45 +0100

    99-beers

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,13 @@
-# your code here
+current_bottles = 99
+next_bottles = current_bottles -1
+
+while current_bottles > 0
+ puts current_bottles.to_s + ' bottles of bears on the wall. ' + current_bottles.to_s + ' bottles of bears on the wall.'
+ puts 'You take one down and pass it around. ' + next_bottles.to_s + ' bottles of beers left on the wall.'
+
+ current_bottles = current_bottles - 1
+ next_bottles = current_bottles - 1
+ if current_bottles == 0
+ break
+ end
+end

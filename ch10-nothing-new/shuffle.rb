f128ee4322852c69d3a51fa55ea4f79443fbf853
ajitsy <sanghera.p@gmail.com>
2015-07-13 21:31:43 +0100

    Remaining exercise solutions for chapter 10

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,19 @@
-def shuffle arr
-  # your code here
+
+def shuffle(unsorted_arr, shuffled_array)
+    if unsorted_arr.length <= 0
+        return
+    end
+    x = 0 + rand(unsorted_arr.length)
+
+    shuffled_array.push(unsorted_arr[x])
+    unsorted_arr.delete(unsorted_arr[x])
+    shuffle(unsorted_arr, shuffled_array)
 end
+
+unsorted_arr = []
+shuffled_array = []
+unsorted_arr.push gets.chomp while unsorted_arr.last != ''
+unsorted_arr.pop
+
+shuffle(unsorted_arr, shuffled_array)
+puts shuffled_array

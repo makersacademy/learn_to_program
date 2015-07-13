ae3701c40c75cb86cd8775ad672ab2b9867ef274
Balint Nemes <squarebe03@gmail.com>
2015-07-13 18:52:36 +0100

    happy-Bday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts 'Which year you were born?'
+year = gets.chomp.to_s
+puts 'Which month you were born?'
+month = gets.chomp.to_s
+puts 'On what day you were born?'
+day = gets.chomp.to_s
+# puts 'Your day of born then: ' + day + ', ' + month + ', ' + year + '.'
+def calculate_number_of_years(year, month, day)
+	minutes = Time.new - Time.mktime(year, month, day)
+	years = ((minutes/525600)/60).to_i
+end
+years = calculate_number_of_years(year, month, day)
+
+puts 'Now you are gonna get just enough spanking!'
+puts
+puts 'SPANK!' * years

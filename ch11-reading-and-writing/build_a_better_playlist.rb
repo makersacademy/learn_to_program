157dca3be6d4a73dcd958f5725f5dfb546fcd0bf
Balint Nemes <squarebe03@gmail.com>
2015-07-13 18:43:13 +0100

    better-PL

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,33 @@
-def music_shuffle filenames
-  # your code here
+def music_shuffle array
+	x = 0
+	track_number = 0
+	number_songs = 0
+	playlist = [ ]
+	full_song_name = [ ]
+	album_on_deck = ''
+	previous_album = ''
+	number_songs = array.length
+	while x < number_songs
+		track_number = rand(number_songs)
+		full_song_name = array[track_number].split'/'
+		album_on_deck = full_song_name[2]
+		if album_on_deck != previous_album
+			if array[track_number] != ''
+				playlist.push array[track_number]
+				array[track_number] = ''
+				x = x + 1
+			else
+			end
+			previous_album = album_on_deck
+		else 
+		end
+	end
+	filename = 'Desktop/playlist.m3u'
+	File.open filename, 'w' do |f|
+		f.puts playlist
+	end
 end
+
+tracks = Dir['Desktop/music/**/*.{ogg,mp3,m4a}']
+
+music_shuffle tracks

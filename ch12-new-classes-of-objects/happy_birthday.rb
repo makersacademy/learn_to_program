22c5bca6566d0318425eeb5245b53ddc3986990d
Ben Hawker <ben.c.hawker@gmail.com>
2015-07-14 12:43:59 +0100

    Update happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+#Happy Brthday
+
+puts "Your Birth year please"
+birth_year = gets.chomp.to_i
+
+puts "Your birth month please"
+birth_month = gets.chomp.to_i
+
+puts "and finally....What day of the month where you born?"
+birth_day = gets.chomp.to_i
+
+timeNow = time.new
+timeBorn = time.local(birth_year, birth_month, birth_day)
+
+age = 1
+
+while timeNow <= timeBorn
+	puts "Spank!"
+	age += 1
+end	

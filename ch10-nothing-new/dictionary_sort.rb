be6c74b3fb30d070c99bb4048a52db1bea89ddf3
Ben Hawker <ben.c.hawker@gmail.com>
2015-07-14 12:40:37 +0100

    Update dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
-def dictionary_sort arr
-  # your code here
+#Dictionary Sort - define method to sort alphabetically including capitals/lowercase & ensure it returns original string
+
+def dictionary_sort some_array
+  recursive_dictionary_sort some_array, []
 end
+
+def recursive_dictionary_sort unsorted_array, sorted_array 
+  if unsorted_array != []
+    smallest = 'z'
+    unsorted_array.each do |word|
+      smallest = word if word.downcase < smallest
+    end
+    sorted_array << smallest
+    unsorted_array.delete_at(unsorted_array.index(smallest)) if smallest != nil
+    recursive_dictionary_sort unsorted_array, sorted_array
+  else
+    sorted_array
+  end
+end
+
+array = ['bob', 'joe', 'timmy', 'Ben', 'ben', 'ilona', 'Ilona']
+
+puts dictionary_sort(array)

efedf11ea38ba2389e95c43bac98f176957efd56
Ben Hawker <ben.c.hawker@gmail.com>
2015-07-14 12:39:08 +0100

    Update roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,20 @@
-def roman_numeral num
-  # your code here
+
+
+def new_roman_numeral number
+  numbers =   [1000, 900, 500, 400, 100,90, 50, 40, 10, 9, 5, 4, 1]
+  numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  roman_numeral = " "
+ 
+  numbers.length.times do |x|
+    while number >= numbers[x]
+      number -= numbers[x]
+      roman_numeral += numerals[x]
+    end
+  end
+ 
+  roman_numeral
 end
+ 
+print "Enter a number and we will convert: "
+ 
+puts new_roman_numeral(gets.chomp.to_i)

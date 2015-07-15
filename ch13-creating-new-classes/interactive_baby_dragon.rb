d164957e529c497289f87df764dd08137d1a50d6
Ben Hawker <ben.c.hawker@gmail.com>
2015-07-15 08:42:03 +0100

    Update interactive_baby_dragon.rb

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,25 @@
-# your code here
+puts 'What would shall we call your Baby Dragon?'
+name = gets.chomp
+pet = Dragon.new name
+
+while true
+  puts
+  puts 'commands: feed, toss, walk, rock, put to bed, exit'
+  command = gets.chomp
+
+  if command == 'exit'
+    return exit
+  elsif command == 'feed'
+    pet.feed
+  elsif command == 'toss'
+    pet.toss
+  elsif command == 'walk'
+    pet.walk
+  elsif command == 'rock'
+    pet.rock
+  elsif command == 'put to bed'
+    pet.put_to_bed
+  else
+    puts 'Huh? Need a command!'
+  end
+end

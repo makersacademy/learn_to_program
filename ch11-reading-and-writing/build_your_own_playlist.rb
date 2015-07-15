c740d9ac92ebe04e19925e8657a14665794faa74
ajitsy <sanghera.p@gmail.com>
2015-07-15 21:10:13 +0100

    Completed exercises for ch11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,21 @@
-# your code here
+
+# This is where she stores her pictures before
+# she gets her YAML on and moves them to the server.
+# Just for my own convenience, I'll go there now.
+Dir.chdir '/home/ps/Music'
+# First we find all of the pictures to be moved.
+songs = Dir['**/*.mp3']
+songs.shuffle
+
+puts 'What would you like to call this playlist?'
+playlist_name = gets.chomp
+
+playlist_file = "#{playlist_name}.m3u"
+
+File.open playlist_file, 'w' do |f|
+  f.write songs
+end
+
+read_string = File.read playlist_file
+
+puts 'Done, cutie!'

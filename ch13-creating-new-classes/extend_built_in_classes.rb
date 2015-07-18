4ac00e60f4a06d3c814e08c1b9c49c6843b38077
ajitsy <sanghera.p@gmail.com>
2015-07-18 20:18:43 +0100

    Solutions to exercises from ch13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,15 @@
 class Integer
-  # your code here
+  def to_factorial
+    if self < 0
+      return 'You can\'t take the factorial of a negative number!'
+    end
+    if self <= 1
+      1
+    else
+      self * (self-1).to_factorial
+    end
+  end
 end
+
+puts 3.to_factorial
+puts 30.to_factorial
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,73 @@
 class OrangeTree
-  # your code here
+  def initialize
+    @treeHeight = 0
+    @treeAge = 0
+    @oranges = 0
+  end
+
+  def height
+        puts "Your tree is now #{@treeHeight}m high"
+  end
+
+  def one_year_passes
+    passage_of_time
+  end
+
+  def count_the_oranges
+    puts "The tree has #{@oranges} oranges!"
+  end
+
+  def pick_an_orange
+    @oranges -= 1
+    if @oranges <= 0
+      puts "No more oranges to eat I'm afraid!"
+    else
+      puts "That was a delicious orange!"
+    end
+  end
+
+  private
+
+  def treeDeath?
+    @treeAge >= 10
+  end
+
+  def orangeCount?
+    @treeAge >= 3
+  end
+
+  def passage_of_time
+    if treeDeath?
+      puts "The tree has died"
+      exit
+    else
+      @treeAge += 1
+      @treeHeight += 0.3
+    end
+
+    if orangeCount?
+      @oranges = 0
+      @oranges += (3*@treeAge*2)
+    end
+  end
 end
+
+mytree = OrangeTree.new
+mytree.one_year_passes
+mytree.one_year_passes
+mytree.height
+mytree.one_year_passes
+mytree.count_the_oranges
+mytree.pick_an_orange
+mytree.count_the_oranges
+mytree.one_year_passes
+mytree.one_year_passes
+mytree.one_year_passes
+mytree.count_the_oranges
+mytree.pick_an_orange
+mytree.count_the_oranges
+mytree.one_year_passes
+mytree.one_year_passes
+mytree.one_year_passes
+mytree.one_year_passes
+mytree.one_year_passes

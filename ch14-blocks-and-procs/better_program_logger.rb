848972c7116b70c32cc0d5c80702f249e5efaaa9
ajitsy <sanghera.p@gmail.com>
2015-07-19 00:56:20 +0100

    Solutions to exercises in ch14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,13 @@
 def log desc, &block
-  # your code here
+  puts "Beginniner outer block"
+  test = block.call
+  puts "'#{desc}' finished and returned: #{test.to_s}"
+
+end
+
+log "this block" do
+  log "a different block" do
+   "hello"
+  end
+  "hello two"
 end
--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
 def profile block_description, &block
-  # your code here
+  Profile_on == false
+      if Profile_on
+        start_time = Time.new
+        block.call
+        duration = Time.new - start_time
+        puts "#{block_description}: #{duration} seconds"
+      else
+        block.call
+      end
 end

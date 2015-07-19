a5193390c0f7eaa7eee4b3bc272ec8e7600f0990
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-07-19 20:56:49 +0100

    Completing Pine Challenges

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,103 @@
-# your code here
+def english_number number
+  if number < 0 #no negative numbers
+    return "Please enter a number that isn't negative."
+  end
+  if number == 0
+    return "zero"
+  end
+
+  num_string ="" #String which will be returned
+
+  ones_place = ["one","two","three","four","five","six","seven","eight","nine"]
+  tens_place = ["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+  teenagers = ["eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+  big_ones = {
+    "thousand" => 3,
+    "million"  => 6,
+    "billion" => 9,
+    "trillion" => 12,
+    "quadrillion" => 15,
+    "quintillion" => 18,
+    "sextillion" => 21,
+    "septillion" => 24,
+    "octillion" => 27,
+    "nonillion" => 30,
+    "decillion" => 33,
+    "undecillion" => 36,
+    "duodecillion" => 39,
+    "tredecillion" => 42,
+    "quattuordecillion" => 45,
+    "quindecillion" => 48,
+    "sexdecillion" => 51,
+    "octodecillion" => 57,
+    "novemdecillion" => 60,
+    "vigintillion" => 63,
+    "centillion" => 303
+  }
+
+  #left is how much of the number we have left to write out
+  #"write" is the part we are writing out now
+
+  left = number
+
+  big_ones.to_a.reverse.to_h.each do |word, power|
+    write = left/(10 ** power)
+    left = left - (write * (10 ** power))
+
+    if write > 0
+      adding = english_number write
+      num_string = num_string + adding + " #{word}"
+      if left > 0
+        num_string = num_string + " "
+      end
+    end
+  end
+
+  write = left/100
+  left = left - write * 100
+
+  if write > 0
+    hundreds = english_number write
+    num_string = num_string + hundreds + " hundred"
+    if left > 0
+      num_string = num_string + " "
+    end
+  end
+
+  write = left/10
+  left = left - write * 10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
+    if left > 0
+      num_string = num_string + "-"
+    end
+  end
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+
+  num_string
+end
+
+
+def bottles number
+  bottles_left = number
+  while bottles_left > 2
+    puts english_number(bottles_left) + " bottles of beer on the wall, " + english_number(bottles_left)+ " bottles of beer!"
+    puts "Take one down, pass it around " + english_number(bottles_left - 1).to_s + " bottles of beer on the wall!"
+    bottles_left -= 1
+  end
+  puts "two bottles of beer on the wall, two bottles of beer!"
+  puts "take one down, pass it around, one bottle of beer on the wall!"
+  puts "one bottle of beer on the wall, one bottle of beer!"
+  puts "take one down, pass it around, no more bottles of beer on the wall!"
+end

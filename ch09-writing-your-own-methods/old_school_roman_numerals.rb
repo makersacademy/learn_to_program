09ab5f90f3163799858a6c3e2459044e0e01b3ed
chweeks <chrisharryweeks@gmail.com>
2015-08-09 12:04:47 +0100

    updated method

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,14 @@
+
 def old_roman_numeral num
-  # your code here
+  numerals = ''
+
+  numerals << 'M' * (num / 1000)
+  numerals << 'D' * (num % 1000 / 500)
+  numerals << 'C' * (num % 500 / 100)
+  numerals << 'L' * (num % 100 / 50)
+  numerals << 'X' * (num % 50 / 10)
+  numerals << 'V' * (num % 10 / 5)
+  numerals << 'I' * (num %5 / 1)
+
+  numerals 
 end

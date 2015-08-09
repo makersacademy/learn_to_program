b5f01162cc0bb5c97760b96dddfdf6b98862e50f
chweeks <chrisharryweeks@gmail.com>
2015-08-09 20:47:07 +0100

    Updated roman_numeral method

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,13 @@
 def roman_numeral num
-  # your code here
+  numerals = ''
+
+  numerals << 'M' * (num / 1000)
+  num % 1000 % 500 != 400 ? numerals << 'D' * (num % 1000 / 500) : (num % 1000 == 900 ? numerals << 'CM' : numerals << 'CD')
+  numerals << 'C' * (num % 500 / 100)
+  num % 100 % 50 !=40 ? numerals << 'L' * (num % 100 / 50) : (num % 100 == 90 ? numerals << 'XC' : numerals << 'XL')
+  numerals << 'X' * (num % 50 / 10)
+  num % 10 % 5 != 4 ? numerals << 'V' * (num % 10 / 5) : (num % 10 == 9 ? numerals << 'IX' : numerals << 'IV') 
+  num % 5 != 4 ? numerals << 'I' * (num %5 / 1) : '' 
+
+  numerals 
 end

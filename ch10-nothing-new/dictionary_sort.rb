d2a34bf0ea19f521be24088a9ca299118a806a2d
chn.challenger <chn.challenger@gmail.com>
2015-08-11 20:02:21 +0100

    Completed ch10 dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,31 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(array,order='ascend')
+	number_of_items = array.length
+	number_of_swaps = 0
+	for x in 0...(number_of_items-1)
+		if order == 'ascend'
+			if  array[x].downcase > array[x+1].downcase
+				holdx = array[x]
+				array[x] = array[x+1]
+				array[x+1] = holdx
+				number_of_swaps += 1
+			end
+		else
+			if  array[x].downcase < array[x+1].downcase
+				holdx = array[x]
+				array[x] = array[x+1]
+				array[x+1] = holdx
+				number_of_swaps += 1
+			end
+		end
+	end
+	if number_of_swaps == 0
+		return array
+	else
+		dictionary_sort(array,order)
+	end
 end
+
+array = ['hello','Alien','we','time']
+puts "#{dictionary_sort(array)}"
+
+

dbade0c7d75fed27f6b38f959f6e4de7f57e54b1
chn.challenger <chn.challenger@gmail.com>
2015-08-11 19:34:33 +0100

    Completed chpt 9 exes

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,12 @@
 def old_roman_numeral num
-  # your code here
+  	num_of_m = (num/1000).to_i
+	num_of_d = ((num%1000)/500).to_i
+	num_of_c = ((num%500)/100).to_i
+	num_of_l = ((num%100)/50).to_i
+	num_of_x = ((num%50)/10).to_i
+	num_of_v = ((num%10)/5).to_i
+	num_of_i = (num%5).to_i
+
+	"#{'M'*num_of_m}#{'D'*num_of_d}#{'C'*num_of_c}#{'L'*num_of_l}#{'X'*
+	num_of_x}#{'V'*num_of_v}#{'I'*num_of_i}"
 end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,43 @@
-def roman_numeral num
-  # your code here
+def romnum_1_to_9(num, one, five, ten, modula)
+
+	m = ((num % (modula*10)) / modula ).to_i
+	num_of_five = (m/5).to_i
+	num_of_one = m%5 
+	
+	if num_of_five == 1 
+		if num_of_one == 4
+			rom_num = one + ten 
+		else
+			rom_num = five + one*num_of_one
+		end		
+	else #num_of_five == 0
+		if num_of_one == 4
+			rom_num = one + five
+		else
+			rom_num = one*num_of_one
+		end
+	end
+
+	return rom_num
 end
+
+#puts romnum_1_to_9(99, 'X', 'Y', 'Z', 10)
+
+def roman_numeral num
+	if num > 999999
+		return "number too big for roman numerals"
+	end
+
+	rom_digits = ['I','V','X','L','C','D','M','P','Q','R','S','T','A','B','E','F']
+	num_of_digits = (num).to_s.length
+	roman_num = ''
+
+	for i in 0...num_of_digits
+		roman_num = romnum_1_to_9(num,rom_digits[i*2],rom_digits[i*2+1],rom_digits[i*2+2],10**i) + roman_num
+	end
+	
+	return roman_num
+
+end
+
+#puts roman_numeral(3497)

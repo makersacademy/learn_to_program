ecfdcc23eb29f53b74d2dd3ab52174881dc8bef0
Harriet Craven <harrietc52@hotmail.co.uk>
2015-08-11 17:54:19 +0100

    Updated old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+  roman = ''
+
+  roman = roman + 'M' * (num / 1000)
+  roman = roman + 'D' * (num % 1000 / 500)
+  roman = roman + 'C' * (num % 500 / 100)
+  roman = roman + 'L' * (num % 100 / 50)
+  roman = roman + 'X' * (num % 50 / 10)
+  roman = roman + 'V' * (num % 10 / 5)
+  roman = roman + 'I' * (num % 5 / 1)
+  roman
 end
+puts old_roman_numeral 3766

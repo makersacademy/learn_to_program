8644e3855e42c685412dba884b9f77cb1636d550
chn.challenger <chn.challenger@gmail.com>
2015-08-11 19:49:53 +0100

    Completed ch10 shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,13 @@
-def shuffle arr
-  # your code here
+def shuffle array
+	array_to_shuffle = []
+	for i in 0...array.length
+		array_to_shuffle << array[i]
+	end 
+	shuffled_array = []
+	for i in 0...array.length
+		selected = array_to_shuffle.sample
+		shuffled_array << selected
+		array_to_shuffle.delete(selected)
+	end
+	return shuffled_array
 end

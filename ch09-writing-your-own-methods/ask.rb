fd9212e5f63e71c4f23247ad88658796d7a3dc71
ALRW <awerner1@googlemail.com>
2015-08-12 14:56:17 +0100

    Added all Ch09 Tasks

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,13 @@
-def ask question
-  # your code here
+def ask(question)
+	while true
+		puts question
+		reply = gets.chomp.downcase
+		if reply == 'yes'
+			return true			
+		end
+		if reply == 'no'
+			return false
+		end
+		puts 'Please answer "Yes" or "No"'
+	end
 end
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,11 @@
-def old_roman_numeral num
-  # your code here
+def roman(num)
+	romanNumeral = ''
+	romanNumeral = romanNumeral + 'M' * (num / 1000)
+	romanNumeral = romanNumeral + 'D' * (num % 1000 / 500)
+	romanNumeral = romanNumeral + 'C' * (num % 500 / 100)
+	romanNumeral = romanNumeral + 'L' * (num % 100 / 50)
+	romanNumeral = romanNumeral + 'X' * (num % 50 / 10)
+	romanNumeral = romanNumeral + 'V' * (num % 10 / 5)
+	romanNumeral = romanNumeral + 'I' * (num % 5 / 1)
+	romanNumeral
 end

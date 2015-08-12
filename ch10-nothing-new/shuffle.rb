eec19cc4e1644fa9da55bfbda6ac7f183ce16d84
Harriet Craven <harrietc52@hotmail.co.uk>
2015-08-12 12:56:52 +0100

    Updated shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
 def shuffle arr
-  # your code here
+  #make new array to put in shuffled values
+  shuffled_array = Array.new
+  counter = 1
+  until arr.empty?
+    #get value out of array
+    rand_value = arr.pop
+    #insert value into array at random index
+    shuffled_array.insert(rand(counter), rand_value)
+    counter += 1
+  end
+  arr = shuffled_array
+  return arr
 end

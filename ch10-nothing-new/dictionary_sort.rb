df78969172ca68433dfb7e5b3063079ee915ff9b
Proskurina <yproskurina@yahoo.com>
2015-08-13 19:00:54 +0200

    Add dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
 def dictionary_sort arr
-  # your code here
+  recursive_dict_sort(arr, [])
+end
+
+def recursive_dict_sort(unsorted_array, sorted_array)
+	
+	if unsorted_array.length == 0
+		return sorted_array
+	end
+	
+	smallest = unsorted_array[0]
+	unsorted_array.each do |word|
+		if word.downcase<smallest.downcase
+			smallest = word
+		end
+	end
+	
+	unsorted_array.delete_at(unsorted_array.index(smallest))
+	sorted_array.push(smallest)
+	
+	recursive_dict_sort(unsorted_array, sorted_array)
+
 end

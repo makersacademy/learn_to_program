2a61d6e747f45c2422cb8c3aa6ed7690f83776af
Proskurina <yproskurina@yahoo.com>
2015-08-13 19:01:27 +0200

    Add shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,21 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle(arr, [])
 end
+
+
+def recursive_shuffle(array, shuffled_array)
+	
+	if array.length == 0
+		return shuffled_array
+	end
+
+	random = array.sample
+	
+	array.delete_at(array.index(random))
+	shuffled_array.push(random)
+	
+	recursive_shuffle(array, shuffled_array)
+
+end
+
+puts shuffle([1,2,3,4,5])

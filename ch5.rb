d2f2eeb0257fcb301f47dc2831ce78c8bf16eade
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-08-18 00:15:07 +0100

    added ch5 exercise

---
+++ ch5.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, and what\'s your first name?'
+first_name = gets.chomp
+puts 'And you middle name?'
+middle_name = gets.chomp
+puts 'Finally, what is you last name?'
+last_name = gets.chomp
+puts 'Pleased to meet you, ' + first_name + middle_name + last_name + ' !'

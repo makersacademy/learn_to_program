9e6271175d959b03bcccf156a3ef00c68accf6d3
Emily Worrall <worrall.emily@gmail.com>
2015-08-17 15:58:50 +0100

    Old school roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
 def old_roman_numeral num
-  # your code here
+
+  roman_num = ''           # => ""
+
+  roman_num = roman_num + 'M' * (num / 1000)        # => ""
+  roman_num = roman_num + 'D' * (num % 1000 / 500)  # => "D"
+  roman_num = roman_num + 'C' * (num % 500 / 100)   # => "DCCCC"
+  roman_num = roman_num + 'L' * (num % 100 / 50)    # => "DCCCC"
+  roman_num = roman_num + 'X' * (num % 50 / 10)     # => "DCCCCXXX"
+  roman_num = roman_num + 'V' * (num % 10 / 5)      # => "DCCCCXXXV"
+  roman_num = roman_num + 'I' * (num % 5 / 1)       # => "DCCCCXXXVII"
+
+  roman_num  # => "DCCCCXXXVII"
 end
+
+old_roman_numeral 937  # => "DCCCCXXXVII"

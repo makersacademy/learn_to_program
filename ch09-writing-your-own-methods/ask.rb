2a2f20dcdcf4524d0ebbead938bd8e408c4086c2
Emily Worrall <worrall.emily@gmail.com>
2015-08-17 15:26:48 +0100

    Refined ask method

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -3,33 +3,14 @@ def ask question
     puts question
     reply = gets.chomp.downcase
 
-    if (reply == 'yes' || reply == 'no')
-      if reply == 'yes'
-        return true
-      else
-        return false
-      end
-
-    else
-      puts 'Please answer "yes" or "no".'
+    if reply == 'yes'
+      return true
+    elsif reply == 'no'
+      return false
+    else puts 'Please answer "yes" or "no".'
     end
+
   end
 end
 
-puts 'Hello, and thank you for...'
-puts
-
-ask 'Do you like eating tacos?'
-ask 'Do you like eating burritos?'
-wets_bed = ask 'Do you wet the bed?'
-ask 'Do you like eating chimichangas?'
-ask 'Do you like eating sopapillas?'
-puts 'Just a few more questions...'
-ask 'Do you like drinking horchata?'
-ask 'Do you like eating flautas?'
-
-puts
-puts 'DEBRIEFING:'
-puts 'Thank you for...'
-puts
-puts wets_bed
+ask 'Do you like cake?'

7363a38203afff04d50951a0f8d2adc572d4c746
chn.challenger <chn.challenger@gmail.com>
2015-08-17 16:28:42 +0100

    Only issue with ch14 left

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,47 @@
 def log desc, &block
-  # your code here
+  $indent ||= 0   #set to 0 unless already defined
+  puts ' '*$indent+"Beginning #{desc.inspect}..."
+  $indent += 1
+  result = block.call
+  $indent -= 1
+  puts ' '*$indent+"...#{desc.inspect} finished, returning: #{result}"
 end
+
+
+=begin
+log 'outer-block' do  
+	log 'inner-block1' do n = 100; n end
+	log 'inner-block2' do
+	r = 0
+	2000.times{r+=1}
+	end
+end
+=end
+
+
+$indent_pl = 0
+def profile_log desc, &block
+	profile_log_on = true
+	if profile_log_on == true
+		puts "  "*$indent_pl + "Starting the block:  #{desc}"
+		$indent_pl += 1
+		time_start = Time.new
+		result = block.call
+		duration = Time.new - time_start
+		$indent_pl -= 1
+		puts "  "*$indent_pl + "#{desc} finished in #{duration} seconds and returned #{result}."
+	else
+		block.call
+	end
+end
+
+
+=begin
+profile_log 'outer-block' do  
+	profile_log 'inner-block1' do n = 100; n end
+	profile_log 'inner-block2' do
+	r = 0
+	2000.times{r+=1}
+	end
+end
+=end
--- ch14-blocks-and-procs/chpt14.rb
+++ ch14-blocks-and-procs/chpt14.rb
@@ -85,5 +85,23 @@ end
 
 #grandfather_clock(chiming)
 
+def log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
+end
+
 
 

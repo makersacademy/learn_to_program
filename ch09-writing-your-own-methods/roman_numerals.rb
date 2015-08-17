f5d4f7decebeca87460207de8ebeb63285e33a2d
Emily Worrall <worrall.emily@gmail.com>
2015-08-17 16:42:46 +0100

    roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,21 @@
 def roman_numeral num
-  # your code here
+
+  output = ""
+
+  roman_numbers = {
+    "M" => 1000,
+    "CM" => 900, "CX" => 90, "IX" => 9,
+    "D"  => 500, "L"  => 50, "V"  => 5,
+    "CD" => 400, "XL" => 40, "IV" => 4,
+    "C"  => 100, "X"  => 10, "I"  => 1
+  }
+
+  roman_numbers.map do |key, value|
+    output << key * (num / value)
+    num = num % value
+  end
+
+output
 end
+
+roman_numeral 4

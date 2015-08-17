351e271d2a4f7d2b368b7dc083a765228871c4d6
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-08-17 23:43:11 +0100

    added ch3 exercises

---
+++ ch3.rb
@@ -0,0 +1,18 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+puts 'I like' + ' apple pie'
+puts 'blink ' * 4
+puts ''
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts ''
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts ''
+puts 'You\'re swell!'
+puts 'backlash at the end ofa string: \\'
+puts 'up\\down'
+puts 'up\down'

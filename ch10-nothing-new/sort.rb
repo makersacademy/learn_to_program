bf99aa39469f2863f54cd0b393a37ee34ac06375
philip <philip.clarke@redhood.co.uk>
2015-08-18 08:43:55 +0200

    sort array with recursive pivot

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,17 @@
-def sort arr
-  # your code here
+def sort array
+  if array.length <= 1
+  	return array
+  	
+  else
+  	pivot = array.pop
+  	less = array.select {|word| word < pivot}
+
+  	more = array.select {|word| word >= pivot}
+  	sorted = sort(less) + [pivot] + sort(more)
+  end
+  return sorted
 end
+
+words = ['hat','coat','bed','door','door', 'zoo']
+
+puts sort(words)

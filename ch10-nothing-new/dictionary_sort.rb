f421b4e1d6efa9f4856c848d348819ee447fe9c1
Michael Roger <bodgeuk12@gmail.com>
2015-08-18 23:26:57 +0100

    ch10 dictionary_sort.rb complete - same as sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,37 @@
 def dictionary_sort arr
-  # your code here
+	recursive_sort arr, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+	i = 0
+	while unsorted_array.length > 1
+		duplicate = unsorted_array.count(unsorted_array[i])
+		n = 0
+		(unsorted_array - [unsorted_array[i]]).each do |a|
+			if unsorted_array[i].upcase <= a.upcase
+				n = n + 1
+			end
+		end
+		if n == (unsorted_array - [unsorted_array[i]]).length
+			sorted_array.push [unsorted_array[i]]*duplicate
+			unsorted_array = (unsorted_array - [unsorted_array[i]])
+			i = 0
+		else
+			i = i + 1
+		end
+	end
+	sorted_array.push unsorted_array[0]
+	puts sorted_array
+end
+
+
+puts "Enter the list of words to be sorted..."
+word = gets.chomp
+array = []
+
+while word!= ""
+	array.push word
+	word = gets.chomp
+end
+
+sort array

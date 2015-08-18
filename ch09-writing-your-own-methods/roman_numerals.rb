cae03df172d1279b5935eda58ff63accbf876c2a
Tim Hyson <tjhyson@gmail.com>
2015-08-18 16:26:07 +0100

    interactive method added to convert integer to 'modern' Roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,41 @@
 def roman_numeral num
-  # your code here
+  m = (num        / 1000)
+  c = (num % 1000 /  100)
+  x = (num %  100 /   10)
+  i = (num %   10 /    1)
+  
+  roman = 'M' * m
+
+  if    c == 9
+  	roman = roman + 'CM'
+  elsif c == 4
+  	roman = roman + 'CD'
+  else
+  	roman = roman + 'D' * (num % 1000 / 500)
+  	roman = roman + 'C' * (num %  500 / 100)
+	end
+
+	if 		x == 9
+		roman = roman + 'XC'
+	elsif x == 4
+		roman = roman + 'XL'
+	else
+		roman = roman + 'L' * (num %  100 /  50)
+		roman = roman + 'X' * (num %   50 /  10)
+	end
+
+	if    i == 9
+		roman = roman + 'IX'
+	elsif i == 4
+		roman = roman + 'IV'
+	else
+		roman = roman + 'V' * (num %   10 /   5)
+		roman = roman + 'I' * (num % 	  5 /   1)
+	end	
+
+	puts roman	
 end
+ 
+puts 'What number would you like converting into Roman numerals?'
+num = gets.chomp
+roman_numeral(num.to_i)

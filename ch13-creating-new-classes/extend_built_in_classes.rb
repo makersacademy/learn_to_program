6a0df1315c4ab1f753d5f5f487758c61cf62ed0e
Proskurina <yproskurina@yahoo.com>
2015-08-18 23:35:21 +0100

    Add extend built in classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,29 @@
 class Integer
-  # your code here
+  
+  def factorial
+
+  	if self <=1
+  		1
+  	else
+  		self*(self-1).factorial
+  	end
+
+  end
+
+  def to_roman
+
+	roman_num = ""
+
+	roman_num << "M"*(self/1000)
+	roman_num << "D"*(self%1000/500)
+	roman_num << "C"*(self%500/100)
+	roman_num << "L"*(self%100/50)
+	roman_num << "X"*(self%50/10)
+	roman_num << "V"*(self%10/5)
+	roman_num << "I"*(self%5)
+
+  end
+
 end
+
+

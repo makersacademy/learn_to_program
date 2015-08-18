9a402357cb015a0174efa4effcbed8b2c6ded1c9
Proskurina <yproskurina@yahoo.com>
2015-08-18 15:15:43 +0100

    Add build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,14 @@
-# your code here
+require_relative '../ch10-nothing-new/shuffle'
+
+music_files = shuffle(Dir['/Users/yana/Desktop/music/**/*.mp3'])
+
+filename = 'playlist.m3u'
+
+File.open filename, 'w' do |f|
+	music_files.each do |line|
+		f.write line+"\n"
+	end
+end
+
+puts File.read filename
+

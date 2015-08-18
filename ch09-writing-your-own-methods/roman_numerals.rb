160ed47c0dd7008ae6e1f3f2d46372e112b196cf
philip <philip.clarke@redhood.co.uk>
2015-08-18 07:57:45 +0200

    old school and modern roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,12 @@
-def roman_numeral num
-  # your code here
+def roman_numeral number
+  result = ''
+  result = result + "M" * (number/1000)
+   result = result + "D" * (number % 1000 /500)
+   result = result + "C" * (number % 500 /100)
+   result = result + "L" * (number % 100 /50)
+   result = result + "X" * (number % 50 /10)
+   result = result + "V" * (number % 10 /5)
+   result = result + "I" * (number % 5)
 end
+
+puts roman_numeral(1777)

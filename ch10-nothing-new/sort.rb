9cd6458c9d966c68dd8683eef17cb610a7b0948f
philip <philip.clarke@redhood.co.uk>
2015-08-18 08:53:22 +0200

    dictionary.sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,13 +1,13 @@
 def sort array
-  if array.length <= 1
+  if array.length <= 1  # we cannot sort an empty array
   	return array
   	
   else
-  	pivot = array.pop
-  	less = array.select {|word| word < pivot}
+  	pivot = array.pop #take a word - the last one in the array
+  	less = array.select {|word| word < pivot} #using enummerable create a new array or all the workds "greater" than the pivot word
 
-  	more = array.select {|word| word >= pivot}
-  	sorted = sort(less) + [pivot] + sort(more)
+  	more = array.select {|word| word >= pivot} # and does it for all the words less than the pivot word and the = for similar words
+  	sorted = sort(less) + [pivot] + sort(more) #now do it recursively
   end
   return sorted
 end

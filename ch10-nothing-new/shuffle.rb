bb0ac6921bc9e3cd48fb3fd65c04fb82bc0918dd
Emily Worrall <worrall.emily@gmail.com>
2015-08-18 14:58:52 +0100

    shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,26 @@
 def shuffle arr
-  # your code here
+  shuffled = []
+
+  while arr.length > 0
+    rand_pos = rand(arr.length)
+    curr_pos = 0
+    new_arr = []
+
+    arr.each do |item|
+      if curr_pos == rand_pos
+        shuffled.push item
+      else
+        new_arr.push item
+      end
+
+      curr_pos = curr_pos + 1
+    end
+
+    arr = new_arr
+  end
+
+  shuffled
 end
+
+
+puts(shuffle ([1,2,3,4,5,6,7,8,9]))

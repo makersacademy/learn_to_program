85e7d79a0699efefd33a503125a6e50fa928ee0a
philip <philip.clarke@redhood.co.uk>
2015-08-18 08:03:13 +0200

    updated to remove tests

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,12 +1,19 @@
 def roman_numeral number
-  result = ''
-  result = result + "M" * (number/1000)
-   result = result + "D" * (number % 1000 /500)
+	  result = '' #creates the string to output
+  result = result + "M" * (number/1000) # how many thousands
+   result = result + "D" * (number % 1000 /500) #how many 500 s
+  
    result = result + "C" * (number % 500 /100)
+  
+
    result = result + "L" * (number % 100 /50)
+  
    result = result + "X" * (number % 50 /10)
    result = result + "V" * (number % 10 /5)
    result = result + "I" * (number % 5)
+
+  #now we have a whole lot of exceptions - not v elegant but I will try and gsub them all out
+result.gsub('VIIII','IX').gsub('IIII','IV').gsub('XXXX','XL').gsub('LXL','XC').gsub('DCD','CM').gsub('CCCC','CD')
 end
 
-puts roman_numeral(1777)
+

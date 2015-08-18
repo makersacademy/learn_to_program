459329e616e1212154554f2e7535913baefec0fa
chweeks <chrisharryweeks@gmail.com>
2015-08-18 23:26:19 +0100

    added build_your_own_playlist method

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,9 @@
-# your code here
+def build_playlist
+	Dir.chdir 'C:\Users\Chris\Desktop'
+	songs = Dir['C:/**/*.{MP3,mp3}']
+
+    File.open("Playlist.m3u", "w+"){|playlist| playlist.puts songs.shuffle}
+end
+
+build_playlist
+

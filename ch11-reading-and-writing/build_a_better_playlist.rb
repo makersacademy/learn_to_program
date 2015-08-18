ee9e843e66ca37e66b09e9b38643bb98c5f514f3
philip <philip.clarke@redhood.co.uk>
2015-08-18 17:22:14 +0200

    better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,22 @@
 def music_shuffle filenames
-  # your code here
+
+puts "What do you want to call this playlist"
+name = gets.chomp
+
+playlist = []
+song_name = []
+track_number = 0
+number_of_tracks = filenames.length
+half = number_of_tracks / 2
+puts number_of_tracks.to_s
+track = filenames[1..half].split'/'
+puts track
 end
+
+
+
+songs = ['aaa/bbb','bbb/aaa','aa/bb','AAA/zzz','AAA/aaa']
+puts(music_shuffle(songs))
+
+
+

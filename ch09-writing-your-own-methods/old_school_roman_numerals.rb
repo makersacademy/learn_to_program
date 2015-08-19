448ce4dbe4752b98c17dc2b87c6b1e641889a7d4
davidbebb <davidbebb@gmail.com>
2015-08-19 15:08:27 +0100

    initial commit of old roman numeral

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,27 @@
+#!/usr/bin/env ruby
+
+I = 1 
+one = 'I'
+V = 5 
+five = 'V'
+X = 10
+ten='X'
+L = 50
+fifty='L'
+C = 100 
+onehundred='C'
+D = 500 
+fivehundred='D'
+M = 1000
+onethousand='M'
+
+
+
+
 def old_roman_numeral num
   # your code here
+
 end
+
+old_roman_numeral 3
+

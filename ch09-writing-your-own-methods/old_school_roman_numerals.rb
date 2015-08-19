4339f77917bf749f9c17aff7b365d2e074a6b419
davidbebb <davidbebb@gmail.com>
2015-08-19 15:54:11 +0100

    changed old roman arrays to hash

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -16,12 +16,27 @@ M = 1000
 onethousand='M'
 
 
-
-
 def old_roman_numeral num
-  # your code here
+  	#num_array=[1000,500,100,50,10,5,1]
+	#letter_array=['M','D','C','L','X','V','I']	
+	numerals_hash={	'M'=>1000, 
+			'D'=>500,
+			'C'=>100,
+			'L'=>50,
+			'X'=>10,
+			'V'=>5,			
+			'I'=>1, }	
 
+	numerals_hash.each do |letter,devisor|	
+	
+	puts n=num/devisor
+	puts letter*n
+	
+	puts num%devisor
+	end
+	
 end
 
-old_roman_numeral 3
 
+#test case 1
+old_roman_numeral 78

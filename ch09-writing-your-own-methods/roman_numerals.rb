15bf532c41feee00a938fe8480ec96cce5dae8cb
davidbebb <davidbebb@gmail.com>
2015-08-19 16:34:16 +0100

    array_or_arrays for roman numerals not working

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,34 @@
+#!/usr/bin/env ruby
+
 def roman_numeral num
-  # your code here
+  	str=''	
+#	numerals_hash={	'M'=>1000, 
+#			'D'=>500,
+#			'C'=>100,
+#			'L'=>50,
+#			'X'=>10,
+#			'V'=>5,			
+#			'I'=>1, }	
+	numerals_array=[['X',10],['V',5],['I',1]]
+
+#	numerals_hash.each_with_index do |(letter,devisor),index|	
+	numerals_array.each_with_index do |foo, index|
+	letter=foo[0] 
+	devisor=foo[1]	
+	n=num/devisor
+	puts n
+	if n >3
+		
+		puts numerals_array[index-1][0]	
+	elsif n>8
+		puts numerals_array[index-2][0]
+	end
+	num=num%devisor
+	str+=letter*n
+	
+	end
+	return str
 end
+
+puts roman_numeral 29
+

79234cb491af16603223d25011340cba638b37d5
chweeks <chrisharryweeks@gmail.com>
2015-08-19 22:27:32 +0100

    updated build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,21 @@
 def music_shuffle filenames
-  # your code here
+  songs = filenames.sort
+  
+  2.times do
+  half1 = 0
+  half2 = songs.length / 2
+  shuffled = []
+    
+  while shuffled.length < songs.length
+    if shuffled.length.even? 
+      shuffled.push(songs[half1]); half1 += 1 
+    else
+      shuffled.push(songs[half2]); half2 += 1
+    end
+  end 
+  
+  end
+  	
+  File.open("Playlist.m3u", "w+"){|playlist| playlist.puts shuffled}
+
 end

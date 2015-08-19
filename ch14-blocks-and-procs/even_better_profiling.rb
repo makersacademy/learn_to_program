b3cd7e6ed84f9b05b8c266c6abc331947ef00009
philip <philip.clarke@redhood.co.uk>
2015-08-19 21:54:03 +0200

    programmer logger

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,36 @@
-def profile block_description, &block
-  # your code here
+def profile descriptionOfBlock, &block  #we pass the block and a parameter
+ status = "on" # we run the test - to turn off we change this to off
+ if status == "on" #run the tests
+   
+ 
+  startTime = Time.now
+
+  block.call
+
+  duration = Time.now - startTime
+
+  puts descriptionOfBlock+':  '+duration.to_s+' seconds'
+else #don't run the tests
+  block.call
+end
+end
+
+profile '25000 doublings' do #we find out how long this takes
+  number = 1
+
+  25000.times do
+    number = number + number
+  end
+
+  # Show the number of digits in this HUGE number.
+  puts number.to_s.length.to_s+' digits'
+end
+
+profile 'count to a million' do
+  number = 0
+
+  100.times do
+    puts number
+    number = number + 1
+  end
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,20 @@
 def log desc, &block
-  # your code here
+  puts " Beginning " + desc
+  result = block.call
+  puts " ending " + desc + " returning " + result.to_s
 end
+
+log " outer"  do 
+	20 * 20
+ log " some smaller block" do
+ 	10  * 10
+ end
+
+ log " block 2" do
+ 	12345.to_s
+ end
+
+ log "block 3" do
+ 	123.is_a? Integer
+ end
+end	

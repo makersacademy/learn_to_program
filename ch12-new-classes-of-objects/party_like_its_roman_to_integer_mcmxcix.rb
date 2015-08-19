958b06c9aa0f05db4f12b4be6c8e965bfefe1d99
philip <philip.clarke@redhood.co.uk>
2015-08-19 10:04:33 +0200

    party roman numerals

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,30 @@
-def roman_to_integer roman
-  # your code here
+
+  # first create an array of values with roman as key and integers as values
+ @data = [
+    ["M"  , 1000],
+    ["CM" , 900],
+    ["D"  , 500],
+    ["CD" , 400],
+    ["C"  , 100],
+    ["XC" ,  90],
+    ["L"  ,  50],
+    ["XL" ,  40],
+    ["X"  ,  10],
+    ["IX" ,   9],
+    ["V"  ,   5],
+    ["IV" ,   4],
+    ["I"  ,   1]
+    ]
+
+def roman2_integer roman
+	result = 0
+
+	for index, value in @data
+		while roman.index(key) == 0
+			result += value
+			roman.slice!(key)
+			
+		end
+	end
+	result
 end
---
+++ ch12-new-classes-of-objects/romaninteger.rb
@@ -0,0 +1,34 @@
+
+  # first create an array of values with roman as key and integers as values
+ @data = [
+    ["M"  , 1000],
+    ["CM" , 900],
+    ["D"  , 500],
+    ["CD" , 400],
+    ["C"  , 100],
+    ["XC" ,  90],
+    ["L"  ,  50],
+    ["XL" ,  40],
+    ["X"  ,  10],
+    ["IX" ,   9],
+    ["V"  ,   5],
+    ["IV" ,   4],
+    ["I"  ,   1]
+    ]
+
+    # ...
+
+def roman_to_integer roman
+	reply = 0 # this is where we will send the output
+ for key, value in @data #look for the key and value
+     while roman.index(key) == 0 #of the first character
+     reply += value #add that number to reply
+     roman.slice!(key) #remove the character
+     end	
+     end
+
+reply #and return
+
+ end
+
+ puts(roman_to_integer("MCM"))

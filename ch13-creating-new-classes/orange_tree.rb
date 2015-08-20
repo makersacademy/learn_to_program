4f67c64db463363f3b84834d68229b000425d0fb
Alex Lemons <alexlemons1@gmail.com>
2015-08-20 17:57:05 +0100

    orange_tree altered

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -3,15 +3,26 @@ class OrangeTree
     @height = 1
     @age = 0
     @orange_count = 0
+    @alive = true
 
     puts "Your tree is born, it is #{@height} foot tall!"
   end
 
   def one_year_passes
     @orange_count = 0
+
+      if @age > 2
+        @orange_count = @age**2
+      end
+
+      if @age > 15
+        @alive = false
+        puts "Your tree has died, sorry"
+        exit
+      end
+
     @age = @age + 1
     @height = @height + (@age*2)
-
     puts "Your tree is now #{@age} years old, it is #{@height} foot tall, and has #{@orange_count} oranges growing on it's branches!"
   end
 
@@ -21,27 +32,10 @@ class OrangeTree
 
   def pick_an_orange
     @orange_count = @orange_count - 1
-    puts "There are #{@orange_count} oranges left in the tree"
-  end
-
-
-  private
-
-  def fruit
-    if @age > 3
-      @orange_count = @age**2
-    end
-  end
-
-  def death
-    if  @age > 15
-    puts "Your tree has died, sorry"
-    exit
-    end
+    puts "You picked an orange, there are now #{@orange_count} oranges left in the tree"
   end
 
 end
-
 
 tree = OrangeTree.new
 
@@ -65,6 +59,7 @@ tree.one_year_passes
 tree.pick_an_orange
 tree.pick_an_orange
 tree.count_the_oranges
+tree.one_year_passes
 tree.one_year_passes
 tree.one_year_passes
 tree.one_year_passes

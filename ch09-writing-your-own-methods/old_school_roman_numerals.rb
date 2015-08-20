1419ebbdaed35b0c46bc9d8e3e8253aa9e973f60
Tim Harrison <mail@trwh.co.uk>
2015-08-20 12:47:58 +0100

    Chapter 9 attempt, all finished

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -21,31 +21,31 @@ def old_roman_numeral num # Return Roman numerals with addition only
   end
 
   # D = 500
-  if (num / 500 ) > 0
+  if (num / 500) > 0
     numeral = numeral + 'D' * (num / 500)
     num = num % 500
   end
 
   # C = 100
-  if (num / 100 ) > 0
+  if (num / 100) > 0
     numeral = numeral + 'C' * (num / 100)
     num = num % 100
   end
 
 # L = 50
-  if (num / 50 ) > 0
+  if (num / 50) > 0
     numeral = numeral + 'L' * (num / 50)
     num = num % 50
   end
 
 # X = 10
-  if (num / 10 ) > 0
+  if (num / 10) > 0
     numeral = numeral + 'X' * (num / 10)
     num = num % 10
   end
 
 # V = 5
-  if (num / 5 ) > 0
+  if (num / 5) > 0
     numeral = numeral + 'V' * (num / 5)
     num = num % 5
   end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -20,34 +20,70 @@ def roman_numeral num # Return Roman numerals with addition only
     numeral = '' # Initalise string.
   end
 
+  # CM = 900
+  if (num / 900) > 0
+    numeral = 'CM' * (num / 900)
+    num = num % 900
+  end
+
   # D = 500
-  if (num / 500 ) > 0
+  if (num / 500) > 0
     numeral = numeral + 'D' * (num / 500)
     num = num % 500
   end
 
+  # CD = 400
+  if (num / 400) > 0
+    numeral = numeral + 'CD' * (num / 400)
+    num = num % 500
+  end
+
   # C = 100
-  if (num / 100 ) > 0
+  if (num / 100) > 0
     numeral = numeral + 'C' * (num / 100)
     num = num % 100
   end
 
+  # XC = 90
+  if (num / 90) > 0
+    numeral = numeral + 'XC' * (num / 90)
+    num = num % 90
+  end
+
 # L = 50
-  if (num / 50 ) > 0
+  if (num / 50) > 0
     numeral = numeral + 'L' * (num / 50)
     num = num % 50
   end
 
+  # XL = 40
+  if (num / 40) > 0
+    numeral = numeral + 'XL' * (num / 40)
+    num = num % 40
+  end
+
 # X = 10
-  if (num / 10 ) > 0
+  if (num / 10) > 0
     numeral = numeral + 'X' * (num / 10)
     num = num % 10
   end
 
+  # IX = 9
+  if (num / 9) > 0
+    numeral = numeral + 'IX' * (num / 9)
+    num = num % 9
+  end
+
 # V = 5
-  if (num / 5 ) > 0
+  if (num / 5) > 0
     numeral = numeral + 'V' * (num / 5)
     num = num % 5
+  end
+
+# IV = 5
+  if (num / 4) > 0
+    numeral = numeral + 'IV' * (num / 4)
+    num = num % 4
   end
 
 # I = 1
@@ -59,7 +95,7 @@ numeral
 
 end
 
+puts roman_numeral(4)
 puts roman_numeral(9)
-puts roman_numeral(1999)
-puts roman_numeral(0.1)
-puts roman_numeral(-1)
+puts roman_numeral(64)
+puts roman_numeral(1019)

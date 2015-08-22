be7c80bd1a48843cc35e28ea3ffee64fad80f64f
Tim Harrison <mail@trwh.co.uk>
2015-08-22 15:08:26 +0100

    Chapter 14 v3

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,20 @@
 def log desc, &block
-  # your code here
+
+  # Using the text Chris specifies on page 120.
+  puts "Beginning \"#{desc}\"..."
+  output = block.call
+  puts "...\"#{desc} finished, returning: #{output}"
+
 end
+
+innerBlock = Proc.new do
+  puts 'This puts comes from the inner block.'
+  next 'This return comes from the inner block.' # Why can't I return from a block? See here: http://stackoverflow.com/questions/2325471/using-return-in-a-ruby-block
+end
+
+outerBlock = Proc.new do
+  log('Inner block', &innerBlock)
+  next 'This return comes from the outer block.' # Why can't I return from a block? See here: http://stackoverflow.com/questions/2325471/using-return-in-a-ruby-block
+end
+
+log('Outer block', &outerBlock)

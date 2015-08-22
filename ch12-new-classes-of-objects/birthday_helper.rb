a743e19a391519d332172d2bafdaf44927da4ac8
chweeks <chrisharryweeks@gmail.com>
2015-08-22 23:47:17 +0100

    refactored birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,14 +1,10 @@
-file = File.open("info.txt", "r")
-names = file.read
+birthdays = Hash.new
 
-info = Hash.new
-names.each_line do |line| 
+File.read("birthdays.txt").each_line do |line| 
   line = line.chomp
-  info[line.downcase[0..-15]] = line[-12..-1]
+  birthdays[line.downcase[0..-15]] = line[-12..-1]
 end
 
 puts 'Input name'
 name = gets.chomp.downcase
-puts "#{name.upcase}'s birthday is #{info[name]}"
-
-
+puts "#{name.upcase}'s birthday is #{birthdays[name]}"

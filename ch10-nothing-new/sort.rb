49f74b6a31fea96142b450e3f7f00137b4dc3da5
davidbebb <davidbebb@gmail.com>
2015-08-22 11:26:56 +0100

    added sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,48 @@
+#!/usr/bin/env ruby
+
 def sort arr
-  # your code here
+  return sorter(arr)
 end
+
+##Copy of dictionary sort, as it fulfils the same reqs and the test looks for method dictionary_sort.... DB
+
+
+def dictionary_sort arr
+	return sorter(arr)
+end
+
+def sorter array
+	
+    temp_array=array
+	
+    l=temp_array.length
+    for i in 0..l
+	for i in 1..(l-1)
+		strA=temp_array[i-1]
+		strB=temp_array[i] 
+		if strA.downcase > strB.downcase 	
+
+			temp_array[i]=strA
+			temp_array[i-1]=strB
+				
+		end
+	end
+     end
+     return temp_array
+	
+end      
+
+
+
+
+#my_array=['on', 'the', 'first', 'day', 'yellow' , 'monkey']
+
+#puts my_sort=dictionary_sort(my_array)
+#ruby_sort=my_array.sort
+
+##Test for if it's worked - ascii sort only 
+#if my_sort==ruby_sort
+#	puts 'WIN!'
+#else
+#	puts 'You fail'
+#end

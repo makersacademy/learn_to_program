20b24f368313d9ce427a738562d87310cab18796
Michael Roger <bodgeuk12@gmail.com>
2015-08-22 19:36:23 +0100

    ch12 roman_to_integer complete

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,30 @@
 def roman_to_integer roman
-  # your code here
+  # create Hash
+	roman_hash = {"M"=>1000, "D"=>500, "C"=>100, "L"=>50, "X"=>10, "V"=>5, "I"=>1}
+
+	roman_int = 0
+	prev = 0
+	index = roman.length - 1
+	while index >= 0
+		p = roman[index].upcase
+		num = roman_hash[p]
+		if !num
+			puts "Not a valid roman numeral. BYE!"
+			return
+		end
+		if num < prev
+			num = num*(-1)
+		else
+			prev = num
+		end
+		roman_int = roman_int + num
+		index = index - 1
+	end
+
+	roman_int
 end
+
+puts roman_to_integer("mcmlxxxiii")
+puts roman_to_integer("mcmxcix")
+puts roman_to_integer("CCCLXV")
+puts roman_to_integer("CCPQsad")

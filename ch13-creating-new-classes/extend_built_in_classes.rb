ea0a3cb83a76a809970a929d1f4e3e29a1768e5d
Tim Harrison <mail@trwh.co.uk>
2015-08-22 09:36:18 +0100

    Chapter 13 v2

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -19,6 +19,32 @@ class Integer
     factorial
   end
 
+  # Extending built-in class Integer
+  def to_roman
+
+    # Initialise Roman string. It looks like I'm allowed to use "old school" ones, e.g. additive only.
+    roman = ''
+
+    # Keep a local varable for the amount of Arabic numeral left to write out.
+    arabic = self
+
+    # Use an array to capture all the substitute values.
+    romanArray = [ ['M', 1000], ['D', 500], ['C', 100], ['L', 50], ['X', 10], ['V', 5], ['I', 1] ]
+
+    romanArray.each do |i|
+
+      while arabic >= i[1]
+        roman = roman + i[0]
+        arabic = arabic - i[1]
+      end
+
+    end
+
+  roman
+  end
+
 end
 
 puts 3.factorial
+
+puts 1019.to_roman

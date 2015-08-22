72068ec89010485cf9f4918442cf05575f5f81ed
Tim Harrison <mail@trwh.co.uk>
2015-08-22 09:13:00 +0100

    Chapter 13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,24 @@
 class Integer
-  # your code here
+
+  # Extending built-in class Integer
+  def factorial
+
+    # Return quickly for 1 and 0
+    if self <= 1
+      return 1
+    end
+
+    # Else, calculate factorial here
+    factorial = 1
+
+    for i in 1..self do
+      # puts "i is #{i} and factorial is #{factorial}"
+      factorial = factorial * i
+    end
+
+    factorial
+  end
+
 end
+
+puts 3.factorial

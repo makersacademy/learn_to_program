271496744e5a3bc14cea564de8ec23efbab31dcc
bat020 <bat020@gmail.com>
2015-08-23 19:17:40 +0000

    fixed errors in sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,16 +1,17 @@
 def sort arr
-  rec_sort(arr, [])
+  sort = rec_sort(arr, [])
 end
 
 def rec_sort unsorted, sorted
-  if unsorted = [] then
-    rec_sort = []
+  if unsorted == [] then
+    rec_sort = sorted
   else
-    k = unsorted[0]
-    for i = 1..(unsorted.length - 1)
-      k = (unsorted[k] <= unsorted[i] ? k : i)
+    k = 0
+    for i in 1..(unsorted.length - 1)
+      k = ((unsorted[k] <=> unsorted[i]) == -1 ? k : i)
     end
     sorted << unsorted[k]
     unsorted.delete_at(k)
-    rec_sort(unsorted, sorted)
+	  rec_sort = rec_sort(unsorted, sorted)
+  end
 end

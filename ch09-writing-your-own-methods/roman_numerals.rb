797d72c7c088f81dfb31f85e4254e553aaa3da40
bat020 <bat020@gmail.com>
2015-08-23 18:16:36 +0000

    added roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,24 @@
 def roman_numeral num
-  # your code here
+    
+  array = []
+  array << num / 1000
+  num = num % 1000
+  array << (num >= 500 ? 1 : 0)
+  num = num % 500
+  array << num / 100
+  num = num % 100
+  array << (num >= 50 ? 1 : 0)
+  num = num % 50
+  array << num / 10
+  num = num % 10
+  array << (num >= 5 ? 1 : 0)
+  num = num % 5
+  array << num
+  
+  for i in 0..6
+    array[i] = ["M","D","C","L","X","V","I"][i] * array[i]
+  end
+  
+  array.join.gsub("DCCCC","CM").gsub("CCCC","CD").gsub("LXXXX","XC").gsub("XXXX","XL").gsub("VIIII","IX").gsub("IIII","IV")
+  
 end

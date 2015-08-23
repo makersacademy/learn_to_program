89a8b0aadcf6cf823de5bcbe42421976b8170ea9
davidbebb <davidbebb@gmail.com>
2015-08-23 21:22:26 +0100

    added better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,19 @@
+#!/usr/bin/env ruby
+
+$LOG_TIMES=0
+
 def log desc, &block
-  # your code here
+  $LOG_TIMES=$LOG_TIMES+1
+  puts (" "*($LOG_TIMES-1)) + "Beginning \"#{desc}\"..."
+  value=block.call
+  puts (" "*($LOG_TIMES-1)) + "...\"#{desc}\" finished, returning: #{value}"
+  $LOG_TIMES=$LOG_TIMES-1
+end
+
+
+log 'test' do
+  3
+  log 'test2' do
+    4
+  end
 end

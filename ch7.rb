faf0b936057fd799fad3fcbf5c68fe73bb55741b
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-08-23 20:50:14 +0100

    updated ch7 and ch8

--- ch7.rb
+++ ch7.rb
@@ -1,9 +1,14 @@
-puts 'Hello, Sonny!'
+puts 'HELLO, SONNY!'
 while true
 	response = gets.chomp
-	if response == response.downcase
-	puts 'HUH?!  SPEAK UP, SONNY!'	
-	elsif response == response.upcase
-		puts 'NO, NOT SINCE 1938' 
+	if response == "BYE"
+	puts 'BYE SONNY!'
+		break
+	end
+	if response != response.upcase
+		puts 'HUH?!  SPEAK UP, SONNY!'	
+	else 
+		random_year = 1930 + rand(21)
+		puts 'NO, NOT SINCE ' + random_year.to_s + '!'
 	end
 end 

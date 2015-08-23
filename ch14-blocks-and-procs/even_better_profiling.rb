2b9ea9396b7c7b37e4c513cfa739fada521205dc
chweeks <chrisharryweeks@gmail.com>
2015-08-23 17:30:21 +0100

    updated even_better_profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
 def profile block_description, &block
-  # your code here
+  profiling = true
+  if profiling 
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else 
+	block.call
+  end
 end

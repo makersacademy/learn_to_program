b2edafb948abb854cb4156a396122a864a95ef92
chweeks <chrisharryweeks@gmail.com>
2015-08-23 18:07:23 +0100

    updated better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,10 @@
+$depth = 0
+
 def log desc, &block
-  # your code here
+  indentation = ' ' * $depth
+  puts "#{indentation}Beginning \"#{desc}\"..."
+  $depth += 1
+  result = block.call
+  $depth -= 1
+  puts "#{indentation}...\"#{desc}\" finished, returning: #{result}"
 end

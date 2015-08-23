861d070c4aecf080692b4c48b9e2ec335026e262
bat020 <bat020@gmail.com>
2015-08-23 18:54:55 +0000

    added sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,16 @@
 def sort arr
-  # your code here
+  rec_sort(arr, [])
+end
+
+def rec_sort unsorted, sorted
+  if unsorted = [] then
+    rec_sort = []
+  else
+    k = unsorted[0]
+    for i = 1..(unsorted.length - 1)
+      k = (unsorted[k] <= unsorted[i] ? k : i)
+    end
+    sorted << unsorted[k]
+    unsorted.delete_at(k)
+    rec_sort(unsorted, sorted)
 end

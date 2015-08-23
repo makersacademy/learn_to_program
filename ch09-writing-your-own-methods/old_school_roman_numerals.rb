63453d8fc1814c54073edc754505d8abc565e44d
bat020 <bat020@gmail.com>
2015-08-23 18:08:51 +0000

    fixed missing parentheses

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -3,15 +3,15 @@ def old_roman_numeral num
   array = []
   array << num / 1000
   num = num % 1000
-  array << num >= 500 ? 1 : 0
+  array << (num >= 500 ? 1 : 0)
   num = num % 500
   array << num / 100
   num = num % 100
-  array << num >= 50 ? 1 : 0
+  array << (num >= 50 ? 1 : 0)
   num = num % 50
   array << num / 10
   num = num % 10
-  array << num >= 5 ? 1 : 0
+  array << (num >= 5 ? 1 : 0)
   num = num % 5
   array << num
   

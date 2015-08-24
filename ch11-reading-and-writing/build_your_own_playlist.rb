6154917741684286e383c04c4d5c00ed8e2f76f4
Anindya Bhattacharyya <bat020@gmail.com>
2015-08-24 17:11:57 +0100

    added build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,24 @@
-# your code here
+def shuffle arr
+  if arr == [] then
+    []
+  else
+    index = rand(arr.length)
+    out = arr[index]
+    arr.delete_at(index)
+    shuffle(arr) << out
+  end
+end
+
+Dir.chdir 'C:/Users/User/Desktop/Music'
+
+music_files = Dir['**/*.m4a']
+
+playlist = shuffle(music_files)
+
+File.open 'random_playlist.m3u', 'w' do |file|
+  while !(playlist == [])
+    file.write playlist.pop + "\n"
+  end
+ end
+ 
+ 

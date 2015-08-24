25321e7f1144108537b99d04e574dd8ad2cce77a
nathanielgreen <ngreen2303@gmail.com>
2015-08-24 09:16:42 +0100

    Exercises added

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,15 @@
-def dictionary_sort arr
-  # your code here
+def sort array
+  if array.length <= 1
+    return array
+  else
+
+  new_array = array.pop
+  first = array.select{ |word| word.downcase < new_array.downcase}
+  last = array.select{ |word| word.downcase >= new_array.downcase}
+
+  sort(first) + [new_array] + sort(last)
 end
+end
+
+alphabet_array =  ["charlie","Dog","alpha","Beta","echo"]
+puts sort( alphabet_array ).join(" ")
--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,7 @@
-# your code here
+def bottle
+
+  beer = 10000.step(1, -1) {|x| puts "#{x} bottles of beer on the wall, #{x} of beer, take one down, pass it around, #{x-1} bottles of beer on the wall!"}
+
+end
+
+bottle

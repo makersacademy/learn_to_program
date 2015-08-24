e714fe332e0c0c7c05e741801b41682b44e5fac5
ana <adscarlos@gmail.com>
2015-08-24 12:05:51 +0100

    adds dictionary_sort answer

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
 def dictionary_sort arr
-  # your code here
+  recursive_dict_sort arr, []
 end
+
+def recursive_dict_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |tested_object| # ---> look at each object of unsorted
+    if tested_object.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = tested_object
+    else
+      still_unsorted.push tested_object
+    end
+  end
+
+  sorted.push smallest # --> add smallest word to the end of sorted array
+
+  recursive_dict_sort still_unsorted, sorted
+end
+
+puts dictionary_sort ['can', 'feel', 'singing', 'like', 'A', 'Can']

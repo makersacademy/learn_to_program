ea655775624cca95a48ed23759aac95047038e77
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-08-24 08:07:09 +0100

    ch09

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
-def old_roman_numeral num
-  # your code here
+class Integer
+  def roman
+    letter = {1000 => 'M',500 => 'D',100 =>'C',50 => 'L',10 => 'X',5 => 'V',1 => 'I'}
+    array = []
+    num = self
+    letter.each do |val, char|
+      if num / val >= 1
+        array.push (char * (num / val))
+        num = num - (val * (num / val))
+      end
+    end
+    array.join
+  end
 end
+
+input = gets.chomp.to_i
+puts input.roman

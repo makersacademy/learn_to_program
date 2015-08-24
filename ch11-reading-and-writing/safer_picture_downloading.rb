e88d08090bfd6701d67fc6f1ef00e9a4003ab471
Anindya Bhattacharyya <bat020@gmail.com>
2015-08-24 16:52:23 +0100

    added safer_picture_downloading.rb

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,25 @@
-# your code here
+Dir.chdir 'C:/Users/User/Desktop/Inbox'
+
+pic_names = Dir['C:/Users/User/Desktop/Kinder/*.jpg']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+
+puts "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+pic_names.each do |name|
+  print '.' # This is our "progress bar".
+  new_name = pic_number < 10 ? "#{batch_name}0#{pic_number}.jpg" : "#{batch_name}#{pic_number}.jpg"
+  if File.exist?(new_name) then
+    puts "ERROR: #{new_name} already exists, process terminated."
+	exit
+  else
+    File.rename name, new_name
+  end
+  pic_number += 1
+end
+
+puts
+puts 'Done'

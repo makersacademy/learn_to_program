8e5200d0fb03e48172a15e98b61bda6a62c39ec3
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-08-25 11:16:27 +0100

    Ch12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,11 @@
-# your code here
+puts 'What year were you born in?'
+year = gets.chomp.to_i
+puts 'And month (in Integer)?'
+month = gets.chomp.to_i
+puts 'And day?'
+day = gets.chomp.to_i
+puts "#{((Time.new - Time.mktime(year, month, day)) / (60 * 60 * 24 * 30 * 12)).to_i} years old"
+require 'date'
+if (Time.new.month == Time.mktime(year, month, day).month and Time.new.day == Time.mktime(year, month, day).day)
+  puts 'SPANK!'
+end
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,3 @@
-# your code here
+time = Time.new
+puts "Seconds from birth: #{(time - Time.mktime(1986, 5, 29)).round(1)}"
+puts "Years to a billion second: #{((Time.mktime(1986, 5, 29) - time + 1000000000) / (60 * 60 * 24 * 365)).round(1)}"

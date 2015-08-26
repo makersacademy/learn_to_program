0f16699d3d6fc1a40b82c7eaf8b244c431b165db
Lucy Fang <lucyfang2@gmail.com>
2015-08-26 11:33:24 +0100

    Amended better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,10 +1,24 @@
 $depth = 0
 
 def log desc, &block
-  nesting_depth = ' ' * $depth
+  nesting_depth = '  ' * $depth
   puts nesting_depth + "Beginning \"#{desc}\"..."
   $depth += 1
   result = block.call
   $depth -= 1
   puts nesting_depth + "...\"#{desc}\" finished, returning: #{result}"
 end
+
+# log 'outer block' do
+#   log 'some little block' do
+#     log 'teeny block' do
+#       'lots of loVE'.downcase
+#     end
+
+#     7*3*2
+#   end
+#   log 'yet another block' do
+#     '!doof naidnI evol I'.reverse
+#   end
+#   '0' == "0"
+# end

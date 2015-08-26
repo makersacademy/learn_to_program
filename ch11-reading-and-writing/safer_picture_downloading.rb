e94036a5fe4b15df144b11efb2ffe51179fe9cb7
bat020 <bat020@gmail.com>
2015-08-26 11:52:30 +0000

    fixed bugs in safer_picture_downloading.rb

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -6,20 +6,26 @@ puts 'What would you like to call this batch?'
 batch_name = gets.chomp
 puts
 
-puts "Downloading #{pic_names.length} files: "
-
+error_flag = false
+new_names = []
 pic_number = 1
 pic_names.each do |name|
-  print '.' # This is our "progress bar".
-  new_name = pic_number < 10 ? "#{batch_name}0#{pic_number}.jpg" : "#{batch_name}#{pic_number}.jpg"
-  if File.exist?(new_name) then
-    puts "ERROR: #{new_name} already exists, process terminated."
-	exit
-  else
-    File.rename name, new_name
-  end
+  new_names << (pic_number < 10 ? "#{batch_name}0#{pic_number}.jpg" : "#{batch_name}#{pic_number}.jpg")
+  error_flag = error_flag || File.exist?(new_names[-1])
   pic_number += 1
 end
 
-puts
-puts 'Done'
+if error_flag then
+  puts "ERROR: files of that name already exist; process terminated."
+else
+  print "Filenames checked, now downloading #{pic_names.length} files: "
+  pic_number = 1
+  pic_names.each do |name|
+    print '.' # This is our "progress bar".
+    File.rename name, new_names[pic_number - 1]
+    pic_number += 1
+  end
+  puts
+  puts 'Pictures moved and renamed.'
+end
+

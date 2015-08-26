6ac70b1bc8f8c56476539d312148a7bffa98abc4
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-08-26 10:43:27 +0100

    completed ch10 ninety nine bottles exercise

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,14 @@
-# your code here
+num_at_start = 99
+
+while num_now > 2
+  puts english_number(num_now).capitalize + ' bottles of beer on the wall, ' +
+       english_number(num_now) + ' bottles of beer!'
+  num_now = num_now - 1
+  puts 'Take one down, pass it around, ' +
+english_number(num_now) + ' bottles of beer on the wall!' 
+end
+
+puts "Two bottles of beer on the wall, two bottles of beer!"
+puts "Take one down, pass it around, one bottle of beer on the wall!"
+puts "One bottle of beer on the wall, one bottle of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall!"

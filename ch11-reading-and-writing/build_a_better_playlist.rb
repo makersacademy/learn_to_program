43a38175946fd8abd32dd666e61b6f642c5d7493
Lucy Fang <lucyfang2@gmail.com>
2015-08-26 10:37:05 +0100

    Amended playlist exercise

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,8 +1,8 @@
 def music_shuffle filenames
-  filesnames = filesnames.sort
+  filenames = filenames.sort
   len = filenames.length
 
-  2.times do 
+  2.times do
     l_index = 0
     r_index = len/2
     shuf = []
@@ -33,7 +33,7 @@ end
 
 music_files = music_shuffle(Dir['/Users/lucetzer/Music/Various_singles/*.{mp3,m4a}'])
 
-File.open 'singles_playlist.m3u', 'w' do |f| 
+File.open 'singles_playlist.m3u', 'w' do |f|
   music_files.each do |song|
     f.write song + "\n"
   end

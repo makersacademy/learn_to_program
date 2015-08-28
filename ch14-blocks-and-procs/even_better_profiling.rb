bfd8bfad3a4bebd764e9b24241bc84ce1d638ba6
bat020 <bat020@gmail.com>
2015-08-28 09:09:52 +0000

    added even_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,8 @@
+$OPT_PROFILING_ON = false
+
 def profile block_description, &block
-  # your code here
+  start_time = Time.new if $OPT_PROFILING_ON
+  block.call
+  duration = Time.new - start_time if $OPT_PROFILING_ON
+  puts "#{block_description}: #{duration} seconds" if $OPT_PROFILING_ON
 end

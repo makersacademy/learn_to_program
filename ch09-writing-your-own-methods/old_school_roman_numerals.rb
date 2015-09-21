3268930e1aeff86f1d62dbf3f28c0b9f9ed74c8c
Ben <benjrome@gmail.com>
2015-09-21 23:20:08 +0100

    Completed old-school Roman Numeral Challenge

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
 def old_roman_numeral num
-  # your code here
+	result = ""
+
+	result = result + "M" * (num / 1000)
+	result = result + "D" * (num % 1000 / 500)
+	result = result + "C" * (num % 500 / 100)
+	result = result + "L" * (num % 100 / 50)
+	result = result + "X" * (num % 50 / 10)
+	result = result + "V" * (num % 10 / 5)
+	result = result + "I" * (num % 5)
+
+	result
+
 end
+
+puts old_roman_numeral 1999

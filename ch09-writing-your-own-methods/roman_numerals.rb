a860cec883f073d26e21ecd869ccd4b8f5a92a54
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 11:50:47 +0100

    first commit, roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,18 @@
-def roman_numeral num
-  # your code here
+def roman_numeral(number)
+
+    m = number / 1000
+    mleft = number % 1000
+    d = mleft / 500
+    dleft = mleft % 500
+    c = dleft / 100
+    cleft = dleft % 100
+    l = cleft / 50
+    lleft = cleft % 50
+    x = lleft / 10
+    xleft = lleft % 10
+    v = xleft / 5
+    vleft = xleft % 5
+    i = vleft
+  
+    return "#{'M'*m}#{'D'*d}#{'C'*c}#{'L'*l}#{'X'*x}#{'V'*v}#{'I'*i}"
 end

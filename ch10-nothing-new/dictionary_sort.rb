50043d6836012767ed8eccff2c8722c59b01afbb
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 12:23:10 +0100

    first commit, dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
-def dictionary_sort arr
-  # your code here
+def sort(list) 													# taking each index and comparing it to each index. 
+  list.each_index { |first| 									# and switching if greater
+	list.each_index { |second| 
+      if list[first].downcase < list[second].downcase
+        list[first], list[second] = list[second], list[first]
+      end
+    print list
+    } 
+  }
 end

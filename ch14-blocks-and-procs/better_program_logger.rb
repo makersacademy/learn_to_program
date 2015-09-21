5cabb28eebf63ef82fa03314d601f676a93962cf
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 16:56:22 +0100

    first commit, bpl

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,18 @@
-def log desc, &block
-  # your code here
+$count = 0
+
+def log( string_desc, &block )
+  puts "#{"  "*$count}starting block #{string_desc}..."
+  $count += 1
+  value = block.call
+  $count -= 1
+  puts "#{"  "*$count}...block #{string_desc} has finished! It returned: #{value}"
 end
+
+log "do something" do
+    number = 1
+    25000.times { number = number + number }
+    log "do another thing" do
+        "Nikesh is a funny man."
+        end
+    "#{number.to_s.length} digits" 
+    end

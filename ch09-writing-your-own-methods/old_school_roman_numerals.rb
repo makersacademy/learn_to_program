e2fb926d7118659cd46b46296d384b78021e03e3
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-21 17:38:32 +0100

    Added old school roman numerals method

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,22 @@
 def old_roman_numeral num
   # your code here
+def old_roman_numeral num
+  # your code here
+
+  chars = { 1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V" }
+  numAry = [1000, 500, 100, 50, 10, 5]
+  roman = ""
+
+  i=0
+  while i < 6
+     if num >= numAry[i]
+       roman = roman + chars [ numAry[i] ] * (num / numAry[i])
+       num = num % numAry[i]
+     end
+     i += 1
+  end
+  if num > 0
+    roman = roman + "I" * num
+  end
+  roman
 end

02b52584509cb10a67905986db841eda3dc022e5
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 11:46:30 +0100

    First commit ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,14 @@
-def ask question
-  # your code here
+def ask(question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    
+    if reply == 'yes' 
+      return true
+    elsif reply == 'no'
+      return false
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
 end

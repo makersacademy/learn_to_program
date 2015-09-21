c220ec06ac3426547a7de5009859d01e98b35ec6
NULL-OPERATOR <x@cryptolab.net>
2015-09-21 17:20:53 +0100

    added old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,21 @@
 def old_roman_numeral num
-  # your code here
+
+  a = [[1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]
+
+  i = num
+  to_print = ''
+  b = 0
+  while i != 0
+    to_cut = (i - (i % a[b][0]))
+    to_print << a[b][1] * (to_cut / a[b][0])
+    i = i - to_cut
+    b += 1
+  end
+
+  to_print
+
 end
+
+
+puts old_roman_numeral 2399
+

a800d5327b55f8c003759dc458fb9235758856d9
samover <samuel.overloop@gmail.com>
2015-09-21 12:11:30 +0200

    first two challenges

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,36 @@
+# LEARNING TO PROGRAM WITH CHRIS PINE, 9.5 ex 1
+###############################################
+# Improved ask method. That ask method I showed you was OK, but I bet you
+# could do better. Try to clean it up by removing the answer variable. You'll
+# have to use return to exit from the loop. (Well, it will get you out of the
+# whole method, but it will get you out of the loop in the process.) How do
+# you like the resulting method? I usually try to avoid using return (a
+# personal preference), but I might make an exception here.
+
 def ask question
-  # your code here
+  loop do
+    puts question
+    reply = gets.chomp.downcase
+
+    return true if reply == 'yes'
+    return false if reply == 'no'
+
+    puts 'Please answer "yes" or "no".'
+  end
 end
+
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,36 @@
+# LEARNING TO PROGRAM WITH CHRIS PINE, 9.5 ex 2
+###############################################
+# Old-school Roman numerals. In the early days of Roman numerals, the
+# Romans didn't bother with any of this new-fangled subtraction "IX"
+# nonsense. No sir, it was straight addition, biggest to littlest-so 9 was
+# written "VIIII" and so on. Write a method that when passed an integer
+# between 1 and 3000 (or so) returns a string containing the proper
+# oldschool Roman numeral. In other words, old_roman_numeral 4 should return
+# 'IIII'. Make sure to test your method on a bunch of different numbers.
+#
+# For reference, these are the values of the letters used:
+# I = 1 V = 5 X = 10 L = 50
+# C = 100 D = 500 M = 1000
+
 def old_roman_numeral num
-  # your code here
+  roman_to_arabic = {
+    'M' => 1_000, 'D' => 500, 'C' => 100,
+    'L' => 50,    'X' => 10,  'V' => 5,
+    'I' => 1
+  }
+
+  roman_to_arabic.map do |key, val|
+    div = num / val
+    num = num % val
+    key * div
+  end.join
 end
+
+puts old_roman_numeral 1
+puts old_roman_numeral 4
+puts old_roman_numeral 54
+puts old_roman_numeral 152
+puts old_roman_numeral 1949
+puts old_roman_numeral 3000
+puts old_roman_numeral 2016
+puts old_roman_numeral 1999

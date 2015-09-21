520dba3e57e9314ffaa681bda75ce4abec0bb1e9
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 13:29:57 +0100

    first commit, hbd

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp.to_i
+puts "What month? (1-12)"
+month = gets.chomp.to_i
+puts "What day? (1-31)"
+day = gets.chomp.to_i
+puts "What hour? (0-23)"
+hour = gets.chomp.to_i
+puts "What minute? (0-59)"
+minute = gets.chomp.to_i
+
+
+now = Time.new
+birthday = Time.local(year, month, day, hour, minute)
+seconds = now - birthday
+
+puts "You are #{seconds/31557600} years old"
+puts "You are #{seconds} seconds old."# your code here

3c95d319f7dcae03d45060a5bc8c1cf12ed2e0f5
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 15:52:11 +0100

    first commit, pl

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
-  # your code here
+def log( string_desc, &block )
+  puts "starting block..."
+  value = block.call
+  puts "...block has finished! It returned:"
+  puts value
 end
+
+
+log "do something" do
+    number = 1
+    25000.times { number = number + number }
+    log "do another thing" do
+        "Nikesh is a funny man."
+        end
+    "#{number.to_s.length} digits" 
+    end

2240ea77811532b8cb9281456b199dc2438f80e3
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-21 12:17:39 +0100

    first commit, shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,8 @@
 def shuffle arr
-  # your code here
+  shuffled = []
+  (arr.length).times { 
+    shuffled << arr.delete_at(rand(0..arr.length-1)) }
+  shuffled	
 end
+
+puts shuffle([ "hello", "my", "name", "is", "edward"])

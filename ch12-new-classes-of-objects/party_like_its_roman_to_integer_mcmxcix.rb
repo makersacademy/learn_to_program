4178ba8bb094cb86cc41925812487b8aeccf3524
NULL-OPERATOR <x@cryptolab.net>
2015-09-22 21:13:20 +0100

    PARTY LIKE ITS MXMXVII

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,26 @@
 def roman_to_integer roman
-  # your code here
+  # 4's
+  roman.upcase!
+  roman.gsub!(/IV/, ' 4')
+  roman.gsub!(/XL/, ' 40')
+  roman.gsub!(/CD/, ' 400')
+
+  roman.gsub!(/CM/, ' 900')
+  roman.gsub!(/XC/, ' 90')
+  roman.gsub!(/IX/, ' 9')
+
+  roman.gsub!(/M/, ' 1000')
+  roman.gsub!(/D/, ' 500')
+  roman.gsub!(/C/, ' 100')
+  roman.gsub!(/L/, ' 50')
+  roman.gsub!(/X/, ' 10')
+  roman.gsub!(/V/, ' 5')
+  roman.gsub!(/I/, ' 1')
+
+  p = (roman.split(' ').map{|x| x.to_i}).reduce(:+)
+  puts "POTATO LIKE IT'S #{p}"
 end
+
+
+roman_to_integer 'MCMLXXXII'
+roman_to_integer 'mcmxcix'

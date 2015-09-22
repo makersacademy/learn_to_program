497ab6a7847798693b176cd779d822e2fdd903ad
Andrew Dowell <adowellphoto@gmail.com>
2015-09-22 16:42:06 +0100

    Answered old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,21 @@
 def old_roman_numeral num
-  # your code here
+romansplit = Hash.new
+romansplit['M'] = 1000
+romansplit['D'] = 500
+romansplit['C'] = 100
+romansplit['L'] = 50
+romansplit['X'] = 10
+romansplit['V'] = 5
+romansplit['I'] = 1
+
+final_output = ""
+romansplit.each do |string, int|
+	unless num < int
+		strcount = num/int
+		num = num%int
+		output = string * strcount
+		final_output = final_output << output
+	end
+end
+return final_output
 end

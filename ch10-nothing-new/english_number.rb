f7aaad054c9a247c6c95b15ec1fd8ddd34fc3c22
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-22 19:43:18 +0100

    made some changes to english_number method

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -3,32 +3,36 @@ def english_number number
 
   str = ""
 
+  if number == 0
+    str= "zero"
+  end
+
   def get_hundred num
 
-    units = ["", "One", "Two", "Three", "Four", "Five", "Six","Seven", "Eight", "Nine"]
-    tens  = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-    teens = ["", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+    units = ["", "one", "two", "three", "four", "five", "six","seven", "eight", "nine"]
+    tens  = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
     str = ""
     while num > 0
       case num.to_s.length
       when 3
-        str = str + units[num.to_s[0,1].to_i] + " "+ "Hundred "
+        str = str + units[num.to_s[0,1].to_i] + " "+ "hundred "
         num = num % 100
       when 2
         num
         if num == 10
-          str = str + "Ten "
+          str = str + "ten "
           num = 0
         elsif (num > 10 && num < 20)
           str = str + teens[num.to_s[1,1].to_i] + " "
           num = 0
          else 
-          str = str + tens[num.to_s[0,1].to_i] + " "
+          str = str + tens[num.to_s[0,1].to_i] + "-"
           num = num % 10
         end
       when 1
-        str = str + units[num.to_s[0,1].to_i]
+        str = str + units[num.to_s[0,1].to_i] + " "
         num = 0
       end
     end
@@ -42,23 +46,23 @@ def english_number number
       str = str + get_hundred(number)
       number = 0
     when (number.to_s.length > 3 && number.to_s.length < 7)
-      str = str + get_hundred(number/1000) + "Thousand "
+      str = str + get_hundred(number/1000) + "thousand "
       number = number%1000
     when (number.to_s.length > 6 && number.to_s.length < 10)
-      str = str + get_hundred(number/1000000) + "Million "
+      str = str + get_hundred(number/1000000) + "million "
       number = number%1000000
     when (number.to_s.length > 9 && number.to_s.length < 13)
-      str = str + get_hundred(number/1000000000) + "Billion "
+      str = str + get_hundred(number/1000000000) + "billion "
       number = number%1000000000
     when (number.to_s.length > 12 && number.to_s.length < 16)
-      str = str + get_hundred(number/1000000000000) + "Trillion "
+      str = str + get_hundred(number/1000000000000) + "trillion "
       number = number%1000000000000
     when (number.to_s.length > 15 && number.to_s.length < 19)
-      str = str + get_hundred(number/1000000000000000) + "Zillion "
+      str = str + get_hundred(number/1000000000000000) + "zillion "
       number = number%1000000000000000
     end
   end
 
-  str
+  str.strip
 
 end

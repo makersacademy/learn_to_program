fbd3ec0ca89a9cff153719c9015886ac0ccb0b03
“DovileSand” <dovile.sand@protonmail.ch>
2015-09-22 17:12:43 +0100

    Modified ask.rb file_First Attempt

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,25 @@
 def ask question
-  # your code here
+    while true
+        puts question
+        reply = gets.chomp.downcase
+    
+        if (reply == 'yes' || reply == 'no')
+            if reply == 'yes'
+                return true
+            end
+        
+            if reply == 'no'
+                return false
+            end
+        
+            break
+            else
+                puts 'Please answer "yes" or "no".'
+        end
+    end
 end
+
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?'
+wets_bed = ask 'Do you wet the bed?'

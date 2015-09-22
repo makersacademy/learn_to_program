9c501990e689596faf4152b53ce42346792d219c
Tom Barnes <ruralmyth@gmail.com>
2015-09-22 14:57:23 +0100

    Updated old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -12,28 +12,28 @@ def old_roman_numeral num
     old_numerals = []
 # The below involves far too much repetition but I ran out of time!
       num_array.length.times {
-        if num_array.length - 1 == 0
+        if num_array.length == 1
           if num_array[0].to_i < 5
             num_array[0].to_i.times { old_numerals << "I" }
           else
             old_numerals << "V"
             (num_array[0].to_i-5).times { old_numerals << "I" }
           end
-        elsif num_array.length - 2 == 0
+        elsif num_array.length == 2
           if num_array[0].to_i < 5
             num_array[0].to_i.times { old_numerals << "X" }
           else
             old_numerals << "L"
             (num_array[0].to_i-5).times { old_numerals << "X" }
           end
-        elsif num_array.length - 3 == 0
+        elsif num_array.length == 3
           if num_array[0].to_i < 5
             num_array[0].to_i.times { old_numerals << "C" }
           else
             old_numerals << "D"
             (num_array[0].to_i-5).times { old_numerals << "C" }
           end
-        else num_array.length - 4 == 0
+        else num_array.length == 4
           if num_array[0].to_i < 5
             num_array[0].to_i.times { old_numerals << "M" }
           end

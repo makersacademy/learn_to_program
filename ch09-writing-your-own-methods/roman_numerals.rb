e7d675a7724928aa0d3cb9932d795ea62f5523ca
Ben <benjrome@gmail.com>
2015-09-22 16:24:42 +0100

    Improved Roman Numerals Challenge

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -10,8 +10,8 @@ def roman_numeral num
 	result = "M" * thousands
 
 	if hundreds != 4 && hundreds != 9
-		result << "D" * (num % 1000) / 500
-		result << "C" * (num % 500) / 100
+		result << "D" * (num % 1000 / 500)
+		result << "C" * (num % 500 / 100)
 	elsif hundreds == 4
 		result << "CD"
 	elsif hundreds == 9
@@ -19,8 +19,8 @@ def roman_numeral num
 	end
 
 	if tens != 4 && tens != 9
-		result << "L" * (num % 100) / 50
-		result << "X" * (num % 50) / 10
+		result << "L" * (num % 100 / 50)
+		result << "X" * (num % 50 / 50)
 	elsif tens == 4
 		result << "XL"
 	elsif tens == 9
@@ -28,7 +28,7 @@ def roman_numeral num
 	end
 
 	if ones != 4 && ones != 9
-		result << "V" * (num % 10) / 5
+		result << "V" * (num % 10 / 5)
 		result << "I" * (num % 5)
 	elsif ones == 4
 		result << "IV"

e5699bb0ad7419afce97b7073453032b127ce1ca
NULL-OPERATOR <x@cryptolab.net>
2015-09-22 21:34:43 +0100

    added build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,14 @@
 def music_shuffle filenames
-  # your code here
+  Dir.chdir ("/Users/NOMNOMg0/Music/")
+  
+  filenames.shuffle!
+  #uses  Fisher–Yates shuffle
+  #instead of Schwartzian transform,
+  #apparently, and so is better than sort_by{rand}
+  f = File.new('#{filenames}_random.m3u', 'w+')
+
+  filenames.each do |x|
+    f.write(x + "\n")
+  end
+  puts 'Shutting down'
 end

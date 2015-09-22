ba0c04e9b966fcc28032beb506f77e7be08a4033
octavian <octavianrotari@gmail.com>
2015-09-22 11:02:51 +0200

    ammend

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -10,24 +10,22 @@ def roman number
 		i_m = "M" * (n/1000)
 		n = n%1000
 	 end
-	 if nl == 3 && n != 500
+	 if nl == 3 && n < 500
 		i_c = "C" * (n/100)
 		n = n%100
-	 elsif nl == 3 && n == 500
-		i_c = "D"
+	 elsif nl == 3 && (n >= 500 && n < 1000)
+		i_c = "D" + "C" * ((n-500)/100)
 		n = n%500
 	 end
-	 if nl == 2 && n != 50
+	 if nl == 2 && n < 50
 		i_x = "X" * (n/10)
 		n = n%10
-	 elsif nl == 2 && n == 50
-		i_x = "L"
+	 elsif nl == 2 && (n >= 50 && n < 100)
+		i_x = "L" + "X" * ((n-50)/10)
 		n = n%50
 	 end
-	 if nl == 1 && (n > 5 && n < 10)
+	 if nl == 1 && (n >= 5 && n < 10)
 		i_i = "V" + "I" * (n - 5)
-	 elsif n == 5
-		i_i = "V"
 	 elsif n < 5
 		i_i = "I" * n
 	 end

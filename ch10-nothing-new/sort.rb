b38c5614ec4737b85ebf042e098d1f0d41e15894
Chuka Ebi <chuka.ebi@outlook.com>
2015-09-22 16:48:05 +0100

    Solved sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,25 @@
-def sort arr
-  # your code here
+def sort some_array # This "wraps" recursive_sort.
+	 recursive_sort some_array, []
 end
+def recursive_sort unsorted_array, sorted_array
+	if unsorted_array.length <= 0
+		return sorted_array
+	end
+	s = []
+	small = unsorted_array.pop
+	unsorted_array.each do |a|
+		if a < small
+			s.push small
+			small = a
+		else 
+			s.push a
+		end
+	end
+
+	sorted_array.push small
+
+	recursive_sort(s, sorted_array)
+
+end
+
+puts sort [1,4,2,0,3,27]

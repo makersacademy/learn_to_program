0fd97db6218712d16de36e247e8bcb87a12bfeca
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-22 19:25:42 +0100

    3 execise updated

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,22 @@
 def old_roman_numeral num
-  # your code here
+  numeral={
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    5 => "V",
+    1 => "I"
+  }
+  result=""
+  numeral.each do |key,value|
+    times = num/key
+    rem = num%key
+    if times > 0
+      section = value*times
+      result += section
+    end
+    num = rem
+  end
+  result
 end

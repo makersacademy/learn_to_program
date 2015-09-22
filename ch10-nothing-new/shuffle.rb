63e0f73624420224524f4f64f0d3a29dc36296a3
alaanzr <alaanzr@gmail.com>
2015-09-22 20:24:39 +0100

    Completed shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,33 @@
 def shuffle arr
-  # your code here
+
+shuf = []
+
+while arr.length > 0
+# Randomly pick one element of the array.
+#While elements still exist in "arr", the while loop will continue to add elements to the "shuf" array until nothing is left in "arr"
+
+rand_index = rand(arr.length)
+# Now go through each item in the array,
+# putting them all into new_arr except for the
+# randomly chosen one, which goes into shuf.
+
+curr_index = 0
+new_arr = []
+
+arr.each do |item|
+if curr_index == rand_index
+	shuf.push item
+else
+	new_arr.push item
 end
+	curr_index = curr_index + 1
+end
+# Replace the original array with the new,
+# smaller array.
+
+arr = new_arr
+
+end
+shuf
+end
+

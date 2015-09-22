a70c379770f1b86e17560608937dd2c791ec01b1
mahmudh <mahmudhssn246@gmail.com>
2015-09-22 14:12:26 +0100

    ask.rb test succeeded

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,20 @@
 def ask question
-  # your code here
+  while true
+  	puts question
+  	reply = gets.chomp.downcase
+
+  	if (reply == 'yes' || reply == 'no')
+  		if reply == 'yes'
+  			return true
+  		else
+  			return false
+  		end
+  		break
+  	else
+  		puts 'Please answer "yes" or "no".'
+  	end
+  end 
 end
+
+wets_bed = ask 'Do you wet the bed?'
+puts wets_bed

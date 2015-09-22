fa37c6f30c29ec315bb8b527935f4d119b158059
octavian <octavianrotari@gmail.com>
2015-09-22 10:15:59 +0200

    old school roman numerals final version

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,38 +1,39 @@
 def roman number
-	i_m = 0
-	i_c = 0
-	i_d = 0
-	i_x = 0
-	i_l = 0
-	i_i = 0
+	i_m = ''
+	i_c = ''
+	i_x = ''
+	i_i = ''
 	nl = number.length
-	number = number.to_i
-	if nl == 4
-		i_m = "M" * (number/1000)
-		number = number%1000
-	end
-	if nl == 3 && number != 500
-		i_c = "C" * (number/100)
-		number = number%100
-	elsif number == 500
-		i_d = "D"
-		number = number%500
-	end
-	if nl == 2 && number != 50
-		i_x = "X" * (number/10)
-		number = number%10
-	elsif number == 50
-		i_l = "L"
-		number = number%50
-	end
-	if nl == 1 && number > 5
-		i_i = "I" * (number - 5)
-	elsif number < 5
-		i_i = "I" * number
-	else 
-		i_i = "v"
-	end
-	puts "your roman number is: #{i_m}#{i_c}#{i_d}#{i_x}#{i_l}#{i_i}"
+	n = number.to_i
+	while nl > 0
+	 if nl == 4
+		i_m = "M" * (n/1000)
+		n = n%1000
+	 end
+	 if nl == 3 && n != 500
+		i_c = "C" * (n/100)
+		n = n%100
+	 elsif nl == 3 && n == 500
+		i_c = "D"
+		n = n%500
+	 end
+	 if nl == 2 && n != 50
+		i_x = "X" * (n/10)
+		n = n%10
+	 elsif nl == 2 && n == 50
+		i_x = "L"
+		n = n%50
+	 end
+	 if nl == 1 && (n > 5 && n < 10)
+		i_i = "V" + "I" * (n - 5)
+	 elsif n == 5
+		i_i = "V"
+	 elsif n < 5
+		i_i = "I" * n
+	 end
+	nl -= 1
+    end
+	puts "your roman number is: #{i_m}#{i_c}#{i_x}#{i_i}."
 end
 
 number = gets.chomp 

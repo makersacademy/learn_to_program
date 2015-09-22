a9c11667de09d6a39e82de20802e78eec8a6df2a
octavian <octavianrotari@gmail.com>
2015-09-22 08:43:42 +0200

    numbers from 5-10

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
-def old_roman_numeral num
-  # your code here
+def roman number
+	i_nu = 0
+	if number > 0 && number != 5
+		i_nu = "I" * number
+	elsif number == 5
+		i_nu = "V"
+	elsif number > 5 && number < 10
+	   i_nu = "V" + "I" * number/2
+	end
 end
+
+number = gets.chomp.to_i
+puts roman number

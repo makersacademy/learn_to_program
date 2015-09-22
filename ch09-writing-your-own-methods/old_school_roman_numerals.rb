7b011702f35b939fa1a12e993d1bbe4a5057ac80
Rebecca <beccollins82@hotmail.com>
2015-09-22 17:32:22 +0100

    Old Roman Numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,18 @@
 def old_roman_numeral num
-  # your code here
+  	
+	answer = ''
+	answer += 'M' * (num/1000)
+	answer += 'D' * (num%1000/500)
+	answer += 'C' * (num%500/100)
+	answer += 'L' * (num%100/50)
+	answer += 'X' * (num%50/5)
+	answer += 'I' * (num%5)
+
+answer
+
+
 end
+puts 'Please give me an integer between 1 and 3000'
+
+num =gets.chomp.to_i
+puts old_roman_numeral(num)

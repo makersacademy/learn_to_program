54c854fbc7b34c64f30ac404a5251bdb42c2dc12
jamierose <jrose111284@gmail.com>
2015-09-22 18:44:46 +0100

    oldschool

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
 def old_roman_numeral num
-  # your code here
+  numbers = [1000, 500, 100, 50, 10, 5, 1]
+  numerals = ["M", "D", "C", "L", "X", "V", "I"]
+  roman_num = ""
+  j=0
+
+  while j <= 6
+  	while num >= numbers[j] 
+  		num = num - numbers[j]
+  		roman_num = roman_num + numerals[j]
+  	end
+  	j = j + 1
+  end
+   roman_num
 end
+puts "enter a number!"
+puts old_roman_numeral(gets.chomp.to_i)

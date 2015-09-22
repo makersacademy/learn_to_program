3128f360e088c216b264e9a219cbfe7e21117c1a
Robert Youngman <rob_youngman@yahoo.com>
2015-09-22 20:46:19 +0100

    Removed some debugging text

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -16,7 +16,7 @@ I = 1 V = 5 X = 10 L = 50 C = 100 D = 500 M = 1000
 
 def old_roman_numeral num
   remainder = num
-  numerals = "#{num} = "
+  numerals = ""
 
   while remainder > 0 do
     case remainder

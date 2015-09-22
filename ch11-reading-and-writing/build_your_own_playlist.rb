ebe98b45b2513ad41c67b46e373b22296cec224c
Hamza Sheikh <hsheikhm@gmail.com>
2015-09-23 00:33:36 +0100

    Completed exercise: build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,13 @@
-# your code here
+Dir.chdir(#Enter path which locates the directory that contains the music files)
+
+puts "What would you like to call this shuffled playlist?"
+playlist_name = gets.chomp
+playlist_name = "#{playlist_name}.m3u"
+
+songs = Dir["*.mp3"].shuffle!
+
+File.open playlist_name, "w" do |file| 
+	songs.each{|song| file.write song + "\n"} 
+end
+
+puts "Done! Playlist: #{playlist_name} has now been created and contains #{songs.length} songs."

2cc1587c0eb835e19e30508a5faf66d235b11aa0
mahmudh <mahmudhssn246@gmail.com>
2015-09-22 22:40:00 +0100

    moder roman numerals challenge

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,27 @@
 def roman_numeral num
-  # your code here
+	 roman = ''
+	 roman = roman + 'M' * (num / 1000)
+	 roman = roman + 'D' * (num % 1000 / 500)
+	 roman = roman + 'C' * (num % 500 / 100)
+	 roman = roman + 'L' * (num % 100 / 50)
+	 roman = roman + 'X' * (num % 50 / 10)
+	 roman = roman + 'V' * (num % 10 / 5)
+	 roman = roman + 'I' * (num % 5 / 1)
+	 roman
 end
+
+number = roman_numeral(54)
+
+if number.include? 'VIIII'
+	puts number.gsub!('VIIII', 'IX')
+elsif number.include? 'IIII'
+	puts number.gsub!('IIII', 'IV')
+else
+	puts number
+end
+
+# 16) roman numerals translates 519 to DXIX
+#      Failure/Error: expect(roman_numeral(i+500)).to eq numeral
+
+#        expected: "DXIX"
+#             got: "DXVIIII"

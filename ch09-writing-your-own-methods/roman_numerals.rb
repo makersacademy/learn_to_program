da0be8b2fdf693302cee3c904701ee146289d828
Chuka Ebi <chuka.ebi@outlook.com>
2015-09-22 15:48:42 +0100

    Added roman numeral.rb and shuffle.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,148 @@
 def roman_numeral num
   # your code here
+   if num < 4
+  	"I" * num
+  elsif(num ==4)
+  	"I" + "V"
+  elsif (num == 5)
+  	 "V"
+  elsif (num > 5 and num < 9)
+  	 "V" + "I"*(num - 5)
+  elsif (num == 9)
+  	"I" + "X"
+  elsif num == 10
+  	 "X"
+  elsif (num > 10 and num < 49)
+  	if ( num%10 >= 5)
+  		if (num%10 != 9)
+  		 "X"*(num/10) + "V" + "I"*(num%10 - 5)
+  		else
+  			"X"*(num/10) + "IX"
+  		end
+  	else
+  		if (num%10 != 4)
+  		 "X"*(num/10) + "I"*(num%10)
+  		else
+  			"X"*(num/10) + "IV"
+  		end
+  	end
+  elsif num == 49
+  	"XLIX"
+  elsif num == 50 
+  	 "L"
+  elsif (num > 50 and num < 99)
+  	if (num%10 >= 5)
+  		if (num%10 != 9)
+  			"L" + "X"*((num-50)/10) + "V" + "I"*(num%10 - 5)
+  		else
+  			"L" + "X"*((num-50)/10)  + "IX"
+  		end
+  	else
+  		if (num%10 != 4)
+  		 	"L" + "X"*((num-50)/10) + "I"*(num%10)
+  		else
+  			"L" + "X"*((num-50)/10) + "IV"
+  		end
+  	end
+  elsif (num == 99)
+  	"XCIX"
+  elsif (num == 100)
+  	 "C"
+  elsif (num > 100 and num < 499)
+  	if ( num%100 >= 50)
+  		if (num%10 >= 5)
+  			 "C"*(num/100) + "L" + "X"*((num%100)/10 - 5) + "V" + "I"*(num%10 -5)
+  		else
+  			 "C"*(num/100) + "L" + "X"*((num%100)/10 - 5) + "I"*(num%10)
+  		end
+  	else
+  		if (num%10 >= 5)
+  			if(num%10 != 9)
+  			 "C"*(num/100) + "X"*(num%100/10) + "V" + "I"*(num%10 -5)
+  			else
+  			 "C"*(num/100) + "X"*(num%100/10) + "IX"
+  			end
+  		else
+  			if (num%10 != 4)
+  			 "C"*(num/100) + "X"*(num%100/10) + "I"*(num%10)
+  			else
+  				"C"*(num/100) + "X"*(num%100/10) + "IV"
+  			end
+  		end
+  	end
+  elsif (num == 499)
+  	"CDXCIX"
+  elsif (num == 500)
+  	 "D"
+
+  elsif (num > 500 and num < 1000)
+  	if ( num%100 >= 50)
+  		if (num%10 >= 5)
+  			if(num != 9)
+  				"D" + "C"*(num/100 - 5) + "L" + "X"*((num%100)/10 - 5) + "V" + "I"*(num%10 -5)
+  			else
+  				"D" + "C"*(num/100 - 5) + "L" + "X"*((num%100)/10 - 5) + "IX"
+  			end
+  		else
+  			if (num%10 != 4)
+  			 	"D" + "C"*(num/100 - 5) + "L" + "X"*((num%100)/10 - 5) + "I"*(num%10)
+  			else
+  				"D" + "C"*(num/100 - 5) + "L" + "X"*((num%100)/10 - 5) + "IV"
+  			end
+  		end
+  	else
+  		if (num%10 >= 5)
+  			if (num != 9)
+  			 	"D" + "C"*(num/100 - 5) + "X"*(num%100/10) + "V" + "I"*(num%10 -5)
+  			else 
+  				"D" + "C"*(num/100 - 5) + "X"*(num%100/10) + "IX"
+  			end
+  		else
+  			if (num != 4)
+  			 	"D" + "C"*(num/100 - 5) + "X"*(num%100/10) + "I"*(num%10)
+  			else
+  				"D" + "C"*(num/100 - 5) + "X"*(num%100/10) + "IV"
+  			end
+  		end
+  	end
+  elsif (num == 1000)
+  	 "M"
+  elsif (num > 1000 and num < 3001)
+  	if (num%1000 >= 500)
+  		if (num%100 >= 50)
+  			if (num%10 >= 5)
+  				 "M"*(num/1000) + "D" + "C"*(((num%1000)/100) - 5) + "L" + "X"*((num%100)/10 - 5) + "V" + "I"*(num%10 -5)
+  			else
+  				 "M"*(num/1000) + "D" + "C"*(((num%1000)/100) - 5) + "L" + "X"*((num%100)/10 - 5)  + "I"*(num%10)
+  			end
+  		else
+  			if (num%10 >= 5)
+  				 "M"*(num/1000)  + "D" + "C"*(((num%1000)/100) - 5)  + "X"*((num%100)/10) + "V" + "I"*(num%10 -5)
+  			else
+  				 "M"*(num/1000)  + "D" + "C"*(((num%1000)/100) - 5) + "X"*((num%100)/10)  + "I"*(num%10)
+  			end
+  		end
+  	else
+  		if (num%100 >= 50)
+  			if (num%10 >= 5)
+  				 "M"*(num/1000)  + "C"*((num%1000)/100) + "L" + "X"*((num%100)/10 - 5) + "V" + "I"*(num%10 -5)
+  			else
+  				 "M"*(num/1000)  + "C"*((num%1000)/100) + "L" + "X"*((num%100)/10 - 5)  + "I"*(num%10)
+  			end
+  		else
+  			if (num%10 >= 5)
+  				 "M"*(num/1000)   + "C"*((num%1000)/100)  + "X"*((num%100)/10) + "V" + "I"*(num%10 -5)
+  			else
+  				 "M"*(num/1000)  + "C"*((num%1000)/100) + "X"*((num%100)/10)  + "I"*(num%10)
+  			end
+  		end
+  	end
+  end
 end
+
+puts roman_numeral 4
+puts roman_numeral 9
+puts roman_numeral 29
+puts roman_numeral 1654
+puts roman_numeral 1155
+puts roman_numeral 2489
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,23 @@
 def shuffle arr
   # your code here
+  	shuf = []
+	while arr.length > 0
+    
+    	rand_index = rand(arr.length)
+		curr_index = 0
+		new_arr = []
+		arr.each do |item|
+			if curr_index == rand_index
+        			shuf.push item
+			else
+       				new_arr.push item
+			end
+      		curr_index = curr_index + 1
+		end
+		arr = new_arr
+	end
+	shuf
 end
+
+
+puts shuffle ['billy','mindy','sally','josh', 'sam']

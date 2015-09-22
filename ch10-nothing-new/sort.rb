04cac61fdac841a1e79abb5d917ae34063a58379
samover <samuel.overloop@gmail.com>
2015-09-22 19:27:47 +0200

    finished chapter 10

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,15 @@
-def sort arr
-  # your code here
+# LEARNING TO PROGRAM WITH CHRIS PINE, ch10
+###########################################
+# A method to SORT an array applying merge sort as 
+# found on https://en.wikipedia.org/wiki/Merge_sort
+
+def sort(arr)
+  return arr if arr.length < 1
+
+  pivot = arr.pop
+  less = arr.select { |item| item < pivot}
+  more = arr.select { |item| item >= pivot}
+
+  sort(less) + [pivot] + sort(more)
 end
+

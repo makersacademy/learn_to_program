8f0ee707905840d24c8ca0d4dc275f7f72af5838
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-22 18:11:38 +0100

    old_school_roman_numerals added

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,74 @@
-def old_roman_numeral num
-  # your code here
+puts "Please pick a number between 1 and 3000 to be converted into Old Roman Numerals"
+num = gets.chomp.to_i
+def old_roman_numeral(num)
+	if (num > 0 && num <3001)
+		roman_num = " "
+ 	m = num/1000
+	 	if m == 1
+	 		roman_num = roman_num + "M"
+	 	elsif m == 2
+	 		roman_num = roman_num + "MM"
+	 	elsif m == 3
+	 		roman_num = roman_num + "MMM"
+	 	end
+
+
+	d = (num % 1000) / 500
+	 	if d == 1
+	 		roman_num = roman_num + "D"
+	 	end
+
+
+	c = ((num % 1000) % 500) / 100
+	 	if c == 1
+	 		roman_num = roman_num + "C"
+	 	elsif c == 2
+	 		roman_num = roman_num + "CC"
+	 	elsif c == 3
+	 		roman_num = roman_num + "CCC"
+	 	elsif c == 4
+	 		roman_num = roman_num + "CCCC"
+	 	end
+ 
+
+ 	l = (((num % 1000) % 500) % 100) / 50
+ 		if l == 1
+ 			roman_num = roman_num + "L"
+ 		end
+ 
+
+ 	x = ((((num % 1000) % 500) % 100) % 50) /10
+ 		if x == 1
+ 			roman_num = roman_num + "X"
+ 		elsif x == 2
+ 			roman_num = roman_num + "XX"
+ 		elsif x == 3
+ 			roman_num = roman_num + "XXX"
+ 		elsif x == 4
+ 			roman_num = roman_num + "XXXX"
+ 		end
+ 
+
+ 	v = (((((num % 1000) % 500) % 100) % 50) % 10) / 5
+ 		if v == 1
+ 			roman_num = roman_num + "V"
+ 		end
+ 
+
+ 	i = ((((((num % 1000) % 500) % 100) % 50) % 10) % 5) / 1
+ 		if i == 1
+ 			roman_num = roman_num + "I"
+ 		elsif i == 2
+ 			roman_num = roman_num + "II"
+ 		elsif i == 3
+ 			roman_num = roman_num + "III"
+ 		elsif i == 4
+ 			roman_num = roman_num + "IIII"
+ 		end
+
+ 	puts roman_num
+ 	else
+ 		"Number is not between 1 and 3000"
+ 	end	
 end
+old_roman_numeral(num)

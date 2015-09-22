85a1446fbd61a9b94ce826683df6492654b97d88
thisdotrob <thisdotrob@gmail.com>
2015-09-22 22:16:56 +0100

    Add shuffle.rb solution

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,25 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle arr, []
+end
+
+def recursive_shuffle arr, shuffled_arr
+  
+  return shuffled_arr if arr.length == 0 
+
+  unshuffled_arr = []
+
+  index = rand(arr.length)
+
+  arr.each do |item|
+    
+    if item == arr[index]
+      shuffled_arr.push(item)
+    else
+      unshuffled_arr.push(item)
+    end
+
+  end
+
+  recursive_shuffle unshuffled_arr, shuffled_arr
+
 end

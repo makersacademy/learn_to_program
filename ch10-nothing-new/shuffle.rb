4c0da6424441f75976d0d28e2f372199290f3819
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-09-23 16:18:02 +0100

    adding shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,21 @@
-def shuffle arr
-  # your code here
+def shuffle some_array
+	recursive_shuffle some_array, []
+end
+
+def recursive_sort unshuffled_array, shuffled_array
+	if unshuffled_array.length <= 0
+		return shuffled_array
+	end
+
+	still_unshuffled = []
+	unshuffled_array.each {|object|
+		if rand < 0.5
+			still_unshuffled.push object
+		else 
+			shuffled_object = object
+			shuffled_array.push shuffled_object
+		end
+			}
+
+		recursive_shuffle still_unshuffled, shuffled_array
 end

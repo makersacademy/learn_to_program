484d68199dcc02f2cda8a86d04acb0ffae030a68
jamierose <jrose111284@gmail.com>
2015-09-23 13:44:04 +0100

    sort new

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,26 @@
 def dictionary_sort arr
-  # your code here
+  rec_sort arr, []
 end
+
+def rec_sort unsorted, sorted 
+  while unsorted.length <= 0
+  	return sorted
+  end
+ 
+ smallest = unsorted.pop
+ unsortedarr = []
+
+unsorted.each do |x|
+	if x < smallest
+		unsortedarr.push smallest
+		smallest = x
+	else
+		unsortedarr.push x
+	end
+end
+
+sorted.push smallest
+
+rec_sort unsortedarr, sorted
+end
+

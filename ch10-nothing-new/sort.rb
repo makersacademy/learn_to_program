5e9bc6a2346fc8255df6ef6ef8432ccdef69c2ee
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-09-23 15:19:58 +0100

    adding sort.rb file

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,22 @@
-def sort arr
-  # your code here
+def sort some_array
+	recursive_sort some_array, []
+end
+
+def recusive_sort unsorted_array, sorted_array
+	if unsorted_array.length <= 0
+		return sorted
+	end
+
+	smallest = unsorted_array.pop
+	still_unsorted = []
+	unsorted.each {|object|
+		if object < smallest
+			still_unsorted.push smallest
+			smallest = object
+		else 
+			still_unsorted.push object
+		end
+			}
+		sorted.push smallest
+		recursive_sort still_unsorted, sorted
 end

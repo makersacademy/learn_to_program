f0f51d5510a7b071f1208aff056fa607c79eab5c
Tom Barnes <ruralmyth@gmail.com>
2015-09-23 20:22:53 +0100

    Updated sort.rb and shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,21 @@
+
 def shuffle arr
-  # your code here
+  rec_shuffle arr, []
+end
+
+def rec_shuffle unshuffled, shuffled
+  seed = rand(unshuffled.length)
+  puts 'SEED: ' + seed.to_s
+  if (seed.is_a? Integer) == false
+    return
+  else
+    selected = 'nothing_selected'
+    unshuffled.each do |x|
+      unshuffled.index(x) == seed ? selected = unshuffled[seed] : selected = selected
+      end
+    shuffled << selected
+    unshuffled = (unshuffled - [selected])
+    rec_shuffle unshuffled, shuffled
+  end
+  return shuffled
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,38 @@
+# Had a few attempts at this and was struggling to set it up so took Chris Pine's answer and worked through it until it made sense.
+# Something to come back to and attempt again from scratch.
+
 def sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted
+  puts unsorted.join(', R! ')
+  if unsorted.length <= 0
+  return sorted
+  end
+
+# So if we got here, then it means we still # have work to do.
+
+  smallest = unsorted.pop
+  still_unsorted = []
+  unsorted.each do |tested_object|
+    if tested_object < smallest
+      still_unsorted.push smallest
+      smallest = tested_object
+    else
+      still_unsorted.push tested_object
+    end
+  end
+
+        #  Now "smallest" really does point to the
+        #  smallest element that "unsorted" contained,
+        #  and all the rest of it is in "still_unsorted".
+
+  sorted.push smallest
+  recursive_sort still_unsorted, sorted
+end
+
+=begin
+words = ['hip', 'snuggle', 'bowl', 'leaves', 'sofa', 'calendar', 'minecraft']
+puts (sort words).join(', ')
+=end

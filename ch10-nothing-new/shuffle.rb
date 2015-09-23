115ce96965f33b8412ee7f93bccb30b305fd2ece
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-23 23:54:39 +0100

    tweak

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,8 +1,5 @@
 def shuffle arr
   shuffled = []
-  (arr.length).times { 
-    shuffled << arr.delete_at(rand(0..arr.length-1)) }
+  (arr.length).times { shuffled << arr.delete_at(rand(0..arr.length-1)) }
   shuffled	
 end
-
-puts shuffle([ "hello", "my", "name", "is", "edward"])

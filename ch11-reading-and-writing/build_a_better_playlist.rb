e46a068b0ab7f1aecce019db0f00f7d0b57adf52
samover <samuel.overloop@gmail.com>
2015-09-23 17:41:52 +0200

    ch11 - last exercise - not correct

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,9 @@
-def music_shuffle filenames
-  # your code here
+# solution suggests mix = 2 * shuffle + 1 * cut
+
+def music_shuffle(filenames)
+  shuffled = filenames.sort.shuffle.shuffle
+  shuffled[filenames.length / 2..-1] + shuffled[0...filenames.length/2]
 end
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+puts music_shuffle(songs).inspect

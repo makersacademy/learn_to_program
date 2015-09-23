0c8c12f88764e4a52c3ef7d93bbc5d5230fc087a
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-23 08:53:59 +0100

    commit old numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,10 @@
 def old_roman_numeral num
-  # your code here
+  rom = "M" * (num / 1000)
+  rom = rom + "D" * (num % 1000 / 500)
+  rom = rom + "C" * (num % 500 /100)
+  rom = rom + "L" * (num % 100 /50)
+  rom = rom + "X" * (num % 50 /10)
+  rom = rom + "I" * (num % 10)
+  rom
 end
+puts (old_roman_numeral (1355))

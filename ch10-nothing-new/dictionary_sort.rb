0ceb64c678c22be5ec990f9c98d526104953fab7
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-09-23 16:44:22 +0100

    new dictionary_sort.rb file

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
-def dictionary_sort arr
-  # your code here
+def sort some_array
+	recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+	if unsorted_array.length <= 0
+		return sorted_array
+	end
+
+	smallest = unsorted_array.pop
+	still_unsorted = []
+	unsorted_array.each {|object|
+		if object.downcase < smallest.downcase
+			still_unsorted.push smallest
+			smallest = object
+		else 
+			still_unsorted.push object
+		end
+			}
+		sorted_array.push smallest
+		recursive_sort still_unsorted, sorted_array
 end

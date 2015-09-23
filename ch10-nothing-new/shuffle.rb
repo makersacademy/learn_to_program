2762d8eb92a39704be5c6d5667317b9bce3eef01
Robert Youngman <rob_youngman@yahoo.com>
2015-09-23 20:24:27 +0100

    First commit of shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,38 @@
 def shuffle arr
-  # your code here
+  length = arr.length
+  shuffles = length*100
+
+  shuffles.times do
+    index = 0
+    while index < (length -1)
+      n = arr[index]
+      nn = arr[index+1]
+
+      shuffle_word = rand(length) < (length/2)
+
+      if shuffle_word
+        arr[index] = nn
+        arr[index+1] = n
+      end
+      index += 1
+    end
+  end
+  return arr
 end
+
+#=begin
+num_array = []
+num = ""
+
+puts "Type in as many numbers as you want, one number per line"
+
+num = gets.chomp
+while num.length != 0
+  num_array.push num
+  num = gets.chomp
+end
+
+shuffle num_array
+
+num_array.each { |n| puts n}
+#=end

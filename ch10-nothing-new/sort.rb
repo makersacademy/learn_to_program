23ce263a80181ff0dd23c7255d80052e85dcafa7
mahmudh <mahmudhssn246@gmail.com>
2015-09-23 16:35:29 +0100

    sort added

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,26 @@
 def sort arr
-  # your code here
+	recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted
+	if unsorted.length <= 0
+    	return sorted 
+  	end
+	item = unsorted.pop
+	still_unsorted = []
+	
+	unsorted.each do |word|
+		if word < item
+			still_unsorted.push item 
+			item = word
+		else 
+			still_unsorted.push(word)
+		end
+	end
+
+	sorted.push(item)
+
+	recursive_sort still_unsorted, sorted
+end
+
+puts sort(["asdas","tetee","rrr"])

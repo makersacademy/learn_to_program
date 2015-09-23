b815f52258c188ef0e9eba3b81d063d75bea6ffd
Ben <benjrome@gmail.com>
2015-09-23 18:52:16 +0100

    Completed bottles of beer challenge

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,11 @@
-# your code here
+beginning_num = 10
+current_num = beginning_num
+
+while current_num > 2
+	puts english_number(current_num).capitalize + " bottles of beer on the wall" + 
+		 english_number(current_num) + " bottles of beer!"
+	current_num = current_num - 1
+	puts "Take one down, pass it around, " + english_number(current_num) +
+		 " bottles of beer on the wall!"
+end
+

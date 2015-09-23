544fb89dc946acf2d6a161e371b97de94ebe0ba0
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-23 15:50:55 +0100

    modified english_number method to deal with all possible numbers

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -36,7 +36,7 @@ def english_number number
         num = 0
       end
     end
-    str
+    str[(str.length-1),1] == "-" ? str[0,(str.length-1)]+" " : str
   end
 
 
@@ -46,23 +46,84 @@ def english_number number
       str = str + get_hundred(number)
       number = 0
     when (number.to_s.length > 3 && number.to_s.length < 7)
-      str = str + get_hundred(number/1000) + "thousand "
-      number = number%1000
+      str = str + get_hundred(number/("1"+"0"*3).to_i) + "thousand "
+      number = number%("1"+"0"*3).to_i 
     when (number.to_s.length > 6 && number.to_s.length < 10)
-      str = str + get_hundred(number/1000000) + "million "
-      number = number%1000000
+      str = str + get_hundred(number/("1"+"0"*6).to_i) + "million "
+      number = number%("1"+"0"*6).to_i 
     when (number.to_s.length > 9 && number.to_s.length < 13)
-      str = str + get_hundred(number/1000000000) + "billion "
-      number = number%1000000000
+      str = str + get_hundred(number/("1"+"0"*9).to_i) + "billion "
+      number = number%("1"+"0"*9).to_i 
     when (number.to_s.length > 12 && number.to_s.length < 16)
-      str = str + get_hundred(number/1000000000000) + "trillion "
-      number = number%1000000000000
+      str = str + get_hundred(number/("1"+"0"*12).to_i) + "trillion "
+      number = number%("1"+"0"*12).to_i 
     when (number.to_s.length > 15 && number.to_s.length < 19)
-      str = str + get_hundred(number/1000000000000000) + "zillion "
-      number = number%1000000000000000
+      str = str + get_hundred(number/("1"+"0"*15).to_i) + "quadrillion "
+      number = number%("1"+"0"*15).to_i 
+    when (number.to_s.length > 18 && number.to_s.length < 22)
+      str = str + get_hundred(number/("1"+"0"*18).to_i) + "quintillion "
+      number = number%("1"+"0"*18).to_i 
+    when (number.to_s.length > 21 && number.to_s.length < 25)
+      str = str + get_hundred(number/("1"+"0"*21).to_i) + "sextillion "
+      number = number%("1"+"0"*21).to_i 
+    when (number.to_s.length > 24 && number.to_s.length < 28)
+      str = str + get_hundred(number/("1"+"0"*24).to_i) + "septillion "
+      number = number%("1"+"0"*24).to_i 
+    when (number.to_s.length > 27 && number.to_s.length < 31)
+      str = str + get_hundred(number/("1"+"0"*27).to_i) + "octillion "
+      number = number%("1"+"0"*27).to_i 
+    when (number.to_s.length > 30 && number.to_s.length < 34)
+      str = str + get_hundred(number/("1"+"0"*30).to_i) + "nonillion "
+      number = number%("1"+"0"*30).to_i 
+    when (number.to_s.length > 33 && number.to_s.length < 37)
+      str = str + get_hundred(number/("1"+"0"*33).to_i) + "decillion "
+      number = number%("1"+"0"*33).to_i 
+    when (number.to_s.length > 36 && number.to_s.length < 40)
+      str = str + get_hundred(number/("1"+"0"*36).to_i) + "undecillion "
+      number = number%("1"+"0"*36).to_i 
+    when (number.to_s.length > 39 && number.to_s.length < 43)
+      str = str + get_hundred(number/("1"+"0"*39).to_i) + "duodecillion "
+      number = number%("1"+"0"*39).to_i 
+    when (number.to_s.length > 42 && number.to_s.length < 46)
+      str = str + get_hundred(number/("1"+"0"*42).to_i) + "tredecillion "
+      number = number%("1"+"0"*42).to_i 
+    when (number.to_s.length > 45 && number.to_s.length < 49)
+      str = str + get_hundred(number/("1"+"0"*45).to_i) + "quattuordecillion "
+      number = number%("1"+"0"*45).to_i 
+    when (number.to_s.length > 48 && number.to_s.length < 52)
+      str = str + get_hundred(number/("1"+"0"*48).to_i) + "quindecillion "
+      number = number%("1"+"0"*48).to_i 
+    when (number.to_s.length > 51 && number.to_s.length < 55)
+      str = str + get_hundred(number/("1"+"0"*51).to_i) + "sexdecillion "
+      number = number%("1"+"0"*51).to_i 
+    when (number.to_s.length > 54 && number.to_s.length < 58)
+      str = str + get_hundred(number/("1"+"0"*54).to_i) + "septendecillion "
+      number = number%("1"+"0"*54).to_i 
+    when (number.to_s.length > 57 && number.to_s.length < 61)
+      str = str + get_hundred(number/("1"+"0"*57).to_i) + "octodecillion "
+      number = number%("1"+"0"*57).to_i 
+    when (number.to_s.length > 60 && number.to_s.length < 64)
+      str = str + get_hundred(number/("1"+"0"*60).to_i) + "novemdecillion "
+      number = number%("1"+"0"*60).to_i 
+    when (number.to_s.length > 63 && number.to_s.length < 67)
+      str = str + get_hundred(number/("1"+"0"*63).to_i) + "vigintillion "
+      number = number%("1"+"0"*63).to_i
+    else
+      number = 0
     end
+
+
+
+
   end
 
   str.strip
 
 end
+
+
+
+puts english_number(560123)
+puts english_number(560123000)
+puts english_number(560123000000)
+puts english_number(560123000000000)

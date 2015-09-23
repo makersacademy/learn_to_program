0f46333145dfe41783c942c0e8bab2d3e7823443
samover <samuel.overloop@gmail.com>
2015-09-23 17:23:17 +0200

    first two exercises of ch11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,5 @@
-# your code here
+require_relative "../ch10-nothing-new/shuffle"
+
+Dir.chdir "#{ENV['HOME']}/mymusic/playlists"
+file_list = Dir["#{ENV['HOME']}/mymusic/**/*.mp3"]
+File.write('playlist.m3u', shuffle(shuffle(file_list)).join("\n"))
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,26 @@
-# your code here
+# LEARNING TO PROGRAM WITH CHRIS PINE, 11.8 ex 1
+################################################
+# Safer picture downloading. Adapt the picture-downloading/file-renaming
+# program to your computer by adding some safety features to make sure
+# you never overwrite a file. A few methods you might find useful are
+# File.exist? (pass it a filename, and it will return true or false) and exit
+# (like if return and Napoleon had a baby—it kills your program right where it 
+# stands; this is good for spitting out an error message and then quitting).
+
+Dir.chdir "#{ENV['HOME']}/PictureInbox"
+pic_names = Dir[ENV['HOME'] + '/camera/*.jpg']
+
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+print "Downloading #{pic_names.length} files:  "
+
+pic_names.each_with_index do |name, i|
+  print '.'
+  new_name = "#{batch_name}#{"%02d" % (i + 1)}"
+  File.exist?("#{new_name}.jpg") ? File.rename(name, "#{new_name}-dup.jpg") : File.rename(name, "#{new_name}.jpg")
+end
+
+puts
+puts 'Done!'

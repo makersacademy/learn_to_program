94590238cdd1709720037b8a375fc9140f7a6e98
jamierose <jrose111284@gmail.com>
2015-09-23 14:39:59 +0100

    bottles

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,9 @@
-# your code here
+  bottle = 99
+  num = 0
+
+while bottle >= num do
+
+puts "#{bottle} bottle\'s of beer #{bottle} bottle\'s on the wall put one down pass it around now theres"
+
+bottle -= 1
+end

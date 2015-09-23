1e8d8e3fefd5995d6c4db16575bd4f9094bcf1c5
octavian <octavianrotari@gmail.com>
2015-09-23 17:04:29 +0200

    understood exercise sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,19 +1,20 @@
-def sort (array,array_sorted)
-	return 0 if array.length <= 0
-	x = 0
-	y = 0
-	while y < array.length
-		if array[x] < array[y]
-			y += 1
-		elsif array[x] > array[y]
-			x = y
-			y += 1
-		else
-			y += 1
-		end 	
-		array_sorted.push(array[x])
-		array.delete(array[x])
-		sort(array,array_sorted)
-	end
+def sort arr
+    rec_sort arr, []
 end
-end
+def rec_sort unsorted, sorted
+    if unsorted.length <= 0
+        return sorted
+    end
+    smallest = unsorted.pop
+    still_unsorted = []
+    unsorted.each do |tested_object|
+        if tested_object < smallest
+            still_unsorted.push smallest
+            smallest = tested_object
+        else
+            still_unsorted.push tested_object
+        end
+    end
+    sorted.push smallest
+    rec_sort still_unsorted, sorted
+end    

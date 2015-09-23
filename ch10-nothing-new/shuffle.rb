fbd2d345c0bd559a7b868c584744d29c9cf0d78a
fergus lemon <fergus.lemon@gmail.com>
2015-09-23 19:39:56 +0100

    updated shuffle file

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,24 @@
 def shuffle arr
-  # your code here
+  shuf = []
+  while arr.length > 0
+  rand_index = rand(arr.length)
+  current_index = 0
+  new_arr = []
+  
+  arr.each do |item|
+  	if current_index == rand_index
+  	  shuf.push item
+  	else
+  	  new_arr.push item
+  	 end
+  	 
+  	 current_index = current_index + 1
+  	 end
+  	 
+  	 arr = new_arr
+  	end
+  	
+  	shuf
 end
+
+puts(shuffle([10,20,30,40,50,60,70,75,82]))

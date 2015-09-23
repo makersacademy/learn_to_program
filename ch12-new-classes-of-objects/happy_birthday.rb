f888bb75481995ea82fbe89edb94a38307b15d52
Hamza Sheikh <hsheikhm@gmail.com>
2015-09-23 22:12:49 +0100

    Completed happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp.to_i
+
+puts "What month were you born in?"
+month = gets.chomp.to_i
+
+puts "What day were you born in?"
+day = gets.chomp.to_i
+
+date_of_birth = Time.gm(year, month, day)
+current_year = Time.new
+
+seconds_in_one_year = date_of_birth - Time.gm(year-1, month, day)
+current_age = ((current_year - date_of_birth) / seconds_in_one_year).to_i
+
+puts "Your current age is: #{current_age} which means you get #{current_age} SPANKs!"
+puts
+current_age.times{puts "SPANK!"}

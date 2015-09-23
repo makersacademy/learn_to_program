ca8088d7bab6b8b69856765518feb6ae62e8c314
mahmudh <mahmudhssn246@gmail.com>
2015-09-23 18:08:30 +0100

    dictory sort added

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort arr 
+  recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted 
+  if unsorted.length <= 0
+    return sorted 
+  end
+
+  item = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |word|
+    if word.downcase < item.downcase
+      still_unsorted.push item
+      item = word
+    else
+      still_unsorted.push word
+    end 
+  end
+  
+  sorted.push item
+  
+  recursive_sort still_unsorted, sorted
+end
+
+puts dictionary_sort(['Arr', 'arr'])
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,44 @@
 def shuffle arr
-  # your code here
+  shuf = []
+
+  while arr.length > 0
+    rand_index = rand(arr.length)
+
+    curr_index = 0
+    new_arr = []
+
+    arr.each do |item|
+      if curr_index == rand_index
+        shuf.push item
+      else
+        new_arr.push item
+      end
+
+      curr_index = curr_index + 1
+    end
+
+    arr = new_arr
+  end
+
+  shuf
 end
+
+puts(shuffle([1,2,3,4,5]))
+
+
+# def shuffle arr
+# 	shuf_arr = []
+# 	# new_arr = []
+# 	sum = 0
+
+# 	while sum <= arr.length
+# 		rand_arr = rand(arr.length)
+# 		item = arr[rand_arr]
+# 		shuf_arr.push item
+
+# 		sum = sum + 1
+# 	end
+# 	shuf_arr
+# end
+
+# puts(shuffle([1,2,3,4,5,6]))

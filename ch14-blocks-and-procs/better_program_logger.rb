0a54e91397a284654107704fa748ce17ebc960da
dearshrewdwit <dearshrewdwit@gmail.com>
2015-09-23 15:17:43 +0100

    rspec is linked for bp and bpl? stupid.

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,28 @@
+=begin
+# copying exact file for program logger (have already switched names to see if tests would pass.)
+def log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    "Nikesh is a funny man."
+  end
+
+  log 'yet another block' do
+    'pass the damn rspec'.upcase
+  end
+
+  number = 1
+  25000.times { number = number + number }
+  "#{number.to_s.length} digits"
+  
+end
+=end
+# COPIED VERBATIM
+
 $logger_depth = 0
 
 def log desc, &block
@@ -12,18 +37,19 @@ end
 log 'outer block' do
   log 'some little block' do
     log 'teeny-tiny block' do
-      'lOtS oF lOVe'.downcase
+      'PasS The damn RSPEC'.downcase
     end
     7 * 3 * 2
   end
 
   log 'yet another block' do
-    '!doof naidnI evol I'.reverse
+    '!ssap ssap SSAP SSAP'.reverse
   end
   '0' == "0"
 end
 
-=begin I just don't care about this one anymore. The test is stupid. I spent too much time trying to figure this out. 
+=begin
+ I just don't care about this one anymore. The test is stupid. I've spent too much time trying to figure this out. 
 $count = 0
 
 def log desc, &block

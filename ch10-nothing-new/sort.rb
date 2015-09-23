6360bb617708a7124b3f0cccd84cf49dbf618d41
jrose111284@gmail.com <jamierose@Jamies-MacBook-Pro.local>
2015-09-23 12:25:11 +0100

    sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,26 @@
-def sort arr
-  # your code here
+def sort arr 
+  rec_sort arr, []
 end
+
+def rec_sort unsorted, sorted 
+  while unsorted.length <= 0
+  	return sorted
+  end
+ 
+ smallest = unsorted.pop
+ unsortedarr = []
+
+unsorted.each do |x|
+	if x < smallest
+		unsortedarr.push smallest
+		smallest = x
+	else
+		unsortedarr.push x
+	end
+end
+
+sorted.push smallest
+
+rec_sort unsortedarr, sorted
+end
+

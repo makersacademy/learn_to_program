e31eb452a7a379615be95febff8e30f0913c731a
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-23 07:42:20 +0100

    Third commit

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,11 +1,9 @@
 def ask question
   while true
     puts question
-    reply = gets.chomp
+    reply = gets.chomp.downcase
     return "true" if reply == "yes"
     return "false" if reply == "no"
     puts 'Please answer "yes" or "no"'
   end
 end
-
-puts(ask "Do you like Tacos")

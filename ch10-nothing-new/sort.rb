8965b8ad1088e79a1dd6b3654463725563a5ad90
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-23 13:06:47 +0100

    sort.rb commit

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,21 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+def recursive_sort array, sorted_arr
+  if array == []
+    return sorted_arr
+  end
+
+  unsorted_arr = []
+  smallest = array.pop
+  array.each do |x|
+    if x < smallest
+      unsorted_arr.push smallest
+      smallest = x
+    else unsorted_arr.push x
+    end
+  end
+    sorted_arr.push smallest
+    recursive_sort unsorted_arr, sorted_arr
+  end

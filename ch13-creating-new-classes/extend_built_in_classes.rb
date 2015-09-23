0c14c86ab91d5b093bbe3c925d0bc8c9aaeb563b
Norm Fasey <norm@fasey.net>
2015-09-23 20:20:00 +0100

    Chapter 13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,17 @@
+require_relative '../ch09-writing-your-own-methods/old_school_roman_numerals'
+
 class Integer
-  # your code here
+  def factorial
+    fact(self, 1)
+  end
+
+  def fact(n, acc)
+  return acc if n <= 1
+    fact(n-1, n*acc)
+  end
+
+  def to_roman
+    old_roman_numeral self
+  end
 end
+
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,59 @@
+ANNUAL_GROWTH = 0.4
+MAX_HEIGHT = 10.0
+MIN_HEIGHT = 2.0
+
 class OrangeTree
-  # your code here
+
+  def initialize
+    @orange_count = 0
+    @height = 0
+    @alive = true
+  end
+
+  def height
+    if @alive
+      @height
+    else
+      'A dead tree is not very tall. :('
+    end
+  end
+
+  def one_year_passes
+    if @alive
+      @height = (@height + ANNUAL_GROWTH).round(1)
+      @orange_count = 0
+
+      if @height > MAX_HEIGHT
+        @alive = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > MIN_HEIGHT
+        @orange_count = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height}m tall, and produced #{@orange_count} oranges."
+      else
+        "This year your tree grew to #{@height}m tall, but is still too young to bear fruit."
+      end
+    else
+      'A year later, the tree is still dead. :('
+    end
+  end
+
+  def pick_an_orange
+    if @alive
+      if @orange_count > 0
+        @orange_count -= 1
+      else
+        'There are no oranges to pick :('
+      end
+    else
+      'A dead tree has nothing to pick. :('
+    end
+  end
+
+  def count_the_oranges
+    if @alive
+      @orange_count
+    else
+      'A dead tree has no oranges. :('
+    end
+  end
 end

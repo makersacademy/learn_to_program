d15cf2efa25757e6d32dc5892491d3b7ccdd0e55
fergus lemon <fergus.lemon@gmail.com>
2015-09-24 18:21:12 +0100

    updated file for my birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,22 @@
-# your code here
+puts 'Please answer the following questions in number format only i.e. dd/mm/yyyy or 26/01/1985'
+
+puts 'What year were you born?'
+birth_year = gets.chomp.to_i
+
+puts 'What month were you born in?'
+birth_month = gets.chomp.to_i
+
+puts 'What day of the month were you born on?'
+birth_day = gets.chomp.to_i
+
+birth = Time.local(birth_year , birth_month, birth_day)
+time = Time.new
+
+age = 1
+
+while Time.local(birth_year + age , birth_month, birth_day) <= time
+puts 'Spank'
+age = age + 1
+
+end
+

76558a423d174e938b7b9cfe72107e1db052045a
thisdotrob <thisdotrob@gmail.com>
2015-09-25 00:22:31 +0100

    Tweak orange_tree.rb to pass Travis CI tests

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,38 +1,55 @@
 class OrangeTree
 
   @height = 0
-  @age = 0
   @fruit = 0
+  @alive = true
+
 
   def height
-    return @height
+    { @alive ? @height : 'A dead tree is not very tall. :(' }
   end
 
-  def one_year_passes
-    @height += 100
-    @age += 1
-    @fruit = (@age-3)**2 if @age > 3
-    tree_dies if @age > 50
-  end
 
   def count_the_oranges
-    @fruit
+    { @alive? ? @fruit : 'A dead tree has no oranges. :(' }
   end
+
+
+  def one_year_passes
+    if @alive
+      @height += 0.4
+      @fruit = 0
+
+      if @height > 12
+        @alive = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > 2
+        @orange_count = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height}m tall, " +
+        "and produced #{@fruit} oranges."
+      else
+        "This year your tree grew to #{@height}m tall, " +
+        "but is still too young to bear fruit."
+      end
+
+    else
+      "A year later, the tree is still dead. :("
+    end
+        
+  end
+  
 
   def pick_an_orange
-    if @fruit > 0
-      @fruit -= 1
-      puts "That was delicious."
+    if @alive
+      if @fruit > 0
+        @fruit -= 1
+        "You pick a juicy, delicious orange!"
+      else
+      "You search every branch, but find no oranges."
+      end
     else
-      puts "No oranges left to pick."
+      "A dead tree has nothing to pick. :("
     end
-  end
-
-  private
-
-  def tree_dies
-    puts "Dead tree."
-    exit
   end
 
 end

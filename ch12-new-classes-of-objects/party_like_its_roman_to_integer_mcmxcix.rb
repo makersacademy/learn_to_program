d63ae12d016390a98f67c67fe20d5ac080b8fcd1
fergus lemon <fergus.lemon@gmail.com>
2015-09-24 18:32:44 +0100

    updated party like its 1999

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,35 @@
 def roman_to_integer roman
-  # your code here
+  digits = {'i' => 1,
+  			'v' => 5,
+  			'x' => 10,
+  			'l' => 50,
+  			'c' => 100,
+  			'd' => 500,
+  			'm' => 1000}
+  	total = 0
+  	previous = 0
+  	index = roman.length - 1
+  	while index >= 0
+  		c = roman[index].downcase
+  		index = index - 1
+  		val = digits[c]
+  		if !val
+  			puts 'This is not a roman numeral'
+  			return
+  		end
+  		
+  		if val < previous
+  		  val = val * -1
+  		else
+  		  previous = val
+  		end
+  		
+  		total = total + val
+  	end
+  	
+  	total		 
 end
+
+puts (roman_to_integer('mddcl'))
+puts (roman_to_integer('mcmxcix'))
+puts (roman_to_integer('MMLLII'))

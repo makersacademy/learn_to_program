8dbd544f159a5399809b39a13b8cea85306dada5
thisdotrob <thisdotrob@gmail.com>
2015-09-24 21:04:43 +0100

    Add happy_birthday.rb solution

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,11 @@
-# your code here
+puts 'What year was the person born in?'
+yearBorn = gets.chomp.to_i
+puts 'and what month?'
+monthBorn = gets.chomp.to_i
+puts 'and day?'
+dayBorn = gets.chomp.to_i
+ageInSeconds = ( Time.new - Time.mktime(yearBorn,monthBorn,dayBorn) ).to_i
+puts 'Age in seconds: ' + ageInSeconds.to_s
+ageInYears = ( ageInSeconds / (365.25*24*60*60) ).to_i
+puts 'Age in years: ' + ageInYears.to_s
+puts 'SPANK! ' * ageInYears# your code here

70f24d43bcb150414159cad05fdbc6b8926e9f1c
Chuka Ebi <chuka.ebi@outlook.com>
2015-09-24 20:40:25 +0100

    Updated party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,16 +1,13 @@
-require '/Users/chukaebi/Makers_Academy/learn_to_program/ch09-writing-your-own-methods/roman_numerals.rb'
+load '/Users/chukaebi/Makers_Academy/learn_to_program/ch09-writing-your-own-methods/roman_numerals.rb'
 
 def roman_to_integer roman
   # your code here
-  puts roman
   b = 0
   prop_roman = roman.upcase
   numbers = (1..3000).to_a
   numbers.each do |rome|
   	if (prop_roman == roman_numeral(rome))
   		b = rome
-  		puts "Found it!"
-  		break
   	else
   		 nil
   	end

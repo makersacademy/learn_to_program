b1175c40594c4f5cf23c61e64712fb705fbe4a8a
alaanzr <alaanzr@gmail.com>
2015-09-24 20:21:03 +0100

    Completed happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,24 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "What month were you born in?"
+month = gets.chomp
+
+puts "What day were you born in?"
+day = gets.chomp
+
+
+time_alive = Time.gm(year, month, day)
+current_time = Time.now
+age = current_time - time_alive
+
+age_in_years = ((((age / 60)/60)/24)/365) 
+# Note: Does not take into account leap years
+rounded_age = age_in_years.floor
+
+(0..rounded_age-1).each do |spank|
+	puts "SPANK!"
+end
+
+
+

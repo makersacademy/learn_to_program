e897255c9b248d48edcfdc7d94b016843e62c8e8
Andrew Dowell <adowellphoto@gmail.com>
2015-09-24 23:39:05 +0100

    Answered chapter 10

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,13 @@
 def shuffle arr
-  # your code here
+  	shuffleMethod arr, []
+end
+
+def shuffleMethod arr, shuffled_arr
+	for i in 0..arr.length-1
+		randomVal = rand(arr.length)
+		findindex = arr.at(randomVal)
+		shuffled_arr << findindex
+		arr.delete findindex
+	end
+	return shuffled_arr
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,8 @@
-def sort arr
-  # your code here
+def sort(arr)
+  return arr if arr.length < 1
+  pivot = arr.pop
+  less = arr.select { |item| item < pivot}
+  more = arr.select { |item| item >= pivot}
+
+  sort(less) + [pivot] + sort(more)
 end

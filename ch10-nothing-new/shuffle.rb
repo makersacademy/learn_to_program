86371ac93696e7cbdd8db4ea2c6d117068aae8b0
octavian <octavianrotari@gmail.com>
2015-09-24 14:40:49 +0200

    shuffle method created

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,18 @@
 def shuffle arr
-  # your code here
+  shuf = []
+  while arr.lengts < 0
+  	index = 0 
+  	rand_ind = rand(arr.lengts)
+  	new_arr = []
+  	arr.each do | var | 
+  		if index == rand_ind
+  			shuf << var
+  		else
+  			new_arr << var
+  		end
+  		index = index + 1
+  	end
+  	arr = new_arr
+  end
+  shuf
 end

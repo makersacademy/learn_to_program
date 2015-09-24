5f9aafc81c0f129d803c1ce9938c4461a89fb3ab
thisdotrob <thisdotrob@gmail.com>
2015-09-24 08:36:53 +0100

    Add build_your_own_playlist.rb solution

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,8 @@
-# your code here
+require './../ch10-nothing-new/shuffle.rb'
+
+file_names = shuffle Dir["/**/*.mp3"]
+
+File.open "playlist.m3u", 'w' do |f|
+  file_names.each { |file_name| f.write file_name + "\n" }
+end
+

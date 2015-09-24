a8837d95cb7617a3d858a2355da8554260da8b73
Mateja <mateja683@gmail.com>
2015-09-24 19:26:36 +0100

    file name changes for roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,36 +1,38 @@
 def roman_numeral num
-	thousands = (num / 1000)
-	hundreds = num % 1000/100)
-	tens = (num % 100/10)
-	ones = (num % 10) 	
- 
- 	roman = roman + "M" * thousands
+	thous = (num / 1000) 
+	hunds = (num % 1000 / 100) 
+	tens=(num%100/ 10) 
+	ones=(num% 10 )
+  	
+  	roman = 'M' * thous
 
- 	if hundreds == 9
- 		roman = roman +"CM"
- 	elsif hundreds == 4
- 		roman = roman + "CD"
+	if hunds == 9
+		roman = roman + 'CM'
+	elsif hunds == 4
+		roman = roman + 'CD'
 	else
     	roman = roman + 'D' * (num % 1000 / 500)
-		roman = roman+'C'*(num% 500/100)
+		roman=roman+'C'*(num% 500/100) 
 	end
 
 	if tens == 9
-		roman = roman + "XC"
+		roman = roman + 'XC'
 	elsif tens == 4
-		roman = roman + "XL"
+		roman = roman + 'XL'
 	else
 		roman=roman+'L'*(num% 100/ 50)
-		roman=roman+'X'*(num% 50/ 10)
+		roman=roman+'X'*(num% 50/ 10) 
 	end
 
 	if ones == 9
-		roman = roman + "IX"
+		roman = roman + 'IX'
 	elsif ones == 4
-		roman = roman + "IV"
+		roman = roman + 'IV'
 	else
-		roman = roman+'V'*(num% 10/ 5)
-	roman = roman+'I'*(num% 5/ 1) 
+		roman=roman+'V'*(num% 10/ 5)
+		roman=roman+'I'*(num% 5/ 1) 
 	end
+	roman
 end
-puts(roman_numeral(2015))
+
+puts(roman_numeral(1999))
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,5 @@
 def shuffle arr
-  # your code here
+	arr.sort_by{rand}
 end
+
+puts(shuffle([a,b,c,d,e,f,g,h,i]))

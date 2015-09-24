090aee5477282b3d8e2ec7d3df5ea618ea1a32d4
Mattia46 <mattia.assogna@gmail.com>
2015-09-24 09:54:50 +0100

    ask2

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -3,13 +3,14 @@ def ask question
 		puts question
 		reply = gets.chomp.downcase
 
-		if reply == 'yes' 
+		if reply == 'yes'
 			return true
 		end
 		if reply == 'no'
-			return false 
-		end	
+			return false
+		end
+
 		puts 'Please answer "yes" or "no".'
 	end
-answer 
+answer
 end

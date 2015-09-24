0f958fca26aab2a606949c73adc98ce505744972
“DovileSand” <dovile.sand@protonmail.ch>
2015-09-24 23:43:40 +0100

    Modified roman_numerals.rb and fixed old_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -6,13 +6,13 @@ def old_roman_numeral(num)
     
     roman_num = roman_num + 'M' * (num / 1000)
     roman_num = roman_num + 'D' * ((num % 1000) / 500)
-    roman_num = roman_num + 'C' * (num / 100)
+    roman_num = roman_num + 'C' * ((num % 500) / 100)
     roman_num = roman_num + 'L' * ((num % 100) / 50)
-    roman_num = roman_num + 'X' * (num / 10)
+    roman_num = roman_num + 'X' * ((num % 50) / 10)
     roman_num = roman_num + 'V' * ((num % 10) / 5)
     roman_num = roman_num + 'I' * ((num % 5) / 1 )
     
     roman_num
 end
 
-old_roman_numeral(2753)
+old_roman_numeral(53)

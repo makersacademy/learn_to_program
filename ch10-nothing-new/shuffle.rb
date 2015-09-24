09f883ca0edaf6729e6b440ea2630d75ddee101c
Robert Youngman <rob_youngman@yahoo.com>
2015-09-24 11:50:14 +0100

    Copied arr to new array to see if this akes a difference to pass CI

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -2,42 +2,24 @@ def shuffle arr
   length = arr.length
   shuffles = length*100
 
+  new_arr = arr
+
   shuffles.times do
     index = 0
     while index < (length -1)
-      n = arr[index]
-      nn = arr[index+1]
+      n = new_arr[index]
+      nn = new_arr[index+1]
 
       shuffle_it = rand(length) < (length/2)
 
       if shuffle_it
-        arr[index] = nn.to_i
-        arr[index+1] = n.to_i
+        new_arr[index] = nn.to_i
+        new_arr[index+1] = n.to_i
       end
       index += 1
     end
   end
-
-  new_arr = arr
   return new_arr
 end
 
-#puts shuffle [1,2,3,4,5,6,7,8,9]
-
-
-=begin
-num_array = []
-num = ""
-
-puts "Type in as many numbers as you want, one number per line"
-
-num = gets.chomp
-while num.length != 0
-  num_array.push num
-  num = gets.chomp
-end
-
-shuffle num_array
-
-num_array.each { |n| puts n}
-=end
+puts shuffle [1,2,3,4,5,6,7,8,9]

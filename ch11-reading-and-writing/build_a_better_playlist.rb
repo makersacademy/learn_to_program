cd94e54c88720bd6b1d0812a88fdf6fc18ad6dcb
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-24 16:43:37 +0100

    added build a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,15 @@
 def music_shuffle filenames
   # your code here
+  mid = filenames.length / 2
+
+  shuf1 = filenames[0..mid]
+  shuf2 = filenames[(mid+1)..filenames.length]
+
+  shuf1.sort!{rand} + shuf2.sort!{rand}
+
 end
+
+songs = ['aa/bbb',   'aa/ccc',   'aa/ddd',
+         'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+
+puts(music_shuffle(songs))

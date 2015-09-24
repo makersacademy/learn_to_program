661761cd103b82548605532e64ed4c9aa15e03e6
Ben <benjrome@gmail.com>
2015-09-24 21:27:06 +0100

    Completed roman to integer challenge

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,34 @@
 def roman_to_integer roman
-  # your code here
+	digital_vals = {
+		'i' => 1,
+		'v' => 5,
+		'x' => 10,
+		'l' => 50,
+		'c' => 100,
+		'd' => 500,
+		'm' => 1000
+	}
+
+	total = 0
+	prev = 0
+	index = roman.length - 1
+
+	while index >= 0
+		c = roman[index].downcase
+		index -= 1
+		val = digital_vals[c]
+		if !val
+			puts 'Not a valid numeral'
+			return
+		end
+
+		if val < prev
+			val *= -1
+		else 
+			prev = val
+		end
+		total += val
+	end
+	total
+
 end

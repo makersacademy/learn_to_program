a069b254bd128b95bd458b80b9c34974853e1e41
Chuka Ebi <chuka.ebi@outlook.com>
2015-09-24 20:30:17 +0100

    Update build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,20 +1,4 @@
-require 'yaml'
 
 def music_shuffle filenames
 # your code here
-music = File.read filenames
-
-music_array = YAML:: load music
-
-
-music_strings = shuffled_songs.join("\n")
-
-puts music_strings
-
-File.open filenames, 'w' do |f| 
-	f.write(music_strings)
- end
-
 end
-
-music_shuffle('playlist.m3u')

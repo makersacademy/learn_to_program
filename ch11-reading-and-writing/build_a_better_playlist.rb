c38279aabe58c47d51032442ca5bdd56d97c2b07
samover <samuel.overloop@gmail.com>
2015-09-24 09:22:36 +0200

    Finished chapter 11

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,9 +1,29 @@
-# solution suggests mix = 2 * shuffle + 1 * cut
+# This is the exercise with the most confusing task description. After studying
+# CP's solution, I conclude this is what he wants, using the analogy of a card
+# deck:
+# 1) sort the deck
+# 2) shuffle them, i.e. odds + evens
+# 3) cut the deck
+#
+# This solution passes the test. Finally.
+
+def shuffle(filenames)
+  filenames.select.with_index { | _, i| i.odd? } +
+  filenames.select.with_index { | _, i| i.even? }
+end
+
+def cut(filenames)
+  # doesn't need to be random, but let's be fancy in stead of boring 
+  # cut in half
+  cutoff = rand(filenames.length)
+  filenames[cutoff..-1] + filenames[0...cutoff]
+end
 
 def music_shuffle(filenames)
-  shuffled = filenames.sort.shuffle.shuffle
-  shuffled[filenames.length / 2..-1] + shuffled[0...filenames.length/2]
+  cut(shuffle(filenames.sort))
 end
 
 songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+
 puts music_shuffle(songs).inspect
+

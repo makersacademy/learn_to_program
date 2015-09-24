415002154f20a33e9aa25f16086848f6c3af36bc
thisdotrob <thisdotrob@gmail.com>
2015-09-25 00:04:51 +0100

    Add orange_tree.rb solution

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,38 @@
 class OrangeTree
-  # your code here
+
+  @height = 0
+  @age = 0
+  @fruit = 0
+
+  def height
+    return @height
+  end
+
+  def one_year_passes
+    @height += 100
+    @age += 1
+    @fruit = (@age-3)**2 if @age > 3
+    tree_dies if @age > 50
+  end
+
+  def count_the_oranges
+    @fruit
+  end
+
+  def pick_an_orange
+    if @fruit > 0
+      @fruit -= 1
+      puts "That was delicious."
+    else
+      puts "No oranges left to pick."
+    end
+  end
+
+  private
+
+  def tree_dies
+    puts "Dead tree."
+    exit
+  end
+
 end

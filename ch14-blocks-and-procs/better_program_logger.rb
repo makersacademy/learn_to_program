64c10ef78e92fe2f2764a3171216307ce4d4b75e
Norm Fasey <norm@fasey.net>
2015-09-24 18:35:39 +0100

    Better Program Logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,14 @@
+$indent = 0
+
 def better_log desc, &block
-  # your code here
+  prefix = String.new
+  $indent.times do
+   prefix << " "
+  end
+  puts "#{prefix}Beginning #{desc.inspect}..."
+  $indent += 1
+  result = block.call
+  $indent -= 1
+  puts "#{prefix}...#{desc.inspect} finished, returning: #{result}"
 end
+

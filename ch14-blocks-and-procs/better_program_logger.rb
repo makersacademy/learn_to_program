0e83a71722052b78e2881c02b54f8cab37fb9ddd
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-25 19:47:24 +0100

    added better_program_logger code

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
+
+$indent=" "
+$level=0
+
 def log desc, &block
   # your code here
+  puts "#{$indent*$level}Beginning \"#{desc}\"..."
+  $level +=1
+  retval=block.call
+  $level -=1
+  puts "#{$indent*$level}...\"#{desc}\" finished, returning: #{retval}"
+end
+
+log "outer block" do
+	log "some little block" do
+    log "teeny-tiny block" do
+      "lots of love"
+    end
+    "42"
+	end
+  log "yet another block" do
+	   "I love Indian food"
+  end
+	"true"
 end

03e0e4235903961d11553c88b61d2d2268f6cce6
blancopado <pablo.dominguezblanco@gmail.com>
2015-09-25 23:17:21 +0200

    shuffle: trying to pass rspec test

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,5 @@
+=begin
+
 def shuffle arr
   # your code here
 	arr_shuffled = []
@@ -19,3 +21,15 @@ end
 arr = [1,2,3,4,5,6,7,8,9]
 
 shuffle arr
+
+=end
+
+def shuffle arr 
+  # arr.sort_by(&:rand)
+  # note the above was how Chris pine did 
+  # do it, but it no longer works, instead do the
+  # following:
+  arr.shuffle
+end
+
+# p(shuffle([1,2,3,4,5,6,7,8,9]))

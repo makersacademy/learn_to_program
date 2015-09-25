870d32e74f7760e15b3c196b3b51263bce023686
jamierose <jrose111284@gmail.com>
2015-09-25 15:33:24 +0100

    extended array

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,26 @@
-class Integer
-  # your code here
+ class Array
+   def shuffle
+     arr = self     
+     shuf = []
+
+     while arr.length > 0
+       
+       rand_index = rand(arr.length)
+
+       curr_index = 0
+       new_arr = []
+       arr.each do |item|
+         if curr_index == rand_index
+           shuf.push item
+         else
+           new_arr.push item
+         end
+
+         curr_index = curr_index + 1
+       end
+       
+       arr = new_arr
+    end
+    shuf
+   end
 end

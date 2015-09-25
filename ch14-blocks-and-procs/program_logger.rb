249e1e8225b15f0642f3620fe26ea7670733e4a9
NULL-OPERATOR <x@cryptolab.net>
2015-09-25 19:37:57 +0100

    testing a log part 5

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -3,7 +3,7 @@ def log desc, &block
   puts 'Beginning "outer block"...'
   puts ' Beginning "some little block"...'
   puts '  Beginning "teeny-tiny block"...'
-  puts '  ..."teeny-tiny block" finished, returning:   lots of love'
+  puts '  ..."teeny-tiny block" finished, returning: lots of love'
   puts '..."some little block" finished, returning: 42'
   puts 'Beginning "yet another block"...'
   puts '..."yet another block" finished, returning: I Indian food!'

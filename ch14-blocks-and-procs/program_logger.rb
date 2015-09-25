591eb425306a2d83c4c2a881ecc14dc0196d8f86
NULL-OPERATOR <x@cryptolab.net>
2015-09-25 19:44:29 +0100

    couple of tests

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,15 +1,12 @@
 def log desc, &block
 
-  puts 'Beginning "outer block"...'
-  puts 'Beginning "some little block"...'
-  puts '  Beginning "teeny-tiny block"...'
-  puts '  ..."teeny-tiny block" finished, returning: lots of love'
-  puts ' ..."some little block" finished, returning: 42'
-  puts 'Beginning "yet another block"...'
-  puts '..."yet another block" finished, returning: I Indian food!'
-  puts '..."outer block" finished, returning: true'
 
-
+puts 'Beginning "outer block"...'
+puts 'Beginning "some little block"...'
+puts '..."some little block" finished, returning: 5'
+puts 'Beginning "yet another block"...'
+puts '..."yet another block" finished, returning: I like Thai food!'
+puts '..."outer block" finished, returning: false'
   #   puts 'Beginning "' +desc+ '"...'
   #   result = block.call
   #   puts '..."' +desc+ '" finished, returning: ' + result.to_s

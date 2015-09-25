670e8b5b0c190d7ca6b89629e9ecf98a87be0a82
“DovileSand” <dovile.sand@protonmail.ch>
2015-09-25 23:53:30 +0100

    Modified shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,29 @@
 def shuffle arr
   # your code here
+  rec_shuffle(arr, [])
 end
+
+
+def rec_shuffle unshuffled, shuffled
+    
+    if  unshuffled.length == 0
+        return shuffled
+    end
+
+    s = []
+    size = unshuffled.length
+    v = rand(size)
+    rv = unshuffled[v]
+
+    unshuffled.each do |f|
+    	if f == rv
+    		shuffled.push f
+    	else
+    		s.push f
+    	end
+    end
+
+    rec_shuffle(s, shuffled)
+end
+
+puts shuffle([1,2,3,4,5])

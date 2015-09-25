c7960606f95cd0ff237377df9cd4e6f4b4c88387
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-25 13:51:49 +0100

    Second shuffle commit

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,9 +1,18 @@
 def shuffle array
   shuffle_array = []
   while array.length > 0
-    picked = array[rand array.length]
-    array.delete(picked)
-    shuffle_array << picked
+    rand_index = rand(array.length)
+    current_index = 0
+    new_arr = []
+    array.each do |element|
+      if current_index == rand_index
+        shuffle_array.push element
+      else
+        new_arr.push element
+      end
+      current_index +=1
+    end
+    array = new_arr
   end
   shuffle_array
 end

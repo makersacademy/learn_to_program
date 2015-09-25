fbaad7880afc7a8ea208b03b391efe035cbef898
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-25 13:57:03 +0100

    Commit build_your_own_playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,27 @@
-# your code here
+def shuffle array
+  shuffle_array = []
+  while array.length > 0
+    rand_index = rand(array.length)
+    current_index = 0
+    new_arr = []
+    array.each do |element|
+      if current_index == rand_index
+        shuffle_array.push element
+      else
+        new_arr.push element
+      end
+      current_index +=1
+    end
+    array = new_arr
+  end
+  shuffle_array
+end
+
+shuffle_music = shuffle(Dir["/Users/Alix/Desktop/Music_2014/*.JPG"])
+play_list = "MyPlayList.m3u"
+
+File.open(play_list, "w") do |f|
+  shuffle_music.each do |song|
+    f.write song + "\n"
+  end
+end

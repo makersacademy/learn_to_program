53bb9cd556e37a99d76be3e5dfa81a95501dfa63
Amy Yang <ayc.yang@gmail.com>
2015-09-25 12:18:23 +0100

    First commit for Dictionary Sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
 def dictionary_sort arr
-  # your code here
+  rec_dict_sort(arr, [])
 end
+
+	def rec_dict_sort(unsorted, sorted)
+		if unsorted.length <= 0 
+		return sorted 
+	end 
+
+	smallest = unsorted.pop 
+	#last element of unsorted arr
+	still_unsorted = []
+
+	unsorted.each do |element|
+		if element.downcase < smallest.downcase 
+			still_unsorted.push smallest
+			smallest = element 
+		else 
+			still_unsorted.push element
+		end
+	end 
+	sorted.push smallest
+
+	rec_dict_sort(still_unsorted, sorted)
+end 
+
+puts (dictionary_sort(["happy", "Bird","ant","singing"]))

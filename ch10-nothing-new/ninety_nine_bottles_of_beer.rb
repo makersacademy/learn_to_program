8c80114b92bf3567a184e451fd9c203fc8f944bc
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-25 20:08:19 +0100

    added code for ninty nine bottles of beer.

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,15 @@
-# your code here
+
+
+
+def bottlesOfBeer number
+
+  require './english_number.rb'
+
+  (1..number).reverse_each do |bottle|
+    puts "#{(english_number (bottle)).capitalize} bottles of beer on the wall, #{english_number bottle} bottles of beer."
+    puts "Take one down and pass it around, #{english_number (bottle-1)} bottles of beer on the wall."
+    puts
+  end
+end
+
+#bottlesOfBeer 10

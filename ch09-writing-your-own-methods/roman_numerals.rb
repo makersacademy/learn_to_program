f97834215ad81a43760be5dc6a2696bab39d5792
yevdyko <evgeny.dyko@gmail.com>
2015-09-25 23:05:53 +0300

    Added ch09-roman_numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,41 @@
 def roman_numeral num
-  # your code here
+  thousands = num / 1000
+  hundreds = num % 1000 / 100
+  tens = num % 100 / 10
+  ones = num % 10
+
+  roman = 'M' * thousands
+
+  if hundreds == 9
+    roman = roman + 'CM'
+  elsif hundreds == 4
+    roman = roman + 'CD'
+  else
+    roman = roman + 'D' * (num % 1000 / 500)
+    roman = roman + 'C' * (num % 500 / 100)
+  end
+
+  if tens == 9
+    roman = roman + 'XC'
+  elsif tens == 4
+    roman = roman + 'XL'
+  else
+    roman = roman + 'L' * (num % 100 / 50)
+    roman = roman + 'X' * (num % 50 / 10)
+  end
+
+  if ones == 9
+    roman = roman + 'IX'
+  elsif ones == 4
+    roman = roman + 'IV'
+  else
+    roman = roman + 'V' * (num % 10 / 5)
+    roman = roman + 'I' * (num % 5 / 1)
+  end
+
+  roman
 end
+
+puts 'Please enter an integer number:'
+number = gets.to_i
+puts(roman_numeral(number))

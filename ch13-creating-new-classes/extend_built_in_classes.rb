ae6f7d6ce007b5dad08207f011ef6e4191ef648f
NULL-OPERATOR <x@cryptolab.net>
2015-09-25 18:26:07 +0100

    extended it

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -3,7 +3,22 @@ class Integer
   def factorial
     self <= 0 ? 1 : self * (self -1).factorial
   end
-  
+
+  def to_roman 
+
+    a = [[1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]
+
+    i = self
+    to_print = ''
+    b = 0
+    while i != 0
+      to_cut = (i - (i % a[b][0]))
+      to_print << a[b][1] * (to_cut / a[b][0])
+      i = i - to_cut
+      b += 1
+    end
+    to_print
+  end
+
 end
 
-puts 5.factorial

15bf4bdacae8daddfd888f488683207c2fcde754
yevdyko <evgeny.dyko@gmail.com>
2015-09-25 22:40:24 +0300

    Added ch09-old_school_roman_numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
 def old_roman_numeral num
-  # your code here
+  old_roman = ''
+
+  old_roman = old_roman + 'M' * (num / 1000)
+  old_roman = old_roman + 'D' * (num % 1000 / 500)
+  old_roman = old_roman + 'C' * (num % 500 / 100)
+  old_roman = old_roman + 'L' * (num % 100 / 50)
+  old_roman = old_roman + 'X' * (num % 50 / 10)
+  old_roman = old_roman + 'V' * (num % 10 / 5)
+  old_roman = old_roman + 'I' * (num % 5 / 1)
+  old_roman
 end
+
+puts 'Please enter an integer number:'
+number = gets.to_i
+puts(old_roman_numeral(number))

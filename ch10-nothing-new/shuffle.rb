06400572c20028d970728c12c1966d90bd865485
Mattia46 <mattia.assogna@gmail.com>
2015-09-25 12:11:35 +0100

    shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,23 @@
 def shuffle arr
   # your code here
+  shuf = []
+
+	while arr.length > 0
+		rand_index = rand(arr.length) #genera index a cazzo
+
+		index = 0
+		new_arr = []
+
+		arr.each do |x|
+			if index == rand_index # quando l'index a cazzo combacia con l'index valutato lo aggiunge al shuffle[]
+				shuf << x
+			else
+				new_arr << x
+			end
+
+			index += 1
+		end
+			arr = new_arr
+	end
+	shuf
 end

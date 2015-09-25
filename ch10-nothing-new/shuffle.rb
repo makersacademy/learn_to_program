ee59c4f6064352b5ce4d7a7d77331870ef73ca1f
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-25 08:22:45 +0100

    commit my shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,9 @@
-def shuffle arr
-  # your code here
+def shuffle array
+  shuffle_array = []
+  while array.length > 0
+    picked = array[rand array.length]
+    array.delete(picked)
+    shuffle_array << picked
+  end
+  shuffle_array
 end

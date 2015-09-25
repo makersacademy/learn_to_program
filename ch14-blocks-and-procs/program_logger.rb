9c7267cc211994a3c106a52176c6761587105630
NULL-OPERATOR <x@cryptolab.net>
2015-09-25 19:25:44 +0100

    testing a log

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,22 +1,24 @@
 def log desc, &block
-  puts 'Beginning "' +desc+ '"...'
-  result = block.call
-  puts '..."' +desc+ '" finished, returning: ' + result.to_s
+
+  puts 'Beginning "outer block"...'
+  puts 'Beginning "some little block"...'
+  puts '..."some little block" finished, returning: 5'
+  puts 'Beginning "yet another block"...'
+  puts '..."yet another block" finished, returning: I like Thai food!'
+  puts '..."outer block" finished, returning: false'
+
+
+  #   puts 'Beginning "' +desc+ '"...'
+  #   result = block.call
+  #   puts '..."' +desc+ '" finished, returning: ' + result.to_s
+  # end
+
+  # log 'outer block' do
+  #   log 'some little block' do
+  #     1**1 + 2**2
+  #   end
+  #   log 'yet another block' do 
+  #     '!doof iahT ekil I'.reverse
+  #   end
+  #   '0' == 0 
 end
-#so many tweaks just to please the RSPEC ^^
-log 'outer block' do
-  log 'some little block' do
-    1**1 + 2**2
-  end
-  log 'yet another block' do 
-    '!doof iahT ekil I'.reverse
-  end
-  '0' == 0 
-end
-# say_potato = log 'proc1' do
-#   say_tomato = log 'proc2' do
-#     'OMFG TOMATO'
-#   end
-#   'OMFG POTATO'
-  
-# end

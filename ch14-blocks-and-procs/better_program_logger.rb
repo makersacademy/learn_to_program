50478a2d4727da529e4f64e86648ccba1ecd4cf2
samover <samuel.overloop@gmail.com>
2015-09-25 19:52:15 +0200

    finished chapter 14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,19 +1,11 @@
-# FIXME: should try to avoid global variables
-$depth = 0
+$indent = 0
 
-def log(desc, &block)
-  puts "#{' ' * $depth}Beginning \"#{desc}\"..."
-  $depth += 1
-  result = block[]
-  $depth -= 1
-  puts "#{' ' * $depth}...\"#{desc}\" finished, returning: #{result}"
+def better_logger(desc, &block)
+  puts "#{' ' * $indent}Beginning \"#{desc}\"..."
+  $indent += 1
+  result = block.call
+  $indent -= 1
+  puts "#{' ' * $indent}...\"#{desc}\" finished, returning: #{result}"
 end
 
-log('outer block') do
-  log('some little block') do
-    log('teeny-tiny block') { 'lots of love' }
-    42
-  end
-  log('yet another block') { 'I love Indian food!' }
-  true
-end
+

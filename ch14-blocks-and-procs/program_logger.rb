5e2ba57a1ab0b6040836d66a972446e35bbd1c40
NULL-OPERATOR <x@cryptolab.net>
2015-09-25 18:01:03 +0100

    dem specs

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -3,11 +3,20 @@ def log desc, &block
   result = block.call
   puts '..."' +desc+ '" finished, returning: ' + result.to_s
 end
-
-say_potato = log 'proc1' do
-  say_tomato = log 'proc2' do
-    'OMFG TOMATO'
+#so many tweaks just to please the RSPEC ^^
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
   end
-  'OMFG POTATO'
-  
+  log 'yet another block' do 
+    '!doof iahT ekil I'.reverse
+  end
+  '0' == 0 
 end
+# say_potato = log 'proc1' do
+#   say_tomato = log 'proc2' do
+#     'OMFG TOMATO'
+#   end
+#   'OMFG POTATO'
+  
+# end

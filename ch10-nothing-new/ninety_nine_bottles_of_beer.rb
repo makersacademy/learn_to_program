2fef2cbfcdce99dc8f9a11813b85e6d03eacc81f
Mattia46 <mattia.assogna@gmail.com>
2015-09-25 16:04:40 +0100

    9999 bottles

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,10 @@
 # your code here
+num = 9999
+
+while num >= 2
+	puts num.to_s + " bottles of beer on the wall, " + num.to_s + ' bottles of beer.'
+	num -= 1
+	puts 'Take one down and pass it around,' + num.to_s + ' bottles of beer on the wall'
+	
+end
+

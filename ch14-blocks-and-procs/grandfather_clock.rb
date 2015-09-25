666804c7bb2442fa5537e11a4545b58059ab9ffe
thisdotrob <thisdotrob@gmail.com>
2015-09-25 07:49:40 +0100

    Tweak grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,5 +1,5 @@
 def grandfather_clock &block
-  hours_passed = Time.now.hour
-  hours_passed -= 12 if hours_passed > 12
+  hours_passed = Time.new.hour
+  hours_passed -= 12 if hours_passed >= 12
   hours_passed.times do block.call end
 end

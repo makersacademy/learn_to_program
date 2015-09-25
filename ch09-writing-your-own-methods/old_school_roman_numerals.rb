00ceff83d1f9156fdcdcbee73b7f2fccaf5273d8
Rebecca <beccollins82@hotmail.com>
2015-09-25 15:48:32 +0100

    Chapter 9 updates

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,14 +1,17 @@
 def old_roman_numeral num
   	
 	answer = ''
-	answer += 'M' * (num/1000)
-	answer += 'D' * (num%1000/500)
-	answer += 'C' * (num%500/100)
-	answer += 'L' * (num%100/50)
-	answer += 'X' * (num%50/5)
-	answer += 'I' * (num%5)
+	answer  += 'M' * (num/1000)
+	answer  += 'D' * (num%1000/500)
+	answer  += 'C' * (num%500/100)
+	answer  += 'L' * (num%100/50)
+	answer  += 'X' * (num%50/10)
+	answer  += 'V' * (num%10/5)
+	answer  += 'I' * (num%5/1)
 
 answer
-puts old_roman_numeral(768)
+
+
 
 end
+puts old_roman_numeral 1019

e5e24e91d2633a8070d0c079a70390ff71ff1c9a
thisdotrob <thisdotrob@gmail.com>
2015-09-25 09:20:24 +0100

    Tweak better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -2,7 +2,8 @@ $nest_depth = 0
 
 def better_log desc, &block
   puts (' ' * $nest_depth) + "Beginning \"#{desc}\"..."
-  returned = block.call
-  puts "...\"#{desc}\" finished, returning: #{returned}"
   $nest_depth += 1
+  returned = block.call
+  $nest_depth -= 1
+  puts (' ' * $nest_depth) + "...\"#{desc}\" finished, returning: #{returned}"  
 end

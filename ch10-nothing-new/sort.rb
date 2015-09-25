1ea40bdc0c28e62144bd26d2a41c033fb4c37482
hasulica <raduasandei@gmail.com>
2015-09-25 14:56:47 +0100

    Added working solutions.

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,18 @@
-def sort arr
-  # your code here
+def sort(arr)
+  recursive_sort arr, []
+end
+
+def recursive_sort(original_array, sorted_array)
+  smallest = original_array[0]
+
+  original_array.each do |word|
+    smallest = word if smallest > word
+  end
+
+  sorted_array << smallest
+  original_array.delete_at original_array.index smallest
+
+  recursive_sort(original_array, sorted_array) unless original_array.empty?
+
+  sorted_array
 end

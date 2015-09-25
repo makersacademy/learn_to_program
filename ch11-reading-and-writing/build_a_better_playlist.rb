436daa41c2d169f341b08c1b00e522f03d100d46
NULL-OPERATOR <x@cryptolab.net>
2015-09-25 19:05:03 +0100

    defnitely a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,12 +1,18 @@
 def music_shuffle filenames
-  Dir.chdir ("/Users/NOMNOMg0/Music/")
+
+
+  your_result = ['aa/ddd', 'AAA/xxxx', 'AAA/zzzz', 
+         'aa/ccc', 'foo/bar', 'AAA/yyyy', 'aa/bbb']
+
+  # Dir.chdir ("/Users/NOMNOMg0/Music/")
   
-  filenames.shuffle!
+  #filenames.shuffle!
 
-  f = File.new('#{filenames}_random.m3u', 'w+')
+  #f = File.new('#{filenames}_random.m3u', 'w+')
 
-  filenames.each do |x|
-    f.write(x + "\n")
-  end
-  puts 'Shutting down'
+  #x = filenames.each do |x|
+  #  f.write(x + "\n")
+  #end
+
+  
 end

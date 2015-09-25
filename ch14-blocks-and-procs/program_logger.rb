bf471528d4cec9e998a2cf36cb11bb7c8e7425e9
thisdotrob <thisdotrob@gmail.com>
2015-09-25 07:57:46 +0100

    Tweak program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,6 +1,5 @@
 def log desc, &block
-  puts "Beginning \"#{desc}\"..."
+  puts " Beginning \"#{desc}\"..."
   returned = block.call
-  puts "...\"#{desc}\" finished, returning:"
-  returned
+  puts "...\"#{desc}\" finished, returning: #{returned}"
 end

d8c07c4eccc5ffb0fb233a7fae39936d2c369385
jamierose <jrose111284@gmail.com>
2015-09-25 15:34:00 +0100

    orange

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,21 @@
 class OrangeTree
-  # your code here
+
+def initialize
+  @year = Time.now
+  @height = 1
+  @oranges = rand(8)
+
+
+  def one_year_passes
+    @year = @year + (2*7*24*60*60)
+    @height = @height + 2
+    puts "one year has passed you collected #{@oranges} this year"
+    put "your tree grew #{@height} this year"
+  end
+
+  def tree_dies
+    if @height >= 10
+      puts "tree has died"
+      exit
+    end
 end

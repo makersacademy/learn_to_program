9e7359baf1fe4f19018e6a7cf2a13ebe6188cf9e
mahmudh <mahmudhssn246@gmail.com>
2015-09-25 15:23:07 +0100

    birthday spanks added

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,26 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp
+puts "What month were you born?"
+month = gets.chomp
+puts "What day were you born?"
+day = gets.chomp
+
+puts "you were born "
+puts birth = Time.local(year, month, day)
+
+puts "today is: "
+puts today = Time.new
+
+puts "Difference is "
+puts difference = today - birth
+
+age = difference / 3600 / 24 / 365
+
+puts "you age is "
+puts age.to_i
+
+i = 1
+while i <= age.to_i
+	puts "SPANK"
+	i += 1
+end

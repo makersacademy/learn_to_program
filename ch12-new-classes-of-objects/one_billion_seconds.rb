130088cc0060a2df132d44b0f8f1128b0ebecaca
Tom Barnes <ruralmyth@gmail.com>
2015-09-25 17:00:21 +0100

    Updated one_million_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,18 @@
-# your code here
+puts 'What year was I born?'
+print '> '
+year = gets.chomp
+
+puts 'What month was I born?'
+print '> '
+month = gets.chomp
+
+puts 'What day was I born?'
+print '> '
+day = gets.chomp
+
+my_birthday = "#{year}, #{month}, #{day}"
+
+puts "I am #{(Time.new - Time.local(my_birthday)).to_i} seconds old!"
+
+one_billion_seconds = Time.local(my_birthday) + 1000000000
+puts "I turned one billion seconds old on #{one_billion_seconds}"

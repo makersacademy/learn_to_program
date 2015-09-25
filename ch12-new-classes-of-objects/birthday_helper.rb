0fcd982c0886893e3cc7042ddc0bca486277490f
Robert Youngman <rob_youngman@yahoo.com>
2015-09-25 18:05:21 +0100

    Not quite finished yet

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,25 @@
-# your code here
+birthdays = {}
+
+File.readlines('birthdays.txt').each do |line|
+  array = line.split(',')
+  name = array[0]
+  date = array[1]
+  year = array[2]
+
+  birthdays[name] =  date.to_s + "," + year.to_s
+end
+
+puts birthdays
+
+puts "Enter name of person? "
+name = gets.chomp
+dob = birthdays[name]
+
+if dob == nil
+  puts "Error, name unknown!"
+else
+  date, year = dob.split(',')
+  year = year.to_i + 1
+
+  puts "Your next brithday will be #{date} #{year}"
+end

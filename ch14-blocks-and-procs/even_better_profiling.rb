276cba44c1b58f40a16258678071bd7173604a5e
thisdotrob <thisdotrob@gmail.com>
2015-09-25 07:40:40 +0100

    Add even_better_profiling.rb solution

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,8 @@
 def profile block_description, &block
-  # your code here
+  if true
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  end
 end

4e1824cfbf30e1af807770c422f3738e928184bb
Chuka Ebi <chuka.ebi@outlook.com>
2015-09-25 20:08:51 +0100

    Updated grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
 def grandfather_clock &block
   # your code here
+  hours_after_twelve = {13 => 1, 14 => 2, 15 => 16, 17 => 5, 18 => 6, 19 => 7,
+  						20 => 8, 21 => 9, 22 => 10, 23 => 11}
+  t = Time.new.hour
+  if t >= 12
+  	hours_after_twelve[t].times do 
+  		block.call
+  	end
+  else
+  	t.times do
+  		block.call
+  	end
+  end
+
 end

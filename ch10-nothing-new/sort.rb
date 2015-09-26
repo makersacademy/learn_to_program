4dfe40e6025ae6ee122de99e9a84c46e8656bc36
yevdyko <evgeny.dyko@gmail.com>
2015-09-26 23:31:11 +0300

    Added ch10-sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,29 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+def recursive_sort unsorted_arr, sorted_arr
+
+  return sorted_arr if unsorted_arr.length == 0
+
+  smallest = unsorted_arr.pop
+  rest_arr = []
+
+  unsorted_arr.each do |object|
+    if object < smallest
+      rest_arr.push smallest
+      smallest = object
+    else
+      rest_arr.push object
+    end
+  end
+
+  sorted_arr.push smallest
+
+  recursive_sort rest_arr, sorted_arr
+
+end
+
+puts(sort(['you', 'do', 'not', 'exist', 'anymore']))
+
+

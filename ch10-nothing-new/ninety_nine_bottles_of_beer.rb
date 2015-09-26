7b12e44970d5c7efa742556e12d0092463e3c0c4
yevdyko <evgeny.dyko@gmail.com>
2015-09-26 19:58:10 +0300

    Added ch10-ninety_nine_bottles_of_beer

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,14 @@
-# your code here
+require './ch10-nothing-new/english_number.rb'
+
+bottles = 5
+num_bottles = bottles
+
+while bottles > 1
+  puts english_number(num_bottles).capitalize + " bottles of beer on the wall, " + english_number(num_bottles) + " bottles of beer!"
+  puts "Take one down and pass it around, " + english_number(num_bottles-1) + " bottles of beer on the wall."
+  puts ""
+  bottles = bottles - 1
+end
+
+puts "One bottle of beer on the wall, one bottle of beer!"
+puts "Take one down and pass it around, no more bottle of beer on the wall!"

86a8f5bdb0f2cbfa813a673a43720feab873dca3
Amy Yang <ayc.yang@gmail.com>
2015-09-27 00:58:02 +0100

    First commit to build a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,37 @@
 def music_shuffle filenames
-  # your code here
+ 
+  filenames = filenames.sort
+  len = filenames.length
+
+  # shuffle twice.
+  2.times do
+    l_idx = 0 # index of card in left pile
+    r_idx = len/2 # index of card in right pile
+    shuf = []
+
+    while shuf.length < len
+      if shuf.length%2 == 0
+        # card from right pile
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        # card from left pile
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+
+    filenames = shuf
+  end
+
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+  
+  arr
 end

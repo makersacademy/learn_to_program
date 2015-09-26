6d3e7f8a5e612b5373d5b6c10da004605fd0816c
samover <samuel.overloop@gmail.com>
2015-09-26 19:20:04 +0200

    updated

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,10 +1,8 @@
 def grandfather_clock(&block)
   ## rspec test insists on using the 'hour' method, so my solution = binned
-  # hour = Time.new.strftime('%I').to_i
+  #hour = Time.new.strftime('%I').to_i
   hour = Time.new.hour > 12 ? Time.new.hour - 12 : Time.new.hour
   hour.times(&block)
 end
 
 grandfather_clock { puts 'DONG!' }
-
-  

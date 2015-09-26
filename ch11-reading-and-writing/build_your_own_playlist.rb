ecc513ef7990c2935aeac6fd05a13669633e6d2c
mahmudh <mahmudhssn246@gmail.com>
2015-09-26 14:16:46 +0100

    playlist added

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,12 @@
-# your code here
+Dir.chdir('/Users/mahmudhussain/documents/makers_mp3/playlist')
+
+# First we find all of the music to be moved.
+music_list = Dir['/Users/mahmudhussain/documents/makers_mp3/*.mp3']
+# puts music_list
+
+file_name = File.new("playlist.m3u",  "w+")
+
+music_list.each do |music_file|
+	# puts music_file
+	file_name.write music_file + "\n"
+end

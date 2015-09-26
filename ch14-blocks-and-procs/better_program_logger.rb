76e2b0baaf37c1926ce3859612e850dc66e2bf31
fergus lemon <fergus.lemon@gmail.com>
2015-09-26 12:34:07 +0100

    update better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,26 @@
+$logger_depth = 0
 def log desc, &block
-  # your code here
+  prefix = ' '*$logger_depth
+  puts prefix + 'Beginning "' + desc + '"...'
+  $logger_depth = $logger_depth + 1
+  result = block.call
+  
+  $logger_depth = $logger_depth - 1
+  puts prefix + '..."' + desc + '"finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+	log 'a little block' do
+		log 'a tiny block' do
+			'LoVE IT'.downcase
+		end
+		
+	20 * 10 * 5
+end
+
+log 'yet another block' do
+	'!em evol attog uoY'.reverse
+end
+
+'0' == "0"
 end

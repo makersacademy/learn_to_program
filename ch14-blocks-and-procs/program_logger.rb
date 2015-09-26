f32dcf3b84619a1f4029d23a48f12b2cd16847c5
alaanzr <alaanzr@gmail.com>
2015-09-26 22:46:07 +0100

    Modified logging programs

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,16 +1,16 @@
-def log desc, &block
+def log1 desc, &block
 puts 'Beginning "' + desc + '"...'
 result = block.call.to_s
 puts '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
 
-log "outer block" do
-	log "some little block" do
+log1 'outer block' do
+	log1 'some little block' do
    		5
    	end
-   	log "yet another block" do
-   		"I like Thai food"
+   	log1 'yet another block' do
+   		'I like Thai food'
    	end
    	false
 end 

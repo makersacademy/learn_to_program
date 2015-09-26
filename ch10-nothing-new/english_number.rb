d0bebecb8d13c27f3e7f16a8cc8fe2cab6a1a07c
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-26 14:58:20 +0100

    updated eng_num still has bug

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -15,28 +15,31 @@ def english_number (number)
   num_array = []
   t = number.to_s.length # 6792001 = 7
 
+left = number #6792001
   while t > 0 #7
-    num_array = num_array << (number / 10**(t-1)) #  6792001/(10^6) = 6  /  num_array = [] << [6] << [7] << ... << [1]  /  [6,7,9,2,0,0,1]
+    wright = left / 10**(t-1) # 6 792 001/1 000 000 = 6
+    left = left - wright*10**(t-1) # 6 792 001 - 1 000 000 = 792 001
+    num_array = num_array << wright #  
     t = t-1
   end
-  
+  puts num_array
   num_array.reverse.each_with_index { |value , index| # [1,0,0,2,9,7,6]
-    if index % 3 == 0 && num_array[index] != 0                                      
-      num_string = num_string << ones_place[value-1]                                # "one"
-    elsif index % 3 == 0 && value > 0 && num_array[index] != 0                      
-      num_string = num_string << other_place[index/3] << ones_place[value-1]        # "thousand" "two"              # "million" "six"
-    elsif index % 3 == 1
-      num_string = num_string << tens_place[value-1]                                # "ninety"
-    elsif index & 3 == 2
-      num_string = num_string << other_place[0] << ones_place[value-1]              # "hundred" "seven"
+    if value == 0 && index != 0 && num_array[index -1] != 0
+      num_string = num_string.insert(t,  "and ")                                              # "and"  
+    elsif index % 3 == 0 && value > 0                                     
+      num_string = num_string.insert(t, ones_place[value-1] + " ")                               # "one"
+    elsif index % 3 == 0 && value > 0                      
+      num_string = num_string.insert(t, ones_place[value-1] + " " + other_place[index/3] + " ")        # "thousand" "two"              # "million" "six"
+    elsif index % 3 == 1 && value > 0
+      num_string = num_string.insert(t, tens_place[value-1] + " ")                               # "ninety"
+    elsif index & 3 == 2 && value > 0
+      num_string = num_string.insert(t, ones_place[value-1] + " " + other_place[0] + " ")              # "hundred" "seven"
     end
   }                                                                                 # num_string = "one thousand two ninety hundred seven million six"
-  print num_string.reverse                                                          # num_string = "six million seven hundred ninety two thousand one"
+  num_string                                                          # num_string = "six million seven hundred ninety two thousand and one"
 end
+puts english_number (6792001)
 
-
-
-english_number (6792001)
 
 
 

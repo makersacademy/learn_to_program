9f99b26bb4e130424ce83ba30e6755eeca64809d
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-26 20:49:35 +0100

    trying if rspec on CI will fail

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,25 +1,11 @@
-
-$level=0
+$level = 0
 
 def log desc, &block
-  # your code here
   indent = " "
-  puts "#{indent*$level}Beginning \"#{desc}\"..."
-  $level +=1
-  retval=block.call
-  $level -=1
-  puts "#{indent*$level}...\"#{desc}\" finished, returning: #{retval}"
+  puts indent*$level+"Beginning #{desc.inspect}..."
+  $level += 1
+  retval = block[]
+  $level -= 1
+  puts indent*$level+"...#{desc.inspect} finished, returning: #{retval}"
 end
 
-#log "outer block" do
-#	log "some little block" do
-#    log "teeny-tiny block" do
-#      "lots of love"
-#    end
-#    "42"
-#	end
-#  log "yet another block" do
-#	   "I love Indian food"
-#  end
-#	"true"
-#end

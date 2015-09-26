7d26474dd7b1a335ca44a154077587001e46c717
Mateja <mateja683@gmail.com>
2015-09-26 14:05:53 +0100

    playlist and pictures

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,3 @@
-# your code here
+Dir.chdir "#{ENV["HOME"]}/Music/playlist"
+file_list = Dir["#{ENV["HOME"]}/Music/*.mp3"] 
+File.write("#{ENV["HOME"]}/Music/playlist/playlist.m3u", file_list.to_s)
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,23 @@
-# your code here
+Dir.chdir "#{ENV['HOME']}/Picture"
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files:  "
+pic_number = 1
+pic_names.each do |name|
+print '.' # This is our "progress bar".
+new_name = if pic_number < 10 "#{batch_name}0#{pic_number}.jpg"
+else
+"#{batch_name}#{pic_number}.jpg" end
+
+File.exist?("#{new_name}.jpg") 
+	if true
+		File.rename ("#{new_name_version_2}.jpg")
+	else
+		File.rename name, new_name
+	end
+end
+pic_number = pic_number + 1
+puts
+puts 'Done'

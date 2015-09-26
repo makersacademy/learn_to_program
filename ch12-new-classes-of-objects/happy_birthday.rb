16cb1aae180d47c33c01af43ddcf9e8af8d33499
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-26 08:41:08 +0100

    Commit happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,24 @@
-# your code here
+puts "What is your birth year?"
+birth_year = gets.chomp.to_i
+
+puts "What is your birth month?"
+birth_month = gets.chomp.to_i
+
+puts "What is your birth day?"
+birth_day = gets.chomp.to_i
+
+time_now = Time.new
+
+age = 0
+
+while Time.local(birth_year+age,birth_month,birth_day) <= time_now
+age += 1
+end
+
+if Time.local(birth_year+age,birth_month,birth_day) > time_now
+age -= 1
+end
+
+puts age
+
+age > 0? age.times {puts "SPANK!"} : "-"

caccb837adb32a0dfa350fad9fd5a44f6c078920
Hamza Sheikh <hsheikhm@gmail.com>
2015-09-26 11:40:13 +0100

    Trying to make orange_tree.rb pass CI test

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,49 +1,56 @@
 class OrangeTree
-  
   def initialize
-  	@height = 0
-  	@orange_count = 0
-  	@alive = true
-  	puts "A tree is born! Its current height is #{@height}m and it has #{@orange_count} oranges."
+    @height = 0
+    @orange_count = 0
+    @alive = true
   end
 
   def height
-  	@alive == true ? @height.round(1) : "A dead tree is not very tall. :("
+    if @alive
+      @height.round(1)
+    else
+      'A dead tree is not very tall. :('
+    end
   end
 
   def count_the_oranges
-    @alive == true ? @orange_count : "A dead tree has no oranges. :("
+    if @alive
+      @orange_count
+    else
+      'A dead tree has no oranges. :('
+    end
   end
 
   def one_year_passes
-  	if @alive
-  		@height += 0.4
-  		@orange_count = 0
-
-  		if @height > 10 && rand(2) > 0
-  			@alive = false
-  			"Oh, no! The tree is too old, and has died. :("
-  		elsif @height > 2
-  			@orange_count = (@height * 15 - 25).to_i
-  			return "This year your tree grew to #{(@height).round(1)}m tall, and produced #{@orange_count} oranges."
-  		else
-  			"This year your tree grew to #{@height}m tall but is still too young to bear fruit."
-  		end
-  	else
-  		"A year later, the tree is still dead. :("
-  	end	
+    if @alive
+      @height = @height + 0.4
+      @orange_count = 0
+      if @height > 10 && rand(2) > 0
+        @alive = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > 2
+        @orange_count = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " and produced #{@orange_count} oranges."
+      else
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " but is still too young to bear fruit."
+      end
+    else
+      'A year later, the tree is still dead. :('
+    end
   end
 
   def pick_an_orange
-  	if @alive
-  		if @orange_count > 0
-  			@orange_count -= 1
-  			"Mmm the orange was #{rand(100)}% juicy!"
-  		else
-  			"Currently no oranges to pick this year I'm afraid!"
-  		end
-  	else
-  		"A dead tree has nothing to pick. :("
-  	end
+    if @alive
+      if @orange_count > 0
+        @orange_count = @orange_count - 1
+        'You pick a juicy, delicious orange!'
+      else
+        'You search every branch, but find no oranges.'
+      end
+    else
+      'A dead tree has nothing to pick. :('
+    end
   end
 end

c8e4f88dfa06cf596e51f26e20fb073239bb19f6
jbhdeconinck <jbhdeconinck.gmail.com>
2015-09-26 16:47:35 +0100

    commit, extend built in class

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,29 @@
+class Array
+  def shuffle
+    self.sort_by{rand}
+  end
+end
+
 class Integer
-  # your code here
+  def factorial
+    raise "Must not use negative integer" if self < 0
+    if self <= 1
+      1
+    else
+       self * (self-1).factorial
+    end
+  end
+
+  def to_roman
+
+    raise "Must use positive integer" if self >= 0
+
+    rom = "M" * (self / 1000)
+    rom = rom + "D" * (self % 1000 / 500)
+    rom = rom + "C" * (self % 500 /100)
+    rom = rom + "L" * (self % 100 /50)
+    rom = rom + "X" * (self % 50 /10)
+    rom = rom + "I" * (self % 10)
+    rom
+  end
 end

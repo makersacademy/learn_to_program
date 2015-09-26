1e122c5e093979f5318366fd1ce86ea9a7b55f24
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-26 12:35:36 +0100

    added english_num code issues

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,42 @@
-def english_number number
-  # your code here
+def english_number (number)
+  if number < 0
+  	return "Please enter a number that isn't negative"
+  end
+  if number == 0
+  	return "Zero"
+  end
+
+  ones_place = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  tens_place = ["ten", "twenty", "thirty", "fory", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  teens_place = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  other_place = ["hundred", "thousand", "million", "billion", "trillion", "quadrillion" ]
+
+  num_string = ""
+  num_array = []
+  t = number.to_s.length # 6792001 = 7
+
+  while t > 0 #7
+    num_array = num_array << (number / 10**(t-1)) #  6792001/(10^6) = 6  /  num_array = [] << [6] << [7] << ... << [1]  /  [6,7,9,2,0,0,1]
+    t = t-1
+  end
+  
+  num_array.reverse.each_with_index { |value , index|
+    if index % 3 == 0 && num_array[index] != 0 
+      num_string = num_string << ones_place[value-1]
+    elsif index % 3 == 0 && value > 0 && num_array[index] != 0
+      num_string = num_string << other_place[index/3] << ones_place[value-1]
+    elsif index % 3 == 1
+      num_string = num_string << tens_place[value-1]
+    elsif index & 3 == 2
+      num_string = num_string << other_place[0] << ones_place[value-1]
+    end
+  }
+  print num_string.reverse
 end
+
+
+
+english_number (6792001)
+
+
+

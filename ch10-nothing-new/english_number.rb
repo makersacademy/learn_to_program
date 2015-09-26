f12432cdab200dd0d7dc4468a4481594566d5172
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-26 15:48:16 +0100

    eng_num correct, but not with teens

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -23,23 +23,28 @@ left = number #6792001
     t = t-1
   end
   puts num_array
-  num_array.reverse.each_with_index { |value , index| # [1,0,0,2,9,7,6]
-    if value == 0 && index != 0 && num_array[index -1] != 0
-      num_string = num_string.insert(t,  "and ")                                              # "and"  
-    elsif index % 3 == 0 && value > 0                                     
-      num_string = num_string.insert(t, ones_place[value-1] + " ")                               # "one"
-    elsif index % 3 == 0 && value > 0                      
+  num_arr_rev = num_array.reverse
+  num_arr_rev.each_with_index { |value , index| # [6,7,9,2,0,0,1]                                           
+    if index % 3 == 0 && value > 0 && index != 0                       
       num_string = num_string.insert(t, ones_place[value-1] + " " + other_place[index/3] + " ")        # "thousand" "two"              # "million" "six"
+    elsif index % 3 == 0 && value > 0 && index == 0  
+      num_string = num_string.insert(t, ones_place[value-1])
     elsif index % 3 == 1 && value > 0
-      num_string = num_string.insert(t, tens_place[value-1] + " ")                               # "ninety"
-    elsif index & 3 == 2 && value > 0
-      num_string = num_string.insert(t, ones_place[value-1] + " " + other_place[0] + " ")              # "hundred" "seven"
+       if value == 1 
+         num_string = num_string.insert(t, teens_place[num_arr_rev[index-1]-1] + " ")
+         next
+      else
+        num_string = num_string.insert(t, tens_place[value-1] + " ")     
+       end
+    elsif index % 3 == 2 && value > 0
+      num_string = num_string.insert(t, ones_place[value-1] + " " + other_place[0] +  " ")              # "hundred" "seven"
     end
-  }                                                                                 # num_string = "one thousand two ninety hundred seven million six"
-  num_string                                                          # num_string = "six million seven hundred ninety two thousand and one"
+  }  
+  num_string                                                        # num_string = "six million seven hundred ninety two thousand and one"
 end
-puts english_number (6792001)
+puts english_number (6792013)
 
 
 
 
+5

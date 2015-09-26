f4cca1054ed91d7451c41276af72376e913f85bf
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-26 19:20:38 +0100

    no luck with build_a_better. rspec still fails

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,17 +1,16 @@
 def music_shuffle filenames
   # your code here
 
+  def shuffle ary
+    ary.sort_by{rand}
+  end
+
   filenames = filenames.sort
   mid = rand(filenames.length)
   
   shuf1 = filenames[0..mid]
   shuf2 = filenames[(mid+1)..filenames.length]
 
-  shuf1.sort!{rand} + shuf2.sort!{rand}
-
-
-  #filenames.sort.shuffle
-
-
+  shuffle(shuf1)+shuffle(shuf2)
 
 end

8a23887e04bdb936231b3c66906049093313c244
Amy Yang <ayc.yang@gmail.com>
2015-09-26 23:52:22 +0100

    3rd commit to English number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -14,8 +14,29 @@ def english_number number
   tenth_num = ["ten", "twenty","thirty", "fourty", "fifty", "sixty", "seventy", "eighty","ninety"]
 
   teen_num = [ "eleven", "twelve", "thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
-
-  zillions = [ ['hundred', 2],['thousand', 3], ['million',6], ['billion', 9], ['trillion',12],['quindecillion',48]]
+  zillion =[['hundred', 2],
+            ['thousand', 3],
+            ['million', 6],
+            ['billion', 9],
+            ['trillion', 12],
+            ['quadrillion', 15],
+            ['quintillion', 18],
+            ['sextillion', 21],
+            ['septillion', 24],
+            ['octillion', 27],
+            ['nonillion', 30],
+            ['decillion', 33],
+            ['undecillion', 36],
+            ['duodecillion', 39],
+            ['tredecillion', 42],
+            ['quattuordecillion', 45],
+            ['quindecillion', 48],
+            ['sexdecillion', 51],
+            ['septendecillion', 54],
+            ['octodecillion', 57]
+            ['novemdecillion', 60],
+            ['vigintillion', 63],
+            ['googol', 100]]
 
   left = number #number left to write
 

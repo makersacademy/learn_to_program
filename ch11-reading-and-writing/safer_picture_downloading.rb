0b1be3f164180e295ffaac971e5bce6d79d396ef
Mattia46 <mattia.assogna@gmail.com>
2015-09-26 12:32:51 +0100

    safer_pict

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,35 @@
-# your code here
+Dir.chdir('/Users/mattia/Desktop/txt_files')
+puts Dir.pwd
+
+foto = Dir["/Users/mattia/Desktop/*.txt"]
+
+
+puts "come vuoi chiamarla?"
+nome_foto = gets.chomp
+
+
+puts "downloading #{foto.length} files: "
+counter = 1
+
+
+	foto.each do |name|
+		print "."
+
+		new_name = if counter < 10
+			"#{nome_foto}0#{counter}.txt"
+				else
+			"#{nome_foto}#{counter}.txt"
+			end 
+
+		if File.exists? new_name
+			puts "already exist, type ew "
+			exit
+		else	
+			File.rename name, new_name
+		
+			puts "File esistente"
+			end
+			counter += 1
+
+ 
+	end

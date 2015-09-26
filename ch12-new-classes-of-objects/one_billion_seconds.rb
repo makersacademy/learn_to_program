290d977b909883270029035f46546dfa540515d2
Mattia46 <mattia.assogna@gmail.com>
2015-09-26 17:16:55 +0100

    one_billion

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,13 @@
+mattia = Time.gm(1984, 11, 5)
+
+now = Time.now
+
+output = now - mattia
+puts output
+
+num = (60*60*24*365) + (60*60*6)
+puts num
+
+diff_2 = output / num.to_f
+puts diff_2
 # your code here

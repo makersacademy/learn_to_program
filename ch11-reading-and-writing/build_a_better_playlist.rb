bc44d8940ef67d1ab1aee26d9852cedb79efa2aa
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-09-26 23:34:54 +0100

    some changes to build a better playlist. Finished all exercises now

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,50 +1,39 @@
 def music_shuffle filenames
-  # We don't want a perfectly random shuffle, so let's
-  # instead do a shuffle like card-shuffling. Let's
-  # shuffle the "deck" twice, then cut it once. That's
-  # not enough times to make a perfect shuffle, but it
-  # does mix things up a bit.
-  # Before we do anything, let's actually *sort* the
-  # input, since we don't know how shuffled it might
-  # already be, and we don't want it to be *too* random.
+
   filenames = filenames.sort
-  len = filenames.length
 
-  # Now we shuffle twice.
   2.times do
-    l_idx = 0 # index of next card in left pile
-    r_idx = len/2 # index of next card in right pile
+
     shuf = []
-    # NOTE: If we have an odd number of "cards",
-    # then the right pile will be larger.
 
-    while shuf.length < len
-      if shuf.length%2 == 0
-        # take card from right pile
-        shuf.push(filenames[r_idx])
-        r_idx = r_idx + 1
+    mid = filenames.length / 2
+    shuf1 = filenames[0..mid-1]
+    shuf2 = filenames[mid..filenames.length]
+    
+    flg = 0
+    while shuf.length < filenames.length
+      if flg == 0
+        flg = 1
+        x=shuf2.shift
       else
-        # take card from left pile
-        shuf.push(filenames[l_idx])
-        l_idx = l_idx + 1
+        flg = 0
+        x = shuf1.shift
       end
+      shuf << x if x != nil
     end
-
     filenames = shuf
   end
-  # And cut the deck.
-  arr = []
-  cut = rand(len) # index of card to cut at
-  idx = 0
 
-  while idx < len
-    arr.push(filenames[(idx+cut)%len])
-    idx = idx + 1
+  ary = []
+  div = rand(filenames.length)
+  i = 0
+
+  while i < filenames.length
+    ary.push(filenames[(i+div)%filenames.length])
+    i += 1
   end
-  
-  arr
+  ary
 end
-# songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
-#          'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
-# puts(music_shuffle(songs))
-
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+          'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+puts(music_shuffle(songs))

f396a95bde365419bc0f981bbfec92f402ea3d55
blancopado <pablo.dominguezblanco@gmail.com>
2015-09-26 19:00:47 +0200

    sort with min-push-delete

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,28 +1,26 @@
-def sort arr
-	rec_sort arr, []
-end
-def rec_sort unsorted, sorted
-	if unsorted.length <= 0
-		return sorted
-	end
-	# So if we got here, then it means we still
-	# have work to do.
-	smallest = unsorted.pop
-	still_unsorted = []
-	
-	unsorted.each do |tested_object|
-		if tested_object < smallest
-			still_unsorted.push smallest
-			smallest = tested_object
-		else
-			still_unsorted.push tested_object
-		end
-	end
+# well, aside from just using the built-in sort method:
 
-sorted.push smallest
-rec_sort still_unsorted, sorted
+# The well-known quicksort algorithm.
+def sort arr
+  
+  
+  length = arr.length
+  arr2 =  []
+  count = 0
+  while count < length
+  	return arr if length <= 1
+  	x = arr.min
+  	
+  	y = arr.find_index(x)
+
+  	arr2.push(x)
+  	
+  	arr.delete_at(y)
+  	count += 1
+  end
+  
+  arr2
+
 end
-puts(sort(['can','feel','singing','like','a','can']))
-# Now "smallest" really does point to the
-# smallest element that "unsorted" contained,
-# and all the rest of it is in "still_unsorted".
+
+puts sort(['can','feel','singing','like','a','can'])

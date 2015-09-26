579d489c584a7e3e4db756caf510e9d8fa7dbae8
mahmudh <mahmudhssn246@gmail.com>
2015-09-26 14:56:35 +0100

    shuffled playlist added

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -6,7 +6,19 @@ music_list = Dir['/Users/mahmudhussain/documents/makers_mp3/*.mp3']
 
 file_name = File.new("playlist.m3u",  "w+")
 
-music_list.each do |music_file|
-	# puts music_file
-	file_name.write music_file + "\n"
+while music_list.length != 0
+	a = rand(music_list.length)
+	#shuffle = music_list.sample(a)
+	file_name.write music_list[a] + "\n"
+	music_list.delete_at(a)
 end
+
+# music_list.sample do |music_file|
+# 	# puts music_file
+# 	file_name.write music_file + "\n"
+# end
+
+# music_list.each do |music_file|
+# 	# puts music_file
+# 	file_name.write music_file + "\n"
+# end

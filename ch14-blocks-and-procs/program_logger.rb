7da76950d293e50c7b9e5ce14c2993b43777f564
Chuka Ebi <chuka.ebi@outlook.com>
2015-09-26 14:32:13 +0100

    Updated program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,6 +1,6 @@
 def log1 desc, &block
   # your code here
-  puts "Beginning #{desc} ... " 
-  block.call
-  puts "...#{desc} finished, returning #{block.call}"
+  puts 'Beginning "' + desc + '"...'    
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
 end

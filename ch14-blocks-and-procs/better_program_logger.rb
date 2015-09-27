d2358bfb0d71fea6459de27f8cd79cafd7c25566
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-27 21:56:46 +0100

    better_program_logger.rb complete

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,23 @@
+$indent = 1
+
 def log desc, &block
-  # your code here
+ prefix = ' '*$indent
+ $indent += 1
+ puts prefix + "Beginning #{desc.inspect}..."
+  result = block.call
+  $indent -= 1
+  puts prefix + "...#{desc.inspect} finished, returning: #{result.to_s}"
+end
+
+log 'outer block' do
+  log 'some little block' do
+  	log 'teeny-tiny block' do
+  		"lots of love"
+  	end
+  	 7 * 3 * 2
+  end
+  log 'yet another block' do
+    "I love Indian food!"
+  end
+  '0' != 0
 end

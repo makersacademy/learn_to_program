13e7e6ca07cdf8773222b97b7c2f8131068e61ed
Ben Rome <benjrome@gmail.com>
2015-09-27 15:32:05 +0100

    Updated better prgm logger again

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,16 +1,11 @@
-def log desc, &block
-  # your code here
-  puts "Beginning \"#{desc}\"..."
-  retval=block.call
-  puts "...\"#{desc}\" finished, returning: #{retval}"
-end
+$level=0
 
-log "outer block" do
-	log "some little block" do
-		"5"
-	end
-    log "yet another block" do
-	    "I like thai food"
-    end
-	"false"
+def log1 desc, &block
+  # your code here
+  indent = " "
+  puts "#{indent*$level}Beginning \"#{desc}\"..."
+  $level +=1
+  retval=block.call
+  $level -=1
+  puts "#{indent*$level}...\"#{desc}\" finished, returning: #{retval}"
 end

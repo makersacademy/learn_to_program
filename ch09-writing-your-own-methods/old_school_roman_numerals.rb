884f455df8d6aff4e312b51f94635456362a5c52
jamierose <jrose111284@gmail.com>
2015-09-27 11:28:46 +0100

    old roman

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,4 @@
+=begin
 def old_roman_numeral num
   numbers = [1000, 500, 100, 50, 10, 5, 1]
   numerals = ["M", "D", "C", "L", "X", "V", "I"]
@@ -5,7 +6,7 @@ def old_roman_numeral num
   j=0
 
   while j <= 6
-  	while num >= numbers[j] 
+  	if num >= numbers[j]
   		num = num - numbers[j]
   		roman_num = roman_num + numerals[j]
   	end
@@ -15,3 +16,19 @@ def old_roman_numeral num
 end
 puts "enter a number!"
 puts old_roman_numeral(gets.chomp.to_i)
+=end
+def old_roman_numeral num
+  raise 'Must use positive integer' if num <= 0
+
+  roman = ''
+
+  roman << 'M' * (num / 1000)
+  roman << 'D' * (num % 1000 / 500)
+  roman << 'C' * (num % 500 / 100)
+  roman << 'L' * (num % 100 / 50)
+  roman << 'X' * (num % 50 / 10)
+  roman << 'V' * (num % 10 / 5)
+  roman << 'I' * (num % 5 / 1)
+
+  roman
+end

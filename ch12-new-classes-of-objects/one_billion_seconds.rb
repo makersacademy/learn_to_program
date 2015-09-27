654ee6c1fb99287e271a39d6287e5a1520c32d7f
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-27 08:42:00 +0100

    Added interactive whats is my birthday in seconds - in words and numbers.

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,18 @@
-# your code here
+def countSeconds date
+  require 'time'
+
+  timeInSecsonds = (Time.now - date).to_i
+end
+
+
+require '../ch10-nothing-new/english_number.rb'
+require 'time'
+puts 'So blood when wer you born?'
+puts 'Use this format for date YYYY-MM-DD.'
+birthDate = gets.chomp
+timeObj = Time.parse(birthDate)
+birthdayInSecs = countSeconds(timeObj)
+birthinSecsWords = english_number(birthdayInSecs)
+
+puts "You were born #{birthdayInSecs} seconds ago!"
+puts "In words, #{birthinSecsWords} seconds ago but you don't look a day over 21!"

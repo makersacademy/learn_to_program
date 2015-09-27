279e59774e0e3e58b533c0d1e280cfa588068d44
Ben Rome <benjrome@gmail.com>
2015-09-27 15:25:18 +0100

    Improved program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,20 +1,5 @@
-def log desc, &block
-  puts "Beginning #{desc.inspect}..."
-
-  result = block[]
-
-  puts "...#{desc.inspect} finished, returning: #{result}"
-
-  end
-
-  log 'outer block' do
-    log 'some little block' do
-    	1**1 + 2**2
-    end
-    log 'yet another block' do
-    	'!doof iahT ekil I'.reverse
-    end
-
-'0' == 0
-
-end
+def log_this desc, &block
+  puts "Beginning \"#{desc}\"..."
+  returned = block.call
+  puts "...\"#{desc}\" finished, returning: #{returned}"
+ end

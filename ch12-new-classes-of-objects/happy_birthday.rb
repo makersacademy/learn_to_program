63c01f6c68f441193fe18fb0247d67610122cf63
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-27 11:14:35 +0100

    Get a spank for every year of birthday.

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,27 @@
-# your code here
+def calcAge(yearBorn,monthBorn,dayBorn)
+  yourAge = (((Time.new - Time.new(yearBorn,monthBorn,dayBorn)).to_i)/(60*60*24*365.25)).to_i
+end
+
+def giveSpanks(yourAge)
+  yourAge.times do |k|
+    puts "******SPANK********"
+  end
+end
+
+
+
+
+
+puts "I know it's rube but I am going to tll you how old you are."
+puts 'What year were you born it?'
+yearBorn = gets.chomp
+puts 'Now tell me the month as a number e.g. March would be 3'
+monthBorn = gets.chomp
+puts 'OK, now the day of the month as a number.'
+dayBorn = gets.chomp
+yourAge = calcAge(yearBorn,monthBorn,dayBorn)
+giveSpanks(yourAge)
+puts "You are #{yourAge} years old."
+
+
+ # calcAge(1963,3,6)
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,7 +1,11 @@
 def countSeconds date
   require 'time'
-
   timeInSecsonds = (Time.now - date).to_i
+end
+
+def secondBorn date
+  require 'time'
+  timeInSecsonds = date.to_i
 end
 
 
@@ -11,8 +15,14 @@ puts 'So blood when wer you born?'
 puts 'Use this format for date YYYY-MM-DD.'
 birthDate = gets.chomp
 timeObj = Time.parse(birthDate)
+
 birthdayInSecs = countSeconds(timeObj)
 birthinSecsWords = english_number(birthdayInSecs)
+secondThatBorn = secondBorn timeObj
+
+DateOfBillion = Time.at(1000000000 + secondThatBorn).strftime('%F')
 
 puts "You were born #{birthdayInSecs} seconds ago!"
 puts "In words, #{birthinSecsWords} seconds ago but you don't look a day over 21!"
+puts "This is #{secondThatBorn}  seconds from 1970!"
+puts "You will or were a billion seconds old on #{DateOfBillion}"

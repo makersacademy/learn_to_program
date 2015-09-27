9b312a1c7423b2472795418b9463558aca675f28
Tom Barnes <ruralmyth@gmail.com>
2015-09-27 21:08:00 +0100

    Updated better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,14 @@
+$nested_depth = 0
+
 def log desc, &block
-  # your code here
+  spacer = '  '
+  $nested_depth.times do print spacer
+  end
+  puts "Beginning \"" + desc + "\"..."
+  $nested_depth = $nested_depth + 1
+  result = block.call
+  $nested_depth = $nested_depth -1
+  $nested_depth.times do print spacer
+  end
+  puts "...\"" + desc + "\" finished, returning: " + result.to_s
 end

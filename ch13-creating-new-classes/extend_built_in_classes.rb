96c2842b25aa55d5825e92b8692b17724f2f743b
blancopado <pablo.dominguezblanco@gmail.com>
2015-09-27 15:21:37 +0200

    extend built in classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,5 @@
+#my own version
+
 =begin
 class Array
 	def shuffle
@@ -32,13 +34,13 @@ class Integer
     # I chose old-school roman numerals just to save space.
     roman = ''
 
-    roman = roman + 'M' * (self / 1000)
-    roman = roman + 'D' * (self % 1000 / 500)
-    roman = roman + 'C' * (self % 500 / 100)
-    roman = roman + 'L' * (self % 100 / 50)
-    roman = roman + 'X' * (self % 50 / 10)
-    roman = roman + 'V' * (self % 10 / 5)
-    roman = roman + 'I' * (self % 5 / 1)
+    roman += 'M' * (self / 1000)
+    roman += 'D' * (self % 1000 / 500)
+    roman += 'C' * (self % 500 / 100)
+    roman += 'L' * (self % 100 / 50)
+    roman += 'X' * (self % 50 / 10)
+    roman += 'V' * (self % 10 / 5)
+    roman += 'I' * (self % 5 / 1)
 
     roman
   end

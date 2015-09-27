60caa9e18c2b1562d7845cca46fb5b16eac9d0c5
Amy Yang <ayc.yang@gmail.com>
2015-09-27 12:33:53 +0100

    4th commit english number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,95 +1,98 @@
 def english_number number
   if number < 0 
-  	return  "Please enter a number that isn\'t negative"
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
   end
 
-  if number == 0 
-  	return "zero" 
-  end
+  num_string = '' 
 
-  num_string = ""
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
 
-  ones_num = ["one", "two","three","four","five","six","seven","eight","nine"]
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
 
-  tenth_num = ["ten", "twenty","thirty", "fourty", "fifty", "sixty", "seventy", "eighty","ninety"]
 
-  teen_num = [ "eleven", "twelve", "thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
-  zillions =[['hundred', 2],
-            ['thousand', 3],
-            ['million', 6],
-            ['billion', 9],
-            ['trillion', 12],
-            ['quadrillion', 15],
-            ['quintillion', 18],
-            ['sextillion', 21],
-            ['septillion', 24],
-            ['octillion', 27],
-            ['nonillion', 30],
-            ['decillion', 33],
-            ['undecillion', 36],
-            ['duodecillion', 39],
-            ['tredecillion', 42],
-            ['quattuordecillion', 45],
-            ['quindecillion', 48],
-            ['sexdecillion', 51],
-            ['septendecillion', 54],
-            ['octodecillion', 57]]
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
 
-  left = number #number left to write
+  zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
 
-  while zillions.length > 0 
+  left = number
+
+  while zillions.length > 0
     zil_pair = zillions.pop
     zil_name = zil_pair[0]
-    zil_base = 10**zil_pair[1]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base # How many zillions left?
+    left = left - write*zil_base # Subtract off those zillions.
 
-    write = left/zil_base 
-    left = left - write*zil_base
+    if write > 0
+      # Now here's the recursion:
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
 
-    if write > 0 
-      prefix = english_number write 
-      num_string = num_string + prefix + ' ' + zil_name 
-
-      if left > 0 
+      if left > 0
+  
         num_string = num_string + ' '
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  write = left/100 #how many hundred left 
-  left = left - write*100 #subtract off hundreds 
+  write = left/10 
+  left = left - write*10 
 
   if write > 0
-  	hundreds = english_number write #recursive occurs 
-  	num_string = num_string + hundreds + ' hundred'
-  	if left > 0 
-  		num_string = num_string + ' '
-  	end 
-  end 
-
-  write = left/10 #how many tenth are left
-  left = left - write*10 #subtract off tenth 
-
-  if write > 0 
-  	if ((write == 1) and (left > 0))
-  		num_string = num_string + teen_num[left-1] 
-  		left = 0 
-  	else 
-  		num_string = num_string + tenth_num[write-1] #whole tenth number
-  	end 
+    if ((write == 1) and (left > 0))
   
-  	if left > 0 
-  	num_string = num_string + '-'
-  	end 
-  end 
+      num_string = num_string + teenagers[left-1]
+  
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+     
+    end
+
+    if left > 0
+
+      num_string = num_string + '-'
+    end
+  end
 
   write = left 
-  left = 0
+  left = 0 
 
-  if write > 0 
-  	num_string = num_string + ones_num[write-1]
-  end
+  if write > 0
+    num_string = num_string + ones_place[write-1]
   
+  end
+
   num_string
-
 end
-

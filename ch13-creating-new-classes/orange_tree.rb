941684ea2c1e84cacf489ab94dce3dd748a84a5c
Ben Rome <benjrome@gmail.com>
2015-09-27 15:37:07 +0100

    Attempt 2 at agreeing with CI

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,54 +1,59 @@
- class OrangeTree
-
+class OrangeTree
   def initialize
     @height = 0
-    @fruit = 0
+    @orange_count = 0
     @alive = true
   end
 
   def height
-    @alive ? @height.round(1) : 'A dead tree is not very tall. :('
+    if @alive
+      @height.round(1)
+    else
+      'A dead tree is not very tall. :('
+    end
   end
-
 
   def count_the_oranges
-    @alive ? @fruit : 'A dead tree has no oranges. :('
+    if @alive
+      @orange_count
+    else
+      'A dead tree has no oranges. :('
+    end
   end
-
 
   def one_year_passes
     if @alive
-      @height += 0.4
-      @fruit = 0
-
-      if @height.round(1) > 10
+      @height = @height + 0.4
+      @orange_count = 0 # old oranges fall off
+      if @height > 10.1
+        # tree dies
         @alive = false
         'Oh, no! The tree is too old, and has died. :('
-       elsif @height > 2
-        @fruit = (@height * 15 - 25).to_i
-        "This year your tree grew to #{@height.round(1)}m tall, " +
-        "and produced #{@fruit} oranges."
+      elsif @height > 2
+        # new oranges grow
+        @orange_count = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " and produced #{@orange_count} oranges."
       else
-        "This year your tree grew to #{@height.round(1)}m tall, " +
-        "but is still too young to bear fruit."
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " but is still too young to bear fruit."
       end
-
     else
-      "A year later, the tree is still dead. :("
+      'A year later, the tree is still dead. :('
     end
-        
   end
-  
 
   def pick_an_orange
     if @alive
-      if @fruit > 0
-        @fruit -= 1
-        "You pick a juicy, delicious orange!"
+      if @orange_count > 0
+        @orange_count = @orange_count - 1
+        'You pick a juicy, delicious orange!'
       else
-      "You search every branch, but find no oranges."
+        'You search every branch, but find no oranges.'
       end
     else
-      "A dead tree has nothing to pick. :("
+      'A dead tree has nothing to pick. :('
     end
   end
+
+end

2a704609bb949f660693e41ed235487d5125920a
octavian <octavianrotari@gmail.com>
2015-09-27 14:40:33 +0200

    chapter 14 finished

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
 def profile block_description, &block
-  # your code here
+	profiling_on = false
+	if profiling_on
+		start_time = Time.new
+		block.call
+		duration = Time.new - start_time
+		puts "#{block_description}: #{duration} seconds"
+	else
+		block.call
+	end
 end

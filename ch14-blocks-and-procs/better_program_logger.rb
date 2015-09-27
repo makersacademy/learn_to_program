acf2df3fb2fcd8d6cf71acf9fc0a48691fc77732
Ben Rome <benjrome@gmail.com>
2015-09-27 15:44:53 +0100

    Attempt 6 at agreeing with CI

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -3,7 +3,7 @@ $level=0
 def log1 desc, &block
   # your code here
   indent = " "
-  puts "#{indent*$level}Beginning \"#{desc}\"..."
+  puts "#{indent*$level} Beginning \"#{desc}\"..."
   $level +=1
   retval=block.call
   $level -=1

3162381aa4b3d2bcfc24ce67cacae758d229746c
jamierose <jrose111284@gmail.com>
2015-09-27 13:48:09 +0100

    shuf

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,21 +1,10 @@
-def shuffle arr
-<<<<<<< HEAD
-
-if arr.length < 0
-=======
+def shuffle(arr)
   if arr.length < 0
->>>>>>> 0f74b31cf6c4b9ba4a2c200fe1e48f74d0afcbc0
-  	return nil
-  else
-  	arr.shuffle
+    if arr.length < 0
+      return nil
+    else
+      arr.shuffle
+    end
   end
-<<<<<<< HEAD
-
+  puts(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 end
-
-puts(shuffle([1,2,3,4,5,6,7,8,9]))
-=======
-   
-end
-
->>>>>>> 0f74b31cf6c4b9ba4a2c200fe1e48f74d0afcbc0

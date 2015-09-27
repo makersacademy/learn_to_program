7e8c87fab391872cbab1723ed2c83b0b3274aabf
Mateja <mateja683@gmail.com>
2015-09-27 11:52:29 +0100

    sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
-def sort arr
-  # your code here
+def sort(arr)
+  return arr if arr.length < 1
+
+  middle = arr.pop
+  before = arr.select { |item| item < middle}
+  after = arr.select { |item| item >= middle}
+
+  sort(before) + [middle] + sort(after)
 end
+
+puts(sort(["I","just","feel","like","dancing"]).join(' '))

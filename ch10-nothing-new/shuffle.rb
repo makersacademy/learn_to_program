8f87d7ab5096cf7dc5fcc2c675339e305aef517d
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-27 13:08:17 +0100

    shuffle complete

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,24 @@
-def shuffle arr
-  # your code here
+def shuffle (arr)
+	shuffled = []
+	t = arr.length
+	while t > 0 
+		new_array = []
+		rand_index = rand(t)
+		index = 0
+
+		arr.each { |value|
+				if index == rand_index
+					shuffled.push value
+				else
+					new_array.push value
+				end
+			t = t - 1
+		}
+		arr = new_array
+	end
+	shuffled
 end
+
+
+#puts(shuffle([1,2,3,4,5,6,7,8,9]))
+

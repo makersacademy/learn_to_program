a2cadf82e93735941c2acbefab3a871ea71bbb71
Rebecca <beccollins82@hotmail.com>
2015-09-27 15:52:29 +0100

    chapter 12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+puts 'What year were you born in?'
+year = gets.chomp.to_i
+
+puts 'What month were you born in?'
+month = gets.chomp.to_i
+
+puts 'What day were you born in?'
+day = gets.chomp.to_i
+
+age = Time.new - Time.gm(year,month,day)
+
+
+sec_in_a_year = (age/(60*60*24*365)).to_i
+
+puts ("SPANK!"+"\n") * sec_in_a_year
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,3 @@
-# your code here
+my_bday = Time.gm(1982,3,16,1,30)
+billion_seconds = my_bday + (10**9)
+puts billion_seconds# your code here

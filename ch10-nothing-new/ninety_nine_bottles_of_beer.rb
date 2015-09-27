8ba7bf02607005d0f47841b503c77ae66b8aa6f9
Mateja <mateja683@gmail.com>
2015-09-27 11:55:01 +0100

    bottles of beer

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,21 @@
-# your code here
+require_relative './english_number'
+
+total_number_of_bottles = 10
+
+until total_number_of_bottles == 0 do
+
+    total_number_of_bottles -= 1
+    if (total_number_of_bottles > 1)
+        puts english_number(total_number_of_bottles) + " bottles of beer on the wall, " + english_number(total_number_of_bottles) + " bottles of beer."
+        puts "Take one down and pass it around, " + english_number(total_number_of_bottles-1) + " bottles of beer on the wall."
+        puts ' '
+    elsif (total_number_of_bottles == 1)
+        puts english_number(total_number_of_bottles) + " bottle of beer on the wall, " + english_number(total_number_of_bottles) + " bottle of beer."
+        puts "Take one down and pass it around, no more bottles of beer on the wall."
+        puts ' '
+    else
+        puts 'No more bottles of beer on the wall, no more bottles of beer.'
+        puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+        puts ''
+    end
+end

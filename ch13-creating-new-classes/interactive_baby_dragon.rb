83c879929dd45fd7d0a161344799c10d7f8b1f61
Andrew Dowell <adowellphoto@gmail.com>
2015-09-27 23:32:10 +0100

    update ch13

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,119 @@
-# your code here
+class Dragon
+	def initialize name
+		@name = name
+		@asleep = false
+		@stuff_in_belly = 10
+		@stuff_in_intestine = 0
+		puts "#{@name} is born."
+	end
+	def feed
+		puts "You feed #{@name}."
+		@stuff_in_belly = 10
+		passage_of_time
+	end
+	def walk
+		puts "You walk #{@name}."
+		@stuff_in_intestine = 0
+		passage_of_time
+	end
+	def put_to_bed
+		puts "You put #{@name} to bed."
+		@asleep = true
+		3.times do
+			if @asleep
+				passage_of_time
+			end
+			if @asleep
+				puts "#{@name} snores, filling the room with smoke."
+			end
+		end
+		if @asleep
+			@asleep = false
+			puts "#{@name} wakes up slowly."
+		end
+	end
+	def toss
+		puts "You toss #{@name} up into the air."
+		puts 'He giggles, which singes your eyebrows.'
+		passage_of_time
+	end
+	def rock
+		puts "You rock #{@name} gently."
+		@asleep = true
+		puts 'He briefly dozes off...'
+		passage_of_time
+			if @asleep
+				@asleep = false
+				puts '...but wakes when you stop.'
+			end
+	end
+	private
+			# "private" means that the methods defined here are
+			# methods internal to the object. (You can feed your
+			# dragon, but you can't ask him whether he's hungry.)
+		def hungry?
+			# Method names can end with "?".
+			# Usually, we do this only if the method
+			# returns true or false, like this:
+			@stuff_in_belly <= 2
+		end
+		def poopy?
+			@stuff_in_intestine >= 8
+		end
+		def passage_of_time
+			if @stuff_in_belly > 0
+				# Move food from belly to intestine.
+				@stuff_in_belly = @stuff_in_belly - 1
+				@stuff_in_intestine = @stuff_in_intestine + 1
+			else # Our dragon is starving!
+				if @asleep
+					@asleep = false
+					puts 'He wakes up suddenly!'
+				end
+					puts "#{@name} is starving! In desperation, he ate YOU!"
+					exit # This quits the program.
+			end
+				if @stuff_in_intestine >= 10
+					@stuff_in_intestine = 0
+					puts "Whoops! #{@name} had an accident..."
+				end
+				if hungry?
+					if @asleep
+						@asleep = false
+						puts 'He wakes up suddenly!'
+					end
+					puts "#{@name}'s stomach grumbles..."
+				end
+				if poopy?
+					if @asleep
+						@asleep = false
+						puts 'He wakes up suddenly!'
+					end
+					puts "#{@name} does the poppy dance..."
+				end
+		end
+	end
+
+puts 'What would you like to name your baby dragon?'
+name = gets.chomp
+pet = Dragon.new name
+while true
+	puts
+	puts 'commands: feed, toss, walk, rock, put to bed, exit'
+	command = gets.chomp
+	if command == 'exit'
+		exit
+	elsif command == 'feed'
+		pet.feed
+	elsif command == 'toss'
+		pet.toss
+	elsif command == 'walk'
+		pet.walk
+	elsif command == 'rock'
+		pet.rock
+	elsif command == 'put to bed'
+		pet.put_to_bed
+	else
+		puts 'Huh? Please type one of the commands.'
+	end
+end
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,39 @@
 class OrangeTree
-  # your code here
+    def initialize
+      @orange=0
+      @height=0
+      @years_old=0
+      puts "You have planted an orange tree."
+    end
+
+    def pick_an_orange
+      if @oranges>0
+        @oranges=@oranges-1
+        puts "That was gooooood. There are #{@oranges} left."
+      else
+        puts "There are no oranges to pick"
+      end
+    end
+
+    def height
+      puts "Your tree is #{@height} meters tall."
+    end
+
+    def count_the_oranges
+      puts "There are #{@oranges} oranges."
+    end
+
+    def one_year_passes
+      if @years_old < 50 #We're not very good at looking after our tree
+        @oranges=0 #last years fall off
+        @height=Math.log(@years_old+1).round(2)#assume that log is a good approximation for tree growth (pun not intended)
+        if @years_old>3
+          @oranges=rand(40..100*Math.log(@years_old).to_i)#Assume random variation, but upwards trend with years
+        end
+        @years_old=@years_old+1 #ages one year
+      else
+        puts "Oh no, your tree is dead"
+        exit
+      end
+    end
 end

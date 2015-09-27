982fc98271cb844ad5fb6c253d73285d82096f8d
blancopado <pablo.dominguezblanco@gmail.com>
2015-09-27 11:35:18 +0200

    old school roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,12 +1,12 @@
 def old_roman_numeral num
 	roman = ''
-	roman = roman + 'M' * (num / 1000)
-	roman = roman + 'D' * (num % 1000 / 500)
-	roman = roman + 'C' * (num % 500 / 100)
-	roman = roman + 'L' * (num % 100 / 50)
-	roman = roman + 'X' * (num % 50 / 10)
-	roman = roman + 'V' * (num % 10 / 5)
-	roman = roman + 'I' * (num % 5 / 1)
+	roman += 'M' * (num / 1000)
+	roman += 'D' * (num % 1000 / 500)
+	roman += 'C' * (num % 500 / 100)
+	roman += 'L' * (num % 100 / 50)
+	roman += 'X' * (num % 50 / 10)
+	roman += 'V' * (num % 10 / 5)
+	roman += 'I' * (num % 5 / 1)
 	roman
 end
-puts(old_roman_numeral(1999))
+puts old_roman_numeral(2543)

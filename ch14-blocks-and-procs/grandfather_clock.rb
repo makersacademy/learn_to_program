de4f5dab1b1b75987ef6aeb6bbfb0ebef8dc8685
Robert Youngman <rob_youngman@yahoo.com>
2015-09-27 19:24:11 +0100

    First attempt at grandfather_clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,15 @@
 def grandfather_clock &block
-  # your code here
+  hour_now = Time.new.hour
+
+
+  case hour_now
+  when 0
+    hour_now = 12
+  when 13..23
+    hour_now = hour_now - 12
+  end
+
+  hour_now.times(&block)
 end
+
+grandfather_clock { puts 'DONG!' }

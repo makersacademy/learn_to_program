bc4edc86bf903243904b19b128378f7853b2d1c6
Mateja <mateja683@gmail.com>
2015-09-27 12:16:27 +0100

    program logger fix

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,15 +1,17 @@
 def log desc, &block
-  puts "Beginning \"#{desc}\"..."
+  puts 'Beginning "' + desc + '"...'
   result = block.call
-  puts "...\"#{desc}\" finished, returning: #{result}"
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
-log "outer block" do
-  log "some little block" do 
-  	5 
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
   end
-  log "yet another block" do 
-  	"I like Thai food!" 
+
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
   end
-  false
+
+  '0' == 0
 end

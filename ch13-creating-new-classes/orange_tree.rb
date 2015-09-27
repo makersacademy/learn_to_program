85229522e9114e23c3fc457d1133a652a0e67b79
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-27 21:56:11 +0100

    Orange_tree.rb complete

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,78 @@
 class OrangeTree
-  # your code here
+	
+	def initialize
+	  	@height = 0
+	  	@orange_count = 0
+	  	@alive = true
+	end
+
+	def height
+		if @alive == true
+			@height.round(1)
+		else 
+			'A dead tree is not very tall. :('
+		end
+	end
+
+	def count_the_oranges
+		if @alive == true
+			@orange_count
+		else 
+			'A dead tree has no oranges. :('
+		end
+	end
+
+	def one_year_passes
+		if @alive
+			@height = @height + 0.4
+			@orange_count = 0
+			if @height >= 10 && rand(2) > 0
+				@alive = false
+				'Oh, no! The tree is too old, and has died. :('
+			elsif @height > 2 
+				@orange_count = (@height * 15 - 25).to_i
+				"This year your tree grew to #{@height.round(1)}m tall, and produced #{@orange_count} oranges."
+			else
+		        "This year your tree grew to #{@height.round(1)}m tall, but is still too young to bear fruit."
+			end
+		else
+		 'A year later, the tree is still dead. :('
+		end
+	end		
+
+	def pick_an_orange
+		if @alive
+			if @orange_count > 0
+				@orange_count = @orange_count - 1
+				'You pick a juicy, delicious orange!'
+			else
+				'You search every branch, but find no oranges.'
+			end
+		else
+			'A dead tree has nothing to pick. :('
+		end
+	end
 end
+
+
+
+
+=begin
+ot = OrangeTree.new
+23.times do
+ot.one_year_passes
+
+
+puts(ot.one_year_passes)
+puts(ot.count_the_oranges)
+puts(ot.height)
+puts(ot.one_year_passes)
+puts(ot.one_year_passes)
+puts(ot.one_year_passes)
+puts(ot.one_year_passes)
+puts(ot.one_year_passes)
+puts(ot.height)
+puts(ot.count_the_oranges)
+puts(ot.pick_an_orange)
+end
+=end

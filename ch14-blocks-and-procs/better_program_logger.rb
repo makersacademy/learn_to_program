bfee86388d9ca6e83c904de38b3b5457dd4fb3ba
Robert Youngman <rob_youngman@yahoo.com>
2015-09-27 20:42:53 +0100

    First commit of better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
+$depth = 0
+
 def log desc, &block
-  # your code here
+  indent = ' '*$depth
+
+  puts "#{indent}Beginning #{desc} ..."
+  $depth = $depth + 3
+
+  block.call
+
+  puts "#{indent}Finished #{desc}"
+  $depth = $depth - 3
+  indent = ' '*$depth
+end
+
+log 'Simple arithemtic' do
+  log 'More arithmetic' do
+    1_000.times {1+1}
+  end
+  1_000.times {1*1}
 end

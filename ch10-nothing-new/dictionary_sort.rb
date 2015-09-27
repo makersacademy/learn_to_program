8877ff6668524d993235cd3f2b52045fc7a9b507
danwheeler <da-wheeler@hotmail.co.uk>
2015-09-27 13:06:32 +0100

    dict_sort complete

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,10 +1,11 @@
 def dictionary_sort arr
+	sorted = arr
 		arr.each_index { |i|
 		arr.each_index { |j| 
 			if arr[i].downcase < arr[j].downcase
 				arr[i], arr[j] = arr[j], arr[i]
 			end	
-		}
+		}	
 	}
-	print arr
-end
+	return arr.each_index { |i| arr[i].downcase}
+end 

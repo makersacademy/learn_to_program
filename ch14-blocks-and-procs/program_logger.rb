20ce197b8e7561aed4687b22ebad552b36564e99
Tom Barnes <ruralmyth@gmail.com>
2015-09-27 20:32:35 +0100

    Cleaned up program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -4,17 +4,3 @@ def log desc, &block
   puts "...\"" + desc + "\" finished, returning: " + block.call.to_s
 
 end
-
-log 'outer block' do
-  log 'some little block' do
-        1**1 + 2**2
-  end
-  log 'yet another block' do
-    'I like Thai food!'
-  end
-'These exercises are well explained' == false  
-end
-
-
-
-

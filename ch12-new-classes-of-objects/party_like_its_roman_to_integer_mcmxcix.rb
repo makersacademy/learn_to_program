b1e280b0d662116bd50ab2f69cb40c5abd85b521
Amy Yang <ayc.yang@gmail.com>
2015-09-28 21:34:59 +0100

    First commit party like its roman to integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,19 @@
+load '/Users/amyyang/Projects/learn_to_program/ch09-writing-your-own-methods/roman_numerals.rb'
+
 def roman_to_integer roman
-  # your code here
+
+	capital = roman.upcase
+	numbers = (1..5000).to_a 
+
+	numbers.each do |integer|
+		if capital == roman_numeral(integer)
+			#if capitalized roman equal output of roman number with roman_numerals method 
+			return integer 
+		else 
+			nil 
+		end 
+	end
 end
+
+puts roman_to_integer 'mcmxcix'
+

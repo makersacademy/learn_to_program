fc0a08e1d9ac156c304a785082d8758a5e7a349c
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-28 22:05:46 +0100

    solution from book for program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,30 @@
+# def log desc, &block
+#   puts "Beginning #{desc}..."
+#   block.call
+#   puts "....#{desc} finished, returning: "
+#   block.call
+#   puts (block.call).inspect
+# end
+
+
+# log("outer block") do
+#   log("some little block") {puts 5}
+#     log("yet another block") {puts "I like Thai food!"}
+# end
+
 def log desc, &block
-  # your code here
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+  '0' == 0
 end

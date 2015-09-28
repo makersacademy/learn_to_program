5d915a6fe084da2edad5900f3355873319bd4765
jamierose <jrose111284@gmail.com>
2015-09-28 10:24:05 +0100

    shuf

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,10 +1,39 @@
-def shuffle(arr)
-  if arr.length < 0
-    if arr.length < 0
-      return nil
-    else
-      arr.shuffle
+=begin
+def shuffle arr
+  shuf = []
+
+  while arr.length > 0
+    # Randomly pick one element of the array.
+    rand_index = rand(arr.length)
+
+    # Now go through each item in the array,
+    # putting them all into new_arr except for the
+    # randomly chosen one, which goes into shuf.
+    curr_index = 0
+    new_arr = []
+
+    arr.each do |item|
+      if curr_index == rand_index
+        shuf.push item
+      else
+        new_arr.push item
+      end
+
+      curr_index = curr_index + 1
     end
+
+    # Replace the original array with the new,
+    # smaller array.
+    arr = new_arr
   end
-  puts(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+  shuf
+end
+=end
+def shuffle arr
+  # arr.sort_by(&:rand)
+  # note the above was how Chris pine did
+  # do it, but it no longer works, instead do the
+  # following:
+  arr.shuffle
 end

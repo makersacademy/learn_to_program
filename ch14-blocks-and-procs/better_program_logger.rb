3ec74446668e218532ba739b801a16632ad418f3
“DovileSand” <dovile.sand@protonmail.ch>
2015-09-28 03:03:31 +0100

    Added ch14 loggers

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,24 @@
-def log desc, &block
-  # your code here
+$logger_depth = 0
+def log1 desc, &block
+  prefix = '   '*$logger_depth
+  puts prefix + "Beginning '#{desc}' ..."
+  $logger_depth = $logger_depth + 1
+  result = block.call
+  $logger_depth = $logger_depth - 1
+  puts prefix + "... '#{desc}' finished, returning: #{result.to_s}"
 end
+
+log1 "outer block" do
+	log1 "some little block" do
+		log1 "teeny-tiny block" do
+			"lOts oF loVE".downcase
+		end
+
+	7*3*2
+	end
+
+	log1 "yet another block" do
+		"!doof iahT ekil I".reverse	
+	end
+	'0' = 0
+end		

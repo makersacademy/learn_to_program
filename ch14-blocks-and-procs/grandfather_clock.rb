1b2665e97676419fcbf27cc1bcf8eab820c42cc1
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-28 21:02:34 +0100

    Grandfather clock that takes a block and repeats by number of hours passed

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,7 @@
 def grandfather_clock &block
-  # your code here
+  hoursPassed =  Time.new.hour+1 < 12 ? Time.new.hour  : Time.new.hour - 12
+  # puts hoursPassed
+  hoursPassed.times {block.call}
 end
+
+# grandfather_clock {puts "Bong!"}

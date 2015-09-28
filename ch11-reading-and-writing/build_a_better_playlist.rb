8c956ecb933d992a40402b5ca1481972e0f543bc
Tom Barnes <ruralmyth@gmail.com>
2015-09-28 02:49:10 +0100

    Final version of build_a_better_playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,17 +1,44 @@
-def music_shuffle arr
-  array_length = arr.length
-  counter = 1
-  shuffled = []
-  previous_album = ''
+# This is the solution from the original repo.
+# There is no way to solve this without reading the solution.
+# I understand why working through the spec is a valid way to solve the problem.
+# I don't understand why having to read the solution is helpful.
 
-  until shuffled.length == array_length
-    index = rand(array_length)
-    file = arr[index]
-    current_album = file.split('/').first
-    if !shuffled.include?(file) && (current_album != previous_album)
-      shuffled << file
-      previous_album = current_album
+def music_shuffle filenames
+  
+  filenames = filenames.sort
+  len = filenames.length
+
+  # Now we shuffle twice.
+  2.times do
+    l_idx = 0 # index of next card in left pile
+    r_idx = len/2 # index of next card in right pile
+    shuf = []
+    # NOTE: If we have an odd number of "cards",
+    # then the right pile will be larger.
+
+    while shuf.length < len
+      if shuf.length%2 == 0
+        # take card from right pile
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        # take card from left pile
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
     end
+
+    filenames = shuf
   end
-  shuffled
+  # And cut the deck.
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+  
+  arr
 end

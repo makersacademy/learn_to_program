6f36925ff844b597d7c284ebeeed854ddeb95c8a
yevdyko <evgeny.dyko@gmail.com>
2015-09-29 01:04:28 +0300

    Added ch14-program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,17 @@
 def log desc, &block
-  # your code here
+  puts "Beginning #{desc.inspect}..."
+  result = block.call
+  puts "...#{desc.inspect} finished, returning: #{result}"
+end
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+
+  log 'yet another block'
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
 end

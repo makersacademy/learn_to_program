3783d91dc54a599660ee2972091764bf2007e59f
ezzy elliott <ezzy.elliott@gmail.com>
2015-09-29 07:17:54 +0100

    Program logger with indents

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,20 @@
 def log desc, &block
-  # your code here
+  $nesting_depth = $nesting_depth == nil ? 0 : $nesting_depth
+  puts ' '*$nesting_depth + 'Beginning "' + desc + '"...'
+  $nesting_depth +=1
+  result = block.call
+  $nesting_depth -=1
+
+  puts ' '*$nesting_depth + '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+  '0' == 0
 end

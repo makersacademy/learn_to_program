74662f544c5ca7ebbcf8d01df99d033d3e0443ae
hasulica <raduasandei@gmail.com>
2015-09-30 11:10:35 +0100

    Added working version.

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,33 @@
-# your code here
+Dir.chdir 'C:/Documents and Settings/Katy/Picture Inbox'
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.'
+
+  new_name = if pic_number < 10
+               "#{batch_name}0#{pic_number}.jpg"
+             else
+               "#{batch_name}#{pic_number}.jpg"
+             end
+
+  if File.exists new_name
+    $stderr.puts 'File #{new_name} already exists. ' \
+                 'Please check and restart program.'
+    exit
+  else
+    File.rename name, new_name
+  end
+  pic_number += 1
+end
+
+puts
+puts 'Done'
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,6 @@
-# your code here
+def billionth_second(time)
+  puts time + 10**9
+end
+
+birthday = Time.local(1990, 5, 29, 19)
+billionth_second(birthday)

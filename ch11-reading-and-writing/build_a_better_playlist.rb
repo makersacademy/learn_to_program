912cf4b9dc0ed91b57731637a8a5423e881fc596
hasulica <raduasandei@gmail.com>
2015-10-01 14:51:55 +0100

    Added working version.

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,32 @@
-def music_shuffle filenames
-  # your code here
+def music_shuffle(filenames)
+  filenames = filenames.sort
+  length = filenames.length
+
+  2.times do
+    left = 0
+    right = length / 2
+    shuffle = []
+
+    while shuffle.length < length
+      if shuffle.length.even?
+        shuffle.push(filenames[right])
+        right += 1
+      else
+        shuffle.push(filenames[left])
+        left += 1
+      end
+    end
+
+    filenames = shuffle
+  end
+  arrray = []
+  split = rand(length)
+  i = 0
+
+  while i < length
+    arrray.push(filenames[(i + split) % length])
+    i += 1
+  end
+
+  arrray
 end

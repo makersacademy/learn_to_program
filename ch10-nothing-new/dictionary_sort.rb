ec81c73cf2679efe4b9a47211aac508cae72c182
Karen Musson <kmusson3@gmail.com>
2015-10-05 12:44:16 +0100

    updated to ignore but keep the case

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,32 @@
-def dictionary_sort arr
-  # your code here
+def sort arr 
+  recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted 
+  if unsorted.length <= 0
+    return sorted 
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |test_obj| 
+    if test_obj.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = test_obj
+    else
+      still_unsorted.push test_obj
+    end 
+  end
+
+  # Now "smallest" really does point to the
+  # smallest element that "unsorted" contained,
+  # and all the rest of it is in "still_unsorted". 
+  sorted.push smallest
+  
+  recursive_sort still_unsorted, sorted
+end
+
+puts(sort(['groceries' ,'feel' ,'moving' ,'like' ,'apple' ,'cat', 'Feel', 'SHOUTING', 'tree ','Really', 'FARMER' ]))
+
+

bfcdcb5d5676cd19b8db256a0e1243a812498ee6
Karen Musson <kmusson3@gmail.com>
2015-10-05 11:06:16 +0100

    roman numerals with wedding date options

---
+++ roman-numerals.rb
@@ -0,0 +1,44 @@
+puts 'Please enter a number between 1 and 3000: '
+
+input = gets.chomp.to_i
+
+def roman_numeral num 
+  thousands = (num / 1000)
+  hundreds = (num % 1000 / 100)
+  tens  = (num % 100 / 10)
+  ones  = (num % 10)
+
+  roman = 'M' * thousands
+
+  if hundreds == 9
+    roman = roman + 'CM'
+  elsif hundreds == 4 
+    roman = roman + 'CD'
+  else
+    roman = roman + 'D' * (num % 1000 / 500)
+    roman = roman + 'C' * (num % 500 / 100) 
+  end
+
+  if tens == 9 
+    roman = roman + 'XC'
+  elsif tens == 4 
+    roman = roman + 'XL'
+  else
+    roman = roman + 'L' * (num % 100 / 50)
+    roman = roman + 'X' * (num % 50 / 10) 
+  end
+
+  if ones == 9 
+    roman = roman + 'IX'
+  elsif ones == 4 
+    roman = roman + 'IV'
+  else
+    roman = roman + 'V' * (num % 10 / 5)
+    roman = roman + 'I' * (num % 5 / 1) 
+  end
+  roman
+end
+
+output = roman_numeral(input)
+
+puts output

a6532ee6afc8edc48e08f2fde15e9c9fd8be78a5
Karen Musson <kmusson3@gmail.com>
2015-10-05 13:03:48 +0100

    Shuffle values in array

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,31 @@
-def shuffle arr
+def shuffle input
   # your code here
+  shuff = []
+
+  while input.length > 0
+    # Randomly pick one element of the array.
+    new_index = rand(input.length)
+
+    # Now go through each item in the array,
+    # putting them all into new_arr except for the
+    # randomly chosen one, which goes into shuff.
+    curr_index = 0
+    new_arr = []
+
+    input.each do |item|
+      if curr_index == new_index
+        shuff.push item
+      else
+        new_arr.push item
+      end
+      curr_index = curr_index + 1
+    end
+
+    # Replace the original array with the new,
+    # smaller array.
+    input = new_arr
+  end
+  shuff
 end
+
+puts(shuffle([1,2,3,4,5,6,7,8,9]))

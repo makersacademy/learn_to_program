e1d362eeab157e6b37ca48ab7115b21880d299d6
Karen Musson <kmusson3@gmail.com>
2015-10-06 11:22:14 +0100

    passing blocks example

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
+
 def log desc, &block
-  # your code here
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    puts 'Output is..' + (1**1 + 2**2).to_s
+  end
+
+  log 'yet another block' do
+    '!siht teg ton od I'.reverse
+  end
+
+  '0' == 0
 end

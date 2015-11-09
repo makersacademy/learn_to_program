48f1b5219559680ea810cbb38527e7c9a00ce83d
michaellennox <michaelctlennox@gmail.com>
2015-11-09 14:42:48 +0000

    Completed ask.rb challenge

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -19,3 +19,16 @@
 #
 #  answer
 #end
+
+def ask(question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes' || reply == 'no'
+      return reply == 'yes' ? true : false
+    else
+      puts "Please answer 'yes' or 'no'."
+    end
+  end
+end

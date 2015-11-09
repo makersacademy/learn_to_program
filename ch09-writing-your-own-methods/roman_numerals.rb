30f0f72fff7398d7e7df492799b12488cd7cbd84
michaellennox <michaelctlennox@gmail.com>
2015-11-09 16:28:30 +0000

    Added roman numerals question

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,5 @@
+# “Modern” Roman numerals. Eventually, someone thought it would be ter- ribly clever if putting a smaller number before a larger one meant you had to subtract the smaller one. As a result of this development, you must now suffer. Rewrite your previous method to return the new-style Roman numerals so when someone calls roman_numeral 4, it should return 'IV'.
+
 def roman_numeral num
   # your code here
 end

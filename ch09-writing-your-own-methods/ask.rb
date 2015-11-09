82a34c882452b29a5e2abd56af1cc17111fae0a1
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-09 18:14:06 +0000

    ch09_submission

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,16 @@
 def ask question
-  # your code here
-end
+    while true
+      puts question
+      reply = gets.chomp.downcase
+        if reply == 'yes'
+          return true
+            break
+          elsif reply == 'no'
+            return false
+            break
+          else
+            puts 'Please answer "yes" or "no"' if reply
+          end
+        end
+      end
+      

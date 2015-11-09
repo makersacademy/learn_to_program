fb5545a64e580967fdaa7f69180c1413fe436341
Chris Wynne <chris.wynndow@gmail.com>
2015-11-09 12:32:21 +0000

    Adds chapter 9 challenge solutions

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,31 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' || reply == 'no')
+      reply == 'yes' ? (return true) : (return false)
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+
+  end
 end
+
+=begin
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?' # Ignore this return value
+ask 'Do you like eating burritos?' # And this one
+wets_bed = ask 'Do you wet the bed?' # Save this return value
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed
+=end
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,46 @@
 def old_roman_numeral num
-  # your code here
+output = ""
+  if num / 1000 != 0
+    output << "M" * (num / 1000)
+    num -= (num/1000)*1000
+  end
+  if num / 500 != 0
+    output << "D" * (num / 500)
+    num -= (num/500)*500
+  end
+  if num / 100 != 0
+    output << "C" * (num / 100)
+    num -= (num/100)*100
+  end
+  if num / 50 != 0
+    output << "L" * (num / 50)
+    num -= (num/50)*50
+  end
+  if num / 10 != 0
+    output << "X" * (num / 10)
+    num -= (num/10)*10
+  end
+  if num / 5 != 0
+    output << "V" * (num / 5)
+    num -= (num/5)*5
+  end
+  if num / 1 != 0
+    output << "I" * (num / 1)
+    num -= (num/1)*1
+  end
+
+output
 end
+
+
+#p old_roman_numeral 15999
+
+=begin
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+=end

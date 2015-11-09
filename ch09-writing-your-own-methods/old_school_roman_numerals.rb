c824026fab477737e1d84fd55251392ada5a5822
edwardkerry <edwardkerry@gmail.com>
2015-11-09 23:04:45 +0000

    ch09 old school roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+  roman = {1000=>'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 5 => 'V', 1 =>'I'}
+  
+  result=''
+
+  roman.each do |english_value ,numeral|
+     
+       
+   divide = (num / english_value) 
+        if divide > 0
+            result << numeral * divide
+        end
+   remainder = num % english_value
+   num = remainder
+end
+   puts result
 end

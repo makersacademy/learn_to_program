c98505572bd539fdf653155f22adbd21a0b82f36
emmabeynon <emma.beynon@gmail.com>
2015-11-09 20:24:12 +0000

    Solution: ch10-nothing-new/shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,19 @@
-def shuffle arr
-  # your code here
+def shuffle(array)
+  shuffled_array = []
+
+  while array.length > 0
+    random_index = rand(0..array.length)
+    current_index = 0
+    new_array = []
+    array.each do |item|
+      if current_index == random_index
+        shuffled_array.push(item)
+      else
+        new_array.push(item)
+      end
+      current_index += 1
+    end
+  array = new_array
+end
+shuffled_array
 end

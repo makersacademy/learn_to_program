dd4a0dcb0ec83593e903269d837be512a59e1dc5
Richard Moss <richard@englishtutorslive.com>
2015-11-10 22:32:36 +0000

    added old_shool

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -3,10 +3,12 @@ def ask question
     puts question
     reply = gets.chomp.downcase
 
-    return true if reply == 'yes'
-    return false if reply == 'no'
+     return true if reply == 'yes'
+     return false if reply == 'no'
 
-puts "please answer yes or no"
+	puts "please answer yes or no"
   end
-answer
+
 end
+
+

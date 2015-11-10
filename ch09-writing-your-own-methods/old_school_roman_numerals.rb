af042722c48d3cd659571ef9bedde6ae049d5d3f
Adam Lancaster <lancaster28@hotmail.com>
2015-11-10 15:18:15 +0000

    2nd ex done

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,12 @@
 def old_roman_numeral num
-  # your code here
+  numeral= ""
+  	numeral << "M"*(num/1000)
+  	numeral << "D"*(num%1000/500)
+  	numeral << "C"*(num%1000%500/100)
+  	numeral << "L"*(num%1000%500%100/50)
+  	numeral << "X"*(num%1000%500%100%50/10)
+  	numeral << "V"*(num%1000%500%100%50%10/5)
+  	numeral << "I"*(num%1000%500%100%50%10%5/1)
+
+  	numeral
 end

360430b80fe289f79960ec3309e798b056e4eb9b
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-10 23:00:02 +0000

    Added ask and old_school_roman_numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,10 @@
 def old_roman_numeral num
-  # your code here
+  thousands = (num / 1000)
+  five_hundreds = (num % 1000 / 500)
+  hundreds = (num % 500 / 100)
+  fifties = (num % 100 / 50)
+  tens = (num % 50 / 10)
+  fives = (num % 10 / 5)
+  ones = (num % 5)
+  in_romans = 'M' * thousands + 'D' * five_hundreds + 'C' * hundreds + 'L' * fifties + 'X' * tens + 'V' * fives + 'I' * ones
 end

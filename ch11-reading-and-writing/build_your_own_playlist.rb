c5813cfc7af121048cede8072f2e640f3167bae9
emmabeynon <emma.beynon@gmail.com>
2015-11-10 21:30:21 +0000

    Solution: ch11-reading-and-writing/build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,16 @@
-# your code here
+def create_playlist(playlist_file)
+  music_files = Dir['Downloads/*.wav']
+  playlist = playlist_file + '.m3u'
+  File.open playlist, 'w' do |f|
+    music_files.each do |file|
+      f.write file+"\n"
+    end
+  end
+end
+
+def view_playlist(playlist)
+  view_playlist = File.read(playlist)
+end
+
+create_playlist('my_playlist')
+view_playlist('my_playlist.m3u')

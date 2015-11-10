5bca44bc06650fada254188f5fa6dc0e42e3131f
Katie Smith <kls.smith@yahoo.co.uk>
2015-11-10 10:55:05 +0000

    chapter 9

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+  new_num = ""
+  
+  new_num << "M" * (num / 1000)
+  new_num << "D" * (num % 1000 / 500)
+  new_num << "C" * (num % 500 / 100)
+  new_num << "L" * (num % 100 / 50)
+  new_num << "X" * (num % 50 / 10)
+  new_num << "V" * (num % 10 / 5)
+  new_num << "I" * (num % 5)
+  
+  new_num
 end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,34 @@
 def roman_numeral num
-  # your code here
+    new_num = ""
+  
+  new_num << "M" * (num / 1000)
+  
+  if (num % 1000 / 100) == 9
+    new_num << "CM"
+  elsif (num % 1000 / 100) == 4
+    new_num << "CD"
+  else
+    new_num << "D" * (num % 1000 / 500)
+    new_num << "C" * (num % 500 / 100)
+  end
+  
+  if (num % 50 / 10) == 9
+    new_new << "XC"
+  elsif (num % 50 / 10) ==4
+    new_num << "XL"
+  else
+    new_num << "L" * (num % 100 / 50)
+    new_num << "X" * (num % 50 / 10)
+  end
+  
+  if (num % 10 / 1) == 9
+    new_num << "IX"
+  elsif (num % 5 / 1) == 4
+    new_num << "IV"
+  else
+    new_num << "V" * (num % 10 / 5)
+    new_num << "I" * (num % 5)
+  end
+  
+  new_num
 end

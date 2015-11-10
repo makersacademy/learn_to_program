981a4d56522ff99947e7fea0963b036444fe1c62
Katie Smith <kls.smith@yahoo.co.uk>
2015-11-10 13:56:26 +0000

    3 from chapter 10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,16 @@
 def dictionary_sort arr
-  # your code here
+  arr_final = []
+  
+  return arr if arr.empty?
+  
+  item = arr[0]
+  arr.each do |x|
+    next if item.downcase < x.downcase
+    item = x
+  end
+  
+  arr.delete_at(arr.index(item))
+  arr_final << item
+  
+  arr_final = arr_final + dictionary_sort(arr)
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,16 @@
 def sort arr
-  # your code here
+  arr_final = []
+  
+  return arr if arr.empty?
+  
+  item = arr[0]
+  arr.each do |x|
+    next if item < x
+    item = x
+  end
+  
+  arr.delete_at(arr.index(item))
+  arr_final << item
+  
+  arr_final = arr_final + sort(arr)
 end

8507e35f1c8279751244213006e49569fe69ff02
giamir <giamir.buoncristiani@gmail.com>
2015-11-10 11:16:42 +0000

    ch11 exercises solved

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,22 @@
-# your code here
+Dir.chdir '/Users/giamirbuoncristiani/Pictures/London'
+pic_names = Dir['/Users/giamirbuoncristiani/Desktop/London/*.JPG']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files:  "
+pic_number = 1
+pic_names.each do |name|
+  print '.' # This is our "progress bar".
+  new_name = if pic_number < 10
+               "#{batch_name}00#{pic_number}.jpg"
+             elsif pic_number < 100
+               "#{batch_name}0#{pic_number}.jpg"
+             else
+               "#{batch_name}#{pic_number}.jpg"
+             end
+  File.rename name, new_name
+  pic_number += 1
+end
+puts
+puts 'Done!'

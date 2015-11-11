395fb6c9b5a1a2b2ff419e3efaf3f819704978d1
giamir <giamir.buoncristiani@gmail.com>
2015-11-11 10:38:20 +0000

    ch12 exercises solved

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+puts "Year of birth"
+year = gets.chomp
+puts "Month of birth"
+month = gets.chomp
+puts "Day of birth"
+day = gets.chomp
+
+birthday = Time.new(year, month, day).utc
+now = Time.now.utc
+age = now.year - birthday.year
+diff = Time.new(now.year, month, day).utc
+age -= 1 if diff > now
+age.times{print "SPANK "}

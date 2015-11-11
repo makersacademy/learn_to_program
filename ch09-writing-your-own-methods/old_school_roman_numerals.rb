dd1bfebc13c511bc6bc3594b78d468d667cead1a
Andrew Htun <andy_htun@hotmail.com>
2015-11-11 15:40:08 +0000

    updated old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,15 @@
 def old_roman_numeral num
-  # your code here
+  number = ""
+  
+  number << 'M' * (num/1000)
+  number << 'D' * (num % 1000 / 500)
+  number << 'C' * (num % 500 / 100)
+  number << 'L' * (num % 100 / 50)
+  number << 'X' * (num % 50 / 10)
+  number << 'V' * (num % 10 / 5)
+  number << 'I' * (num % 5 / 1)
+  
+puts number  
 end
+
+old_roman_numeral 4

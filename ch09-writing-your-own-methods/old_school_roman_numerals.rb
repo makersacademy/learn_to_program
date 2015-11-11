9416965116ecb0ff4927acab04a73836350d949e
Colin McCulloch <mccolane@gmail.com>
2015-11-11 15:50:09 +0000

    Old school Roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+
+  converted = ""
+
+  converted << "M" * (num / 1000)
+  converted << "D" * (num % 1000 / 500 )
+  converted << "C" * (num % 500 / 100 )
+  converted << "L" * (num % 100 / 50 )
+  converted << "X" * (num % 50 / 10)
+  converted << "V" * (num % 10 / 5)
+  converted << "I" * (num % 5 / 1)
+
 end
+
+puts old_roman_numeral(20)
+puts old_roman_numeral(2978)

6fe9b2adffb17115fc398531622e8ac63df62786
Charlie Person <charlie.person@hyperisland.se>
2015-11-11 17:31:32 +0000

    sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,46 @@
-def sort arr
-  # your code here
+puts 'Enter a list of words, press \'enter\' to quit and they will be returned sorted.'
+word = 'one'
+list =[]
+
+while word != ''
+  word = gets.chomp
+  list.push word
+end
+
+def non_recursive_checker (arr)
+  best = []
+  while arr.length > 0
+    first = arr.first
+    pos = 0
+    arr.each.with_index do |item, index|
+      if item < first
+        first = item
+        pos   = index
+      end
+    end
+    arr.delete_at(pos)
+    best << first
+  end
+  puts best
+end
+
+
+def recursive_checker (arr, output)
+  return output if arr.length == 0
+    first = arr.first
+    pos = 0
+    arr.each.with_index do |item, index|
+      if item < first
+        first = item
+        pos   = index
+      end
+    end
+    arr.delete_at(pos)
+    output << first
+    recursive_checker(arr, output)
+  end
+
+
+def sort (arr)
+  recursive_checker(arr, [])
 end

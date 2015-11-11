3626076b5672155ca60d443aea9af2987d1c97bf
michaellennox <michaelctlennox@gmail.com>
2015-11-11 17:53:45 +0000

    Added birthday program

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,14 @@
-# your code here
+#Happy birthday! Ask what year a person was born in, then the month, and then the day. Figure out how old they are, and give them a big SPANK! for each birthday they have had.
+
+puts "Hey there, what year were you born in?"
+year = gets.chomp
+
+puts "and what month?"
+month = gets.chomp
+
+puts "and what day?"
+day = gets.chomp
+
+((Time.new - Time.gm(year, month, day)) / 31536000).to_i.times do
+  puts "SPANK!"
+end

a51bac1bdd87062146c6e2c204b6cc32bbe3344d
Andrew Htun <andy_htun@hotmail.com>
2015-11-11 13:56:14 +0000

    updated ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,26 @@
 def ask question
-  code entered
+while true
+puts question
+reply = gets.chomp.downcase
+if (reply == 'yes' || reply == 'no') then return true
+else
+puts 'Please answer "yes" or "no".'
 end
+end
+end
+
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?' 
+ask 'Do you like eating burritos?' 
+wets_bed = ask 'Do you wet the bed?' 
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed

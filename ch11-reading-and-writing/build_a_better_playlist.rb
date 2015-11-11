af3c728edc975a7436ffa1bc2e03a7b31ff2761d
Katie Smith <kls.smith@yahoo.co.uk>
2015-11-11 17:33:18 +0000

    Chapter 11s

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,25 @@
 def music_shuffle filenames
-  # your code here
+  #sorts the files to start
+  filenames.sort!
+  
+  #runs the riffle_shuffle method twice
+  filenames = riffle_shuffle filenames
+  filenames = riffle_shuffle filenames
+  
+  #cuts the 'deck' and returns 
+  listB = filenames[0...(filenames.length / 2)]
+  filenames -= filenames[0...(filenames.length / 2)]
+  filenames += listB
+end
+
+def riffle_shuffle arrayA
+  arrayB = arrayA[0...(arrayA.length / 2)]
+  arrayA -= arrayA[0...(arrayA.length / 2)]
+  arrayC = []
+
+  until arrayA.empty? && arrayB.empty?
+    arrayC << arrayA.pop
+    arrayC << arrayB.pop
+  end
+  arrayA = arrayC.reverse
 end

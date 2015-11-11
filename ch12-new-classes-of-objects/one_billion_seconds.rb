4a113fbc63ecefd83914b73362465dee8fab9a7d
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-11 21:13:01 +0000

    ch11_ch12_update

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,15 @@
-# your code here
+def billion_sec_time (year, month, date, hour, minute, second)
+
+birth_day = Time.gm(year,month,date,hour,minute,second)
+
+puts "You will turn one billion seconds at:
+        #{Time.at(birth_day.to_i+1000000000)}"
+
+end
+
+=begin
+
+Epoch is at 1 January 1970, GMT - so need to convert time to GMT before finding
+seconds to then from Epoch
+
+=end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,72 @@
 def roman_to_integer roman
-  # your code here
+
+roman = roman.upcase
+
+return 'This is not a valid roman numeral' if roman.count("\\IVXLCDM") != roman.length
+
+roman_numerals = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100,
+                  "D" => 500, "M" => 1000}
+
+i = -1
+integer = 0
+
+while i >= -roman.length
+  if i == -1
+    integer += roman_numerals.values_at("#{roman[i]}").pop
+elsif roman_numerals.values_at("#{roman[i]}").pop <
+             roman_numerals.values_at("#{roman[i+1]}").pop
+
+    integer -= roman_numerals.values_at("#{roman[i]}").pop
+else
+  integer += roman_numerals.values_at("#{roman[i]}").pop
+end
+i -= 1
+end
+
+puts integer
+end
+
+
+
+
+=begin
+
+
+def roman_numeral num
+  thousands = (num/1000)
+  hundreds = (num%1000/100)
+  tens = (num%100/10)
+  ones = (num%10)
+
+  roman = 'M' * thousands
+
+  case hundreds
+  when 9
+    roman += 'CM'
+  when 4
+    roman += 'CD'
+  else
+    roman += 'D' * (num % 1000/500)
+    roman += 'C' * (num % 500/100)
+  end
+
+  case tens
+  when 9
+    roman += 'XC'
+  when 4
+    roman += 'XL'
+  else
+    roman += 'L' * (num % 100/50)
+    roman += 'X' * (num % 50/10)
+  end
+
+  case ones
+  when 9
+    roman += 'IX'
+  when 4
+    roman += 'IV'
+  else
+    roman += 'V' * (num % 10/5)
+    roman += 'I' * (num % 5)
+  end
 end

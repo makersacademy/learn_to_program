19466057e7b3bc8425b802c85362b3817d69098d
emmabeynon <emma.beynon@gmail.com>
2015-11-11 14:03:56 +0000

    Solution: ch12-new-classes-of-objects/birthday_helper.rb

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,21 @@
-# your code here
+birth_dates = {}
+File.read('names_birthdays.txt').each_line do |line|
+  line = line.chomp
+  first_comma = 0
+  while line[first_comma] != ',' && first_comma < line.length
+    first_comma += 1
+  end
+
+  name = line[0..(first_comma - 1)]
+  date = line[-12..-1]
+  birth_dates[name] = date
+end
+
+puts "Type in the first name and last name of the person whose birthday you'd like to find."
+name = gets.chomp
+birthday = birth_dates[name]
+if birthday == nil
+  puts "I'm afraid I can't find that person's birthday."
+else
+  puts "#{name}'s next birthday is #{birthday[0..5]}."
+end

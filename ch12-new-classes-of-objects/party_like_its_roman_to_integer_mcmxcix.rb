c95eaf34282818ce2fea00e769e020b938a224d8
michaellennox <michaelctlennox@gmail.com>
2015-11-11 18:45:53 +0000

    Roman to number challenge complete

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,23 @@
+# Party like it’s roman_to_integer 'mcmxcix'! Come on, you knew it was coming, didn’t you? It’s the other half of your roman_numeral 1999 method. Make sure to reject strings that aren’t valid Roman numerals.
+
 def roman_to_integer roman
-  # your code here
+  roman_values = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
+
+  total = 0
+  last = 0
+
+  roman.length.times do
+    value = roman_values[roman[-1].upcase]
+    roman = roman[0..-2]
+
+    if value >= last
+      total += value
+      last = value
+    else
+      total -= value
+    end
+
+  end
+
+  total
 end

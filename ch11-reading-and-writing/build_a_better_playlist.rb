66ada6fa59c5516c845a422a97575a1e84f05810
edwardkerry <edwardkerry@gmail.com>
2015-11-11 21:03:05 +0000

    ch11 build a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,34 @@
-def music_shuffle filenames
-  # your code here
-end
+def music_shuffle(playlist)
+
+    playlist = playlist.sort
+    tracks = playlist.length
+
+    2.times do
+    left_deck = 0
+    right_deck = tracks/2
+    shuffler = []
+      
+      while shuffler.length < tracks
+        if shuffler.length.even?
+           shuffler.push(playlist[right_deck])
+           right_deck += 1
+        else
+           shuffler.push(playlist[left_deck])
+           left_deck += 1
+        end
+      end
+    playlist = shuffler
+    end
+    
+    new_playlist = []
+    cut = rand(tracks)
+    index = 0
+    
+    while index < tracks
+        new_playlist.push(playlist[(index+cut)%tracks])
+        index += 1
+    end
+   
+    new_playlist
+end    
+

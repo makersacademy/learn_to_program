96e3f3c9c8a99c2a9277740bf897c83c74bcbd1f
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-11 14:33:12 +0000

    committing ask

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,15 @@
 def ask question
   # your code here
+	while true
+	puts question
+	reply = gets.chomp
+
+		if (reply == 'yes' || reply == 'no')
+			return
+			break		
+		else
+			puts 'Please answer "yes" or "no".'
+		end
+	end
+	return
 end

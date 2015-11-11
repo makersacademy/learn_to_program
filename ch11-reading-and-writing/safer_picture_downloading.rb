5ecdd397469cbfcbe9aa17a356ce6208f819b5ae
edwardkerry <edwardkerry@gmail.com>
2015-11-11 15:48:48 +0000

    ch11 Safer Picture Downloading

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,34 @@
-# your code here
+Dir.chdir '/home/ubuntu/workspace/photos'
+pic_names = Dir['/home/ubuntu/workspace/new_photos/*.jpg']
+
+
+puts 'What would you like to call this batch?'
+
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files."
+
+pic_number = 1
+pic_names.each do |name|
+    print '.'
+    
+    new_name = if pic_number < 10
+        "#{batch_name}0#{pic_number}.jpg"
+    else
+        "#{batch_name}#{pic_number}.jpg"
+    end
+    
+    if File.exist? new_name  
+       puts "File already exist! Shutting down..."
+       exit
+    else
+        File.rename name, new_name
+    end
+    
+    pic_number = pic_number + 1
+
+end
+
+puts
+puts "Move complete!"

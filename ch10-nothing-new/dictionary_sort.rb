39f4619b05a015e597cacdbc2a79c771cd5680d8
Andrew Htun <andy_htun@hotmail.com>
2015-11-11 17:33:49 +0000

    updated dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -2,4 +2,4 @@ def dictionary_sort arr
   arr.split.map.sort_by{|input| input.downcase}.join(" ")
 end
 
-puts dictionary_sort 'hello Goodbye AWESOME'
+puts dictionary_sort 'hello Goodbye AWESOME Code'

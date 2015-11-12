4b0c7c497702209649d18c89119babf62b58c3bb
gareth4192 <gareth-123@live.com>
2015-11-12 13:13:48 +0000

    bottles of beer solution added

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,120 @@
 # your code here
+
+def english_number number
+  if number < 0 # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+  # No more special cases! No more returns!
+
+  num_string = '' # This is the string we will return.
+
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
+
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
+
+
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
+
+  zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
+
+  # "left" is how much of the number
+  # we still have left to write out.
+  # "write" is the part we are
+  # writing out right now.
+  # write and left...get it? :)
+  left = number
+
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base
+
+    if write > 0
+
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+
+      num_string = num_string + teenagers[left-1]
+
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+
+    end
+
+    if left > 0
+
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+
+  end
+
+  num_string
+end
+
+def bottles_of_beer
+start = 5
+while start > 2
+  puts english_number(start).capitalize + ' bottles of beer on the wall, ' + english_number(start) + ' bottles of beer!'
+  start -=1
+  puts 'Take one down pass it arround, ' + english_number(start) + ' bottles of beer on the wall!'
+end
+puts "Two bottles of beer on the wall, two bottles of beer!"
+puts "Take one down pass it arround, one bottle of beer on the wall!"
+puts "One bottle of beer on the wall, one bottle of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall"
+end

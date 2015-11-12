c349320e1650d5bf9ce36b3ff786249f0d80731d
Andrew Htun <andy_htun@hotmail.com>
2015-11-12 12:55:44 +0000

    updated english_number.rb

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,131 @@
 def english_number number
-  # your code here
+ if number < 0 
+  return 'Please enter a number that isn\'t negative'
+ end
+ if number == 0
+  return 'zero'
+ end
+
+num_string = '' 
+
+ones_place = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+tens_place = [ 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninty']
+teenagers = [ 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+left = number
+
+write = left/1000000000000
+left = left - write*1000000000000
+
+ if write > 0
+trillions = english_number write
+num_string = num_string + trillions + ' trillion'
+
+ if left > 0
+num_string = num_string + ' '
+ end
+ end
+
+
+write = left/1000000000
+left = left - write*1000000000
+
+if write > 0
+billions = english_number write
+num_string = num_string + billions + ' billion'
+
+if left > 0
+num_string = num_string + ' '
 end
+end
+
+
+write = left/1000000
+left = left - write*1000000
+
+if write > 0
+millions = english_number write
+num_string = num_string + millions + ' million'
+
+if left > 0
+num_string = num_string + ' '
+end
+end
+
+
+write = left/1000
+left = left - write*1000
+
+if write > 0
+thousands = english_number write
+num_string = num_string + thousands + ' thousand'
+
+if left > 0
+num_string = num_string + ' '
+end
+end
+
+write = left/100 
+left = left - write*100 
+
+if write > 0
+
+hundreds = english_number write
+num_string = num_string + hundreds + ' hundred'
+
+if left > 0
+
+num_string = num_string + ' '
+end
+end
+
+write = left/10 
+left = left - write*10 
+
+if write > 0
+if ((write == 1) and (left > 0))
+
+num_string = num_string + teenagers[left-1]
+
+left = 0
+else
+num_string = num_string + tens_place[write-1]
+end
+
+if left > 0
+
+num_string = num_string + '-'
+end
+end
+
+write = left
+left = 0 
+
+if write > 0
+num_string = num_string + ones_place[write-1]
+end
+
+
+num_string
+end
+
+puts english_number( 0)
+puts english_number( 9)
+puts english_number( 10)
+puts english_number( 11)
+puts english_number( 17)
+puts english_number( 32)
+puts english_number( 88)
+puts english_number( 99)
+puts english_number(100)
+puts english_number(101)
+puts english_number(234)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)
+puts english_number(1000)
+puts english_number(10000)
+puts english_number(1000000)
+puts english_number(10000000)
+puts english_number(1000000000)
+puts english_number(10000000000)

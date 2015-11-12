58a2842edb19ea0d524fe261959f852c2c60a781
Adam Lancaster <lancaster28@hotmail.com>
2015-11-12 22:19:33 +0000

    improved birthday answer

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -9,11 +9,19 @@ def happy_birthday(year, month, day)
 	birthday = Time.new(year, month, day)
 	age = (Time.now - birthday)
 	puts "You are #{age} seconds old, which is #{age/60} minutes,\n #{age/60/60} hours,\n #{age/60/60/24} days\n or #{age/60/60/24/365.25} years old!"
-
+	(age/60/60/24/365.25).round.times do
+		puts "SPANK"
+	end
 end
 
-happy_birthday(1989, 9, 11) # => nil
-# >> You are 825873987.988976 seconds old, which is 13764566.466482934 minutes,
-# >>  229409.4411080489 hours,
-# >>  9558.72671283537 days
-# >>  or 26.170367454716963 years old!
+happy_birthday(2010, 9, 11) # => 5
+# >> SPANK
+# >> You are 163207123.314425 seconds old, which is 2720118.7219070834 minutes,
+# >>  45335.31203178472 hours,
+# >>  1888.9713346576966 days
+# >>  or 5.171721655462551 years old!
+# >> SPANK
+# >> SPANK
+# >> SPANK
+# >> SPANK
+# >> SPANK

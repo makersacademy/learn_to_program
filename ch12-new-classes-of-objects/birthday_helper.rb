ce6be952758e704fd1bb0a7db3fa107f1c315e9c
edwardkerry <edwardkerry@gmail.com>
2015-11-12 08:45:59 +0000

    ch12 birthday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,16 @@
-# your code here
+def birthday_helper(filename)
+
+  birthdays = {}
+  list = File.read filename
+
+  list.each_line do |line|
+    name, date, year = line.split(',')
+    birthdays[name] = (date + ', ' +  year.chomp)     
+  end 
+ 
+  puts "What is your name?"
+  name = gets.chomp
+  dob = birthdays[name]
+  puts "Hi #{name}, your date of birth is #{dob}.\n" 
+  
+end

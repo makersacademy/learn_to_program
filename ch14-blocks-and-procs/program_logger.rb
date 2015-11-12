83dfd898dd0096e1a56db411ddd1ed82e68f510c
emmabeynon <emma.beynon@gmail.com>
2015-11-12 14:21:11 +0000

    Solution: ch14-blocks-and-procs/program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+puts "Beginning \"#{desc}\"..."
+returned = block.call
+puts "...\"#{desc}\" finished, returning: #{returned}"
 end
+
+block2 = Proc.new { 2 + 3 }
+block3 = Proc.new {'I like Thai food!'}
+block1 = Proc.new do
+  program_log 'some little block', &block2
+  program_log 'yet another block', &block3
+  false
+end
+
+program_log 'outer block', &block1

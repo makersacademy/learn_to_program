8c94871ae007d7e9640f8b216010f8555ec1de1a
jborrell <james.borrell92@gmail.com>
2015-11-12 21:49:26 +0000

    Ch10 Partial

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,39 @@
 def roman_numeral num
-  # your code here
+  romanNumeral = ""
+    
+    thousands = (num / 1000)
+    hundreds = (num % 1000 / 100)
+    tens = (num % 100 / 10)
+    ones = (num % 10)
+    
+    romanNumeral << ("M" * thousands)
+    
+    if hundreds == 9
+        romanNumeral << "CM"
+    elsif hundreds == 4
+        romanNumeral << "CD"
+    else
+        romanNumeral << ("D" * (num % 1000 / 500))
+        romanNumeral << ("C" * (num % 500 / 100))
+    end
+    
+    if tens == 9
+        romanNumeral << "XC"
+    elsif tens == 4
+        romanNumeral << "XL"
+    else
+        romanNumeral << ("L" * (num % 100 / 50))
+        romanNumeral << ("X" * (num % 50 / 10))
+    end
+    
+    if ones == 9
+        romanNumeral << "IX"
+    elsif ones == 4
+        romanNumeral << "IV"
+    else
+        romanNumeral << ("V" * (num % 10 / 5))
+        romanNumeral << ("I" * (num % 5))
+    end
+    
+    return romanNumeral
 end

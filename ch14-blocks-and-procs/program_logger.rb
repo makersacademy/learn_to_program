9b70a6259291cb828757d17608362b4e5118dd15
Chris Wynne <chris.wynndow@gmail.com>
2015-11-12 13:53:20 +0000

    Adds solution for ch14 program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,17 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  output = block.call
+  puts "...\"#{desc}\" finished, returning: #{output}"
 end
+
+=begin
+program_log "outer block" do
+  program_log "some little block" do
+    10 / 2
+  end
+  program_log "yet another block" do
+    "I like Thai food!"
+  end
+  false
+end
+=end

bce2cb631a61cbe3f7c6c457075715dcfd1dde96
Charlie Person <charlie.person@hyperisland.se>
2015-11-12 20:03:36 +0000

    ninety_nine_bottles_of_beer.rb

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,21 @@
-# your code here
+require './english_number.rb'
+
+def song (n)
+
+  def bottles (n)
+    if n == 1
+       "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  while n > 0
+    puts "#{english_number(n).capitalize} #{bottles(n)} of beer on the wall, ",
+         "#{english_number(n)} #{bottles(n)} of beer, ",
+         "take one down, pass it around, ",
+         "#{english_number(n - 1)} #{bottles(n-1)} of beer on the wall."
+
+         n -= 1
+   end
+ end

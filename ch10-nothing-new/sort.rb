1f4050a5720832b374d493812e85d470d5965d47
gareth4192 <gareth-123@live.com>
2015-11-12 13:14:41 +0000

    sort solution added

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,24 @@
 def sort arr
   # your code here
+rec_sort arr, []
 end
+
+def rec_sort(unsorted, sorted)
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |i|
+    if i < smallest
+      still_unsorted.push smallest
+      smallest = i
+    else
+      still_unsorted.push i
+    end
+  end
+  sorted.push smallest
+  rec_sort(still_unsorted, sorted)
+end 

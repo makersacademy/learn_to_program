017f6ca3d680b99c54fd1ccf815be2cc191f8214
Chris Wynne <chris.wynndow@gmail.com>
2015-11-12 13:38:05 +0000

    Updates grandfather clock solution

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,4 +1,8 @@
 def grandfather_clock &block
+  if Time.new.hour == 0
+    hour = 12
+  else
   Time.new.hour > 12 ? hour = Time.new.hour - 12 : hour = Time.new.hour
+  end
   hour.times(&block)
 end

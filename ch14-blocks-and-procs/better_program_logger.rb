b98c2dee91cf215919d2df752ed1dfcccd27f8c9
Chris Wynne <chris.wynndow@gmail.com>
2015-11-12 15:55:52 +0000

    Adds solution to ch14 better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,20 @@
-def log desc, &block
-  # your code here
+def better_log desc, &block
+$indent ? $indent : $indent = 0
+  puts "#{" " * $indent}Beginning \"#{desc}\"..."
+    $indent += 1
+  output = block.call
+    $indent -= 1
+  puts "#{" " * $indent}...\"#{desc}\" finished, returning: #{output}"
 end
+
+=begin
+better_log "outer block" do
+  better_log "some little block" do
+    10 / 2
+  end
+  better_log "yet another block" do
+    "I like Thai food!"
+  end
+  false
+end
+=end

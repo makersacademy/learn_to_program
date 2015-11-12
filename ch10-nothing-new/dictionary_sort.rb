03d47d73d1dad49cd0b8849dd07b3f481371e770
Charlie Person <charlie.person@hyperisland.se>
2015-11-12 20:01:18 +0000

    dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,16 @@
 def dictionary_sort arr
-  # your code here
+  outcome = []
+  while arr.length != 0
+    first = arr.first
+    pos = 0
+    arr.each_with_index do |item, index|
+      if item.downcase < first.downcase
+        first = item
+        pos = index
+      end
+    end
+      arr.delete_at(pos)
+      outcome << first
+  end
+    outcome
 end

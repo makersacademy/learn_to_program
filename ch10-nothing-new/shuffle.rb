785703bdbc2ac6d07f794597e25ed2e888849049
Tristan Crossley <trcrossley@gmail.com>
2015-11-13 16:37:24 +0000

    Commit 2: ch10

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,22 @@
 def shuffle arr
-  # your code here
+  shuffle = []
+
+  while arr.length > 0
+    rand_index = rand(arr.length)
+    curr_index = 0
+    new_arr = []
+
+    arr.each do |x|
+      if curr_index == rand_index
+        shuffle.push item
+      else
+        new_arr.push x
+      end
+
+      curr_index = curr_index + 1
+    end
+
+    arr = new_arr
+  end
+  shuffle
 end

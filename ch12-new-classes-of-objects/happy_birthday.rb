99f25378069df03fea124ab5da735ce4a873f469
Tom <bradley.tom.r@gmail.com>
2015-11-13 12:24:23 +0000

    Ch12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,18 +1,18 @@
 puts "What year were you born?"
-  b_year = gets.chomp.to_i
+year = gets.chomp.to_i
 
-puts "What month were you born?"
-  b_month = gets.chomp.to_i
+puts "What number month were you born?"
+month = gets.chomp.to_i
 
-puts "Which day were you born?"
-  b_day = gets.chomp.to_i
+puts "What day of the month were you born?"
+day = gets.chomp.to_i
 
-b = Time.local(b_year, b_month, b_day)
-t = Time.new
+born = Time.local(year, month, day)
+current_time = Time.new
 
 age = 1
 
-while Time.local(b_year + age, b_month, b_day) <= t
+while Time.local(year + age, month, day) <= current_time
   puts 'SPANK!'
-  age = age + 1
+  age += 1
 end

14a55814de8993818c0d02f8195ce2138195e4c9
michaellennox <michaelctlennox@gmail.com>
2015-11-13 14:30:27 +0000

    modified die.rb

--- ch13-creating-new-classes/die.rb
+++ ch13-creating-new-classes/die.rb
@@ -1,13 +1,19 @@
 class Die
 
   def roll
-    1 + rand(6)
+    @number_showing = 1 + rand(6)
+  end
+
+  def showing
+    @number_showing
   end
 
 end
 
-dice = [Die.new, Die.new]
-
-dice.each do |die|
-  puts die.roll
-end
+die = Die.new
+die.roll
+puts die.showing
+puts die.showing
+die.roll
+puts die.showing
+puts die.showing

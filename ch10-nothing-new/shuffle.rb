03ca7835d02ad821112938322797f7244f852793
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-13 20:20:11 +0000

    Chapter 10 completed

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,7 @@
 def shuffle arr
-  # your code here
+  shuffled_arr = []
+  arr.each do |item|
+    shuffled_arr << arr[rand((arr.length) -1)]
+  end
+  shuffled_arr
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
 def sort arr
-  # your code here
+  it_is_already_sorted = true
+  arr[0..-2].each_with_index do |item, index|
+    if arr[index] > arr[index+1]
+      it_is_already_sorted = false
+      arr[index], arr[index+1] = arr[index+1], arr[index]
+    end
+  end
+  sort arr unless it_is_already_sorted
+  arr.join(', ')
 end

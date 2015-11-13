6226ac3020aacf67cb7b399dcba5750bb6221939
Jelgar <jonathanelgardiner@gmail.com>
2015-11-13 22:20:24 +0000

    CH10 1

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,19 @@
 def shuffle arr
-  # your code here
+	shuffled_array = []
+	while arr.length > 0
+		random_index = rand(arr.length)
+		trash_arr = []
+		curr_index = 0
+
+		arr.each do |item|
+			if curr_index == random_index
+				shuffled_array.push item
+			else
+				trash_arr.push item
+			end
+			curr_index = curr_index + 1
+		end
+		arr = trash_arr
+	end
+	shuffled_array
 end

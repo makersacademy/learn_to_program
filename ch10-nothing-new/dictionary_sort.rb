6f5730d52711215144d79605d5a201cab6ac2248
gimi-q <med.batt.uk@gmail.com>
2015-11-13 13:55:23 +0000

    Pine_LTP: ch 10 answers

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,24 @@
-def dictionary_sort arr
-  # your code here
+
+def dictionary_sort arr 
+	sorted_array = []
+	
+	recursive_sort arr, sorted_array	
 end
+
+def recursive_sort unsorted_array, sorted_array 
+	
+	
+	match_count = unsorted_array.length
+		while sorted_array.length < match_count 
+
+			smallest_word = unsorted_array.min
+			sorted_array << smallest_word
+            unsorted_array.slice!(unsorted_array.index(smallest_word))
+			#unsorted_array.delete(smallest_word)
+			
+		end
+	sorted_array 
+		
+	
+end
+

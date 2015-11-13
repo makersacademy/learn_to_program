4c8c268437a648be798d399fb7297396e7cd34fe
michaellennox <michaelctlennox@gmail.com>
2015-11-13 19:24:02 +0000

    Added proc notes

--- ch14-blocks-and-procs/proc_notes.rb
+++ ch14-blocks-and-procs/proc_notes.rb
@@ -5,3 +5,13 @@ end
 toast.call
 toast.call
 toast.call
+
+# Proc is effectively an object that acts like a method. Can be stored or passed around
+
+do_you_like = Proc.new do |good_stuff|
+  puts "I *really* like #{good_stuff}"
+end
+
+#Can input parameters into a Proc
+do_you_like.call 'chocolate'
+do_you_like.call 'Ruby'

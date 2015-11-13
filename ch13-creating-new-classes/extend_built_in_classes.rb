de3dbcd94583883a91fba7a0115d9cb39acd6d89
Katie Smith <kls.smith@yahoo.co.uk>
2015-11-13 19:34:32 +0000

    Built in classes and baby dragon

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,33 @@
+class Array
+  def shuffle
+    new_arr = []
+    until self.empty?
+      pick = self[rand(self.length)]
+      new_arr << pick
+      self.delete(pick)
+    end
+    new_arr
+  end
+end
+
 class Integer
-  # your code here
+  def factorial
+    if self == 1
+      return 1
+    else
+      self * (self-1).factorial
+    end
+  end
+  
+  def to_roman
+    roman = String.new
+    roman << "M" * (self / 1000)
+    roman << "D" * (self % 1000 / 500)
+    roman << "C" * (self % 500 / 100)
+    roman << "L" * (self % 100 / 50)
+    roman << "X" * (self % 50 / 10)
+    roman << "V" * (self % 10 / 5)
+    roman << "I" * (self % 5 / 1)
+    roman
+  end 
 end
--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "Ok then, what's the dragon called? "
+name = gets.chomp.capitalize
+pet = Dragon.new(name)
+puts "Right then, select one of the following options: "
+
+while true
+  puts "feed, walk, put to bed, toss, rock, exit "
+  choice = gets.chomp
+  break if choice == "exit"
+  case choice
+    when "feed" then pet.feed
+    when "walk" then pet.walk
+    when "put to bed" then pet.put_to_bed
+    when "toss" then pet.toss
+    when "rock" then pet.rock
+    else puts "Sorry, I didn't understand your choice."
+  end
+end

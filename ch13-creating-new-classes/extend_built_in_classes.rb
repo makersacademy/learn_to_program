cadce2b26ba39ebd80fa73a7e61cdc8545970699
michaellennox <michaelctlennox@gmail.com>
2015-11-13 14:05:20 +0000

    Extended challenge - factorial & shuffleify added

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,34 @@
-class Integer
-  # your code here
+class Array
+
+  def shuffleify
+    remaining = self.length
+    returned = []
+    until remaining == 0
+      count = rand(remaining)
+      returned.push(self[count])
+      remaining -= 1
+      self[count] = self[remaining]
+      self.pop
+    end
+    returned
+  end
+
 end
+
+class Integer
+
+  def factorial
+    if self < 0
+      return "You can't take the factorial of a negative number!"
+    end
+
+    if self <= 1
+      1
+    else
+      self * (self-1).factorial
+    end
+  end
+
+end
+
+p 4.factorial

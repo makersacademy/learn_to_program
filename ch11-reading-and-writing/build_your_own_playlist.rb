2fdbb404c9faacd3b98a033d88275205079c4f79
gareth4192 <gareth-123@live.com>
2015-11-13 23:15:21 +0000

    own playlist solution added

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,15 @@
 # your code here
+
+def create_playlist
+Dir.chdir('/Users/garethmckenna/music/') #change working directory
+#find all mp3s to be moved
+music = Dir['./*.{MP3,mp3}']
+File.open 'Playlist.m3u'.shuffle, 'w' do |i|
+music.each do |a|
+  f.write "#{a} \n"
+  end
+end
+puts "Playlist created"
+end
+
+create_playlist

b54a179468718a2c526aed20f22ef11b82c50a67
Tristan Crossley <trcrossley@gmail.com>
2015-11-13 16:36:38 +0000

    Commit 1: ch09

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,13 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if reply == "yes"
+      return true
+    end
+    if reply == "no"
+      return false
+    end
+    puts "Please answer \"yes\" or \"no\"."
+  end
 end

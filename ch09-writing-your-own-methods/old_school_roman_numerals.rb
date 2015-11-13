b2c8a0de9cf26a6a9f591e05f21a36a034d1f2d6
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-13 13:09:13 +0000

    All ch09

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -6,5 +6,12 @@ def old_roman_numeral num
   tens = (num % 50 / 10)
   fives = (num % 10 / 5)
   ones = (num % 5)
-  in_romans = 'M' * thousands + 'D' * five_hundreds + 'C' * hundreds + 'L' * fifties + 'X' * tens + 'V' * fives + 'I' * ones
+
+  in_romans = 'M' * thousands
+  in_romans += 'D' * five_hundreds
+  in_romans += 'C' * hundreds
+  in_romans += 'L' * fifties
+  in_romans += 'X' * tens
+  in_romans += 'V' * fives
+  in_romans += 'I' * ones
 end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,38 @@
 def roman_numeral num
-  # your code here
+  thousands = (num / 1000)
+  five_hundreds = (num % 1000 / 500)
+  hundreds = (num % 1000 / 100)
+  special_hundreds = (num % 500 / 100)
+  fifties = (num % 100 / 50)
+  tens = (num % 100 / 10)
+  special_tens = (num % 50 / 10)
+  fives = (num % 10 / 5)
+  ones = (num % 10)
+  special_ones = (num % 5)
+
+  in_romans = 'M' * thousands
+  if hundreds == 9
+    in_romans += 'CM'
+  elsif hundreds == 4
+    in_romans += 'CD'
+  else
+    in_romans += 'D' * five_hundreds
+    in_romans += 'C' * special_hundreds
+  end
+  if tens == 9
+    in_romans += 'XC'
+  elsif tens == 4
+    in_romans += 'XL'
+  else
+    in_romans += 'L' * fifties
+    in_romans += 'X' * special_tens
+  end
+  if ones == 9
+    in_romans += 'IX'
+  elsif ones == 4
+    in_romans += 'IV'
+  else
+    in_romans += 'V' * fives
+    in_romans += 'I' * special_ones
+  end
 end

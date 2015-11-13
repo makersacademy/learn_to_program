1dbc54112a653c2658ba29da3f2d607bd08a0b67
edwardkerry <edwardkerry@gmail.com>
2015-11-13 20:00:32 +0000

    ch14 better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,29 @@
-def log desc, &block
-  # your code here
+$indent = 0
+
+def  better_log desc, &block
+  prefix = ' ' * $indent
+  
+  puts prefix + 'Beginning "' + desc + '"...'
+  
+  $indent += 1
+  
+  result = block.call
+  
+  $indent -= 1
+  
+ puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
+    
+end
+
+better_log 'outer block' do 
+   better_log 'some little block' do 
+        better_log 'teeny-tiny block' do
+            "lots of love" * 1
+            end
+          41 + 1
+        end
+      better_log 'yet another block' do
+      "I Like Indian food!" * 1
+    end
+  9 == 9
 end

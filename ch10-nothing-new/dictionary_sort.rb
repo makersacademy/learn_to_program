23ece6acebaf60cef2faaa2e6e421f41f2652d9e
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-13 00:38:53 +0000

    chapter 10 dictionary & shuffle

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,4 @@
 def dictionary_sort arr
   # your code here
+  (arr.min(arr.length) { |a, b| a.downcase <=> b.downcase })
 end
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,4 @@
 def shuffle arr
   # your code here
+  arr.sort_by { rand }
 end

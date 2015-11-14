55f16a92e500b20be7e7a2db632b28dcb00c72dd
camilla_foodity <camillacolser@hotmail.com>
2015-11-14 22:59:27 +0000

    FIxed Ch 9 Old roman numbers

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,55 +1,55 @@
 def old_roman_numeral num
 
     if num >= 1000
-      m = num / 1000
-      remainder = num % 1000
+      emmes = num / 1000
+      remainder = num - emmes*1000
     else
-      m = 0
+      emmes = 0
       remainder = num
     end
 
     if remainder >= 500
-      d = remainder / 500
-      remainder = remainder % 500
+      ds = remainder / 500
+      remainder = remainder - ds*500
     else
-      d = 0
+      ds = 0
     end
 
     if remainder >= 100
-      c = remainder / 100
-      remainder = remainder % 100
+      cs = remainder / 100
+      remainder = remainder - cs*100
     else
-      c = 0
+      cs = 0
     end
 
     if remainder >= 50
-      l = remainder / 50
-      remainder = remainder % 50
+      elles = remainder / 50
+      remainder = remainder - elles*50
     else
-      l = 0
+      elles = 0
     end
 
     if remainder >= 10
-      x = remainder / 10
-      remainder = remainder % 10
+      xes = remainder / 10
+      remainder = remainder - xes*10
     else
-      x = 0
+      xes = 0
     end
 
     if remainder >= 5
-      v = remainder / 5
-      remainder = remainder % 5
+      vs = remainder / 5
+      remainder = remainder - vs*5
     else
-      v = 0
+      vs = 0
     end
 
-    i = remainder / 1
+    is = remainder / 1
 
-  "M"*m+"D"*d+"C"*c+"L"*l+"X"*x+"V"*v+"I"*i
+  "M"*emmes+"D"*ds+"C"*cs+"L"*elles+"X"*xes+"V"*vs+"I"*is
 
 end
 
-
-puts roman_numeral 365
-puts roman_numeral 20
-puts roman_numeral 120
+# puts old_roman_numeral 4
+# puts old_roman_numeral 365
+# puts old_roman_numeral 20
+# puts old_roman_numeral 120

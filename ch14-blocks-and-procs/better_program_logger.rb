7e5481b76071946ccf1c0b8fdd5875a542eff0bb
michaellennox <michaelctlennox@gmail.com>
2015-11-14 17:11:23 +0000

    Added the better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,18 @@
-def log desc, &block
+def better_log desc, &block
   # your code here
+end
+
+
+better_log 'outer block' do
+  better_log 'some little block' do
+    better_log 'teeny-tiny block' do
+      'lOtS oF lOVe'.downcase
+    end
+    7 * 3 * 2
+  end
+
+  better_log 'yet another block' do
+    '!doof naidnI evol I'.reverse
+  end
+  '0' == "0"
 end

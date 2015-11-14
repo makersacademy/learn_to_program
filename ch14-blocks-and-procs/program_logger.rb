2121bd2b9491e4aec6776c246a5f186e63fcac92
michaellennox <michaelctlennox@gmail.com>
2015-11-14 17:09:18 +0000

    Removed test case from challenge

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -3,15 +3,3 @@ def program_log desc, &block
   result = block.call
   puts "...\"#{desc}\" finished, returning: #{result}"
 end
-
-program_log 'outer block' do
-  program_log 'some little block' do
-    1**1 + 2**2
-  end
-
-  program_log 'yet another block' do
-    '!doof iahT ekil I'.reverse
-  end
-
-  '0' == 0
-end

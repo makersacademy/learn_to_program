9ad3df6680093f60ea7178a216f9ad40d3862f3d
hibreez <4ngus.whitehead@gmail.com>
2015-11-14 05:47:56 +0000

    ch9 complete

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,9 @@
 def old_roman_numeral num
-  # your code here
+  numerals = [[1000, "M"], [500, "D"], [100, "C"], [50, "L"],
+    [10, "X"], [5, "V"], [1, "I"]]
+  numerals.map { |numeral|
+    multi = num / numeral[0]
+    num -= (numeral[0] * multi)
+    numeral[1] * multi
+  }.join('')
 end

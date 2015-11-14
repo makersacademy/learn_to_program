286ae0bfbbbf37047b76b7b358ccff74cd8e46d9
hibreez <4ngus.whitehead@gmail.com>
2015-11-14 05:52:33 +0000

    complete sorting + shuffling exercises

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,16 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arr)
+  rec_sort(arr, [])
+end
+
+def rec_sort(arr, sorted)
+  unsorted = []
+  arr.each{ |e|
+    bigger = 0
+    arr.each { |f|
+      bigger += 1 if e.downcase > f.downcase
+    }
+    bigger == 0 ? sorted << e : unsorted << e
+  }
+  return sorted if unsorted == []
+  rec_sort(unsorted, sorted)
 end

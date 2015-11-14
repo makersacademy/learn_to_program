b499d707ada53983ec66494d1209b36f93ef65a3
michaellennox <michaelctlennox@gmail.com>
2015-11-14 16:53:13 +0000

    Updated grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,5 +1,7 @@
 def grandfather_clock &block
   current_hour = Time.new.hour
+  current_hour -= 12 if current_hour > 12
+  current_hour = 12 if current_hour == 0
   current_hour.times { block.call }
 end
 

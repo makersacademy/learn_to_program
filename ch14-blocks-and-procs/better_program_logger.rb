3e9cfb53f37bd11412c2692ba06f2affca44e8b1
michaellennox <michaelctlennox@gmail.com>
2015-11-14 17:15:08 +0000

    Completed better logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,18 +1,10 @@
+$nesting_depth = 0
+
 def better_log desc, &block
-  # your code here
-end
-
-
-better_log 'outer block' do
-  better_log 'some little block' do
-    better_log 'teeny-tiny block' do
-      'lOtS oF lOVe'.downcase
-    end
-    7 * 3 * 2
-  end
-
-  better_log 'yet another block' do
-    '!doof naidnI evol I'.reverse
-  end
-  '0' == "0"
+  indent = " " * $nesting_depth
+  puts indent + "Beginning \"#{desc}\"..."
+  $nesting_depth += 1
+  result = block.call
+  $nesting_depth -= 1
+  puts indent + "...\"#{desc}\" finished, returning: #{result}"
 end

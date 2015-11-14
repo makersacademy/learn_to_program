8e52ec2de0f9c5c66a85e734ad8c35243df9fdc7
Richard Moss <richard@englishtutorslive.com>
2015-11-14 17:42:20 +0000

    updated english number for zillions

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,72 +1,96 @@
 def english_number number
+  if number < 0 # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
 
-  if number < 0
-      return 'Please enter a number that isn\'t negative.'
-    end
-    if number == 0
-      return 'zero'
-    end
+  num_string = ''
 
-    numString = ''
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
 
-    onesPlace = ['one',     'two',       'three',    'four',     'five',
-                 'six',     'seven',     'eight',    'nine']
-    tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
-                 'sixty',   'seventy',   'eighty',   'ninety']
-    teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
-                 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
 
 
-    left  = number
-    write = left/1000
-    left = left - write*1000
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
 
-    if write > 0
-      thousands = englishNumber write
-      numString = numString + thousands + ' thousand'
+  zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
 
-      if left > 0
-        numString = numString + ' '
-      end
-    end
+  left = number
 
-    write = left/100
-    left  = left - write*100
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base
 
     if write > 0
       # recursion!
-      hundreds  = englishNumber write
-      numString = numString + hundreds + ' hundred'
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
 
       if left > 0
-        numString = numString + ' '
+        num_string = num_string + ' '
       end
     end
-
-    write = left/10
-    left  = left - write*10
-
-    if write > 0
-      if ((write == 1) and (left > 0))
-
-        numString = numString + teenagers[left-1]
-
-        left = 0
-      else
-        numString = numString + tensPlace[write-1]
-      end
-
-      if left > 0
-        numString = numString + '-'
-      end
-    end
-
-    write = left
-    left  = 0
-
-    if write > 0
-      numString = numString + onesPlace[write-1]
-    end
-
-    numString
   end
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+
+      num_string = num_string + teenagers[left-1]
+
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+
+    end
+
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+
+  # return the string! 
+  num_string
+end

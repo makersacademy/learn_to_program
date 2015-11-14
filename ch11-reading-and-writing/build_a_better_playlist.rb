c9cf7b267d05f99c1e315f50e7716f2d17fd6841
Richard Moss <richard@englishtutorslive.com>
2015-11-14 22:18:59 +0000

    playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,33 @@
 def music_shuffle filenames
-  # your code here
+
+	filenames.sort!
+	len = filenames.length
+
+	2.times do
+		left = 0
+		right = len/2
+		shuf = Array.new
+
+		while shuf.length < len
+			if shuf.length%2 == 0
+				shuf.push(filnames[right])
+				right += 1
+			else
+				shuf.push(filenames[left])
+				left += 1
+			end
+		end
+		filenames = shuf
+	end
+
+	arr = Array.new
+	cut = rand(len)
+	index 0
+
+	while index < len
+		arr.push(filenames[(index+cut)%len])
+		index += 1
+	end
+
+	arr
 end

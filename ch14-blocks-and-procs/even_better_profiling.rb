b0f75369456ac7d1bff088c06f3f48d331a65be4
jborrell <james.borrell92@gmail.com>
2015-11-14 10:44:13 +0000

    Ch14

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,6 @@
 def profile block_description, &block
-  # your code here
+        start_time = Time.new
+        block.call
+        duration = Time.new - start_time
+            puts "#{block_description}: #{duration} seconds"
 end

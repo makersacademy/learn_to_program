27b87dbe796cd12ded9a7644f88d1c23e02f2f74
William Bautista <williambautista@gmail.com>
2015-11-14 11:27:05 +0000

    "Adding challenge ch09-2 and ch09-3"

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,20 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral (num)
+  qty_1000s = (num / 1000)
+  qty_500s = (num % 1000 / 500)
+  qty_100s = (num % 500 / 100)
+  qty_50s = (num % 100 / 50)
+  qty_10s = (num % 50 / 10)
+  qty_5s = (num % 10 / 5)
+  qty_1s = (num % 5)
+  "#{'M' * qty_1000s}#{'D' * qty_500s}#{'C' * qty_100s}#{'L' * qty_50s}#{'X' * qty_10s}#{'V' * qty_5s}#{'I' * qty_1s}"
 end
+
+
+# p old_roman_numeral (1)
+# p old_roman_numeral (4)
+# p old_roman_numeral (9)
+# p old_roman_numeral (15)
+# p old_roman_numeral (24)
+# p old_roman_numeral (60)
+# p old_roman_numeral (100)
+# p old_roman_numeral (14789)
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,40 @@
-def roman_numeral num
-  # your code here
+def old_roman_numeral (num)
+  qty_1000s = (num / 1000)
+  qty_500s = (num % 1000 / 500)
+  qty_100s = (num % 500 / 100)
+  qty_100s_exception = (num % 500 / 100)
+  qty_50s = (num % 100 / 50)
+  qty_10s = (num % 50 / 10)
+  qty_10s_exception = (num % 50 / 10)
+  qty_5s = (num % 10 / 5)
+  qty_1s = (num % 5)
+  qty_1s_exception = (num % 5)
+
+  arabic = 'M' * qty_1000s
+  if qty_100s == 9
+    arabic += 'CM'
+  elsif qty_100s == 4
+    arabic += 'CD'
+  else
+    arabic += 'D' * five_hundreds
+    arabic += 'C' * special_hundreds
+  end
+  if qty_10s == 9
+    arabic += 'XC'
+  elsif qty_10s == 4
+    arabic += 'XL'
+  else
+    arabic += 'L' * qty_50s
+    arabic += 'X' * qty_10s_exception
+  end
+  if qty_1s == 9
+    arabic += 'IX'
+  elsif qty_1s == 4
+    arabic += 'IV'
+  else
+    arabic += 'V' * qty_5s
+    arabic += 'I' * qty_1s
+  end
 end
+
+# Took me a while to find out that I needed some exceptions to pass the correct arabic number

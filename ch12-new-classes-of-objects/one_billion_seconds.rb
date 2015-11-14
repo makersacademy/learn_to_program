73c59241a0ed924f7ad82e47bb48f7484c1fa41b
Andrew Htun <andy_htun@hotmail.com>
2015-11-14 13:19:02 +0000

    updated one_billion_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,3 +1,3 @@
 birth_time = Time.gm(1990, 06, 29, 13, 00, 36)
-billion_seconds_old = birth_time + 1000000000
-puts billion_seconds_old
+date_billion_seconds_old = birth_time + 1000000000
+puts date_billion_seconds_old

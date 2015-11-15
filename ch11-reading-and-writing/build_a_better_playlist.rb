8d064bc21901e3aac0c58ca2237feff74ab9e177
Charlie Person <charlie.person@hyperisland.se>
2015-11-15 16:29:46 +0000

    build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,19 @@
-def music_shuffle filenames
-  # your code here
+def music_shuffle (songs)
+  def shuffle arr
+    arr.each_with_index do |item, index|
+      arr.insert(rand(arr.length), arr.delete_at(index))
+    end
+  end
+
+  Dir.chdir('.')
+
+  tunes_names = Dir['./**/*.mp3']
+
+  shuffle(tunes_names)
+
+  File.open 'tunes.m3u', 'w' do |f|
+    tunes_names.each do |mp3|
+      f.write mp3 + "\n\n"
+    end
+  end
 end

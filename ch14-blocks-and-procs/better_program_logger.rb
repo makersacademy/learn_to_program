2cedc556fdb9d022c02c75eeb8b458c472cd0213
Sara Tateno <sara.tateno@gmail.com>
2015-11-15 23:12:13 +0900

    Chap 14 better program logger solution

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,27 @@
-def log desc, &block
-  # your code here
+$nesting = 0
+
+def better_log desc, &block
+  indent = " " * $nesting
+  puts indent + "Beginning #{desc.inspect}..."
+  $nesting += 1
+  result = block[]
+  $nesting -= 1
+  puts indent + "...#{desc.inspect} finished, returning: #{result}"
+end
+
+
+
+better_log "outer block" do
+  better_log "some little block" do
+      better_log "teeny-tiny block" do
+        "lOts of lOVe".downcase
+      end
+  7 * 3 * 2
+  end
+
+  better_log "yet another block" do
+    "I love Indian food!"
+  end
+
+  "I love Indian food" == "I love Indian food"
 end

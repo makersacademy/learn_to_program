a4ab8fc905afb0ae7326250b89646acc4b05b4dc
William Bautista <williambautista@gmail.com>
2015-11-15 16:56:24 +0000

    "Adding Challenge CH12 - happy_birthday"

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,21 @@
-# your code here
+def happy_birthday
+  puts 'Enter the year you born:'
+  year = gets.chomp.to_i
+
+  puts 'Enter the month you were born, eg, Jan'
+  month = gets.chomp.downcase
+  months = {jan => 1, feb => 2, mar =>3, apr => 4, may => 5, jun => 6, jul => 7,
+            aug => 8, sep => 8, oct => 9, nov => 11, dec => 12}
+  month = months[month]
+
+  puts 'What day of the month were you born?'
+  day = gets.chomp.to_i
+
+  birthday = Time.gm(year, month, day)
+
+  actual_time = Time.now.utc.to_date
+
+  final_age = actual_time.year - birthday.year - ((actual_time.month > birthday.month || (actual_time.month == birthday.month && now.day >= dob.day)) ? 0 : 1)
+
+  final_age
+end

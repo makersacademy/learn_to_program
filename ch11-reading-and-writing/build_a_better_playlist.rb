85f7ed34f776dd39bd52f129295c28d2b8e01db2
William Bautista <williambautista@gmail.com>
2015-11-15 13:26:23 +0000

    "Adding challenge CH11 building_a_better_playlist"

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,22 @@
-def music_shuffle filenames
-  # your code here
-end
+# I found out an algorithm called Knuth-Fischer-Yates or Knuth shuffle,
+# which main goal is to shuffle elements within a finite set of elements in an Array.
+
+  def music_shuffle filenames
+    size = filenames.size
+
+      until size == 0
+        k = rand(size)
+        size -= 1
+        filenames[size], filenames[k] = filenames[k], filenames[size]
+      end
+
+      filenames
+
+  end
+
+
+# test_array = *(1..10)
+# songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+#      'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+#
+# p music_shuffle(songs)

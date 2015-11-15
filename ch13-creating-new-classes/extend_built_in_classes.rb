a131496ae5c247bc95c2f8ea933547b003608677
Tristan Crossley <trcrossley@gmail.com>
2015-11-15 13:16:57 +0000

    Commit 8: ch13 extend_built_in_classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,19 @@
 class Integer
-  # your code here
+
+  def factorial
+    return 1 if self == 1
+    self * (self-1).factorial
+  end
+
+  def to_roman
+    roman = ""
+    roman = roman + 'M' * (self / 1000)
+    roman = roman + 'D' * (self % 1000 / 500)
+    roman = roman + 'C' * (self % 500 / 100)
+    roman = roman + 'L' * (self % 100 / 50)
+    roman = roman + 'X' * (self % 50 / 10)
+    roman = roman + 'V' * (self % 10 / 5)
+    roman = roman + 'I' * (self % 5 / 1)
+    roman
+  end
 end

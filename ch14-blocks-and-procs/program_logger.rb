2adced8e3364a6b347e1ce5cb2dc71d4a3ff2deb
Sara Tateno <sara.tateno@gmail.com>
2015-11-15 23:02:16 +0900

    Chap 14 program logger solution

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
+end
+
+program_log "outer block" do
+  program_log "some little block" do
+      2 + 3
+  end
+  program_log "yet another block" do
+    "I like Thai food!"
+  end
+
+  "My fave food" == "Chinese"
 end

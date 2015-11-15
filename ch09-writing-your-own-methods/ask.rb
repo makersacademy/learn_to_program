730044793a3926a16ea2c1f1c90cb99b141ce08c
Charlie Person <charlie.person@hyperisland.se>
2015-11-15 16:20:43 +0000

    ask.rb testing CI

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -2,12 +2,12 @@ def ask question
   while true
     puts question
     reply = gets.chomp.downcase
-    if reply == 'yes'
-      return true
-    elsif reply == 'no'
-      return false
+
+    if (reply == 'yes' || reply == 'no')
+      reply == 'yes' ? (return true) : (return false)
     else
       puts 'Please answer "yes" or "no".'
     end
+
   end
 end

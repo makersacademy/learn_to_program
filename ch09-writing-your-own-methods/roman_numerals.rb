51bd472d28b427781cfa322f4fad5b58801a6925
Charlie Person <charlie.person@hyperisland.se>
2015-11-15 18:37:38 +0000

    roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -10,7 +10,7 @@ def roman_numeral num
     @num = @left
   end
 
-  hash = {"M"=>1000, "D"=>500, "C"=>500, "L"=>50, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1}
+  hash = {"M"=>1000, "D"=>500, "C"=>100, "L"=>50, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1}
   hash.each {|key, value| get_it(key, value)}
 
   @string

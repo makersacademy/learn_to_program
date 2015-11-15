d8e89aa6b54216428e541f70d096df775157848a
Tony Young <ty_training@hotmail.co.uk>
2015-11-15 20:43:52 +0000

    ch10 dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,20 @@
-def dictionary_sort arr
+def dictionary_sort(arr)
   # your code here
+  return arr if arr.length <= 1
+    new_arr = ""
+
+    compare_word = arr.pop
+    less_word = arr.select {|w| w.downcase < compare_word.downcase}
+    more_word = arr.select {|w| w.downcase >= compare_word.downcase}
+
+
+    dictionary_sort(less_word) + [compare_word] + dictionary_sort(more_word)
+
+
 end
+
+
+words = ['tony', 'Chris', 'Adam', 'sally', 'john', 'zebedee', 'asheley']
+word = []
+p dictionary_sort(words)
+p dictionary_sort(word)

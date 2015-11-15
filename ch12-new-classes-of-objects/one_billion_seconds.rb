6f3e2f7c1654cc46d995534bf7d7767803072fae
Tristan Crossley <trcrossley@gmail.com>
2015-11-15 13:02:38 +0000

    Commit 7: ch12 update

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,3 @@
-puts
+def one_billion_seconds
+  Time.local(1987, 8, 9) + 10**12
+end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,31 @@
 def roman_to_integer roman
-  # your code here
+  roman_numerals = {
+    "m" => 1000,
+    "d" => 500,
+    "c" => 100,
+    "l" => 50,
+    "x" => 10,
+    "v" => 5,
+    "i" => 1 }
+
+  total = 0
+  previous = 0
+
+  index = roman.length - 1
+  while index >= 0
+    c = roman[index].downcase
+    index = index - 1
+    value = roman_numerals[c]
+    if !value
+      puts "This is not a valid roman numeral!"
+      return
+    end
+    if value < previous
+      value = value * -1
+    else
+      previous = value
+    end
+    total = total + value
+  end
+  total
 end

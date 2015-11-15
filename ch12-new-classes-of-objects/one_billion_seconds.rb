f24199df95e297b6a0fb67973533cf467223b95e
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-15 18:05:57 +0000

    one billion secs

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,5 +1,10 @@
 # your code here
 
-puts Time.local(1985, 4, 8) + 1_000_000_000
-# >> 1985-04-08 00:00:00 +0100
+def one_billion_seconds
+	puts Time.local(1985, 4, 8) + 1_000_000_000
+end
+
+puts one_billion_seconds
 # >> 2016-12-15 00:46:40 +0000
+# >> 2016-12-15 00:46:40 +0000
+# >> 

8786239f5061259e8c9c04ee933511d266a2c5d1
William Bautista <williambautista@gmail.com>
2015-11-15 17:17:33 +0000

    Adding Challenge CH12 happy_birthday refactored

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -2,20 +2,24 @@ def happy_birthday
   puts 'Enter the year you born:'
   year = gets.chomp.to_i
 
-  puts 'Enter the month you were born, eg, Jan'
+  puts "Enter the month you were born, eg, Jan"
   month = gets.chomp.downcase
-  months = {jan => 1, feb => 2, mar =>3, apr => 4, may => 5, jun => 6, jul => 7,
-            aug => 8, sep => 8, oct => 9, nov => 11, dec => 12}
-  month = months[month]
-
-  puts 'What day of the month were you born?'
+  puts "What day of the #{month.capitalize} were you born:"
   day = gets.chomp.to_i
 
-  birthday = Time.gm(year, month, day)
+  months = {'jan' => 1, 'feb' => 2, 'mar' =>3, 'apr' => 4, 'may' => 5, 'jun' => 6, 'jul' => 7,
+            'aug' => 8, 'sep' => 8, 'oct' => 9, 'nov' => 11, 'dec' => 12}
+  month = months[month].to_i
 
-  actual_time = Time.now.utc.to_date
+
+  actual_time = Time.now.utc
+  birthday = Time.utc(year, month, day)
+
 
   final_age = actual_time.year - birthday.year - ((actual_time.month > birthday.month || (actual_time.month == birthday.month && now.day >= dob.day)) ? 0 : 1)
 
-  final_age
+  final_age.times {puts "SPANK!"}
 end
+
+
+happy_birthday

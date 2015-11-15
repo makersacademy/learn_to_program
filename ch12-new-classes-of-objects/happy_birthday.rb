cf2b55b1da9843eb6fd721816e6efc62d816f026
Tony Young <ty_training@hotmail.co.uk>
2015-11-15 22:48:28 +0000

    ch12 billion seconds and happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp.to_i
+puts "What month?"
+month = gets.chomp.to_i
+puts "What day?"
+day = gets.chomp.to_i
+
+bday = Time.local(year,month,day)
+now = Time.now
+
+count = 1
+while Time.local(year + count, month, day) <= now
+  puts 'SPANK!'
+  count += 1
+end

7277737ed86b5371c9147845ecad76bf89f88b12
Richard Moss <richard@englishtutorslive.com>
2015-11-15 22:15:43 +0000

    tyring for rspec pass

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,25 +1,26 @@
-def roman n
+def roman_numeral(num)
 
-		roman_numerals = {
-		1000 => "M",
-		900 => "CM",
-		500 => "D",
-		400 => "CD",
-		100 => "C",
-		90 => "XC",
-		50 => "L",
-		40 => "XL",
-		10 => "X",
-		9 => "IX",
-		5 => "V",
-		4 => "IV",
-		1 => "I",
-	}
+    numeral_match_key = {
+    1000 => "M",
+     900 => "CM",
+     500 => "D",
+     400 => "CD",
+     100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
+     }
 
-		roman = ""
-		roman_numerals.each do |decimal, letter|
-			roman << letter * (n / decimal)
-			n = n % decimal
-		end
-		roman
+    finished_r_numeral = ""
+    numeral_match_key.each do |x, correct_numeral|
+      finished_r_numeral << correct_numeral*((num) / x  )
+
+      num = num % x
+    end
+    finished_r_numeral
 end

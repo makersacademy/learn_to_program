22e9d135488643443750a397bf967cbaa783d0a8
jborrell <james.borrell92@gmail.com>
2015-11-15 18:32:09 +0000

    Playlist & Program logger

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,9 @@
 def music_shuffle(playlist)
-    playlist.shuffle
+    shuffled = Array.new
+    music = playlist.shuffle
+    until music == [] do
+    x = music.pop
+    shuffled.push(x)
+    end
+    return shuffled
 end

0ba7442169b4ca5ff4526ae87372dde5b31880f6
Kirk Smith <kirks@hotmail.co.uk>
2015-11-15 00:34:25 +0000

    ch10-nothing-new

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,7 @@
-def sort arr
-  # your code here
+def sort (arr)
+  return arr if arr.length <= 1
+  middle = arr.pop
+  less = arr.select{|x| x < middle}
+  more = arr.select{|x| x >= middle}
+  sort(less) + [middle] + sort(more)
 end

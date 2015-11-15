fe5e7caaa0a8dc7c73e38eaa4d1d2a26d4105ca2
Adam Lancaster <lancaster28@hotmail.com>
2015-11-15 23:32:23 +0000

    updates to pass travis CI

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,5 @@
+=begin
+
 def music_shuffle filenames
 
 	filenames.each {|x|  split_filenames << x.split("/") }
@@ -7,4 +9,43 @@ def music_shuffle filenames
 	File.open filename 'w' do |f|
 		f.puts playlist
 	end
+end
+
+=end 
+
+def music_shuffle filenames
+
+  filenames = filenames.sort
+  len = filenames.length
+
+  2.times do
+    l_idx = 0 # index of next card in left pile
+    r_idx = len/2 # index of next card in right pile
+    shuf = []
+   
+    while shuf.length < len
+      if shuf.length%2 == 0
+      
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+       
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+
+    filenames = shuf
+  end
+  
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+  
+  arr
 end

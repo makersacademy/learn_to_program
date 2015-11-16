cabfa0dbe8ac24005642c5c95020a928d4d3c01e
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-16 11:42:12 +0000

    partying romans

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,31 @@
 def roman_to_integer roman
   # your code here
+  digit_vals = {'i' => 1,
+                'v' => 5,
+                'x' => 10,
+                'l' => 50,
+                'c' => 100,
+                'd' => 500,
+                'm' => 1000}
+  total = 0
+  prev = 0
+  index = roman.length - 1
+  while index >= 0
+    x = roman[index].downcase
+    index = index - 1
+    val = digit_vals[x]
+    if !val
+      puts 'This is not a valid roman numeral!'
+      return
+    end
+
+    if val < prev
+      val = val * -1
+    else
+      prev = val
+    end
+    total = total + val
+  end
+
+  total
 end

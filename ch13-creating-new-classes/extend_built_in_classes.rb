be24d4e6ef306724400e0e8ef3e53579c68ee65a
Andrew Htun <andy_htun@hotmail.com>
2015-11-16 15:29:06 +0000

    updated extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,24 @@
 class Integer
-  # your code here
+  
+  def factorial
+    self.downto(1).inject(:*)
+  end
+
+  def to_roman
+    roman = {1000=>'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', 5 => 'V', 1 =>'I'}
+    result=''
+    num = self
+    
+    roman.each do |english_value ,numeral|
+      divide = (num / english_value) 
+        if divide > 0
+          result << numeral * divide
+        end
+      remainder = num % english_value
+      num = remainder
+    end
+    
+    result
+  end
+
 end

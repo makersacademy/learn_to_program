4c5d7dab0337d4fa5ad8a726b6970e227d2f88cb
William Bautista <williambautista@gmail.com>
2015-11-16 00:04:06 +0000

    "Adding Challenge CH13 extended_baby_dragon"

---
+++ test.rb
@@ -0,0 +1,20 @@
+def birthday_time
+  def feed
+    p "inside feed"
+  end
+
+
+  decision = "4"
+
+  if decision =~ /[123456]/
+    puts true
+  else
+    puts false
+  end
+
+
+
+
+end
+
+birthday_time

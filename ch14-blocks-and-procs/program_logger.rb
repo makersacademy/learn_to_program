0a716e63aa2a76a10fff794d208af88f68bf32f6
Andrew Htun <andy_htun@hotmail.com>
2015-11-16 20:43:54 +0000

    updated program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,17 +1,5 @@
-def log desc, &block
-  puts 'Beginning "' + desc + '"...'
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
   result = block.call
-  puts '..."' + desc + '" finished, returning: ' + result.to_s
-end
-
-log 'outer block' do
-  log 'some little block' do
-    1**1 + 2**2
-  end
-
-  log 'yet another block' do
-    '!doof iahT ekil I'.reverse
-  end
-
-  '0' == 0
+  puts "...\"#{desc}\" finished, returning: #{result}"
 end

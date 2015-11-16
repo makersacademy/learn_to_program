b1e51ac00f7f02af7a90257cefe618e08259ef84
Tristan Crossley <trcrossley@gmail.com>
2015-11-16 12:18:13 +0000

    Commit 13: ch14 grandfather_block

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,17 @@
 def grandfather_clock &block
-  # your code here
+  thetime = Time.new.hour
+
+  if thetime == 0
+    thetime = 12
+  end
+
+  if thetime >= 13
+    thetime = thetime - 12
+  end
+
+  thetime.times(&block)
+end
+
+grandfather_clock do
+  puts "DONG!"
 end

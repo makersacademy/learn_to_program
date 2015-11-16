40b7025eb83eab2f310377c107c7954b99d0bd72
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-16 09:51:21 +0000

    playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,14 @@
 # your code here
+require './Projects/learn_to_program/ch10-nothing-new/shuffle.rb'
+
+Dir.chdir('.')
+
+songs = Dir['./**/*.mp3']
+
+shuffle(songs)
+
+File.open 'tunes.m3u', 'w' { |f|
+  songs.each { |mp3|
+    f.write mp3 + "\n\n"
+  }
+}

da606258b62ae5cb77990980429ddf729c326164
Tristan Crossley <trcrossley@gmail.com>
2015-11-16 20:11:54 +0000

    Commit 26: ch13 ojtree & ch14 proglog

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -28,7 +28,7 @@ class OrangeTree
     if @alive
       @height = @height + 0.4
       @orange_count = 0 # old oranges fall off
-      if @height > 10 && rand(2) > 0
+      if @height > 10.4
         # tree dies
         @alive = false
         'Oh, no! The tree is too old, and has died. :('
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,17 +1,5 @@
 def log desc, &block
-  puts "Beginning #{desc.inspect}..."
-  result = block[]
+  puts "Beginning" \"#{desc.inspect}\"..."
+  result = block.call
   puts "...#{desc.inspect} finished, returning: #{result}"
-end
-
-log 'outer block' do
-  log 'some little block' do
-    1**1 + 2**2
-  end
-
-  log 'yet another block' do
-    '!doof iahT ekil I'.reverse
-  end
-
-  '0' == 0
 end

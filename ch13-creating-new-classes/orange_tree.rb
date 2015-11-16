9179a9d18b456b7d7891b2c2095c4521f14839f6
Andrew Htun <andy_htun@hotmail.com>
2015-11-16 15:58:53 +0000

    updated orange_tree.rb

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,91 @@
 class OrangeTree
-  # your code here
+
+def initialize
+@age = 0
+@tall = 0
+@fruit = 0
+puts 'A new Orange Tree is planted.'
 end
+
+def height
+puts 'The tree is ' + @tall.to_s + ' feet tall.'
+end
+
+def count_the_oranges
+puts 'The tree has ' + @fruit.to_s + ' oranges.'
+end
+
+def pick_an_orange
+if @fruit < 1
+puts 'Sorry, there are no oranges to pick this year.'
+else
+puts 'You pick an orange from the tree. It\'s very delicious.'
+@fruit = @fruit - 1
+end
+end
+
+def one_year_passes
+puts 'One year passes'
+@age = @age + 1
+@tall = @tall + 1
+@fruit = 0
+
+if dead?
+puts 'The Orange Tree dies'
+exit
+end 
+
+if @age > 2
+@fruit = @age*10
+else
+@fruit = 0
+end
+
+end
+
+private
+
+def dead?
+@age > 5
+end
+
+end
+
+puts ''
+tree = OrangeTree.new
+tree.height
+tree.count_the_oranges
+tree.pick_an_orange
+puts ''
+tree.one_year_passes
+tree.height
+tree.count_the_oranges
+tree.pick_an_orange
+puts ''
+tree.one_year_passes
+tree.one_year_passes
+tree.height
+tree.count_the_oranges
+tree.pick_an_orange
+tree.pick_an_orange
+tree.pick_an_orange
+tree.pick_an_orange
+tree.pick_an_orange
+tree.pick_an_orange
+tree.count_the_oranges
+puts ''
+tree.one_year_passes
+tree.height
+tree.count_the_oranges
+tree.pick_an_orange
+tree.pick_an_orange
+tree.pick_an_orange
+tree.count_the_oranges
+tree.pick_an_orange
+tree.count_the_oranges
+puts ''
+tree.one_year_passes 
+tree.height
+tree.count_the_oranges 
+puts ''
+tree.one_year_passes 

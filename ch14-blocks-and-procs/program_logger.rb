c651bd6b7f2ee505153feb0eb5b4dc9a62fe59d1
William Bautista <williambautista@gmail.com>
2015-11-16 08:58:52 +0000

    "Adding Challenge CH14 program_logger"

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,6 @@
 def log desc, &block
-  # your code here
+  puts "Starting block within Proc log using #{desc.inspect}"
+  result = block.call
+  puts "#{desc.inspect} block has finished"
+  puts "Your final retult value is #{result}"
 end

f70e2101525881db89ec97bf5105fad8a4e9884f
Charlie Person <charlie.person@hyperisland.se>
2015-11-16 01:31:02 +0000

    better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,8 @@
-def log desc, &block
-  # your code here
+def better_log desc, &block
+$indent ? $indent : $indent = 0
+  puts "#{" " * $indent}Beginning \"#{desc}\"..."
+    $indent += 1
+  output = block.call
+    $indent -= 1
+  puts "#{" " * $indent}...\"#{desc}\" finished, returning: #{output}"
 end

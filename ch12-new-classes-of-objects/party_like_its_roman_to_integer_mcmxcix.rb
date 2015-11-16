a3722b48df756bf3b30f2397ae34ba129fad2ce7
William Bautista <williambautista@gmail.com>
2015-11-16 10:54:42 +0000

    "Refactoring CH10 party_like_its_roman_to_integer_mcmxcix"

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,18 +1,32 @@
 def roman_to_integer roman
-
-  roman_mapping = {
-      1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC",
-      50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"
-                  }
-
-  result = 0
-  "Roman numeral not valid" if roman.empty?
-
-  roman_mapping.values.each do |value|
-    if roman.start_with?(value)
-      result += roman_mapping.invert[value]
-      roman = roman.slice(value.length, roman.length)
-      roman_to_integer(roman, result)
+  digit_vals = {'i' => 1,
+                'v' => 5,
+                'x' => 10,
+                'l' => 50,
+                'c' => 100,
+                'd' => 500,
+                'm' => 1000}
+  total = 0
+  prev = 0
+  roman.reverse.each_char do |c_or_C|
+    c = c_or_C.downcase
+    val = digit_vals[c]
+    if !val
+      puts 'This is not a valid roman numeral!'
+      return
     end
+
+    if val < prev
+      val *= -1
+    else
+      prev = val
+    end
+
+    total += val
   end
+
+  total
 end
+
+
+# p roman_to_integer('i')

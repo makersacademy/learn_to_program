561f67a67001f56c91d3b7f0a7beda33cc1148b2
Charlie Person <charlie.person@hyperisland.se>
2015-11-16 01:12:31 +0000

    program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,45 @@
 def log desc, &block
-  # your code here
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
 end
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
+end
+
+
+
+
+
+
+# def log desc, &block
+#   block.call
+# end
+#
+# log "outer block" do
+#   puts "Beginning \"outer block\""
+#
+#   log "some little block" do
+#     puts "Beginning \"some little block\"..."
+#     puts "\"some little block\" finished, returning: "
+#     p 5
+#     end
+#
+#         log "yet another block" do
+#         puts "Beginning \"yet another block\" "
+#         puts "\"yet another block\" finished, returning: "
+#         p "I like Thai Food!"
+#         end
+#
+#
+#   puts "\"outer block\" finished, returning: " p false
+# end

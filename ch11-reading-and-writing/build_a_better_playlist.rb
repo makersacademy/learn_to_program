cbad5e05e371ef49acdf24f5fa791d0e116e19c1
Kirk Smith <kirks@hotmail.co.uk>
2015-11-16 10:57:23 +0000

    ch11 playlists

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,35 @@
 def music_shuffle filenames
-  # your code here
+  #sort before shuffle
+  filenames = filenames.sort
+  len = filenames.length
+
+  #shuffle twice
+  2.times do
+    l_idx = 0
+    r_idx = len/2
+    shuf = []
+
+    while shuf.length < len
+      if shuf.length % 2 == 0
+        #take card from right pile
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        #take card from the left
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+    filenames = shuf
+  end
+  #and cut the deck
+  arr = []
+  cut = rand(len) #index of cards to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx + cut) % len])
+    idx = idx + 1
+  end
+  arr
 end
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
 # your code here
+def new_playlist
+  all_mp3s = shuffle(Dir['**/*.mp3'])
+
+  File.open 'New_playlist' 'w' do |f|
+    all_mp3s.each do |mp3|
+    f.write mp3+"\n"
+  end
+end
+
+puts "Done!"

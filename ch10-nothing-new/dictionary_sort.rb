dc86685715dcbb548646fd28734b77cd399d2cba
Tom <bradley.tom.r@gmail.com>
2015-11-16 19:55:34 +0000

    Update dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,26 +1,39 @@
-def sort arr
-  rec_sort arr, []
+def dictionary_sort arr
+  return arr if arr.length <= 1
+  middle = arr.pop
+  less = arr.select{|x| x.downcase < middle.downcase}
+  more = arr.select{|x| x.downcase >= middle.downcase}
+
+  dictionary_sort(less) + [middle] + dictionary_sort(more)
 end
 
-def rec_sort unsorted, sorted
-  if unsorted.length <= 0
-    return sorted
-  end
+words = ['can','feel','singing.','like','A','can']
+puts(dictionary_sort(words).join(' '))
 
-  smallest       = unsorted.pop
-  still_unsorted = []
 
-  unsorted.each do |item|
-    if item.downcase < smallest.downcase
-      still_unsorted.push smallest
-      smallest = item
-    else
-      still_unsorted.push item
-    end
-  end
+# def sort arr
+#   rec_sort arr, []
+# end
 
-  sorted.push smallest
-  rec_sort still_unsorted, sorted
-end
+# def rec_sort unsorted, sorted
+#   if unsorted.length <= 0
+#     return sorted
+#   end
 
-puts(sort(['Foxtrot', 'hotel', 'bravo', 'Golf', 'charlie', 'delta', 'Alpha', 'echo']).join(' '))
+#   smallest       = unsorted.pop
+#   still_unsorted = []
+
+#   unsorted.each do |item|
+#     if item.downcase < smallest.downcase
+#       still_unsorted.push smallest
+#       smallest = item
+#     else
+#       still_unsorted.push item
+#     end
+#   end
+
+#   sorted.push smallest
+#   rec_sort still_unsorted, sorted
+# end
+
+# puts(sort(['Foxtrot', 'hotel', 'bravo', 'Golf', 'charlie', 'delta', 'Alpha', 'echo']).join(' '))

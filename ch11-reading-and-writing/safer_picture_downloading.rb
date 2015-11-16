c0b8665b926aeffc637ac99a3eeb7e0bc53ad7a6
Kirk Smith <kirks@hotmail.co.uk>
2015-11-16 12:57:21 +0000

    ch11 all

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,30 @@
-# your code here
+# get pics from here
+Dir.chdir '/mac/pictures/new_photos'
+pic_names = Dir['/mac/pictures/photos/*.jpg']
+
+
+puts 'What would you like to call this batch?'
+
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files."
+#are counter
+pic_number = 1
+pic_names.each do |name|
+    print '.' #progress bar
+
+    new_name = if pic_number < 10
+        "#{batch_name}0#{pic_number}.jpg"
+    else
+        "#{batch_name}#{pic_number}.jpg"
+    end
+
+        File.rename name, new_name
+
+    pic_number = pic_number + 1
+
+end
+
+puts
+puts "Move complete!"

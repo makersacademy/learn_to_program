9089752c0c9e923bdea5bba2ed6e5b0cb14a834a
Tom <bradley.tom.r@gmail.com>
2015-11-16 19:23:07 +0000

    Ch14 complete

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,8 @@
-def log desc, &block
-  # your code here
+def better_log desc, &block
+  prefix = " "*$depth
+  puts prefix+"Beginning \"#{desc}\"..."
+  $depth += 1
+  result = block.call
+  $depth -= 1
+  puts prefix+"...\"#{desc}\" finished, returning: #{result}"
 end

18eb828680ad605eaa02c5710fe6aa31513a3586
Tristan Crossley <trcrossley@gmail.com>
2015-11-16 12:03:14 +0000

    Commit 11: ch14 better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
-def log desc, &block
-  # your code here
+def better_log desc, &block
+  prefix = " " * $indent
+  puts prefix + "Beginning #{desc.inspect}..."
+  $indent += 1
+  result = block.call
+  $indent -= 1
+  puts prefix + "...#{desc.inspect} finished, returning: #{result}."
+end
+
+better_log "outer block" do
+  better_log "some little block" do
+    better_log "teeny-tiny block" do
+      "l0ts of l0Ve".downcase
+    end
+  7 * 3 * 2
+  end
+
+  better_log "yet another block" do
+    "!doof naidnI evol I".reverse
+  end
+  "0" == "0"
 end

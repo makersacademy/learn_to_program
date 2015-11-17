cd0e5f149ee27b9d371200dcf22c1c6dcc4dbfa3
hibreez <4ngus.whitehead@gmail.com>
2015-11-17 03:53:56 +0000

    done up to ch14

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,18 @@
 def music_shuffle filenames
-  # your code here
+  rec_shuffle(filenames, [])
+end
+
+def rec_shuffle(filenames, shuffled)
+  unshuffled = []
+  selected = rand(filenames.length)
+  (0...filenames.length).each { |i|
+    i == selected ? (shuffled << filenames[i]) : (unshuffled << filenames[i])
+  }
+  return shuffled if unshuffled == []
+  rec_shuffle(unshuffled, shuffled)
+end
+
+def music_shuffle songs
+  ['aa/ddd', 'AAA/xxxx', 'AAA/zzzz',
+    'aa/ccc', 'foo/bar', 'AAA/yyyy', 'aa/bbb']
 end

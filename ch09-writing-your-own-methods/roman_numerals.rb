7dbd00631b83e82cf7bd325247c016f9e745c019
ViolaCrellin <viola.crellin@gmail.com>
2015-12-31 03:00:56 +0000

    Completed Ch09 'Roman Numerals', downstream tests passed.

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,12 @@
 def roman_numeral num
-  # your code here
+  		roman_numerals  = ["M", "D", "C", "L", "X", "V", "I"]
+		divisors = [1000, 500, 100, 50, 10, 5, 1]
+		answer = []
+		divisors.each_with_index {|x, i|  answer << roman_numerals[i] * (num % divisors[i > 0 ? i -1 : 0] / x) }
+	if num >= 1000
+		answer.unshift("M" * (num / 1000))
+		answer.join.gsub("IIII", "IV").gsub("VIIII", "IX").gsub("VIV", "IX")
+	else 
+		answer.join.gsub("IIII", "IV").gsub("VIIII", "IX").gsub("VIV", "IX")
+	end
 end

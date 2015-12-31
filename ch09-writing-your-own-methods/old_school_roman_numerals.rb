131032c500c35b8187af9a65adcab5c42f1c449c
ViolaCrellin <viola.crellin@gmail.com>
2015-12-31 02:05:14 +0000

    Completed Ch09 'Old School Roman Numerals'

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,10 @@
 def old_roman_numeral num
-  # your code here
+	roman_numerals  = ["M", "D", "C", "L", "X", "V", "I"]
+	divisors = [1000, 500, 100, 50, 10, 5, 1]
+	answer = []
+	divisors.each_with_index {|x, i|  answer << roman_numerals[i] * (num % divisors[i > 0 ? i -1 : 0] / x) }
+	puts answer.join
 end
+
+old_roman_numeral 1999
+

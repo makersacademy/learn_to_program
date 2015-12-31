b8da91a2008b3cc9f377a94ac66305ca4529c8d2
ViolaCrellin <viola.crellin@gmail.com>
2015-12-31 02:32:50 +0000

    Completed Ch09 'Old School Roman Numerals', tests passed downstream.

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,9 +1,14 @@
 def old_roman_numeral num
-	roman_numerals  = ["M", "D", "C", "L", "X", "V", "I"]
-	divisors = [1000, 500, 100, 50, 10, 5, 1]
-	answer = []
-	divisors.each_with_index {|x, i|  answer << roman_numerals[i] * (num % divisors[i > 0 ? i -1 : 0] / x) }
-	puts answer.join
+		roman_numerals  = ["M", "D", "C", "L", "X", "V", "I"]
+		divisors = [1000, 500, 100, 50, 10, 5, 1]
+		answer = []
+		divisors.each_with_index {|x, i|  answer << roman_numerals[i] * (num % divisors[i > 0 ? i -1 : 0] / x) }
+	if num >= 1000
+		answer.unshift("M" * (num / 1000))
+		answer.join
+	else 
+		answer.join
+	end
 end
 
 

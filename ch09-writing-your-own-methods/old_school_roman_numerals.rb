36a3a73be0318679238f6a7e4f22f87bf893fbad
Alex Avlonitis <avlonitis@msn.com>
2016-01-04 23:01:53 +0100

    ch09.02

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
 def old_roman_numeral num
-  # your code here
+
+  hash = { 1000 => "M",
+           500 => "D",
+           100 => "C",
+           50 => "L",
+           10 => "X",
+           5 => "V",
+           1 => "I" }
+
+  str = ""
+  hash.each do |key, value|
+    while num >= key
+      str << value
+      num -= key
+    end
+  end
+  str
 end

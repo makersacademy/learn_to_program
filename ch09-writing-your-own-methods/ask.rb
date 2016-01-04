a20c67633a510b748760dc411c3e687d43d27f01
Ina Tsetsova <inatsetsova@gmail.com>
2016-01-04 19:23:16 +0200

    edited ask program

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,9 @@
-def ask question
-  # your code here
+def ask(question)
+    puts question
+    reply = gets.chomp.downcase
+    until (reply == "yes" || reply == "no")
+      puts "Please answer 'yes' or 'no'"
+      reply = gets.chomp.downcase
+    end
+    reply == "yes" ? true : false
 end

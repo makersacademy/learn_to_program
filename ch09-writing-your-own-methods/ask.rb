682708f6f839d0645d6055c7f3c57c93c7543839
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-04 18:57:01 +0000

    Completed Ch09 Ask

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
 def ask question
-  # your code here
+
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes'
+      return true
+    elsif reply == 'no'
+      return false
+    else
+      puts "Please answer yes or no"
+    end
+
+  end
+
 end

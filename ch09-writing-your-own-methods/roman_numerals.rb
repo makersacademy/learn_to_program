b589d7ef87c0cd46e5a9d8f9511bb0ded6098c24
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-04 20:15:42 +0000

    Completed roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,12 @@
 def roman_numeral num
-  # your code here
+  output = ""
+  roman = {"M" => 1000, "DM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1 }
+
+  roman.each do |k, v|
+    if num / v > 0
+  	  output << k*(num/v)
+  	  num = num % v
+    end
+  end
+  output
 end

d729bc18897da363995f0f65a9605b5ba95e39df
seanhawkridge <sean@hawkridge.co>
2016-01-04 17:25:21 +0000

    Chapter 10.1

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,31 @@
-def sort arr
-  # your code here
+
+def sort(arr)
+  recursive_sort(arr, [])
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+
+    if unsorted_array.count <= 0
+      return sorted_array
+    end
+
+    smallest = unsorted_array.pop
+    p 'first the smallest is ' + smallest
+    unsorted = []
+
+    unsorted_array.each do |w|
+      if w < smallest
+        unsorted.push(smallest)
+        smallest = w
+        p 'now the smallest is ' + smallest
+      else
+        unsorted.push(w)
+      end
+    end
+
+    sorted_array.push smallest
+    recursive_sort(unsorted, sorted_array)
+
+end
+
+p sort(['egg','balloon','fish','apple', 'Apple', 'Monkey'])

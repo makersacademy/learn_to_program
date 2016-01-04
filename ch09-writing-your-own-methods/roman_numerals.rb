81f535350980811439f8273579e1c4216c5c9acd
Alex Avlonitis <avlonitis@msn.com>
2016-01-04 22:50:27 +0100

    ch09.03

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,24 @@
 def roman_numeral num
-  # your code here
+  hash = { 1000 => "M",
+           900 => "CM",
+           500 => "D",
+           400 => "CD",
+           100 => "C",
+           90 => "XC",
+           50 => "L",
+           40 => "XL",
+           10 => "X",
+           9 => "IX",
+           5 => "V",
+           4 => "IV",
+           1 => "I" }
+
+  str = ""
+  hash.each do |number, sub|
+    while num >= number
+      str << sub
+      num -= number
+    end
+  end
+  str
 end

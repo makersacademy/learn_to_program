cbc86029c11d5ee66effa7d53df43ddc31c08bfe
Itsindigo <mattbridgesbusiness@gmail.com>
2016-01-04 17:10:15 +0000

    Old School Roman Numerals Answer

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+	output = ''
+
+	output << 'M' * (num/1000)
+	output << 'D' * (num%1000/500)
+	output << 'C' * (num%1000%500/100)
+	output << 'L' * (num%1000%500%100/50)
+	output << 'X' * (num%1000%500%100%50/10)
+	output << 'V' * (num%1000%500%100%50%10/5)
+	output << 'I' * (num%1000%500%100%50%10%5/1)
 end
+
+puts old_roman_numeral(2688)
+
+#I = 1 V = 5 X = 10
+#C = 100 D = 500 M = 1000
+#L = 50

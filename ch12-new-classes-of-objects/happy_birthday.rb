a9479e282670a3d167a9da755864d880146814fe
Henry Gordon <hwgordon247@gmail.com>
2016-01-04 16:35:04 +0000

    Chapter 12 Happy Birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+require "time"
+
+def happy_birthday
+  puts "What year were you born?"
+  year = gets.chomp
+  puts "Month?"
+  month = gets.chomp
+  puts "Day?"
+  day = gets.chomp
+  age = Time.new - Time.gm(year,month,day)
+  age_year = (age/60/60/24/365).to_i
+  puts "You are #{age_year} years old."
+  puts "SPANK!\n"*age_year
+end
+
+happy_birthday

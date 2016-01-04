a7d79e1ac3bc35c77466037cf51b35e61d492a97
Henry Gordon <hwgordon247@gmail.com>
2016-01-04 15:06:17 +0000

    Chapter 10 English Number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,71 @@
 def english_number number
-  # your code here
+  if number < 0 
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+  num_string = ''
+  ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  
+  left = number
+  
+  write = left/1000000
+  left = left - write*1000000
+  if write > 0
+    millions = english_number write
+    num_string = num_string + millions + ' million'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/1000
+  left = left - write*1000 
+  if write > 0
+    thousands = english_number write
+    num_string = num_string + thousands + ' thousand'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/100 # How many hundreds left?
+  left = left - write*100 # Subtract off those hundreds.
+  if write > 0
+    hundreds = english_number write
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/10 # How many tens left?
+  left = left - write*10 # Subtract off those tens.
+  if write > 0
+
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left # How many ones left to write out?
+  left = 0 # Subtract off those ones.
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+  num_string
 end
+
+
+puts english_number(267999999)
+
+

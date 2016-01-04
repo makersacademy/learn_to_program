3187d2d066b5dd306303921c4e4d159a97a83ee4
seanhawkridge <sean@hawkridge.co>
2016-01-04 17:26:00 +0000

    Chapter 10.2

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,31 @@
-def dictionary_sort arr
-  # your code here
-end
+
+  def dictionary_sort arr
+    recursive_sort(arr, [])
+  end
+
+  def recursive_sort(unsorted_array, sorted_array)
+
+      if unsorted_array.count <= 0
+        return sorted_array
+      end
+
+      smallest = unsorted_array.pop
+      p 'first the smallest is ' + smallest
+      unsorted = []
+
+      unsorted_array.each do |w|
+        if w.downcase < smallest.downcase
+          unsorted.push(smallest)
+          smallest = w
+          p 'now the smallest is ' + smallest
+        else
+          unsorted.push(w)
+        end
+      end
+
+      sorted_array.push smallest
+      recursive_sort(unsorted, sorted_array)
+
+  end
+
+  p dictionary_sort(['egg','balloon','fish','apple', 'Apple', 'Monkey'])

5e0c6f3b34106f45050c715a8877d2e5458bb356
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-04 19:41:17 +0100

    ch09 new roman numbers

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,40 @@
 def roman_numeral num
-  # your code here
+  roman = ""
+  left = num
+
+  thousands = num / 1000
+  roman << "M" * thousands
+  left %= 1000
+
+  hundreds = left / 100
+  roman << build_string_part(hundreds,"D","C","M")
+  left %= 100
+
+  tens = left / 10
+  roman << build_string_part(tens,"L","X","C")
+  left %= 10
+
+  roman << build_string_part(left,"V","I","X")
 end
+
+def build_string_part number, roman1, roman2, roman3
+  string_part = ""
+  case number
+  when 4
+    string_part << (roman2 + roman1)
+  when 9
+    string_part << (roman2 + roman3)
+  else
+    if number >= 5
+      string_part << (roman1 + roman2 * (number - 5))
+    else
+      string_part << roman2 * number
+    end
+  end
+  string_part
+end
+
+puts roman_numeral 4999
+puts roman_numeral 4444
+puts roman_numeral 500
+puts roman_numeral 89

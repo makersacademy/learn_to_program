01c40f5e7058111f1d4514654d3cdd37dd6da4ed
hedudelgado <hedu.delgado@gmail.com>
2016-01-04 18:42:44 +0000

    ch09-writing-your-own-methods/ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,20 @@
 def ask question
-  # your code here
+ while true
+
+     puts question
+     reply = gets.chomp.downcase
+    if reply == 'yes' or reply == 'no'
+        if reply == 'yes'
+            return true
+        end
+        if reply == 'no'
+            return false
+        end
+    elsif reply == ''
+        break
+    else
+        puts 'Please, answer "yes" or "no".'
+    end
+     
+ end
 end

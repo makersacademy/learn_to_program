c2f8d5109af6bc3f2667b2d8e229276d19421d98
Henry Gordon <hwgordon247@gmail.com>
2016-01-04 14:39:09 +0000

    Chapter 10 Dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,29 @@
 def dictionary_sort arr
-  # your code here
+  dic = Hash.new
+  diction = Array.new
+  cap = 0
+  arr.each {|k|
+    if k == k.capitalize
+      cap = 1
+      dic[k.downcase] = cap
+    else
+      cap = 0
+      dic[k] = cap
+    end
+    diction.push(k.downcase)
+  }
+
+  diction.sort!
+  dic_order = Array.new
+  diction.each {|r|
+    cap = dic[r]
+    if cap == 1
+      r.capitalize!
+    end
+    dic_order.push(r)
+  }
+  dic_order
+  
 end
+
+p dictionary_sort ["Abby", "jack", "hello", "Dude", "Zac"]

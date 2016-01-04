ff83cc675d2271edc46b0296fd4ce03ee9ca03d2
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-04 19:44:43 +0000

    Completed Ch09 Old School Roman Numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,23 @@
 def old_roman_numeral num
-  # your code here
+
+  thousands = num / 1000
+  five_hundreds = (num % 1000) / 500
+  hundreds = (num % 500) / 100
+  fifties = (num % 100) / 50
+  tens = (num % 50) / 10
+  fives = (num % 10) / 5
+  ones = (num % 5) / 1
+
+  str = ""
+
+  str <<  "M" * thousands
+  str <<  "D" * five_hundreds
+  str << "C" * hundreds
+  str << "L" * fifties
+  str << "X" * tens
+  str << "V" * fives
+  str << "I" * ones
+
+  str
+
 end

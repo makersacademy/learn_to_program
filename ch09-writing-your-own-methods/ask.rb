88be80f38a53fa5e42c65c1aae5c34e2173555c0
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-04 19:16:26 +0000

    Completed ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,4 +1,13 @@
 def ask question
-  # test
-  # your code here
+  while true
+  	puts question
+  	reply = gets.chomp.downcase
+  	if reply == 'yes'
+  		return true
+    elsif reply == 'no'
+      return false
+    else
+    	puts 'Please answer "yes" or "no".'
+    end
+  end
 end

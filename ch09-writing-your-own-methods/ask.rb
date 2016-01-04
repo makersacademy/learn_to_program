84c5101ef43c4e4a3b3955f0abcd5017b4681fe9
Mic Cassano <mic.cassano@gmail.com>
2016-01-04 17:15:19 +0000

    Add answers for ch09/ask.rb and ch09/old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,16 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == "yes" || reply == "no")
+      if reply == "yes"
+        return true
+      else
+        return false
+      end
+    else
+      puts "Please answer \"yes\" or \"no\"."
+    end
+  end
 end
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,39 @@
 def old_roman_numeral num
-  # your code here
+  numeral = ""
+
+  if num >= 1000
+    thousands = num / 1000
+    num = num % 1000
+    numeral << "M" * thousands
+  end
+
+  if num >= 100
+    hundreds = num / 100
+    num = num % 100
+    if hundreds >= 5
+      numeral << "D"
+      hundreds -= 5
+    end
+    numeral << "C" * hundreds
+  end
+
+  if num >= 10
+    tens = num / 10
+    num = num % 10
+    if tens >= 5
+      numeral << "L"
+      tens -= 5
+    end
+    numeral << "X" * tens
+  end
+
+  if num > 0
+    if num >= 5
+      numeral << "V"
+      num -= 5
+    end
+    numeral << "I" * num
+  end
+
+  numeral
 end

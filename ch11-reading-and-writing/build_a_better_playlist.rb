0e73f7fc1cac79972c02f1251fd6314cdf88fdb0
Henry Gordon <hwgordon247@gmail.com>
2016-01-05 15:11:05 +0000

    Chapter 11 Better playist edit

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,5 +1,5 @@
 
-def music_shuffle filenames
+def shuffle filenames
   random_store = []
   2.times do
     for i in 0..(filenames.length-1)
@@ -24,4 +24,54 @@ def music_shuffle filenames
   random_cut
 end
 
-p music_shuffle ['a','b','c','d','e','f','g','h']
+p shuffle ['a','b','c','d','e','f','g','h']
+
+def music_shuffle filenames
+  songs_and_paths = filenames.map do |s|
+    [s, s.split('/')] # [song, path]
+  end
+
+  tree = {:root => []}
+
+  insert_into_tree = proc do |branch, song, path|
+    if path.length == 0 
+      branch[:root] << song
+    else 
+      sub_branch = path[0]
+      path.shift 
+
+      if !branch[sub_branch]
+        branch[sub_branch] = {:root => []}
+      end
+
+      insert_into_tree[branch[sub_branch], song, path]
+    end
+  end
+
+  songs_and_paths.each{|sp| insert_into_tree[tree, *sp]}
+
+  shuffle_branch = proc do |branch|
+    shuffled_subs = []
+
+    branch.each do |key, unshuffled|
+      shuffled_subs << if key == :root
+      unshuffled 
+      else
+        shuffle_branch[unshuffled]
+      end
+    end
+
+    weighted_songs = []
+
+    shuffled_subs.each do |shuffled_songs|
+      shuffled_songs.each_with_index do |song, idx|
+        num = shuffled_songs.length.to_f
+        weight = (idx + rand) / num
+        weighted_songs << [song, weight]
+      end
+    end
+
+    weighted_songs.sort_by{|s,v| v}.map{|s,v| s}
+  end
+  shuffle_branch[tree]
+end

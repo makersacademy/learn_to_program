95c47279e73e1c90ff1e7bfb720c8d7cd90b165d
Ina Tsetsova <inatsetsova@gmail.com>
2016-01-05 17:10:17 +0200

    did sort recursively

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,8 +1,10 @@
-def sort(array)
-  sorted_array = []
-  array.length.times do
+def recursive_sort(sorted_array, array)
+    return sorted_array if array == []
     sorted_array<<array.min
     array.delete_at(array.index(array.min))
-  end
-  sorted_array
+    recursive_sort(sorted_array,array)
+end
+
+def sort(array)
+  recursive_sort([], array)
 end

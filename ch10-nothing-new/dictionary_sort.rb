9f960f8aad2d679a4d49a4e884e5ec0f04d3b81c
Itsindigo <mattbridgesbusiness@gmail.com>
2016-01-05 20:47:11 +0000

    Updated dictionary sort with different solution.

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,26 +1,10 @@
-def dictionary_sort(arr) 
-  rec_dict_sort(arr, [])
+def dictionary_sort(arr)
+  sorted_words = Array.new
+    while arr.empty? != true
+        sorted_words << arr.min
+       	arr.delete_at(arr.index(arr.min) || arr.length)
+    end
+  return sorted_words
 end
 
-def rec_dict_sort(unsorted, sorted)
-  if unsorted.length <= 0
-    return sorted 
-  end
-
-  smallest = unsorted.pop
-  still_unsorted = []
-
-  unsorted.each do |x|
-    if x.downcase < smallest.downcase
-      still_unsorted << smallest
-      smallest = x
-    else
-      still_unsorted << x
-    end 
-  end
-  sorted << smallest
-  
-  rec_dict_sort still_unsorted, sorted
-end
-
-#puts dictionary_sort(["dog", "cat", "wolf", "rabbit"])
+puts dictionary_sort(["dog", "cat", "wolf", "rabbit"])

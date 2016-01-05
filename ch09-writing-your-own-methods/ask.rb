3f7b0d03e772517dd2c9c479590f42d0339bc044
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-05 11:35:02 +0000

    chp9 ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,49 +1,36 @@
 def ask question
 
- puts question
- reply = gets.chomp.downcase
+puts question
+reply = gets.chomp.downcase
 
- while reply != "yes" && reply != "no"
- 	puts "please enter yes or no"
+while reply != 'yes' && reply != 'no'
+puts 'Please answer "yes" or "no".'
+puts question
+reply = gets.chomp.downcase
+end
 
- 	if reply == "yes"
- 		return true
- 	else
- 		return false 
- 	end 
+if reply == 'yes'
+return true
+else
+return false
+end
 
+end
 
-end 
-end 
-
-
-
-puts 'Hello, and thank you for...'
-
+puts 'Hello, and thank you for ...'
 puts
 
-ask 'Do you like eating tacos?' # Ignore this return value
-
-ask 'Do you like eating burritos?' # And this one
-
-wets_bed = ask 'Do you wet the bed?' # Save this return value
-
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
 ask 'Do you like eating chimichangas?'
-
 ask 'Do you like eating sopapillas?'
-
 puts 'Just a few more questions...'
-
 ask 'Do you like drinking horchata?'
-
 ask 'Do you like eating flautas?'
 
 puts
-
-puts 'DEBRIEFING:'
-
-puts 'Thank you for...'
-
+puts 'DEBRIEFING'
+puts 'Thank you for ...'
 puts
-
 puts wets_bed

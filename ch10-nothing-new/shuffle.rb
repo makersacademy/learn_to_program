a2d33183143034a3fc4601f2257432a53e36f849
russellvaughan <russellvaughan@gmail.com>
2016-01-05 17:25:33 +0000

    complete shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,25 @@
-def shuffle arr
-  # your code here
+def shuffle arr 
+  rec_sort(arr, [])
+end
+
+def rec_sort unsorted_array, shuffled_array 
+if unsorted_array == [] 
+  return shuffled_array
+end
+  
+  still_unsorted_array = []
+  randomized_value = []
+
+    unsorted_array.each do |x| 
+
+     randomized_value = unsorted_array[rand(unsorted_array.length)]
+    
+    still_unsorted_array = unsorted_array - [randomized_value]
+ 
+end
+
+  shuffled_array << randomized_value
+
+rec_sort still_unsorted_array, shuffled_array
+
 end

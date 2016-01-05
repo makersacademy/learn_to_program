9656980a124221462d29013a115058cbcb96b181
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-05 01:34:53 +0000

    Completed shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,19 @@
+
 def shuffle arr
-  # your code here
+  shuff_arr = []
+  while arr.length > 0
+  	temp_arr = []
+    index = rand(0...(arr.length))
+  	
+  	arr.each do |x|
+  		if x == arr[index]
+  			shuff_arr.push(x)
+  		else
+  			temp_arr.push(x)
+  		end
+    
+    end 
+    arr = temp_arr
+  end
+  shuff_arr
 end

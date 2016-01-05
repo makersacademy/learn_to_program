5ea79af3f41767960d239e69a43e56822b3a40b2
Henry Gordon <hwgordon247@gmail.com>
2016-01-05 11:43:57 +0000

    Chapter 11 Playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,21 @@
-# your code here
+Dir.chdir '/Users/Harry/code/project_files/music'
+
+def shuffle arr
+  random_store = []
+  for i in 0..(arr.length-1)
+    position = rand(arr.length)
+    random_store.push(arr[position])
+    arr[position] = nil
+    arr.compact!
+  end
+  random_store
+end
+all_songs = shuffle(Dir['*.{mp3,m4a,m4p}'])
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_songs.each {|x|
+    f.write x + "\n"
+  }
+end
+
+puts 'Finished'

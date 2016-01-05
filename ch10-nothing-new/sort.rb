0435e4503f60a016407852b1dea3a328c810f1c6
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-05 17:23:19 +0000

    Completed Ch10 sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,30 @@
 def sort arr
-  # your code here
+ #["d", 'a', 'c', 'b']
+  to_sort = []
+  sorted = []
+
+
+while arr.length >= 1
+  smallest = arr.pop
+
+    arr.each do |word|
+      if smallest < word == true
+        to_sort << word
+      else
+        to_sort << smallest
+        smallest = word
+      end
+    end
+
+    sorted << smallest
+    arr = to_sort
+    to_sort = []
+
+
+  end
+  sorted
 end
+
+test = ["a", 'd', 'c', 'b']
+
+p sort test

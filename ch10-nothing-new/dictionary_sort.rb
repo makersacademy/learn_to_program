42be745970d01908a1c71b3df37c672dbf6487a8
Yan-Yi Li <liyanyi@gmail.com>
2016-01-05 21:33:15 +0000

    completed ch10 dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,14 @@
 def dictionary_sort arr
-  # your code here
+  unsorted_arr = arr
+  sorted_arr = []
+
+  while unsorted_arr != []
+    smallest_word = unsorted_arr.first
+    unsorted_arr.each do |word|
+      smallest_word = word if (word.downcase < smallest_word.downcase)
+    end
+    sorted_arr << smallest_word
+    unsorted_arr.delete_at(unsorted_arr.index(smallest_word))
+  end
+  sorted_arr
 end

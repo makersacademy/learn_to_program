0d76c61466a81872d0ca2d1f3026cab373bbda07
Reiss Johnson <reissjohnson@me.com>
2016-01-05 11:28:20 +0000

    added solution for old school roman num

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,23 @@
-def old_roman_numeral num
-  # your code here
+KEY = [
+  ["M", 1000], 
+  ["D", 500], 
+  ["C", 100], 
+  ["L", 50], 
+  ["X", 10], 
+  ["V", 5], 
+  ["I", 1], 
+]
+
+def old_school_roman n
+  roman = ""
+
+  KEY.each do |x|
+    letter = x[0]
+    value = x[1]
+    roman += letter*(n / value)
+    n = n % value
+  end
+  return roman
 end
+
+p old_school_roman 1005

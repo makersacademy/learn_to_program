a59661cf2c26dcb1b3bb26c382160dbd0f946543
Tom Pickard <tcpickard94@gmail.com>
2016-01-05 09:10:25 +0000

    Completed Ch09 Old roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,28 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+  array = num.to_s.split("").map {|n| n.to_i}
+  string = ''
+  
+  while array.length > 0
+  array.each_with_index do |n, i|
+    if array.length >= 4 && i == 0
+      string += "M" * n 
+      array.delete_at(i)
+    elsif array.length == 3 && i == 0
+      string += "D" + "L" * (n % 5) if n >= 5
+      string += "C" * n if n < 5
+      array.delete_at(i)
+    elsif array.length == 2 && i == 0
+      string += "L" + "X" * (n % 5) if n >= 5
+      string += "X" * n if n < 5
+      array.delete_at(i)
+    elsif array.length == 1
+      string += "V" + "I" * (n % 5) if n >= 5
+      string += "I" * n if n < 5
+      array.delete_at(i)
+    end
+  end
+  end
+  string
 end
+
+puts old_roman_numeral(1009)

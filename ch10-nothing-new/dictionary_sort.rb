6a461e871dd7b64f7ef0f9e29ff820cd07bba806
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-05 00:49:43 +0000

    Completed dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,31 @@
 def dictionary_sort arr
-  # your code here
+  recursion_sort arr, []
 end
+
+def recursion_sort unsorted_arr, sorted_arr
+  
+	return sorted_arr if unsorted_arr == []
+
+  if unsorted_arr.length == 1
+  	sorted_arr << unsorted_arr[0]
+  	unsorted_arr = []
+
+  else
+  	smallest = unsorted_arr[0]
+  	index = 0
+
+  	unsorted_arr.each_with_index do |x, i|	
+  		if x.downcase < smallest.downcase #just this line needed changing from the sort.rb!		
+  			smallest = x
+  			index = i
+  		end
+  	end
+
+  sorted_arr << smallest
+  unsorted_arr.delete_at(index)
+  
+  end	
+  recursion_sort unsorted_arr, sorted_arr
+
+end
+

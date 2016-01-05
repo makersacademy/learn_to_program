af07b203523d457ff04c15a29f9082b71b3275e0
Ina Tsetsova <inatsetsova@gmail.com>
2016-01-05 17:09:32 +0200

    finished sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,8 @@
-def sort arr
-  # your code here
+def sort(array)
+  sorted_array = []
+  array.length.times do
+    sorted_array<<array.min
+    array.delete_at(array.index(array.min))
+  end
+  sorted_array
 end

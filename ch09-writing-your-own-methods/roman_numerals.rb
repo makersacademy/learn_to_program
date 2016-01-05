ec633edf870af089d5ac6ff4dcd9f8a07d0ef2c4
Iryna Howarth <irynahowarth@gmail.com>
2016-01-05 12:31:36 +0000

    ch09 - 03 solved

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,15 @@
 def roman_numeral num
-  # your code here
+  roman_numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+  roman_letters = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M']
+  i = 12
+  result=''
+  while num != 0
+    if num >= roman_numbers[i]
+      result+=roman_letters[i]
+      num -= roman_numbers[i]
+    else
+      i-=1
+    end
+  end
+  result
 end

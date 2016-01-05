f29a54d1c1f9b1cffc181a79136027a033edd2b4
JoeWroe <joe_wroe@icloud.com>
2016-01-05 22:01:25 +0100

    Before the pre-course 2

---
+++ ch01-08-before-the-precourse/Chapter5/biggerBetter.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+fav_num = gets.chomp
+better_num = fav_num.to_i + 1
+puts "Thats a good number but isn't " + better_num.to_s + " both a little bigger and better?"
---
+++ ch01-08-before-the-precourse/Chapter5/fullName.rb
@@ -0,0 +1,6 @@
+puts "Afternoon sir, what's your last name?"
+last = gets.chomp
+puts "Excellent, thank you Mr " + last + "."
+puts "and your first name is?"
+first = gets.chomp
+puts "What a great name " + first + " " + last + "!"

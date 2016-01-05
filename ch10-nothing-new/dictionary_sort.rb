46c99cd9281cd5f1f714a4aa0791bb14af4133a8
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-05 18:14:07 +0100

    ch10 dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
 def dictionary_sort arr
-  # your code here
+
+  swap = 'on'
+  while swap == 'on'
+    swap = 'off'
+    n = 0
+
+   while n <= (arr.length - 2)
+
+     if arr[n].downcase > arr[n+1].downcase
+       temp     = arr[n]
+       arr[n]   = arr[n+1]
+       arr[n+1] = temp
+       swap = 'on'
+     end
+     n = n + 1
+   end
+  end
+  arr
 end
+
+# puts dictionary_sort ['can','feel','singing.','like','A','can']

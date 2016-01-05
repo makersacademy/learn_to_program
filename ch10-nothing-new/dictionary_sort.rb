24742120d09b16d438c5001b72647caf401bf789
hedudelgado <hedu.delgado@gmail.com>
2016-01-05 20:09:56 +0000

    ch10-nothing-new/dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,30 @@
 def dictionary_sort arr
-  # your code here
+	sorting arr,[]
 end
+	
+def sorting (arrdirty,arrsorted)
+		
+	if arrdirty.length < 1
+		return arrsorted
+	end
+
+	mini = arrdirty.pop
+	unsorted = []
+
+	arrdirty.each do |a|
+		if a.downcase < mini.downcase
+			unsorted.push(mini)
+			mini = a
+		else
+			unsorted.push(a)
+		end
+	end
+arrsorted.push(mini)
+
+sorting unsorted,arrsorted
+end
+puts(dictionary_sort([' can' ,' feel' ,' singing.' ,' like' ,' A' ,' can' ]))
+#arrfirst = [ a,b,g,f] its arr and arrdirty
+#unsorted = [ ]
+#arrsecond = [   ]
+#min es g.

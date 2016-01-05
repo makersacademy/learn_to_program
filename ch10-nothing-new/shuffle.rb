d23490323bf6fbe64217110788d80359be259915
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-05 16:55:21 +0100

    Added solution to ch10 shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
 def shuffle arr
-  # your code here
+  word_hash = {}
+  arr.each do |item|
+  	loop do
+  		rando = rand(1..arr.length)
+  		next if word_hash.values.include? rando
+		word_hash[item] = rando
+		break
+	end
+  end
+  (1..arr.length).map do |num|
+  	word_hash.key(num)
+  end
 end

b3cc3c8eaab35f02d7bbba2e632fc1a5afd68d82
Mic Cassano <mic.cassano@gmail.com>
2016-01-05 18:38:55 +0000

    Add answer to ch10/dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,19 @@
 def dictionary_sort arr
-  # your code here
+  rec_sort arr, []
+end
+
+def rec_sort unsorted, sorted
+  to_sort = []
+  min = unsorted.pop
+  unsorted.each do |testing|
+    if testing.downcase < min.downcase
+      to_sort << min
+      min = testing
+    else
+      to_sort << testing
+    end
+  end
+  sorted << min
+  rec_sort(to_sort, sorted) unless to_sort.size == 0
+  sorted
 end

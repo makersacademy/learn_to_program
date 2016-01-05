8a5798786799862e6c6ce36ad3caa7a7c4a73ae6
Mark Hill <futonfutonda@gmail.com>
2016-01-05 12:32:11 +0000

    roman numerals 1

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
 def old_roman_numeral num
-  # your code here
+  thousands = num/1000
+  hundreds = (num-thousands*1000)/100
+  tens = ((num-thousands*1000)-hundreds*100)/10
+  units = (((num-thousands*1000)-hundreds*100)-tens*10)
+  d = hundreds/5
+  c = hundreds-(d*5)
+  l = tens/5
+  x = tens - (l*5)
+  v = units/5
+  i = units - (v*5)
+
+  end
+"M"*thousands + "D"*d +"C"*c +"L"*l +"X"*x +"V"*v +"I"*i
+
+
+
+  
 end

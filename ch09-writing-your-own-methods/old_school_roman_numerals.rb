cafb4730ae7cd13376ac35fdc74b9cabe7cb60a1
barrygrubb <barrygrubb@users.noreply.github.com>
2016-01-05 17:24:21 +0000

    ch09_old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
 def old_roman_numeral num
-  # your code here
+  if num >= 0 && num <= 3000
+    remaining = num
+    result = ""
+
+    result += "M" * (num / 1000)
+    result += "D" * (num % 1000 / 500)
+    result += "C" * (num % 500 / 100)
+    result += "L" * (num % 100 / 50)
+    result += "X" * (num % 50 / 10)
+    result += "V" * (num % 10 / 5)
+    result += "I" * (num % 5 / 1)
+
+  else
+    puts "Please enter a value between 0 and 3000"
+  end
+
+  result
 end

e71f39b2fe25f210d709a124e08ac4182796f6b5
seanhawkridge <sean@hawkridge.co>
2016-01-05 16:28:03 +0000

    Ch11.2

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,10 @@
-# your code here
+
+Dir.chdir '/Users/seanhawkridge/Desktop/playlists'
+playlist_file = File.new "playlist.txt","w"
+song_names = Dir["/Users/seanhawkridge/Music/iTunes/iTunes\ Media/Music/**/*.mp3"][0..20]
+
+random = song_names.sort_by{rand}
+
+random.each do |song|
+playlist_file << "#{song}\n"
+end

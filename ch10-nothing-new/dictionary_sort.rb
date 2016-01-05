cf81cc4ddde17a325225bdd570105af5b5ea8fba
Marco Bagnasco <admin@hecticmojo.com>
2016-01-05 21:38:44 +0000

    Dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,5 @@
-def dictionary_sort arr
-  # your code here
-end
+def dictionary_sort arr, res=[]
+    return res if arr.empty?
+    res << arr.slice!(arr.index(arr.inject {|m, w| m.downcase < w.downcase ? m : w}))
+    dictionary_sort(arr, res)
+  end

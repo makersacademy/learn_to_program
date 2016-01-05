6bc93a0e544854ea3c859052fb770feee3d51c43
innlouvate <l.j.franklin85@googlemail.com>
2016-01-05 16:16:42 +0000

    os num consolidated

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -2,23 +2,16 @@ def old_roman_numeral num
 
   numerals = ["M", "D", "C", "L", "X", "V", "I"]
   values = [1000, 500, 100, 50, 10, 5, 1]
-  div=[]
   output = ""
 
-  values.each do |v|
-    div << (num/v).floor
+  values.each_with_index do |v, i|
+    output << "#{numerals[i]*(num/v).floor}" if num/v.floor != 0
     num = num % v
-  end
-
-  div.each_with_index do |v,i|
-    output << "#{numerals[i]*v}" if v!=0
   end
 
   return output
 
 end
 
-
-print old_roman_numeral 2763
-puts "next"
-print old_roman_numeral 321
+puts old_roman_numeral 8721
+puts old_roman_numeral 3891

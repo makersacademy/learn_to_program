597ac0db01fba1fc386c78f4b2b4645d20afd44d
Yan-Yi Li <liyanyi@gmail.com>
2016-01-05 15:22:49 +0000

    updated ch09 old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+  to_return = ""
+  to_write = num
+  letters = [[1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]
+
+  while to_write != 0
+    letters.each do |value|
+      to_return << value[1] * (to_write / value[0])
+      to_write = to_write % value[0]
+    end
+  end
+  to_return
 end

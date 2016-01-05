64aaaf20d8705fc5368f6f34e7faad88a32a97a6
mo <mdeerow@gmail.com>
2016-01-05 20:20:59 +0000

    09-3

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,28 @@
 def roman_numeral num
-  # your code here
+    
+    hash = {
+        "M" => 1000,
+        "CM"=> 900,
+        "D"=> 500,
+        "CD"=> 400,
+        "C"=> 100,
+        "XC"=> 90,
+        "L"=> 50,
+        "XL"=> 40,
+        "X"=> 10,
+        "IX"=> 9,
+        "V"=> 5,
+        "IV"=> 4,
+        "I" => 1
+    }
+  
+    answer = ""
+  
+    hash.each do |k, v|
+        answer << k * (num/v) 
+        num = num % v
+    end
+    
+    answer
+    
 end

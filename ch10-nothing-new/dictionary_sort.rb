17dce9619effe2b40a3eb79f03ff3cf867f831e0
Vicky Gray <vickygray26@hotmail.com>
2016-01-05 16:31:11 +0000

    CH10 dictionary_sort answer

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
 def dictionary_sort arr
-  # your code here
+  sorted_array = []
+
+	while !arr.empty?
+  	index = arr.index(arr.min)
+  	sorted_array << arr.delete_at(index)
+  	end
+
+	return sorted_array
 end

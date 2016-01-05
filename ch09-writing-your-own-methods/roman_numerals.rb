61bc1177edbfd0ff83ff17b93d1cf9b99502e68a
Reiss Johnson <reissjohnson@me.com>
2016-01-05 11:28:42 +0000

    added solution for roman nums

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,31 @@
-def roman_numeral num
-  # your code here
+KEY = [
+  ["M", 1000], 
+  ["CM", 900],
+  ["D", 500], 
+  ["CD", 400],
+  ["C", 100], 
+  ["XC", 90],
+  ["L", 50], 
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5], 
+  ["IV", 4],
+  ["I", 1], 
+]
+
+def roman_numeral n
+  roman = ""
+
+  KEY.each do |x|
+    letter = x[0]
+    value = x[1]
+    roman += letter*(n / value)
+    n = n % value
+  end
+  return roman
 end
+
+p roman_numeral 1009
+
+

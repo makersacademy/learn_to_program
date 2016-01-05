a79da3ea965252d85617c1f0597f3230de602972
eilw <eirikwiig@gmail.com>
2016-01-05 16:53:17 +0000

    Ch09 done

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,4 +1,10 @@
 def ask question
-  # your code here
-  #testing out
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    return true if reply == 'yes'
+    return false if reply == 'no'
+
+    puts 'Please answer "yes" or "no".'
+  end
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,28 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
+  #answer = recursive_sort arr, []
+  #puts answer.join(' ')
 end
+
+def recursive_sort unsorted_arr, sorted_arr
+  #break if unsorted_arr.length == 0
+  smallest = unsorted_arr[0]
+  if unsorted_arr.length > 0
+    unsorted_arr.each do |word|
+      smallest = word if word < smallest
+    end
+    sorted_arr << smallest
+    unsorted_arr.delete_at(unsorted_arr.index(smallest))
+    recursive_sort(unsorted_arr,sorted_arr)
+  else
+    return sorted_arr
+  end
+#find smallest word and add to sorted list
+  #move the rest of your words to unsorted list
+  #repeat
+end
+=begin
+sort ["Eirik","Abel", "Delta", "Xerox", "Eirik"]
+sort []
+sort ["with one"]
+=end

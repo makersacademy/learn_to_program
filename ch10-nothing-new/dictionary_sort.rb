06144a6a20ba7e0a3d3de25ec1cb653eb576382c
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-05 15:58:40 +0100

    Added solution to ch10 dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
 def dictionary_sort arr
-  # your code here
+   solution = []
+   arr.each do |item|
+   	 inserted = false
+     if solution.length > 1
+       solution.each.with_index do |i, ind|
+         if item.downcase < i.downcase
+          solution.insert(ind, item)
+          inserted = true
+          break
+         end
+       end
+       solution << item unless inserted
+     end
+     solution << item and next if solution.length == 0
+     if solution.length == 1
+       solution << item if item.downcase > solution[0].downcase
+       solution.insert(0, item) if item.downcase < solution[0].downcase
+     end
+   end
+   solution
 end

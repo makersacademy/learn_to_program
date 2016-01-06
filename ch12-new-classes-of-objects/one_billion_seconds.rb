01f95c417c30df646b1a250e963ca020b710258a
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-06 15:56:13 +0000

    ch12/one_billion_seconds completed - amend puts to return

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -6,4 +6,4 @@ birthday = Time.gm(1986, 3, 8, 2, 2, 1)
   #years = ((1000000000-seconds-(mins*60)-(hours*60*60)(days*24*60*60))/365)
   #puts "#{years} years #{days} days #{hours} hours #{mins} mins #{seconds} seconds in a billion seconds"
 bil_sec_old = birthday + 1000000000
-puts bil_sec_old
+return bil_sec_old

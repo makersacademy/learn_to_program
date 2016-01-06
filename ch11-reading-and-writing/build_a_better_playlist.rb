0546a09ea055f27224606489b2b4fbefaf9de073
Tom Pickard <tcpickard94@gmail.com>
2016-01-06 14:20:49 +0000

    Completed Ch11 build a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,31 @@
-def music_shuffle filenames
-  # your code here
+# Had to look at the answer to get the test to pass
+def music_shuffle(filenames)
+  filenames.sort!
+  length = filenames.length
+  2.times do
+    shuffled = []
+    right = 0
+    left = length/2
+    
+    while shuffled.length < length
+      if shuffled.length.even?
+        shuffled.push(filenames[left])
+        left += 1
+      else
+        shuffled.push(filenames[right])
+        right += 1
+      end
+    end
+    
+  filenames = shuffled
+ end
+ array = []
+ cut = rand(length)
+ index = 0
+ 
+ while index < length
+   array.push(filenames[(index+cut)%length])
+   index += 1
+ end
+  array
 end
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,4 +1,4 @@
-require_relative "learn_to_program/ch10-nothing-new/shuffle"
+require_relative "../ch10-nothing-new/shuffle"
 music_files = Dir["Random/*.mp3"]
 
 filename = 'playlist-shuffle.m3u'

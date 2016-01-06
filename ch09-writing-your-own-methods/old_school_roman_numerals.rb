221923d614c5b321c9df0036d59d7b339ab3a2ef
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-06 16:29:13 +0000

    ch09-writing-your-own-methods/old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,11 @@
 def old_roman_numeral num
-  # your code here
-end
+	to_return = ""
+	to_return << "M"*(num        / 1000) 
+	to_return << "D"*(num % 1000 /  500) 
+	to_return << "C"*(num % 500  /  100) 
+	to_return << "L"*(num % 100  /   50) 
+	to_return << "X"*(num % 50   /   10) 
+	to_return << "V"*(num % 10   /    5) 
+	to_return << "I"*(num % 5    /    1) 
+	to_return
+end 

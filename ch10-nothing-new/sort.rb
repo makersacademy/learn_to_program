a12b9f26c178c2a7d36cd6e3a02ee6130dddbbc7
esbaddeley <esbaddeley@gmail.com>
2016-01-06 15:23:39 +0000

    Completed ch10 qu 1 with Recursive sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,19 +1,43 @@
 def sort arr
-  plain_old_sort arr, []
+  recursive_sort arr, []
 end
 
 
-def plain_old_sort unsorted, sorted
+# def plain_old_sort unsorted, sorted
+#
+#   while unsorted.empty? == false
+#     unsorted.each do |element|
+#       if unsorted.any? {|comparison| comparison < element } == false
+#       sorted << element
+#       unsorted.delete_at(unsorted.index(element))
+#       else
+#       end
+#     end
+#   break if unsorted.empty?
+#   end
+#   return sorted
+# end
 
-  while unsorted.empty? == false
-    unsorted.each do |element|
-      if unsorted.any? {|comparison| comparison < element } == false
-      sorted << element
-      unsorted.delete_at(unsorted.index(element))
-      else
-      end
-    end
-  break if unsorted.empty?
+def recursive_sort unsorted, sorted
+
+  if unsorted.empty? == true
+    return sorted
   end
-  return sorted
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |test|
+    if test < smallest
+      still_unsorted.push smallest
+      smallest = test
+    else
+      still_unsorted.push test
+    end
+  end
+
+  sorted.push smallest
+
+  recursive_sort still_unsorted, sorted
+
 end

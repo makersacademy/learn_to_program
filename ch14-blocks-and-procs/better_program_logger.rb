248be969be3d54cbcad7935166fd3f61a9b67d0a
Henry Gordon <hwgordon247@gmail.com>
2016-01-06 12:19:48 +0000

    chapter 14 Better logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
-def log desc, &block
-  # your code here
+$nesting = 0
+
+def better_log desc, &block
+  puts (' '*$nesting)+'Beginning "'+ desc +'"...'
+  $nesting += 1
+  final = block.call
+  $nesting -= 1
+  puts (' '*$nesting)+'..."'+desc+'" finished, returning: ' + final.to_s
+
+
 end
+
+better_log "outer block" do
+  better_log "some little block" do
+    better_log "teeny-tiny block" do
+      'lots of love'
+    end
+    5
+  end
+  better_log "yet another block" do
+    'I like Thai food!'
+  end
+  false
+end
+

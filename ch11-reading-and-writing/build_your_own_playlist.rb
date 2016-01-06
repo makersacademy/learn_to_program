5e3f39072f87af7ccb7338d90596a667d7e41f9d
Iryna Howarth <irynahowarth@gmail.com>
2016-01-06 23:58:04 +0000

    ch11 - 02 solved

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,23 @@
-# your code here
+def shuffle arr
+  recursive_shuffle arr, []
+end
+
+def recursive_shuffle not_shuffled, shuffled
+  return shuffled if not_shuffled.size == 0
+  prng = Random.new
+  i = prng.rand(0...not_shuffled.size)
+  shuffled << not_shuffled[i]
+  not_shuffled.delete_at(i)
+  recursive_shuffle not_shuffled, shuffled
+end
+
+Dir.chdir '/Users/irina/Music/iTunes/iTunes Media/Music/'
+pic_name = shuffle(Dir['../**/*.mp3'])
+
+File.open("playlist.m3u", "w") do |f|
+  pic_name.each do |x|
+    f.write x+"\n"
+  end
+end
+
+puts "Playlist was created in directory: #{Dir.pwd}"

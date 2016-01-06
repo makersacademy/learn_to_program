fe7cb9bc7aaf1b2ef408b05493bb1c2972f45e1b
esbaddeley <esbaddeley@gmail.com>
2016-01-06 17:46:16 +0000

    Completed ch10 qu3 Dictionary Sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
 def dictionary_sort arr
-  # your code here
-end
+
+    unsorted = arr
+    sorted = []
+
+    while unsorted.empty? == false
+      unsorted.each do |element|
+        if unsorted.any? {|comparison| comparison.downcase < element.downcase } == false
+        sorted << element
+        unsorted.delete_at(unsorted.index(element))
+        else
+        end
+      end
+    break if unsorted.empty?
+    end
+    return sorted
+  end

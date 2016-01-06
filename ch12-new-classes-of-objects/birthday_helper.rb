a5813f5cb9ab6e904fe4115395fcd58532b24a08
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-06 23:13:13 +0000

    ch12 birthday_helper completed

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,22 @@
-# your code here
+imported_text = File.read "/Users/GotWai/Desktop/people.txt"
+
+birth_dates = Hash.new
+
+imported_text.each_line do |x|
+	temp_array = x.split(", ")
+	birth_dates[temp_array[0]] = [temp_array[1], temp_array[2]]
+end
+
+puts "Please enter a name"
+answer = gets.chomp.downcase
+
+found = 0
+
+birth_dates.each do |k, v|
+	if answer == k.downcase
+		puts "#{k} has a birthday on #{v[0]}."
+		found = 1
+		break
+	end
+end
+puts "Don't know that person's birthday" if found != 1

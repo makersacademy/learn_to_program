dd1b091796829bc20e41672e528c756aeaaf7a2d
Yan-Yi Li <liyanyi@gmail.com>
2016-01-06 20:34:28 +0000

    completed ch12 one_billion_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,5 @@
-# your code here
+born = Time.local(1985, 2, 8, 7, 58)
+one_billion_old = born + 10**9
+
+puts "Time you were born: #{born}"
+puts "Time when you will turn/turned one billion seconds old: #{one_billion_old}"

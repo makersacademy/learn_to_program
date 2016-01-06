f4a896a29e85943d991445b40544d6fb35743966
seanhawkridge <sean@hawkridge.co>
2016-01-06 19:14:14 +0000

    Fixing Ch12 better program logger to pass rspec

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,8 +1,8 @@
 
 $depth = 0
 
-def log desc, &block
-  spacing = '   '*$depth
+def better_log desc, &block
+  spacing = ' '*$depth
   puts spacing + 'Beginning "' + desc + '"...'
     $depth = $depth + 1
     result = block.call
@@ -10,15 +10,15 @@ def log desc, &block
   puts spacing + '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
-log 'The long winter' do
-  log 'Days til Christmas' do
-    log 'Christmas Elf' do
+better_log 'The long winter' do
+  better_log 'Days til Christmas' do
+    better_log 'Christmas Elf' do
       "<|:)"
     end
     y = Time.new.year
     (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
   end
-  log 'Excitement Level' do
+  better_log 'Excitement Level' do
     y = Time.new.year
     dtc = (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
     "#{100 - dtc}%"

ee9d07d29f4e083d79da04f041ad51de1f525fff
Finble <elainefinn@hotmail.com>
2016-01-06 10:30:01 +0000

    old_school_numeral response

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,5 +1,20 @@
-def old_roman_numeral num
+def old_roman_numeral (num)
 	
-	# put code here
+	raise 'Number must be positive' if num <= 0
+	
+	roman_num = ''
 
+	roman_num << 'M' * (num/1000)
+	roman_num << 'D' * (num % 1000/500) 
+	roman_num << 'C' * (num % 500/100)
+	roman_num << 'L' * (num % 100/50)
+	roman_num << 'X' * (num % 50/10)
+	roman_num << 'V' * (num % 10/5)
+	roman_num << 'I' * (num % 5/1)
+
+	return roman_num
 end
+
+puts old_roman_numeral(1999)
+
+#have done exercise a lot in the past... 

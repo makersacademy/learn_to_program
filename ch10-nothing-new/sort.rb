5e4f02c7ff160536f7bd41dfad4540afad108fae
innlouvate <l.j.franklin85@googlemail.com>
2016-01-06 22:16:49 +0000

    sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,30 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+
+def recursive_sort unsorted_array, sorted_array
+  return unsorted_array if unsorted_array.length <=1
+
+  small = unsorted_array.pop
+  remainder_array = []
+  unsorted_array.each do |word|
+    if word < small
+      remainder_array << small
+      small = word
+    else
+      remainder_array << word
+    end
+  end
+
+  sorted_array << small
+
+  recursive_sort remainder_array, sorted_array
+
+  return sorted_array
+
+end
+
+
+
+puts sort %w(hello why how what bye seeya)

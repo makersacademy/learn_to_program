b2eac04f747753f2baf74845d59658cfc9081d9f
Finble <elainefinn@hotmail.com>
2016-01-06 16:27:29 +0000

    sort response + correction to dictionary_sort response

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,15 @@
 def sort arr
-  # your code here
+	return arr if arr.length <= 1
+
+	x = arr.pop
+	lower_sort = arr.select {|i|i < x}
+	higher_sort = arr.select {|i| i >= x}
+
+	sort(lower_sort) + x + sort(higher_sort)
 end
+
+# I went all over the place with this, but gave me a chance to review bubble, merge and divide & conquer sort methods
+
+
+
+

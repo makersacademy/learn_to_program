90ff132be2f0080daf03d0f4b2ac3d9497ccaa51
valogopedina <valentina.monetta@tiscali.it>
2016-01-06 14:26:47 +0000

    Second ch9

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,20 @@
 def old_roman_numeral num
-  # your code here
-end
+	result = []
+	if num <= 0
+		puts "Must be a positive number"
+	else
+		result << "M" * (num / 1000)
+		result << "D" * ((num % 1000) / 500)
+		result << "C" * ((num % 500) / 100)
+		result << "L" * ((num % 100) / 50)
+		result << "X" * ((num % 50) / 10)
+		result << "V" * ((num % 10) / 5)
+		result << "I" * ((num % 5) / 1)
+	end
+	 result.join
+end 
+
+
+
+
+ 

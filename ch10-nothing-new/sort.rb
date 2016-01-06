ee7358c6ebdbd27c3bde8cd14244e34e3acefdce
esbaddeley <esbaddeley@gmail.com>
2016-01-06 14:52:04 +0000

    Completed Ch10 qu1 Sort - without recursive sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,19 @@
 def sort arr
-  # your code here
+  plain_old_sort arr, []
+end
+
+
+def plain_old_sort unsorted, sorted
+
+  while unsorted.empty? == false
+    unsorted.each do |element|
+      if unsorted.any? {|comparison| comparison < element } == false
+      sorted << element
+      unsorted.delete_at(unsorted.index(element))
+      else
+      end
+    end
+  break if unsorted.empty?
+  end
+  return sorted
 end

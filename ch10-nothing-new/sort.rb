cd842744732ee89c3644bfda8e6ae2643ef47a09
hedudelgado <hedu.delgado@gmail.com>
2016-01-06 14:28:09 +0000

    ch10-nothing-new/sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,25 @@
 def sort arr
-  # your code here
+	sorting arr,[]
+end
+	
+def sorting (arrdirty,arrsorted)
+		
+	if arrdirty.length < 1
+		return arrsorted
+	end
+
+	mini = arrdirty.pop
+	unsorted = []
+
+	arrdirty.each do |a|
+		if a < mini
+			unsorted.push(mini)
+			mini = a
+		else
+			unsorted.push(a)
+		end
+	end
+arrsorted.push(mini)
+
+sorting unsorted,arrsorted # your code here
 end

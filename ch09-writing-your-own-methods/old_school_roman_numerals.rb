a0c9b073335715df8cdbaf7f572d13c896c4f47a
esbaddeley <esbaddeley@gmail.com>
2016-01-06 11:59:06 +0000

    Completed Ch09 qu 2

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,18 @@
 def old_roman_numeral num
-  # your code here
+  numeral_to_print = ''
+
+    if num == 0
+      numeral_to_print << 'Zero'
+    else
+      numeral_to_print << 'M' * (num/1000)
+      numeral_to_print << 'D' * (num % 1000 / 500)
+      numeral_to_print << 'C' * (num % 500 / 100)
+      numeral_to_print << 'L' * (num % 100 / 50)
+      numeral_to_print << 'X' * (num % 50 / 10)
+      numeral_to_print << 'V' * (num % 10 / 5)
+      numeral_to_print << 'I' * (num % 5)
+    end
+
+  return numeral_to_print
+
 end

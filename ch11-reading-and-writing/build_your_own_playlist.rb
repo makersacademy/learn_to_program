2154e7b9b8f7fe87152b49ea1f5bd6e242df74aa
Alex Avlonitis <avlonitis@msn.com>
2016-01-06 16:05:16 +0100

    my own shuffle

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,8 +1,21 @@
+def shuffle arr
+  mixer = Random.new
+  repeater = arr.size - 1
+  mixed = []
+  while arr.length > 0 
+    m = mixer.rand(0..arr.size - 1)
+    mixed << arr.delete_at(m)
+  end
+
+ mixed 
+
+end
+
 songs_path_dir = "C:/Users/Public/Music/Sample Music/"
 
 song_names =  Dir[songs_path_dir + '*.mp3']
 
-song_names.shuffle!
+song_names = shuffle song_names
 
 filename = 'playlist.m3u'
 

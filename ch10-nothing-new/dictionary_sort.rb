e0febc6ee91c0bbfce1f59511f8d48be0767480b
mo <mdeerow@gmail.com>
2016-01-06 14:18:04 +0000

    10-3

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
 def dictionary_sort arr
-  # your code here
+    dictionary_array = []
+  
+    while arr.empty? == false
+        dictionary_array << arr.min
+        arr.delete_at(arr.index(arr.min))
+    end
+  
+    dictionary_array
 end

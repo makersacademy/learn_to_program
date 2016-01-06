55ece7d3133499baf1333858514c834bfea08970
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-06 22:03:37 +0000

    engnum/betterprof

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -3,65 +3,115 @@
 #this is the file we are given have to find a way to do above
 
 
-
 def english_number number
   if number < 0
-    return "not today young man"
+    return 'Please enter a number that isn\'t negative.'
   end
+  if number == 0
+    return 'zero'
+  end
+
+  num_string = ''
+
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
+
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
+
+
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
+
+  others = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
+
+  left = number
+
+  while others.length > 0
+    pair = others.pop
+    name = pair[0]
+    base = 10 ** pair[1]
+    write = left/base
+    left = left - write * base
+
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
+
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+
+
+  num_string
 end
 
-num_string = " "
-@ones_place = ['zero', 'one', 'two', 'three',
-              'four', 'five', 'six',
-              'seven', 'eight', 'nine']
-
-@tens_place = ['ten', 'twenty', 'thirty',
-              'forty', 'fifty', 'sixty',
-              'seventy', 'eighty', 'ninety']
 
 
-@teenagers = ['eleven', 'twelve', 'thirteen',
-             'fourteen', 'fifteen', 'sixteen',
-             'seventeen', 'eighteen', 'nineteen']
-
-@others =    [['hundred', 2],
-            ['thousand', 3],
-            ['million', 6],
-            ['billion', 9],
-            ['trillion', 12],
-            ['quadrillion', 15],
-            ['quintillion', 18],
-            ['sextillion', 21],
-            ['septillion', 24],
-            ['octillion', 27],
-            ['nonillion', 30],
-            ['decillion', 33],
-            ['undecillion', 36],
-            ['duodecillion', 39],
-            ['tredecillion', 42],
-            ['quattuordecillion', 45],
-            ['quindecillion', 48],
-            ['sexdecillion', 51],
-            ['septendecillion', 54],
-            ['octodecillion', 57],
-            ['novemdecillion', 60],
-            ['vigintillion', 63],
-            ['googol', 100]]
-
-numbers = [@ones_place, @tens_place]
-
-def recursion (accumalator = "", remainder = 0)
-  x = @ones_place[remainder]
-  accumalator += x
-  return accumalator if remainder - 10 <= 0
-  recursion accumalator, remainder
-end
-
-#return if ones_place
-#call recursion with arguments accumalator, remainder
-
-
-
-
-#index of arrays (n-1)
-[1,1,1].last
+# numbers = [@ones_place, @tens_place]
+#
+# def recursion (accumalator = "", remainder = 0)
+#   x = @ones_place[remainder]
+#   accumalator += x
+#   return accumalator if remainder - 10 <= 0
+#   recursion accumalator, remainder
+# end
+#
+# #return if ones_place
+# #call recursion with arguments accumalator, remainder
+#
+#
+#
+#
+# #index of arrays (n-1)
+# [1,1,1].last
--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,37 @@
+#
+# def profile (block_description, &block)
+#   start_time = Time.new
+#   block.call if true
+#   duration = Time.new - start_time
+#   puts "#{block_description}: #{duration} seconds"
+# end
+
+
 def profile block_description, &block
-  # your code here
+  profiling_on = false
+  if profiling_on
+    start_time = Time.new
+    block.call
+
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+# profile 'count' do
+#   number = 1
+#   25000.times do
+#     number = number + number
+#   end
+#
+# puts "#{number.to_s.length} digits"
+# end
+#
+# profile 'count to a million' do
+#   number = 0
+#   100000.times do
+#     number = number + 1
+#   end
+# end

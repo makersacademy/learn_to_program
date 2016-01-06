4151741b82fec391d954416ede298feda5493981
seanhawkridge <sean@hawkridge.co>
2016-01-06 10:46:04 +0000

    Ch12 roman to integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,25 @@
 def roman_to_integer roman
-  # your code here
+  digit_vals = {'i' => 1,
+    'v' => 5,
+    'x' => 10,
+    'l' => 50,
+    'c' => 100,
+    'd' => 500,
+    'm' => 1000
+  }
+
+      nums = roman.downcase.split('').map do |r|
+        r = digit_vals.select{|k,v| r==k }
+        numbers_array = r.values.join.to_i
+      end
+
+      nums.map.with_index do |n,i|
+       if  n > nums[i-1] && i > 0
+         n - (nums[i-1]*2)
+      else
+        n
+      end
+    end.inject(:+)
 end
+
+p roman_to_integer('MDCLIV')

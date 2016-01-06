0205f7923d6ad634d367647d04e520f80034c2a7
Reiss Johnson <reissjohnson@me.com>
2016-01-06 17:30:29 +0000

    added solution for ch11 challenge 2 had to check solution in order to find right syntax got close

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,17 @@
-# your code here
+def shuffle arr
+	shuffled_arr = Array.new
+	shuffled_arr = arr.sort_by{rand}
+	shuffled_arr
+end
+
+
+aiffs = Dir['/Users/reissjohnson/desktop/projects/music_test/*.aiff']
+all_aiffs = shuffle aiffs
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_aiffs.each do |aiff|
+    f.write aiff+"\n"
+  end
+end
+
+puts 'Done!'

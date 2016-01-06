1859ebc0773a96afd134bcfccca3096bc42017eb
seanhawkridge <sean@hawkridge.co>
2016-01-06 10:45:10 +0000

    Ch12 happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,10 @@
-# your code here
+
+puts "What year were you born in?"
+year = gets.chomp
+puts "And the month?"
+month = gets.chomp
+puts "And the day?"
+day = gets.chomp
+age = ((Time.new - Time.new(year,month,day))/365.25/24/60/60).to_i
+puts "You're #{age}"
+puts "SPANK! "*age

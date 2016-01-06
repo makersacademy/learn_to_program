8484c051d8a95d15447ff5d2544346873190e60a
Alex Avlonitis <avlonitis@msn.com>
2016-01-06 14:50:49 +0100

    ch11.02

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
-# your code here
+songs_path_dir = "C:/Users/Public/Music/Sample Music/"
+
+song_names =  Dir[songs_path_dir + '*.mp3']
+
+song_names.shuffle!
+
+filename = 'playlist.m3u'
+
+File.open filename, 'w' do |song|
+	song.puts(song_names)
+end

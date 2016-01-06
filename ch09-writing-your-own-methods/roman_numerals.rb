45689550e0941f6a41d737016379817cb464dc16
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-06 16:35:30 +0000

    ch09-writing-your-own-methods/roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,25 @@
 def roman_numeral num
-  # your code here
+
+	roman_numerals = {
+    1000 => "M",  
+     900 => "CM",  
+     500 => "D",  
+     400 => "CD",
+     100 => "C",  
+      90 => "XC",  
+      50 => "L",  
+      40 => "XL",  
+      10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I",  
+    }
+
+  	to_return = ""
+  	roman_numerals.each do | number, letter |
+  		to_return << letter*(num/number)
+  		num = num % number
+  	end
+  	to_return
 end

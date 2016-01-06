64a60f6dbdf6742c7b5023fd6c1ef27d0e02cf72
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-06 18:24:11 +0100

    A spank for every birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What was the year that you were born?"
+year = gets.chomp
+puts "What was the month"
+month = gets.chomp
+puts "And what day"
+day = gets.chomp
+t1 = Time.new(year, month, day)
+t2 = Time.new
+birthdays = t2.year - t1.year
+birthdays -= 1 if t1.yday > t2.yday
+print "SPANK!" * birthdays
+puts

27a895b428f8094893a8e9a9e7280b8faa9f92b1
Marcus Bullock <marcusbullock@hotmail.co.uk>
2016-01-06 15:53:05 +0000

    Completed 12:2

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+puts "Tell me the year you were born"             
+year = gets.chomp 
+puts "month"
+month = gets.chomp
+puts "day"
+day = gets.chomp
+
+birthday = Time.gm(year, month, day, 0, 0, 0)  
+now = Time.now 
+
+age_in_seconds = now - birthday
+seconds_in_year = 60*60*24*365
+
+age = age_in_seconds / seconds_in_year
+
+age.floor.times do #need to floor it so it knows that 7.9 year olds are still 7
+  puts "SPANK!"
+end
+
+

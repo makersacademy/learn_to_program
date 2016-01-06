a8cb94916e9d76ae41dcc446aa4ffb1fb7107eab
seanhawkridge <sean@hawkridge.co>
2016-01-06 18:07:29 +0000

    Program loggers

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,24 @@
+
+$depth = 0
+
 def log desc, &block
-  # your code here
+  spacing = '   '*$depth
+  puts spacing + 'Beginning "' + desc + '"...'
+    $depth = $depth + 1
+    result = block.call
+    $depth = $depth - 1
+  puts spacing + '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'The long winter' do
+  log 'Days til Christmas' do
+    y = Time.new.year
+    (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
+  end
+  log 'Excitement Level' do
+    y = Time.new.year
+    dtc = (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
+    "#{100 - dtc}%"
+  end
+  ":("
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
 def log desc, &block
-  # your code here
+  puts 'Beginning "' + desc + '"...'
+    result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'The long winter' do
+  log 'Days til Christmas' do
+    y = Time.new.year
+    (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
+  end
+  log 'Excitement Level' do
+    y = Time.new.year
+    dtc = (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
+    "#{100 - dtc}%"
+  end
+  ":("
 end

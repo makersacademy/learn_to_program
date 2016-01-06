47ec2308fcfc919f5285fd569a0dc02abeb51454
seanhawkridge <sean@hawkridge.co>
2016-01-06 10:44:01 +0000

    Ch11 build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,10 +1,11 @@
 
 Dir.chdir '/Users/seanhawkridge/Desktop/playlists'
-playlist_file = File.new "playlist.txt","w"
+playlist_file = File.new "playlist.mpu","w"
 song_names = Dir["/Users/seanhawkridge/Music/iTunes/iTunes\ Media/Music/**/*.mp3"][0..20]
 
 random = song_names.sort_by{rand}
 
 random.each do |song|
-playlist_file << "#{song}\n"
+  song_title = song.split('/')[-3..-1].join('/')
+  playlist_file << "#{song}\n"
 end

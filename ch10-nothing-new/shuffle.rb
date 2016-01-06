48db125487d9c4c9f4b2efdef80e197977dafc07
Alex Avlonitis <AlexAvlonitis@users.noreply.github.com>
2016-01-06 17:13:28 +0000

    Update shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,11 +1,10 @@
 def shuffle arr
-  repeater = arr.size - 1
   mixed = []
   while arr.length > 0 
-    m = mixer.rand(0..repeater)
+    m = rand(0..arr.size - 1)
     mixed << arr.delete_at(m)
   end
-
- mixed 
+  
+  mixed 
 
 end

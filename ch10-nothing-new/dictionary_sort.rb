369a6c44b1d7a4f4efc5481a574f5e34be606a8d
Matt <mattpgough@gmail.com>
2016-01-06 11:55:49 +0000

    ch10 dictionary_sort.rb Solution

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,11 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+	if array.length <=1
+		return array
+	else
+	middle = array.pop
+	less = array.select{|x|x.downcase<middle.downcase}
+	more = array.select{|x|x.downcase>=middle.downcase}
+
+	dictionary_sort(less) + [middle] + dictionary_sort(more)
+    end
 end

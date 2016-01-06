0e7e2b275aa02eb40aa78eca4c4e92b6e0e014e5
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-06 22:38:00 +0100

    Added solutions for ch12 one billion seconds and ch12 happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "When is your bithday?"
+puts "Year (YYYY):"
+year = gets.chomp
+puts "Month (MM):"
+month = gets.chomp
+puts "Day (DD):"
+day = gets.chomp
+puts "Thanks! Preparing birthday spanks..."
+
+years_lived = Time.now.to_s.slice(0, 4).to_i - year.to_i
+current_month = Time.now.to_s.slice(6, 8).to_i
+current_day = Time.now.to_s.slice(10, 12).to_i
+
+(years_lived - 1).times do
+	puts "SPANK!"
+end
+
+puts "SPANK" if current_month > month.to_i and current_day > day.to_i

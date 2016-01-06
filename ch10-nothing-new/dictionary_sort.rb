13e54bc776e1157ca226fcb09fd1a5244514ba0f
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-06 10:57:39 +0000

    Completed dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,21 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  return sorted if unsorted.length <= 0
+
+  sorting = unsorted.pop
+  not_sorted = []
+
+  unsorted.each { |x|
+    #as long as it checks lowercase versions of the words, it will sort the words in order, regardless of capital letters.
+    if x.downcase < sorting.downcase
+      not_sorted << sorting
+      sorting = x
+    else
+      not_sorted << x
+    end }
+  sorted << sorting
+  recursive_sort not_sorted, sorted
 end

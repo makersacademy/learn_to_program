2d0cce39c1c3ef6f23d617828bb40314c9bd9d44
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-06 23:40:57 +0000

    ch13 extend_built_in_classes completed

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,23 @@
 class Integer
-  # your code here
+
+	def to_roman
+	  output = ""
+	  roman = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1 }
+	  num = self
+	  roman.each do |k, v|
+	    if num / v > 0
+	  	  output << k*(num/v)
+	  	  num = num % v
+	    end
+	  end
+	  output
+	end
+
+  def factorial
+		num = self
+		return "Can't process negative numbers" if num < 0
+		return 1 if num <= 1
+		return num * (num - 1).factorial
+  end
+
 end

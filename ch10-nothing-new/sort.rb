873e6756c40eee58eeb132fc8bc44380a0e50f7b
Matt <mattpgough@gmail.com>
2016-01-06 10:25:24 +0000

    ch10 sort.rb Solution

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
-def sort arr
-  # your code here
+def sort array
+	if array.length <=1
+		return array
+	else
+	middle = array.pop
+	less = array.select{|x|x<middle}
+	more = array.select{|x|x>=middle}
+
+	sort(less) + [middle] + sort(more)
+    end
 end

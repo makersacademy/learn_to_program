4dce83805875a5803ca225340d25174045c673b7
esbaddeley <esbaddeley@gmail.com>
2016-01-06 17:34:01 +0000

    Completed ch10 qu2 - Shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,20 @@
 def shuffle arr
-  # your code here
+  arr.shuffle
 end
+
+
+
+
+
+
+
+
+
+# def shuffle arr
+#     shuffled_array = arr
+#     shuffled_array.each do |element|
+#       index = rand(shuffled_array.length)
+#       shuffled_array.insert(index, shuffled_array.delete_at(shuffled_array.index(element))
+#       end
+#     return shuffled_array
+# end

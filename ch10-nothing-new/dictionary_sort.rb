de11bc55b695a74ec8f824cc6f198734100dd0ab
Finble <elainefinn@hotmail.com>
2016-01-06 16:15:00 +0000

    dictionary_sort reponse

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,15 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arr)
+	return arr if arr.length <= 1
+
+	x = arr.pop
+	lower_sort = arr.select {|i|i < x}
+	higher_sort = arr.select {|i| i >= x}
+
+	dictionary_sort(lower_sort) + x + dictionary_sort(higher_sort)
 end
+
+# I went all over the place with this, but gave me a chance to review bubble, merge and divide & conquer sort methods
+
+
+
+

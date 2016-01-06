bba527ee018680ad12e175ccbb72aa6a26012993
mo <mdeerow@gmail.com>
2016-01-06 13:12:19 +0000

    10-1

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
 def sort arr
-  # your code here
+    sorted_array = []
+  
+    while arr.empty? == false
+        sorted_array << arr.min
+        arr.delete_at(arr.index(arr.min))
+    end
+  
+    sorted_array
+  
 end

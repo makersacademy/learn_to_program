55eaab776651886d1f6753fd73a977ba466459b8
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-06 21:02:21 +0000

    ch11 build_a_better_playlist completed

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,21 @@
 def music_shuffle filenames
-  # your code here
+  # doing this via the deck of cards split method
+
+filenames = filenames.sort
+
+ 3.times do #repeat this 3 times. More times will increase the randomness and lessen the mix.
+  	left = []
+  	right = []
+  	filenames.each do |x|
+  		if rand(2).even?  #randomly assign each song to left or right array
+  			left << x
+  		else
+  			right << x
+  		end
+  	filenames = left + right #combine the two arrays
+  	end
+  end
+  filenames
 end
+
+#puts music_shuffle ['aa/bbb', 'aa/ccc', 'aa/ddd','AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']

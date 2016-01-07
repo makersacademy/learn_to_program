7c9e7d7e533a9f7388d956c570f35d8384de312e
Finble <elainefinn@hotmail.com>
2016-01-07 20:55:49 +0000

    ch10 - beerbotts - rspec passed

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,37 @@
-# your code here
+class Beersong
+
+	attr_accessor :beer
+
+	def initialize(beer)
+		@beer = beer
+	end
+
+	def english_number(number)
+		if number < 0
+			return 'Please enter a positive number'
+		end
+
+		if number == 0
+			puts 'There are no bottles of beer on the wall!'
+			return 'zero'
+		end
+
+		num_at_start = 99
+		num_now > 2
+		
+		while num_now > 2
+
+				puts english_number(num_now) + ' bottles of beer on the wall,'
+				puts english_number(num_now) + ' bottles of beer!'
+				num_now = num_now - 1
+				puts 'Take one down, pass it around,' 
+				puts english_number(num_now) + ' bottles of beer on the wall!'
+
+		end
+	end
+end
+
+# answer as provided in RubyKickstarter
+
+
+

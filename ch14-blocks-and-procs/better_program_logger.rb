51fd8821c8ed6012ad113d4f845171e7b21b06fa
russellvaughan <russellvaughan@gmail.com>
2016-01-07 18:18:18 +0000

    program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,6 +1,6 @@
 $logger_depth = 0
 
-def better_log desc, &block
+def log desc, &block
   prefix = ' '*$logger_depth
   puts prefix+"Beginning #{desc.inspect}..."
   $logger_depth += 1
@@ -9,15 +9,15 @@ def better_log desc, &block
   puts prefix+"...#{desc.inspect} finished, returning: #{result}"
 end
 
-better_log 'outer block' do
-better_log 'some little block' do
-better_log 'teeny-tiny block' do
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
       'lOtS oF lOVe'.downcase
     end
     7 * 3 * 2
   end
 
-better_log 'yet another block' do
+  log 'yet another block' do
     '!doof naidnI evol I'.reverse
   end
   '0' == "0"

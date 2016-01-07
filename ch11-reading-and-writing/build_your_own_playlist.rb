4baba330a12f35f405c965c6bf3c787298a3b117
Mic Cassano <mic.cassano@gmail.com>
2016-01-07 12:42:32 +0000

    Add answer to ch11/build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,23 @@
-# your code here
+Dir.chdir '~/Music/Playlists'
+
+def shuffle arr
+  rec_shuffle arr, []
+end
+
+def rec_shuffle unsorted_arr, sorted_arr
+  sorted_arr << unsorted_arr.delete_at(rand(0...unsorted_arr.size))
+
+  rec_shuffle unsorted_arr, sorted_arr unless unsorted_arr.size == 0
+  sorted_arr
+end
+
+
+filename = jazz_playlist.m3u
+song_names = Dir['~/Music/Jazz/**/*.mp3']
+shuffle song_names
+
+File.open filename, 'w' do |f|
+  song_names.each do |song|
+    f.write(song)
+  end
+end

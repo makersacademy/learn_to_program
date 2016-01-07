89ce54811ef792cf26b6fa6fe5d65f664c7e95c9
arnold manzano <arnold.manzano@gmail.com>
2016-01-07 18:28:54 +0000

    Answered ch09 roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,18 @@
 def roman_numeral num
-  # your code here
+
+  hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+  tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+  ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+
+  output_roman = ""
+
+  output_roman << "M" * (num/1000)
+  output_roman << hundreds[(num%1000/100)]
+  output_roman << tens[(num%100/10)]
+  output_roman << ones[(num%10)]
+
+  output_roman
+
 end
+

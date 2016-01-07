58d3559c69e8dd58b317e55bb03de2a2e2f62a5b
Finble <elainefinn@hotmail.com>
2016-01-07 19:55:34 +0000

    ch11 - saferpic - no solution

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,23 @@
-# your code here
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What should this batch be called?'
+batch_name = gets.chomp
+
+puts
+print 'Downloading #{pic_names.length} files: '
+
+pic_number = 1
+
+pic_names.each do |name|
+	print '.'
+	new_name = if pic_number < 10
+		'#{batch_name}0#{pic_number}.jpg'
+	else
+		'#{batch_name}#{pic_number}.jpg'
+	end
+
+	File.rename name, new_name
+	pic_number = pic_number + 1
+end
+
+# so I need to set up a jpg file (and adapt) to make this work - looked at CP solution which blew my mind...

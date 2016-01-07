e26cda65aa62b1dbf736c15ffb5820f9634bc709
David Parry <drjparry@gmail.com>
2016-01-07 19:25:07 +0100

    added better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,14 +1,32 @@
 def profile block_description, &block
-  # To turn profiling on/off, set this
-  # to true/false.
-  profiling_on = false
-  if profiling_on
-    start_time = Time.new
-    block.call
 
-    duration = Time.new - start_time
-    puts "#{block_description}: #{duration} seconds"
-  else
-    block.call
-  end
+profile_on = true
+
+if profile_on
+
+ start_time = Time.new
+
+ block.call
+
+ duration = Time.new - start_time
+
+ puts "#{block_description}: #{duration} seconds"
+
+else
+
+block.call
+
+end
+
+end
+
+profile 'count to a million' do
+
+number = 0
+
+1000000.times do
+
+number = number + 1
+
+end
 end

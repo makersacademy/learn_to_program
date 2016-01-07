b81412ec24da69d159328c41a27f5b4504ac5c64
Finble <elainefinn@hotmail.com>
2016-01-07 21:38:55 +0000

    ch10 - sort - rspec passed

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,15 +1,14 @@
-def sort arr
+def sort(arr)
 	return arr if arr.length <= 1
 
 	x = arr.pop
 	lower_sort = arr.select {|i|i < x}
 	higher_sort = arr.select {|i| i >= x}
 
-	sort(lower_sort) + x + sort(higher_sort)
+	sort(lower_sort) + [x] + sort(higher_sort)
 end
 
 # I went all over the place with this, but gave me a chance to review bubble, merge and divide & conquer sort methods
-
-
+# nice error re return x not being in an array - took a while to fix that!
 
 

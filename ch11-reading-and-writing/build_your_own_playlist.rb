4cf6c29b44a705acedb2ee3b0f13af92de945bc0
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-07 00:58:31 +0000

    Completed Ch11 Build Your Own Playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,12 @@
-# your code here
+Dir.chdir '/Users/zeshanrasul/Documents/Music/'
+
+playlist_files = Dir['/Users/zeshanrasul/Documents/Music/**/*.mp3']
+playlist_files.shuffle!
+
+filename = 'playlist.m3u'
+
+File.open filename, 'w' do |f|
+  playlist_files.each do |file|
+      f.write file + "\n"
+    end
+end

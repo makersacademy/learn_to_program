a5d31c1ea8ff72c16c6bee4237f71d05e55085cd
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-07 19:33:18 +0100

    birthday helper requires textfile birthdays.txt

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,20 @@
-# your code here
+filename = "birthdays.txt"
+names_str = File.read filename
+names_hash = {}
+names_str.each_line do |line|
+  arr = line.split(',')
+  names_hash[arr[0]] = arr[1].lstrip + ',' + arr[2].chomp
+end
+
+puts "Enter a name, or part of it and I tell you the person's birthday"
+name = gets.chomp
+found = false
+names_hash.each do |key,value|
+  if key =~ /#{name}/i
+    puts "The birthday of #{key} is on #{value}"
+    found = true
+  end
+end
+if found == false
+  puts "This name is not in my list"
+end

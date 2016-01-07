d438d76d0d0fcd45f6c531948bd2d7190871f41a
arnold manzano <arnold.manzano@gmail.com>
2016-01-07 18:28:23 +0000

    Answered ch09 old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,20 @@
 def old_roman_numeral num
-  # your code here
+
+  output_roman = ""
+
+  thousand = num / 1000
+  hundred = num % 1000 / 100
+  tens = num % 100 / 10
+  ones = num % 10
+
+  output_roman << "M" * thousand
+  output_roman << "D" if hundred >= 5
+  output_roman << "C" * (hundred % 5)
+  output_roman << "L" if tens >= 5
+  output_roman << "X" * (tens % 5)
+  output_roman << "V" if ones >= 5
+  output_roman << "I" * (ones % 5)
+
+  output_roman
+
 end

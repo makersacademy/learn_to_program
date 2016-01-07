e686963cfe2debba0fbd9d2ec8285192074a1283
esbaddeley <esbaddeley@gmail.com>
2016-01-07 17:22:19 +0000

    Completed Ch 12 Qu 1, 2 3

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+Puts "Please enter the year you were born"
+b_year = gets.chomp.to_i
+
+Puts "Please enter the month you were born"
+b_month = gets.chomp.to_i
+
+Puts "Please enter the day you were born"
+b_day = gets.chomp.to_i
+
+persons_birthday = Time.gm(year, month, day)
+years_old = (Time.now - persons_birthday)/(60*60*24*365)
+print "Spank!" * years_old
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,2 @@
-# your code here
+my_birthday = Time.new(1989, 3, 2, 9, 30)
+my_billionth_birthday = my_birthday + (10 ** 9)

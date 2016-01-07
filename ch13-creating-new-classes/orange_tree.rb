3097185ff62bc109d5d1b8323899f485cb1381ed
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-07 15:29:40 +0000

    ch 13 amended file - orange tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -3,10 +3,48 @@ class OrangeTree
     @height = rand(5)
     @age = 0
     @oranges = 0
+    count_the_oranges
+  end
 
+  def count_the_oranges
+    if @oranges > 0
+      puts "You have #{@oranges} on your tree, would you like to pick one?"
+      #to make this interactive remove #
+      #answer = gets.chomp
+      #if answer.downcase.include? "yes"
+      #  pick_an_orange
+      #else
+      #  one_year_passes
+      #end
+    else
+      puts "There are no oranges on your tree this year."
+      one_year_passes
+  end
+
+  def pick_an_orange
+    if @oranges > 1
+      @oranges -= 1
+      puts "You pick a delicious looking orange, peel and eat it. Mmmm vitamin C goodness!"
+    elsif @oranges == 1
+      puts "The last orange of the year, you pick it from the tree, peel and eat it. Mmmm, why is the last one always the most delicious?"
+    else
+      puts "There are no more organges this year :("
+      one_year_passes
+  end
+
+
+  private
+
+  def death?
+    #after a certain number of years the tree dies
+    if @age >= 25
+      puts "The tree is old and dies afer a happy life of sunshine and soil. It will grow no more oranges."
+      exit
+    end
   end
 
   def one_year_passes
+    puts "Autumn changes to Winter and your tree is dark and bare. Before you know it the seasons turn again and it is Spring!"
     #ages the tree one year
     @age += 1
     puts "Your tree is now #{@age} years old"
@@ -17,14 +55,7 @@ class OrangeTree
     @oranges = 0
     oranges?
     death?
-  end
-
-  def death?
-    #after a certain number of years the tree dies
-    if @age >= 25
-      puts "The tree is old and dies afer a happy life of sunshine and soil. It will grow no more oranges."
-      exit
-    end
+    count_the_oranges
   end
 
   def oranges?
@@ -38,21 +69,10 @@ class OrangeTree
     end
   end
 
-  def count_the_oranges
-    if @oranges > 0
-      puts "You have #{@oranges} on your tree"
-    else
-      puts "There are no more oranges on your tree this year."
-  end
-
-  def pick_an_organge
-    if @oranges > 1
-      @oranges -= 1
-      puts "You pick a delicious looking orange, peel and eat it. Mmmm vitamin C goodness!"
-    elsif @oranges == 1
-      puts "The last orange of the year, you pick it from the tree, peel and eat it. Mmmm, why is the last one always the most delicious?"
-    else
-      puts "There are no more organges this year :("
-  end
-
 end
+
+tree = OrangeTree.new
+tree.pick_an_orange
+tree.count_the_oranges
+tree.pick_an_orange
+tree.pick_an_orange

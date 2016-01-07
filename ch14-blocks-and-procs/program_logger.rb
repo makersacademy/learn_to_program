ee19a5ad0e1a2ee7ef163a8365f7bd71c67f23e8
seanhawkridge <sean@hawkridge.co>
2016-01-07 10:40:51 +0000

    Updating Ch14 program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,15 +1,15 @@
-def log desc, &block
+def program_log desc, &block
   puts 'Beginning "' + desc + '"...'
     result = block.call
   puts '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
-log 'The long winter' do
-  log 'Days til Christmas' do
+program_log 'The long winter' do
+  program_log 'Days til Christmas' do
     y = Time.new.year
     (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
   end
-  log 'Excitement Level' do
+  program_log 'Excitement Level' do
     y = Time.new.year
     dtc = (((Time.new(y,12,25) - Time.new))/60/60/24).to_i
     "#{100 - dtc}%"

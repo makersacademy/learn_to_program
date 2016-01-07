909c9de0f8126cae6dd266c2b0cf645288461b1a
Reiss Johnson <reissjohnson@me.com>
2016-01-07 12:19:47 +0000

    added solution for happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp.to_i
+puts "And the month..."
+month = gets.chomp.to_i
+puts "and what day..."
+day = gets.chomp.to_i
+
+birthday = Time.local(year, month, day)
+
+current_time = Time.new 
+
+difference = current_time - birthday
+
+age = difference / 31536000
+
+puts
+
+puts "SPANK!\n" * age.to_i

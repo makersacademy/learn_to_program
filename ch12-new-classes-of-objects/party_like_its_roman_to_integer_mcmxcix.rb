86f9a51ac608ca2a5909514a95da7e872e70d8e7
Alex Avlonitis <avlonitis@msn.com>
2016-01-07 13:15:03 +0100

    roman to integer completed

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,38 @@
 def roman_to_integer roman
-  # your code here
+
+	roman = roman.upcase # to make sure everything is upcase before testing
+
+	hash = { 1000 => "M",
+			 900 => "CM",
+			 500 => "D",
+			 400 => "CD",
+			 100 => "C",
+			 90 => "XC",
+			 50 => "L",
+			 40 => "XL",
+			 10 => "X",
+			 9 => "IX",
+			 5 => "V",
+			 4 => "IV",
+			 1 => "I" }
+
+	hash = hash.invert # to swap the values with the keys, lazy to rebuild it
+
+	check = hash.keys  # isolate the keys to check on them
+	splitter = roman.upcase.chars  # array for each roman numeral
+
+	result = 0
+
+	if splitter.map {|x| check.include?(x)}.include?(false)  
+		raise ArgumentError.new("Not a valid roman numeral")
+	else
+		hash.each do |key, value|
+			while roman.index(key) == 0  # while the current key is at the begging of our roman string
+				result += value	     # add the corresponding value to the result integer
+				roman.slice!(key)		 # and delete the previously found roman letters to start all over again.
+			end
+		end	
+	end
+
+	result
 end

87736ad79ef9389e37eda10e301862bf4da2bbe6
Ina Tsetsova <inatsetsova@gmail.com>
2016-01-07 18:00:16 +0000

    finished happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp
+puts "What month were you born in?"
+month = gets.chomp
+puts "What day were you born in?"
+day = gets.chomp
+
+birthday = Time.gm(year, month,day)
+puts birthday
+spank = ((Time.new - birthday)/(60 * 60 *24* 365)).floor
+puts spank
+spank.times {puts "SPANK"}

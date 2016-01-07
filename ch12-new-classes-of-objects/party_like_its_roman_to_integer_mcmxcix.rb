8144af3a08d5e7a505b0d7877f28e0de64309646
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-07 01:02:39 +0000

    Completed Ch12 Party Like Its Roman To Integer MCMXCIX

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,35 @@
 def roman_to_integer roman
-  # your code here
+
+  rom_hash = {'M' =>1000,
+              'a'=>900,
+              'D'=>500,
+              'b'=>400,
+              'C'=>100,
+              'c'=>90,
+              'L'=>50,
+              'd'=>40,
+              'X'=>10,
+              'e'=>9,
+              'V'=>5,
+              'f'=>4,
+              'I'=>1}
+
+  roman.upcase!
+
+  #Sub out special exceptions
+  roman = roman.gsub("CM", "a")
+  roman = roman.gsub("CD", "b")
+  roman = roman.gsub("XC", "c")
+  roman = roman.gsub("XL", "d")
+  roman = roman.gsub("IX", "e")
+  roman = roman.gsub("IV", "f")
+
+  letters = roman.split("")
+  int = 0
+  letters.each do |letter|
+    int = int + rom_hash[letter]
+  end
+
+  int
+
 end

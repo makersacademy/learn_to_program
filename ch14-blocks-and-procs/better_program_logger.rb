fbce094e61f18a28e23880626c44e16ba0e4b43e
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-01-07 20:17:56 +0000

    ch14 better_program_logger completed

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
-def log desc, &block
-  # your code here
+$depth = ""
+def better_log desc, &block
+
+  puts "#{$depth}Beginning \"#{desc}\"..."
+  $depth = $depth + " "
+  result = block.call
+  $depth = $depth.slice(0, $depth.length - 1)
+  puts "#{$depth}...\"#{desc}\" finished, returning: #{result}"
+end
+
+
+better_log "outer block" do 
+	better_log "some little block" do
+		"5"
+	end
+
+	better_log "yet another block" do
+		"I like Thai food!"
+	end
+	false
+	
 end

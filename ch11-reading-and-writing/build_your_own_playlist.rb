226fe02355a9bc204f90a73dcccf920de852c3e8
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-07 21:47:28 +0000

    ch11-Build_your_own_playlist-first_commit

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,15 @@
-# your code here
+def shuffle arr 
+  arr.shuffle
+end
+
+Dir.chdir "/Users/jono442244/Music/Music/CodingPlaylist"
+
+all_mp3s = shuffle(Dir["*.mp3"])
+
+File.open "CodingTechno.m3u", "w" do | f |
+	all_mp3s.each do | mp3 |
+	f.write mp3+"\n"
+	end 
+end
+
+puts "Finito"

16147805d5b44a567080e2bbee8bde15536725f7
russellvaughan <russellvaughan@gmail.com>
2016-01-07 18:15:45 +0000

    program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,20 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+
+  puts "Beginning \"#{desc}\"..."
+  result = block.call
+  puts "...\"#{desc}\" finished, returning: " + result.to_s
+
 end
+
+
+program_log "outer block" do
+
+  program_log "some little" do
+      5
+  end
+    program_log "yet another block" do
+    "I like Thai food!"
+
+end
+end
+

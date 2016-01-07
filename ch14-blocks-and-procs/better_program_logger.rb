9cf1b6c49287cf7ac39c8053cee0fbcdf56d39c8
Yan-Yi Li <liyanyi@gmail.com>
2016-01-07 22:38:52 +0000

    ch14 better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
-def log desc, &block
-  # your code here
+$nesting_depth = 0
+def better_log desc, &block
+  puts "#{" "*$nesting_depth}Beginning \"#{desc}\"..."
+  $nesting_depth += 1
+  returned = block.call
+  $nesting_depth -= 1
+  puts "#{" "*$nesting_depth}...\"#{desc}\" finished, returning: #{returned}"
+end
+
+better_log "outer block" do
+  better_log "some little block" do
+    better_log "teeny-tiny block" do
+      "lots of love"
+    end
+    42
+  end
+  better_log "yet another block" do
+    "I like Indian food!"
+  end
+  true
 end

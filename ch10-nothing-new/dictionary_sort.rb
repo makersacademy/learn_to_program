c66eda118447c8621651ab5aa473f656c87383a4
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-01-07 11:35:38 +0000

    Dictionary Sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,37 @@
 def dictionary_sort arr
-  # your code here
+	recursive_sort arr, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+  # your code here
+  return sorted_array if unsorted_array.length <= 0
+  
+  small = unsorted_array.pop
+
+  still_2sort = []
+
+  unsorted_array.each do |e|
+
+  	if e.downcase < small.downcase
+
+  		still_2sort << small
+
+  		small = e
+
+  	else
+
+  		still_2sort << e
+
+  	end
+
+  end
+
+  sorted_array << small
+
+  # puts "#{unsorted_array}\n#{still_2sort}\n#{small}\n#{sorted_array}\n"
+
+  recursive_sort(still_2sort,sorted_array)
+
+end
+
+# puts dictionary_sort(['can','Feel','singing','like','a','Can'])

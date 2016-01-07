23b7512153a57cb07ad98048972a145787c4d7c3
esbaddeley <esbaddeley@gmail.com>
2016-01-07 11:18:31 +0000

    Completed chapter 11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,9 @@
-# your code here
+all_songs = shuffle(Dir['**/*.mp3']
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_songs.each do |song|
+    f.write song+"\n"
+  end
+end
+
+puts 'Done!'
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,32 @@
-# your code here
+Dir chdir '/Users/emmabaddeley/Pictures'
+
+pic_names = Dir[/F/Users/**/*.jpg]
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+pic_names.each do |name|
+  print '.' # This is our "progress bar".
+
+  if File.exist? (name)
+    puts
+    puts "There is already a picture named #{name}, the downloader will now exit"
+    exit
+  else
+    new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+    else
+    "#{batch_name}#{pic_number}.jpg"
+    end
+  end
+
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end
+
+puts
+puts "Done!"

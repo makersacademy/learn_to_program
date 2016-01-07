6be5b9a5b63558700b3e387653439f3040cd3e57
Marco Bagnasco <admin@hecticmojo.com>
2016-01-07 11:43:17 +0000

    Safer_pictures_downloading

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,61 @@
-# your code here
+#!/usr/local/bin ruby
+LOC_HOME = Dir.home
+
+Dir.exist?(File.join(LOC_HOME, "Desktop/pics")) ? "" : Dir.mkdir(File.join(LOC_HOME, "Desktop/pics"))
+
+Dir.chdir(File.join(LOC_HOME, "/Desktop/pics"))
+
+pic_names = Dir["#{LOC_HOME}/Pictures/*.jpg"]
+
+err   = false
+err_n = 0
+
+def exiting(err, err_n)
+  at_exit do 
+   puts "Some file could not be overwritten" if err
+   puts "Process Terminated with #{err_n} files not copied."
+  end
+end
+
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts 
+print "Downloading #{pic_names.size} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.' 
+  
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  if File.exist?("#{LOC_HOME}/Desktop/pics/#{new_name}") 
+  begin
+    err = true
+    err_n += 1
+    raise NameError.new "File #{new_name} exists."
+  rescue => e
+    puts e
+  end
+  else
+    File.rename(name, new_name) 
+  end
+  
+
+  pic_number += 1
+  
+  
+  
+ end
+
+ exiting(err, err_n)
+
+
+
+ 

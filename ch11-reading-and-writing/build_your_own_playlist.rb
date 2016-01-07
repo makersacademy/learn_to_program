05be7c685ba14a7c5071f1e4e78e6b9b9254b914
Finble <elainefinn@hotmail.com>
2016-01-07 19:13:12 +0000

    ch11 - build plylist response

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,13 @@
-# your code here
+def music_shuffle filenames
+  File.open 'filenames'.m3u 'w' do |f|
+  	all_mp3.each do |mp3|
+  		f.write mp3.sort_by{rand} + '\n'  #write method returns a string?
+  	
+  	end
+  end
+end
+
+puts 'Done!'
+
+#included shuffle method from earlier exercise
+

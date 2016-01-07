9b67e5bc86e17b62dde1d41ec1bc1d67ae2e756a
innlouvate <l.j.franklin85@googlemail.com>
2016-01-07 15:04:47 +0000

    playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,24 @@
-# your code here
+def shuffle arr
+  rec_shuffle arr, []
+end
+
+def rec_shuffle unshuffled_array, shuffled_array
+
+  if unshuffled_array.length <= 0
+    return shuffled_array
+  else
+    r=Random.rand(0...unshuffled_array.length)
+    shuffled_array << unshuffled_array[r]
+    unshuffled_array.delete_at(r)
+  end
+
+  rec_shuffle unshuffled_array, shuffled_array
+end
+
+playlist_file = "playlist.m3u"
+playlist = shuffle Dir['/Users/louisefranklin/Music/iTunes/iTunes\ Media/Music/**/*.m4a']
+puts playlist
+
+File.open playlist_file, 'w' do |f|
+  f.write(playlist.join("\n"))
+end

3ee3e12db8d88a738f515911241ae4d1d38f0c41
Finble <elainefinn@hotmail.com>
2016-01-07 21:43:10 +0000

    ch10 - dict sort - rspec passed

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,15 +1,16 @@
 def dictionary_sort(arr)
+
 	return arr if arr.length <= 1
 
 	x = arr.pop
-	lower_sort = arr.select {|i| i.downcase < x}
+	lower_sort = arr.select {|i|i.downcase < x}
 	higher_sort = arr.select {|i| i.downcase >= x}
 
-	dictionary_sort(lower_sort) + x + dictionary_sort(higher_sort)
+	dictionary_sort(lower_sort) + [x] + dictionary_sort(higher_sort)
 end
 
-# I went all over the place with this, but gave me a chance to review bubble, merge and divide & conquer sort methods
-# CORRECTED THIS - as did not take into account upper and lower case (albeit rspec passed tests?!)
-# How to put downcased words back into upper case...?!  
+
+# CORRECTED THIS (is an extension of sort method) - as did not take into account upper and lower case 
+# rspec passed
 
 

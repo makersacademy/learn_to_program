1dd223766a4c3fa759d6e2d7b0c61c0e93f1187a
hedudelgado <hedu.delgado@gmail.com>
2016-01-08 13:44:38 +0000

    build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,10 @@
 # your code here
+Dir.chdir'/Users/sorry/Downloads'
+mp3 = (Dir['/Users/sorry/Downloads/**/*.{mp3}']).sample(Dir['/Users/sorry/Downloads/**/*.{mp3}'].length)
+
+File.open 'playlist.m3u', 'w' do |f|
+	mp3.each do |x|
+		f.write x
+	end
+end
+puts 'finished! :D Enjoy!'

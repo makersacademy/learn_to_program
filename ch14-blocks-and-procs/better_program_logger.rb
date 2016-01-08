d41b6f6fc52aea72da3f1fb885c8b49097200439
Alex Avlonitis <avlonitis@msn.com>
2016-01-08 16:48:43 +0100

    finished

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,8 @@
-def log desc, &block
-  # your code here
+$nesting = 0
+def better_log desc, &block
+  puts "#{"\s"*$nesting}Beginning \"#{desc}\"..."
+  $nesting += 1 
+  output = block.call
+  $nesting -= 1 
+  puts "#{"\s"*$nesting}...\"#{desc}\" finished, returning: #{output}"
 end

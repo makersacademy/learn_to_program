b9f2e8f1944cd2acca43f88b7abc803ea5a368d6
valogopedina <valentina.monetta@tiscali.it>
2016-01-08 09:17:12 +0000

    ch10 - 1

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,24 @@
 def dictionary_sort arr
-  # your code here
+	recursive_dictionary_sort(arr, [])
 end
+
+
+def recursive_dictionary_sort(unsorted, sorted)
+ 
+  if unsorted.size == 0
+		return sorted
+	end
+ 	
+ 
+	m = unsorted.min {|x,y| x.downcase <=> y.downcase}
+
+	unsorted.delete_at(unsorted.find_index(m)) 	
+
+	sorted.push(m)
+
+	recursive_dictionary_sort(unsorted, sorted)
+
+end
+
+  
+

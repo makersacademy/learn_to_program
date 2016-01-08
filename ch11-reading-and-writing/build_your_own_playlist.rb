7dcf0cc4468d72a2ce32e6121b0cc8488603090e
Yan-Yi Li <liyanyi@gmail.com>
2016-01-08 17:16:43 +0000

    completed ch11 build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,18 @@
-# your code here
+def shuffle arr
+  to_return = []
+  while arr != []
+    random_index = rand(arr.length)
+    to_return << arr[random_index]
+    arr.delete_at(random_index)
+  end
+  to_return
+end
+
+songs = shuffle Dir["**/*.m3u"]
+filename = "playlist.txt"
+
+File.open filename, "w" do |f|
+  songs.each do |song|
+    f.write song + "\n"
+  end
+end

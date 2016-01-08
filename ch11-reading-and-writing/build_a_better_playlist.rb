b3d142460da51f6e83e49c430e0b32db14102a17
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-08 17:36:42 +0000

    Added solution to build a better program - had to  use chris pine solution

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,37 @@
 def music_shuffle filenames
-  # your code here
+  filenames = filenames.sort
+  len = filenames.length
+
+  2.times do
+    shuffled = []
+    lindex = 0
+    rindex = len/2
+
+    while shuffled.length < len
+      if shuffled.length.even?
+        shuffled << filenames[rindex]
+        rindex += 1
+      else
+        shuffled << filenames[lindex]
+        lindex += 1
+      end
+    end
+    filenames = shuffled
+  end
+
+  cut_point = rand(len)
+  count = 0
+  final = []
+
+  while count < len
+    final << filenames[(count+cut_point)%len]
+    count += 1
+  end
+
+  p final
+
 end
+
+filenames = [1,2,3,4,5,6,7]
+
+music_shuffle filenames

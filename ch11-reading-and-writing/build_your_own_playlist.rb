c968338e028ba86bfc39baf9a85a3f470a934ade
ViolaCrellin <viola.crellin@gmail.com>
2016-01-08 10:30:13 +0000

    Completed Ch11 'Build Your Own Playlist' - works when run

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,33 @@
-# your code here
+# Start by getting the program to find files in the music directory that match a user input. 
+require 'find'
+
+tunes_filepaths = []
+
+Dir.chdir("/Users/Viola/Music/iTunes/iTunes Media/")
+
+puts "Enter a keyword"
+keyword = gets.chomp.capitalize
+
+#Find.find("/Users/Viola/Music/iTunes/iTunes Media/") do |path|
+#	tunes_filepaths << path if path =~ /**/**/*#{keyword}*.mp3
+#end
+
+
+def shuffle arr
+	arr.sort_by{rand(arr.length)}
+end
+
+tunes_filepaths = Dir.glob("**/**/*#{keyword}*.mp3")
+
+puts tunes_filepaths
+
+tunes = shuffle(tunes_filepaths)
+
+
+File.open 'keywordplaylist.m3u', 'w' do |f|
+	tunes.each do |tune|
+		f.write tune+"\n"
+	end
+end
+
+puts "Playlist made using keyword '#{keyword}'"

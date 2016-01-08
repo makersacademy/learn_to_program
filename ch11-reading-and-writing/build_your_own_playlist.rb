7459608cbc36f6849dff30d5ec036bd7cc0c2d03
hedudelgado <hedu.delgado@gmail.com>
2016-01-08 15:03:42 +0000

    build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,10 +1,12 @@
 # your code here
+
+#this code actually mix up properly not like shuffle
 Dir.chdir'/Users/sorry/Downloads'
-mp3 = (Dir['/Users/sorry/Downloads/**/*.{mp3}']).sample(Dir['/Users/sorry/Downloads/**/*.{mp3}'].length)
+mp3 = (Dir['/Users/sorry/**/*.{mp3}']).sample(Dir['/Users/sorry/**/*.{mp3}'].length)
 
 File.open 'playlist.m3u', 'w' do |f|
 	mp3.each do |x|
-		f.write x
+		f.write x+"\n"
 	end
 end
 puts 'finished! :D Enjoy!'

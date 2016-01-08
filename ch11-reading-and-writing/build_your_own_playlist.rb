73030cd9267e23991909a17f10c4510aa5c45e06
Finble <elainefinn@hotmail.com>
2016-01-08 15:59:23 +0000

    ch11 - buildyrownplaylist - partial solution, no rspec tests

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,3 +1,6 @@
+# my incomplete solution below, but got timed out... no rspec tests
+
+
 def music_shuffle filenames
   File.open 'filenames.m3u', 'w' do |f|
   	all_mp3.each do |mp3|
@@ -9,5 +12,6 @@ end
 
 puts 'Done!'
 
-#included shuffle method from earlier exercise
+
+#included shuffle method from earlier exercise 
 

dcc20708ceb4a65bf67f91a0e0fe981e0f8a2fbf
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-08 21:35:13 +0000

    updated to correctly put the integer value of a roman numeral input

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,30 @@
-def roman_to_integer roman
-  # your code here
+def roman_to_integer roman 
+  digit_vals = {'i' => 1, 
+                'v' => 5,
+                'x' => 10,
+                'l' => 50,
+                'c' => 100,
+                'd' => 500,
+                'm' => 1000}
+  prev = 0
+  total = 0
+
+  roman.reverse.each_char do | char |
+    char.downcase!
+    numeral = digit_vals[char]
+    if !numeral
+      puts "Please enter valid roman numerals!"
+      return
+    end
+
+    if numeral < prev
+      numeral *= -1
+    else
+      prev = numeral
+    end
+
+    total += numeral
+  end
+  total
 end
+

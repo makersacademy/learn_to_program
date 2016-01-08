d1f4d727601a2684a6c83c845173648d05a0ffb2
valogopedina <valentina.monetta@tiscali.it>
2016-01-08 22:47:59 +0000

    ch12 -2

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,19 @@
-# your code here
+puts 'What year were you born?'
+a_year = gets.chomp.to_i
+
+puts 'What month were you born?(1-12)'
+a_month = gets.chomp.to_i
+
+puts 'What day were you born?'
+a_day = gets.chomp.to_i
+
+
+t = Time.new
+
+age = 1
+
+while Time.local(a_year + age, a_month, a_day) <= t
+	puts 'SPANK!'
+	age = age +1
+end
+

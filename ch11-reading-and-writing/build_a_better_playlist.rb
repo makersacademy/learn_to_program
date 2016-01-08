9592e05a627f2e893483571e0392a4f6c3685e63
hedudelgado <hedu.delgado@gmail.com>
2016-01-08 15:03:24 +0000

    build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,10 @@
 def music_shuffle filenames
-  # your code here
+  
+ filenames.sample(filenames.length)
+ # your code here
 end
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+          'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+puts(music_shuffle(songs))
+

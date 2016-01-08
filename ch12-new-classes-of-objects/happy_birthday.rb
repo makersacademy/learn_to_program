c15dfa43037c65114ee76bb6145d40577eca2547
hedudelgado <hedu.delgado@gmail.com>
2016-01-08 15:59:01 +0000

    happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,23 @@
 # your code here
+puts 'Year of birth?'
+birth = gets.chomp.to_i
+
+puts 'Month?'
+month = gets.chomp.to_i
+
+puts 'Day?'
+day   = gets.chomp.to_i
+
+born = Time.local(birth, month, day)
+
+now = Time.new
+
+years = 1
+
+while Time.local(birth + years , month, day) <= now
+	puts 'SPANK'
+
+	years = years + 1
+
+end
+puts "You are #{years-1} SPANKS old!"

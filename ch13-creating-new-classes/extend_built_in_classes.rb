3778587e089f9cfa3a75e1bf174b254f5ab099c7
Marco Bagnasco <admin@hecticmojo.com>
2016-01-08 19:47:47 +0000

    Got an error on Travis, even if Rspec is happy on my system

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -2,7 +2,7 @@ class Integer
   def to_roman 
     dic = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
     return dic[self] if dic.has_key?(self)
-    dig = self.to_s.chars.reverse
+    dig = self.to_s.reverse.chars
     "#{dig[3].to_i < 5 ? dic[1000] * (dig[3].to_i) : dic[1000] * (dig[3].to_i)}"+
     "#{dig[2].to_i < 5 ? dic[100] * (dig[2].to_i) : dic[500] + dic[100] * (dig[2].to_i - 5)}"+
     "#{dig[1].to_i < 5 ? dic[10] * (dig[1].to_i) : dic[50] + dic[10] * (dig[1].to_i - 5)}"+
@@ -15,5 +15,3 @@ class Integer
     self * (self-1).factorial
   end
 end
-
-p 4.to_roman

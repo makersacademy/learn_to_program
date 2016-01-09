98900f7401e3016e5e588e2a2e3af573a7dd176a
Alain Lemaire <jaxdid@gmail.com>
2016-01-09 13:14:20 +0100

    Answered Exercise 9-2

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,11 @@
 def old_roman_numeral num
-  # your code here
+  m = 'M' * (num / 1000)
+  d = 'D' * (num % 1000 / 500)
+  c = 'C' * (num % 500 / 100)
+  l = 'L' * (num % 100 / 50)
+  x = 'X' * (num % 50 / 10)
+  v = 'V' * (num % 10 / 5)
+  i = 'I' * (num % 5 / 1)
+
+  "#{m}#{d}#{c}#{l}#{x}#{v}#{i}"
 end

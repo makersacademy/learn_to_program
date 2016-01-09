c2f03e6e8b5c43c82d456b8b84096897ca84f300
arnold manzano <arnold.manzano@gmail.com>
2016-01-09 08:32:36 +0000

    Answered ch11 safer_picture_downloading.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,17 @@
-# your code here
+def create_playlist
+
+  music_folder = '/home/ubuntu/testdir/music'
+  filename = 'playlist.m3u'
+
+  out_file = music_folder + "/" + filename
+  music_files = Dir[music_folder + '/**/*.mp3']
+
+  song_list = music_files.sort.shuffle.join("\n")
+
+  File.open out_file, 'w' do |f|
+    f.write song_list
+  end
+
+end
+
+create_playlist

cd611362a7c7dbdce8cac6419ebf1b04e15f66b3
Mark Hill <futonfutonda@gmail.com>
2016-01-09 16:36:59 +0000

    sort and dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,28 @@
 def dictionary_sort arr
-  # your code here
+    rec_sort arr, []
 end
+
+  def rec_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted[-1].downcase
+  smallest_orig = unsorted.pop
+
+  still_unsorted =[]
+  unsorted.each do |x|
+    if x.downcase < smallest
+      still_unsorted.push smallest_orig
+      smallest = x.downcase
+      smallest_orig = x
+    else
+      still_unsorted.push x
+    end
+  end
+  sorted.push smallest_orig
+
+  rec_sort still_unsorted, sorted
+  end
+
+ dictionary_sort(["zebra","ele", "Apple", "Ball"])
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,22 @@
 def sort arr
-  # your code here
+  rec_sort arr, []
+end
+
+def rec_sort unsorted, sorted
+if unsorted.length <= 0
+  return sorted
+end
+smallest = unsorted.pop
+still_unsorted =[]
+unsorted.each do |x|
+  if x < smallest
+    still_unsorted.push smallest
+    smallest = x
+  else
+    still_unsorted.push x
+  end
+end
+sorted.push smallest
+
+rec_sort still_unsorted, sorted
 end

127115e635ce5bcf65425081a9cacaaeb840dc79
arnold manzano <arnold.manzano@gmail.com>
2016-01-09 01:53:58 +0000

    Answered ch10 dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
 def dictionary_sort arr
-  # your code here
+  recur_dict_sort arr, []
+end
+
+def recur_dict_sort unsorted_array, sorted_array
+  return [] if unsorted_array.empty?
+  m_entry = unsorted_array.first.downcase
+  m_index = 0
+
+  unsorted_array.each_with_index do |x, i|
+    if x.downcase < m_entry
+      m_entry = x
+      m_index = i
+    end
+  end
+
+  sorted_array << unsorted_array[m_index]
+  unsorted_array.delete_at(m_index)
+
+  recur_dict_sort unsorted_array, sorted_array
+
+  sorted_array
 end

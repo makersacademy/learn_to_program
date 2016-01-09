78641e1159eb58c176200f3aa004482bc999aa29
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-09 01:02:50 +0000

    Minor change to birthday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -4,11 +4,11 @@ File.read('birthdates.txt').each_line do |line|
   line = line.chomp
   length = line.length
   c_idx = line.index(',')
-  bdates[line[0..c_idx-1]] = line[c_idx+2...length]
+  bdates[line[0..c_idx-1].downcase] = line[c_idx+2...length]
 end
 
 puts "Who's birthday do you want to find?"
-name = gets.chomp
+name = gets.chomp.downcase
 
 if bdates.has_key?(name)
   puts bdates[name][0..5]

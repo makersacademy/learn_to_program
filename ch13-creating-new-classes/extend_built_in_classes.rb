93c33c41dd326c43b7533897813a1a143776a49b
Ina Tsetsova <inatsetsova@gmail.com>
2016-01-09 16:55:01 +0000

    finished extending built_in_classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,46 @@
 class Integer
-  # your code here
+  def factorial
+    if self < 0
+      return 'You can\'t take the factorial of a negative number!'
+    end
+
+    if self <= 1
+      1
+    else
+      self * (self-1).factorial
+    end
+  end
+
+  def to_roman
+    num = self
+    return "" if num == 0
+    roman_nums = {
+      "M" => 1000,
+      "D" => 500,
+      "C" => 100,
+      "L" => 50,
+      "X" => 10,
+      "V" => 5,
+      "I" => 1}
+
+    letter,step = roman_nums.select{|key,value| value<=num}.first
+    integer = (num - (num/step * step))
+    letter * (num/step) + (integer).to_roman
+  end
 end
+
+class Array
+  def shuffle(shuffled_array=[])
+      array = self
+      return shuffled_array if array == []
+      number = Random.new
+      shuffled_array << array.delete_at(number.rand(array.length))
+      shuffle(shuffled_array)
+  end
+end
+
+
+
+
+
+

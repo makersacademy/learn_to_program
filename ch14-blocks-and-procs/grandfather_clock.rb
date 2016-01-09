3e8dc79c67a8951fbe2a1a88c02b5cbbe9802411
barrygrubb <barrygrubb@users.noreply.github.com>
2016-01-09 15:15:11 +0000

    ch14_grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour
+
+  if hour > 12
+    hour -= 12
+  end
+
+  if hour == 0
+    hour = 12
+  end
+
+  hour.times do block.call end
 end

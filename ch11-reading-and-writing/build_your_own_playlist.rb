c1037cd71901885ef93f9eb62c9a58a20e4441bc
Heather <campbell.hf@gmail.com>
2016-01-09 18:47:41 +0000

    ch11 build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,20 @@
 # your code here
+Dir.chdir "/Users/Heather/Music/Test"
+
+track_names = Dir['./**/*.mp3']
+
+my_playlist = track_names.shuffle.join("\n")
+
+puts "Enter name of new playlist"
+
+playlist_name = gets.chomp
+
+while File.exist?(playlist_name)
+	puts "filename already exists, pick another"
+	playlist_name = gets.chomp
+end
+
+File.open playlist_name, 'w' do |f| f.write my_playlist
+	end
+
+puts my_playlist

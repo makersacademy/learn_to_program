c2ef0d08caa84891f6b6b1db8c89357012c3f353
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-09 13:28:03 +0100

    Added solution for ch14 program_logger.\n FOUND BUG: RSPEC TESTS EXPECT FUNCTION TO BE CALLED 'program_log' BUT THE EXERCISE NAMES IT 'log'.

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,36 @@
+# Commenting out my functional solution in order to pass rspec tests
+
+=begin
 def log desc, &block
-  # your code here
+  puts "Initializing '#{desc}'..."
+  puts "...wrapping up '#{desc}'; evaluated to '#{block[]}'"
+end
+
+log "outer block" do
+	log "foursquare" do
+	  4**2
+	end
+	log "recursive block"  do
+		"we must go deeper!"
+	end
+	"Log process complete."
+end
+=end
+
+def log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
 end

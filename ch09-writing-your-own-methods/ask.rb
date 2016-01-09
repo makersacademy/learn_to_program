eb376532369d97329d95223905005151cb099695
Alain Lemaire <jaxdid@gmail.com>
2016-01-09 13:10:35 +0100

    Answered Exercise 9-1

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,4 +1,17 @@
-# Commenting to justify pull request
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+  
+    if (reply == 'yes' || reply == 'no')
+      if reply == 'yes'
+        return true
+      else
+        return false
+      end
+      break
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
 end

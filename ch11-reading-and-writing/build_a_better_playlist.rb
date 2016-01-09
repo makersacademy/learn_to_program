2479fd683f9cc28910cf41a83829de7ddd5c4838
arnold manzano <arnold.manzano@gmail.com>
2016-01-09 10:11:33 +0000

    Answered ch11 build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,9 @@
 def music_shuffle filenames
-  # your code here
+
+  filenames.sort!
+
+  shuffled = filenames.select.each_with_index { |x, i| i.odd? } + filenames.select.each_with_index { |x, i| i.even? }
+  shuffled.rotate(2)
+
 end
+

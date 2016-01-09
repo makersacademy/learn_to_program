3154bbec3dda9d04ca0f6b7cfdc3424a858781e2
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-09 00:59:51 +0000

    Completed Birthday Helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,17 @@
-# your code here
+bdates = {}
+
+File.read('birthdates.txt').each_line do |line|
+  line = line.chomp
+  length = line.length
+  c_idx = line.index(',')
+  bdates[line[0..c_idx-1]] = line[c_idx+2...length]
+end
+
+puts "Who's birthday do you want to find?"
+name = gets.chomp
+
+if bdates.has_key?(name)
+  puts bdates[name][0..5]
+else
+  puts "Sorry, that person isn't saved."
+end

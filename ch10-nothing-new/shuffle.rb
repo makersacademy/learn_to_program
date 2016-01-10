6dc7b5f5a14c0959e8658c39294a9039b75a5450
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 23:40:47 +0000

    chp10_shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,27 +1,6 @@
-
- require "set"
+ def shuffle arr
+   # your code here
+   
+  arr.sort_by{rand(arr.length)}
  
- def shuffle *arr
-    # your code here
- end 
-   arr.length > 1 ? shuffArray = arr[1] : shuffArray = []
- 
-   origArray = arr[0]
- 
-   if origArray.length <= 0
- 
-   	return shuffArray
- 
-   end
- 
-   i = rand(origArray.length).to_i
- 
-   shuffArray << origArray.fetch(i)
- 
-   origArray.delete_at(i)
- 
-   shuffle(origArray, shuffArray)
- 
- end
- 
- puts shuffle(["A","B","C","D","E"])
+  end 

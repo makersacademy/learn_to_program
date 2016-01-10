f3f148f0ce41bd0231afc60ac22e1bbf5d6e391c
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 22:59:04 +0000

    chp14_better_program_blogger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,28 @@
 def log desc, &block
-  # your code here
-end
+   # your code here
+ end 
+ $indent_log = 0
+ 
+ def better_log(desc, &block)
+   indent = ' '*$indent_log
+   puts indent + 'Beginning "' + desc + '"...'
+   $indent_log += 1
+   x = block.call
+   $indent_log -= 1
+   puts indent + '..."' + desc + '" finished, returning: ' + x.to_s
+ end 
+ 
+ better_log "outer block" do 
+   better_log "some little block" do
+     better_log "teeny tiny block" do
+       "lotS Of lOve".downcase
+     end
+   22 + 20
+   end
+   better_log "yet another block" do
+     '!doof naidnI evol I'.reverse
+   end
+ 1 == 1
+ end
+ 
+ 

8a15db2ee21d6defde28ba74352d4371d86459ad
ViolaCrellin <viola.crellin@gmail.com>
2016-01-10 16:49:05 +0000

    Completed Ch14 'Even Better Profiling'

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,12 @@
 def profile block_description, &block
-  # your code here
+#Used OPT_PROFILING_ON by looking at SPEC
+	if $OPT_PROFILING_ON
+  		start_time = Time.new
+  		block.call
+  		duration = Time.new - start_time
+  		puts "#{block_description}: #{duration} seconds"
+	else
+		block.call
+	end
 end
+

c0af5d8c55e0cb8a00a3a609491a98b2991b9308
Alain Lemaire <jaxdid@gmail.com>
2016-01-10 10:51:02 +0100

    Answered Exercise 10-3

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,25 @@
 def dictionary_sort arr
-  # your code here
+  recursive_dict_sort(arr, [])
+end
+
+def recursive_dict_sort (unsorted_arr, sorted_arr)
+  if unsorted_arr.count <= 0
+    return sorted_arr
+  end
+
+  to_add = unsorted_arr.shift
+  still_unsorted = []
+
+  unsorted_arr.each do |current_obj|
+    if current_obj.downcase < to_add.downcase
+      still_unsorted << to_add
+      to_add = current_obj
+    else
+      still_unsorted << current_obj
+    end
+  end
+
+  sorted_arr << to_add
+  
+  recursive_dict_sort(still_unsorted, sorted_arr)
 end

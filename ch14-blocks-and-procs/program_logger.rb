994081c2aab1a9b5e5b393539cbdecdadc7c4436
Reiss Johnson <reissjohnson@me.com>
2016-01-10 18:26:05 +0000

    added solution for program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,16 +1,16 @@
 
-def log desc, &block
+def program_log desc, &block
   puts "Beginning #{desc.inspect}..."
   result = block[]
   puts "...#{desc.inspect} finished, returning: #{result}"
 end
 
-log 'outer block' do
-  log 'some little block' do
+program_log 'outer block' do
+  program_log 'some little block' do
     1**1 + 2**2
   end
 
-  log 'yet another block' do
+  program_log 'yet another block' do
     '!doof iahT ekil I'.reverse
   end
   

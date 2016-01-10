9c351071d573317a929a12b52bb4fce45b9c61ec
Iryna Howarth <irynahowarth@gmail.com>
2016-01-10 22:09:53 +0000

    ch14 - 03 solved

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,12 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  puts "...\"#{desc}\" finished, returning: #{block.call}"
+end
+
+program_log 'first' do
+  program_log 'second' do
+    puts "yes"
+    false
+  end
+  true
 end

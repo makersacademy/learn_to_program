a855a6f0eb61a50be292883bee23f8214a134974
valogopedina <valentina.monetta@tiscali.it>
2016-01-10 00:37:58 +0000

    ch11 - 2

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,10 @@
-# your code here
+songs = Dir['/home/valentina/Music/**/*.mp3']
+puts "What name do you want to give to the playlist?"
+playlist_name = "/home/valentina/Music/" + gets.chomp + ".m3u"
+File.open playlist_name, 'w' do |f|
+	songs.shuffle.each { |song_name|
+		f.write song_name + "\n"
+	}
+end
+puts "Done!"
+

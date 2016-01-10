d4764fb2f8f181a4ec1ecc8ed78641d0ab5f91c6
Mark Hill <futonfutonda@gmail.com>
2016-01-10 16:55:32 +0000

    logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,33 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
+end
+
+program_log 'outer block' do
+  program_log 'some little block' do
+    1**1 + 2**2
+  end
+
+  program_log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
+end
+
+
+
+def my_log desc, &block
+  puts "Starting #{desc}"
+  puts "#{block.call}"
+  puts "Ending #{desc}"
+end
+my_log "outer block" do
+  my_log "some little block" do
+    1+1
+  end
+  my_log "yet another block" do
+    "hello"
+  end
 end

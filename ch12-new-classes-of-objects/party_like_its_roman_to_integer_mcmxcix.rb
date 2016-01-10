96d3aa3de2545dfb4a89443f25491f3fdb6b8c0f
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-10 13:27:10 +0000

    Completed roman_to_integer.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,24 +1,17 @@
 def roman_to_integer roman
-  roman = roman.upcase
+  roman = roman.upcase.reverse
   number = 0
   numerals = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
   roman = roman.split ""
   roman.each_index { |index|
     letter = roman[index]
-    next_letter = roman[index+1]
-    if next_letter == nil
-      numerals[next_letter] = 0
-    end
-    if index > 0
-      prev_letter = roman[index-1]
-    end
-    if numerals[next_letter] > numerals[letter] && (numerals[letter] * 10) <= numerals[next_letter]
-      number = number + numerals[next_letter] - numerals[letter]
-    elsif index == 0
+    prev_letter = roman[index - 1]
+    divide = numerals[prev_letter]/numerals[letter]
+    if index == 0
       number += numerals[letter]
-    elsif numerals[prev_letter] < numerals[letter] && (numerals[prev_letter] * 10) <= (numerals[letter])
-      number += 0
-    elsif numerals[prev_letter] >= numerals[letter]
+    elsif divide == 5 || divide == 10
+      number -= numerals[letter]
+    elsif divide == 0 || divide == 1
       number += numerals[letter]
     else
       return "This is not a valid roman numeral"
@@ -27,5 +20,4 @@ def roman_to_integer roman
   number
 end
 
-#for some reason, this doesn't work for the Roman Numeral "iv". It works for all the others.
 

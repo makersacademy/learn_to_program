bf1d3af478bba4c6c30f74ed56ab0f537a313ae8
valogopedina <valentina.monetta@tiscali.it>
2016-01-10 21:37:05 +0000

    ch14 - 1

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,28 @@
+
+$PROFILE_IS_ON = true
 def profile block_description, &block
-  # your code here
+	if $PROFILE_IS_ON
+		start_time = Time.new
+		block.call
+		duration = Time.new - start_time
+		puts "#{block_description}: #{duration} seconds"
+	end
+end
+
+
+
+profile '25000 doublings' do
+	number = 1
+
+	25000.times do
+		number = number + number
+	end
+	puts "#{number.to_s.length} digits"
+end
+
+profile 'count to a million' do
+	number = 0
+	1000000.times do
+		number = number + 1
+	end
 end

f23a836a5f9b4476981b6d2355c96b1ddf6fc104
hedudelgado <hedu.delgado@gmail.com>
2016-01-10 16:14:11 +0000

    program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -4,14 +4,10 @@ def log desc, &block
   puts '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
-
-
-
 log 'outer block' do
   log 'some little block' do
     1**1 + 2**2
   end
-
 
   log 'yet another block' do
     '!doof iahT ekil I'.reverse

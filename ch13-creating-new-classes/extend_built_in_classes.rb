50b535193327d9c8f024b9bd1c4165f5efe3519b
arnold manzano <arnold.manzano@gmail.com>
2016-01-10 15:07:27 +0000

    Answered ch13 extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,22 @@
 class Integer
-  # your code here
+  def factorial
+    (1..self).inject(:*)
+  end
+
+  def to_roman
+    output_roman = ""
+    thousand = self / 1000
+    hundred = self % 1000 / 100
+    tens = self % 100 / 10
+    ones = self % 10
+    output_roman << "M" * thousand
+    output_roman << "D" if hundred >= 5
+    output_roman << "C" * (hundred % 5)
+    output_roman << "L" if tens >= 5
+    output_roman << "X" * (tens % 5)
+    output_roman << "V" if ones >= 5
+    output_roman << "I" * (ones % 5)
+    output_roman
+  end
 end
+

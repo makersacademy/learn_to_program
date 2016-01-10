ae9bf580e59dd417471df78b457e1e96eaff5c63
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 23:03:10 +0000

    chp14_program_blogger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,21 @@
 def log desc, &block
-  # your code here
+   # your code here
+ end 
+ def program_log(desc, &block)
+   puts 'Beginning "' + desc + '"...'
+   x = block.call
+   puts '..."' + desc + '" finished, returning: ' + x.to_s
+ end
+ 
+ program_log('outer block') do
+   
+   program_log('some little block') do
+     25/5
+   end
+ 
+   program_log('yet another block') do
+     "I like Thai food!"
+   end
+   false
+   
 end

671eae6469f72a26082cfe142541c59f3fd0fe2b
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-10 19:42:21 +0000

    Completed Ch14 Better Program Logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,24 @@
-def log desc, &block
-  # your code here
+$logger_depth = 0
+$depth = " "
+
+def better_log desc, &block
+  prefix = $depth * $logger_depth
+  puts "#{prefix}Beginning \"#{desc}\"..."
+  $logger_depth += 1
+  value = block.call
+  $logger_depth -=1
+  puts "#{prefix}...\"#{desc}\" finished, returning: #{value}"
+end
+
+better_log('outer block') do
+  better_log('some little block') do
+    better_log('beginning teeny-tiny block') do
+      'lots of love'
+    end
+    42
+  end
+  better_log('yet another block') do
+    "I like Thai food"
+  end
+  false
 end

f74889cd0b461f0383fc707c92a176abc55b4af9
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-10 17:11:19 +0000

    Completed even_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,13 @@
 def profile block_description, &block
-  # your code here
+  profiling = true
+  if profiling
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+#want to be able to change one line of code to turn profiling on or off. This will give the time taken if profiling remains equal to true. To turn it off, just change to false.

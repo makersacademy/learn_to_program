8cb4a3ef84bb555221eab98ad6fe56a380599777
arnold manzano <arnold.manzano@gmail.com>
2016-01-10 05:34:07 +0000

    Answered ch12 happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,23 @@
-# your code here
+def happy_bday
+
+  puts "Input year of birth: (e.g. 1984)"
+  year = gets.chomp.to_i
+
+  puts "Input month of birth: (e.g. 7 for July)"
+  month = gets.chomp.to_i
+
+  puts "Input day of birth:"
+  day = gets.chomp.to_i
+
+  timenow = Time.now
+  timebday = Time.local(year, month, day)
+
+  age = timenow.year - timebday.year
+  age -= 1 if Time.local(year+age, month, day) > timenow
+
+  age.times { puts "SPANK!" }
+  age
+
+end
+
+puts happy_bday

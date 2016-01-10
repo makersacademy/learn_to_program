509d324993bfe591e661a6d1a2c082850e207c17
Reiss Johnson <reissjohnson@me.com>
2016-01-10 14:57:50 +0000

    ammended old school numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,6 @@
+#original solution that wouldn't pass rspec
+=begin
+
 KEY = [
   ["M", 1000], 
   ["D", 500], 
@@ -20,4 +23,26 @@ def old_school_roman n
   return roman
 end
 
-p old_school_roman 1005
+puts old_school_roman 1005
+
+=end 
+
+
+
+def old_roman_numeral num
+  raise 'Must use positive integer' if num <= 0
+
+  roman = ''
+
+  roman << 'M' * (num / 1000) 
+  roman << 'D' * (num % 1000 / 500) 
+  roman << 'C' * (num % 500 / 100) 
+  roman << 'L' * (num % 100 / 50) 
+  roman << 'X' * (num % 50 / 10) 
+  roman << 'V' * (num % 10 / 5) 
+  roman << 'I' * (num % 5 / 1)
+
+  roman
+end
+
+# puts(old_roman_numeral(1999))

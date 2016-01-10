92635a5f747cbe00c651426d7836609a7fb6a36f
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 17:48:57 +0000

    chp11_Safer_picutre_downloading

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,32 @@
-# your code here
+require "fileutils"
+ 
+ include FileUtils
+ 
+ Dir.chdir "/home/sarah/Pictures/PineCh11"
+ # First we find all of the pictures to be moved.
+ pic_names = Dir["/media/jeremy/MY CAMERA/DCIM/100CAMEA/*.{JPG,jpg}"]
+ puts 'What would you like to call this batch?'
+ batch_name = gets.chomp
+ puts
+ print "Downloading #{pic_names.length} files:"
+ # This will be our counter. We'll start at 1 today,
+ # though normally I like to count from 0.
+ pic_number = 1
+ pic_names.each do |name|
+ 	print '.' # This is our "progress bar".
+ 	new_name = if pic_number < 10
+ 		"#{batch_name}0#{pic_number}.jpg"
+ 	else	
+ 		"#{batch_name}#{pic_number}.jpg"
+ 	end	
+ 	if File.exist?(new_name)
+ 		puts "Overwrite attempt.  Download stopped."
+ 		exit		
+ 	else
+ 		FileUtils.cp name, new_name
+ 	end
+ 	# Finally, we increment the counter.
+ 	pic_number = pic_number + 1
+ end
+ puts # This is so we aren't on progress bar line.
+ puts "Done."# your code here

ea79298ba9449f0f012385f880258e5da869b82b
Mark Hill <futonfutonda@gmail.com>
2016-01-10 13:20:39 +0000

    happy birthday and one billion seconds

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,21 @@
-# your code here
+puts "Please enter your date of birth. \n Year:"
+year = gets.chomp
+puts "Month:"
+month = gets.chomp
+puts "Day:"
+day = gets.chomp
+
+birth_date = Time.local(year, month, day)
+now = Time.new
+if now.month < birth_date.month
+  diff = now.year - birth_date.year
+elsif now.month > birth_date.month
+  diff = now.year - (birth_date.year + 1)
+else
+  if now.day <= birth_date.day
+    diff = now.year - birth_date.year
+  else
+    diff = now.year - (birth_date.year + 1)
+  end
+end
+puts "SPANK!\n"*diff

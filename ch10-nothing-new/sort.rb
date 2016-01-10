fed1be56fec42509559a9aeac61699b9a1202cce
frazerWatson <frazerw@tutanota.com>
2016-01-10 17:43:53 +0000

    sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,13 +1,30 @@
-# The well-known quicksort algorithm.
-def dictionary_sort arr
-  return arr if arr.length <= 1
-
-  middle = arr.pop
-  less = arr.select{|x| x.downcase < middle.downcase} 
-  more = arr.select{|x| x.downcase >= middle.downcase}
-
-  dictionary_sort(less) + [middle] + dictionary_sort(more)
+def sort arr 
+  rec_sort arr, []
 end
 
-# words = ['can','feel','singing.','like','A','can'] 
-# puts(dictionary_sort(words).join(' '))
+def rec_sort unsorted, sorted 
+  if unsorted.length <= 0
+    return sorted 
+  end
+  # So if we got here, then it means we still # have work to do.
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |tested_object| 
+    if tested_object < smallest
+      still_unsorted.push smallest
+      smallest = tested_object
+    else
+      still_unsorted.push tested_object
+    end 
+  end
+
+  # Now "smallest" really does point to the
+  # smallest element that "unsorted" contained,
+  # and all the rest of it is in "still_unsorted". l
+  sorted.push smallest
+  
+  rec_sort still_unsorted, sorted
+end
+
+# puts(sort([' can' ,' feel' ,' singing' ,' like' ,' a' ,' can' ]))

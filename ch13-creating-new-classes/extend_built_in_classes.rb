55e30dfe7ff92b05361573d1c8c5a17eae261b70
Marco Bagnasco <admin@hecticmojo.com>
2016-01-10 11:10:54 +0000

    Changes to comply with 1.9.3-p551

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -2,7 +2,7 @@ class Integer
   def to_roman 
     dic = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
     return dic[self] if dic.has_key?(self)
-    dig = self.to_s.reverse.chars
+    dig = self.to_s.reverse
     "#{dig[3].to_i < 5 ? dic[1000] * (dig[3].to_i) : dic[1000] * (dig[3].to_i)}"+
     "#{dig[2].to_i < 5 ? dic[100] * (dig[2].to_i) : dic[500] + dic[100] * (dig[2].to_i - 5)}"+
     "#{dig[1].to_i < 5 ? dic[10] * (dig[1].to_i) : dic[50] + dic[10] * (dig[1].to_i - 5)}"+
@@ -15,3 +15,6 @@ class Integer
     self * (self-1).factorial
   end
 end
+
+
+

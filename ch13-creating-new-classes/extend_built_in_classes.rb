9be4a92a09598bbe9daefa715f4d097e53424193
valogopedina <valentina.monetta@tiscali.it>
2016-01-10 00:48:06 +0000

    ch13 - 1

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,22 @@
+require_relative '../ch09-writing-your-own-methods/roman_numerals.rb'
 class Integer
-  # your code here
+	def factorial
+		value = self.to_i #use self because I'm calling a method within the object itself.
+		return (1..value).to_a.reduce(:*)
+	end
+
+	def to_roman
+		value = self.to_i
+		roman = ''
+        roman = roman + 'M' * (value / 1000)
+        roman = roman + 'D' * (value % 1000 / 500)
+        roman = roman + 'C' * (value % 500 / 100)
+ 		roman = roman + 'L' * (value % 100 / 50)
+ 		roman = roman + 'X' * (value % 50 / 10)
+ 		roman = roman + 'V' * (value % 10 / 5)
+ 		roman = roman + 'I' * (value % 5 / 1)
+	end
 end
+
+
+ok

4d210e8f1dde5409507ef2ea4b8f36ee67cc891e
valogopedina <valentina.monetta@tiscali.it>
2016-01-10 21:41:23 +0000

    ch14 - 4

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,9 @@
-def log desc, &block
-  # your code here
+ $NESTING_DEPTH = 0
+def better_log block_name, &block
+	puts " "*$NESTING_DEPTH + 'Beginning "' + block_name + '"...'
+	$NESTING_DEPTH += 1
+	val = block.call
+	$NESTING_DEPTH -= 1
+	puts " "*$NESTING_DEPTH + '..."' + block_name + '" finished, returning: ' + val.to_s
 end
+

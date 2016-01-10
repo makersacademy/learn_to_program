2bf8e9000f87a9b4a014c52170396fa4881751f3
Reiss Johnson <reissjohnson@me.com>
2016-01-10 18:00:46 +0000

    added solution for program logger had to check solution

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,19 +1,18 @@
 
 def log desc, &block
-  puts 'Beginning "' + desc + '"...'
-  result = puts block.call 
-  puts '..."' + desc + '" finished, returning: ' + result.to_s
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
 end
 
 log 'outer block' do
-  log "some little block "  do
+  log 'some little block' do
     1**1 + 2**2
   end
 
   log 'yet another block' do
     '!doof iahT ekil I'.reverse
   end
-
+  
   '0' == 0
 end
-

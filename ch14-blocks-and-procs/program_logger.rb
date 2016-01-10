21a89f4f1859086f3322c73edec038545c721c7a
blastoff <blastoff2@hotmail.com>
2016-01-10 20:30:17 +0000

    chpter14

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,31 @@
 def log desc, &block
-  # your code here
+
+
+puts 'Beginning "' + desc + '"...'
+
+result = block.call
+
+puts '..."' + desc + '" finished, returning: ' + result.to_s
+
+end
+
+log 'outer block' do
+
+log 'some little block' do
+
+1**1 + 2**2
+
+end
+
+log 'yet another block' do
+
+'!doof iahT ekil I'.reverse
+
+end
+
+'0' == 0
+
+
+
+
 end

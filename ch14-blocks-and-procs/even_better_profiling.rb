3071076e70fde82872adce9381576ecdda08cb48
mo <mdeerow@gmail.com>
2016-01-10 18:26:43 +0000

    14-3

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,11 +1,11 @@
 def profile block_description, &block
-    profiling_on = false
+    profiling_on = true
     if profiling_on
         start_time = Time.new
         block.call
         duration = Time.new - start_time
         puts "#{block_description}: #{duration} seconds"
     else
-        block.call
+    block.call
     end
 end

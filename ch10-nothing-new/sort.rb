3005168253e8f14e0a128f0ef2a7f4c6da6a40fa
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 17:37:46 +0000

    chp10_Sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,14 @@
-def sort arr
-  # your code here
-end
+ def sort arr
+    # your code here
+ end 
+   return arr if arr.length <= 1
+   
+   middle = arr.pop
+   less = arr.select{|x| x < middle}
+   more = arr.select{|x| x >= middle}
+ 
+   sort(less) + [middle] + sort(more)
+ 
+ end
+ 
+ puts sort(['sarah','alice','kate','ben','luke','nikesh'])

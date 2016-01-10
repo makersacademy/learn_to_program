2597f393e5374b85d3a47a59390bb1d4eddcaec4
arnold manzano <arnold.manzano@gmail.com>
2016-01-10 15:10:07 +0000

    Answered ch14 program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,13 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  return_val = block.call
+  puts "...\"#{desc}\" finished, returning: #{return_val}"
 end
+
+#program_log 'outer block' do
+#  program_log 'inner block' do
+#    1+1
+#  end
+#  true
+#end
+

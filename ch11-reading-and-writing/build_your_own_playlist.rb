6ceac160cae81f1de27580782da87cc617370334
Matthew Drake <matdrake@live.co.uk>
2016-01-10 21:35:25 +0000

    Pictures and playlist 1 complete

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,23 @@
-# your code here
+Dir.chdir '/Users/Mat/Music'
+
+music_files = Dir['Albums/**/*.mp3']
+
+play = music_files.sample(5)
+
+filename = 'Playlist.m3u'
+
+File.open filename, 'w' do |f|
+  play.each do |names|
+    f.write "#{names}\n"
+  end
+end
+
+
+
+#File.open filename, 'w' do |f|
+#  play.each do |names|
+#    f.write names
+#  end
+#end
+
+#filename = 'Playlist.xml'
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,32 @@
-# your code here
+Dir.chdir '/Users/Mat/Pictures'
+
+pic_names = Dir['/Volumes/KINGSTON/**/*.{jpg,JPG}']
+
+puts "What would you like to call this batch?"
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print "."
+
+  if File.exist?(name)
+    exit
+  else
+    new_name = if pic_number < 10
+      "#{batch_name}0#{pic_number}.jpg"
+    else
+      "#{batch_name}#{pic_number}.jpg"
+    end
+  end
+
+  File.rename name, new_name
+
+  pic_number = pic_number + 1
+end
+
+puts
+puts "Complete"

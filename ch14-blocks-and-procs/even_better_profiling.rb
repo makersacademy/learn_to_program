7506d7e1235d9767b0b18e994f368272a0d9c605
Reiss Johnson <reissjohnson@me.com>
2016-01-10 14:49:23 +0000

    had to check solution

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,35 @@
 def profile block_description, &block
-  # your code here
+
+	start_time = Time.new
+	block.call
+	duration = Time.new - start_time
+
+	
 end
+
+profile "25000 doublings" do 
+	number = 1
+
+	25000.times do 
+		number = number + number
+	end
+
+	"#{number.to_s.length}"
+
+end
+
+profile 'count to a million' do
+	number = 0
+	1000000.times do
+		number = number + 1
+	end
+
+end
+
+profile 'times_by_300' do
+  number = 1 
+  300.times do
+  number = number * 300
+  end
+end
+

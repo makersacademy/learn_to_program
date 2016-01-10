a16ddb962c3111d48c6dcee37fe13a31b5e2d23a
hedudelgado <hedu.delgado@gmail.com>
2016-01-10 16:03:52 +0000

    even_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,10 @@
 def profile block_description, &block
-  # your code here
+    profiling_on = false
+    if profiling_on == true
+        start_time = Time.new
+        block.call
+        duration = Time.new - start_time
+        puts "#{block_description}: #{duration} seconds"
+    end
+         # your code here
 end

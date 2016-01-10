b62ed0441f0a4edb5e9e66a9559e988e53f3a140
arnold manzano <arnold.manzano@gmail.com>
2016-01-10 15:10:31 +0000

    Answered ch14 better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,23 @@
-def log desc, &block
-  # your code here
+
+def better_log desc, &block
+  space =  " "*$nest_depth
+  puts "#{space}Beginning \"#{desc}\"..."
+  $nest_depth += 1
+  return_val = block.call
+  $nest_depth -= 1
+  space = " "*$nest_depth
+  puts "#{space}...\"#{desc}\" finished, returning: #{return_val}"
 end
+
+$nest_depth = 0
+
+#better_log 'outer block' do
+#  better_log 'middle block' do
+#    better_log 'inner block' do
+#      1+1
+#    end
+#    "abc!"
+#  end
+#  true
+#end
+

39d4e62dcf3d4ddc9666679faa5aa58ca60547fd
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-10 22:32:43 +0000

    updated to work correctly with indentations

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,10 @@
-def log desc, &block
-  # your code here
+$logger_depth = 0
+
+def better_log desc, &block
+  prefix = ' '*$logger_depth
+  puts prefix+"Beginning #{desc.inspect}..."
+  $logger_depth += 1
+  result = block[]
+  $logger_depth -= 1
+  puts prefix+"...#{desc.inspect} finished, returning: #{result}"
 end

033b9e610700f78066fa7ed3853b83765af2695c
ViolaCrellin <viola.crellin@gmail.com>
2016-01-10 17:54:41 +0000

    Completed Ch14 'Program Logger'

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,14 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  result = block.call
+  puts "...\"#{desc}\" finished, returning: #{result}"
+end
+
+program_log "outer block" do 
+	program_log "some little block" do
+		2 + 3
+	end
+	program_log "yet another block" do
+		"I like Thai Food"
+	end
 end

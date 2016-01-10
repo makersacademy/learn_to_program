ad45a1d47f171e419bd5cdcd2a24c631d52f834e
arnold manzano <arnold.manzano@gmail.com>
2016-01-10 05:30:23 +0000

    Answered ch12 one_billion_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,18 @@
-# your code here
+def one_bil_sec
+
+  puts birthday = Time.local(1984, 7, 20, 12, 0, 0)
+  puts billion = 10 ** 9
+  
+  onebil_bday = birthday + billion
+  time_now = Time.new
+
+  if onebil_bday < time_now
+  	puts "You are one billion seconds old last #{onebil_bday.strftime("%-d %B %Y")}!"
+  else
+	puts "You will be one billion seconds old on #{onebil_bday.strftime("%-d %B %Y")}! Mark your calendar!"
+  end
+
+end
+
+one_bil_sec
+

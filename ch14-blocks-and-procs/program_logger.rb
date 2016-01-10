02aa8627cbe018ca3d77423f5cce29002c40725e
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 23:20:13 +0000

    chp14_program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,18 +1,21 @@
+
+@@ -1,3 +1,17 @@
  def log desc, &block
- -  # your code here
- +  puts "Beginning #{desc.inspect}..."
- +  result = block[]
- +  puts "...#{desc.inspect} finished, returning: #{result}"
- +end
- +
- +log 'outer block' do
- +  log 'some little block' do
- +    1**1 + 2**2
- +  end
- +
- +  log 'yet another block' do
- +    '!doof iahT ekil I'.reverse
- +  end
- +  
- +  '0' == 0
-  end
+ def program_log desc, &block
+    # your code here
+ end 
+   puts "Beginning \"#{desc}\"..."
+   a = block.call
+   puts "...\"#{desc}\" finished, returning: #{a}"
+ end
+ 
+ =begin
+ program_log "outer block" do 
+   program_log "some little block" do
+     5
+   end
+   program_log "yet another block" do
+     "I like Thai food!"
+   end
+   false
+ =end

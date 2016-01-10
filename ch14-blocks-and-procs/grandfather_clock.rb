2388ca8a2e42348607b8c9b5765f294ce3ce17ac
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-10 17:16:50 +0000

    Completed grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
 def grandfather_clock &block
-  # your code here
+  hours = Time.new.hour
+  #returns a number between 0 and 23, but want it to only ever run between 0-12 times. Use the modulus (aka. 23%12 = 11). This will work for any time except 12pm (Because 12%12 = 0, so will not call block)
+  if hours == 12
+    12.times do
+      block.call
+    end
+  else
+    (hours % 12).times do
+      block.call
+    end
+  end
 end

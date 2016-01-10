b71c7db59758dc5f8486f8aef35e5eb37e6d76a3
Iryna Howarth <irynahowarth@gmail.com>
2016-01-10 22:09:21 +0000

    ch14 - 02 solved

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,8 @@
 def grandfather_clock &block
-  # your code here
+  Time.new.hour == 0 ? n=12 : n = Time.new.hour - 12*(Time.new.hour/13)
+  n.times(&block)
+end
+
+grandfather_clock do
+  puts 'DONG!'
 end

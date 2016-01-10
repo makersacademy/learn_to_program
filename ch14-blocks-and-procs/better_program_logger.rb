5802872ff73cdb2c824b69c2452f45fb7918b377
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-10 17:33:16 +0000

    Completed better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,8 @@
-def log desc, &block
-  # your code here
+$tabs = -1
+def better_log desc, &block
+  $tabs  += 1
+  puts " "*$tabs + "Beginning \"#{desc}\"..."
+  response = block.call
+  puts " "*$tabs + "...\"#{desc}\" finished, returning: #{response}"
+  $tabs -= 1
 end

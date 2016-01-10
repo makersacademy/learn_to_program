f4c90090caa1dc866e89390f910e2f864509412b
Ina Tsetsova <inatsetsova@gmail.com>
2016-01-10 17:47:09 +0000

    finished grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,20 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock(&block)
+
+hours =  Time.new.hour
+
+  if hours > 12
+    hours = hours - 12
+  end
+
+  if hours == 0
+    hours = 12
+  end
+
+  hours.times do
+    block.call
+  end
+end
+
+grandfather_clock do
+  puts 'DONG!'
 end

edccb9ab1e3541175c07403cc34d09445ce80ca5
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-10 18:04:35 +0000

    chp13_interactive_baby_dragon

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,20 @@
-# your code here
+puts "What is the name of your dragon"
+ name = gets.chomp
+ dragon = Dragon.new(name)
+ 
+ puts "What would you like to do to the dragon? Feed/walk/toss/put to bed/rock?"
+ input = gets.chomp.downcase
+ 
+ if input == "feed"
+   dragon.feed
+ elsif input == "walk"
+   dragon.walk
+ elsif input == "toss"
+   dragon.toss
+ elsif input == "put to bed"
+   dragon.put_to_bed
+ elsif input == "rock"
+   dragon.rock
+ else 
+   puts "Learn to spell."
+ end# your code here

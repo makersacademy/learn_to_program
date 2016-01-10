c18a6d165ad76c18e61b736c483ece9b7b1ee270
Reiss Johnson <reissjohnson@me.com>
2016-01-10 17:21:28 +0000

    updated ch14 even_better_profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,4 +1,22 @@
 def profile block_description, &block
+  # To turn profiling on/off, set this
+  # to true/false.
+  profiling_on = false
+  if profiling_on
+    start_time = Time.new
+    block.call
+    
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
+end
+
+
+#my solution that did what it asked.
+=begin
+def profile block_description, &block
 
 	start_time = Time.new
 	block.call
@@ -32,4 +50,6 @@ profile 'times_by_300' do
   number = number * 300
   end
 end
+
+=end
 

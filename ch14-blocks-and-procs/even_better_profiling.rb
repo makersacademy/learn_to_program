9aa7a8526059f71c3b391a1daa61a5a014e9b80d
arnold manzano <arnold.manzano@gmail.com>
2016-01-10 15:09:27 +0000

    Answered ch14 even_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,22 @@
 def profile block_description, &block
-  # your code here
+  if $OPT_PROFILING_ON
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+#$OPT_PROFILING_ON = true
+#profile 'test code' do
+#  sleep 2
+#  puts "Code done!"
+#end
+#
+#$OPT_PROFILING_ON = false
+#profile 'test code' do
+#  sleep 2
+#  puts "Code done!"
+#end

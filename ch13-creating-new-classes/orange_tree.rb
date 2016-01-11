0a195a997bcbcf10bdf84c2f0a3777cc084a1651
innlouvate <l.j.franklin85@googlemail.com>
2016-01-11 02:57:22 +0000

    orange ytree fix to pass

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,66 +1,59 @@
 class OrangeTree
+  # your code here
+
   def initialize
-    @age = 0
-    @fruit = 0
     @height = 0
-    @dead = false
-    puts "You plant an orange tree and wait."
-  end
-
-  def one_year_passes
-    if @dead == true
-     return "A year later, the tree is still dead. :("
-    end
-
-    @age += 1
-    @height += 0.4 #add max height and make height change
-    @fruit += 5 #if @age > 5
-
-    if @age > 25
-      @dead = true
-      "Oh, no! The tree is too old, and has died. :("
-    else
-      "This year your tree grew to #{@height.round(1)}m tall, and produced #{@fruit} oranges."
-    end
+    @oranges = 0
+    @alive = true
   end
 
   def height
-    if @dead == false
+    if @alive
+      #returns height rounded to one decimal place
       @height.round(1)
     else
       "A dead tree is not very tall. :("
     end
   end
-
-  def age
-    if @dead == false
-     "Your tree is #{@age} years old now."
+  def count_the_oranges
+    if @alive
+      @oranges
     else
-     "A dead tree is not very old. :("
+      "A dead tree has no oranges. :("
     end
   end
 
   def pick_an_orange
-    if @dead == true
-      return "A dead tree has nothing to pick. :("
-    end
-
-    if @fruit == 0
-      "You have no oranges :("
+    if @alive
+      if @oranges > 0
+        @oranges = @oranges - 1
+        "You pick a juicy, delicious orange!"
+      else
+        "You search every branch, but find no oranges."
+      end
     else
-      "You picked an orange. Mmmm delicious!"
-        @fruit = @fruit - 1
+      "A dead tree has nothing to pick. :("
     end
   end
 
-  def count_the_oranges
-    if @dead == true
-      return "A dead tree has no oranges. :("
-    end
-    if @fruit > 0
-      @fruit
+  def one_year_passes
+    if @alive
+      @height = @height + 0.4
+      @oranges = 0
+      if @height > 10.1 # && rand(2) > 0
+    #want the tree to die when bigger than 10m, but not as soon as its bigger than 10. So there's a 2/3 chance that it will die each year it's bigger. But with this, it doesn't pass the spec. Set it as 10.1 so that it would work.
+        @alive = false
+        "Oh, no! The tree is too old, and has died. :("
+      elsif @height > 2
+    #only starts growing oranges after its 2m tall. Also, can't have a float, becaue you can't have 2.5 oranges.
+        @oranges = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height.round(1)}m tall, and produced #{@oranges} oranges."
+      else
+        "This year your tree grew to #{@height.round(1)}m tall, but is still too young to bear fruit"
+      end
     else
-     "You have no oranges.."
+      "A year later, the tree is still dead. :("
     end
   end
+
 end

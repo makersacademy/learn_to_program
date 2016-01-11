af04595d5ff805ce1b6b88ebd0dfb69ba96667f6
hedudelgado <hedu.delgado@gmail.com>
2016-01-11 01:25:38 +0000

    better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,27 @@
+$logcounter = 0
 def log desc, &block
-  # your code here
+	cheat = ' ' * $logcounter 
+	puts cheat + 'Beginning "' + desc + '"...'
+	$logcounter = $logcounter + 1
+  	result = block.call
+  	$logcounter = $logcounter -1
+  	puts cheat + '..."' + desc + '" finished, returning: ' + result.to_s # your code here
+end
+
+
+
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lOtS oF lOVe'.downcase
+    end
+
+    7*6
+  end
+
+  log 'yet another block' do
+    '!doof naidnI evol I'.reverse
+  end
+
+  '0' == "0"
 end

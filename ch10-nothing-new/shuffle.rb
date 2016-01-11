5873c3d3872bd4ecc61370b624743b2d427e3c07
innlouvate <l.j.franklin85@googlemail.com>
2016-01-11 01:03:23 +0000

    shuffle rspec

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,7 +1,40 @@
 def shuffle arr
-  #rec_shuffle arr, []
-  arr.shuffle
+  shuf = []
+
+  while arr.length > 0
+    # Randomly pick one element of the array.
+    rand_index = rand(arr.length)
+
+    # Now go through each item in the array,
+    # putting them all into new_arr except for the
+    # randomly chosen one, which goes into shuf.
+    curr_index = 0
+    new_arr = []
+
+    arr.each do |item|
+      if curr_index == rand_index
+        shuf.push item
+      else
+        new_arr.push item
+      end
+
+      curr_index = curr_index + 1
+    end
+
+    # Replace the original array with the new,
+    # smaller array.
+    arr = new_arr
+  end
+
+  shuf
 end
+
+
+
+
+#def shuffle arr
+  #rec_shuffle arr, []
+#end
 
 #def rec_shuffle unshuffled_array, shuffled_array
 #

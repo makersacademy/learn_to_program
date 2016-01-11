ee53ce44fdd76e0ba95feee8057bd3b62d2e471a
Marion <marion.levisage@gmail.com>
2016-01-11 17:09:24 +0100

    Updated orange_tree.rb

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,7 +7,7 @@ def initialize
 
   def height
     if @alive
-       @height
+       @height.to_i
     else
       'A dead tree is not very tall. :('
     end
@@ -23,7 +23,7 @@ def initialize
 
   def one_year_passes
     if @alive
-       @height = (@height + 0.4).to_i
+       @height = (@height + 0.4)
        @orange_count = 0
 
     if @height > 10 && rand(2) > 0

d8fca69f77fababd7e32973d75a7e520e987ff89
Alain Lemaire <jaxdid@gmail.com>
2016-01-11 10:33:53 +0100

    Answered Exercise 11-2

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,9 @@
-# your code here
+Dir.chdir("../../Music/Standalone Files/")
+
+mp3s = Dir["*.mp3"]
+
+File.open("playlist.m3u", "a") do |f|
+  mp3s.shuffle.each do |x|
+    f.write(x + "\n")
+  end
+end

fe042d4cffb6c500f77dec176dbe19eb9ef104f3
Alain Lemaire <jaxdid@gmail.com>
2016-01-11 14:22:20 +0100

    Answered Exercise 12-1

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,7 @@
-# your code here
+birthday = Time.local(1987, 6, 30)
+birthday_billion = birthday + 1000000000
+# current_time = Time.new
+
+happened = Time.new < birthday_billion ? "will turn" : "turned"
+
+puts "I #{happened} one billion seconds old on: #{birthday_billion}."

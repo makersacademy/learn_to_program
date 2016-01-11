72fdf457b87661994fe870b815ef6c6fdfa16f15
innlouvate <l.j.franklin85@googlemail.com>
2016-01-11 01:02:10 +0000

    shuffle funhuffle func

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,18 +1,19 @@
 def shuffle arr
-  rec_shuffle arr, []
+  #rec_shuffle arr, []
+  arr.shuffle
 end
 
-def rec_shuffle unshuffled_array, shuffled_array
-
-  if unshuffled_array.length <= 0
-    return shuffled_array
-  else
-    r=Random.rand(0...unshuffled_array.length)
-    shuffled_array << unshuffled_array[r]
-    unshuffled_array.delete_at(r)
-  end
-
-  rec_shuffle unshuffled_array, shuffled_array
-end
+#def rec_shuffle unshuffled_array, shuffled_array
+#
+#  if unshuffled_array.length <= 0
+#    return shuffled_array
+#  else
+#    r=Random.rand(0...unshuffled_array.length)
+#    shuffled_array << unshuffled_array[r]
+#    unshuffled_array.delete_at(r)
+#  end
+#
+#  rec_shuffle unshuffled_array, shuffled_array
+#end
 
 #print shuffle %w(a b c)

237e7d5b42e40c1d038c34b41a164926b0eb113d
Marion <marion.levisage@gmail.com>
2016-01-11 17:22:12 +0100

    Answers for sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
 def sort arr
-  # your code here
+  puts 'Give me some words, and I will sort them:'
+  words = []
+
+  while true
+    word = gets.chomp if word == ''
+    break
+  end
+
+  words.push word
 end

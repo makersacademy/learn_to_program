64a55c1aff9485acf36d5cc5cc68d2a004b22708
Alain Lemaire <jaxdid@gmail.com>
2016-01-11 10:34:23 +0100

    Answered Exercise 11-3

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,33 @@
 def music_shuffle filenames
-  # your code here
+  filenames.sort!
+  len = filenames.length
+
+  2.times do
+    l = 0
+    r = len / 2
+    shuf = []
+  
+    while shuf.length < len
+      if shuf.length % 2 == 0
+        shuf << filenames[r]
+        r += 1
+      else
+        shuf << filenames[l]
+        l += 1
+      end
+    end
+
+    filenames = shuf
+  end
+
+  arr = []
+  cut = rand(len)
+  i = 0
+
+  while i < len
+    arr << filenames[(i + cut) % len]
+    i += 1
+  end
+
+  arr
 end

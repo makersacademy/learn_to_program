88b7bfd3ef218e52026961b69578ded3bc0985aa
Matt <mattpgough@gmail.com>
2016-01-11 09:37:54 +0000

    updated roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -4,7 +4,7 @@ def roman_numeral num
     second = {1=>"X", 2=> "XX", 3=> "XXX", 4=>"XL", 5=> "L", 6=> "LX", 7=>"LXX", 8=>"LXXX", 9=>"XC"}
     third = {1=> "C", 2=> "CC", 3=> "CCC", 4=>"CD", 5=> "D", 6=> "DC", 7=>"DCC", 8=>"DCCC", 9=>"CM"}
     fourth = {1=> "M", 2=> "MM", 3=> "MMM"}
-a = num.to_s.chars.reverse
+a = num.to_s.reverse
 array << first[a[0].to_i]
 array << second[a[1].to_i]
 array << third[a[2].to_i]

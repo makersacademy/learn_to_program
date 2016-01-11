45103cca266d5b184b3c7f5f5caf1433af56670a
Matt <mattpgough@gmail.com>
2016-01-11 09:56:47 +0000

    Updating extend_built_in_classes,rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -11,7 +11,7 @@ def to_roman
     second = {1=>"X", 2=> "XX", 3=> "XXX", 4=>"XXXX", 5=> "L", 6=> "LX", 7=>"LXX", 8=>"LXXX", 9=>"LXXXX"}
     third = {1=> "C", 2=> "CC", 3=> "CCC", 4=>"CCCC", 5=> "D", 6=> "DC", 7=>"DCC", 8=>"DCCC", 9=>"DCCCC"}
     fourth = {1=> "M", 2=> "MM", 3=> "MMM", 4=>"MMMM", 5=> "MMMMM"}
-a = self.to_s.chars.reverse
+a = self.to_s.reverse
 array << first[a[0].to_i]
 array << second[a[1].to_i]
 array << third[a[2].to_i]

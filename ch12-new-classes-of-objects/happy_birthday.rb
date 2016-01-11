f084efa3a2f671070612cbc502e04a240a6b61df
Heather <campbell.hf@gmail.com>
2016-01-11 12:00:54 +0000

    ch12 - happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,38 @@
-# your code here
+# My first attempt:
+#
+#def birthday
+#  puts "What year were you born in?"
+#    year = gets.chomp.to_i
+#    puts "What month of the year were you born in?"
+#      month = gets.chomp.to_i
+#    puts "What day in the month where you born?"
+#      day = gets.chomp.to_i
+#
+#  dob = Time.local(year, month, day)
+#
+#  age = Time.new - dob
+#
+#end
+
+#The solution:
+
+puts birthday
+
+puts 'What year were you born?'
+b_year = gets.chomp.to_i
+
+puts 'What month were you born? (1-12)'
+b_month = gets.chomp.to_i
+
+puts 'What day of the month were you born?'
+b_day = gets.chomp.to_i
+
+b = Time.local(b_year, b_month, b_day)
+t = Time.new
+
+age = 1
+
+while Time.local(b_year + age, b_month, b_day) <= t
+  puts 'SPANK!'
+  age = age + 1
+end

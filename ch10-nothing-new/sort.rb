15973dae6984b1d0b41325a263fc5410ede6fb14
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-11 22:17:52 +0000

    chp10_sort_change

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,31 +1,28 @@
-def sort arr 
-   
-   recursive_sort arr, [] 
-
+def sort arr
+ def sort(arr)
+    # your code here
+ end 
+   recursive_sort(arr,[])
  end
  
- def recursive_sort unsorted, sorted 
+ def recursive_sort(unsorted, sorted)
+   if unsorted.length <= 0
+     return sorted
+   end
+   smallest = unsorted.pop
+   still_unsorted = []
+   
+   unsorted.each do |tested_word|
+     if tested_word < smallest
+       still_unsorted.push(smallest)
+       smallest = tested_word
+     else
+       still_unsorted.push(tested_word)
+     end
+   end
+   
+   sorted.push(smallest)
+   recursive_sort(still_unsorted, sorted)
+ end
  
- 
- 	if unsorted.length <= 0 
- 	end
- 
- 	smallest = unsorted.pop 
-
- 	yet_to_sort = [] 
- 
- 	unsorted.each do |tested_element|  
- 
- 		if tested_element < smallest
- 			yet_to_sort << smallest  
- 			smallest = tested_element 
- 		else
- 			yet_to_sort << tested_element 
- 		end
- 	end
- 
- 	sorted << smallest 
- 
-   	recursive_sort yet_to_sort, sorted 
- 
-  end
+ puts(sort(['can','feel','singing','like','a','can']))

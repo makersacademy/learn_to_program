f1b4bc29eafb8809d3b7b53df507cd7c4361425a
Alain Lemaire <jaxdid@gmail.com>
2016-01-11 14:27:56 +0100

    Answered Exercise 12-2

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "What YEAR were you born in?"
+year = gets.chomp
+
+puts "What MONTH were you born in?"
+month =gets.chomp
+
+puts "What DATE were you born on?"
+date = gets.chomp
+
+birthday = Time.mktime(year, month, date)
+age = (Time.new - birthday).to_i / 60 / 60 / 24 / 365
+
+puts "You were born on #{birthday}."
+puts "You are #{age} years old."
+
+age.times { puts "SPANK!" }

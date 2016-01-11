0ff2bceb4b82c457b7c1ed49b139a3617c45c29d
Heather <campbell.hf@gmail.com>
2016-01-11 14:04:06 +0000

    roman to integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,35 @@
 def roman_to_integer roman
-  # your code here
+  digit_vals = {'i' => 1,
+                'v' => 5,
+                'x' => 10,
+                'l' => 50,
+                'c' => 100,
+                'd' => 500,
+                'm' => 1000}
+  total = 0
+  prev = 0
+  index = roman.length - 1
+  while index >= 0
+    c = roman[index].downcase
+    index = index - 1
+    val = digit_vals[c]
+    if !val
+      puts 'This is not a valid roman numeral!'
+      return
+    end
+
+    if val < prev
+      val = val * -1
+    else
+      prev = val
+    end
+    total = total + val
+  end
+
+  total
 end
+
+puts(roman_to_integer('ix'))
+puts(roman_to_integer('54'))
+# puts(roman_to_integer('mcmxcix'))
+# puts(roman_to_integer('CCCLXV'))

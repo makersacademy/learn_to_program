e174cf4fd7120f33ad8ac8843c6eff69747635ae
innlouvate <l.j.franklin85@googlemail.com>
2016-01-11 02:31:56 +0000

    playlist cp fix

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,48 +1,49 @@
-
-
-def shuffle arr
-  rec_shuffle arr, []
-end
-
-def rec_shuffle unshuffled_array, shuffled_array
-
-  if unshuffled_array.length <= 0
-    return shuffled_array
-  else
-    r=Random.rand(0...unshuffled_array.length)
-    shuffled_array << unshuffled_array[r]
-    unshuffled_array.delete_at(r)
-  end
-
-  rec_shuffle unshuffled_array, shuffled_array
-end
-
-def super_shuffle arr
-  return arr if arr.length <= 1
-  first_half = []
-  second_half = []
-  #half deck randomly
-  rand_index=Random.rand(arr.length)
-  shuffle(arr).each_with_index do |item, index|
-    if index <= (rand_index-1)
-      first_half << item
-    else
-      second_half << item
-    end
-  end
-
-  #shuffle contents and recursively super_shuffle until none left
-  super_shuffle(first_half) + super_shuffle(second_half)
-end
-
 def music_shuffle filenames
-  playlist_file = "playlist2.m3u"
-  playlist = super_shuffle filenames
+  # We don't want a perfectly random shuffle, so let's
+  # instead do a shuffle like card-shuffling. Let's
+  # shuffle the "deck" twice, then cut it once. That's
+  # not enough times to make a perfect shuffle, but it
+  # does mix things up a bit.
+  # Before we do anything, let's actually *sort* the
+  # input, since we don't know how shuffled it might
+  # already be, and we don't want it to be *too* random.
+  filenames = filenames.sort
+  len = filenames.length
 
-  File.open playlist_file, 'w' do |f|
-    f.write(playlist.join("\n"))
+  # Now we shuffle twice.
+  2.times do
+    l_idx = 0 # index of next card in left pile
+    r_idx = len/2 # index of next card in right pile
+    shuf = []
+    # NOTE: If we have an odd number of "cards",
+    # then the right pile will be larger.
+
+    while shuf.length < len
+      if shuf.length%2 == 0
+        # take card from right pile
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        # take card from left pile
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+
+    filenames = shuf
   end
+  # And cut the deck.
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+
+  arr
 end
-
-
-music_shuffle Dir['/Users/louisefranklin/Music/iTunes/iTunes\ Media/Music/**/*.m4a']
+# songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+#          'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+# puts(music_shuffle(songs))

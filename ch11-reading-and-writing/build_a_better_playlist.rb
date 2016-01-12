96dcd2047792c449fdb8ea4d4c2351a349786bf0
hedudelgado <hedu.delgado@gmail.com>
2016-01-12 20:30:02 +0000

    ch11-reading-and-writing/build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,12 +1,37 @@
-def music_shuffle filenames
-  
- arr = filenames.sample(filenames.length)
- 
- arr
- # your code here
+ def music_shuffle filenames
+ 	filenames = filenames.sort
+	quan = filenames.length
+
+ 2.times do
+		a = 0 
+		b = quan/2 
+ 		arr1 = []
+
+	while arr1.length < quan
+ 		if arr1.length%2 == 0
+			arr1.push(filenames[b])
+			b = b + 1
+		else
+			arr1.push(filenames[a])
+ 			a = a + 1
+ 		end
+	end
+
+    filenames = arr1
+ end
+
+ arr = []
+ cut = rand(quan) 
+ idx = 0
+
+ 	while idx < quan
+		arr.push(filenames[(idx+cut)%quan])
+		idx = idx + 1
+	end
+
+arr
 end
 
-songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
-          'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
-puts(music_shuffle(songs))
+#songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+#puts(music_shuffle(songs))
 

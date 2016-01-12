c3798d1c9a697098bb695274b2a81f85dc40167f
Alain Lemaire <jaxdid@gmail.com>
2016-01-12 06:08:53 +0100

    Answered Exercise 12-4

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,16 @@
-# your code here
+birthdays = {}
+
+File.readlines('birthdays.txt').each do |l|
+  name, date, year = line.split(',')
+  birthdays[name] = Time.gm(year, *(date.split))
+end
+
+puts 'Whose birthday would you like to know?'
+name = gets.chomp
+bday = birthdays[name]
+
+if !bday
+  puts 'Unknown name!'
+else
+  puts bday
+end

9f1bcd7a05397f4e45df7a94c3f9a4131ed10c01
Finble <elainefinn@hotmail.com>
2016-01-12 18:07:45 +0000

    ch14 - bttrprogram - CPs soln, rspec passed

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,32 @@
-def log desc, &block
-  # your code here
+$logger_depth = 0  # Global variable
+
+def log desc, &block  # proc method name = log, takes args including a proc = &block
+  prefix = ' '*$logger_depth  # new var to store current logger_depth, which at beginning, has a depth of 0?
+
+  puts prefix + 'Beginning "' + desc + '"...'
+
+  $logger_depth = $logger_depth + 1
+  
+  result = block.call
+
+  $logger_depth = $logger_depth - 1
+  puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
 end
+
+log 'outer block' do
+  log 'some little block' do # nested blocks
+    log 'teeny-tiny block' do  # nested blocks
+      'lOtS oF lOVe'.downcase
+    end
+
+    7 * 3 * 2
+  end
+
+  log 'yet another block' do
+    '!doof naidnI evol I'.reverse
+  end
+
+  '0' == "0"  # still not getting this...!
+end
+
+# mmmm, lack of context has not helped - why would you do this in the real-world?  Going to practice this one in pairing.

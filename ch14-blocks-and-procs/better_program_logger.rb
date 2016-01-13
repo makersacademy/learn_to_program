c47fd8a07f6fd2e62174ebce37a7042686be061f
hedudelgado <hedu.delgado@gmail.com>
2016-01-13 00:03:53 +0000

    ch14-blocks-and-procs/better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,27 +1,35 @@
 $logcounter = 0
 def log desc, &block
-	cheat = ' ' * $logcounter 
-	puts cheat + 'Beginning "' + desc + '"...'
-	$logcounter = $logcounter + 1
-  	result = block.call
-  	$logcounter = $logcounter -1
-  	puts cheat + '..."' + desc + '" finished, returning: ' + result.to_s # your code here
+# your code here
+
+end
+
+
+def better_log desc, &block
+  cheat = ' ' * $logcounter 
+  puts cheat + 'Beginning "' + desc + '"...'
+  $logcounter = $logcounter + 1
+  result = block.call
+  $logcounter = $logcounter -1
+  puts cheat + '..."' + desc + '" finished, returning: ' + result.to_s 
 end
 
 
 
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
+
+better_log 'outer block' do
+  better_log 'some little block' do
+    better_log 'teeny-tiny block' do
       'lOtS oF lOVe'.downcase
     end
 
-    7*6
+    42
   end
 
-  log 'yet another block' do
-    '!doof naidnI evol I'.reverse
+  better_log 'yet another block' do
+    'I love Indian food!'
   end
 
-  '0' == "0"
+  true
+
 end

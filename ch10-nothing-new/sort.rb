96b53a31ef30e7d8563f8ec588ba4725a30dbb7b
Murilo Dal Ri <nope@nope.com>
2016-02-15 21:21:34 +0000

    ch10 sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,23 @@
-def sort arr
-  # your code here
+def sort (arr)
+  recursive_sort(arr, [])
+end
+
+def recursive_sort (unsorted, sorted)
+  if unsorted.size <= 0
+    return sorted
+  end
+  small = unsorted.pop
+  to_sort = []
+
+  unsorted.each do |x|
+    if x < small
+      to_sort << small
+      small = x
+    else
+      to_sort << x
+    end
+  end
+  sorted << small
+
+  recursive_sort(to_sort, sorted)
 end

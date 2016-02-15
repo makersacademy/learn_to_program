61c0767d7e3a10e6cfd6145a5908887193751a46
MisaOgura <misa.ogura01@gmail.com>
2016-02-15 17:59:54 +0000

    Tidy solution up for ch09 roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -7,9 +7,7 @@ def roman_numeral num
   roman << "X" * (num % 50 / 10)
   roman << "V" * (num % 10 / 5)
   roman << "I" * (num % 5)
-
   old_to_new = {/DCCCC/ => "CM", /CCCC/ => "CD", /LXXXX/ => "XC", /XXXX/ => "XL", /VIIII/ => "IX", /IIII/ => "IV"}
-  p roman
   old_to_new.each {|k, v| roman.sub!(k, v)}
-  p roman
+  roman
 end

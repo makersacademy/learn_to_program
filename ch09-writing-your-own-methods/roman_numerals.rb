b38eeb96dbbf2f80eff16a24dd497117c71d177f
Lorenzo Turrino <neuronefunzionante@gmail.com>
2016-02-15 17:33:22 +0000

    solve 9.2 and 9.3

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,10 @@
 def roman_numeral num
-  # your code here
+  roman = { 1 => 'I', 4 => 'IV', 5 => 'V', 9 => 'IX', 10 => 'X', 40 => 'XL', 50 => 'L', 90 => 'XC', 100 => 'C', 400 => 'CD', 500 => 'D', 900 => 'CM', 1000 => 'M' }
+  out = String.new
+  roman.keys.reverse.each do |key| 
+      val = (num / key)
+      num -= key * val
+      out << roman[key] * val
+  end
+  out
 end

c70e26cd9c9077adf89b37afb44ffb710fdd2d90
MisaOgura <misa.ogura01@gmail.com>
2016-02-15 15:27:38 +0000

    ch09 ask solution

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,10 @@
 def ask question
-  # your code here
+    puts question
+    reply = gets.chomp.downcase
+    unless (reply == "yes" || reply == "no")
+      puts 'Please answer "yes" or "no".'
+      ask question
+    else
+      reply == "yes" ? true : false
+    end
 end

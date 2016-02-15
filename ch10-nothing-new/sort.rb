cb1916dc36aaa8e55d846ba674d622d2a1d1af2e
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-15 14:57:52 +0000

    Updated exercise ch10/sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,13 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  n = 1
+  size = unsorted.size
+  while n <= size do
+    unsorted.each { |word| sorted << unsorted.delete_at(unsorted.index(word)) if word == unsorted.min }
+    n += 1
+  end
+  return sorted
 end

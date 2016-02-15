c91d06ff8569b8d5049e1995729d9a324a425842
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-15 15:34:58 +0000

    Updated challenge ch10/dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  n = 1
+  size = unsorted.size
+  while n <= size do
+    unsorted.each do |word|
+      if word.downcase == unsorted.join(" ").downcase.split.min
+        sorted << unsorted.delete_at(unsorted.index(word))
+      end
+    end
+    n += 1
+  end
+  return sorted
 end

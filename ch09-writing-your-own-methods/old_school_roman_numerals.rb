a220936aaf8fc1bfba16a5951b7897969f047637
Paul <paulalexrees@gmail.com>
2016-02-15 15:50:13 +0000

    Answer to old school numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+    arr = []
+    hash = {0 => "M", 1 => "D", 2 => "C", 3 => "L", 4 => "X", 5 => "V", 6 => "I"}
+    # iterate over number divisions
+    for i in [1000,500,100,50,10]
+      num/i > 0 ? arr << num/i : arr << 0
+      num -= num/i * i
+    end
+    num < 5 ? (arr << 0 << num) : (arr << 1 << num % 5)
+
+    # return joined array with arr indices converted to letters
+    arr.map.with_index{|x,i| hash.values_at(i) * x}.join
 end

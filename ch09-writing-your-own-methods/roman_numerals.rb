328a20165d0ffb74dec0646417d2ea1ec818bdb7
Tobenna <tobennandu@gmail.com>
2016-02-15 23:41:07 +0000

    Roman numerals completed

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,7 @@
-def roman_numeral num
-  # your code here
+def roman_numeral(_num)
+  roman = %w(M CM D CD C XC L XL X IX V IV I)
+  arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  values = roman.zip(arab)
+  c = 0
+  values.map { |l, v| c, _num = _num.divmod v; l * c }.join ''
 end

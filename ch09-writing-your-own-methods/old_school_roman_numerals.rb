dabf16eabb5653f4ec4329d06f5fe65c06ebeafc
MisaOgura <misa.ogura01@gmail.com>
2016-02-15 16:04:23 +0000

    Solve cd09 old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+  thousands = num / 1000
+  five_hundreds = num % 1000 / 500
+  hundreds = num % 1000 % 500 / 100
+  fiftys = num % 1000 % 500 % 100 / 50
+  tens = num % 1000 % 500 % 100 % 50 / 10
+  fives = num % 1000 % 500 % 100 % 50 % 10 / 5
+  ones = num % 1000 % 500 % 100 % 50 % 10 % 5
+  "M"*thousands + "D"*five_hundreds + "C"*hundreds + "L"*fiftys + "X"*tens + "V"*fives + "I"*ones
 end
+
+# lettets to use: I = 1, V = 5, X = 10,
+# L = 50, C = 100, D = 500, M = 1000

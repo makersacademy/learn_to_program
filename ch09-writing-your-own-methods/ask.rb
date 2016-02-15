4128299e80b59f1924260c48ebac1e08ea04cb19
Tobenna <tobennandu@gmail.com>
2016-02-15 23:07:46 +0000

    Ask question

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,20 @@
 def ask question
-  # your code here
+  goodAnswer = false
+  while (not goodAnswer)
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' or reply == 'no')
+      goodAnswer = true
+      if reply == 'yes'
+        answer = true
+      else
+        answer = false
+      end
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+
+  answer  # This is what we return (true or false).
 end

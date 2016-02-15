d4f4f796b57d9016650eb55a9ec9e1f57c5ac24c
Pete BURCH <peteburch223@gmail.com>
2016-02-15 19:07:29 +0100

    Answered ch09 ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,4 +1,13 @@
 def ask question
-  # your code here 
-  
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == 'yes' || reply == 'no')
+      return reply == 'yes'
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+
 end
+

e8338a54231a3f5db53c61852bd6992628671288
Tobenna <tobennandu@gmail.com>
2016-02-15 23:08:51 +0000

    Ask question

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -6,15 +6,9 @@ def ask question
 
     if (reply == 'yes' or reply == 'no')
       goodAnswer = true
-      if reply == 'yes'
-        answer = true
-      else
-        answer = false
-      end
+      return reply == 'yes' ? true : false
     else
       puts 'Please answer "yes" or "no".'
     end
   end
-
-  answer  # This is what we return (true or false).
 end

5bb4978efff7ad3f064ef203db7453ceea9d4085
Paul <paulalexrees@gmail.com>
2016-02-15 16:09:21 +0000

    Slight refactor using str instead of array

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,13 +1,10 @@
 def old_roman_numeral num
-    arr = []
-    hash = {0 => "M", 1 => "D", 2 => "C", 3 => "L", 4 => "X", 5 => "V", 6 => "I"}
+    str = ""
+    hash = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 6 => "I"}
     # iterate over number divisions
-    for i in [1000,500,100,50,10]
-      num/i > 0 ? arr << num/i : arr << 0
+    for i in [1000,500,100,50,10,5]
+      str << hash.values_at(i).join * (num/i) if num/i > 0
       num -= num/i * i
     end
-    num < 5 ? (arr << 0 << num) : (arr << 1 << num % 5)
-
-    # return joined array with arr indices converted to letters
-    arr.map.with_index{|x,i| hash.values_at(i) * x}.join
+    str << "I" * num
 end

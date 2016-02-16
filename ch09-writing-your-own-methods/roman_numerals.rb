6c89d74c27a59fac899756342e08c9640754785a
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-16 18:51:39 +0000

    Roman numerals complete

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,26 @@
 def roman_numeral num
-  # your code here
+  letters = ""
+
+  roman = {
+          "M"  => 1000,
+          "CM" => 900,
+          "D"  => 500,
+          "CD" => 400,
+          "C"  => 100,
+          "XC" => 90,
+          "L"  => 50,
+          "XL" => 40,
+          "X"  => 10,
+          "IX" => 9,
+          "V"  => 5,
+          "IV" => 4,
+          "I"  => 1}
+
+  roman.each do |key, val|
+    until num < val
+      num -= val
+      letters << key
+    end
+  end
+ return letters
 end

943a3dc8f51cd31abc02a3783b2d07345edf146c
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-16 13:11:56 +0100

    shuffle done

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,11 @@
-def shuffle arr
-  # your code here
+def shuffle array
+rec_meth(array, [])
+end
+
+def rec_meth(array,shuffled)
+  if array == []
+    return shuffled
+  end
+  shuffled << array.delete_at(array.index(array.0))
+  rec_meth(array,shuffled)
 end

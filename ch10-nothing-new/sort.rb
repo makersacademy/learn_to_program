1e45d56180c14e65aa36122291ad4dd1f043cf36
Tobenna <tobennandu@gmail.com>
2016-02-16 15:40:07 +0000

    Sorted array

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,18 @@
 def sort arr
-  # your code here
+  recursive_sort(arr, [])
+end
+def recursive_sort arr1, arr2
+  return arr2 if arr1.length == 0
+  arr2 << arr1.slice!(arr1.index(arr1.min))
+  recursive_sort(arr1, arr2)
+  arr2
+end
+
+def sort unsorted
+  sorted = []
+   until unsorted == []
+     sorted << unsorted.min
+     unsorted.slice!(unsorted.index(unsorted.min))
+   end
+ sorted
 end

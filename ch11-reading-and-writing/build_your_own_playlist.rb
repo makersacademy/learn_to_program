2be00d8e3d6ba145c50db85242416e2aa223191d
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-16 19:20:24 +0000

    Updated exercise ch11/build_your_own_playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
-# your code here
+Dir.chdir "/Users/junyuanxue/Music/Smash"
+
+music = Dir['*.m4a'].shuffle
+p music
+File.open "smash_playlist.m3u", "w" do |f|
+  music.each do |song|
+    f.write "#{File.basename song}\n"
+  end
+end
+
+puts "Playlist created!"

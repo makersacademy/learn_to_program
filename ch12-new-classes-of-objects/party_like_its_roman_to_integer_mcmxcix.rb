a7cffddf4be46d70bda988e8dfec5f03713328d2
Hanif Fakira <fakira.work@gmail.com>
2016-02-16 16:28:46 +0000

    chap 12 rom to int

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,25 @@
+ROM_VAL = {"i"=>1, "v" => 5, "x" => 10, "l" =>50, "c" => 100, "d" => 500, "m"=>1000}
+
 def roman_to_integer roman
-  # your code here
+  num_arr = roman.downcase.split("").inject([]){|arr, c| arr.push(ROM_VAL[c]) }
+
+  sum = 0
+  while num_arr !=[]
+    a = num_arr.max
+    if num_arr[0] == a
+      sum += num_arr[0]
+      num_arr.shift
+    elsif num_arr[1] == a
+      subt = num_arr[1] - num_arr[0]
+      sum += subt
+      num_arr.shift(2)
+    else
+      puts "Numerals in wrong order"
+      exit
+    end
+  end
+  sum
+
 end
+
+p roman_to_integer 'mcmxcix'

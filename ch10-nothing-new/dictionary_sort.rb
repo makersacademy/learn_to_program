f99d9902f989d8a2989230689e2952dd2ef4c6c8
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-16 14:14:26 +0100

    ch10 dictionary style completed

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,14 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+rec_meth(array, [])
 end
+
+def rec_meth(array,sorted_array)
+  if array == []
+    return sorted_array
+  end
+  sorted_array << array.delete_at(array.map{|b| b.downcase}.index(array.map{|a| a.downcase}.min))
+  rec_meth(array,sorted_array)
+  
+end
+
+ puts dictionary_sort("A can can feel like singing.".split)

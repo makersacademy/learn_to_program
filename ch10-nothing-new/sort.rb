a961169153a7d1f0fea915f8f245e20c59067701
MisaOgura <misa.ogura01@gmail.com>
2016-02-16 07:40:30 +0000

    Solve ch10 sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,7 @@
 def sort arr
-  # your code here
+  return arr if arr.length <= 1
+  mid = arr.pop
+  before = arr.select {|word| word < mid}
+  after = arr.select {|word| word >= mid}
+  sort(before) + [mid] + sort(after)
 end

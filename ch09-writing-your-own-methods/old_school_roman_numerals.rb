3e631f40b0e2df8bcaa75044ea36f06506f8e60b
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-16 14:59:40 +0000

    Corrected ch09 old_school_roman_numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -4,6 +4,7 @@ def old_roman_numeral num
   c = (num-m*1000-d*500)/100.to_i
   l = (num-m*1000-d*500-c*100)/50.to_i
   x = (num-m*1000-d*500-c*100-l*50)/10.to_i
-  i = num-m*1000-d*500-c*100-l*50-x*10
-  return "M"*m + "D"*d + "C"*c + "L"*l + "X"*x + "I"*i
+  v = (num-m*1000-d*500-c*100-l*50-x*10)/5.to_i
+  i = num-m*1000-d*500-c*100-l*50-x*10-v*5
+  return "M"*m + "D"*d + "C"*c + "L"*l + "X"*x + "V"*v + "I"*i
 end

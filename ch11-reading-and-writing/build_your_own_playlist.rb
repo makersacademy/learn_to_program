6f3c4ddc565743dd23906f61dc0ae2e5a8c5ae7d
Hanif Fakira <fakira.work@gmail.com>
2016-02-16 13:58:40 +0000

    Chap 11 build playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,18 @@
-# your code here
+def shuffle arr
+  arr.sample(arr.length)
+end
+
+#load list of songs in array
+array = Dir['/home/han/ruby/Pine/solution/music/*.mp3']
+#shuffled array
+shuffled_music = shuffle(array)
+#copy array into file and name playlist.m3u
+#Ask user for name of playlist
+
+filename = 'shuffled music playlist.m3u'
+
+File.open filename, 'w' do |f|
+f.write shuffled_music.join("\n")
+end
+
+puts "Done...Your playlist #{filename} is ready"

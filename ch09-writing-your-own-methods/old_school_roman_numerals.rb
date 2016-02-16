e1ac42a0cb32c01c9d6b803c0c3c87d6704d1b53
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-16 13:04:01 +0000

    ch09 old_school_roman_numerals completed

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,27 @@
 def old_roman_numeral num
-  # your code here
+  result = ""
+  thousands = num/1000.to_i
+  thousands.times do result << "M" end
+  hundreds = (num-thousands*1000)/100.to_i
+  if hundreds >= 5
+    result << "D"
+    (hundreds-5).times do result << "C" end
+  else
+    hundreds.times do result << "C" end
+  end
+  tens = (num - thousands*1000 - hundreds*100)/10.to_i
+  if tens >= 5
+    result << "L"
+    (tens-5).times do result << "X" end
+  else
+    tens.times do result << "X" end
+  end
+  ones = num - thousands*1000 - hundreds*100 - tens*10
+  if ones >= 5
+    result << "V"
+    (ones-5).times do result << "I" end
+  else
+    ones.times do result << "I" end
+  end
+  return result
 end

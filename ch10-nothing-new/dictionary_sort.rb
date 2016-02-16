42d6bdabb16b7a2ed4542a251109ec45cc0266bb
Lorenzo Turrino <neuronefunzionante@gmail.com>
2016-02-16 16:47:40 +0000

    solve some of challenge 10, add continent_grower

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
 def dictionary_sort arr
-  # your code here
+  
+  arr.length.times do                                                                    #arr.length-1 times should be enough.
+    completion_flag = true
+    (0..arr.length-2).each do |index|
+      arr[index], arr[index+1] = arr[index+1], arr[index] if arr[index].downcase > arr[index+1].downcase
+    end
+  end
+  arr
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,3 @@
 def sort arr
-  arr.length < 2 ? arr : [arr.slice!(arr.index(arr.min))] << sort(arr)
+  arr.length < 2 ? arr : [arr.slice!(arr.index(arr.min))] + sort(arr)
 end

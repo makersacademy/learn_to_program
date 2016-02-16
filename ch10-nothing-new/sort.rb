05b9e21e78e74f7705409e45b50c7b9e80dd95b3
Paul <paulalexrees@gmail.com>
2016-02-16 12:47:14 +0000

    Recursive sort solution

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,14 @@
 def sort arr
-  # your code here
+  recursive_sort(arr,[])
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+  length = 1
+  while unsorted_array.size != sorted_array.size
+    unsorted_array.each{|x| sorted_array << x if x.length == length}
+    length += 1
+  end
+  sorted_array
+end
+
+#puts sort ["a", "can", "singing", "like", "feel", "can"]

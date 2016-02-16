bd6867b2e2b631ae2a48729857b56b353aaf9326
Murilo Dal Ri <nope@nope.com>
2016-02-16 15:50:07 +0000

    ch10 dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
 def dictionary_sort arr
-  # your code here
+    sort(arr, [])
+  end
+
+  def sort (unsorted, sorted)
+    return sorted if unsorted.size <= 0
+
+    small = unsorted.pop
+    to_sort = []
+
+    unsorted.each do |x|
+      if x.downcase < small.downcase
+        to_sort << small
+        small = x
+      else
+        to_sort << x
+      end
+    end
+    
+    sorted << small
+
+    sort(to_sort, sorted)
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -3,21 +3,20 @@ def sort (arr)
 end
 
 def recursive_sort (unsorted, sorted)
-  if unsorted.size <= 0
-    return sorted
-  end
-  small = unsorted.pop
-  to_sort = []
+  return sorted if unsorted.length == 0
+
+  smallest = unsorted.pop
+  sort_this_shit = []
 
   unsorted.each do |x|
-    if x < small
-      to_sort << small
-      small = x
+    if smallest < x
+      sort_this_shit << x
     else
-      to_sort << x
+      sort_this_shit << smallest
+      smallest = x
     end
   end
-  sorted << small
 
-  recursive_sort(to_sort, sorted)
+  sorted << smallest
+  recursive_sort(sort_this_shit, sorted)
 end

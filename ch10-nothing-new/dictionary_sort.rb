d16938ffe4463f424528f7bfbc88ccc0e19355f8
yasgreen <yasmin.green@btinternet.com>
2016-02-16 19:47:19 +0000

    Completed dictionary_sort. Had to consult answers to get an idea as to what the question was asking as it was a bit unclear

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
-def dictionary_sort arr
+def dictionary_sort(arr)
   # your code here
+  rec_sort(arr, [])
 end
+
+def rec_sort(unsorted, sorted)
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |test_obj|
+    if test_obj.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = test_obj
+    else
+      still_unsorted.push test_obj
+    end
+  end
+
+  sorted.push smallest
+  rec_sort(still_unsorted, sorted)
+end
+
+# puts dictionary_sort(['can','feel','singing.','like','A','can'])

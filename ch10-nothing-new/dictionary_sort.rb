bcbd5bd1ea61ebcf8c288c5a927afa61e55fc5c8
Paul <paulalexrees@gmail.com>
2016-02-16 13:16:58 +0000

    Dictionary sort solution

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
+
 def dictionary_sort arr
-  # your code here
+  recursive_sort(arr,[])
 end
+
+def recursive_sort(unsorted_array, sorted_array, length=1)
+  unless unsorted_array.size == sorted_array.size
+    unsorted_array.each{|x| sorted_array << x if x.length == length}
+    recursive_sort(unsorted_array,sorted_array, length+1)
+  end
+  #end
+  sorted_array
+end
+
+
+
+puts dictionary_sort(["hello","can","Be","I", "be"])

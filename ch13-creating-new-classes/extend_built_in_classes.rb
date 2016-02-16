26cd434109747b3c0a7004d3e5e747dcb7d58919
Hanif Fakira <fakira.work@gmail.com>
2016-02-16 16:42:02 +0000

    chap 13 extend classes edit

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -4,22 +4,6 @@ class Integer
     (self- 1).factorial * self
   end
 
-  ROMAN = {
-    "M"   => 1000,
-    "CM"  => 900,
-    "D"   => 500,
-    "CD"  => 400,
-    "C"   => 100,
-    "XC"  => 90,
-    "L"   => 50,
-    "XL"  => 40,
-    "X"   => 10,
-    "IX"  => 9,
-    "V"   => 5,
-    "IV"  => 4,
-    "I"   =>1
-  }
-
   def to_roman
     num = self
     remainder_1000 = num % 1000

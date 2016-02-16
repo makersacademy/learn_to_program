53b8882139e5ac5cdb83efabe50fde1a32a55d6d
adilw3nomad <adiln1993@gmail.com>
2016-02-16 16:47:51 +0000

    Shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,7 @@
-def shuffle arr
-  # your code here
+def shuffle(array)
+  array.size.times do |i|
+      random = i + rand(array.size-i)
+      array[i], array[random] = array[random], array[i]
+  end
+  array
 end

442c172ed0d9720b9389163dc4201db50523db5a
Hanif Fakira <fakira.work@gmail.com>
2016-02-16 14:36:49 +0000

    chap 11 build better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,16 @@
 def music_shuffle filenames
-  # your code here
+  filenames.map!{|song| song.split("/")}
+  mixed_playlist = []
+  mixed_playlist << filenames.delete(filenames.sample)
+  until filenames.empty?
+    rand_song = filenames.sample
+    if mixed_playlist.last[0] != rand_song[0]
+      mixed_playlist << filenames.delete(rand_song)
+    end
+  end
+  mixed_playlist.map!{|song| song.join("/")}
 end
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+
+p music_shuffle songs

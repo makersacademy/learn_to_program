098099831725786f43ad6e98157b42f6aced41e6
Shane Oston Stowe <shane@shaneoston.com>
2016-02-17 12:34:39 +0000

    adding ch10/shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,11 @@
+# Completed in pair programming Kevin McCarthy
 def shuffle arr
-  # your code here
+	shuffled_arr = []
+
+	arr.size.times  do
+		index = rand(arr.length)
+		shuffled_arr << arr[index]
+		arr.delete_at(index)
+	end
+	return shuffled_arr
 end

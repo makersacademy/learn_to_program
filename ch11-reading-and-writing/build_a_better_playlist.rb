c030df88d696043b95c0fe905d735509527cf77f
Hanif Fakira <fakira.work@gmail.com>
2016-02-17 16:01:45 +0000

    Chap 11 better shuffle edit

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -2,15 +2,12 @@ def music_shuffle filenames
   filenames.map!{|song| song.split("/")}
   mixed_playlist = []
   mixed_playlist << filenames.delete(filenames.sample)
-  until filenames.empty?
+  while filenames.size >0
     rand_song = filenames.sample
-    if mixed_playlist.last[0] != rand_song[0]
-      mixed_playlist << filenames.delete(rand_song)
-    end
+    # if mixed_playlist.last[0] != rand_song[0]
+    #    mixed_playlist << filenames.delete(rand_song)
+    # end
+    mixed_playlist << filenames.delete(rand_song)
   end
   mixed_playlist.map!{|song| song.join("/")}
 end
-
-songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
-
-p music_shuffle songs

1348d909c6c9053e933f977299ed044e3bff079e
Murilo Dal Ri <nope@nope.com>
2016-02-17 15:18:45 +0000

    playlists

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,13 @@
 def music_shuffle filenames
-  # your code here
+  Dir.chdir '/Users/murilodalri/Desktop'
+  def shuffle(arr)
+    arr.shuffle
+  end
+  mp3s = Dir['/Users/murilodalri/Desktop/Music/**/*.mp3']
+  mp3s.shuffle!
+  File.open 'playlist.m3u', 'w' do |f|
+    mp3s.each do |mp3|
+      f.write mp3+"\n"
+    end
+  end
 end

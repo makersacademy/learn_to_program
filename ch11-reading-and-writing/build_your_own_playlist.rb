104bcab608d6686e6540cc9f58939bbf08267aa6
MisaOgura <misa.ogura01@gmail.com>
2016-02-17 17:29:30 +0000

    Solve ch11  build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,22 @@
-# your code here
+# This programme finds all the mp3 files in the computer
+# and creates a shuffled playlist
+
+# 1. moves to the Music directory
+Dir.chdir "/Users/misaogura/Music"
+
+# 2. looks for ALL the mp3 files in the directory and subdirectories
+mp3_list = Dir["**/*.mp3"]
+
+# 3. creates a shuffled list with a file name in each line
+shuffled_list = mp3_list.sample(mp3_list.length).join("\n")
+
+# 4. takes user input for the playlist name
+puts "Enter the name of the playist you would like to create:"
+filename = gets.chomp + ".m3u"
+
+# 5. creates a playlist
+File.open filename, 'w' do |file|
+  file.write shuffled_list
+end
+
+puts "All done! You have created a playlist called #{filename}"

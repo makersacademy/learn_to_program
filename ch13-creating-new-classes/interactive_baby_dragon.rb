88490eab24029b6d0a2d41adc0fd955bf605547b
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-17 22:56:21 +0000

    Updated exercise ch13/interactive_baby_dragon

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,22 @@
-# your code here
+puts "Please give your baby dragon a name:"
+name = gets.chomp
+dragon = Dragon.new name
+puts "Please type in your command:"
+puts "feed, walk, put to bed, toss, rock, exit"
+command = gets.chomp
+
+if command == "feed"
+  dragon.feed
+elsif command == "walk"
+  dragon.walk
+elsif command == "put to bed"
+  dragon.put_to_bed
+elsif command == "toss"
+  dragon.toss
+elsif command == "rock"
+  dragon.rock
+elsif command == "exit"
+  exit
+else
+  return "Please type in one of the following commands: feed, walk, put to bed, toss, rock, exit"
+end

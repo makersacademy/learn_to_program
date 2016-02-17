3b9a3fd0de0cbfaa4363badcfd92a3d32a0e6aa9
eripheebs <erikapheby@gmail.com>
2016-02-17 13:05:14 +0000

    Chapter 10 Dictionary Sort Answered

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
 def dictionary_sort arr
-  # your code here
+	recursive_sort arr, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+	return sorted_array if unsorted_array.length <= 0
+
+	item = unsorted_array.pop
+	still_unsorted = []
+	unsorted_array.each do |x| 
+		if x.downcase < item.downcase #this is the only change I made to the non-dictionary sort code
+			still_unsorted.push item
+			item = x 
+		else
+			still_unsorted.push x
+		end
+	end
+
+	sorted_array.push item
+
+	recursive_sort still_unsorted, sorted_array
 end

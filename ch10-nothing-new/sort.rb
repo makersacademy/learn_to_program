c3e3be942570ae18aac093617d447b3432b8a22c
Andrew Cookson <acookson91@gmail.com>
2016-02-17 06:46:19 +0000

    Created sort without recursion

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,23 @@
 def sort arr
-  # your code here
+  rec_sort arr, []
+end
+
+def rec_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+smallest = unsorted.pop
+still_unsorted = []
+
+  unsorted.each do |newword|
+    if newword < smallest
+      still_unsorted.push smallest
+      smallest = newword
+    else
+      still_unsorted.push newword
+    end
+    end
+    sorted.push smallest
+  rec_sort still_unsorted, sorted
 end

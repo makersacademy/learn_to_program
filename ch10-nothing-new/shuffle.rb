d8a1f04a8678ad7e94c1128a0c59fffba6637284
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-17 10:33:56 +0000

    ch10 shuffle completed

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,10 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle arr, []
+end
+
+def recursive_shuffle unshuffled, shuffled
+  return shuffled if unshuffled.empty?
+  move = unshuffled.delete_at(rand(unshuffled.length))
+  shuffled << move
+  recursive_shuffle unshuffled, shuffled
 end

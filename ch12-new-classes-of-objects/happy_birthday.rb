71b67e748a1976dc92ba197da102db408b0751dc
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-17 20:36:55 +0100

    happy birthday with spanks done

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "what month were you born?"
+month = gets.chomp
+
+puts "What day were you born?"
+day= gets.chomp# your code here
+
+
+years = (((Time.new - Time.new(year,month,day))/31104000).to_i)-1
+
+puts "You are #{years} years old"
+
+years.times do
+  puts "SPANK!"
+end

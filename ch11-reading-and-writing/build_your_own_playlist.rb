9b0678d13017a756a82fbc109e35fdca0b9e17fb
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-17 13:32:31 +0000

    ch11 build_your_own_playlist completed

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,8 @@
-# your code here
+Dir.chdir "/Users/Rhiannon/Documents"
+puts "What would you like to name this playlist of 5 random songs?"
+playlist_name = gets.chomp
+possibilities = Dir['/Users/Rhiannon/Music/iTunes/iTunes Media/Music/**/*.mp3']
+playlist = possibilities.sample(5)
+playlist_text = ""
+playlist.each {|x| playlist_text << "#{x}\n"}
+File.open "#{playlist_name}.m3u",'w' do |f| f.write "#{playlist_text}" end

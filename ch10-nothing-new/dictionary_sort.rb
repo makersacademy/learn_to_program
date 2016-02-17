c036da44c09d40522ef9b0562a9336e7ecf826de
adilw3nomad <adiln1993@gmail.com>
2016-02-17 11:00:22 +0000

    dictionary sort fixed to pass test

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,9 +1,10 @@
 def dictionary_sort array
-    array2 = []
-    temp_array = array.map{|x| x.capitalize}
-    until temp_array.empty?
-        array2 << temp_array.min
-        temp_array.delete_at(temp_array.find_index(temp_array.min))
-    end
-    array2 
+    rec_sort(array,[])
+end
+
+def rec_sort(ary,ary2)
+    return ary2 if ary.length == 0
+    temp_array = ary.map{|x| x.upcase}
+    ary2 << ary.slice!(temp_array.index(temp_array.min))
+    rec_sort(ary,ary2)
 end

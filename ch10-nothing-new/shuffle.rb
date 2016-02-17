63e9fe0fde963e7c945eb8287cc0702fc0358775
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-17 15:19:06 +0000

    Answer shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,7 @@
-def shuffle arr
-  # your code here
+def shuffle arr 
+  # arr.sort_by(&:rand)
+  # note the above was how Chris pine did 
+  # do it, but it no longer works, instead do the
+  # following:
+  arr.shuffle
 end

c9698b250ac430c3946fd9169a23d6156ebd6cc8
adilw3nomad <adiln1993@gmail.com>
2016-02-17 04:39:05 +0000

    Dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,9 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+    array2 = []
+    temp_array = array.map{|x| x.capitalize}
+    until temp_array.empty?
+        array2 << temp_array.min
+        temp_array.delete_at(temp_array.find_index(temp_array.min))
+    end
+    array2 
 end

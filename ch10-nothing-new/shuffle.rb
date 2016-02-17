5ce4621c401a9443ba5873b59edf217d861cd00d
adilw3nomad <adiln1993@gmail.com>
2016-02-17 10:54:10 +0000

    shuffle made to pass rspec

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,7 +1,12 @@
 def shuffle(array)
+  array.shuffle
+end
+
+def shuffl(array)
   array.size.times do |i|
       random = i + rand(array.size-i)
       array[i], array[random] = array[random], array[i]
   end
   array
 end
+

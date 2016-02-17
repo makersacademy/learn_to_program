e5da9a42a96ff72a55c17d429fd135741a9e40ee
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2016-02-17 19:22:00 +0000

    Chris Pine, chpt 10, dictionary_sort.rb: complete

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,35 @@
+
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+def recursive_sort(unsorted_arr, sorted_arr)
+  if unsorted_arr.length <= 0
+    return sorted_arr
+  end
+
+  new_unsorted_arr = []
+  last_word = unsorted_arr.pop
+
+  unsorted_arr.each do |word|
+    if word < last_word
+      new_unsorted_arr << last_word
+      last_word = word
+    else
+      new_unsorted_arr << word
+    end
+  end
+  sorted_arr << last_word
+  unsorted_arr = new_unsorted_arr
+
+  recursive_sort(new_unsorted_arr, sorted_arr)
+end
+
+
+# Alternative from GVK: works for both sort & dictionary_sort
+# def recursive_sort unsorted, sorted
+#     until unsorted == []
+#       sorted << unsorted.slice!(unsorted.index(unsorted.min))
+#     end
+#   sorted
+# end

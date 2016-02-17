d3ac43e64cadb712c1061cdc4780bc4b8d228689
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-02-17 14:23:39 +0000

    completed english_num

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,119 +1,114 @@
-#https://pine.fm/LearnToProgram/chap_08.html
-def englishNumber number
+def english_number number
 	if number < 0 # No negative numbers.
-		return "Please enter a number isn't negative."
+		return 'Please enter a number that isn\'t negative.' 
 	end
-	if number == 0
-		return "zero"
+	if number == 0 
+		return 'zero'
 	end
-
 	# No more special cases! No more returns!
-
-	numString = "" # This is the string we will return.
-
-	onesPlace = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-	tensPlace = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-	teenagers = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-
-	# "left" is how much of the number we still have left to write out.
-	# "write" is the part we are writing out right now.
-	# "write" and "left".... get it? :)
-
-	left = number
-
-	write = left / 1000000000
-	left = left - write * 1000000000
-	if write > 0
-		billions = englishNumber write
-		numString = numString + billions + " billion"
-
-		if left > 0
-			numString = numString + " "
-		end
-	end
-
-	write = left / 1000000
-	left = left - write * 1000000
-	if write > 0 
-		millions = englishNumber write
-		numString = numString + millions + " million"
-
-		if left > 0
-			numString = numString + " "
-		end
-	end
-
-	write = left / 1000
-	left = left - write * 1000
-
-	if write > 0
-		thousands = englishNumber write
-		numString = numString + thousands + " thousand"
-		
-		if left > 0
-			numString = numString + " "
-		end
-	end
-	write = left / 100 # How many hundreds left to write out?
-	left = left - write * 100 # Subtract off those hundreds.
+	num_string = '' # This is the string we will return.
+	ones_place = ['one',       'two',      'three',
+	            'four',      'five',     'six',
+	            'seven',     'eight',    'nine']
+	tens_place = ['ten',       'twenty',   'thirty',
+	            'forty',     'fifty',    'sixty',
+	            'seventy',   'eighty',   'ninety']
+	teenagers  = ['eleven',    'twelve',   'thirteen',
+	            'fourteen',  'fifteen',  'sixteen',
+				'seventeen', 'eighteen', 'nineteen'] 
+	powers_of_ten = {
+        (10**63) => "vigintillion",
+        (10**60) => "novemdecillion",
+        (10**57) => "octodeceillion",
+        (10**54) => "septendecillion",
+        (10**51) => "sexdecillion",
+        (10**48) => "quindecillion",
+        (10**45) => "quattuordecillion",
+        (10**42) => "tredecillion",
+        (10**39) => "duodecillion",
+        (10**36) => "undecillion",
+        (10**33) => "decillion",
+        (10**30) => "nonillion",
+        (10**27) => "octillion",
+        (10**24) => "septillion",
+        (10**21) => "sextillion",
+        (10**18) => "quintillion",
+        (10**15) => "quadrillion",
+        (10**12) => "trillion",
+        (10**9) => "billion",
+        1000000 => "million",
+        1000 => "thousand",
+    }
 	
-
-	if write > 0
-		# Now here's a really sly trick:
-		hundreds = englishNumber write
-		numString = numString + hundreds + " hundred"
-		# That's called "recursion". So what did I just do?
-		# I told this method to call itself, but with "write" instead of
-		# "number". Remember that "write" is (at the moment) the number of
-		# hundreds we have to write out. After we add "hundreds" to
-		# "numString", we add the string " hundred" after it. 
-		# So, for example, if we originally called englishNumber with
-		# 1999 (so "number" = 1999), then at this point "write" would
-		# be 19, and "left" would be 99. The laziest thing to do at this
-		# point is to have englishNumber write out the "nineteen" for us,
-		# then we write out " hundred", and then the rest of 
-		# englishNumber writes out "ninety-nine".
-
-		if left > 0
-			# So we don't write "two hundredfifty-one"
-			numString = numString + " "
+	left = number
+	powers_of_ten.each do |key, value|
+		write = left / key
+		left = left - write * key
+		num_string += "#{english_number write} #{value}" if write > 0
+		if write > 0 && left > 0
+		  num_string << " "
 		end
 	end
 
-	write = left / 10 			# How many tens left to write out?
-	left = left - write * 10	# Subtract off those tens.
+	write = left/100 # How many hundreds left?
+	left = left - write*100 # Subtract off those hundreds.
+	if write > 0
+	# Now here's the recursion:
+		hundreds = english_number write
+		num_string = num_string + hundreds + ' hundred' 
+		if left > 0
+	      	#  So we don't write 'two hundredfifty-one'...
+	      	num_string = num_string + ' '
+		end 
+	end
+
+	write = left/10 # How many tens left?
+	left = left - write*10 # Subtract off those tens.
 
 	if write > 0
-		if ((write == 1)) and (left > 0)
-			# Since we can't write "tenty-two" instead of "twelve",
-			# we have to make a special exception for these.
-			numString = numString + teenagers[left - 1]
-			# the "-1" is because teenagers[3] is "fourteen", not "thirteen".
-			# Since we took care of the digit in the ones place already,
-			# we have nothing left to write.
+		if ((write == 1) and (left > 0))
+	      	#  Since we can't write "tenty-two" instead of
+	      	#  "twelve", we have to make a special exception
+	      	#  for these.
+	      	num_string = num_string + teenagers[left-1]
+			# The "-1" is because teenagers[3] is
+			# 'fourteen', not 'thirteen'.
+			# Since we took care of the digit in the
+			# ones place already, we have nothing left to write. 
 			left = 0
 		else
-			numString = numString + tensPlace[write - 1]
-			# The "-1" is because tensPlace[3] is "forty", not "thirty".
+	     	num_string = num_string + tens_place[write-1]
+	      	#  The "-1" is because tens_place[3] is
+			# 'forty', not 'thirty'. 
 		end
-
 		if left > 0
-			# So we don't write "sixtyfour"
-			numString = numString + "-"
-		end
+			# So we don't write 'sixtyfour'... 
+			num_string = num_string + '-'
+		end 
 	end
-
-	write = left 	# how many ones left to write out?
-	left = 0		# Subtract off those ones.
-
+	write = left # How many ones left to write out? 
+	left = 0 # Subtract off those ones.
+	
 	if write > 0
-		numString = numString + onesPlace[write - 1]
-		# The "-1" is because onesPlace[3] is "four", not "three"
+		num_string = num_string + ones_place[write-1] 
+		# The "-1" is because ones_place[3] is
+		# 'four', not 'three'.
 	end
-
-	# Now we just return "numString"....
-	numString
+	  #  Now we just return "num_string"...
+	num_string
 end
 
-puts englishNumber(  0)
-
+puts english_number(  0)
+puts english_number(  9)
+puts english_number( 10)
+puts english_number( 11)
+puts english_number( 17)
+puts english_number( 32)
+puts english_number( 88)
+puts english_number( 99)
+puts english_number(100)
+puts english_number(101)
+puts english_number(234)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)

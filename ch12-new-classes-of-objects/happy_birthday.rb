67d364e284de47bb68719eba8d5149944bac41d0
Paul <paulalexrees@gmail.com>
2016-02-17 19:48:56 +0000

    Happy Birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,21 @@
-# your code here
+def spanky_birthday
+  puts "What year were you born?"
+  year = gets.chomp
+  puts "What month?"
+  month = gets.chomp
+  puts "What day?"
+  day = gets.chomp
+
+  time = Time.local(year, month, day)
+  today = Time.new
+
+  if ((time.month <= today.month) && (time.day <= today.day))
+    puts "#{today.year - time.year}!"
+    puts "SPANK " * (today.year - time.year)
+  else
+    puts "#{today.year - time.year - 1}!"
+    puts "SPANK " * (today.year - time.year - 1)
+  end
+end
+
+spanky_birthday

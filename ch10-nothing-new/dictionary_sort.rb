23b373f936ad9d572a2a2aa1fc97e31c6706ea71
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-17 10:42:21 +0000

    ch10 dictionary_sort updated

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -6,7 +6,7 @@ def recursive_dictionary_sort(unsorted,sorted)
   return sorted if unsorted.empty?
   to_test = unsorted.pop
   smallest = true
-  unsorted.each {|x| smallest = false if to_test > x}
+  unsorted.each {|x| smallest = false if to_test.downcase > x.downcase}
   if smallest
     sorted << to_test
   else

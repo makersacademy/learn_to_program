f457fe75c834a66894b68972d82a705f2bdb1b6c
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-17 20:50:16 +0000

    Updated exercise ch12/roman_to_integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,27 @@
 def roman_to_integer roman
-  # your code here
+  roman.upcase!
+  return "Not a valid Roman numeral." if roman =~ /[^ivxlcdm]/i
+  num = 0
+  integers = {"M" => 1000,
+              "D" => 500,
+              "C" => 100,
+              "L" => 50,
+              "X" => 10,
+              "V" => 5,
+              "I" => 1}
+  index = 0
+  while index < roman.length - 1
+    letter = roman[index]
+    next_letter = roman[index + 1]
+    if integers [letter] >= integers[next_letter]
+      num += integers[letter]
+    else
+      num -= integers[letter]
+    end
+    index += 1
+  end
+  num += integers[roman[-1]]
+  return num
 end
+
+p roman_to_integer "MCX"

902ae422f0b4ad834973e69caf5f7fea14006cee
Tobenna <tobennandu@gmail.com>
2016-02-17 10:43:08 +0000

    Chpt 10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,9 +1,10 @@
-def dictionary_sort arr
+def dictionary_sort(arr)
   recursive_sort(arr, [])
 end
-def recursive_sort arr1, arr2
+
+def recursive_sort(arr1, arr2)
   return arr2 if arr1.length == 0
-  arr3 = arr1.map {|a| a.to_s.downcase}
+  arr3 = arr1.map { |a| a.to_s.downcase }
   arr2 << arr1.slice!(arr3.index(arr3.min))
   recursive_sort(arr1, arr2)
   arr2

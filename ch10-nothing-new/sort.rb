11b342421fb6f21dc29101507b0edc184d86a799
Shane Oston Stowe <shane@shaneoston.com>
2016-02-17 11:39:35 +0000

    adding ch10, sort and dictionary_sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,22 @@
-def sort arr
-  # your code here
+def sort some_array # This "wraps" recursive_sort.  ????
+	recursive_sort some_array, []
 end
+def recursive_sort unsorted_array, sorted_array 
+	small = unsorted_array[0]
+	small_index = 0
+	if unsorted_array.size == 0
+		return sorted_array
+	else
+		unsorted_array.each_with_index do |word,index| 
+			if word <= small 
+				small = word
+				small_index = index
+			end
+		end
+		sorted_array << small
+		unsorted_array.delete_at(small_index)
+		recursive_sort(unsorted_array, sorted_array)
+	end
+end
+
+# sort(['can','feel','singing.','like','A','can'])

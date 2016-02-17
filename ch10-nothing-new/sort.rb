a07a15e2306eef702bffedc1a0e1d7bf4ef2bb64
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-17 10:42:34 +0000

    ch10 sort completed

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,16 @@
 def sort arr
-  # your code here
+  recursive_sort(arr,[])
+end
+
+def recursive_sort(unsorted,sorted)
+  return sorted if unsorted.empty?
+  to_test = unsorted.pop
+  smallest = true
+  unsorted.each {|x| smallest = false if to_test > x}
+  if smallest
+    sorted << to_test
+  else
+    unsorted.unshift(to_test)
+  end
+  recursive_sort(unsorted,sorted)
 end

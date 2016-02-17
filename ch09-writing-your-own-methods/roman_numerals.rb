506f43584bdedcd85da4cc0db3a2f56c55dd0004
chrisparker <cjp214@gmail.com>
2016-02-17 20:38:12 +0000

    roman numerals challenge 2

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,24 @@
 def roman_numeral num
-  # your code here
+  roman = ""
+  m = num.to_i
+      roman << "M" * (m / 1000)
+      d = m % 1000
+      roman << "D" * (d / 500)
+      c = d % 500
+      roman << "C" * (c / 100)
+      l = c % 100
+      roman << "L" * (l / 50)
+      x = l % 50
+      roman << "X" * (x / 10)
+      v = x % 10
+      roman << "V" * (v / 5)
+      i = v % 5
+      roman << "I" * i
+      roman.gsub!("DCCCC", "DM")
+      roman.gsub!("CCCC", "CD")
+      roman.gsub!("LXXXX", "XC")
+      roman.gsub!("XXXX", "XL")
+      roman.gsub!("VIIII", "IX")
+      roman.gsub!("IIII", "IV")
+      return roman
 end

8de710120e01cb3283f1ee964144e381a2e2ede2
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-17 21:20:21 +0000

    Updated exercise ch12/birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,26 @@
-# your code here
+birth_dates = Hash.new
+File.read(birthdays.txt).each_line do |line|
+  name, date, year = line.split(",")
+  birth_dates[name] = Time.gm(year, date)
+end
+
+puts "Whose birthday would you like to know?"
+person = gets.chomp
+birthday = birth_dates[person]
+
+if birthday == nil
+  puts "Name not found!"
+else
+  now = Time.new
+  age = now.year - birthday.year
+  if now.month > birthday.month || (now.month == birthday.month && now.day > birthday.day)
+    age += 1
+  end
+
+  if now.month == birthday.month && now.day == birthday.day
+    puts "#{person} turns #{age} today! :D"
+  else
+    date = birthday.strftime("%-d %B")
+    puts "#{person} will be #{age} on #{date}. :)"
+  end
+end

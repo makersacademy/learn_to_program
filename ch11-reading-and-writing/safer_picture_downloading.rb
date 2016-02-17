c0812c83ee3410396a027b64a2bb74aee8560392
eripheebs <erikapheby@gmail.com>
2016-02-17 17:55:34 +0000

    Chapter 11 Safer Picture Downloading Answered

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,49 @@
-# your code here
+
+
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox'
+
+# First we find all of the pictures to be moved.
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+
+batch_name = gets.chomp
+
+puts
+
+print "Downloading #{pic_names.length} files: "
+
+# This will be our counter. 
+
+pic_number = 1
+
+pic_names.each do |name|
+
+	print '.' # This is our "progress bar".
+
+	new_name = if pic_number < 10
+
+					"#{batch_name}0#{pic_number}.jpg"
+
+				else
+
+					"#{batch_name}#{pic_number}.jpg"
+
+				end
+	if File.exist? new_name == true
+		"Attempt to overwrite a file stopped- Please pick different name"
+		exit 
+	else
+		File.rename name, new_name
+	end
+
+	# Finally, we increment the counter.
+
+	pic_number = pic_number + 1
+
+end
+
+puts # This is so we aren't on progress bar line.
+
+puts 'Done, cutie!'

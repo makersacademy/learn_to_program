81fcccae83a028294201364b0c9894a5699b73a6
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-17 16:42:03 +0100

    correction to shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,11 +1,3 @@
 def shuffle array
-rec_meth(array, [])
-end
-
-def rec_meth(array,shuffled)
-  if array == []
-    return shuffled
-  end
-  shuffled << array.delete_at(array.index(array.0))
-  rec_meth(array,shuffled)
+array.shuffle
 end

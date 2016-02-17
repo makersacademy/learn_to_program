70126fd0f236d73c84f35f2d0ddfc6406c719c71
Lorenzo Turrino <neuronefunzionante@gmail.com>
2016-02-17 18:17:58 +0000

    solve chapter12 challenges except roman conversion

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,58 @@
-# your code here
+def parse_line line   #parse a line and return a hash value
+   
+  parse_array = line.split(',')
+  parse_array[1] = Time.new(*parse_array[1].strip.split(' '))
+  [parse_array].to_h
+  
+end
+
+
+def reader filename     #reads a file
+  parsed_values = Hash.new
+  text = File.open filename
+
+  text.each_line {|line| parsed_values.update(parse_line(line))}
+  parsed_values
+
+end
+
+def create_info_string(name, birthday_hash)
+  output_string = String.new
+  birth = [birthday_hash[name].year,birthday_hash[name].month,birthday_hash[name].day]
+  now = [Time.now.year,Time.now.month,Time.now.day]
+  
+  if now[1] > birth[1] || ( now[1] == birth[1] && now[2] >= birth[2] )
+    age = now[0] - birth[0]
+    next_birth = Time.new(now[0]+1, birth[1], birth[2])
+  else
+    age = now[0] - birth[0] - 1
+    next_birth = Time.new(now[0], birth[1], birth[2])
+  end
+  
+  output_string << "\t#{name} was born on #{birthday_hash[name].to_s.split[0]}\n"
+  output_string << "\tHe is #{age} years old\n"
+  output_string << "\tHis next birthday will be on #{next_birth.to_s.split[0]}"
+end
+
+
+def user_prompt       #user interface. no error checking
+
+    puts "Hello, please tell me which file to read"
+    birthdays = reader gets.chomp
+    puts "File read. #{birthdays.size} dates of birth loaded."
+    
+    while true
+      puts "Write 'namelist' to display a list of people, 'quit' to exit or the name of the person that interest you"
+      if ( user_choice = gets.chomp ) == 'namelist'
+        puts birthdays.keys
+        user_choice = gets.chomp
+      elsif user_choice == 'quit'
+        puts "Goodbey"
+        exit(0)
+      end
+      puts create_info_string(user_choice,birthdays)
+    end
+
+end
+
+user_prompt
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,29 @@
-# your code here
+def happy_birthday
+  #various year definition in seconds
+  sidereal_year = 31558149.504
+  julian_year = 31557600
+  draconic_year = 29947974.5562912
+  anomalistic_year = 31558432.5504
+  
+  birthday = Array.new
+  puts "Hi, please enter the year you've been born in. This is a very rough program so no error checking or escape char, sorry"
+  birthday << gets.chomp.to_i
+  puts "Now enter the month"
+  birthday << gets.chomp.to_i
+  puts "And finally, the day"
+  birthday << gets.chomp.to_i
+  
+  age_in_seconds = Time.now - Time.new(*birthday)
+  age_in_years = Time.now.month > birthday[1] || ( Time.now.month == birthday[1] && Time.now.day >= birthday[2] ) ? Time.now.year - birthday[0] : Time.now.year - birthday[0] - 1
+  
+  puts "You're #{age_in_seconds.to_i} seconds old, which means:
+  #{(age_in_seconds/sidereal_year).round(3)} sidereal years old
+  #{(age_in_seconds/julian_year).round(3)} julian years old
+  #{(age_in_seconds/draconic_year).round(3)} draconic years old
+  #{(age_in_seconds/anomalistic_year).round(3)} anomalistic years old"
+  
+  puts "Or, a boring #{age_in_years} 'normal' years old"
+  puts "\n\nright, almost forgot!"
+  puts "SPANK! "  * age_in_years
+  
+end

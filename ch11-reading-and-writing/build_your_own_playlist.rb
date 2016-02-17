ada3c674197d74d570c973accaaf4cd62a1c832f
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-17 13:54:55 +0100

    create playlist completed

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,27 @@
-# your code here
+
+
+puts "What do you want your playlist to be called?"
+filename= gets.chomp + ".m3u"
+
+
+puts "Which folder contains the songs for your playlist?"
+song_dir = '/Users/Office/Music' #gets.chomp
+
+puts "Where do you want to safe the playlist?"
+playlist_dir = '/Users/Office/Desktop' #gets.chomp
+
+Dir.chdir(song_dir)
+
+song_name = (Dir['**/*.{m4a,mp3}']).sort_by{rand}
+
+
+Dir.chdir(playlist_dir)
+
+File.open(filename,'w') do |f|
+  song_name.each do |a|
+  f.write(a + "\n")
+  end
+end
+
+
+

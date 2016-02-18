9999b683db4f00ff7866c6ca65593b97592c034d
Pete BURCH <peteburch223@gmail.com>
2016-02-18 15:30:48 +0100

    answered ch14 grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
 def grandfather_clock &block
+  hour = (Time.now.hour + 1) % 12
+  hour.times do
+    block.call
+  end
+
   # your code here
 end
+
+# chime = Proc.new do
+#   puts "DONG!"
+# end
+#
+# grandfather_clock do
+#   puts "DONG!"
+# end

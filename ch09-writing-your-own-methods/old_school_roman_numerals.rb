971d924dfd4181b13be77f2ed53c784881146049
Hanna <hannaeberhoefer@yahoo.com>
2016-02-18 14:51:29 +0000

    o-s-r-n

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,21 @@
 def old_roman_numeral num
-  # your code here
+	x = self
+	roman = “”
+	NUM_ROM.each do |n, l|
+		roman << l * (x / n)
+		x = x % n
+	end
+	return roman
 end
+
+
+NUM_ROM = {
+	1000 => “M”,
+	 500 => “D”, 
+	 100 => “C”,
+	  50 => “L”,
+	  10 => “X”,
+	   5 => “V”,
+	   1 => “I”
+		     }
+

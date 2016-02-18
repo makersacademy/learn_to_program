599af9c0d057581099252155fccb2750332c7289
olmesm <olmesm@gmail.com>
2016-02-18 12:52:29 +0000

    Completed 10:3

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,11 @@
 def dictionary_sort arr
-  # your code here
+  return arr if arr.length <= 1
+
+  middle = arr.pop
+  head = arr.select { |item| middle.downcase > item.downcase }
+  tail = arr.select { |item| middle.downcase <= item.downcase }
+
+  dictionary_sort(head) + [middle] + dictionary_sort(tail)
 end
+
+# p dictionary_sort ["a", "b", "A", "Ab", "B"]

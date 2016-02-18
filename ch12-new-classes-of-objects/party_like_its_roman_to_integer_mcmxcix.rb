b1f680065e1c4b7c155358782d51a9aca10670ba
Lorenzo Turrino <neuronefunzionante@gmail.com>
2016-02-18 15:47:44 +0000

    solve last chapter12 challenge

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,17 @@
 def roman_to_integer roman
-  # your code here
+
+  roman_numbers = { 'M' => 1000 , 'CM' => 900 , 'D' => 500 , 'CD' => 400 , 'C' => 100 , 'XC' => 90 , 'L' => 50 , 'XL' => 40 , 'X' => 10 , 'IX' => 9 , 'V' => 5, 'IV' => 4 , 'I' => 1 }
+  roman_to_parse = roman.upcase
+  converted_number = 0
+
+  while roman_to_parse.length > 0
+    roman_numbers.each_pair do |key, value|
+      if roman_to_parse.slice(0,key.length) == key 
+        converted_number += value
+        roman_to_parse.slice!(0,key.length)
+      end
+    end  
+  end
+  
+  converted_number
 end

581db1d6a48b209121b780370ba44ba1f566dd94
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-02-18 18:47:58 +0000

    did the birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,19 @@
-# your code here
+Dir.chdir '/Users/kevinmccarthy/Documents/'
+file = File.open("birthdays.txt", "r")
+contents = file.read
+bob = Hash.new(0)
+contents.split("\n").each { |i|
+                          a = i.split(",")
+                          bob[a[0]] = a[1] + a[2]}
+
+puts "Whose birthday would you like to know?"
+name = gets.chomp
+puts "#{name}'s birth date was #{bob[name]}."
+
+
+
+
+
+
+
+

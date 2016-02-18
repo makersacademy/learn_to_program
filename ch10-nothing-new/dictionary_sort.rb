d1fec447a1e3ca473a0a54b944f066230a82e09a
chrisparker <cjp214@gmail.com>
2016-02-18 17:46:34 +0000

    dictionary sort challenge

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,7 @@
 def dictionary_sort arr
-  # your code here
+    arr2 = arr.map { |word| (word.upcase != word.swapcase ? word + "zzz" : word) }
+    arr3 = arr2.map { |word| (word.upcase != word.swapcase ? word.swapcase : word) }
+    arr4 = arr3.sort.map { |word| (word.upcase != word.swapcase ? word.swapcase : word) }
+    arr5 = arr4.map { |word| (word.upcase != word.swapcase ? word.chop.chop.chop : word) }
+    return arr5
 end

efaf2e30f149423f1034072b013003af8349f897
Pete BURCH <peteburch223@gmail.com>
2016-02-18 15:44:10 +0100

    answered ch14 program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  result = block.call
+  puts "...\"#{desc}\" finished, returning: #{result}"
   # your code here
+end
+
+
+program_log 'foo' do
+     program_log 'bar' do
+      puts 'Jump around'
+      'cabbage'
+     end
+     puts "I'm the king of the castle"
+     'parsnips'
 end

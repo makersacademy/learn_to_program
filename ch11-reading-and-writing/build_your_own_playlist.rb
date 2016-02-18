a98395641d5c64f63e1151f7a4bf7cadcabd06f1
Gabrielle <gabivk@gmail.com>
2016-02-18 11:22:38 +0000

    CH11 playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,8 @@
-# your code here
+Dir.chdir '/Users/Gabrielle/Music/iTunes/iTunes Media/Test'
+
+playlist = Dir['**/*.mp3']
+
+playlist_name = 'playlist.m3u'
+    File.open playlist_name, 'w' do |p|
+      p.write playlist.shuffle
+    end

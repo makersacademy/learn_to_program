62c6a062344944b57a40d2eb0573718fac345ba9
Patrick <p_donati@hotmail.com>
2016-02-18 14:45:55 +0000

    answered roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,9 @@
-def roman_numeral num
-  # your code here
+def roman_numerals(num)
+  roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  ours = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  number = roman.zip(ours)
+  c = 0
+  number.map {|k, v| c, num = num.divmod v; k*c }.join""
 end
+
+puts roman_numerals(1093)

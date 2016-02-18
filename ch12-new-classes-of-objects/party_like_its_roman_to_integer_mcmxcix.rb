0a11927071900afab8db825d732e7f1f88887d6e
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-18 12:14:30 +0000

    ch12 party_like_its_roman_to_integer_mcmxcix completed

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,37 @@
 def roman_to_integer roman
-  # your code here
+  return "Sorry, that is not a roman numeral" if roman =~ (/[^MDCLXVI]/i)
+  roman = roman.upcase
+  result = 0
+  if roman =~ (/CM/)
+    roman.sub!(/CM/,"")
+    result = result + 900
+  end
+  if roman =~ (/CD/)
+    roman.sub!(/CD/,"")
+    result = result + 400
+  end
+  if roman =~ (/XC/)
+    roman.sub!(/XC/,"")
+    result = result + 90
+  end
+  if roman =~ (/XL/)
+    roman.sub!(/XL/,"")
+    result = result + 40
+  end
+  if roman =~ (/IX/)
+    roman.sub!(/IX/,"")
+    result = result + 9
+  end
+  if roman =~ (/IV/)
+    roman.sub!(/IV/,"")
+    result = result + 4
+  end
+  result = result + 1000*(roman.count "M")
+  result = result + 500*(roman.count "D")
+  result = result + 100*(roman.count "C")
+  result = result + 50*(roman.count "L")
+  result = result + 10*(roman.count "X")
+  result = result + 5*(roman.count "V")
+  result = result + (roman.count "I")
+  return result
 end

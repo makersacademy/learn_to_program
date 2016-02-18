7f0f3ce5aa1531ae60c97cc754ac5b3f932e3f36
MisaOgura <misa.ogura01@gmail.com>
2016-02-18 12:15:39 +0000

    party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,37 @@
+# This programme convers roman numerals (between 1 & 1999) to integers
 def roman_to_integer roman
-  # your code here
+
+  # setting the hashes for special and normal cases
+  specials = {"IV" => 4, "IX" => 9, "XL" => 40,
+              "XC" => 90, "CD" => 400, "CM" => 900}
+
+  normals = {"I" => 1,  "V" => 5, "X" => 10, "L" => 50,
+             "C" => 100, "D" => 500, "M" => 1000}
+
+  integer = 0
+
+  # 1. first converts the specials to number, add it to the integer
+  # => remove from string once converted
+  specials.each do |k, v|
+    if roman.upcase.include?(k)
+      integer += v
+      roman.sub!(/#{k}/i, "")
+    end
+  end
+
+  # 2. takes care of the normals
+  roman.upcase.each_char do |x|
+    if normals.keys.include?(x)
+      integer += normals[x]
+    end
+  end
+
+  # 3. returns the total
+  integer
 end
+
+# lettets to use: I = 1, V = 5, X = 10,
+# L = 50, C = 100, D = 500, M = 1000
+
+# new roman numeral rules
+# CM: 900, CD: 400, XC: 90, XL: 40, IX: 9, IV: 4

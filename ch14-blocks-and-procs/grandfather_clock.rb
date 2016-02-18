a489fa6080cd418cc4fe4aa74f9c740a2ae82a86
Hanif Fakira <fakira.work@gmail.com>
2016-02-18 11:07:45 +0000

    Chap 14 Grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,11 @@
 def grandfather_clock &block
   # your code here
+  hour = Time.new.hour
+  num_hours = hour == 0 ? 12 : hour % 12
+  (1..num_hours).each(&block)
+end
+
+
+grandfather_clock do
+  puts "DONG!"
 end

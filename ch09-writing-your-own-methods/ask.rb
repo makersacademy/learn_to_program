918ddc88f16157f5287ff2ae043aa0d302117294
hkp108 <harsheetpatel@hotmail.com>
2016-02-18 11:06:37 +0000

    Add Ask Problem

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,18 @@
 def ask question
-  # your code here
+  puts question
+  while true
+  reply = gets.chomp.downcase
+    if (reply == 'yes' || reply == 'no')
+      if reply == 'yes'
+        return true
+      elsif reply == 'no'
+        return false
+      end
+    else
+    puts 'Please answer "yes" or "no".'
+    end
+  end
+
 end
+
+

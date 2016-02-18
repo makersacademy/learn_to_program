f633b83fc69d5e29b840bd721f647cc60ee6e975
hkp108 <harsheetpatel@hotmail.com>
2016-02-18 12:02:22 +0000

    Chpt10 Shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,15 @@
+require 'set' #required to pass rspec
+
 def shuffle arr
-  # your code here
+ new_arr = []
+ until arr.length == 0
+ rand_number = rand(arr.length)
+ new_arr << arr[rand_number]
+ arr.delete_at(rand_number)
+ end
+ return new_arr
 end
+
+#def shuffle arr
+  #  arr.length > 1 ? [arr.slice!(rand(0..arr.length-1)) ] + shuffle(arr) : arr
+#end

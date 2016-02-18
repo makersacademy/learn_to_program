5b4a7e55ff670543c44c59d891dab8040990ae29
Pete BURCH <peteburch223@gmail.com>
2016-02-18 08:42:27 +0100

    answered ch12 happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+
+
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "What month were you born in?"
+month = gets.chomp
+
+puts "On what day were you born"
+day = gets.chomp
+
+birthday = Time.new(year, month, day)
+age = Time.now.year - birthday.year
+
+(1..age).each {|i| puts 'SPANK!'}

2da824820c17bad78c42729b4a2def324b3b7a4a
Kyle Buttner <kylebuttner@gmail.com>
2016-02-18 14:41:21 -0500

    initial roman numerals solution

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,20 @@
 def roman_numeral num
-  # your code here
+  ten_less = %w[I II III IV V VI VII VIII IX]
+  hundy_less = %w[X XX XXX XL L LX LXX LXXX XC]
+  thou_less = %w[C CC CCC CD D DC DCC DCCC CM]
+  three_thou = %w[M MM MMM]
+
+  final = ""
+
+  if num > 3000
+    puts "Sorry, this number is too large"
+  elsif num < 10
+    final = "#{ten_less[num-1]}"
+  elsif num < 100 && num > 9
+    final = "#{hundy_less[num / 10 - 1]}" + "#{ten_less[num % 10 - 1]}"
+  elsif num < 1000 && num > 99
+    final = "#{thou_less[num / 100 - 1]}" + "#{hundy_less[(num % 100) / 10 - 1]}" + "#{ten_less[num % 10 - 1]}"
+  elsif num > 999
+    final = "#{three_thou[num / 1000 -1]}" + "#{thou_less[num / 100 - 1]}" + "#{hundy_less[(num % 100) / 10 - 1]}" + "#{ten_less[num % 10 - 1]}"
+  end
 end

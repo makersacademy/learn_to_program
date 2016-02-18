f8cb35d7f806af08eead8d8732e846c26f658a9a
Pete BURCH <peteburch223@gmail.com>
2016-02-18 08:25:23 +0100

    answerered ch12 one billion seconds

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,12 @@
-# your code here
+
+
+def one_billion_seconds(birth_date)
+
+  birth_date + 1000000000
+end
+
+
+birth_time = Time.new(1971, 8, 7, 5, 27, 0, 0)
+
+puts one_billion_seconds(birth_time).inspect
+

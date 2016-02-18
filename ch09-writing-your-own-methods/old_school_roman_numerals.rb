08ec8ecb645d39c31175b680c36beead19e78b48
Anne <annemarie.k89@gmail.com>
2016-02-18 23:12:24 +0000

    added old school numerals method

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,35 @@
 def old_roman_numeral num
-  # your code here
+  roman_num = ""
+  if num >= 1000
+    roman_num << "M" * (num/1000)
+    num = num % 1000
+  end
+  if num >= 500
+    roman_num << "D"
+    num = num % 500
+  end
+  if num >= 100
+    roman_num << "C" * (num/100)
+    num = num % 100
+  end
+  if num >= 50
+    roman_num << "L"
+    num = num % 50
+  end
+  if num >= 10
+    roman_num << "X" * (num/10)
+    num = num % 10
+  end
+  if num >= 5
+    roman_num << "V"
+    num = num % 5
+  end
+  if num >= 1
+    roman_num << "I" * num
+    num = num % 1
+  end
+    roman_num
 end
+
+# old_roman_numeral(2776) # => MMDCCLXXVI
+# old_roman_numeral(1444) # => MCCCCXXXXIIIV

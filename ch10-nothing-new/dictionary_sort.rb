9ef9d4e5d442a1c58094e0b27e054cb11d2cad4b
Patrick <p_donati@hotmail.com>
2016-02-18 17:09:33 +0000

    did 2 more

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
 def dictionary_sort arr
-  # your code here
+
+return arr if arr.length <= 1
+
+middle = arr.pop
+
+less = arr.select{|x| x.downcase < middle.downcase}
+
+more = arr.select{|x| x.downcase >= middle.downcase}
+
+dictionary_sort(less) + [middle] + dictionary_sort(more)
+
 end
+
+words = ['can','feel','singing.','like','A','can']
+
+puts(dictionary_sort(words).join(' '))

b7791a1c12f5a9f661b2b67af3aef420fe6e31ef
chrisparker <cjp214@gmail.com>
2016-02-18 18:34:45 +0000

    better solution for dictionary sort challenge

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,7 +1,7 @@
 def dictionary_sort arr
     arr2 = arr.map { |word| (word.upcase != word.swapcase ? word + "zzz" : word) }
-    arr3 = arr2.map { |word| (word.upcase != word.swapcase ? word.swapcase : word) }
-    arr4 = arr3.sort.map { |word| (word.upcase != word.swapcase ? word.swapcase : word) }
-    arr5 = arr4.map { |word| (word.upcase != word.swapcase ? word.chop.chop.chop : word) }
+    arr3 = arr2.map { |word| (word.upcase != word.swapcase ? word.downcase : word) }
+    arr4 = arr3.sort.map { |word| (word[-1] == "z" && word[-2] == "z" && word[-3] == "z" ? word.capitalize : word) }
+    arr5 = arr4.map { |word| (word[-1] == "z" && word[-2] == "z" && word[-3] == "z" ? word.chop.chop.chop : word) }
     return arr5
 end

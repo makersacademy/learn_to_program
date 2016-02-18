e5063e58d5bc84caa5d004e6ce2a66c570acadfc
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-18 20:18:58 +0000

    ch14 program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+program_log 'outer block' do
+  program_log 'some little block' do
+    1**1 + 2**2
+  end
+  program_log 'yet another block' do
+    "I like Thai food!"
+  end
+  false
 end

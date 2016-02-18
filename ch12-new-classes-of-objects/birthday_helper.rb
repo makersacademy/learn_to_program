31deb7135eaee5da4acd18081f3b403ea3239ba9
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-18 20:43:15 +0100

    birthday helper done

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,30 @@
-# your code here
+
+require 'pry'
+Dir.chdir "/Users/Office/Dropbox (Personal)/Development/Makers"
+
+text = File.read "Pines_Birthday_list.txt"
+
+
+lines = []
+text.each_line do |a|
+lines << a.split("\n")
+end
+
+count = lines.length
+
+words= Array.new
+lines.each do |a|
+words << a.join.split(",")
+end
+words.flatten!
+ 
+
+
+hash1 = Hash.new
+count.times do
+hash1[words.shift] = (words.shift+words.shift)
+
+end
+
+puts hash1["Christopher Alexander"]
+

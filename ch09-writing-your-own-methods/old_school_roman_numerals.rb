04e7b5946a79a24307a51f3fb36637ad45f6ea3c
olmesm <olmesm@gmail.com>
2016-02-18 08:23:39 +0000

    Completed 9:2

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+  char_hash = {
+      1000 => "M", 500 => "D", 100 => "C", 50 => "L",
+      10 => "X", 5 => "V", 1 => "I" 
+    }
+  roman_number = []
+  
+  char_hash.each do |key, value|
+	  roman_number <<  value * ( num.to_i / key )
+	  num = ( num.to_i % key )
+  end
+  roman_number.join("")
 end

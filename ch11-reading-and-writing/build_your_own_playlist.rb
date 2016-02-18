d752fb2781e3242e342ae299854436422f6f2c0c
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-18 17:09:48 +0000

    Build own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,15 @@
-# your code here
+def shuffle(arr)
+ arr.shuffle
+end
+
+all_oggs = shuffle(Dir['**/*.ogg'])
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_oggs.each do |ogg|
+    f.write ogg+"\n"
+  end
+end
+
+puts 'Done!'
+
+#Pines version, no idea messing with files and didn't want to delete anything, need to come back to

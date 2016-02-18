fcee4bffa8bdd85e5365aa012461f863f1eb9958
eripheebs <erikapheby@gmail.com>
2016-02-18 15:09:13 +0000

    Chapter 12 Party Like It's roman_to_integer 'mcmxcix'! Answered

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,23 @@
 def roman_to_integer roman
-  # your code here
+
+	hash_of_letters = { 'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000}
+
+	# get last letter, see if it has a letter smaller than it in front
+	# if so, subtract that from the letter
+	
+	final_value = 0
+
+	len = roman.length
+	x = 1
+	until x == ( len + 1 )
+		if (roman[-(x+1)] != nil) && ( hash_of_letters[roman[-(x+1)].upcase] < hash_of_letters[roman[-x].upcase] )
+			final_value += ( hash_of_letters[roman[-x].upcase] - hash_of_letters[roman[-(x+1)].upcase] )
+			x += 2
+		else
+			final_value += hash_of_letters[roman[-x].upcase]
+			x += 1
+		end
+	end
+
+	final_value
 end

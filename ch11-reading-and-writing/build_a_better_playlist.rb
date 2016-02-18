098e287c7dc44f3f24c8274a5f409291225a4eb4
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-02-18 15:51:19 +0000

    completed party_like_its_roman

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,18 @@
-def music_shuffle filenames
-  # your code here
+Dir.chdir '/Users/kevinmccarthy/Desktop'
+
+names = Dir['/Users/**/*.mp3']
+
+names = names.shuffle
+
+filename = 'playlist2.m3u'
+
+File.open filename, 'w' do |f|
+	f.write names
+end
+
+def music_shuffle names
+	names.each { |i|
+		i.each {|i|
+			i.split("/")}}
+  #
 end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,53 @@
-def roman_to_integer roman
-  # your code here
+def roman_to_integer(numeral)
+  bob = ["m","d","c","l","x","v","i"]
+  
+  numeral = numeral.downcase
+  new_array = []
+  numeral.downcase.split("").each_with_index {|i,index|
+
+  if bob.index(i) == nil
+     puts "no good, give me real roman numeral"
+     exit
+  elsif index + 1 == numeral.length 
+    new_array << i
+  elsif bob.index(i) <= bob.index(numeral[index+1])
+     new_array << i
+     new_array << "!"
+     flag = true
+  else
+    new_array << i
+  end}
+count = 0
+
+
+numerals_hash = { 
+    "m"   =>  1000,
+    "cm"  =>  900,
+    "d"   =>  500,
+    "cd"  =>  400,
+    "c"   =>  100,
+    "xc"  =>  90,
+    "l"   =>  50,
+    "x"   =>  10,
+    "ix"  =>  9,
+    "v"   =>  5,
+    "iv"  =>  4,
+    "i"   =>  1}
+
+
+if new_array.include? "!"
+  final_array = new_array.join("").split("!")
+  final_array.each {|i, index|
+      count += numerals_hash[i] 
+    }
+else
+  count = numerals_hash[new_array.join("")]
 end
+
+count
+
+
+
+count
+ end
+ roman_to_integer("iv")

bf46bdbfc4aba000c5a28547e8104f8113f41094
Patrick <p_donati@hotmail.com>
2016-02-18 16:49:49 +0000

    fixed old school

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,13 +1,13 @@
-def old_roman_numerals(number)
+def old_roman_numeral(number)
   more = number.divmod(10)
   more2 = number.divmod(100)
   more3 = number.divmod(1000)
   if (1000..4999).include? number
-    "M"*more3.at(0) + roman_numerals(more3.at(1))
+    "M" * more3.at(0) + old_roman_numeral(more3.at(1))
   elsif (500..999).include? number
-    "D" + "C"*(more2.at(0)-5) + roman_numerals(more2.at(1))
+    "D" + "C"*(more2.at(0)-5) + old_roman_numeral(more2.at(1))
   elsif (100..499).include? number
-     "C"*more2.at(0) + roman_numerals(more2.at(1))
+     "C"*more2.at(0) + old_roman_numeral(more2.at(1))
   elsif (10..49).include? number
     if more.at(1) < 5
        "X"*more.at(0) + "I"*more.at(1)
@@ -32,9 +32,10 @@ def old_roman_numerals(number)
     elsif number > 5
       "V" + "I"*(number-5)
     end
+  elsif number == 0
+    ""
   end
 
 end
 
-
-puts roman_numerals(1009)
+puts old_roman_numeral(1000)

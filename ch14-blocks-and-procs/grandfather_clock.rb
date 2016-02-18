dd7cfe512bf223a85bc4e2aa5d704acf8502955c
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-18 20:11:03 +0000

    Updated exercise ch14/grandfather_clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,12 @@
 def grandfather_clock &block
-  # your code here
+  time = Time.new.hour
+  if time > 12
+    time += 12
+  end
+  if time == 0
+    time = 12
+  end
+  time.times do
+    block.call
+  end
 end

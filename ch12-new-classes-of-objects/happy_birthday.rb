e616c84315ab7855fc55f09b6258d5f6014bd953
Gabrielle <gabivk@gmail.com>
2016-02-18 12:07:44 +0000

    CH12 Spanks

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp
+puts "What month were you born in?"
+month = gets.chomp
+puts "What day where you born on?"
+day = gets.chomp
+
+year_in_s = 365*24*60*60
+birthday = Time.local(year, month, day)
+age = (Time.now - birthday).to_i/year_in_s
+
+age.times { print "SPANK! " }

ee798124f44744e3b7651d089e78ae6d745b1d83
Anne <annemarie.k89@gmail.com>
2016-02-19 11:32:03 +0000

    added dict sort method

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,20 @@
 def dictionary_sort arr
-  # your code here
+  recursive_dict_sort arr, []
+end
+
+def recursive_dict_sort(unsorted, sorted)
+  return sorted if unsorted.length <= 0
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |item|
+    if item.downcase < smallest.downcase
+      still_unsorted.push(smallest)
+      smallest = item
+    else
+      still_unsorted.push(item)
+    end
+  end
+  sorted.push(smallest)
+  recursive_dict_sort(still_unsorted, sorted)
 end

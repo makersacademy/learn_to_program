cf07cc148a4a3b6ca5314f37b003f2f637a6ee1b
MisaOgura <misa.ogura01@gmail.com>
2016-02-19 13:52:57 +0000

    Slove ch14 program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
-def log desc, &block
-  # your code here
+# This programme notifies you when it
+# starts/ends the block that was passed in
+
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  value = block.call
+  puts "...\"#{desc}\" finished, returning: #{value}"
+end
+
+program_log 'outer block' do
+  program_log 'some little block' do
+    5
+  end
+  program_log 'yet another block' do
+    'I love Thai food!'
+  end
+  false
 end

0b5d22754dd7870071b3b8a342bf21016480aa07
adilw3nomad <adiln1993@gmail.com>
2016-02-19 12:17:01 +0000

    Orange_tree.rb and extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,110 @@
 class Integer
-  # your code here
+    def factorial
+        return 1 if self == 1
+        return self * (self-1).factorial
+    end
+    
+    def english_number
+    number = self
+    if number < 0 
+        return 'Please enter a number that isn\'t negative.' 
+    end 
+    if number == 0 
+        return 'zero' 
+    end
+    
+    num_string = '' 
+    ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'] 
+    tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'] 
+    teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'] 
+    powers_of_ten = {
+        (10**63) => "vigintillion",
+        (10**60) => "novemdecillion",
+        (10**57) => "octodeceillion",
+        (10**54) => "septendecillion",
+        (10**51) => "sexdecillion",
+        (10**48) => "quindecillion",
+        (10**45) => "quattuordecillion",
+        (10**42) => "tredecillion",
+        (10**39) => "duodecillion",
+        (10**36) => "undecillion",
+        (10**33) => "decillion",
+        (10**30) => "nonillion",
+        (10**27) => "octillion",
+        (10**24) => "septillion",
+        (10**21) => "sextillion",
+        (10**18) => "quintillion",
+        (10**15) => "quadrillion",
+        (10**12) => "trillion",
+        (10**9) => "billion",
+        1000000 => "million",
+        1000 => "thousand",
+    }
+    left = number 
+    powers_of_ten.each do |k,v|
+        write = left / k
+        left -= write * k
+        num_string << "#{english_number(write)} #{v}" if write > 0
+        if left > 0 && write > 0
+            num_string << " "
+        end
+    end
+    
+    write = left/100 
+    left -= write*100 
+    
+    if write > 0 
+        hundreds = english_number write
+        num_string = num_string + hundreds + ' hundred' 
+        if left > 0 
+            num_string = num_string + ' ' 
+        end
+    end
+    
+    write = left/10
+    left -= write*10 
+    
+    if write > 0 
+        if ((write == 1) and (left > 0)) 
+            num_string = num_string + teenagers[left-1] 
+            left = 0
+        else
+            num_string = num_string + tens_place[write-1] 
+        end
+        if left > 0 
+        num_string = num_string + '-' 
+        end 
+    end
+    write = left 
+    left = 0 
+    if write > 0 
+        num_string = num_string + ones_place[write-1] 
+    end 
+    num_string 
+    end
+    
+    def to_roman
+        num = self
+        to_return = ""
+    to_return << "M" * (num/1000)
+    to_return << "D" * (num% 1000/500)
+    to_return << "C" * (num% 500/100)
+    to_return << "L" * (num% 100/50)
+    to_return << "X" * (num% 50/10)
+    to_return << "V" * (num% 10/5)
+    to_return << "I" * (num% 5/1)
+    to_return
+    end
 end
+
+class Array
+    def my_shuffle
+        array = self
+        array.size.times do |i|
+            random = i + rand(array.size-i)
+            array[i], array[random] = array[random], array[i]
+        end
+        array
+    end
+end
+
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,54 @@
 class OrangeTree
-  # your code here
+  def initialize 
+      @height = 0
+      @oranges = 0
+      @life = true
+  end
+  def height
+      return @height.round(1) if @life
+      return 'A dead tree is not very tall. :(' if @life == false
+  end
+  
+  def count_the_oranges
+    if @life
+        @oranges
+    else
+        'A dead tree has no oranges. :('
+    end
+  end
+  
+  def one_year_passes
+    if @life 
+      @height += 0.4
+      @oranges = 0
+      if @height >= 10.4
+        @life = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > 2
+          @oranges = (@height * 15 - 25).to_i
+          "This year your tree grew to #{@height.round(1)}m tall, and produced #{@oranges.round} oranges."
+      else
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " but is still too young to bear fruit."
+      end
+    else
+      'A year later, the tree is still dead. :('
+    end
+
+  end
+  
+  def pick_an_orange
+      if @live
+          if @oranges > 0
+             @oranges -= 1
+             'You pick a juicy delicious orange!'
+         else
+             'You search for an orange, but the tree is bare'
+         end
+     else
+         'A dead tree has nothing to pick. :('
+     end
+  end
+    
+  
 end

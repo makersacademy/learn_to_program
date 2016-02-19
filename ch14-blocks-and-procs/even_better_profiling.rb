1d406885d939c9cd5c6e8904dbda7e2ed89b20a8
Paul <paulalexrees@gmail.com>
2016-02-19 13:46:53 +0000

    Toggling profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,22 @@
+# Better method - use a global variable such as $OPT_PROFILING_ON = true
+
 def profile block_description, &block
-  # your code here
+  profiling = true        # toggles whether to output the profiling
+  if profiling == true
+    start_time = Time.new
+    yield
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    yield
+  end
 end
+
+million = Proc.new do
+  number = 0
+  1000000.times do
+    number += 1
+  end
+end
+
+profile "Count to a million", &million

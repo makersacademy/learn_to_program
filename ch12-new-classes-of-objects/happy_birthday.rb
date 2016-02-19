38e9a4f640e04e4a8560d923863a47d531cefe6b
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-19 10:09:45 +0000

    One-billion-secs completed

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,8 @@
-# your code here
+puts "Please enter the year that you were born in, ie 1977"
+year = gets.chomp
+puts "Please enter the month that your were born, numerically, ie January will be 1"
+month = gets.chomp
+puts "Please enter the day that you were born during the month that you were born"
+day = gets.chomp
+
+age = year + month + day
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,8 @@
-# your code here
+# your code herefind out the exact second you were born, when you did turn 1bsecs, mark calender
+birth_time = Time.local(1977, 1, 26, 11, 11)
+
+billion_seconds_old = birth_time + 1000000000
+
+puts billion_seconds_old
+
+#Comes out correct when check against net

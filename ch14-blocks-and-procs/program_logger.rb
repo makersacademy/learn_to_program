47cf06702512870e20ea7427b5fcfdec526d548d
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-02-19 12:27:53 +0000

    program_logger complete

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  puts '..."' + desc + '" finished, returning: '+ block.call.to_s
+ 
   # your code here
 end
+
+#log "outer block" do 
+ # log 'some little block' do
+  #  1**1 + 2**2
+ # end
+#log 'yet another block' do
+ # '!doof iahT ekil I'.reverse
+#end
+#end
+

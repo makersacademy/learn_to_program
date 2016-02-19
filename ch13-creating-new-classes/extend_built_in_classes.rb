4876cc9be97eeb184487f44f38ced56b4babdd31
phoebe <phoebe.greig@gmail.com>
2016-02-19 10:36:59 +0000

    ch13 extend_built_in_classes complete

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,24 @@
 class Integer
-  # your code here
+	def factorial 
+		if self < 0
+			return 'You can\'t take the factorial of a negative number!'
+		end
+
+		if self <= 1
+			1
+		else
+			self * (self-1).factorial #factorial(num-1)
+		end
+	end
+
+	def to_roman
+  		str=""
+  		str << "M"*(self/1000)
+  		str << "D"*((self%1000)/500)
+  		str << "C"*((self%500)/100)
+  		str << "L"*((self%100)/50)
+  		str << "X"*((self%50)/10)
+  		str << "V"*((self%10)/5)
+  		str << "I"*(self%5)
+	end
 end

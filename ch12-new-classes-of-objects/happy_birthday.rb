f55dce69d21a5442f4af98f30b6451610d8336f1
hkp108 <harsheetpatel@hotmail.com>
2016-02-19 13:18:23 +0000

    Chpt12 Happy Birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,10 @@
-# your code here
+puts "Enter the year you were born"
+year = gets.chomp.to_i
+puts "Enter the month you were born (1-12)"
+month = gets.chomp.to_i
+puts "Enter the month you were born (1-31)"
+day = gets.chomp.to_i
+birthday = Time.local(year,month,day)
+age = (Time.now - birthday)/60/60/24/365
+
+age.to_i.times {puts 'SPANK!'}

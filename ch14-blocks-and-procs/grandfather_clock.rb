28ce5a52f3b411fd55962bae71e4a45c218d0b36
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-19 15:48:59 +0100

    grandfather_clock completed

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,12 @@
-def grandfather_clock &block
-  # your code here
+
+def grandfather_clock(&block)
+count= Time.new.hour
+if count > 12
+  count =(count)-12
 end
+count.times do |variable|
+  block.call
+end
+end
+
+grandfather_clock {puts "Dong"}

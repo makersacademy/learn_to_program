edbd1c1fb5bf3b6b53926978ff72517d45bb0035
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-19 10:29:15 +0000

    Happy Birthday Spank

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,8 +1,14 @@
 puts "Please enter the year that you were born in, ie 1977"
-year = gets.chomp
-puts "Please enter the month that your were born, numerically, ie January will be 1"
-month = gets.chomp
+year = gets.chomp.to_i
+puts "Please enter the month that your were born, numerically, i.e January will be 1"
+month = gets.chomp.to_i
 puts "Please enter the day that you were born during the month that you were born"
-day = gets.chomp
+day = gets.chomp.to_i
 
-age = year + month + day
+birth_time = Time.gm(year, month, day)
+age = Time.now - birth_time
+
+years = age / (60 * 60 * 24 * 365)
+
+print "SPANK!" * years
+

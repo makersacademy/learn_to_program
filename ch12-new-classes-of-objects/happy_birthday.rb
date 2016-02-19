4908671a0a82a75235591b7aeffc20d037b6f60c
Patrick <p_donati@hotmail.com>
2016-02-19 16:21:26 +0000

    happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+def happy_birthday
+  puts "what year were you born?"
+  answer = gets.chomp.to_i
+  puts "what month?"
+  answer2 = gets.chomp.to_i
+  puts "what day?"
+  answer3 = gets.chomp.to_i
+
+  birthy = Time.local(answer, answer2, answer3)
+  now = Time.new
+
+  age = 1
+
+  while Time.local(answer + age, answer2, answer3) <= now
+    puts "SPANK"
+    age = age + 1
+  end
+end
+
+happy_birthday

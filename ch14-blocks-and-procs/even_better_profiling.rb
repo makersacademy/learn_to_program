794cfdd7011bd1929b5000a708434e35147627ad
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-02-19 12:03:23 +0000

    even_better_profiling complete

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,26 @@
+
+
 def profile block_description, &block
-  # your code here
+  start_time = Time.new 
+  block.call
+  if $OPT_PROFILING_ON == true
+    duration = Time.new - start_time 
+    puts "#{block_description}: #{duration} seconds"
+  end
+end
+
+profile '25000 doublings' do
+  number = 1
+
+  25000.times do
+    number = number + number
+  end
+  puts "#{number.to_s.length} digits"
+end
+
+profile 'count to a million' do 
+  number = 0
+  1000000.times do
+    number = number + 1
+  end
 end

666daa2b869f29efa55a32d2dfce02765de067fe
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-02-19 10:05:45 +0000

    Corrected ch09 old_school_roman_numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -6,13 +6,13 @@ def roman_numeral num
   l = (num-m*1000-d*500-c*100)/50.to_i
   x = (num-m*1000-d*500-c*100-l*50)/10.to_i
   v = (num-m*1000-d*500-c*100-l*50-x*10)/5.to_i
-  i = num-m*1000-d*500-c*100-l*50-x*10
+  i = num-m*1000-d*500-c*100-l*50-x*10-v*5.to_i
 
   result = ""
 
   result << "M"*m
 
-  if c == 9
+  if d == 1 && c == 4
     result << "CM"
   elsif c == 4
     result << "CD"
@@ -20,7 +20,7 @@ def roman_numeral num
     result << "D"*d + "C"*c
   end
 
-  if x == 9
+  if l == 1 && x == 4
     result << "XC"
   elsif x == 4
     result << "XL"
@@ -28,14 +28,14 @@ def roman_numeral num
     result << "L"*l + "X"*x
   end
 
-  if i == 9
+  if v == 1 && i == 4
     result << "IX"
   elsif i == 4
     result << "IV"
   else
-    result << "V"*v + "I"*(i-v*5)
+    result << "V"*v + "I"*i
   end
 
   return result
-  
+
 end

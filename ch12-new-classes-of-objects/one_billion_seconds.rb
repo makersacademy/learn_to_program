2dcb8bd65bf8e52e349918cc412590277fe974eb
hkp108 <harsheetpatel@hotmail.com>
2016-02-19 13:15:42 +0000

    Chpt12 One Billion seconds

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,6 @@
-# your code here
+
+my_birthday = Time.local(1988,9,7,12,28,05)
+time_now = Time.new
+age_in_seconds = time_now - my_birthday
+current_age = age_in_seconds/60/60/24/365
+billion_second_birthday = my_birthday + 10**9

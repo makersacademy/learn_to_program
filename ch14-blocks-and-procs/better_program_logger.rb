1d9a159c130bae65c26a11a5af52802d8f8b93c1
Lorenzo Turrino <neuronefunzionante@gmail.com>
2016-02-19 17:08:55 +0000

    solve chapter14 challenges

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,9 @@
-def log desc, &block
-  # your code here
+$logging_depth = 0
+
+def better_log block_description, &block
+  puts ' ' * $logging_depth + "Beginning \"#{block_description}\"..."
+  $logging_depth += 1
+  result = block.call
+  $logging_depth -= 1
+  puts ' ' * $logging_depth + "...\"#{block_description}\" finished, returning: #{result.to_s}"
 end
--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,9 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour 
+  hour -= 12 if hour > 12
+  hour.times do 
+      block.call
+  end
 end
+
+grandfather_clock { puts "Hello mr Dong!" }

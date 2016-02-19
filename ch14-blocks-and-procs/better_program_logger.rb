e271aafb989b15230e1d4519c3894b3d2949f7ec
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-19 14:35:42 +0100

    even_better_profiling done

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,15 @@
 def log desc, &block
-  # your code here
+start_time = Time.new
+block.call
+duration = Time.new - start_time
+
+puts "#{desc}: #{duration} seconds."
+end
+number = ""
+count = 1
+log "test" do
+  1000000.times do
+  number = count.to_s + ":"
+  count += 1
+  end
 end

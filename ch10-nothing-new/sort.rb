c018302d85ec4bfa7dda221ba4c8dbf783d3725d
Anne <annemarie.k89@gmail.com>
2016-02-19 11:21:20 +0000

    sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,20 @@
-def sort arr
-  # your code here
+def sort(arr)
+  recursive_sort(arr, [])
+end
+
+def recursive_sort(unsorted, sorted)
+  return sorted if unsorted.length <= 0
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |item|
+    if item < smallest
+      still_unsorted.push smallest
+      smallest = item
+    else
+      still_unsorted.push(item)
+    end
+  end
+  sorted.push(smallest)
+  recursive_sort(still_unsorted, sorted)
 end

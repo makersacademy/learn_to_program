5faf6c7c0a2fc2843b4ac13be13bdf4d3b0ddaf9
yasgreen <yasmin.green@btinternet.com>
2016-02-20 17:32:28 +0000

    Found the question unclear so was not sure what to do. Went through solution and now understand.

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
-def log desc, &block
+def program_log(desc, &block)
   # your code here
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
+end
+
+program_log 'outer block' do
+  program_log 'some little block' do
+    1**1 + 2**2
+  end
+
+program_log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
 end

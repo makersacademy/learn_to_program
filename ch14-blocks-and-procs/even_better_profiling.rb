205f1e19baf834e8663524ecea4153b78522da85
hkp108 <harsheetpatel@hotmail.com>
2016-02-20 20:20:53 +0000

    Chpt14 Even better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
+$profile_on = true
 def profile block_description, &block
-  # your code here
+  if $profile_on
+start_time = Time.new
+block.call
+duration = Time.new - start_time
+puts "#{block_description}: #{duration} seconds"
+else
+block.call
+end
 end

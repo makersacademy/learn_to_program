2d4712206761afc9f1d3797fa56628325a299c06
phoebe <phoebe.greig@gmail.com>
2016-02-20 10:42:23 +0000

    ch14 even_better_profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,13 @@
-def profile block_description, &block
-  # your code here
+$profiling_on = true #need the $ sign at start as otherwise will not recognise variable in method- local vs global
+
+def profile block_description, &block 
+  	if $profiling_on
+  		start_time = Time.new
+		block.call
+		duration = Time.new - start_time
+		puts "#{block_description}: #{duration} seconds"
+	else
+		block.call
+	end
 end
+

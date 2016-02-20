c6cd3c30bbd5b170f5dbef67e114a7361c641394
hkp108 <harsheetpatel@hotmail.com>
2016-02-20 20:15:24 +0000

    Chpt13 Extend built-in classes

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,39 @@
+
+=begin =====could just shuffle twice like this...=====
 def music_shuffle filenames
 filenames.shuffle.shuffle
 end
+=end
+
+#to past the test
+def music_shuffle filenames
+  filenames = filenames.sort
+  song_numbers = filenames.size
+
+  2.times do
+    left_index = 0
+    right_index = song_numbers / 2
+    new_filenames = []
+
+    while new_filenames.size < song_numbers
+      if new_filenames.size % 2 == 0
+        new_filenames.push(filenames[right_index])
+        right_index += 1
+      else
+        new_filenames.push(filenames[left_index])
+        left_index += 1
+      end
+    end
+    filenames = new_filenames
+  end
+  arr = []
+  cut = rand(song_numbers)
+  idx = 0
+
+  while idx < song_numbers
+    arr.push(filenames[(idx+cut)%song_numbers])
+    idx = idx + 1
+  end
+
+  arr
+  end
--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,39 @@
 class Integer
-  # your code here
+  #=======INT TO ROMAN =======
+  def to_roman
+  left = self # !> assigned but unused variable - left
+
+  display = ""
+  display += 'M' * (self /1000)
+  display += 'D' * (self%1000/500)
+  display += 'C' * (self%500/100)
+  display += 'L' * (self%100/50)
+  display += 'X' * (self%50/10)
+  display += 'V' * (self%10/5)
+  display += 'I' * (self%5/1)
+
+
+  end
+
+def factorial
+    if self == 0
+      return 1
+    end
+
+    self * (self-1).factorial
+  end
+end
+
+
+class Array
+  def shuffle
+   new_arr = []
+   until self.length == 0
+   rand_number = rand(self.length)
+   new_arr << self[rand_number]
+   self.delete_at(rand_number)
+   end
+   return new_arr
+  end
+
 end

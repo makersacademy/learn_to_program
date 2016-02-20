fc1ef3f3a48ad31322356cf0ccad7a45bea0357f
Anne <annemarie.k89@gmail.com>
2016-02-20 16:41:02 +0100

    Added birthday programme

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,21 @@
-# your code here
+def birthday
+  require 'Time'
+  puts "What year were you born in?"
+  year = gets.chomp
+  puts "What month were you born?"
+  month = gets.chomp
+  puts "What day were you born?"
+  day = gets.chomp
+
+## figure out age
+
+  birthday = Time.local(year,month,day)
+  age = Time.new - birthday # age in secs
+
+  age = age / 60 / 60 / 24 / 365
+
+
+  puts "SPANK\n" * age.to_i
+end
+
+birthday

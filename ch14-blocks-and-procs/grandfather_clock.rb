c8445599461d18897f7610e9b2048578efb8d224
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2016-02-20 14:29:01 +0000

    Chris Pine, chpt 14, grandfather_clock.rb: complete

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
+# Write a method that takes a block and calls it once for
+# each hour that has passed today.
+# Hint: You can use Time.new.hour to get the current hour.
+# However, this returns a number between 0 and 23, so you
+# will have to alter those numbers in order to get ordinary
+# clock-face numbers (1 to 12).
+
+
 def grandfather_clock &block
-  # your code here
+  hours_passed = Time.new.hour%12
+  hours_passed.times do
+    block.call
+  end
 end
+
+grandfather_clock {puts 'DONG!'}

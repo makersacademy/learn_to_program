47162de59824e7864af3e53f1566b205b5461eb8
adilw3nomad <adiln1993@gmail.com>
2016-02-20 09:28:10 +0000

    fixed better_program_logger to pass tests

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,5 +1,9 @@
-def program_log desc, &block
-  puts "Beginning \"#{desc}\"..."
+$indent = 0
+def better_log desc, &block
+  indention = ' '*$indent
+  puts indention + "Beginning \"#{desc}\"..."
+  $indent += 1
   r = block.call
-  puts "...\"#{desc}\" finished, returning: #{r.to_s}"
+  $indent -= 1
+  puts indention + "...\"#{desc}\" finished, returning: #{r.to_s}"
 end

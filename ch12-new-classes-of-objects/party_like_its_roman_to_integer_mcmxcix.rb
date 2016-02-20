ddcd3cd4f82093467ab26fbb7da58b3097bf726f
Anne <annemarie.k89@gmail.com>
2016-02-20 22:47:54 +0100

    added roman to int method

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,35 @@
 def roman_to_integer roman
-  # your code here
+  roman = roman.upcase
+
+  hash = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 1000
+
+  }
+
+  num = 0
+  left = 0
+
+  roman.reverse.each_char do |hash_key|
+    is_roman = hash[hash_key]
+    return 'This is not a roman number' if is_roman == false
+
+    if left > is_roman
+      is_roman = is_roman * -1
+    else
+      left = is_roman
+    end
+
+    num = num + is_roman
+  end
+
+  num
 end
+
+puts(roman_to_integer('mcmxcix')) # => 1999
+puts(roman_to_integer('CCCLXV')) # => 365

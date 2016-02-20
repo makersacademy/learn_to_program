e18062a4a59561c288e5e62ce10be56ad38f703b
Jojo <joseph.sharratt@gmail.com>
2016-02-20 14:30:17 +0000

    Sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,14 @@
 def sort arr
-  # your code here
-end
+    b = 0
+    while b < arr.length
+      c = b+1
+      while c < arr.length
+        if arr[b] < arr[c]
+          arr[b] , arr[c] = arr[c] , arr[b]
+        end
+        c += 1
+      end
+      b += 1
+    end
+    arr.reverse
+  end

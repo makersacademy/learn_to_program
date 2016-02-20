f9d145d0ea91635b8fc3e90bb9e79fe2052e28cb
Jojo <joseph.sharratt@gmail.com>
2016-02-20 13:40:14 +0000

    Shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,16 @@
-def shuffle arr
-  # your code here
+require 'set'
+
+# def shuffle arr         # My own method that worked, but did not pass
+#   leng = arr.length     # Chris Pine's rspec test.
+#   shuffled = []
+#   until shuffled.leng == leng
+#     shuffled << arr(rand(arr.length))
+#   end
+# end
+
+def shuffle arr         # Suggested solution method
+  arr.shuffle
 end
+
+ # arr = ["a", "can", "can", "feel", "like", "singing"]
+ # p arr.shuffle

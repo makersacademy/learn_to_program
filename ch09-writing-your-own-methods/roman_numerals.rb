b161dcc9721cc32a79cdb501a9f80d9399f635e3
Anne <annemarie.k89@gmail.com>
2016-02-20 21:14:11 +0100

    added roman numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,36 +1,49 @@
+# special numbers
+# CM = 900
+# CD = 400
+# XC = 90
+# XL = 40
+
+
 def roman_numeral num
   roman_num = ""
-  if num >= 1000
-    roman_num << "M" * (num/1000)
-    num = num % 1000
+  c = (num % 1000 / 100)
+  x = (num % 100 / 10)
+  i = (num % 10)
+
+  roman_num << "M" if num >= 1000
+
+  if c == 9
+    roman_num << "CM"
+  elsif c == 4
+    roman_num << "CD"
+  else
+    roman_num << "D" * (num % 1000 / 500)
+    roman_num << "C" * (num % 500 / 100)
   end
-  if num >= 500
-    roman_num << "D"
-    num = num % 500
+
+  if x == 9
+    roman_num << "XC"
+  elsif x == 4
+    roman_num << "XL"
+  else
+    roman_num << "L" * (num % 100 / 50)
+    roman_num << "X" * (num % 50 / 10)
   end
-  if num >= 100
-    roman_num << "C" * (num/100)
-    num = num % 100
-  end
-  if num >= 50
-    roman_num << "L"
-    num = num % 50
-  end
-  if num >= 10
-    roman_num << "X" * (num/10)
-    num = num % 10
-  end
-  return roman_num << "IX" if num == 9
-  if num.between?(6,9)
-    roman_num << "V"
-    roman_num << "I" * (num - 5)
-  end
-  if num.between?(1,6)
-    return roman_num << "V" if num == 5
-    return roman_num << "IV" if num == 4
-    if num >= 3
-      roman_num << "I" * num
-    end
+
+  if i == 9
+    roman_num << "IX"
+  elsif i == 4
+    roman_num << "IV"
+  else
+    roman_num << "V" * (num % 10 / 5)
+    roman_num << "I" * (num % 5)
   end
   roman_num
 end
+
+puts("1999 " + roman_numeral(1999)) # => MCMXCIX
+puts("1800 " + roman_numeral(1800)) # => MDCCC
+puts("500 " + roman_numeral(500)) # => D
+puts("600 " + roman_numeral(600)) # => DC
+puts("400 " + roman_numeral(400)) # => CD

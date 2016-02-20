f78d8bbfc9a8c87519371eb8dc3e841caeb54531
chrisparker <cjp214@gmail.com>
2016-02-20 00:07:49 +0000

    roman to integer challenge

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,21 @@
 def roman_to_integer roman
-  # your code here
+    roman.gsub!("IX", "9 ")
+    roman.gsub!("IV", "4 ")
+    roman.gsub!("XC", "90 ")
+    roman.gsub!("XL", "40 ")
+    roman.gsub!("CM", "900 ")
+    roman.gsub!("CD", "400 ")
+    roman.gsub!("M", "1000 ")
+    roman.gsub!("D", "500 ")
+    roman.gsub!("C", "100" )
+    roman.gsub!("L", "50 ")
+    roman.gsub!("X", "10 ")
+    roman.gsub!("V", "5 ")
+    roman.gsub!("I", "1 ")
+    numbers = roman.split(" ")
+    sum = []
+    numbers.each do |n|
+        sum << n.to_i
+    end
+    return sum.reduce(:+)
 end

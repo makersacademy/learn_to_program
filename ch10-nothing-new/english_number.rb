0197752260b6ea59305ea91995e5ba0d98373c68
Anne <annemarie.k89@gmail.com>
2016-02-20 18:51:20 +0100

    working version of english numbers

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -12,7 +12,7 @@ def english_number number
   teenagers  = ['eleven',    'twelve',   'thirteen',
                   'fourteen',  'fifteen',  'sixteen',
                   'seventeen', 'eighteen', 'nineteen']
-  big_nums = [['hundred', 2],
+  zillions = [['hundred', 2],
               ['thousand', 3],
               ['million', 6],
               ['billion', 9],
@@ -36,18 +36,27 @@ def english_number number
               ['vigintillion', 63],
               ['googol', 100]]
   left = number
-  write = left/100
-  left = left - write*100
 
-  if write > 0
-    hundreds = english_number write
-    num_string = num_string + hundreds + ' hundred'
-    if left > 0
-      num_string = num_string + ' '
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base
+
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+        num_string = num_string + ' '
+      end
     end
   end
-  write = left/10 # How many tens left?
-  left = left - write*10 # Subtract off those tens.
+
+  write = left/10
+  left = left - write*10
+
   if write > 0
     if ((write == 1) and (left > 0))
       num_string = num_string + teenagers[left-1]
@@ -55,15 +64,33 @@ def english_number number
     else
       num_string = num_string + tens_place[write-1]
     end
+
     if left > 0
       num_string = num_string + '-'
     end
   end
+
   write = left
+  left = 0
+
   if write > 0
     num_string = num_string + ones_place[write-1]
   end
   num_string
 end
 
-puts(english_number(100))
+puts english_number( 0)
+puts english_number( 9)
+puts english_number( 10)
+puts english_number( 11)
+puts english_number( 17)
+puts english_number( 32)
+puts english_number( 88)
+puts english_number( 99)
+puts english_number(100)
+puts english_number(101)
+puts english_number(234)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)
+puts english_number(109238745102938560129834709285360238475982374561034)

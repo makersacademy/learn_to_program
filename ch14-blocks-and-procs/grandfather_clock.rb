5be10cec9f7515a74df347da6d0972df466d162f
phoebe <phoebe.greig@gmail.com>
2016-02-20 10:58:49 +0000

    ch14: grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,14 @@
 def grandfather_clock &block
   # your code here
+  the_24hour = Time.new.hour 
+  the_12hour = the_24hour%12
+  if the_12hour ==0
+  	the_12hour =12
+  else
+  	the_12hour
+  end
+
+  the_12hour.times do 
+  	block.call
+  end
 end

e7411e3236724a8e57c6a725fc5917c1ffac9e26
adilw3nomad <adiln1993@gmail.com>
2016-02-20 09:17:25 +0000

    better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,5 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  r = block.call
+  puts "...\"#{desc}\" finished, returning: #{r.to_s}"
 end

bd3a7ac0dd31ee33aa7d6f8f1f0c215daa3c2bba
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-02-20 16:27:20 +0100

    all done

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,15 +1,44 @@
-def log desc, &block
-start_time = Time.new
-block.call
-duration = Time.new - start_time
 
-puts "#{desc}: #{duration} seconds."
+
+
+$nesting_depth = 0
+def better_log desc, &block
+  space = ' '*$nesting_depth
+  start_message = 'Beginning "' + desc + '"...'
+  end_message = '..."'+ desc + '" finished, returning: '
+  puts space+start_message
+  $nesting_depth += 1
+  result= block.call
+  $nesting_depth -= 1
+  puts space+end_message+result.to_s
 end
-number = ""
-count = 1
-log "test" do
-  1000000.times do
-  number = count.to_s + ":"
-  count += 1
+
+better_log "Testlog1" do    
+    better_log "testlog2" do
+     "Yes!"
+    end
+    "Finally testlog1"
   end
+
+
+
+=begin
+$nesting_depth = 0
+def better_log desc, &block
+  start_message = "Beginning #{desc}"
+  end_message = "#{desc} finished, returning:"
+  puts start_message.rjust($nesting_depth+(start_message.length))
+  $nesting_depth += 1
+  result= block.call
+  $nesting_depth -= 1
+  puts end_message.rjust($nesting_depth+(end_message.length))
+  puts result.rjust(result.to_s.length+$nesting_depth)
 end
+
+better_log "Testlog1" do    
+    better_log "testlog2" do
+     "Yes!"
+    end
+    "Finally testlog1"
+  end
+=end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
+
+def program_log desc, &block
+  puts 'Beginning "' +desc+ '"...'
+  result= block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+program_log "Testlog1" do    
+    program_log "testlog2" do
+     "Yes!"
+    end
+    "Finally testlog1"
+  end
+  
+=begin
 def log desc, &block
   puts "Beginning #{desc}"
   result= block.call
@@ -10,3 +25,5 @@ log "Testlog1" do
     end
     "Finally testlog1"
   end
+
+=end

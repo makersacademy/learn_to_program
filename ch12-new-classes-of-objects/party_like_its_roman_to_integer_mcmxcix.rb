01c5c06f6e7722f52ae4311263951403dc44ee9c
Gabrielle <gabivk@gmail.com>
2016-02-20 09:15:17 +0000

    CH12 Romans to numerals

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,34 @@
 def roman_to_integer roman
-  # your code here
+  number = 0
+  roman.upcase!
+
+  romans = {
+          "M"  => 1000,
+          "CM" => 900,
+          "D"  => 500,
+          "CD" => 400,
+          "C"  => 100,
+          "XC" => 90,
+          "L"  => 50,
+          "XL" => 40,
+          "X"  => 10,
+          "IX" => 9,
+          "V"  => 5,
+          "IV" => 4,
+          "I"  => 1
+  }
+
+  i = 0
+
+  while i < roman.length
+    if romans.key?(roman[i,2])
+      number += romans[roman[i,2]]
+      i += 2
+    else
+      number += romans[roman[i]]
+      i += 1
+    end
+  end
+
+  number
 end

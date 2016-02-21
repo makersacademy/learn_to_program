9318bc91aefc3514c109f947288c8864d6f8effe
Gabrielle <gabivk@gmail.com>
2016-02-21 16:15:46 +0000

    CH14 Better Profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,25 @@
 def profile block_description, &block
-  # your code here
+  start_time = Time.new
+  block.call
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+end
+
+private
+
+profile '25000 doublings' do
+  number = 1
+
+  25000.times do
+    number *= 2
+  end
+
+  puts "#{number.to_s.length} digits"
+end
+
+profile 'count to a million' do
+  number = 0
+  1000000.times do
+    number += 1
+  end
 end

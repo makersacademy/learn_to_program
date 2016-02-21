abb41220395a9345773b33169453dca0276f1c0f
Gabrielle <gabivk@gmail.com>
2016-02-21 15:06:55 +0000

    CH09 New Romans

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,27 @@
 def roman_numeral num
-  # your code here
+  letters = ""
+
+  romans = {
+          "M"  => 1000,
+          "CM" => 900,
+          "D"  => 500,
+          "CD" => 400,
+          "C"  => 100,
+          "XC" => 90,
+          "L"  => 50,
+          "XL" => 40,
+          "X"  => 10,
+          "IX" => 9,
+          "V"  => 5,
+          "IV" => 4,
+          "I"  => 1
+  }
+
+  romans.each do |key, value|
+    until num < value
+      num -= value
+      letters << key
+    end
+  end
+ return letters
 end

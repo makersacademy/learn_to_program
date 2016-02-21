fd85c9caef6a7e318232f584cf336bef49f0e34a
chrisparker <cjp214@gmail.com>
2016-02-21 23:04:10 +0000

    readded sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,9 @@
 def sort arr
-  # your code here
+  return arr if arr.length <= 1
+  
+  middle = arr.pop
+  first = arr.select { |word| word < middle }
+  second = arr.select { |word| word >= middle }
+
+  sort(first) + [middle] + sort(second)
 end

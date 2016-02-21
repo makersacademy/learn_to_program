ffc6358502a4bce98c3c773ff42f9cda5d2196bc
Andrew Cookson <acookson91@gmail.com>
2016-02-21 11:35:38 +0000

    Completed dictory to sort both cases

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,13 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arr)
+  return arr if arr.length <= 1
+
+middle = arr.pop
+
+less = arr.select{|x| x.downcase < middle.downcase}
+
+more = arr.select{|x| x.downcase >= middle.downcase}
+
+dictionary_sort(less) + [middle] + dictionary_sort(more)
+
+
 end

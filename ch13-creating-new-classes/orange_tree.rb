fea26b787a8f749b02cc32b4f2e880be11bab14f
Andrew Cookson <acookson91@gmail.com>
2016-02-21 15:51:37 +0000

    Created orange tree, not fully tested

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,37 @@
 class OrangeTree
-  # your code here
+  def initialize
+    @height = 0
+    @age = 0
+    @orange_count = 0
+  end
+
+  def height
+      @height
+  end
+
+  def count_the_oranges
+    @fruit
+  end
+
+  def pick_an_orange
+      if orange_count <= 0
+        return "No more oranges"
+      else
+        @orange_count -= 1
+        return "jheez these are good"
+      end
+  end
+
+  def one_year_passes
+      @orange_count = 0
+      if @age >= 10
+        puts "DEAD Yo"
+        exit
+      else
+        @age += 1
+        @height + (20/@age)
+        @orange_count +(1*@age) unless @age <= 3
+      end
+  end
+
 end

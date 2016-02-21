c6e0a0ef704d1b7da45216d6f48d2436682a8f75
Andrew Cookson <acookson91@gmail.com>
2016-02-21 11:39:56 +0000

    bult my own playlists

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,22 @@
-# your code here
+Dir.chdir '/Users/andrewcookson/Makers/Tests/renamedphotos'
+pic_name= Dir['/Users/andrewcookson/Makers/Tests/photos/**.{jpg,JPG}']
+
+
+batch_name = "mentalphotos"
+print "Downloading #{pic_name.length} files:"
+
+pic_number = 1
+
+  pic_name.each do |name|
+      print '.'
+      new_name = "#{batch_name}#{pic_number}.jpg"
+      if File.exist?("#{batch_name}#{pic_number}.jpg")
+        print "This picture is already here"
+        exit
+      else
+      File.rename name, new_name
+      puts "Copied #{batch_name}#{pic_number}.jpg"
+      pic_number += 1
+    end
+
+  end

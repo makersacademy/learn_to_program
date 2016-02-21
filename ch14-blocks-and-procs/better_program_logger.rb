af2668f2c0724be4f69cdc0d6d79fce2733ce0ed
chrisparker <cjp214@gmail.com>
2016-02-21 21:35:28 +0000

    better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
+$log_depth = 0
+
 def log desc, &block
-  # your code here
+    gap = " " * log_depth
+  puts "#{gap}Beginning #{desc}..."
+  log_depth += 1
+  result = block.call
+  log_deptch -= 1
+  puts "#{gap}...#{desc} finished, returning: #{result.to_s}"
+end
+
+log "outer block" do
+    log "some little block" do
+        10 / 2
+    end
+    
+    log "yet another block" do
+        "I like Thai food!"
+    end
+    false
 end

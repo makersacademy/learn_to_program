e9e592fc77b79c11135a20b3aa0372cff60110a4
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-21 16:49:12 +0000

    Grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,28 @@
 def grandfather_clock &block
-  # your code here
+
+
+hours_passed = Time.new.hour 
+
+if hours_passed > 12
+   hours_passed -= 12
 end
+	hours_passed.times(&block)
+end
+
+grandfather_clock {puts 'Dong'}
+
+
+# This programme will output "Dong" once for
+# each hour that has passed today
+
+# require './ch14-blocks-and-procs/grandfather_clock.rb'
+
+# describe 'grandfather clock' do
+#   it 'dongs or does whatever you put in the block' do 
+#     allow(Time).to receive_message_chain('new.hour') { 4 }
+#     expect(STDOUT).to receive(:puts).with('DONG!').exactly(4).times
+#     grandfather_clock do
+#       puts 'DONG!'
+#     end
+#   end
+# end

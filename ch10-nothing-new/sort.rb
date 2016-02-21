0b92a69599d47af021a6d15375b469e9683c53ea
Gabrielle <gabivk@gmail.com>
2016-02-21 15:18:59 +0000

    CH10 Sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,10 @@
 def sort arr
-  # your code here
+recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+    until unsorted == []
+      sorted << unsorted.slice!(unsorted.index(unsorted.min))
+    end
+  sorted
 end

271256dcc93fe402b9779b699e35d74dc1f48f5e
Andrew Cookson <acookson91@gmail.com>
2016-02-21 15:47:54 +0000

    Completed birthday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,13 @@
-# your code here
+year = 1991
+month = 06
+day = 24
+
+birthday = Time.local(year, month, day)
+time = Time.new
+
+age = 1
+
+while Time.local(year + age, month, day) <= time
+  puts "SPANK!"
+  age = age + 1
+end

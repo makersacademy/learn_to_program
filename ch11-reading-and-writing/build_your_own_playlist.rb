31ca408049ee98ff0d40df3b1c41bf311e848690
Jojo <joseph.sharratt@gmail.com>
2016-02-21 11:00:50 +0000

    Chapter 11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,13 @@
-# your code here
+Dir.chdir "/Users/jojo/Projects/tracks"
+
+playlist    = Dir["**/*.mp3"]
+
+playlist.shuffle
+
+File.open "playlist.m3u", "w" do |f|
+  playlist.each do |mp3|
+    f.write mp3+"\n"
+  end
+end
+
+puts "Finished"

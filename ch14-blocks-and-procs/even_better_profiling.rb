cfdf02a1eba656bb9776dd7f1d93e493a6ef894e
Simon T Glancy <si.glancy@gmail.com>
2016-02-21 21:40:25 +0000

    Finished Chris Pine

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,20 +1,14 @@
-
-
+$OPT_PROFILING_ON = false  
 
 def profile block_description, &block
 
-prof_on = false
 
-if prof_on
-
-start_time = Time.new
-block.call
-duration = Time.new - start_time
-
-puts "#{block_description}: #{duration} seconds"
-
-else
-block.call
-end
-
+  if $OPT_PROFILING_ON
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end

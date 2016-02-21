4836803139eb701f86ce865284c7ee94ea01f970
Andrew Cookson <acookson91@gmail.com>
2016-02-21 15:49:17 +0000

    Finished party like its roman

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,31 @@
 def roman_to_integer roman
-  # your code here
+
+roarray = {'i' => 1,
+          'v' => 5,
+          'x' => 10,
+          'l' => 50,
+          'c' => 100,
+          'd' => 500,
+          'm' => 1000
+          }
+
+total = 0
+prev = 0
+index = roman.length - 1
+  while index >= 0
+    c = roman[index].downcase
+    index = index - 1
+    val = roarray[c]
+      if roarray[c] == nil
+      puts "This isn't a roman yo"
+    end
+
+    if val < prev
+       val = val * -1
+     else
+       prev = val
+     end
+    total = total + val
+ end
+total
 end

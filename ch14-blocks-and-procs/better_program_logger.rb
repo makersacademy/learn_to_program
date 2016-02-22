3baed438391fd3cb4786d509be64f4d82e62c75f
Jojo <joseph.sharratt@gmail.com>
2016-02-22 20:10:37 +0000

    Chapter 14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
-def log desc, &block
-  # your code here
+$margin = 0
+
+def better_log desc, &block
+  indent = " "*$margin
+  puts indent+'Beginning "'+desc+'"...'
+  $margin+=1
+  result = block.call
+  $margin-=1
+  puts indent+'..."'+desc+'" finished, returning: ' +result.to_s
+end
+
+better_log 'outer block' do
+  better_log 'some little block' do
+    10 / 2
+  end
+
+  better_log 'yet another block' do
+    'I like Thai food!'
+  end
+  false
 end

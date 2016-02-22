2bc025eb6ec1381957793295e6b1d54e9174cea6
Sachin Karia <sachin.karia01@gmail.com>
2016-02-22 10:39:55 +0300

    Chapter 14 Solutions

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,6 +1,6 @@
 def roman_to_integer roman
 
-digit_vals = {'i' => 1,'v' => 5,'x' => 10,'l' => 50,'c' => 100,'d' => 500'm' => 1000}
+digit_vals = {'i' => 1,'v' => 5,'x' => 10,'l' => 50,'c' => 100,'d' => 500,'m' => 1000}
 total = 0
 prev = 0
 

fe69bbbfa70de8b2db6af3a6d0a5ab18170f3992
Jazzy <jasmina.gasper@gmail.com>
2016-02-22 17:14:48 +0000

    better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,26 @@
+$indent = 0
+
 def log desc, &block
-  # your code here
+	indent = " " * $indent
+  puts indent + "Beginning '#{desc}' ..."
+
+  $indent += 1
+  
+  result = block.call
+  
+  $indent -= 1
+  
+  puts indent + "... '#{desc}' finished, returning: '#{result}'"
+end
+
+log "outer block"  do
+	log "some little block" do
+	5
+end
+
+log "yet another block" do
+	"I like Thai food!"
+end
+
+false
 end

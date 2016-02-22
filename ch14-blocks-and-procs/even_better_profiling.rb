d2592098b854cdc08d20fd27458cb7b09602285a
Andrew Cookson <acookson91@gmail.com>
2016-02-22 08:45:00 +0000

    Updated program logger - runs block now even if false

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,14 @@
-def profile block_description, &block
-  # your code here
+def timing block_description, &block
+  timing_on = false
+
+if timing_on == true
+  start_time = Time.new
+  block.call
+  duration = start_time - Time.new
+  puts "#{blockdes} completed in #{duration}"
+
+else
+  block.call
+end
+
 end

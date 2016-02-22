5c62e62cc8b5311cdce114293a67fd0d695e30d9
phoebe <phoebe.greig@gmail.com>
2016-02-22 08:42:03 +0000

    ch14: better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,24 @@
-def log desc, &block
-  # your code here
+
+$sizing = 0
+
+def better_log desc, &block
+  indent = ' '*$sizing
+  puts indent + 'Beginning "' + desc + '"...'
+  $sizing += 1
+  result = block.call
+  $sizing -= 1
+  puts indent + '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+better_log 'outer block' do
+  better_log 'some little block' do
+    better_log 'teeny-tiny block' do
+      'lOtS oF lOVe'.downcase
+    end
+    7 * 3 * 2
+  end
+  better_log 'yet another block' do
+    '!doof naidnI evol I'.reverse
+  end
+  '0' == "0"
 end

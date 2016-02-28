e7090a8781560a23da939e6453b9f9b688e37ea7
Andrew Cookson <acookson91@gmail.com>
2016-02-28 13:21:09 +0000

    Removed random from orange tree to pass 100%

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,37 +1,63 @@
 class OrangeTree
   def initialize
     @height = 0
-    @age = 0
     @orange_count = 0
+    @alive = true
   end
 
   def height
-      @height
+    if @alive
+      @height.round(1)
+    else
+      'A dead tree is not very tall. :('
+    end
   end
 
   def count_the_oranges
-    @fruit
-  end
-
-  def pick_an_orange
-      if orange_count <= 0
-        return "No more oranges"
-      else
-        @orange_count -= 1
-        return "jheez these are good"
-      end
+    if @alive
+      @orange_count
+    else
+      'A dead tree has no oranges. :('
+    end
   end
 
   def one_year_passes
-      @orange_count = 0
-      if @age >= 10
-        puts "DEAD Yo"
-        exit
+    if @alive
+      @height = @height + 0.4
+      @orange_count = 0 # old oranges fall off
+      if @height > 10.1
+        # tree dies
+        @alive = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > 2
+        # new oranges grow
+        @orange_count = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " and produced #{@orange_count} oranges."
       else
-        @age += 1
-        @height + (20/@age)
-        @orange_count +(1*@age) unless @age <= 3
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " but is still too young to bear fruit."
       end
+    else
+      'A year later, the tree is still dead. :('
+    end
   end
 
+  def pick_an_orange
+    if @alive
+      if @orange_count > 0
+        @orange_count = @orange_count - 1
+        'You pick a juicy, delicious orange!'
+      else
+        'You search every branch, but find no oranges.'
+      end
+    else
+      'A dead tree has nothing to pick. :('
+    end
+  end
+end
+
+tree = OrangeTree.new
+30.times do
+p tree.one_year_passes
 end

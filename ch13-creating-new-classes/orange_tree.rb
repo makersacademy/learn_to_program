6831073048cfcb8133159c42f64fac0b12bb2f07
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2016-03-16 10:03:17 +0000

    fixed wording in instruction of orange_tree_fix

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,7 +1,7 @@
-# Please have your OrangeTree grow by 0.4 per year, 
-# have it produce oranges starting in its sixth year at a
-# rate of (height * 15 - 25) per year.
-# have the tree die after 25 years. 
+# have your OrangeTree grow by 0.4 per year.
+# have it produce oranges starting in its sixth year at a rate of (height * 15 - 25) per year.
+# have the tree die after 25 years.
+# have fun.
 
 
 class OrangeTree

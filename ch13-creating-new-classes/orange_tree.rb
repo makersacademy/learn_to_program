5ca0f0f07efc7b5e398ee3b910cfde7875926759
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-16 09:01:35 +0000

    updated ch13 - orange tree with better instructions

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,6 +1,8 @@
-# Please have your OrangeTree grow by 0.4 per year, have it produce oranges at a
+# Please have your OrangeTree grow by 0.4 per year, 
+# have it produce oranges starting in its sixth year at a
 # rate of (height * 15 - 25) per year.
 # have the tree die after 25 years. 
+
 
 class OrangeTree
   # your code here

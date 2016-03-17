2ed3da0219cd6d43b62fcebc1737347ab5ee5629
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2016-03-17 22:57:45 +0000

    added mention of spec file

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,8 +1,9 @@
+# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
 # have your OrangeTree grow by 0.4 per year.
 # have it produce no oranges in its first 5 years
 # starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
 # have the tree die after 25 years.
-# have fun.
+# check out the rspec (spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
 
 
 class OrangeTree

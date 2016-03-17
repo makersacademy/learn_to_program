c10f6edf61229d7ee4cf570f4f44a7efe2155554
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2016-03-17 16:50:04 +0000

    fixed wording of orange_tree_instructions

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,5 +1,6 @@
 # have your OrangeTree grow by 0.4 per year.
-# have it produce oranges starting in its sixth year at a rate of (height * 15 - 25) per year.
+# have it produce no oranges in its first 5 years
+# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
 # have the tree die after 25 years.
 # have fun.
 

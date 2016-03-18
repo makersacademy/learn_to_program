ccec61202c37a662a0d3e22cd0b246382f936f00
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2016-03-18 08:22:33 +0000

    Update orange_tree.rb

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -3,7 +3,7 @@
 # have it produce no oranges in its first 5 years
 # starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
 # have the tree die after 25 years.
-# check out the rspec (spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
+# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
 
 
 class OrangeTree

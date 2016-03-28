88e575db4e6951d6b1a0c0dbd7ae8cf687bf5a9b
omajul <omajul2@hotmail.com>
2016-03-28 15:34:11 +0000

    Finish chapter 9 exercices

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,23 @@
-def old_roman_numeral num
-  # your code here
+ROMANS = {
+    1000    => 'M',
+    500     => 'D',
+    100     => 'C',
+    50      => 'L',
+    10      => 'X',
+    5       => 'V',
+    1       => 'I'
+}
+def old_roman_numeral(number)
+    return "" if number <= 0 or number > 3000
+    result = ""
+    ROMANS.each do |k,v|
+        x = number / k
+        if x > 0
+            x.times do
+                result += ROMANS[k]
+                number -= k
+            end
+        end
+    end
+    result
 end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,29 @@
-def roman_numeral num
-  # your code here
+ROMANS = {
+    1000    => 'M',
+    900     => 'CM',
+    500     => 'D',
+    400     => 'CD',
+    100     => 'C',
+    90      => 'XC',
+    50      => 'L',
+    40      => 'XL',
+    10      => 'X',
+    9       => 'IX',
+    5       => 'V',
+    4       => 'IV',
+    1       => 'I'
+}
+def roman_numeral(number)
+    return "" if number <= 0 or number > 3999
+    result = ""
+    ROMANS.each do |k,v|
+        x = number / k
+        if x > 0
+            x.times do
+                result += ROMANS[k]
+                number -= k
+            end
+        end
+    end
+    result
 end

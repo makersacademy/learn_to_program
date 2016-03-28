b6b99557212da3f0ab837df75ee2b8c0179f319f
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-28 14:12:26 +0100

    refactor: 9.5.2 old_roman_numeral
    I have refactored this to make it simpler and help to solve the next
    challenge.

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,17 +1,13 @@
 def old_roman_numeral num
   # your code here
-  letters = {M:1000,D:500, C:100,L: 50,X: 10,V: 5,I: 1}
+  letters = { M:1000,D:500, C:100,L: 50,X: 10,V: 5,I: 1 }
   nb_digits_roman = []
   result = ""
 
-  letters.values.each do |value|
+  letters.values.each_with_index do |value,index|
     remainder = num / value
+    result += letters.keys[index].to_s*remainder
     num -= remainder*value
-    nb_digits_roman << remainder
-  end
-
-  letters.keys.each_with_index do |key,index|
-    result += key.to_s * nb_digits_roman[index]
   end
   result
 end

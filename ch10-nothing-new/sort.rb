44a6b331fedc2061708a14db98c4e7d66fa003a5
Amy Nicholson <missamynicholson@gmail.com>
2016-03-28 15:21:58 +0100

    Ch10 sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,17 @@
-def sort arr
-  # your code here
+def sort array
+  recursive_sort array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+   smallest_word = unsorted_array.first
+  while unsorted_array.length > 0 do
+    unsorted_array.each do |word|
+        smallest_word = word if word < smallest_word
+    end
+
+    unsorted_array.slice!(unsorted_array.index(smallest_word))
+    sorted_array << smallest_word
+    recursive_sort unsorted_array, sorted_array
+  end
+    return sorted_array
 end

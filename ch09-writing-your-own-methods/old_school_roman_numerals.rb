8031ad08188fd468d4f7e182864b31de212cd722
hnamitha1 <namitha.h@gmail.com>
2016-03-28 23:34:13 +0100

    adding old_school_roman_numerals.rb from chapter 9

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,40 @@
 def old_roman_numeral num
   # your code here
+  roman = ""
+until num == 0 do  
+  
+  if num >= 1000 
+  	rem = num/1000
+  	roman += "M" *rem
+  	num = num % 1000
+  elsif  num >= 100 && num < 1000
+    rem = num/100
+    if rem >= 5
+    	roman += "D"
+    	rem = rem - 5
+    end	
+    roman += "C" * rem
+    num = num % 100    
+  elsif num >= 10 && num < 100
+  	rem = num / 10
+  	if rem >= 5
+    	roman += "L"
+    	rem = rem - 5
+    end	
+  	roman +=  "X" * rem
+  	num = num % 10  	
+  elsif num < 10
+  	rem =  num
+  	if rem >= 5
+    	roman += "V"
+    	rem = rem - 5
+    end	
+  	roman += "I"* rem
+  	num = 0
+ end
+end  	 
+  
+  return roman			
+    		  	
 end
+

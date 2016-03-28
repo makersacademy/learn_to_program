d05999d9dbbe0b224f9b7cca9e218288f38779b4
Robin Heathcote <robin.heathcote@icloud.com>
2016-03-28 22:51:19 +0100

    Solved old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
 def old_roman_numeral num
-  # your code here
+
+  roman = '' # Creating an empty string pointed to by roman
+
+  roman = roman + 'M' * (num / 1000) #For an example of 1999 adds 'M' once to empty string roman because 1999 / 1000 = 1
+  roman = roman + 'D' * (num % 1000 / 500) #(For modulo) The remainder of 1999 / 1000 is 999. 999/500 = 1. Add 'D' once to roman after 'M'
+  roman = roman + 'C' * (num % 500 / 100)# The rest does the same as above for each roman numeral
+  roman = roman + 'L' * (num % 100 / 50)
+  roman = roman + 'X' * (num % 50 / 10)
+  roman = roman + 'V' * (num % 10 / 5)
+  roman = roman + 'I' * (num % 5/ 1)
+
+
+  roman # Returns the old roman numeral
+
+
 end

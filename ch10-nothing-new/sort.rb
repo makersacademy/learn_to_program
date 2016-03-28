fa7e3095452b66f9fcb93eefe6ac6f77fd3307a4
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-28 22:22:19 +0100

    add week3 ch10 basic sort recursive

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,28 @@
 def sort arr
   # your code here
+  still_unsorted = []
+  already_sorted = []
+  smallest = arr.first
+  sorted_idx = 0
+# take list of words, and find "smallest" word comparing with "<", make note of index
+  arr.each_index do |i|
+    if arr[i] < smallest
+      smallest = arr[i]
+      sorted_idx = i
+    end
+  end
+
+  already_sorted << smallest
+  arr.slice!(sorted_idx)
+  still_unsorted = arr
+  while still_unsorted.size > 0
+    already_sorted << sort(still_unsorted)
+  end
+  already_sorted.flatten
 end
+
+#def recursive_sort unsorted, sorted
+# sorted << sort unsorted
+#end
+#a = sort ["c","b","Z","a","b"]
+#p a

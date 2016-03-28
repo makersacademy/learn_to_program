42f67311e85542fc030bcafdb21c40c2577b49cc
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-28 13:07:57 +0100

    add week3 ltp 9.5.2 old_roman_numeral

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
 def old_roman_numeral num
   # your code here
+  letters = {M:1000,D:500, C:100,L: 50,X: 10,V: 5,I: 1}
+  nb_digits_roman = []
+  result = ""
+
+  letters.values.each do |value|
+    remainder = num / value
+    num -= remainder*value
+    nb_digits_roman << remainder
+  end
+
+  letters.keys.each_with_index do |key,index|
+    result += key.to_s * nb_digits_roman[index]
+  end
+  result
 end

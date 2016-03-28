e70a3ec0f7f83c2a437ad7090015a7363ac1cef0
omajul <omajul2@hotmail.com>
2016-03-28 17:54:03 +0000

    Add dictionary_sort.rb from ch10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,14 @@
-def dictionary_sort arr
-  # your code here
+def recursive_sort(ary, res = [])
+    return res if ary.empty?
+    1.upto(ary.length - 1) do |i|
+        if ary[i] < ary[0]
+            ary[0], ary[i] = ary[i], ary[0]
+        end
+    end
+    res << ary.shift
+    recursive_sort(ary, res)
+end
+
+def dictionary_sort(words)
+    recursive_sort(words)
 end

957e94f8d2b6d18eb4267187b34a369d0d327d73
Lexi Tran <hazukitran@gmail.com>
2016-03-28 16:38:47 +0700

    Fixed bugs on old_roman challenge

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,12 +1,6 @@
-require 'pry'
-
-puts "Please enter a number no more than 4 digits: "
-  
-num = gets.chomp.to_i
-
 def old_roman_numeral num 
 
-  ROMAN = { -1 => ["I", "V"],
+  roman = { -1 => ["I", "V"],
             -2 => ["X", "L"], 
             -3 => ["C", "D"], 
             -4 => ["M"]}
@@ -16,20 +10,15 @@ def old_roman_numeral num
 
   i = -1
   loop do
-    return 0 if num_array[i] == nil 
-    binding.pry
-    if num_array[i] > 5 
-      final_array << ROMAN[i][1] + ROMAN[i][0] * (num_array[i] - 5)
+    break if num_array[i] == nil 
+    if num_array[i] >= 5 
+      final_array << roman[i][1] + roman[i][0] * (num_array[i] - 5)
     else
-      final_array << ROMAN[i][0] * num_array[i].to_i
+      final_array << roman[i][0] * num_array[i].to_i
     end
     i -= 1
-    break if i == 4
+    break if i == -5
   end
   
-  puts final_array.reverse.inject(:+)
-
+  final_array.reverse.inject(:+)
 end
- 
- 
-

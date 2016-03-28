8ffa382bad5eed5170931b46ca67730963e5c52e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-28 16:22:04 +0100

    9:1 - exercise 1 complete

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,39 @@
 def ask question
-  # your code here
+  goodAnswer = false
+  while (not goodAnswer)
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' or reply == 'no')
+      goodAnswer = true
+      if reply == 'yes'
+        return true
+      end
+      if reply == 'no'
+        return false
+      end
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+
+  answer  # This is what we return (true or false).
 end
+
+puts 'Hello, and thank you for...'
+puts
+
+ask 'Do you like eating tacos?'      # We ignore this return value.
+ask 'Do you like eating burritos?'
+wetsBed = ask 'Do you wet the bed?'  # We save this return value.
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+ask 'Do you like eating tamales?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wetsBed

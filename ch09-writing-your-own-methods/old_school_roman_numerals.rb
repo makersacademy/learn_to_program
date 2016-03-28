78004043546c03e2cec605a10a4a699b8496544c
Max Provin <maxprovin@outlook.com>
2016-03-28 15:33:04 +0000

    ch09 old shool numerals done

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,30 @@
-def old_roman_numeral num
-  # your code here
+def old_string_numeral(num)
+ # I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+ 
+thousands = num / 1000
+
+five_huns = (num % 1000) / 500
+
+huns = ((num % 1000) % 500) / 100
+
+fifties = (((num % 1000) % 500) % 100) / 50
+
+tens = ((((num % 1000) % 500) % 100) % 50) / 10
+
+fives = (((((num % 1000) % 500) % 100) % 50) % 10) / 5
+
+ones = ((((((num % 1000) % 500) % 100) % 50) % 10) % 5) 
+
+m = ("M" * thousands)
+d = ("D" * five_huns)
+c = ("C" * huns)
+l = ("L" * fifties)
+x = ("X" * tens)
+v = ("V" * fives)
+i = ("I" * ones)
+
+
+string = "#{m+d+c+l+x+v+i}"
+string
+
 end

ed29afb17bcec4326bb93ff794ac7c5bce559410
iammatthewward@gmail.com <iammatthewward@gmail.com>
2016-03-28 23:56:14 +0100

    Completed up to end of shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,20 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle(arr, [])
+end
+
+def recursive_shuffle(unshuffled, shuffled)
+
+  if unshuffled.size <= 0
+    return shuffled
+  end
+
+  # generate number between 1 and length of unshuffled
+  index = rand(unshuffled.size)
+  selected = unshuffled[index]
+  # use this number to find the item at that index then delete from unshuffled
+  unshuffled.delete_at(index)
+  # add the item to shuffled
+  shuffled << selected
+  # recursively repeat
+  recursive_shuffle(unshuffled, shuffled)
 end

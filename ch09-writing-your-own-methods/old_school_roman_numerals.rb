fc8c4d773038203d348f26acdd7d1c9c3dd25564
ollieh-m <ollie.hm@cantab.net>
2016-03-28 17:16:27 +0100

    Do chapter 9 exercises

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,30 @@
 def old_roman_numeral num
-  # your code here
+  answer = ''
+  #thousands
+  to_write = num / 1000
+  num %= 1000
+  answer += 'M' * to_write
+  #500s
+  to_write = num / 500
+  num %= 500
+  answer += 'D' * to_write
+  #100s
+  to_write = num / 100
+  num %= 100
+  answer += 'C' * to_write
+  #50s
+  to_write = num / 50
+  num %= 50
+  answer += 'L' * to_write
+  #10s
+  to_write = num / 10
+  num %= 10
+  answer += 'X' * to_write
+  #5s
+  to_write = num / 5
+  num %= 5
+  answer += 'V' * to_write
+  #1s
+  answer += 'I' * num
+  answer
 end

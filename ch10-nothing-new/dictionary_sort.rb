b2602a958ae751b1fe0a3352e3408e41bd7579bb
Lexi Tran <hazukitran@gmail.com>
2016-03-28 21:09:42 +0700

    dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
 def dictionary_sort arr
-  # your code here
+  new_array = []
+  while true
+    smallest = arr.min
+    new_array << smallest
+    arr.slice!(arr.index(smallest))
+    break if arr.empty?
+  end
+  new_array
 end

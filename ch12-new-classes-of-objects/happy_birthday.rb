2a4153fc9047a2df12a4045328d24d6709354cce
Amy Nicholson <missamynicholson@gmail.com>
2016-03-29 16:12:51 +0100

    Ch12 happy_birthday.rb & one_billion_seconds.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+def happy_birthday
+  puts "What year were you born?"
+  year = gets.chomp.to_i
+  puts "What month were you born? i.e. for April, write 4."
+  month = gets.chomp.to_i
+  puts "What day of the month were you born?"
+  day = gets.chomp.to_i
+
+
+  years_old = (Time.new - Time.gm(year, month, day))/(60*60*24*365)
+  puts "SPANK!" * years_old
+
+end

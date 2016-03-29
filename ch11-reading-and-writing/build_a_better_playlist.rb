832c6a7b9273fc291e700a972486b880081d8960
OMGDuke <lex1989@gmail.com>
2016-03-30 00:44:15 +0100

    Non working ch11:build_a_better_playlist WIP

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,33 @@
 def music_shuffle filenames
-  # your code here
+  total = filenames.length
+  left = 0
+  if total % 2 == 0
+    right = total/2
+  else
+    right = (total/2)
+  end
+  shuff_playlist = []
+    while shuff_playlist.length < total || left < total/2#alternate left to right
+      shuff_playlist << filenames[right]
+      shuff_playlist << filenames[left]
+      left += 1
+      right += 1
+    end
+
+
+  print shuff_playlist.sort
+  puts
+  #print rand(0..shuff_playlist.length-1)
+  puts left
+  puts right
+
+
+
 end
+
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+     'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+num = [1,2,3,4,5,6,7]
+
+music_shuffle(num)

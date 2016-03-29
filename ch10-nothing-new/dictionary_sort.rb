d7ab05b290e1d6bb348c18c7ea458a60519aa837
iammatthewward@gmail.com <iammatthewward@gmail.com>
2016-03-29 22:55:21 +0100

    All chapter 10 exercises complete

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort(arr, [])
+end
+
+def recursive_sort unsorted, sorted
+  if unsorted.size <= 0
+    return sorted
+  end
+
+  lowercase = unsorted.map(&:downcase)
+  index     = lowercase.index(lowercase.min)
+  smallest  = lowercase[index]
+
+  if unsorted[index] == smallest.capitalize
+      unsorted.delete_at(index)
+      sorted << smallest.capitalize
+    else
+      unsorted.delete_at(index)
+      sorted << smallest
+    end
+      recursive_sort(unsorted, sorted)
 end

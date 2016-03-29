c09043918942ebae02e87e3132593b352499eea2
Nick <nm.richards@yahoo.com>
2016-03-29 11:04:24 +0430

    Chapter 9 completed

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,25 @@
 def old_roman_numeral num
-  # your code here
+  left = num
+  thousands = left /1000
+  left = left - thousands*1000
+  five_hundreds = left / 500
+  left  = left - five_hundreds * 500
+  hundreds = left / 100
+  left = left - hundreds * 100
+  fifties = left / 50
+  left = left - fifties * 50
+  tens = left / 10
+  left = left - tens * 10
+  fives = left / 5
+  left = left - fives * 5
+  ones = left
+  array = []
+  thousands.times{|x| array << "M"}
+  five_hundreds.times{|x| array << "D"}
+  hundreds.times{|x| array << "C"}
+  fifties.times{|x| array << "L"}
+  tens.times{|x| array << "X"}
+  fives.times{|x| array << "V"}
+  ones.times{|x| array << "I"}
+  array.join
 end

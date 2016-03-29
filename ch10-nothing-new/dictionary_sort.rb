dddad14ad816a48a8cd5bf04c488dc2cb1ed3da3
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-29 13:17:12 +0100

    add week 3 ch10 dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,50 @@
+=begin
+Dictionary sort. Your sorting algorithm is pretty good, sure. But there was
+always that sort of embarrassing point you were hoping I’d just sort of
+gloss over, right? About the capital letters? Your sorting algorithm is good
+for general-purpose sorting, but when you sort strings, you are using the
+ordering of the characters in your fonts (called the ASCII codes) rather
+than true dictionary ordering. In a dictionary, case (upper or lower) is
+irrelevant to the ordering. So, make a new method to sort words (something
+like dictionary_sort ). Remember, though, that if I give your program words
+starting with capital letters, it should return words with those same cap-
+ital letters, just ordered as you’d find in a dictionary.
+
+=end
+
 def dictionary_sort arr
   # your code here
+  sort arr, true
 end
+
+def sort(arr, case_down = false)
+  # your code here
+  still_unsorted = []
+  already_sorted = []
+  smallest = arr.first
+  sorted_idx = 0
+# take list of words, and find "smallest" word comparing with "<", make note of index
+  arr.each_index do |i|
+    if case_down
+      condition = arr[i].downcase < smallest.downcase
+    else
+      condition = arr[i] < smallest
+    end
+    if condition
+      smallest = arr[i]
+      sorted_idx = i
+    end
+  end
+
+  already_sorted << smallest
+  arr.slice!(sorted_idx)
+  still_unsorted = arr
+  while still_unsorted.size > 0
+    already_sorted << sort(still_unsorted,true)
+  end
+  already_sorted.flatten
+end
+
+puts dictionary_sort ["ZZ","a","c","d"]
+
+

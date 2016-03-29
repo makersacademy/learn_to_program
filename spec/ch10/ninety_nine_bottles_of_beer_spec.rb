f7a71cb2d2bfca9ace335a927e8532e236374eb6
omajul <omajul2@hotmail.com>
2016-03-29 13:32:25 +0000

    Add ninety_nine_bottles_of_beer.rb ch10 and its spec file

--- spec/ch10/ninety_nine_bottles_of_beer_spec.rb
+++ spec/ch10/ninety_nine_bottles_of_beer_spec.rb
@@ -1,3 +1,7 @@
-describe 'ninety nine bottles of beer' do
-  
+require_relative '../../ch10-nothing-new/ninety_nine_bottles_of_beer'
+
+RSpec.describe 'bottlesOfBeerSong' do
+  it 'returns nil when passed negative numbers' do
+    expect(bottlesOfBeerSong(-1)).to eq nil
+  end
 end

0da24d146e9bfa6fb98d8a25036fc3f5de145044
omajul <omajul2@hotmail.com>
2016-03-29 13:50:59 +0000

    Add shuffle.rb ch10

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,11 @@
-def shuffle arr
-  # your code here
+def recursive_shuffle(ary, res = [])
+    return res if ary.empty?
+    index = rand(0...ary.length)
+    res << ary[index]
+    ary.delete_at index
+    recursive_shuffle(ary, res)
+end
+
+def shuffle(ary)
+    recursive_shuffle(ary)
 end

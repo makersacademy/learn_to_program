33a5dc0dd8971998d53122bb092b11389443552d
Jackrim1 <jriminton@gmail.com>
2016-03-29 17:18:21 +0100

    ch09 no.3 done

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,44 @@
 def roman_numeral num
   # your code here
+m = num / 1000
+num = num - m*1000
+
+m9 = num / 900
+num = num - m9*900
+
+d = num / 500
+num = num - d*500
+
+d4 = num / 400
+num = num - d4*400
+
+c = num /100
+num = num - c*100
+
+c90 = num/90
+num = num - c90*90
+
+l = num/50
+num = num - l*50
+
+l40 = num/40
+num = num - l40*40
+
+x = num/10
+num = num - x*10
+
+x9 = num/9
+num = num - x9*9
+
+v = num/5
+num = num - v*5
+
+v4 = num/4
+num = num - v4*4
+
+i = num
+
+puts 'M'*m + 'CM'*m9 + 'D'*d + 'CD'*d4 + 'C'*c + 'XC'*c90 + 'L'*l + 'XL'*l40 + 'X'*x + 'IX'*x9 + 'V'*v + 'IV'*v4 + 'I'*i
+
+
 end

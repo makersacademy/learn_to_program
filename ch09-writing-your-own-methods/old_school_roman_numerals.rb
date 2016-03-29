5c63dda0ce734b8d9b36c18491a0db80a416b6b8
Emilio <emsmnndz@gmail.com>
2016-03-29 16:28:44 +0100

    old_school_roman_numerals.rb Solved

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
+
 def old_roman_numeral num
-  # your code here
+	values = [["M", 1000], ["D", 500], ["C", 100], ["L", 50], ["X", 10], ["V", 5], ["I", 1]]
+  
+  roman = ""
+
+  values.map do |letter, value|
+   roman << letter*(num / value) 
+   num = num % value #setting number to the remainder, for example if num = 18 remainder array is 
+   					 #[18, 18, 18, 18, 8, 3, 0]
+   
+   end
+  return roman
 end
+
+#puts old_roman_numeral 5452

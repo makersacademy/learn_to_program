a79869bee3a81d764f65a07744287db85ba08381
Fergus Orbach <fo4786@gmail.com>
2016-03-29 18:36:42 +0100

    ch10 dict sort and english

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
 def dictionary_sort arr
-  # your code here
+  sorted_arr = []
+  rec_sort arr, sorted_arr
+end
+
+def rec_sort arr, sorted_arr
+
+  return if arr == []
+  down_arr = arr.map {|el| el.downcase}
+  min_index = down_arr.index(down_arr.min)
+  sorted_arr << arr[min_index]
+  arr.delete_at(min_index)
+  rec_sort arr, sorted_arr
+
+  sorted_arr
+
 end

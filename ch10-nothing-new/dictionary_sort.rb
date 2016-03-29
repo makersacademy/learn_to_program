a36ffb59017a4e93edd16d1369b3ce7f276eef6b
OMGDuke <lex1989@gmail.com>
2016-03-29 19:45:20 +0100

    Completed ch10:dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,11 @@
 def dictionary_sort arr
-  # your code here
+  recursive_dic_sort(arr,[])
+end
+
+def recursive_dic_sort(unsorted_array, sorted_array)
+  #iterate through the array, check each value (made lowercase) if it is equal to the current min (made lowercase), if it is move it to sorted array
+  unsorted_array.each_with_index  {|val, index| val.downcase == unsorted_array.min.downcase ? sorted_array.push(unsorted_array.delete_at(index)) : false}
+  #keep running recurive sort until the unsorted_array is empty
+  recursive_dic_sort(unsorted_array, sorted_array) until unsorted_array.length == 0
+  sorted_array
 end

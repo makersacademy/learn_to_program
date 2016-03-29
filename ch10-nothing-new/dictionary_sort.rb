55b8bc20ae350d53cd7a58fda787d1dde6bd8bd4
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-29 12:56:34 +0100

    10:3 shuffle complete

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
 def dictionary_sort arr
-  # your code here
+   x = arr.map.each{|x|
+
+
+    x.downcase}.sort
+p x
 end
+arr = ["i","AM","a","Massive","cunt"]
+
+p dictionary_sort arr
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle arr, []
 end
+
+def recursive_shuffle unshuff_arr, shuff_arr
+  #sort_arr <<
+  unshuff_arr.shuffle
+
+#puts sort_arr
+end
+#
+p harry = [1,2,3,4,5,5,4,3,2,1]
+p shuffle harry
+#p sort harry-new

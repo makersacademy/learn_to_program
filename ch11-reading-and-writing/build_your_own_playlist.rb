e381a7e71d04421c3681749eb8893b38044703b5
OMGDuke <lex1989@gmail.com>
2016-03-29 23:38:25 +0100

    Completed ch11:build_your_own_playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,8 @@
-# your code here
+Dir.chdir '../../../Music' #Change to my music folder
+music_names = Dir['**/*.{mp3,ogg,flac,m4a}'] #Search for all my music files within subdirectories.
+music_names = music_names.join("\n")
+file_name = "music_playlist.m3u"
+
+File.open file_name, 'w' do |f|
+  f.write music_names
+end

da80a07cdcaa994a06833f7781654a1b9982478a
omajul <omajul2@hotmail.com>
2016-03-30 12:09:34 +0000

    Add exercices ch12

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,4 @@
-# your code here
+one_billion_seconds = 1000**3
+omar_birthdate = Time.local(1985,12,25,18,5)
+puts "I was born #{Time.new - omar_birthdate} seconds ago"
+puts "I will turn one billion seconds old on #{omar_birthdate + one_billion_seconds}"

523b12ee6042c5ba5074c5a185deff94d45f7775
Darrenf <darrenf54321@gmail.com>
2016-03-30 08:15:27 +0100

    99 bottles solved

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -118,3 +118,21 @@ left = 0
 
 num_string
 end
+
+num_at_start = 5
+
+num_now = num_at_start
+
+while num_now > 2
+
+puts english_number(num_now).capitalize + ' bottles of beer on the wall, ' +
+
+english_number(num_now) + ' bottles of beer!'
+
+num_now = num_now - 1
+
+puts 'Take one down, pass it around, ' +
+
+english_number(num_now) + ' bottles of beer on the wall!'
+
+end

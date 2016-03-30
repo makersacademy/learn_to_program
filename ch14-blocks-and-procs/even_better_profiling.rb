3cf45703f96703696886cc567c6e77b352d5ef77
Amy Nicholson <missamynicholson@gmail.com>
2016-03-30 10:33:08 +0100

    Ch14 even_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
+$PROFILING = true
 def profile block_description, &block
-  # your code here
+  if $PROFILING
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end

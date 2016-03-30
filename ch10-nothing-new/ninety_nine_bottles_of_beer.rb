c68121dca8e88759af7562f6eb45f36031d137b5
letianw <tmac4ever0824@gmail.com>
2016-03-30 15:11:56 +1100

    chap 10 finished and passed

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,3 +1,19 @@
 require './english_number.rb'
 
-def bottles
+class Bottle_Song
+  def initialize(number)
+    @number = number < 0 ? 0 : number
+  end
+
+  attr_accessor :number
+
+  def print_song
+    while self.number > 0
+        puts "#{english_number(self.number).capitalize} #{self.number == 1? "bottle" : "bottles"} of beer on the wall,"
+        puts "#{english_number(self.number).capitalize} #{self.number == 1? "bottle" : "bottles"} of beer,"
+        puts "Take one down, pass it around,"
+        self.number -= 1
+        puts "#{english_number(self.number).capitalize} #{self.number == 1? "bottle" : "bottles"} of beer on the wall."
+    end
+  end
+end

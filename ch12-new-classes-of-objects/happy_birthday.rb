75f59df82cf31a277541196b3e23b5d5a7fea6db
naridas <gregsparta@gmail.com>
2016-03-30 17:27:00 +0100

    fixed my ch12.1

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,9 +1,24 @@
 # forgot to do the gets.chomp and thought they would just input directly
-puts 'Hey, when were you born? (Please use YYYYMMDD format.)'
-birthday = gets.chomp
-
-def age(birthday)
+require 'date'
+def age(dob)
   now = Time.now.utc.to_date
-  now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
 end
-age(birthday).times{puts "SPANK!"}
+puts 'Hey, when were you born? (Please use YYYY, MM, DD format.)'
+input = gets.chomp #<= this messes up my code hmm was the ""
+dob = Time.gm(input)
+x = age(dob)
+x.to_i.times{puts 'SPANK!'}
+
+
+
+
+
+=begin example using to help me
+def age(dob)
+  now = Time.now.utc.to_date
+  now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+end
+puts age(Time.gm("1992, 10, 03"))
+=end
+

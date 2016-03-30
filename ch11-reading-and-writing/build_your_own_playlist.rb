594ff066ebbac71f555257eb45c7169594734209
naridas <gregsparta@gmail.com>
2016-03-30 14:27:44 +0100

    ch11 complete and ch12-1

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,14 @@
-# your code here
+#using as reference to help me write my code
+# File.open filename, 'w' do |f|
+#f.write test_string
+#end
+def shuffle arr
+  arr.shuffle
+end
+
+mp3_files = shuffle(Dir['F:**/*.mp3']) #nonexisten
+File.open 'playlist.m3u', 'w' do |f|
+  mp3_files.each do |file|
+    f.write file+ "\n"
+  end
+end
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,2 @@
 # your code here
+puts(Time.gm(1992, 10, 03, 12, 00) + 10**9)

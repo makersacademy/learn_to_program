542bf743a93b904d879d199cc15abcfd80e7ae48
omajul <omajul2@hotmail.com>
2016-03-30 07:33:56 +0000

    Add ch11-reading-and-writing exercices

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,22 @@
+require_relative '../ch10-nothing-new/shuffle'
+
 def music_shuffle filenames
-  # your code here
+  shuffle(filenames)
 end
+
+# # Change directory to the folder containing my music
+# Dir.chdir '/home/ubuntu/workspace/projects/music'
+
+# # Find the path of each song using Dir and put them on an array
+# song_files = Dir['**/*.mp3']
+
+# # Add the full path for each song
+# song_files.map!{ |file| Dir.pwd + '/' + file }
+
+# # Ask the user the name for the playlist
+# print "Please type the name for your playlist: "
+# playlist_name = gets.chomp
+
+# File.open(playlist_name + '_shuffled.m3u', 'w') do |f|
+#     f.write(music_shuffle(song_files).join("\n"))
+# end

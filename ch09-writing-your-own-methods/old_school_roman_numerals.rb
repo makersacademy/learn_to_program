ff46380d359dd93c49e36e046e7eb21a78ca918f
Tania Rodin <trodin09@gmail.com>
2016-03-30 18:59:22 +0000

    Solved ch09 old school roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,15 +1,14 @@
-def old_roman_numeral num
-  raise 'Must use positive integer' if num <= 0
-
+def old_roman_numeral (num) 
   roman = ''
-
-  roman << 'M' * (num / 1000) 
-  roman << 'D' * (num % 1000 / 500) 
-  roman << 'C' * (num % 500 / 100) 
-  roman << 'L' * (num % 100 / 50) 
-  roman << 'X' * (num % 50 / 10) 
-  roman << 'V' * (num % 10 / 5) 
-  roman << 'I' * (num % 5 / 1)
-
+  
+  
+  roman = roman + 'M' * (num / 1000)
+  roman = roman + 'D' * (num % 1000 / 500) 
+  roman = roman + 'C' * (num % 500 / 100) 
+  roman = roman + 'L' * (num % 100 / 50) 
+  roman = roman + 'X' * (num % 50 / 10) 
+  roman = roman + 'V' * (num % 10 / 5) 
+  roman = roman + 'I' * (num % 5 / 1)
   roman
-end
+  
+end 

8e099a43b6c5171089d6860b28c6288e967a8269
Hanna <hannaeberhoefer@yahoo.com>
2016-03-30 19:10:16 +0100

    picture_downl complete

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,29 @@
-# your code here
+Dir.chdir "C:/Documents and Settings/Katy/PictureInbox"
+
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.'
+
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  if File.exists?(new_name)
+    puts "Error. File already exists!"
+    exit
+  else
+   File.rename(name, new_name)
+  end
+
+  pic_number = pic_number + 1
+end

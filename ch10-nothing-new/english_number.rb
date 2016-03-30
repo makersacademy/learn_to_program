373ff73595410f966f2b4063e39e367cecbc09e0
naridas <gregsparta@gmail.com>
2016-03-30 15:40:45 +0100

    changes to ch10engnum and finished ch12hpybdy

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -3,6 +3,62 @@ def english_number number
   require 'humanize'
   number.humanize
 end
+also tried hashes
+def in_words(int)
+  numbers_to_name = {
+      ...
+      1000000 => "million",
+      1000 => "thousand",
+      100 => "hundred",
+      90 => "ninety",
+      80 => "eighty",
+      70 => "seventy",
+      60 => "sixty",
+      50 => "fifty",
+      40 => "forty",
+      30 => "thirty",
+      20 => "twenty",
+      19=>"nineteen",
+      18=>"eighteen",
+      17=>"seventeen", 
+      16=>"sixteen",
+      15=>"fifteen",
+      14=>"fourteen",
+      13=>"thirteen",              
+      12=>"twelve",
+      11 => "eleven",
+      10 => "ten",
+      9 => "nine",
+      8 => "eight",
+      7 => "seven",
+      6 => "six",
+      5 => "five",
+      4 => "four",
+      3 => "three",
+      2 => "two",
+      1 => "one",
+    }
+  str = ""
+  if int < 0 
+    return "Please enter a number that isn\'t negative."
+  end
+  if int == 0
+    return "zero"
+  end
+  numbers_to_name.each do |num, name|
+    if int == 0
+      return str
+    elsif int.to_s.length == 1 && int/num > 0
+      return str + "#{name}"      
+    elsif int < 100 && int/num > 0
+      return str + "#{name}" if int%num == 0
+      return str + "#{name} " + in_words(int%num)
+    elsif int/num > 0
+      return str + in_words(int/num) + " #{name} " + in_words(int%num)
+    end
+  end
+end
+
 =end
 
 def english_number number
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,9 @@
-# your code here
+# forgot to do the gets.chomp and thought they would just input directly
+puts 'Hey, when were you born? (Please use YYYYMMDD format.)'
+birthday = gets.chomp
+
+def age(birthday)
+  now = Time.now.utc.to_date
+  now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+end
+age(birthday).times{puts "SPANK!"}

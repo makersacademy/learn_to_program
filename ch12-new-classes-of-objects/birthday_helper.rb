0a5266aaf0c72e158f96cb62afdb5d0a469056d3
naridas <gregsparta@gmail.com>
2016-03-31 00:00:21 +0100

    ch12 complete

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,19 @@
-# your code here
+# your code here used 
+bdays = {} #to insert the text from the text file
+File.read('bdays.txt').each_line do |line|
+   line.chomp #need to get rid of \n
+   comma1 = line.index(",") #gets the first comma
+   name = line[0..(comma1 - 1)]#messed up this part by not adding -1
+   date = line[-12..-1]
+   bdays[name] = date
+end
+
+puts 'Whose birthday would you like to know?'
+name = gets.chomp
+date = bdays[name]
+
+if date == nil
+  puts "Name not found, please put another name instead."
+else
+  puts date[0..5]
+end

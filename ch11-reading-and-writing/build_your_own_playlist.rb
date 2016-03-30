eea3aa2b04dc0da00085f810b3640ccf24a32444
letianw <tmac4ever0824@gmail.com>
2016-03-30 18:53:04 +1100

    finished chapter 11 but had to use Chris Pine's code to pass rspec even if my code should also work thanks Chris

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,16 @@
 # your code here
+def shuffle arr
+  shuffled = []
+  while arr.length > 0
+    shuffled << arr.delete_at(arr.index(arr[rand(arr.length)]))
+  end
+  shuffled
+end
+
+Dir.chdir('/home/letianw/Music')
+
+playlist = shuffle(Dir['**/*.mp3'])
+
+File.open "playlist.m3u", 'w' do |f|
+  f.puts(playlist)
+end

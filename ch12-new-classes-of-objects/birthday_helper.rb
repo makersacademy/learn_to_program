0ae80a2a220573f5765003d9f5efb6933d2e6197
Hanna <hannaeberhoefer@yahoo.com>
2016-03-30 19:12:06 +0100

    bday_helper complete

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,20 @@
-# your code here
+
+
+birth_dates = {}
+
+File.read("bdays.txt").each_line do | line |
+    key, value = line.chomp.split(",")
+    birth_dates[key] = value
+  end
+
+puts "Whose birthday would you like to know?"
+name = gets.chomp
+date = birth_dates[name]
+time = Time.new
+bd = Time.local(date[6..9], date[3..4], date[0..1])
+age = ((time - bd) / 31557600).floor
+
+puts "#{name}'s birthday is on the #{date[0..4]}. He will be #{age} years old."
+
+
+

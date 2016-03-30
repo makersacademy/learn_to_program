4a5ad887ba46d6941d231e718f06b2bd19bed062
pixelandpage <mags@pixelandpage.co.uk>
2016-03-30 14:48:35 +0100

    ch10 dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,16 @@
 def dictionary_sort arr
-  # your code here
+  	unsorted_list = arr
+	sorted_list = []
+
+	while true
+		smallest = unsorted_list[0]
+		unsorted_list.each {|x| 
+			if x.downcase < smallest.downcase then smallest = x end}
+  		sorted_list.push(smallest)
+  		unsorted_list.delete_at(arr.find_index(smallest))
+  		break if unsorted_list == []
+  	end
+
+	return sorted_list
+
 end

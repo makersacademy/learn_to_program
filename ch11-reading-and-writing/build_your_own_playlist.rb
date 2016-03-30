b4c502a77ac00816b24e89abcc9dd83104a7a23a
Darrenf <darrenf54321@gmail.com>
2016-03-30 09:43:29 +0100

    Ch11 build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,19 @@
-# your code here
+#This needs revisiting........
+
+def shuffle input
+input.sort_by{rand}
+end
+
+all_mp4 = shuffle(Dir['**/*.mp4'])
+
+File.open 'test_playlist.mp4files', 'w' do |f|
+
+all_mp4.each do |mp4|
+
+f.write mp4+"\n"
+
+end
+
+end
+
+puts 'Done!'

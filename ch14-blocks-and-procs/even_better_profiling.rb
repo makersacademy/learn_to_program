e0765c2fe0490a9ebcce45293d64649c0eca628c
ollieh-m <ollie.hm@cantab.net>
2016-03-30 21:26:39 +0100

    Do chapter 14 exercises

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,12 @@
-def profile block_description, &block
-  # your code here
+def profile block_description, &block 
+  	profiling = true
+  	if profiling
+  		before_block = Time.new
+  		block.call
+  		after_block = Time.new
+  		duration = after_block - before_block
+  		puts block_description + ': ' + duration.to_s + ' seconds'
+  	else
+  		block.call
+  	end
 end

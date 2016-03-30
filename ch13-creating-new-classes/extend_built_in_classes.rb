f2517237d7dcb104b48cd3a7d55decdd2f0c3be2
omajul <omajul2@hotmail.com>
2016-03-30 15:46:07 +0000

    Add exercices ch13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,13 @@
+require './ch09-writing-your-own-methods/old_school_roman_numerals'    
+
 class Integer
-  # your code here
+    # calculates factorial
+    def factorial
+        (1..self).reduce(1,:*)
+    end
+    
+    # converts to old school roman numerals
+    def to_roman
+        old_roman_numeral(self)
+    end
 end

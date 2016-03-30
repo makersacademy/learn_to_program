b289b3637103c930cd2bf86834b371825ba0198a
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-30 21:33:22 +0100

    add week3 ch13 old roman built-in class

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -13,4 +13,16 @@ class Integer
     return fact = 1 if self == 0
     self*(self-1).factorial
   end
+
+  def to_roman
+    letters = { M: 1000,D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 }
+    result =""
+    num = self
+    letters.values.each_with_index do |value,index|
+      digit = num / value
+      result += letters.keys[index].to_s*digit
+      num -= digit*value
+    end
+  result
+  end
 end

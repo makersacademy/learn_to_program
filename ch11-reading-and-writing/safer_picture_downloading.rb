9a911a0cf90e6cd03be4624e0d9b46412e5cb9fc
Robin Heathcote <robin.heathcote@icloud.com>
2016-03-30 13:02:22 +0100

    Solved Ch11 safer_picture_downloading.rb

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,32 @@
-# your code here
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox' #Change dirctory to where we want to move files to
+
+
+#  First we find all of the pictures to be moved.
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files:  "
+# This will be our counter. We'll start at 1 today, # though normally I like to count from 0.
+pic_number = 1
+pic_names.each do |name|
+print '.' # This is our "progress bar".
+new_name = if pic_number < 10
+
+  "#{batch_name}0#{pic_number}.jpg"
+else
+"#{batch_name}#{pic_number}.jpg"
+end
+
+unless File.exist? (new_name)
+  File.rename name, new_name
+else
+  puts "Error file already exists!"
+  exit
+end
+
+pic_number = pic_number + 1
+end
+puts
+puts 'Import completed'

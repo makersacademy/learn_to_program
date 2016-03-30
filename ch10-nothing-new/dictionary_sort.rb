3091a690d6ea6a9d89b95f39612aa3f22881a081
Jackrim1 <jriminton@gmail.com>
2016-03-30 14:32:58 +0100

    dictionary done

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,24 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+        recursiv_dict_sort array, []
 end
+
+def recursive_dict_sort unsorted, sorted
+        if unsorted.length <= 0
+                return sorted
+        end
+
+        smallest_unsorted = unsorted.pop
+        still_unsorted = []
+        unsorted.each do |tested_object|
+                if tested_object.downcase < smallest_unsorted.downcase
+                        still_unsorted << smallest_unsorted
+                        smallest_unsorted = tested_object
+                else
+                        still_unsorted.push tested_object
+                end
+        end
+
+        sorted.push smallest_unsorted
+        recursive_dict_sort still_unsorted, sorted
+end
+

405c1b5a6817f09e00165295e9d1575d47a782f8
Michael Harrison <harrim91@hotmail.co.uk>
2016-03-30 15:35:09 +0100

    completed shuffle task

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,17 @@
 def shuffle arr
-  # your code here
+  shuffle_recursive arr, []
+end
+
+def shuffle_recursive unshuffled, shuffled
+  return unshuffled if unshuffled.length == 0
+
+  random_index = rand(unshuffled.length)
+  random_element = unshuffled[random_index]
+
+  shuffled << random_element
+  unshuffled.delete_at(random_index)
+
+  shuffle_recursive unshuffled, shuffled
+
+  return shuffled
 end

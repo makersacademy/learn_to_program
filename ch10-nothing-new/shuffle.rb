461d6ff53272e353c8a49f0ef08eb43a60707f9d
pixelandpage <mags@pixelandpage.co.uk>
2016-03-30 14:39:01 +0100

    ch10 shuffle array fixed

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
 def shuffle arr
-  	return arr.shuffle
+  	unshuffled_list = arr
+	shuffled_list = []
+
+	while true
+		current = unshuffled_list.sample
+		shuffled_list.push(current)
+  		unshuffled_list.delete_at(unshuffled_list.find_index(current))
+  		break if unshuffled_list == []
+  	end
+
+	return shuffled_list
+
 end

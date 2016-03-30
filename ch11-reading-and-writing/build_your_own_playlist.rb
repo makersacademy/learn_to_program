6ad8c299fd460e75f176f08110533888d2d1d45d
Michael Harrison <harrim91@hotmail.co.uk>
2016-03-30 18:52:09 +0100

    safer picture downloading (added overwrite prevention to his solution) and build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,34 @@
-# your code here
+def shuffle arr
+  shuffle_recursive arr, []
+end
+
+def shuffle_recursive unshuffled, shuffled
+  return unshuffled if unshuffled.length == 0
+
+  random_index = rand(unshuffled.length)
+  random_element = unshuffled[random_index]
+
+  shuffled << random_element
+  unshuffled.delete_at(random_index)
+
+  shuffle_recursive unshuffled, shuffled
+
+  return shuffled
+end
+
+music_dir = "/Users/michaelharrison/Music/iTunes/iTunes\ Media/Music/"
+save_dir = "/Users/michaelharrison/Desktop/"
+playlist_name = "It's The Bea.m3u"
+search_string =  "The\ Bea*/*/*.m4a"
+
+Dir.chdir (music_dir)
+
+songs = Dir[search_string]
+
+shuffled_songs = shuffle (songs)
+
+Dir.chdir(save_dir)
+
+File.open playlist_name, 'w' do |f|
+  shuffled_songs.each { |song| f.write music_dir+song+"\n" }
+end
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,30 @@
-# your code here
+def generate_new_filename batch_name, pic_number
+  pic_number < 10 ? "#{batch_name}0#{pic_number}.jpg" : "#{batch_name}#{pic_number}.jpg"
+end
+
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox'
+# First we find all of the pictures to be moved.
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+Dir 'C:/Documents and Settings/Katy/PictureInbox'.
+print "Downloading #{pic_names.length} files: "
+# This will be our counter. We'll start at 1 today,
+# though normally I like to count from 0.
+pic_number = 1
+pic_names.each do |name|
+print '.' # This is our "progress bar".
+new_name = generate_new_filename batch_name, pic_number
+
+while File.exists?(new_name)
+  pic_number += 1
+  new_name = generate_new_filename batch_name,pic_number
+end
+
+File.rename name, new_name
+
+pic_number = pic_number + 1
+end
+puts # This is so we aren't on progress bar line.
+puts 'Done, cutie!'

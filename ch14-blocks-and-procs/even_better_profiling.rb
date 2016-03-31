cf2d3afaf0a216b6a90005c050bb2d020ce1d7a0
Lexi Tran <hazukitran@gmail.com>
2016-03-31 14:13:56 +0700

    Even better profilling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,21 @@
+PROFILE_ON = true
 def profile block_description, &block
-  # your code here
+  if PROFILE_ON
+    start_time = Time.new
+    block.call if block_given?
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+# profile '25000 doublings' do
+#   number = 1
+#   25000.times { number += number }
+#   puts "#{number.to_s.length} digits"  
+# end
+
+# profile 'count to a million' do
+#   1000000.times { |x| x }
+# end

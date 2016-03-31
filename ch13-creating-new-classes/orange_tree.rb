4d7982cb1b2c7541f45cf98be6368df95a2edd21
Max Provin <maxprovin@outlook.com>
2016-03-31 21:23:04 +0000

    orange tree should work now

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -120,14 +120,3 @@ end
  23.times do
    ot.one_year_passes
  end
- puts(ot.one_year_passes)
- puts(ot.count_the_oranges)
- puts(ot.height)
- puts(ot.one_year_passes)
- puts(ot.one_year_passes)
- puts(ot.one_year_passes)
- puts(ot.one_year_passes)
- puts(ot.one_year_passes)
- puts(ot.height)
- puts(ot.count_the_oranges)
- puts(ot.pick_an_orange)

7896a43c420328d38eb3c3b1dcec1c352d1ee2df
omajul <omajul2@hotmail.com>
2016-03-31 13:34:54 +0000

    Add old_roman code ch13-creating-new-classes/extend_built_in_classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,4 +1,28 @@
-require './ch09-writing-your-own-methods/old_school_roman_numerals'    
+# require './ch09-writing-your-own-methods/old_school_roman_numerals'    
+ROMANS = {
+    1000    => 'M',
+    500     => 'D',
+    100     => 'C',
+    50      => 'L',
+    10      => 'X',
+    5       => 'V',
+    1       => 'I'
+}
+
+def old_roman_numeral(number)
+    return "" if number <= 0 or number > 3000
+    result = ""
+    ROMANS.each do |k,v|
+        x = number / k
+        if x > 0
+            x.times do
+                result += ROMANS[k]
+                number -= k
+            end
+        end
+    end
+    result
+end
 
 class Integer
     # calculates factorial

e0a8505f88aad28a6b661bb9f774cb67b8a0e48f
Nick <nm.richards@yahoo.com>
2016-03-31 09:18:50 +0430

    Shuffle.rb completed

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,10 @@
 def shuffle arr
-  # your code here
+  return arr if arr.length <= 1
+  random = rand(arr.length)
+  randomed = arr.select.with_index{|x,y| x if y == random}
+  arr.delete_at(random)
+  remaining = arr
+
+  shuffle(remaining) + randomed
+
 end

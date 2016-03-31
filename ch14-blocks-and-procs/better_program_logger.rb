3c03138648a3dd7cebebc5bcbafc327a07d8bbb7
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-31 18:47:04 +0100

    add week3 ch14 better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,23 @@
-def log desc, &block
-  # your code here
+$nest_depth = -1
+
+def better_log desc, &block
+  $nest_depth += 1
+  padding = " "*$nest_depth
+  puts padding + "Beginning \"#{desc}\"..."
+  result = block.call
+  puts padding + "...\"#{desc}\" finished, returning: #{result}"
+  $nest_depth -= 1
+end
+
+better_log "outer block" do
+  better_log "some little block" do
+    better_log "teeny-tiny block" do
+      "lots of love"
+    end
+    42
+  end
+  better_log "yet another block" do
+      "I love Indian food!"
+  end
+  true
 end

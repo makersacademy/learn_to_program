1c724b217ee96caa293f7b5da9c0df838e8a917e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-31 16:55:58 +0100

    knuth-fisher-yates shuffle

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,15 @@
+Dir.chdir('/users/harrywynnwilliams/music/')
+  p Dir.pwd
+filenames = Dir["*.mp3"]
 def music_shuffle filenames
-  # your code here
-end
+  size = filenames.length
+    for i in 0..size
+      r=rand(size-i)+i
+      filenames[r], filenames[i] = filenames[i], filenames[r]
+      end
+      filenames
+    end
+
+
+
+p music_shuffle filenames

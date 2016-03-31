878dd1895c45f236a49da3535eecb2b2ea5b8f1b
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-03-31 18:09:44 +0100

    add week 3 ch14 program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
+def program_log desc, &block
   # your code here
+  puts "Beginning \"#{desc}\"..."
+  result = block.call
+  puts "...\"#{desc}\" finished, returning: #{result}"
+end
+
+program_log "outer block" do
+  program_log "some little block" do
+    5
+  end
+  program_log "yet another block" do
+      "I like Thai food!"
+  end
+  false
 end

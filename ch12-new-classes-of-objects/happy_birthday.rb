5f5b3a206f50fc90b94a4e512c33319165724151
Michael Harrison <harrim91@hotmail.co.uk>
2016-03-31 16:44:28 +0100

    1 billion seconds and birthday spanks

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "What year were you born?"
+
+year = gets.chomp.to_i
+
+puts "What number month were you born?"
+
+month = gets.chomp.to_i
+
+puts "What number day of the month were you born?"
+
+day = gets.chomp.to_i
+
+dob = Time.new(year,month,day)
+now = Time.new
+
+age = now - dob
+
+(age / (60 * 60 * 24 * 7 * 52)).floor.times {puts "SPANK!\n"}
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,3 +1,13 @@
 puts "What year were you born?"
 
 year = gets.chomp.to_i
+
+puts "What number month were you born?"
+
+month = gets.chomp.to_i
+
+puts "What number day of the month were you born?"
+
+day = gets.chomp.to_i
+
+puts(Time.gm(year, month, day) + 10**9)

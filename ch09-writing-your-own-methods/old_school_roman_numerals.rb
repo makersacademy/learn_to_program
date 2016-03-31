eee51ac9272ab3c97124b84bdbdd41c665186af7
Max Provin <maxprovin@outlook.com>
2016-03-31 12:54:44 +0000

    Done ch12 apart from roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -33,7 +33,7 @@ end
 def old_roman_numeral num 
   roman = ''
 
-￼￼roman = roman + 'M' * (num / 1000)
+  roman = roman + 'M' * (num / 1000)
   roman = roman + 'D' * (num % 1000 / 500) 
   roman = roman + 'C' * (num % 500 / 100) 
   roman = roman + 'L' * (num % 100 / 50) 
--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,36 @@
-# your code here
+
+birthdays = File.read('/home/ubuntu/workspace/messing_about/random_stuff/birthdays.txt')
+
+hash = {}
+
+b = birthdays.split("\n")
+b.each do |entry|
+    name, bday, year = entry.split(", ")
+    
+    hash[name] = year + " " + bday
+    
+end
+
+puts "Please enter a name"
+name = gets.chomp
+
+bday = hash[name]
+
+if bday == nil 
+    puts "Sorry I don't know who that is!" 
+    exit 
+end
+
+n = bday.length
+
+bmonth_day = bday[4...n]
+
+byear = bday[0..3]
+
+cur_year = Time.new.to_s[0..3]
+
+age = (cur_year.to_i - byear.to_i)
+
+next_bday = (cur_year.to_i + 1).to_s + bmonth_day
+
+puts "#{name}'s D.O.B is #{bday}, their next birthday will be #{next_bday} and they will be #{age+1} years old!"

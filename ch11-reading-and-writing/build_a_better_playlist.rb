4b6d895291131ef2d9560f80427820f480356b4f
Jackrim1 <jriminton@gmail.com>
2016-03-31 12:22:59 +0100

    ch12 no.1 done

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,42 @@
+
 def music_shuffle filenames
-  # your code here
+filenames = Dir['**/*.{m4a,M4A,mp3,MP3}']
+
+x = 0
+track_number = 0
+num_of_songs = 0
+playlist = []
+full_song_name = []
+current_album = ''
+previous_album = ''
+num_of_songs = filenames.length
+
+
+puts 'Playlist creator: what would you like to call this playlist?'
+playlist_name = gets.chomp
+playlist_name = "#{playlist_name}.m3u"
+puts "Downloading #{filenames.length} files: "
+
+while x < num_of_songs
+track_number = rand(num_of_songs)
+full_song_name = filenames(track_number).split'/'
+current_album = full_song_name[2]
+if current_album != previous_album
+    if filenames(track_number) != ''
+      playist << filenames[track_number]
+      filenames[track_number] = ''
+      x = x + 1
+    else
+    end
+    previous_album = current_album
+  else
+  end
 end
+filename = 'playlist.m3u'
+
+File.open filename, 'w' do |f|
+  f.puts playlist
+end
+end
+
+music_shuffle filenames
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,9 @@
-# your code here
+birthday = Time.local(1986, 8, 4, 11, 35)
+time_now = Time.new
+age_in_secs = time_now - birthday
+puts age_in_secs.to_i
+
+turning_billion = 1000000000 - age_in_secs.to_i
+on_this_date = turning_billion + Time.new.to_i
+here =  Time.at(on_this_date)
+puts here# your code here

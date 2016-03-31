45b5054b0e2416684df08f0b0653c184213bf4d5
Hanna <hannaeberhoefer@yahoo.com>
2016-03-31 13:40:42 +0100

    own_playlist complete

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,18 @@
-# your code here
+def shuffle(arr)
+  arr.shuffle
+end
+
+list = Dir["/Users/Hanna/Files/Music/*.ogg"]
+
+songs = shuffle(list)
+
+filename = "playlist.m3u"
+
+File.open(filename, "w") do |f|
+  songs.each do |song|
+  f.write song
+end
+end
+
+
+

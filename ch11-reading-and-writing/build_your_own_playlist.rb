3837c2ace69575e1f4bc53b498df28359140fdb3
Emilio <emsmnndz@gmail.com>
2016-03-31 17:27:14 +0100

    build_your_own_playlist.rb Working

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,14 @@
-# your code here
+require 'yaml'
+
+def save_method list, filename
+	File.open filename, 'w' do |name|
+		name.write(list.to_yaml)
+	end
+end
+
+
+string = Dir['*.mp3'] #again only working in the current directory
+
+filename = "playlist.m3u"
+
+save_method string, filename

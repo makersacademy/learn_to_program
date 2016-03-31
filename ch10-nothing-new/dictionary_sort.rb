3128774f8dd05f8f4c0d5ead39e86349263dbf00
Nick <nm.richards@yahoo.com>
2016-03-31 08:54:43 +0430

    The real dictionary sort, again help from Chris needed.

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,9 @@
 def dictionary_sort arr
-  arr.sort
+  return arr if arr.length <= 1
+
+  middle = arr.pop
+  less = arr.select{|x| x.downcase < middle.downcase}
+  more = arr.select{|x| x.downcase >= middle.downcase}
+
+  dictionary_sort(less) + [middle] + dictionary_sort(more)
 end

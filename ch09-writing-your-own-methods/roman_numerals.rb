d5028bc35926502365d52615795740795ed05ffd
Ruta <ruta@baltiejus.com>
2016-03-31 10:29:10 +0000

    Chapter 9 - Roman Numerals Challenge

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,25 @@
+Romans = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+     90 => "XC",
+     50 => "L",
+     40 => "XL",
+     10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I",
+}
+
+
 def roman_numeral num
-  # your code here
+  numerals = ""
+  Romans.each do |number, letter|
+      numerals << letter * (num / number)
+      num = num % number
+  end
+  return numerals
 end

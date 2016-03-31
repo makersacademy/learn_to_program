d879df1522c0e9c09905b6711584d20ccd39bf6e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-31 16:38:27 +0100

    playlist built

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,27 @@
 # your code here
+#x = 1
+#newname = "/users/harrywynnwilliams/music/song #{x}.mp3"
+x = 1
+while x <11 do
+
+file = File.open("/users/harrywynnwilliams/music/song#{x}.mp3", 'w')
+x+=1
+end
+puts Dir['users/harrywynnwilliams/music'] #use array brackets when using DIR
+puts Dir.pwd
+Dir.chdir("/users/harrywynnwilliams/music")
+puts Dir.pwd
+tunes = Dir["*.mp3"]
+tunes = tunes.shuffle
+File.open "playlist", "w" do |f|
+tunes.each{|tune| f.write tune+"\n"}
+end
+
+
+#  while x < 10
+#   File.open( newname, "w"){
+#     |f| f.write ""
+# x +=1
+
+# }
+# end

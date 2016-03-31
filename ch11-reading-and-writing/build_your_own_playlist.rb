92828215a3ec8a3e046c37d28347ea5efe46c088
Lexi Tran <hazukitran@gmail.com>
2016-03-31 17:30:10 +0700

    Build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,22 @@
-# your code here
+songs = Dir["/Users/hatran/Documents/Music/*.mp3"]
+
+File.open("playlist.m3u", "w") do |file|
+  songs.each do |song|
+    file.write(song + "\n")
+  end
+end
+
+array = File.readlines("playlist.m3u")
+
+def shuffle arr
+  new_array = []
+  while true
+    random = arr.sample
+    new_array << random
+    arr.slice!(arr.index(random))
+    break if arr.empty?
+  end
+  new_array
+end
+
+puts shuffle(array)

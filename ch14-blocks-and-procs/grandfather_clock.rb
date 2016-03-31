bc95885653396646773d95330778ab8713697de9
omajul <omajul2@hotmail.com>
2016-03-31 13:03:09 +0000

    Add exercices ch14

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,10 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock(&block)
+    h = (Time.new.hour + 11)%12 + 1
+    h.times do
+        block.call
+    end
+end
+
+grandfather_clock do
+    puts 'DONG!'
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
-def log desc, &block
-  # your code here
+def program_log(description, &block)
+    puts "Beginning \"#{description}\"..."
+    output = block.call
+    puts "...\"#{description}\" finished, returning: #{output}"
+end
+
+program_log 'outer block' do
+    program_log 'some little block' do
+        program_log 'yet another block' do
+            "I like Thai food!"
+        end
+        5
+    end
+    false
 end

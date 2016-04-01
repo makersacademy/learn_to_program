e8984ba46943c78a83bfdadd3d0129ee7d5d5ce2
chriselevate <coates.c@hotmail.com>
2016-04-01 20:57:30 +0100

    Adding Chapter 9 Challenges

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,23 @@
+=begin
+Old-school Roman numerals. In the early days of Roman numerals, the Romans didn’t bother with any of this new-fangled subtraction “IX” nonsense. No sir, it was straight addition, biggest to littlest—so 9 was written “VIIII,” and so on. Write a method that when passed an integer between 1 and 3000 (or so) returns a string containing the proper old- school Roman numeral. In other words, old_roman_numeral 4 should return 'IIII'. Make sure to test your method on a bunch of different numbers. Hint: Use the integer division and modulus methods on page 32.
+For reference, these are the values of the letters used:
+I = 1 V = 5 X = 10 L = 50 C = 100 D = 500 M = 1000
+=end
+
 def old_roman_numeral num
-  # your code here
+  raise 'Must use positive integer' if num <= 0
+
+  roman = ''
+
+  roman << 'M' * (num / 1000)
+  roman << 'D' * (num % 1000 / 500)
+  roman << 'C' * (num % 500 / 100)
+  roman << 'L' * (num % 100 / 50)
+  roman << 'X' * (num % 50 / 10)
+  roman << 'V' * (num % 10 / 5)
+  roman << 'I' * (num % 5 / 1)
+
+  roman
 end
+
+# puts(old_roman_numeral(1999))
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,42 @@
-def roman_numeral num
-  # your code here
+=begin
+“Modern” Roman numerals. Eventually, someone thought it would be ter- ribly clever if putting a smaller number before a larger one meant you had to subtract the smaller one. As a result of this development, you must now suffer. Rewrite your previous method to return the new-style Roman numerals so when someone calls roman_numeral 4, it should return 'IV'.
+=end
+
+def roman_numeral(num)
+  thousands = (num / 1000)
+  hundreds = (num % 1000 / 100)
+  tens  = (num % 100 / 10)
+  ones  = (num % 10)
+
+  roman = 'M' * thousands
+
+  if hundreds == 9
+    roman = roman + 'CM'
+  elsif hundreds == 4
+    roman = roman + 'CD'
+  else
+    roman = roman + 'D' * (num % 1000 / 500)
+    roman = roman + 'C' * (num % 500 / 100)
+  end
+
+  if tens == 9
+    roman = roman + 'XC'
+  elsif tens == 4
+    roman = romann + 'XL'
+  else
+    roman = roman + 'L' * (num % 100 / 50)
+    roman = roman + 'X' * (num % 50 / 10)
+  end
+
+  if ones == 9
+    roman = roman + 'IX'
+  elsif ones == 4
+    roman = roman + 'IV'
+  else
+    roman = roman + 'V' * (num % 10 / 5)
+    roman = roman + 'I' * (num % 5 / 1)
+  end
+  roman
 end
+
+# puts(roman_numeral(1999))

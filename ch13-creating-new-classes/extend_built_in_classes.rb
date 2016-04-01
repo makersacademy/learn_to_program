3ed92e0d7e21ff08e47501d86a9e3886e20b752c
Emilio <emsmnndz@gmail.com>
2016-04-01 11:43:07 +0100

    extend_built_in_classes.rb Solved

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,24 @@
 class Integer
-  # your code here
+
+  def factorial
+    raise 'Must not use negative integer' if self < 0
+    (self <= 1) ? 1 : self * (self-1).factorial
+  end
+
+  def to_roman 
+ values = [["M", 1000], ["D", 500], 
+          ["C", 100], ["L", 50], ["X", 10], 
+          ["V", 5], ["I", 1]]
+          #since we need every different value in order to have num = num % values below working
+
+  roman = ""
+  num = self #so we don't need a parameter for to_roman method, thus
+  			 # it can be call like 5.to_roman
+values.each do |letter, value|
+  roman << letter *(num / value) #as long as num/value is an integer, the resul of letter *(num/value) will be o so
+  num = num % value    		     # nothing will be added to roman
+
+end
+roman
+end
 end

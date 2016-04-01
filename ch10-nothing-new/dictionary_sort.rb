ee1b3b3564c1fd20ba3d5af16c16b98352b3a7bd
Adam Skuse <a.t.skuse@gmail.com>
2016-04-02 01:18:42 +0800

    Ch10 done

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,25 @@
-def dictionary_sort arr
-  # your code here
+# I'm gonna cheat with this and use the sort method as I failed so miserably
+# at the previous one - not going to reinvent the wheel if I haven't even invented
+# it for the first time...
+
+#def dictionary_sort arr
+arr = ['zed', 'Fish', 'bugle', 'eggs']
+  arr.each_with_index do |word, index|
+    if word =~ /[A-Z]/
+      arr.delete_at(index)
+      word.downcase!
+      word << "+"
+      arr.insert(index, word)
+    end
+  end
+arr.sort!
+arr.each_with_index do |word, index|
+  if word =~ /\+/
+    word.delete! "+"
+    word.capitalize!
+    arr.delete_at(index)
+    arr.insert(index, word)
+  end
 end
+puts arr
+#end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,77 @@
 def sort arr
-  # your code here
+  rec_sort arr, []
 end
+
+def rec_sort unsorted, sorted
+if unsorted.length <= 0 # so this is the bottom for our recursive method
+  return sorted
+end
+
+smallest = unsorted.pop
+still_unsorted = [] # this is what I was missing!
+
+# now no need for a score variable to keep track
+# score = 0
+unsorted.each do |tested_object| # here's where I'm stealing from Pine after grappling
+      if tested_object < smallest
+        still_unsorted.push < smallest
+        smallest = tested_object # okay, following this now, as in next iteration
+        # tested object will be the next in the array, and will be being compared
+        # to what we were just comparing...er...actually, maybe this is an instance
+        # where the code is easier to read than text. Maybe need a flow diagram?
+      else
+      still_unsorted.push tested_object # so this is our third array. a holder for
+      # nything that wasn't the smallest
+    end
+  end
+
+  sorted.push smallest
+
+  rec_sort still_unsorted, sorted #crafty!
+end
+
+
+
+
+
+
+
+
+
+# first attempt below, which failed - was only using one variable as the test subject, and not making an extra unsorted array  and
+# then trying to iterate through the remaining elements in the unsorted array, resulting
+# in ugly nested loops.
+# def sort arr# your code here
+# end
+=begin
+unsorted = ['taste', 'food', 'beagle', 'apple', 'woodchuck']
+sorted = []
+
+while unsorted.length > 1 do
+  testee = unsorted.shift
+  score = 0
+    unsorted.each do |word|
+      if testee < word
+        score +=1
+      end
+      if score == unsorted.length
+        sorted << testee
+      else
+        unsorted << testee
+      end
+end
+end
+
+puts unsorted
+puts
+puts sorted
+=end
+
+
+
+# we have an unsorted array of X elements
+# we remove our first element from unsorted array
+# compare that to each element of our unsorted array (num of elements is now X-1)
+# if it is smaller than all of those elements, we push it into sorted array
+# if it is larger, we put it back into unsorted
+# this keeps going until unsorted array = zero entries

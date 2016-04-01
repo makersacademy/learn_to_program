2831ec4b740bcdaececdc7eea392ec3f2efe7004
Jack Hardy <jackhardy1@gmail.com>
2016-04-01 10:28:43 +0100

    Ch10/sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,9 @@
-def sort arr
-  # your code here
+def sort(arr)
+ recursive_sort(arr,[])
+end
+
+def recursive_sort(unsorted_array, sorted_array)
+ unsorted_array.each_with_index {|val, index| val == unsorted_array.min ? sorted_array.push(unsorted_array.delete_at(index)) : false}
+ recursive_sort(unsorted_array, sorted_array) until unsorted_array.length == 0
+ sorted_array
 end

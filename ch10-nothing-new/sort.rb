90c72f67cdc5eb9cb7aef6b7439d80cd373609d1
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-01 23:06:16 +0200

    ch 10 done

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,17 @@
 def sort arr
-  # your code here
+	recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted
+
+	smallest = unsorted[1]
+
+	unsorted.each do |test_object|
+
+		if test_object.lowercase < smallest.lowercase
+			sorted.push(smallest)
+			smallest = test_object
+		else
+			sorted.push(test_object)
+		end 
+

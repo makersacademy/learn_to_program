d88118369d17d83e327c49658ba88bb90f4dc5bb
Max Provin <maxprovin@outlook.com>
2016-04-01 11:28:18 +0000

    grandfather clock done

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock(&block)
+    time = Time.new.hour
+    
+    if time > 12
+        time -= 12
+    end
+    
+    time.times do 
+        block.call
+    end
+    
 end
+

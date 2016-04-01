2e2aad546d780cf24d07f6eaf7a46e91bba8efbe
Max Provin <maxprovin@outlook.com>
2016-04-01 14:55:23 +0000

    ch14 done

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,26 @@
-def profile block_description, &block
-  # your code here
+def profile(block_description, &block)
+    if block_description == "off"
+        block.call
+    else
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+    end
+end
+
+
+profile("25000 doublings") do 
+    num = 1
+    25000.times do 
+        num= num+num
+    end
+    puts "#{num.to_s.length} digits"
+end
+
+profile("off") do
+    num = 0
+    1000000.times do
+        num += 1
+    end
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,4 @@
+=begin
 def log(desc, &block)
     puts "Starting #{desc}"
     return_val = block.call
@@ -16,3 +17,22 @@ log("outer block") do
     false
 end
 
+=end
+
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
+end

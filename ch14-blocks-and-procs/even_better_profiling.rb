994faa3d4c0ded0954cb0f90f4df22bbac042a50
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-01 21:03:44 +0100

    better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,59 @@
 def profile block_description, &block
-  # your code here
+  profiling_on = true
+  if profiling_on
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+# here's something cool i tried...
+
+# class Profile
+#   def initialize
+#     @profiling_on = true
+#   end
+
+#   attr_reader :profiling_on
+
+#   private
+#   attr_writer :profiling_on
+
+#   public
+#   def profile block_description, &block
+#     if profiling_on
+#       start_time = Time.new
+#       block.call
+#       duration = Time.new - start_time
+#       puts "#{block_description}: #{duration} seconds"
+#     else
+#       puts "Profiling is turned off"
+#       block.call
+#     end
+#   end
+
+#   def switch
+#     self.profiling_on = !@profiling_on
+#   end
+# end
+
+# profiler = Profile.new
+
+# profiler.profile "count to a million" do
+#   number = 0
+#   1000000.times do
+#     number = number + 1
+#   end
+# end
+
+# profiler.switch
+
+# profiler.profile "count to a million" do
+#   number = 0
+#   1000000.times do
+#     number = number + 1
+#   end
+# end

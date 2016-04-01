4ddac591cd9fb823c9878e656708c800cab9e6b6
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-01 13:22:23 +0200

    numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,61 @@
 def roman_numeral num
-  # your code here
+  to_return = ""
+  	thous = (num / 1000)
+	hunds = (num % 1000 / 100)
+  	tens  = (num % 100 / 10)
+  	ones  = (num % 10)
+
+  	if ones < 4
+  		to_return << "I" * ones
+  	elsif ones == 4
+    	to_return << "IV" 
+    elsif ones == 9 
+    	to_return << "IX" 
+    else
+    	to_return << "I" * (ones-5)
+    	to_return << "V"
+    end
+   
+   one = ""
+   one << to_return 
+   to_return = ""
+
+    if tens < 4
+  		to_return << "X" * tens
+  	elsif tens == 4
+    	to_return << "XL" 
+    elsif tens == 9 
+    	to_return << "XC" 
+    else
+    	to_return << "X" * (tens-5)
+    	to_return << "L"
+    end
+
+    ten = ""
+   	ten << to_return 
+   	to_return = ""
+    
+    	
+    if hunds < 4
+  		to_return << "C" * hunds
+  	elsif hunds == 4
+    	to_return << "CD" 
+    elsif hunds == 9 
+    	to_return << "CM" 
+    else
+    	to_return << "C" * (hunds-5)
+    	to_return << "L"
+    end
+
+    hundred = ""
+    hundred << to_return
+
+   	mill = ""
+  	mill << "M" * thous
+
+ 	"#{mill}#{hundred}#{ten}#{one}"
+
+
+
 end
+

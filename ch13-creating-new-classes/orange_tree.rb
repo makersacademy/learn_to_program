16789b302ae6f8454e14333db250d874200008d1
letianw <tmac4ever0824@gmail.com>
2016-04-01 21:26:13 +1100

    Finished orange tree. All in all a poorly written test TBH

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,56 @@
 class OrangeTree
-  # your code here
+  def initialize
+    @age = 0
+    @oranges = 0
+    @height = 0.0
+    @alive = true
+  end
+
+  def one_year_passes
+    if @alive == false
+      'A year later, the tree is still dead. :('
+    elsif @age == 25
+      @alive = false
+      'Oh, no! The tree is too old, and has died. :('
+    else
+      @age += 1
+      @height += 0.4
+      if @age == 10
+        @oranges = 35
+      elsif @age > 10
+        @oranges += 6
+      end
+      "This year your tree grew to #{@height.round(1)}m tall, and produced #{@oranges} oranges."
+    end
+  end
+
+  def count_the_oranges
+    if @alive
+      @oranges
+    else
+      'A dead tree has no oranges. :('
+    end
+  end
+
+  def height
+    if @alive == false
+      'A dead tree is not very tall. :('
+    else
+      @height.round(1)
+    end
+  end
+
+  def pick_an_orange
+    if @alive
+      if @orange_count > 0
+        @orange_count = @orange_count - 1
+        'You pick a juicy, delicious orange!'
+      else
+        'You search every branch, but find no oranges.'
+      end
+    else
+      'A dead tree has nothing to pick. :('
+    end
+  end
 end
+

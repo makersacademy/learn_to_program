0bed43748ffb2282c9b06312518dad18774e97e6
Jack Hardy <jackhardy1@gmail.com>
2016-04-01 11:15:44 +0100

    Ch13/Extend_Built_in_classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,30 @@
 class Integer
-  # your code here
+def to_roman
+
+roman_numerals = []
+
+m_num = self / 1000
+d_num = self % 1000 / 500
+c_num = self % 500 / 100
+l_num = self % 100 / 50
+x_num = self % 50 / 10
+v_num = self % 10 / 5
+i_num = self % 5 / 1
+
+roman_numerals << ("M")* m_num
+roman_numerals << ("D")* d_num
+roman_numerals << ("C")* c_num
+roman_numerals << ("L")* l_num
+roman_numerals << ("X")* x_num
+roman_numerals << ("V")* v_num
+roman_numerals << ("I")* i_num
+
+roman_numerals.join
 end
+
+def factorial
+  self.downto(1).reduce(:*)
+end
+end
+
+#done!!!

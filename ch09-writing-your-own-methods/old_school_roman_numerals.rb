85fd045a0f7c654ed66320d478be8f385c91f67e
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-01 12:56:19 +0200

    numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,36 @@
 def old_roman_numeral num
-  # your code here
-end
+	to_return = ""
+  	thous = (num / 1000)
+	hunds = (num % 1000 / 100)
+  	tens  = (num % 100 / 10)
+  	ones  = (num % 10)
+
+  	if ones < 5
+  	to_return << "I" * ones
+  	else
+    to_return << "I" * (ones-5)
+    to_return << "V"
+  	end
+
+  	if tens < 5
+  	to_return << "X" * tens
+  	else
+    to_return << "X" * (tens-5)
+    to_return << "L"
+  	end
+
+  	if hunds < 5
+  	to_return << "C" * hunds
+  	else
+    to_return << "C" * (hunds-5)
+    to_return << "D"
+  	end
+
+  	to_return << "M" * thous
+
+  	p to_return.reverse
+
+
+ end
+
+old_roman_numeral(4)

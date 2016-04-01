ecb4d4201dd282d6ea8146833225b94eee35f4ed
Max Provin <maxprovin@outlook.com>
2016-04-01 11:52:40 +0000

    program_logger done

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
-def log desc, &block
-  # your code here
+def log(desc, &block)
+    puts "Starting #{desc}"
+    return_val = block.call
+    puts "#{desc.capitalize} has ended returning #{return_val}"
 end
+
+log("outer block") do 
+    log("inner block") do 
+        5
+    end 
+
+    log("2nd inner block") do
+       "I like thai food"
+    end
+    
+    false
+end
+

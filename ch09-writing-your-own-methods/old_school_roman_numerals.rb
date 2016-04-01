3fed97bc738ebd511653459cee1c8d98c6cf8362
Ben Stein <bumstain@gmail.com>
2016-04-01 22:25:25 +1000

    Completed Chapter 9 exercises

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,14 @@
 def old_roman_numeral num
-  # your code here
+  puts 'Enter an integer.'
+  num = gets.chomp.to_i
+
+  m = 'M' * (num / 1000)
+  d = 'D' * (num % 1000 / 500)
+  c = 'C' * (num % 500  / 100)
+  l = 'L' * (num % 100  / 50)
+  x = 'X' * (num % 50   / 10)
+  v = 'V' * (num % 10   / 5)
+  i = 'I' * (num % 5)
+
+  puts m + d + c + l + x + v + i
 end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,28 @@
 def roman_numeral num
-  # your code here
+  puts 'Enter an integer.'
+  num = gets.chomp.to_i 
+
+  romans = {
+    1000 => 'M',
+    900  => 'CM',
+    500  => 'D',
+    400  => 'CD',
+    100  => 'C',
+    90   => 'XC',
+    50   => 'L',
+    40   => 'XL',
+    10   => 'X',
+    9    => 'IX',
+    5    => 'V',
+    4    => 'IV', 
+    1    => 'I'
+  }
+
+  answer = ""
+  romans.each do |value, key|
+    answer << key * (num / value)
+    num = num % value
+  end
+  puts answer
 end
+

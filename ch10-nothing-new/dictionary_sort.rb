699b17e7aa6cdc86ef20a2244f6d2ea548423217
Jack Hardy <jackhardy1@gmail.com>
2016-04-01 11:13:16 +0100

    ch10/Dictionary_Sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,9 @@
-def dictionary_sort arr
-  # your code here
+def sort(arr)
+ recursive_sort(arr,[])
+end
+
+def recursive_sort(unsorted_array, sorted_array)
+ unsorted_array.each_with_index {|val, index| val.downcase == unsorted_array.min ? sorted_array..downcase.push(unsorted_array.delete_at(index)) : false}
+ recursive_sort(unsorted_array, sorted_array) until unsorted_array.length == 0
+ sorted_array
 end

24bcd86585b0199dc068cdd1ed89ea17060eeee2
Jackrim1 <jriminton@gmail.com>
2016-04-01 12:48:24 +0100

    extend shuffle done

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,25 @@
-class Integer
-  # your code here
+class Array
+
+
+def shuffle array
+	final_array = []
+	while array.length > 0
+	num = 0
+	rand_index = rand(array.length)
+	size = array.length
+	holding_pen = []
+
+	array.each do |x|
+		if num == rand_index
+		final_array << x
+		else
+		holding_pen << x
+		end
+		num = num + 1
+	end
+array = holding_pen
+end
+final_array
+
+end
 end

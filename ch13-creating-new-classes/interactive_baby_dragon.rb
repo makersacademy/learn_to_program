2e526c0b6775b29b869eb1e37841facd6fa669f6
pixelandpage <mags@pixelandpage.co.uk>
2016-04-02 21:40:37 +0100

    ch13 interactive baby dragon

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,29 @@
-# your code here
+class Dragon
+
+	def initialize name
+		@name = name
+		@asleep = false
+		@stuff_in_belly = 10 # He's full.
+		@stuff_in_intestine = 0 # He doesn't need to go.
+		puts "#{@name} is born."
+	end
+
+	def feed
+		puts "You feed #{@name}."
+		@stuff_in_belly = 10
+	end
+
+	def walk
+		puts "You walk #{@name}."
+		@stuff_in_intestine = 0
+	end
+end
+
+dragon = Dragon.new('tinyfin')
+
+while true
+	puts "command: "
+	cmd = gets.chomp.downcase
+	break if cmd == "stop"
+	dragon.send(cmd)
+end

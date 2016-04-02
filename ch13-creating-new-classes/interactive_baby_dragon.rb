cc828ea1e70527270d3e19391811af976bbc3d50
OMGDuke <lex1989@gmail.com>
2016-04-02 13:02:51 +0100

    Completed ch13:interactive_baby_dragon

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,21 @@
-# your code here
+puts "Enter your Dragon's name"
+name = gets.chomp
+pet = Dragon.new name
+while true
+  puts "what would you like to do with your dragon?"
+  input = gets.chomp.downcase
+  case input
+    when "feed"
+      pet.feed
+    when "walk"
+      pet.walk
+    when "put to bed"
+      pet.put_to_bed
+    when "toss"
+      pet.toss
+    when "rock"
+      pet.rock
+    else
+      puts "The available commands are feed, walk, put to bed, toss, rock."
+  end
+end

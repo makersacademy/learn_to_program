12a5a57134e16452a149e1924a61660147bb00ae
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-02 16:26:13 +0100

    updated sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,12 +1,22 @@
-def sort(array)  
-	sorted = []
-    while true
-	smallest = array.min 
-    sorted << smallest 
+def sort(arr)  
+  recursive_sort(arr, [])
+ end    
+def recursive_sort unsorted, sorted 
+   return sorted if unsorted.length <= 0
+   still_to_sort = []
+    smallest = unsorted.pop 
+    unsorted.each do |x|
+    if x < smallest
+    still_to_sort << smallest
+    smallest = x
+    else
+    still_to_sort << x
+    end
+    end
+    sorted << smallest
+recursive_sort still_to_sort , sorted
+end
 
-    array.slice!(array.index(smallest))
-  break if array.empty?
+  #puts(sort(["a", "can", "can", "feel", "like", "singing"]))
+  
 
-   end
-    sorted
-   end

4ac3468bb1abce1ae359a2b48eac96c909198dea
Ruta <ruta@baltiejus.com>
2016-04-02 11:24:44 +0000

    Chapter 10 - Dictionary Sort Challenge

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,15 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  unsorted.each_with_index do |word, index|
+    if word == unsorted.min.downcase
+      sorted << (unsorted.delete_at(index))
+    end
+  end
+  
+  recursive_sort(unsorted, sorted) until unsorted.length == 0
+  
+  return sorted
 end

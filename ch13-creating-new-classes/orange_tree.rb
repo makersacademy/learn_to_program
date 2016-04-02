95c1cb4499227d30c3d3efc98171306b99070b39
Jackrim1 <jriminton@gmail.com>
2016-04-02 16:48:24 +0100

    Orange tree done

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,50 @@
 class OrangeTree
-  # your code here
+
+#initialize the growing of the tree
+def initialize
+@height = 0
+@orange_count = 0
+@age = 0
+grow
+end
+
+#define what growing means
+def grow
+one_year_passes
+
+#start = Time.gm(2016,4,1,12,00)
+#now = Time.new
+#@age = now - start
+#@age = @age.to_i
+#puts @age
+end
+
+def one_year_passes
+  if @alive
+  @age = @age + 1
+  @height = @height + 1
+  if @age > 2
+    @orange_count = @orange_count * @age
+  end
+else
+
+end
+
+def count_the_oranges
+  puts "There are #{@orange_count} oranges."
+end
+
+def pick_an_orange
+  @orange_count = @orange_count - 1
+  puts "Mmm that orange was bloody delicious!"
+end
+
+
+
+
+end
+  tree = OrangeTree.new
+20.times do
+  tree.one_year_passes
+end
 end

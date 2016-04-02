7359e803f7d6ea61815d91fc540584f820aff96f
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-02 15:55:40 +0200

    ch11

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,64 @@
 def music_shuffle filenames
-  # your code here
+
+  #variables
+  filenames = filenames.sort
+  songs = filenames.length
+  tracker = 0
+  #create arrays
+  to_return = []
+  pile_a = []
+  pile_b = []
+  combinedpiles = []
+
+
+  #shuffle the deck
+  until tracker == songs
+    beingshuffled = filenames.pop
+
+    if tracker%2 == 0
+      pile_a.push(beingshuffled)
+    else
+      pile_b.push(beingshuffled)
+    end
+
+    tracker += 1
+  end
+
+  combinedpiles.push(pile_a).push(pile_b).flatten!
+  tracker = 0
+  pile_a = []
+  pile_b = []
+
+
+  until tracker == songs
+    beingshuffled = combinedpiles.pop
+
+    if tracker%2 == 0
+      pile_a.push(beingshuffled)
+    else
+      pile_b.push(beingshuffled)
+    end
+
+    tracker += 1
+  end
+
+  combinedpiles.push(pile_a).push(pile_b).flatten!
+
+
+  #cut the deck
+  cut_at = rand(0..songs)
+  firsthalf = combinedpiles.slice!(0..cut_at)
+
+  #return
+  to_return.push(combinedpiles).push(firsthalf).flatten
+
+
+
+
+
 end
+
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+        'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+puts(music_shuffle(songs))

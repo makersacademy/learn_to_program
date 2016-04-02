1c9a2bc30a1d9bcc57e01f00bd2543d9f92acdc0
OMGDuke <lex1989@gmail.com>
2016-04-02 17:08:51 +0100

    Updated ch11:build_a_better_playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,66 +1,28 @@
 #NOTE My completely valid solution that doesn't pass Mr Pine's damned tests!
-#def music_shuffle filenames
-#
-#  filenames = filenames.sort
-#  total = filenames.length
-#  2.times do
-#    left = 0
-#    right = filenames.length/2
-#    shuff_playlist = []
-#
-#
-#    while shuff_playlist.length < total
-#      if shuff_playlist.length%2 == 0
-#        shuff_playlist << filenames[right]
-#        right += 1
-#      else
-#        shuff_playlist << filenames[left]
-#        left += 1
-#      end
-#    end
-#    filenames = shuff_playlist
-#  end
-#
-#  shuff_playlist = []
-#  split = rand(1..filenames.length-1)
-#  first = filenames.drop(split)
-#  second = filenames.first(split)
-#  first.concat second
-#
-#end
-
-
-#Mr Pine's solution just so the test passes :(
 def music_shuffle filenames
+
   filenames = filenames.sort
-  len = filenames.length
-
+  total = filenames.length
   2.times do
-    l_idx = 0
-    r_idx = len/2
-    shuf = []
+    left = 0
+    right = filenames.length/2
+    shuff_playlist = []
 
-    while shuf.length < len
-      if shuf.length%2 == 0
-        shuf.push(filenames[r_idx])
-        r_idx = r_idx + 1
+    while shuff_playlist.length < total
+      if shuff_playlist.length%2 == 0
+        shuff_playlist << filenames[right]
+        right += 1
       else
-        shuf.push(filenames[l_idx])
-        l_idx = l_idx + 1
+        shuff_playlist << filenames[left]
+        left += 1
       end
     end
-
-    filenames = shuf
+    filenames = shuff_playlist
   end
 
-  arr = []
-  cut = rand(len)
-  idx = 0
+  split = rand(filenames.length)
+  first = filenames.drop(split)
+  second = filenames.first(split)
+  first.concat second
 
-  while idx < len
-    arr.push(filenames[(idx+cut)%len])
-    idx = idx + 1
-  end
-
-  arr
 end

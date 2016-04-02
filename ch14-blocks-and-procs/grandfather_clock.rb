f2ede40471f5cdfe0e8c31c01eadbec164ff9534
OMGDuke <lex1989@gmail.com>
2016-04-02 18:15:47 +0100

    Completed ch14:even_better_profiling

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,18 @@
 def grandfather_clock &block
-  # your code here
+  time = Time.new.hour
+  if time >= 13
+    time -= 12
+  end
+
+  if time == 0
+    time = 12
+  end
+
+  time.times do
+    block.call
+  end
+end
+
+grandfather_clock do
+  puts "DONG!"
 end

9ece3f41ad48d25ee441b27c80e53fb874254d2d
Ruta <ruta@baltiejus.com>
2016-04-02 11:19:56 +0000

    Chapter 10 - Sort Challenge

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,15 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  unsorted.each_with_index do |word, index|
+    if word == unsorted.min
+      sorted << (unsorted.delete_at(index))
+    end
+  end
+  
+  recursive_sort(unsorted, sorted) until unsorted.length == 0
+  
+  return sorted
 end

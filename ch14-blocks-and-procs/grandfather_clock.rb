e013bb9832afd889ff70e9c34d71118418e6569b
Jackrim1 <jriminton@gmail.com>
2016-04-02 20:08:11 +0100

    grandfather clock done

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,15 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock description, &block
+
+now = Time.new.hour
+if now > 12
+  now = now - 12
+end
+now.times do
+  block.call
+end
+end
+
+
+grandfather_clock 'Dong' do
+  puts 'DONG!'
 end

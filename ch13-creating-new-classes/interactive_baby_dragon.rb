540b66e9bd8d3534dc39ce9809567f7cdc05dac0
chriselevate <coates.c@hotmail.com>
2016-04-02 14:07:32 +0100

    Adding Chapter 13 to Challenges

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,51 @@
-# your code here
+#How Chris Pine would do it
+# using the Dragon class from the chapter
+puts 'What would you like to name your baby dragon?'
+name = gets.chomp
+pet = Dragon.new name
+obj = Object.new # just a blank, dummy object
+
+while true
+  puts
+  puts 'commands: feed, toss, walk, rock, put_to_bed, exit'
+  command = gets.chomp
+  if command == 'exit'
+    exit
+  elsif pet.respond_to?(command) && !obj.respond_to?(command)
+    # I only want to accept methods that dragons have,
+    # but that regular objects *don't* have.
+    pet.send command
+  else
+    puts 'Huh? Please type one of the commands.'
+  end
+end
+
+
+#How you could do it
+=begin
+# using the Dragon class from the chapter
+puts 'What would you like to name your baby dragon?'
+name = gets.chomp
+pet = Dragon.new name
+
+while true
+  puts
+  puts 'commands: feed, toss, walk, rock, put to bed, exit'
+  command = gets.chomp
+
+  if command == 'exit'
+    exit
+  elsif command == 'feed'
+    pet.feed
+  elsif command == 'toss'
+    pet.toss
+  elsif command == 'walk'
+    pet.walk
+  elsif command == 'rock'
+    pet.rock
+  elsif command == 'put to bed'
+    pet.put_to_bed
+  else
+    puts 'Huh? Please type one of the commands.'
+  end
+end

fdd24b983c2c36838fdacf3bd2b647325dcd83fe
Ruta <ruta@baltiejus.com>
2016-04-02 15:46:59 +0000

    Chapter 12 - Roman to Integer Challenge

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,45 @@
 def roman_to_integer roman
-  # your code here
+  roman_nums = {
+      "M" => 1000,
+      "CM" => 900,
+      "D" => 500,
+      "CD" => 400,
+      "C" => 100,
+      "XC" => 90,
+      "L" => 50,
+      "XL" => 40,
+      "X" => 10,
+      "IX" => 9,
+      "V" => 5,
+      "IV" => 4,
+      "I" => 1
+}
+
+
+roman = roman.upcase.split(//)
+num = 0
+
+roman.each_with_index do |val, index|
+  
+  if index + 1 < roman.length && roman_nums.has_key?(val + roman[index + 1])
+      num = num + roman_nums[val + roman[index + 1]]
+  elsif roman_nums.has_key?(val)
+    next if roman_nums.has_key?(roman[index - 1] + val) && index - 1 >= 0 
+    num = num + roman_nums[val]
+  else
+    return "Error"
+  end
+  end
+  return num
 end
+
+
+
+#roman_nums.each do |letter, number|
+    
+ #   if roman[0] == letter
+  #      intergers << number
+   #     roman = roman[0].delete
+#    end
+ #   roman_to_interger roman
+ # end

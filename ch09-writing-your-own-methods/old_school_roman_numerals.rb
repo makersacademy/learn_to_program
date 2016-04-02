cadd71e1a9148670a30241dc98733a748964df9a
Macey <macebake@gmail.com>
2016-04-02 10:51:16 -0700

    Ch 09 after edits

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,57 @@
 def old_roman_numeral num
-  # your code here
+roman = ''
+
+roman << 'M' * (num / 1000)
+
+roman << 'D' * (num % 1000 / 500)
+
+roman << 'C' * (num % 500 / 100)
+
+roman << 'L' * (num % 100 / 50)
+
+roman << 'X' * (num % 50 / 10)
+
+roman << 'V' * (num % 10 / 5)
+
+roman << 'I' * (num % 5 / 1)
+
+roman
+
+#  newstr = ""
+#  m = num/1000
+#  d = num/500
+#  c = num/100
+#  l = num/50
+#  x = num/10
+#  v = num/5
+
+#  if m > 1
+#      newstr << "M"*m.to_i
+#      old_roman_numeral(num-(m*1000))
+#    end
+#  if (num-m*1000)/500 > 1
+#        newstr << "D"*d.to_i
+#        old_roman_numeral(num-(d*500))
+#      end
+#  if (num-d*500)/100 > 1
+#        newstr << "C"*c.to_i
+#        old_roman_numeral(num-(c*100))
+#      end
+#  if (num-c*100)/50 > 1
+#        newstr << "L"*l.to_i
+#        old_roman_numeral(num-(l*50))
+#  end
+#  if (num-l*50)/10 > 1
+#        newstr << "X"*x.to_i
+#        old_roman_numeral(num-(x*10))
+#  end
+#  if (num-x*10)/5 > 1
+#        newstr << "V"*v.to_i
+#        old_roman_numeral(num-(v*5))
+#  end
+#  if (num-v*5) % 5 != 0
+#        newstr << "I"*((num-v*5) % 5)
+#  else
+#  end
 end
+
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,47 @@
-def roman_numeral num
-  # your code here
+def roman_numeral(num)
+  numeral = ""
+
+  m = (num / 1000)
+  d = (num % 1000) / 500
+  c = (num % 500) / 100
+  l = (num % 100) / 50
+  x = (num % 50) / 10
+  v = (num % 10) / 5
+  i = num % 5
+
+  numeral = "M" * m
+
+  if c == 9
+  numeral << "CM"
+  elsif c == 4
+  numeral << "CD"
+  numeral << "D" * d
+  numeral << "C" * c
+  else
+  numeral << "D" * d
+  numeral << "C" * c
+  end
+
+  if x == 9
+  numeral << "XC"
+  elsif x == 4
+  numeral << "XL"
+  numeral << "L" * l
+  numeral << "X" * x
+  else
+  numeral << "L" * l
+  numeral << "X" * x
+  end
+
+  if num%10 == 9
+  numeral << "IX"
+  elsif num%10 == 4
+  numeral << "IV"
+  numeral << "V" * v
+  else
+  numeral << "V" * v
+  numeral << "I" * i
+  end
+
+numeral
 end

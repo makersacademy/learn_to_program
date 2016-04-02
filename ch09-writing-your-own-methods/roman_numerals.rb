b15b0308330bd88577aa35735ce7f69179dc4da2
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-02 15:28:06 +0100

    roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,23 @@
-def roman_numeral num
-  # your code here
-end
+def roman_numeral  num
+ result = ""
+ romans = {"M"=> 1000,
+          "CM"=> 900, 
+         "D"=> 500,
+         "CD"=> 400,  
+           "C"=> 100,
+           "XC"=> 90,  
+           "L"=> 50,
+           "XL"=> 40,   
+           "X"=> 10, 
+           "IX"=> 9,  
+           "V"=> 5,
+           "IV"=> 4,    
+           "I"=> 1}   
+
+
+ romans.each do | roman, number | 
+result << roman * (num / number)
+num = num % number     
+ end
+  result 
+ end

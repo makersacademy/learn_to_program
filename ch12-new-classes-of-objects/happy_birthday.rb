140ccf126ca5c451be628f2ad09cb1fc04e54203
Adam Skuse <a.t.skuse@gmail.com>
2016-04-02 13:51:31 +0800

    ch12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp.to_i
+puts "And what month?"
+month = gets.chomp.to_i
+puts "And what day?"
+day = gets.chomp.to_i
+birthday = Time.local(year, month, day)
+
+age = Time.now.year - birthday.year
+age = age - 1 if Time.now.month < birthday.month
+puts "Spank!\n" * age
+puts age

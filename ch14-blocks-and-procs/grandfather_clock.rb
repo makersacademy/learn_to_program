44e1f6c8eb2beff2281b00500c38a2d2d5cdb99d
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-03 13:49:09 +0100

    grandfather clock calling in block

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,12 @@
 def grandfather_clock &block
-  # your code here
+ current_hour = Time.new.hour # your code here
+  if current_hour > 12
+  current_hour = current_hour - 12
+  else
+  current_hour
+  end
+  current_hour.times(&block)
 end
+
+
+grandfather_clock {puts "bong"}

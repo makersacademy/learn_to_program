3a82b1d8960b037952fc261ca58c311c4be56259
Jackrim1 <jriminton@gmail.com>
2016-04-03 10:17:56 +0100

    program logger done

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,24 @@
 def log desc, &block
-  # your code here
+  puts 'Beginning ' + desc + '...' #"something"
+call = block.call
+
+  puts '...' + desc + ' finished, returning: ' + call.to_s
+
 end
+
+log 'outer block' do
+  log 'some little block' do
+    2 + 2 + 1
+  end
+
+log 'yet another block' do
+  puts 'I like Thai food!'
+end
+
+'0' == 0
+end
+
+
+#some other block do - puts - end
+
+

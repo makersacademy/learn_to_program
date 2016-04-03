281f631cdac932b5ad96c492b0aedbdbd09b648e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-03 11:31:38 +0100

    Chris Pines version mine returns 1 year too short

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,42 +1,110 @@
+# class OrangeTree
+#    def initialize
+#       @height = 0
+#       @oranges = 0
+#       @tree = true
+#     end
+
+#   def height
+#     if @tree == true
+#       @height.round(1)
+#     else
+#        'A dead tree is not very tall. :('
+#     end
+#   end
+#   def one_year_passes
+#     if @tree
+#     @height = @height + 0.4
+#     @oranges = 0
+#     if @height > 10 && rand(2) > 0
+#       puts "tree has died"
+#       @tree == false
+#         'Oh, no! The tree is too old, and has died. :('
+#     elsif @height > 2
+#       @oranges = (@height * 15 - 25).to_i
+#       "This year your tree grew to #{@height.round(1)}m tall," +
+#           " and produced #{@oranges} oranges."
+
+#     else
+#      "This year your tree grew to #{@height.round(1)}m tall," +
+#           " but is still too young to bear fruit."
+#     end
+#   else
+#       'A year later, the tree is still dead. :('
+#     end
+#   end
+#    def pick_an_orange
+#     if @tree==true
+#       if @oranges > 0
+#         @oranges = @oranges - 1
+#         'You pick a juicy, delicious orange!'
+#       else
+#         'You search every branch, but find no oranges.'
+#       end
+#     else
+#       'A dead tree has nothing to pick. :('
+#     end
+#   end
+
+#   def count_the_oranges
+#     if @tree == true
+#       @oranges
+#      #puts "tree has #{@oranges} oranges"
+#     else
+#        'A dead tree has no oranges. :('
+#     end
+#   end
+
+# end
 class OrangeTree
-   def initialize
-      @height = 0
-      @oranges = 0
-      @tree = true
-    end
+  def initialize
+    @height = 0
+    @orange_count = 0
+    @alive = true
+  end
 
   def height
-    if @tree == true
+    if @alive
       @height.round(1)
     else
-       'A dead tree is not very tall. :('
+      'A dead tree is not very tall. :('
     end
   end
-  def one_year_passes
-    if @tree
-    @height = @height + 0.4
-    @oranges = 0
-    if @height > 10 && rand(2) > 0
-      puts "tree has died"
-      @tree == false
-        'Oh, no! The tree is too old, and has died. :('
-    elsif @height > 2
-      @oranges = (@height * 15 - 25).to_i
-      "This year your tree grew to #{@height.round(1)}m tall," +
-          " and produced #{@oranges} oranges."
 
+  def count_the_oranges
+    if @alive
+      @orange_count
     else
-     "This year your tree grew to #{@height.round(1)}m tall," +
-          " but is still too young to bear fruit."
+      'A dead tree has no oranges. :('
     end
-  else
+  end
+
+  def one_year_passes
+    if @alive
+      @height = @height + 0.4
+      @orange_count = 0 # old oranges fall off
+      if @height > 10 && rand(2) > 0
+        # tree dies
+        @alive = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > 2
+        # new oranges grow
+        @orange_count = (@height * 15 - 25).to_i
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " and produced #{@orange_count} oranges."
+      else
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " but is still too young to bear fruit."
+      end
+    else
       'A year later, the tree is still dead. :('
     end
   end
-   def pick_an_orange
-    if @tree==true
-      if @oranges > 0
-        @oranges = @oranges - 1
+
+  def pick_an_orange
+    if @alive
+      if @orange_count > 0
+        @orange_count = @orange_count - 1
         'You pick a juicy, delicious orange!'
       else
         'You search every branch, but find no oranges.'
@@ -45,18 +113,7 @@ class OrangeTree
       'A dead tree has nothing to pick. :('
     end
   end
-
-  def count_the_oranges
-    if @tree == true
-      @oranges
-     #puts "tree has #{@oranges} oranges"
-    else
-       'A dead tree has no oranges. :('
-    end
-  end
-
 end
-
 # tree =OrangeTree.new
 # tree.height
 # p tree.one_year_passes

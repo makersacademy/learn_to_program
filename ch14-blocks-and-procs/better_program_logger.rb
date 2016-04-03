30df3bea5a2218cb30061eeec11348cc823908f2
iammatthewward@gmail.com <iammatthewward@gmail.com>
2016-04-03 14:06:05 +0100

    All challenges completed up to end of ch14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,10 @@
-def log desc, &block
-  # your code here
+$nesting_depth = 0
+
+def better_log desc, &block
+  tabs = " " * $nesting_depth
+  puts "#{tabs}Beginning \"#{desc}\"..."
+  $nesting_depth += 1
+  result = block.call
+  puts "#{tabs}...\"#{desc}\" finished, returning: #{result}"
+  $nesting_depth -= 1
 end
--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
 def grandfather_clock &block
-  # your code here
+  current_time = Time.new.hour
+  if current_time > 12
+    current_time -= 12
+  end
+  current_time.times do
+    block.call
+  end
+end
+
+grandfather_clock do
+  puts "BONG"
 end

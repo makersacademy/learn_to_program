1eb8829e0e49f056b5bfe80e87ee1021167d464a
Hanna <hannaeberhoefer@yahoo.com>
2016-04-03 13:22:12 +0100

    better_logger complete

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
-def log desc, &block
-  # your code here
+$depth = 0
+def better_log(block_desc, &block)
+  nesting = " " * $depth
+  puts nesting + 'Beginning "' + block_desc  + '"...'
+  $depth = $depth + 1
+  result = block.call
+  $depth = $depth - 1
+  puts nesting + '..."' + block_desc  + '" finished, returning: ' + result.to_s
+end
+
+better_log("outer block") do
+  better_log("some little block") do
+    better_log("teeny-tiny block") do
+    "lots of love"
+    end
+    6 * 7
+  end
+    better_log("yet another block") do
+    "I love Indian food!"
+    end
+  true
 end

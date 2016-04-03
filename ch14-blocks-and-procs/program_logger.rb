f5a104ea9108b8671107dc549b38e249ea02a97e
letianw <tmac4ever0824@gmail.com>
2016-04-03 11:51:13 +1000

    better program_logger left

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,6 @@
-def log desc, &block
+def program_log desc, &block
   # your code here
+  puts "Beginning #{desc.inspect}..."
+  result = block.call
+  puts "...#{desc.inspect} finished, returning: #{result}"
 end

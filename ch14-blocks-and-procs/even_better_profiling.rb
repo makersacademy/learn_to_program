6ef445a182b0c8ccbb4c54c56df524e487adc326
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-03 13:28:05 +0100

    if else added

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,12 @@
-def profile block_description, &block
-  # your code here
+
+  def profile block_description, &block
+profile = false
+if profiling == true
+  start_time = Time.new
+  block.call
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+else
+  block.call
+end
 end

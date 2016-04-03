8455dccea2ccdd1f12c366e8b9e1ae5151b4a846
sitypop <sityemilyshah@gmail.com>
2016-04-03 14:15:27 +0100

    Added ch13/extend_built_in_classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,34 @@
 class Integer
-  # your code here
+
+  def factorial
+  	self.downto(1).reduce(:*)
+  end
+
+  def to_roman
+
+ 	conversion = {
+  	1000 => "M",
+  	500 => "D",
+  	100 => "C",
+  	50 => "L",
+  	10 => "X",
+  	5 => "V",
+  	1 => "I"	
+  	}
+
+  	n = self
+
+  	word = ""
+  
+  	conversion.each do |integer, letter|
+		result = n/integer 
+			if result > 0 
+				word << letter * result
+					n -= integer * result
+			end
+  	end
+  	word
+	end
+
 end
+

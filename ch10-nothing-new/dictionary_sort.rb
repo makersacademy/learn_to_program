11e681757acbc571b1d76b4cdf96d619fda80f55
Darrenf <darrenf54321@gmail.com>
2016-04-03 17:11:35 +0100

    Dic. sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,16 +1,9 @@
 def dictionary_sort arr
-  # your code here
+  return arr if arr.length <= 1
 
-return arr if arr.length <= 1
+  middle = arr.pop
+  less = arr.select{|x| x.downcase < middle.downcase}
+  more = arr.select{|x| x.downcase >= middle.downcase}
 
-middle = arr.pop
-
-less = arr.select{|x| x.downcase < middle.downcase}
-
-more = arr.select{|x| x.downcase >= middle.downcase}
-
-dictionary_sort(less) + [middle] + dictionary_sort(more)
-
+  dictionary_sort(less) + [middle] + dictionary_sort(more)
 end
-
-puts(dictionary_sort(words).join(' '))

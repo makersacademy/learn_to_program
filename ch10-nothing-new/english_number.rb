360336a4b336e27dbb0748a48f2df72f36892eb0
Darrenf <darrenf54321@gmail.com>
2016-04-03 17:27:43 +0100

    english number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,120 +1,100 @@
 # your code here
 def english_number number
-
-  if number < 0
-  return 'The number should not be negative.'
+  if number < 0 # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
   end
-
   if number == 0
-  return 'zero'
+    return 'zero'
   end
 
-num_string = ''
+  num_string = ''
 
-ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
 
-tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-
-teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-
-left = number
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
 
 
-write = left/1000000000000
-left = left - write*1000000000000
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
 
-  if write > 0
+  zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
 
-  trillions = english_number write
-  num_string = num_string + trillions + ' trillion'
 
-    if left > 0
-    num_string = num_string + ' '
+  left = number
+
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base
+
+    if write > 0
+
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+
+        num_string = num_string + ' '
+      end
     end
-end
+  end
 
-
-write = left/1000000000
-left = left - write*1000000000
-
-  if write > 0
-
-  billions = english_number write
-  num_string = num_string + billions + ' billion'
-
-    if left > 0
-    num_string = num_string + ' '
-    end
-end
-
-
-
-write = left/1000000
-left = left - write*1000000
+  write = left/10
+  left = left - write*10
 
   if write > 0
-
-  millions = english_number write
-  num_string = num_string + millions + ' million'
-
-    if left > 0
-    num_string = num_string + ' '
-    end
-end
-
-
-write = left/1000
-left = left - write*1000
-
-  if write > 0
-
-  thousands = english_number write
-  num_string = num_string + thousands + ' thousand'
-
-    if left > 0
-    num_string = num_string + ' '
-    end
-end
-
-write = left/100
-left = left - write*100
-
-
-  if write > 0
-
-  hundreds = english_number write
-  num_string = num_string + hundreds + ' hundred'
-
-    if left > 0
-    num_string = num_string + ' '
-    end
-end
-
-write = left/10
-left = left - write*10
-
-  if write > 0
-
     if ((write == 1) and (left > 0))
-    num_string = num_string + teenagers[left-1]
-    left = 0
 
+      num_string = num_string + teenagers[left-1]
+
+      left = 0
     else
-    num_string = num_string + tens_place[write-1]
+      num_string = num_string + tens_place[write-1]
+
     end
 
-  if left > 0
-  num_string = num_string + '-'
-  end
-end
+    if left > 0
 
-write = left
-left = 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left
+  left = 0
 
   if write > 0
-  num_string = num_string + ones_place[write-1]
+    num_string = num_string + ones_place[write-1]
+
   end
 
-
-num_string
+  num_string
 end

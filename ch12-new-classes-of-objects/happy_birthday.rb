3d54e726a5861530e57a1e9360a603e3af023f0e
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-04-03 18:40:19 +0100

    Chapter 12 happy_birthday.rb added

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,22 @@
-# your code here
+def happy_birthday
+  puts "Please enter your year of birth:"
+  year = gets.chomp.to_i
+
+  puts "Now the month (as a number please):"
+  month = gets.chomp.to_i
+
+  puts "Finally please tell me the day:"
+  day = gets.chomp.to_i
+
+  birthdate = Time.local(year, month, day)
+  age = 1
+
+  while Time.local(year + age, month, day) <= Time.new
+    puts 'SPANK!'
+    age = age + 1
+  end
+
+end
+
+happy_birthday
+

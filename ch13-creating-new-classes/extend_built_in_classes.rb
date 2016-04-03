70dcb5cecebf0dbd9956628e32c46cd396f63d66
Darrenf <darrenf54321@gmail.com>
2016-04-03 17:18:54 +0100

    extend classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,90 +1,21 @@
 #shamlessly copied I'm afraid!
 
 class Integer
-
-def shuffle
-
-arr = self
-
-shuf = []
-
-while arr.length > 0
-
-rand_index = rand(arr.length)
-
-
-curr_index = 0
-
-new_arr = []
-
-arr.each do |item|
-
-if curr_index == rand_index
-
-shuf.push item
-
-else
-
-new_arr.push item
-
-end
-
-curr_index = curr_index + 1
-
-end
-
-
-arr = new_arr
-
-end
-
-shuf
-
-end
-
-end
-
-class Integer
-
-def factorial
-
-if self <= 1
-
-1
-
-else
-
-self * (self-1).factorial
-
-end
-
-end
-
-def to_roman
-
-Prepared exclusively for Nikesh Ashar report erratum • discuss
-
-# I chose old-school roman numerals just to save space.
-
-roman = ''
-
-roman = roman + 'M' * (self / 1000)
-
-roman = roman + 'D' * (self % 1000 / 500)
-
-roman = roman + 'C' * (self % 500 / 100)
-
-roman = roman + 'L' * (self % 100 / 50)
-
-roman = roman + 'X' * (self % 50 / 10)
-
-roman = roman + 'V' * (self % 10 / 5)
-
-roman = roman + 'I' * (self % 5 / 1)
-
-roman
-
-end
-
-end
+  def factorial
+    raise 'Must not use negative integer' if self < 0
+    (self <= 1) ? 1 : self * (self-1).factorial
+  end
+  def to_roman
+    # I chose old-school roman numerals just to save space.
+    raise 'Must use positive integer' if self <= 0
+    roman = ''
+    roman << 'M' * (self / 1000)
+    roman << 'D' * (self % 1000 / 500)
+    roman << 'C' * (self % 500 / 100)
+    roman << 'L' * (self % 100 / 50)
+    roman << 'X' * (self % 50 / 10)
+    roman << 'V' * (self % 10 / 5)
+    roman << 'I' * (self % 5 / 1)
+    roman
+  end
 end

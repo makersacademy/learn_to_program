10d9c4576e09d33dca10cf020a97b5d9992626f2
Robin Heathcote <robin.heathcote@icloud.com>
2016-04-03 15:22:20 +0100

    Solved Ch14 grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
 def grandfather_clock &block
-  # your code here
+current_time = Time.new.hour
+  if current_time >= 13
+    current_time = current_time - 12
+  end
+  if current_time == 0
+    hour = 12
+  end
+
+  current_time.times do
+    block.call
+  end
+  end
+  grandfather_clock do
+    puts 'DONG!'
 end

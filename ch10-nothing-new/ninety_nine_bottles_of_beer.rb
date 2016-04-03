90bbead732244b51606e308e3c7e1a318a846ecb
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-03 15:24:59 +0100

    correction ninety_nine_bottles_of_beer.rb

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -9,7 +9,7 @@ class BeerSong
     return 'zero'
     end
 
-  words = ''
+  num_string = ''
 
   ones  = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
   tens  = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]

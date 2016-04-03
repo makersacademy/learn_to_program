57695563ebb15e8641e3cc1a709db5c82c406389
rahulrama <rahulrama@gmail.com>
2016-04-03 19:13:43 +0100

    Chapter 10: Challenge 5

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,10 @@
 def sort arr
   # your code here
+  return arr if arr.length <= 1
+  
+  middle = arr.pop
+  less = arr.select{|x| x < middle}
+  more = arr.select{|x| x >= middle}
+
+  sort(less) + [middle] + sort(more)
 end

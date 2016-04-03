619d11f8e6d7af1d76429ec94accba85b9d53fda
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-04-03 21:36:00 +0100

    Chapter 14 even better profiling added

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,31 @@
 def profile block_description, &block
-  # your code here
+
+  toggle = true
+
+  if toggle
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+profile '25000 doublings' do
+  number = 1
+  25000.times do
+    number = number + number
+  end
+
+  puts "#{number.to_s.length} digits"
+  # That's the number of digits in this HUGE number.
+end
+
+profile 'count to a million' do
+  number = 0
+  1000000.times do
+    number = number + 1
+  end
+end
+

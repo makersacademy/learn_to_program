5ec7cc95b608ab59bb0c555c44d87ff2362176e8
Jackrim1 <jriminton@gmail.com>
2016-04-03 11:25:00 +0100

    better program done

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
+$indent = 0
 def log desc, &block
-  # your code here
+  spaces = '   '*$indent
+  puts 'Beginning ' + desc + '...'
+  $indent = $indent + 1
+call = block.call
+
+  puts '...' + desc + ' finished, returning: ' + call.to_s
+
+end
+
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      puts 'lots of love'
+    end
+    42
+  end
+
+log 'yet another block' do
+  puts 'I like Indian food!'
+end
+
+'0' == 0
 end

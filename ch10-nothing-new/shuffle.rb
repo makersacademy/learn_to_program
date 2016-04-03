44df69d025c1e6c8a7483755842b8556b373b1fc
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-04-03 16:11:51 +0100

    Chapter 10 shuffle.rb added

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,12 @@
 def shuffle arr
-  # your code here
+  shuffled = []
+
+  while arr.length > 0 do
+    shuffled.push(arr.delete_at(rand(arr.length)))
+  end
+
+  shuffled
+
 end
+
+puts shuffle [1,2,3,4,5,6,7,8,9,10]

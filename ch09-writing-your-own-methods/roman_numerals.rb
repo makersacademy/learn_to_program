3c42ca431b16f213184e7225bf0f6ec2a3b6426a
Darrenf <darrenf54321@gmail.com>
2016-04-03 17:32:11 +0100

    Roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,39 +1,35 @@
 #I admit to needing to copy this from the book after spending quite some time trying to crack the problem....
 
 def roman_numeral num
+  raise 'Must use positive integer' if num <= 0
 
-    thous = (num / 1000)
-    hunds = (num % 1000 / 100)
-    tens = (num % 100 / 10)
-    ones = (num % 10 )
+  digit_vals = [['I', 5, 1],
+                ['V', 10, 5],
+                ['X', 50, 10],
+                ['L', 100, 50],
+                ['C', 500, 100],
+                ['D', 1000, 500],
+                ['M', nil, 1000]]
 
-roman = 'M' * thous
+  roman = ''
+  remaining = nil
 
-    if hunds == 9
-    roman = roman + 'CM'
-    elsif hunds == 4
-    roman = roman + 'CD'
-    else
-    roman = roman + 'D' * (num % 1000 / 500)
-    roman = roman + 'C' * (num % 500 / 100)
+  # Build string "roman" in reverse.
+  build_rev = proc do |l,m,n|
+    num_l = m ? (num % m / n) : (num / n)
+    full  = m && (num_l == (m/n - 1))
+
+  if full && (num_l>1 || remaining)
+    # must carry
+    remaining ||= l # carry l if not already carrying
+  else
+    if remaining
+      roman << l + remaining
+      remaining = nil end
+      roman << l * num_l
     end
+  end
+  digit_vals.each {|l,m,n| build_rev[l,m,n]}
 
-    if tens == 9
-    roman = roman + 'XC'
-    elsif tens == 4
-    roman = roman + 'XL'
-    else
-    roman = roman + 'L' * (num % 100 / 50)
-    roman = roman + 'X' * (num % 50 / 10)
-    end
-
-    if ones == 9
-    roman = roman + 'IX'
-    elsif ones == 4
-    roman = roman + 'IV'
-    else
-    roman = roman + 'V' * (num % 10 / 5)
-    roman = roman + 'I' * (num % 5 / 1)
-    end
-roman
+  roman.reverse
 end

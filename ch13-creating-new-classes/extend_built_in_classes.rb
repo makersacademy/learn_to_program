ff117f1d661b94bde05ae4faad5436671dca8910
Jackrim1 <jriminton@gmail.com>
2016-04-03 16:34:49 +0100

    extending built in - changes made

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,6 +1,4 @@
 class Array
-
-
 def shuffle array
 	final_array = []
 	while array.length > 0
@@ -20,6 +18,29 @@ def shuffle array
 array = holding_pen
 end
 final_array
+end
+end
+
+class Integer
+	def factorial
+		if self <= 1
+			1
+		else
+			self * (self-1).factorial
+		end
+	end
+
+	def to_roman
+conv = ''
+conv = conv + 'M' * (self / 1000)
+conv = conv + 'D' * (self % 1000 / 500)
+conv = conv + 'C' * (self % 500 / 100)
+conv = conv + 'L' * (self % 100 / 50)
+conv = conv + 'X' * (self % 50 / 10)
+conv = conv + 'V' * (self % 10 / 5)
+conv = conv + 'I' * (self % 5 / 1)
+return conv
 
 end
 end
+

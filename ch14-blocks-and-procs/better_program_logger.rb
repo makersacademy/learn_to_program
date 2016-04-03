d2c4a316ee316c27baa3787e7d233d0a97857187
sitypop <sityemilyshah@gmail.com>
2016-04-03 14:17:07 +0100

    Added ch14/better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
-def log desc, &block
-  # your code here
+$block_indent = 0
+
+def better_log(desc, &block)
+  puts " "*$block_indent + "Beginning \"#{desc}\"..."
+  $block_indent += 1
+  result = block.call
+  $block_indent -= 1
+  puts " "*$block_indent + "...\"#{desc}\" finished, returning: #{result}"
+end 
+
+better_log "outer block" do 
+	better_log "some little block" do
+		better_log "teeny-tiny block" do
+			"lots of love"
+		end
+		42
+	end
+
+	better_log "yet another block" do
+		"I love Indian food!"
+	end
+
+ true
 end
+	

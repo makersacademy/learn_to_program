359d6a4fd06b7d4e10b67c88c7558c6e60df2ab5
DXTime <ibbakalov@gmail.com>
2016-04-03 19:51:13 +0100

    Chapter 9 Ex. 2 & 3

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,12 @@
 def old_roman_numeral num
-  # your code here
+  roman_bank = { 1000=>"M", 500=>"D", 100=>"C", 50=>"L", 10=>"X", 5=>"V", 1=> "I" }
+
+  # Check for thousands
+  roman = ""
+
+  roman_bank.each do |value, letter|
+  	roman << letter*(num / value)
+  	num = num % value
+  end
+  return roman
 end

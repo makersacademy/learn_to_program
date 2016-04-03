48bf7ad56d4c198bb416cfd66d3fd6abf113c93d
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-04-03 21:44:50 +0100

    Chapter 14 grandfather_clock.rb added

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,10 @@
 def grandfather_clock &block
   # your code here
+  i = 0
+  loops = Time.new.hour < 13 ? Time.new.hour : Time.new.hour - 12
+  while i < loops do
+    block.call
+    i += 1
+  end
 end
+

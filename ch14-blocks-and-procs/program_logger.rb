26d5ef59b71aa67ac86c4332c0744e6a49c3245f
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-03 14:32:22 +0100

    program logger and better program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,21 @@
 def log desc, &block
   # your code here
+
+
+
+puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+
+
+
+
+
 end
+log 'outer block' do
+  (15*19).to_s + "hello"
+  log 'some little block' do
+    1**1 + 2**2
+  end
+  (15*19).to_s + "hello" #need to put outer block to return here
+  end

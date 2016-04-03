4e7cac02f26720af164ba2a000a939fc1803dca0
Emilio <emsmnndz@gmail.com>
2016-04-03 09:44:51 +0100

    better_program_logger.rb Solved

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,9 @@
-def log desc, &block
-  # your code here
+$space = 0
+def better_log desc, &block
+	before = " " * $space
+	puts before + "Beginning #{desc.inspect}..."
+	$space += 1
+  result = block.call
+  	$space -= 1
+  puts  before + "...#{desc.inspect} finished, returning: #{result}"
 end

d9385c8d98410ddc2ef728ea95bce5d488b019da
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-03 14:43:09 +0100

    program logger changed to pass rspec v2

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -12,3 +12,14 @@ puts 'Beginning "' + desc + '"...'
 
 
 end
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
+  end
+
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
+end

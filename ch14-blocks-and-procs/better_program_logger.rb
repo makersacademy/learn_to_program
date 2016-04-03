e4e8c645bb21b9ed97a10b615c33fdf8f3544c3e
Darrenf <darrenf54321@gmail.com>
2016-04-03 18:12:33 +0100

    better prog

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,17 +1,24 @@
 $logger_depth = 0
 
 def log desc, &block
+  prefix = ' '*$logger_depth
+  puts prefix+"Beginning #{desc.inspect}..."
+  $logger_depth += 1
+  result = block[]
+  $logger_depth -= 1
+  puts prefix+"...#{desc.inspect} finished, returning: #{result}"
+end
 
-prefix = ' '*$logger_depth
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lOtS oF lOVe'.downcase
+    end
+    7 * 3 * 2
+  end
 
-puts prefix+"Beginning #{desc.inspect}..."
-
-$logger_depth += 1
-
-result = block[]
-
-$logger_depth -= 1
-
-puts prefix+"...#{desc.inspect} finished, returning: #{result}"
-
+  log 'yet another block' do
+    '!doof naidnI evol I'.reverse
+  end
+  '0' == "0"
 end

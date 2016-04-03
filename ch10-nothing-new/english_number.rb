af7b1002c133a1f9e4b1772e27b9066f146dc6b7
Jackrim1 <jriminton@gmail.com>
2016-04-03 13:11:31 +0100

    changed to pass the test

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,60 +1,63 @@
-def eng_num number
-  num = ''
+def english_number number
+  if number < 0
+    return 'Please enter a positive number'
+  end
   if number == 0
-    num = 'zero'
-  else
+    return 'zero'
+  end
+num = ''
   ones = ['one',     'two',       'three',    'four',     'five',
                'six',     'seven',     'eight',    'nine']
   tens = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
                'sixty',   'seventy',   'eighty',   'ninety']
   teens = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
                'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion',18],
+              ['sextillion',21],
+              ['septillion',24],
+              ['octillion',27],
+              ['nonillion',30],
+              ['decillion',33],
+              ['undecillion',36],
+              ['duodecillion',39],
+              ['tredecillion',42],
+              ['quattuordecillion',45],
+              ['quindecillion',48],
+              ['sexdecillion',51],
+              ['septendecillion',54],
+              ['octodecillion',57],
+              ['novemdecillion',60],
+              ['vigintillion',63],
+              ['googol',100]]
+
+
   first  = number
-  
-  second = first/1000000000
-  first = first - second*1000000000
-  if second > 0
-  	billions = eng_num second
-  	num = num + billions + ' billion'
-  	if first > 0
-  		num = num + ' '
-  	end
-  end
-  
-  second = first/1000000
-  first = first - second*1000000
-  if second > 0
-  	millions = eng_num second
-  	num = num + millions + ' million'
-  	if first > 0
-  		num = num + ' '
-  	end
-  end
-  
-  second = first/1000
-  first = first - second*1000
-  
-  if second > 0
-  	thousands = eng_num second
-  	num = num + thousands + ' thousand'
-  	if first > 0
-  		num = num + ' '
-  	end
-  end
- 
-  second = first/100
-  first = first - second*100
+while zillions.length > 0
+  zil_pair = zillions.pop
+  zil_name = zil_pair[0]
+  zil_base = 10 ** zil_pair[1]
+  second = first/zil_base
+  first = first - second*zil_base
 
   if second > 0
-    hundreds = eng_num second
-    num = num + hundreds + ' hundred'
+    prefix = english_number second
+    num = num + prefix + ' ' + zil_name
     if first > 0
-      num = num + ' '
-    end
+    num = num + ' '
   end
+end
+end
 
-  second = first/10
-  first  = first - second*10
+
+second = first/10
+first  = first - second*10
 
   if second > 0
     if ((second == 1) and (first > 0))
@@ -72,6 +75,7 @@ def eng_num number
   if second > 0
     num = num + ones[second-1]
   end
-  end
+
+
   num
 end

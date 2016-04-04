c42507294b0e68782efb70cb546b8bfcbeafba93
Lucy <l.l.c.clarke@gmail.com>
2016-04-04 10:24:49 +0200

    oranges

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,3 +1,53 @@
+
 class OrangeTree
-  # your code here
+
+	def initialize
+		@age = 0
+		@height = 0
+		@numberoranges = 0
+	end
+
+	def height
+		puts "The tree is #{@height} feet tall."
+	end
+
+	def count_the_oranges
+		puts "The tree has #{@numberoranges} oranges."
+	end
+
+	def pick_an_orange
+		if @numberoranges < 1
+			puts 'There are no oranges to pick.'
+		else
+			puts 'You pick an orange from the tree.'
+				@numberoranges = @numberoranges - 1
+		end
+	end
+
+	def one_year_passes
+		puts 'One year passes'
+		@age = @age + 1
+		@height = @height + 1
+		@numberoranges = 0
+
+		if dead?
+			puts 'The Orange Tree dies'
+			exit
+		end 
+
+		if @age > 2
+			@numberoranges = @age*20
+			else
+			@numberoranges = 0
+		end
+
+	end
+
+	private
+
+		def dead?
+			@age > 5
+		end
+
 end
+

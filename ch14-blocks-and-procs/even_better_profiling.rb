ae75e1ef38f5e4c4c455ecd605abb33582e1e6ec
rahulrama <rahulrama@gmail.com>
2016-04-04 07:39:06 +0100

    Chapter 14: Challenge 1 updated

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,14 +1,13 @@
-$profiling_on = false
+
 def profile block_description, &block
   # your code here
-  if $profiling_on
+profiling_on = false
+  if profiling_on
     start_time = Time.new
     block.call
-
     duration = Time.new - start_time
     puts "#{block_description}: #{duration} seconds"
   else
     block.call
   end
-end
 end

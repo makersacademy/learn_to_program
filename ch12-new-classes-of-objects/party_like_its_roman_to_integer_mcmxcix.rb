c4a2a2e79950414989914c69c7a3e59e8fb5a247
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-04 08:27:01 +0100

    party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,39 @@
 def roman_to_integer roman
-  # your code here
-end
+		 result = 0
+		 singles = {"M" => 1000, "D" => 500, "C" => 100, "L"=> 50, "X" => 10, "V" => 5, "I" => 1}   
+
+		double = {"CM" => 900, "CD" => 400, "XC" => 90, "XL" => 40,  "IX" => 9, "IV" => 4}
+
+		roman = roman.upcase
+		something = roman.split("")
+
+		something.each do |key|
+			unless singles.include?(key)
+		  		puts "not valid roman numeral"
+			end
+		end
+			
+
+		while roman.length > 0
+			double.each do |key, value|
+				if roman.include?(key)
+		 			result += value
+		 			roman.slice!(key)
+			 	end
+		 	end
+		
+
+		 	singles.each do |key, value|
+		 		if roman.include?(key)
+		 			result += value
+		 			roman.slice!(key)
+				end 
+			
+			end 
+
+		end 
+	result
+ end
+
+ puts roman_to_integer("lxxxix")
+

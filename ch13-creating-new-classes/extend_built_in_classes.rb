eefe76d03a604ccf1f0c420870b06858305cbcf3
Lucy <l.l.c.clarke@gmail.com>
2016-04-04 18:23:34 +0200

    romans added to int class

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,36 @@
 class Integer
   # your code here
+
+  def to_roman
+  
+
+thous = self / 1000
+
+remain = self % 1000
+
+funfhunds = remain / 500
+
+remain = remain % 500
+
+hunds = remain / 100
+
+remain = remain % 100
+
+funfzig = remain /50
+
+remain = remain % 50
+
+zehn = remain /10
+
+remain = remain %10
+
+funf = remain / 5
+remain = remain % 5
+
+eins = remain
+
+
+return "#{"M" * thous}#{"D" * funfhunds}#{"C" * hunds}#{"L" * funfzig}#{"X" * zehn}#{"V" * funf}#{"I" * eins}" 
 end
+end
+

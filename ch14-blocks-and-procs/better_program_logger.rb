4143f0200d17afda135ad01ea30666f85138e7ae
Nick <nm.richards@yahoo.com>
2016-04-04 11:46:16 -0400

    Chapter 14 Complete

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,9 @@
-def log desc, &block
-  # your code here
+$indent = 0
+def better_log desc, &block
+  indentation = " "*$indent
+  puts indentation + "Beginning \"#{desc}\"..."
+  $indent += 1
+  result = block.call
+  $indent -= 1
+  puts indentation + "...\"#{desc}\" finished, returning: #{result.to_s}"
 end

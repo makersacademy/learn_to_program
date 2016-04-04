d52ebca9321111ddbf89f4543da9e01d6e5d5e6c
Richard Watkins <richardwatkins1@gmail.com>
2016-04-04 10:33:08 +0100

    Update ninety_nine_bottles_of_beer.rb

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,103 @@
-# your code here
+def english_number(number)
+  if number < 0 
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+  num_string = '' # This is the string we will return.
+
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
+
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
+
+
+  teenagers = ['eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen',
+               'seventeen', 'eighteen', 'nineteen']
+
+  english_num_expand = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
+
+  left = number
+
+  while english_num_expand.length > 0
+    num_ex = english_num_expand.pop
+    num_name = num_ex[0]
+    num_base = 10 ** num_ex[1]
+    write = left/num_base 
+    left = left - write*num_base 
+
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + 'num_name'
+
+      if left > 0
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+  write = left/10 
+  left = left - write*10 
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
+
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left 
+  left = 0 
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+
+  num_string
+end
+
+num_at_start = 900 
+num_now = num_at_start
+while num_now > 2
+  puts english_number(num_now).capitalize + ' bottles of beer on the wall, ' +
+    english_number(num_now) + ' bottles of beer!'
+  num_now = num_now - 1
+  puts 'Take one down, pass it around, ' +
+    english_number(num_now) + ' bottles of beer on the wall!'
+end
+
+

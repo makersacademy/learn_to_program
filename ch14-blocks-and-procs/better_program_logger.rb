f3ed21778606d337d832f5229f8ad02855c19ec1
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-04 07:03:38 +0100

    method name changed to match spec

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,5 +1,5 @@
 $indent = 0
-def log desc, &block
+def better_log desc, &block
 tab =  " " * $indent
 
 puts tab + 'Beginning "' + desc + '"...'
@@ -8,18 +8,18 @@ puts tab + 'Beginning "' + desc + '"...'
   $indent = $indent -=1
 puts tab + '..."' + desc + '" finished, returning: ' + result.to_s
 end
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
-      'lOtS oF lOVe'.downcase
-    end
+# log 'outer block' do
+#   log 'some little block' do
+#     log 'teeny-tiny block' do
+#       'lOtS oF lOVe'.downcase
+#     end
 
-    7 * 3 * 2
-  end
+#     7 * 3 * 2
+#   end
 
-  log 'yet another block' do
-    '!doof naidnI evol I'.reverse
-  end
+#   log 'yet another block' do
+#     '!doof naidnI evol I'.reverse
+#   end
 
-  '0' == "0"
-end
+#   '0' == "0"
+# end

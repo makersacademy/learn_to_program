835a9ee84b4ee3ab278e27862bcb5132798645f7
Lucy <l.l.c.clarke@gmail.com>
2016-04-04 18:52:04 +0200

    new and improved

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -2,12 +2,14 @@
 
 def profile block_description, &block
   # your code here
-profileon = false
+profileon = true
 if profileon == true
   start_time = Time.new
   block.call
   duration = Time.new - start_time
   puts "#{block_description}: #{duration} seconds"
+else 
+	block.call
 end
 
 end

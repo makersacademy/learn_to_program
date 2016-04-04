b8af053e1151d6a2f1ebddcbe7e9195ce1df9b44
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-04 06:19:28 +0100

    copied pines version to pass spec

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,17 +1,9 @@
 def log desc, &block
-  # your code here
-
-
-
-puts 'Beginning "' + desc + '"...'
-  result = block.call
-  puts '..."' + desc + '" finished, returning: ' + result.to_s
-
-
-
-
-
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
 end
+
 log 'outer block' do
   log 'some little block' do
     1**1 + 2**2

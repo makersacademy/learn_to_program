c585951c2c7ec170611301cea95c0594e4573098
DXTime <ibbakalov@gmail.com>
2016-04-04 04:27:29 +0100

    Chapter 11

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,60 @@
 def music_shuffle filenames
-  # your code here
+	music_files = Dir['**/*.mp3']
+
+	playlist = []
+	songs_num = music_files.length
+	count = 0
+	prev_album = ""
+
+
+	while songs_num > count
+		track_num = rand(0..songs_num-1)
+		song_name = music_files[track_num].split'/'
+		current_album = song_name[2]
+		if current_album != prev_album
+			if music_files[track_num] != "" then
+				playlist << music_files.pop[track_num]
+				music_files[track_num] = ""
+				count += 1
+			end
+			prev_album = current_album
+		end
+	end
+
+	# Create playlist
+	File.open 'playlist.m3u', 'w' do |f|
+		f.write playlist
+	end
+end
+
+
+
+
+
+
+  len = filenames.length
+  2.times do
+      l_idx = 0
+      r_idx = len/2
+      shuf = []
+      while shuf.length < len
+      if shuf.length%2 == 0
+        shuf.push(filenames[r_idx])
+          r_idx = r_idx + 1
+      else
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+    filenames = shuf
+  end
+  arr = []
+  cut = rand(len)
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+  arr
 end
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,39 @@
-# your code here
+# This is where she stores her pictures before
+# she gets her YAML on and moves them to the server.
+# Just for my own convenience, I'll go there now.
+Dir.chdir '1'
+# First we find all of the pictures to be moved.
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files: "
+# This will be our counter. We'll start at 1 today,
+# though normally I like to count from 0.
+pic_number = 1
+pic_names.each do |name|
+print '.' # This is our "progress bar".
+new_name = if pic_number < 10
+"#{batch_name}0#{pic_number}.jpg"
+else
+"#{batch_name}#{pic_number}.jpg"
+end
+# This renames the picture, but since "name"
+# has a big long path on it, and "new_name"
+# doesn't, it also moves the file to the
+# current working directory, which is now
+# Katy's PictureInbox folder.
+# Since it's a *move*, this effectively
+# downloads and deletes the originals.
+# And since this is a memory card, not a
+# hard drive, each of these takes a second
+# or so; hence, the little dots let her
+# know that my program didn't hose her machine.
+# (Some marriage advice from your favorite
+# author/programmer: it's all about the
+# little things.)
+# Now where were we? Oh, yeah...
+File.rename name, new_name
+# Finally, we increment the counter.
+pic_number = pic_number + 1
+end

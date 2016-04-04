d0ee10faa96ccb161fdb206292a7e356f9dc767a
Nick <nm.richards@yahoo.com>
2016-04-04 19:34:06 +0430

    Ch13 complete

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,128 @@
-# your code here
+class Dragon
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 10
+    @stuff_in_intestine = 0
+
+    puts "#{@name} is born."
+    interactive
+  end
+
+  def interactive
+    puts "What would you like to do to #{@name}?"
+    puts "Feed"
+    puts "Walk"
+    puts "Rock"
+    puts "Toss"
+    puts "Put to bed"
+    puts "Exit"
+    answer = gets.chomp
+
+    case answer
+    when "Feed"
+      feed
+    when "Walk"
+      walk
+    when "Rock"
+      rock
+    when "Toss"
+      toss
+    when "Put to bed"
+      put_to_bed
+    when "Exit"
+      exit
+    end
+    puts
+    interactive
+  end
+  def feed
+    puts "You fed #{@name}."
+    @stuff_in_belly = 10
+    passage_of_time
+  end
+
+  def walk
+    puts "You walked #{@name}."
+    @stuff_in_intestine = 0
+    passage_of_time
+  end
+
+  def put_to_bed
+    puts "You put #{@name} to bed."
+    @asleep = true
+    3.times do
+      if @asleep
+        passage_of_time
+      end
+      if @asleep
+        puts "#{@name} snores, filling the room with smoke."
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts "#{@name} wakes up slowly."
+    end
+  end
+
+  def toss
+    puts "You toss #{@name} up into the air."
+    puts "He giggles, which singes your eyebrows."
+    passage_of_time
+  end
+
+  def rock
+    puts "You rock #{@name} gently."
+    @asleep = true
+    puts "He briefly dozes off..."
+    passage_of_time
+    if @asleep
+      @asleep = false
+      puts "...but wakes when you stop."
+    end
+  end
+
+  private
+
+  def hungry?
+    @stuff_in_belly <= 2
+  end
+
+  def poopy?
+    @stuff_in_intestine >= 8
+  end
+
+  def passage_of_time
+    if @stuff_in_belly > 0
+      @stuff_in_belly = @stuff_in_belly - 1
+      @stuff_in_intestine = @stuff_in_intestine + 1
+    else
+      if @asleep
+        @asleep = false
+        puts "He wakes suddenly"
+      end
+      puts "#{@name} is starving! In desparation, he ate YOU!"
+      exit
+    end
+    if @stuff_in_intestine >= 10
+      @stuff_in_intestine = 0
+      puts "Whoops! #{@name} had an accident..."
+    end
+
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly"
+      end
+      puts "#{@name}'s stomach grumbles..."
+    end
+
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly!"
+      end
+      puts "#{@name} does the potty dance..."
+    end
+  end
+end

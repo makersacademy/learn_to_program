a5e0509ecf2c5c3d4107964a9bbf667f410dba78
Richard Watkins <richardwatkins1@gmail.com>
2016-04-04 10:09:31 +0100

    Update dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,9 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arr)
+return arr if arr.length <= 1
+  mid = arr.pop
+  smallest = arr.select { |x| x < mid.downcase }
+  biggest = arr.select { |x| x >= mid.downcase }
+  dictionary_sort(smallest) + [mid] + dictionary_sort(biggest)
 end
+
+# Initially kept returning ["A", "can", "can", "feel", "singing.", "like"] then realised I needed to add dictionar_sort(smallest) and biggest.

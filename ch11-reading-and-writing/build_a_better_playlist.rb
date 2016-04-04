66a8a76c74645de6f8777e980203c06f023e2d2f
Jack Hardy <jackhardy1@gmail.com>
2016-04-04 09:01:14 +0100

    Build a better playlist updated

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,36 +1,49 @@
 def music_shuffle filenames
+  songs_and_paths = filenames.map do |s|
+    [s, s.split('/')]
+  end
 
-filenames = filenames.sort
-len = filenames.length
+  tree = {:root => []}
 
-2.times do
-  left_index = 0
-  right_index = len/2
-  shuffled = []
-
-  while shuffled.length < len
-    if shuffled.length % 2 == 0
-      shuffled << filenames[right_index]
-      right_index = right_index + 1
+  insert_into_tree = proc do |branch, song, path|
+    if path.length == 0
+      branch[:root] << song
     else
-      shuffled << filenames[left_index]
-end
-end
+      sub_branch = path[0]
+      path.shift
 
-filenames = shuffled
+      if !branch[sub_branch]
+        branch[sub_branch] = {:root => []}
+      end
 
-array = []
-cut = rand(len)
-index = 0
+      insert_into_tree[branch[sub_branch], song, path]
+    end
+  end
 
-while index < len
-  array << filenames[(index+cut)%len]
-  index = index + 1
+  songs_and_paths.each{|sp| insert_into_tree[tree, *sp]}
+
+  shuffle_branch = proc do |branch|
+    shuffled_subs = []
+
+    branch.each do |key, unshuffled|
+      shuffled_subs << if key == :root
+      unshuffled 
+      else
+        shuffle_branch[unshuffled]
+      end
+    end
+
+    weighted_songs = []
+
+    shuffled_subs.each do |shuffled_songs|
+      shuffled_songs.each_with_index do |song, idx|
+        num = shuffled_songs.length.to_f
+        weight = (idx + rand) / num
+        weighted_songs << [song, weight]
+      end
+    end
+
+    weighted_songs.sort_by{|s,v| v}.map{|s,v| s}
+  end
+  shuffle_branch[tree]
 end
-array
-end
-end
-
-songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
-
-puts (music_shuffle(songs))

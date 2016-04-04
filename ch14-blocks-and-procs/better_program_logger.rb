a1409672445d1104265432c95bd497488e851cb9
rahulrama <rahulrama@gmail.com>
2016-04-04 07:26:49 +0100

    Chapter 14: Challenge 4

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,27 @@
 def log desc, &block
   # your code here
+  $logger_depth = 0
+
+def log desc, &block
+  prefix = ' '*$logger_depth
+  puts prefix + "Beginning #{desc.inspect}..."
+  $logger_depth += 1
+  result = block.call
+  $logger_depth -= 1
+  puts prefix+"...#{desc.inspect} finished, returning: #{result}"
+end
+
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lots of love'
+    end
+    42
+  end
+
+  log 'yet another block' do
+    'I love Indian food!'
+  end
+  false
+end
 end

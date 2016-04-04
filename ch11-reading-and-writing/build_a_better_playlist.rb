61efc3f5d361b860a90a70538a4b32b64197deee
Jack Hardy <jackhardy1@gmail.com>
2016-04-04 08:13:17 +0100

    Chapter 11 done

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,36 @@
 def music_shuffle filenames
-  # your code here
+
+filenames = filenames.sort
+len = filenames.length
+
+2.times do
+  left_index = 0
+  right_index = len/2
+  shuffled = []
+
+  while shuffled.length < len
+    if shuffled.length % 2 == 0
+      shuffled << filenames[right_index]
+      right_index = right_index + 1
+    else
+      shuffled << filenames[left_index]
 end
+end
+
+filenames = shuffled
+
+array = []
+cut = rand(len)
+index = 0
+
+while index < len
+  array << filenames[(index+cut)%len]
+  index = index + 1
+end
+array
+end
+end
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd', 'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+
+puts (music_shuffle(songs))
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
-# your code here
+def music_shuffle arr
+arr.sort_by{rand}
+end
+
+Dir.chdir('/Users/JackHardy/Music/iTunes/iTunes Media/Music/Coldplay/Mylo Xyloto')
+playlist = Dir['*.m4a']
+shuffled_playlist = music_shuffle(playlist)
+filename = "shuffled_coldplay_playlist.m3u"
+File.open filename, 'w' do |f|
+shuffled_playlist.each{|title| f.write title + "\n"}
+end

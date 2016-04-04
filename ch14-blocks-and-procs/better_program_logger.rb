349cfbea7ecef8a537fd6b83011e9a3dfd03aac4
rahulrama <rahulrama@gmail.com>
2016-04-04 08:12:21 +0100

    Challenges updated to pass test

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -2,15 +2,11 @@ $logger_depth = 0
 
 def log desc, &block
   prefix = ' '*$logger_depth
-
-  puts prefix + 'Beginning "' + desc + '"...'
-
-  $logger_depth = $logger_depth + 1
-
-  result = block.call
-
-  $logger_depth = $logger_depth - 1
-  puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
+  puts prefix+"Beginning #{desc.inspect}..."
+  $logger_depth += 1
+  result = block[]
+  $logger_depth -= 1
+  puts prefix+"...#{desc.inspect} finished, returning: #{result}"
 end
 
 log 'outer block' do
@@ -18,13 +14,11 @@ log 'outer block' do
     log 'teeny-tiny block' do
       'lOtS oF lOVe'.downcase
     end
-
     7 * 3 * 2
   end
 
   log 'yet another block' do
     '!doof naidnI evol I'.reverse
   end
-
   '0' == "0"
 end

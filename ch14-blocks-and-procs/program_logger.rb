9c4fd2a2e8ecbfbd484d1cb1baaddc7fc3dfb816
rahulrama <rahulrama@gmail.com>
2016-04-04 07:23:20 +0100

    Chapter 14: Challenge 3

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,20 @@
 def log desc, &block
   # your code here
+  def log desc, &block
+  puts "Beginning #{desc.inspect}..."
+  result = block[]
+  puts "...#{desc.inspect} finished, returning: #{result}"
+end
+
+log 'outer block' do
+  log 'some little block' do
+    5
+  end
+
+  log 'yet another block' do
+    'I like Thai food!'
+  end
+
+  false
+end
 end

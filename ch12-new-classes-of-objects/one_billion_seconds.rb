1c615a03a21acf10cfbeeb7c4aba6cc1b56fda86
DXTime <ibbakalov@gmail.com>
2016-04-04 05:19:15 +0100

    Chapter 12

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,4 @@
-# your code here
+b_day = (Time.new) - Time.gm(1994, 1, 30)
+p b_day.to_i
+
+p turning_ancient = Time.at(((10 ** 9) - b_day.to_i) + Time.new.to_i)
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,16 @@
 def roman_to_integer roman
-  # your code here
+	roman_bank = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC",
+	 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I", }
+
+	 output = 0
+
+	 for value, letter in roman_bank
+	 	while roman.index(letter) == 0
+	 		output += value
+	 		roman.slice!(letter)
+	 	end
+	 end
+	 output
 end
+
+p roman_to_integer "MCMXCIV"

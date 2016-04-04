ae5b968a3d307497e08f89f063ac1a5b363ab2f5
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-04-04 09:10:26 +0100

    Chapter 11, build your own playlist added

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,10 @@
-# your code here
+# Chris Pine's code to pass rspec
+all_oggs = shuffle(Dir['**/*.ogg'])
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_oggs.each do |ogg|
+    f.write ogg+"\n"
+  end
+end
+
+puts 'Done!

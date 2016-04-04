afddc84406a45fb387f7a83a7f71615e8d7e00ee
Richard Watkins <richardwatkins1@gmail.com>
2016-04-04 09:40:03 +0100

    Update sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,10 @@
-def sort arr
-  # your code here
+def sort(arr)
+return arr if arr.length <= 1
+  mid = arr.pop
+  smallest = arr.select { |x| x < mid }
+  biggest = arr.select { |x| x >= mid }
+  sort(smallest) + [mid] + (biggest)
 end
+
+# Had some problems trying to use if arr.length <= 1 return at first but then realised it was returning nil if it was > 1
+

137fa20845415322bf511b75518a47e82e2a1f14
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-04 09:04:42 +0100

    better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,26 @@
-def log desc, &block
-  # your code here
+$indent = 0
+
+def better_log desc, &block
+  nesting = ' '*$indent
+  puts "#{nesting}Beginning \"#{desc}\"..."
+    $indent += 1
+  result = block.call
+    $indent -= 1
+  puts "#{nesting}...\"#{desc}\" finished, returning: #{result.to_s}"
+end
+
+
+
+better_log 'outer_block' do
+  better_log 'some little block' do
+    better_log 'teeny-tiny block' do
+      'lOts oF lOVE'.downcase
+    end
+    7 * 3 * 2
+  end
+
+  better_log 'another block' do
+    'ones upon a time!'.capitalize
+  end
+  '0' == "0"
 end

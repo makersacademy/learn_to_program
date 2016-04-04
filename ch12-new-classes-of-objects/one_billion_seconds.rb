87eeacc71d8fc8b6e273d932f546635a9b6a0a36
Jack Hardy <jackhardy1@gmail.com>
2016-04-04 08:15:46 +0100

    Chapter 12 done

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,8 @@
-# your code here
+def one_billion_seconds
+puts "When is your birthday? - (in format 'Year,Month,Day')"
+birthday = gets.chomp
+one_billion_second_birthday = Time.new(birthday)+(10**9)
+return "your billion second birthday is on #{one_billion_second_birthday}"
+end
+
+#works

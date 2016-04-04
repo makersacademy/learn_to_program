1f855f31d922fa5d3ce433c51245d17ea7d15934
Jack Hardy <jackhardy1@gmail.com>
2016-04-04 08:18:33 +0100

    Chapter 14 done

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,23 @@
-def log desc, &block
-  # your code here
+$depth = 0
+def better_log desc, &block
+  prefix = ' '*$depth
+  puts prefix + 'Beginning "'+ desc + '"...'
+  $depth = $depth + 1
+  result = block.call
+$depth = $depth - 1
+puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
 end
+
+better_log 'outer block' do
+better_log 'some little block' do
+better_log 'teeny-tiny block' do
+           'lOtS oF lOVe'.downcase
+         end
+         7 * 3 * 2
+       end
+
+       better_log 'yet another block' do
+         '!doof naidnI evol I'.reverse
+       end
+       '0' == "0"
+     end

a2e300221d8f1e43a74899d430e7ae4e30facf22
MariaRomero <fantasysystems@gmail.com>
2016-04-05 20:59:47 +0100

    finished dictionary sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,12 @@
-def sort arr
-  # your code here
+def sort arr, new_arr=[]
+	if arr.empty?
+		return new_arr
+	end
+	min_value = arr.min
+	index_of_min = arr.index(min_value)
+	new_arr.push(min_value)
+	arr.delete_at(index_of_min)
+ 	sort arr, new_arr
 end
+
+

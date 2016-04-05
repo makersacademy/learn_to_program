2599cbff98c7c55e54996e7c20ab4c0d153772d7
Richard Watkins <richardwatkins1@gmail.com>
2016-04-05 14:23:48 +0100

    Update interactive_baby_dragon.rb

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,130 @@
-# your code here
+class Dragon
+
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuffInBelly     = 10  
+    @stuffInIntestine =  0  
+
+    puts @name + ' is born.'
+  end
+
+  def feed
+    puts 'You feed ' + @name + '.'
+    @stuffInBelly = 10
+    passageOfTime
+  end
+
+  def walk
+    puts 'You walk ' + @name + '.'
+    @stuffInIntestine = 0
+    passageOfTime
+  end
+
+  def putToBed
+    puts 'You put ' + @name + ' to bed.'
+    @asleep = true
+    3.times do
+      if @asleep
+        passageOfTime
+      end
+      if @asleep
+        puts @name + ' snores, filling the room with smoke.'
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts @name + ' wakes up slowly.'
+    end
+  end
+
+  def toss
+    puts 'You toss ' + @name + ' up into the air.'
+    puts 'He giggles, which singes your eyebrows.'
+    passageOfTime
+  end
+
+  def rock
+    puts 'You rock ' + @name + ' gently.'
+    @asleep = true
+    puts 'He briefly dozes off...'
+    passageOfTime
+    if @asleep
+      @asleep = false
+      puts '...but wakes when you stop.'
+    end
+  end
+
+  private
+
+  def hungry?
+    @stuffInBelly <= 2
+  end
+
+  def poopy?
+    @stuffInIntestine >= 8
+  end
+
+  def passageOfTime
+    if @stuffInBelly > 0
+      @stuffInBelly     = @stuffInBelly     - 1
+      @stuffInIntestine = @stuffInIntestine + 1
+    else  
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts @name + ' is starving!  In desperation, he ate YOU!'
+      exit 
+    end
+
+    if @stuffInIntestine >= 10
+      @stuffInIntestine = 0
+      puts 'Whoops!  ' + @name + ' had an accident...'
+    end
+
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts @name + '\'s stomach grumbles...'
+    end
+
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts @name + ' does the potty dance...'
+    end
+  end
+
+end
+
+# using the Dragon class from the chapter
+puts 'What would you like to name your baby dragon?'
+name = gets.chomp
+pet = Dragon.new name
+
+while true
+  puts 'command: feed, toss, walk, rock, putToBed, exit'
+  command = gets.chomp
+
+  if command == 'exit'
+    exit
+  elsif command == 'feed'
+    pet.feed
+  elsif command == 'toss'
+    pet.toss
+  elsif command == 'walk'
+    pet.walk
+  elsif command == 'rock'
+    pet.rock
+  elsif command == 'put to bed'
+    pet.put_to_bed
+  else
+    puts 'Huh? Please type one of the commands.'
+  end
+end
+

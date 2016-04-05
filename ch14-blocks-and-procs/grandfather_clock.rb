5179ce6ad494c99a0358502e05b708f1925956b4
chriselevate <coates.c@hotmail.com>
2016-04-05 13:11:40 +0100

    Reverting to commit 23869eff - resolving conflicts

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,30 +1,3 @@
-#How Chris Pine would do it
 def grandfather_clock &block
-  hour = (Time.new.hour + 11)%12 + 1
-
-  hour.times(&block)
+  # your code here
 end
-
-grandfather_clock { puts 'DONG!' }
-
-#How you could do it
-=begin
-def grandfather_clock &block
-  hour = Time.new.hour
-
-  if hour >= 13
-    hour = hour - 12
-  end
-
-  if hour == 0
-    hour = 12
-  end
-
-  hour.times do
-    block.call
-  end
-end
-grandfather_clock do
-  puts 'DONG!'
-end
-=end

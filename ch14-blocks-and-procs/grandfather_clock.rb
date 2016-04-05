8e104de6c24cad111ab3ce30e1b95cb0ba9e9bae
Richard Watkins <richardwatkins1@gmail.com>
2016-04-05 17:13:51 +0100

    Update grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,18 @@
-def grandfather_clock &block
-  # your code here
+def clock some_block
+
+current_hour = Time.new.hour
+if current_hour == 0
+current_hour = current_hour + 12
+elsif current_hour > 12
+current_hour = current_hour - 12
+end
+
+current_hour.to_i.times do
+some_block.call
+end
+
+end 
+
+dong = Proc.new do
+	puts 'DONG!'
 end

d3e798cfffd9de4702e0d822fb248ca71fd4dd04
Lucy <l.l.c.clarke@gmail.com>
2016-04-05 10:18:46 +0200

    new and improved

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,13 @@
-def log desc, &block
+$indent_block = 0
+  def better_log desc, &block
   # your code here
+		prefix = " "  * $indent_block
+  puts prefix + "Beginning \"#{desc}\"..."
+  		$indent_block += 1
+  result =block[]
+		$indent_block -= 1
+puts prefix + "...\"#{desc}\" finished, returning: #{result}"
 end
+
+
+

ec08238d74b4d85a2da2c7c15fe3437a062689c8
Lucy <l.l.c.clarke@gmail.com>
2016-04-05 10:26:28 +0200

    version turns green in rspec but the CI version still appears red

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,16 +1,32 @@
 
 
+$OPT_PROFILING_ON = false
+
 def profile block_description, &block
-  # your code here
-profileon = true
-if profileon == true
-  start_time = Time.new
-  block.call
-  duration = Time.new - start_time
-  puts "#{block_description}: #{duration} seconds"
-else 
-	block.call
+  if $OPT_PROFILING_ON
+    start_time = Time.new
+    block[]
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block[]
+  end
 end
 
-end
+
+#my code passes the rspec but not CI
+
+#def profile block_description, &block
+  # your code here
+#profileon = true
+#if profileon == true
+#  start_time = Time.new
+#  block.call
+#  duration = Time.new - start_time
+#  puts "#{block_description}: #{duration} seconds"
+#else 
+#	block.call
+#end
+
+#end
 

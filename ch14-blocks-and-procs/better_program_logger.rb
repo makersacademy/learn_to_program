307642c5fe5d71a22a4a1df0cf6fbcc2556a40be
josephchin19293 <josephchin19293@gmail.com>
2016-04-08 18:42:42 +0100

    chapter 14 done

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,23 @@
+$logger_depth = 0
 def log desc, &block
-  # your code here
+  start = " "*$logger_depth
+  puts "#{start}Beginning \"#{desc}\""
+  $logger_depth += 1
+  result = block[]
+  $logger_depth -= 1
+  puts "#{start}...\"#{desc}\" finished returning: #{result}"
+end
+
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lOtS oF lOVe'.downcase
+    end
+    7 * 3 * 2
+  end
+
+  log 'yet another block' do
+    '!doof naidnI evol I'.reverse
+  end
+  '0' == "0"
 end

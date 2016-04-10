224f828cac619725ed3311beb72e34b4dcd179cc
DXTimer <ibbakalov@gmail.com>
2016-04-10 07:12:53 -0700

    Ch13 Oranges fixed wording and refactored code

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -9,24 +9,36 @@ class OrangeTree
 
 	def one_year_passes
 		if @alive != true
-			"A year later the tree is still dead."
-		elsif @age == 50
+			'A year later, the tree is still dead. :('
+		elsif @age == 25
 			@alive = false
-			"Oh, no! The tree is too old, and has died."
+			'Oh, no! The tree is too old, and has died. :('
 		else
-			@height += 0.2
+			@height += 0.4
 			@age += 1
-			if @age == 10 then @num_oranges = 30 elsif @age > 10 then @num_oranges += 10 end
-			"This year your tree grew to #{@height}m tall, and produced#{@num_oranges} oranges."
+			if @age == 10
+				@num_oranges = 35
+			elsif @age > 10
+				@num_oranges += 6
+			end
+			"This year your tree grew to #{@height.round(1)}m tall, and produced #{@num_oranges} oranges."
 		end
 	end
 
-	def coun_the_oranges
-		if @alive then @num_oranges else "A dead tree has no oranges!" end
+	def count_the_oranges
+		if @alive
+			@num_oranges
+		else
+			'A dead tree has no oranges. :('
+		end
 	end
 
 	def height
-		if @alive then @height else "A dead tree is not very tall." end
+		if @alive
+			@height.round(1)
+		else 
+			"A dead tree is not very tall. :(" 
+		end
 	end
 
 	def pick_an_orange
@@ -34,7 +46,7 @@ class OrangeTree
 			@num_oranges = @num_oranges - 1
 			"You have picked a very delicious orange!"
 		else
-			@alive ? "You could not find any oranges." : "The tree is dead, no oranges can be found"
+			@alive ? "You could not find any oranges." : "A dead tree has nothing to pick. :("
 		end
 	end
 end

9803f6961dcb3f65a9502f509d439ac40949aff9
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-04 02:26:07 +0100

    added initial ask.rb from Chris Pine exercise

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,19 @@
 def ask question
-  # your code here
+  correct = false
+  while !correct
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes' || reply == 'no'
+      correct = true
+      if reply == 'yes'
+        answer = true
+      else
+        answer = false
+      end
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+  answer
 end

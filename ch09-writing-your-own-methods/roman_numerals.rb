1e733d0aeb495e25552d90bf100c3b50b0265d5c
riyapabari <riya.pabari@gmail.com>
2016-05-05 15:21:37 +0000

    Added ch09 roman_numerals method

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,11 @@
-def roman_numeral num
-  # your code here
+def roman_numeral (num)
+    arr = []
+    hash = {"M" => 1000, "CM" =>900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1}
+    
+    hash.each do |k,v|
+        arr << (k * (num / v))
+        num = num % v
+    end
+    
+    arr.join
 end

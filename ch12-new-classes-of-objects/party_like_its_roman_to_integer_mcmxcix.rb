8de3eb5e2329b74caba2afe326bdbcf16bc6cf8b
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-05 23:24:31 +0100

    added chapter 12 roman_to_integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,31 @@
 def roman_to_integer roman
-  # your code here
+  roman = roman.upcase
+
+  numerals = Hash[
+  'I' => 1,
+  'V' => 5,
+  'X' => 10,
+  'L' => 50,
+  'C' => 100,
+  'D' => 500,
+  'M' => 1000
+  ]
+
+  total = []
+  arr = roman.split("")
+  arr.map!{|x| numerals[x]}
+
+  if arr.count == 1
+    return arr[0]
+  else
+    while arr.count > 1
+      arr[-1] <= arr[-2] ? total << arr[-1] + arr[-2] : total << arr[-1] - arr[-2]
+      arr.pop(*2)
+    end
+    if arr[0] != nil
+      return arr[0] + total.reduce(:+)
+    else
+      return total.reduce(:+)
+    end
+  end
 end

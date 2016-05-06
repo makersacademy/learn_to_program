eeb31afae9d7149c7b2cf38369aaab487e67b3cd
riyapabari <riya.pabari@gmail.com>
2016-05-06 17:13:51 +0000

    Added ch10 beer_song method

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,16 @@
-# your code here
+require './english_number.rb'
+
+def beer_song (n)
+    
+    while n > 0
+        bottles = n == 1 ? "bottle" : "bottles"
+    
+        puts "#{english_number(n).capitalize} #{bottles} of beer on the wall,"
+        puts "#{english_number(n).capitalize} #{bottles} of beer,"
+        puts "Take one down, pass it around"
+        n -= 1
+        puts "#{english_number(n).capitalize} bottles of beer on the wall."
+        
+        n = n-1
+    end
+end

78c36f9c0f3e178bbfe45f12ea56ca4803021524
riyapabari <riya.pabari@gmail.com>
2016-05-07 17:42:47 +0000

    Added ch13 orange_tree program

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,5 +7,57 @@
 
 
 class OrangeTree
-  # your code here
+
+  def initialize
+      @age = 0
+      @height = 0
+      @orange_count = 0
+  end
+  
+  def one_year_passes
+      @age += 1
+      @height = (@age * 0.4).round(1)
+      if @age < 6
+          @orange_count = 0
+      elsif (6..25).include? @age
+          @orange_count = (@height * 15 - 25).to_i
+      end
+      
+      if @age == 26
+         "Oh, no! The tree is too old, and has died. :("
+      elsif @age > 26
+          "A year later, the tree is still dead. :("
+      else
+          "This year your tree grew to #{@height}m tall, and produced #{@orange_count} oranges."
+      end
+  end
+
+  def count_the_oranges
+      if @age > 25
+          "A dead tree has no oranges. :("
+      else
+          @orange_count
+      end
+  end
+  
+  def height
+      if @age > 25
+          "A dead tree is not very tall. :("
+      else
+          @height
+      end
+  end
+  
+  def pick_an_orange
+      @orange_count -= 1
+      if (@orange_count > 0 && @age <= 25)
+          "Orange was delicious!"
+      elsif @age > 25
+          "A dead tree has nothing to pick. :("
+      else
+          "No more oranges."
+      end
+  end
+  
 end
+

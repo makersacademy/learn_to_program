67ec7f582d6b0ecb60f521495a48374364eb4f74
riyapabari <riya.pabari@gmail.com>
2016-05-07 15:31:02 +0000

    Added ch13 exercise extending existing class methods

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,31 @@
-class Integer
-  # your code here
+class Array
+    
+    def shuffle
+        self.sort_by{rand}
+    end
+    
 end
+
+
+class Integer
+    
+    def factorial
+        raise 'Must not use negative integer' if self < 0
+        (1..self).reduce(:*) || 1
+    end
+      
+    def to_roman
+    raise 'Must use positive integer' if self <= 0
+    roman = ''
+    roman << 'M' * (self / 1000)
+    roman << 'D' * (self % 1000 / 500)
+    roman << 'C' * (self % 500 / 100)
+    roman << 'L' * (self % 100 / 50)
+    roman << 'X' * (self % 50 / 10)
+    roman << 'V' * (self % 10 / 5)
+    roman << 'I' * (self % 5 / 1)
+    roman
+    end
+  
+end
+

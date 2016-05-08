f36d6367248d7a5ffce1a1301a4f7fef10032235
apsaxena24 <apsaxena24@gmail.com>
2016-05-08 23:55:36 +0100

    ch-09 ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,15 @@
-def ask question
-  # your code here
+def askquestion
+		while (true)
+			puts "question"
+			reply = gets.chomp.downcase
+					if (reply == 'yes')
+						return true
+						
+					elsif (reply == 'no')
+						 return true
+				  else
+					puts ' Please answer "yes" or "no".'
+				end
+		end
+	#answer # This is what we return (true or false).
 end

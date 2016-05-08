3856deb6ce2e05b0c244b0239e39ee4da981ab11
riyapabari <riya.pabari@gmail.com>
2016-05-08 11:38:34 +0000

    Added ch14 better_log method, rspec passed

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
-def log desc, &block
-  # your code here
+$nesting_depth = 0
+
+def better_log (desc, &block)
+    space = " " * $nesting_depth
+    puts "#{space}Beginning \"#{desc}\"..."
+    $nesting_depth += 1
+    output = block.call
+    $nesting_depth -=1
+    puts "#{space}...\"#{desc}\" finished, returning: #{output}"
+end
+
+#test below
+
+better_log "outer block" do
+    better_log "some little block" do
+        better_log "teeny-tiny block" do
+        "lots of love"
+        end
+    42
+    end
+    better_log "yet another block" do
+        "I love Indian food!"
+    end
+true
 end

104d687755ff9ff7b5afee6cb89b627b9f615a8c
toby676 <tjcmui@gmail.com>
2016-05-09 12:23:01 +0100

    ch10 sort complete

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,25 @@
-def sort arr
-  # your code here
+def sort some_array
+	recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+ if unsorted_array.size == 0
+	return sorted_array
+ else
+ stilltosort = []
+ smallest = unsorted_array.pop
+
+ unsorted_array.each do |test|
+ 	if test < smallest
+ 		stilltosort << smallest
+ 		smallest = test
+ 	else
+ 		stilltosort << test
+ 	end
+    end
+
+  sorted_array << smallest
+  
+  recursive_sort stilltosort, sorted_array
+ end
 end

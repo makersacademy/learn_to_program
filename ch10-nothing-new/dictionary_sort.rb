31076d736e3061776015ee6586bd13056435f67a
Matt Butler <mattb-w@hotmail.co.uk>
2016-05-09 18:51:24 +0000

    Dictionary Sort Fixed

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,10 +1,10 @@
 def dictionary_sort arr
-  recursive_sort arr, []
+  recursive_sort arr, [] 
 end
 
 def recursive_sort unsorted_array, sorted_array
 unsorted_array.map do |word|
-  unsorted_array.any? { |oword| word > oword} ? () : (sorted_array << word ; unsorted_array.delete_at(unsorted_array.index(word)))
+  unsorted_array.any? { |oword| word.downcase > oword.downcase} ? () : (sorted_array << word ; unsorted_array.delete_at(unsorted_array.index(word)))
 end
 
 return sorted_array if unsorted_array.size < 1

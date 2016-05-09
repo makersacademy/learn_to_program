ca060e674b4a725af5fae0485358db08e03bd75f
danielschambers <daniel.s.chambers@gmail.com>
2016-05-09 10:34:56 +0100

    Chapter 9: old skool roman numerals complete

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,32 @@
-def old_roman_numeral num
-  # your code here
+NUMERALS = {
+  1000 => "M",
+  500 => "D",
+  100 => "C",
+  50 => "L",
+  10 => "X",
+  5 => "V",
+  1 => "I"
+}
+
+def old_roman_numeral(num)
+  n = num
+  numbers = ""
+  NUMERALS.each do |english, roman|
+    numbers << roman * (n / english)
+    n = n % english
+  end
+  p numbers
 end
+
+old_roman_numeral(129)
+
+
+#Old-school Roman numerals. In the early days of Roman numer- als, the Romans
+#didn’t bother with any of this new-fangled sub- traction “IX” nonsense. No sir,
+#it was straight addition, biggest to littlest—so 9 was written “VIIII,” and so
+#on. Write a method that when passed an integer between 1 and 3000 (or so) returns
+#a string containing the proper old-school Roman numeral. In other words,
+#old_roman_numeral 4 should return 'IIII'. Make sure to test your method on a
+#bunch of different numbers. Hint: Use the inte- ger division and modulus methods on page 37.
+#For reference, these are the values of the letters used:
+#I =1 V=5 X=10 L=50 C=100 D=500 M=1000
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,33 @@
-def roman_numeral num
-  # your code here
+NUMERALS = {
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    5 => "V",
+    1 => "I"
+  }
+  def roman_numeral(num)
+    # your code here
+    n = num
+    numbers = ""
+    NUMERALS.each do |english, roman|
+      numbers << roman * (n / english)
+      n = n % english
+    end
+    p numbers
+  end
 end
+
+roman_numeral(124)
+
+
+
+
+#“Modern” Roman numerals. Eventually, someone thought it would be terribly clever
+#if putting a smaller number before a larger one meant you had to subtract the
+#   smaller one. As a result of this development, you must now suffer. Rewrite your
+#   previous method to return the new-style Roman numerals so when someone calls
+#   roman_numeral 4, it should return 'IV'.
+#Mybetisthatitwasa stone carver in some year that ended in a 9, tasked with dating
+#public buildings or statues or something.

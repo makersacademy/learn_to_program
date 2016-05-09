de404566d465c41b6ecc4b41c9df5ac428bbed93
Stephen Dawes <sdawes@outlook.com>
2016-05-09 22:59:44 +0100

    ch09-writing-your-own-methods/old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,18 @@
 def old_roman_numeral num
-  # your code here
-end
+  roman_hash = {
+    1000 => "M",
+     500 => "D",
+     100 => "C",
+      50 => "L",
+      10 => "X",
+        5 => "V",
+        1 => "I",
+    }
+
+    roman_numeral = ""
+    roman_hash.each do |value, letter|
+      roman_numeral << letter * (num / value)
+      num = num % value
+    end
+    return roman_numeral
+end                                            # => :old_roman_numeral

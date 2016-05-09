135162e7158c469d4b4f2258dc37516fc55867c4
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-09 22:23:21 +0100

    add happy birthday chapter 12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+
+while true
+  puts "What year were you born (in digit format)?"
+  year = gets.chomp.to_i
+  puts "And the month (in digit format)?"
+  month = gets.chomp.to_i
+  puts "And the day (in digit format)?"
+  day = gets.chomp.to_i
+
+  begin
+  (((Time.now - Time.mktime(year,month,day)) / 60 / 60 / 24 / 365).floor).times {puts "SPANK!"}
+    exit
+  rescue
+    puts "You need to insert a digit for each question! Try again!"
+  end
+end

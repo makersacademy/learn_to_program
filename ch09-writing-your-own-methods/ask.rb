c4ad8389729ce71db69b44fce67537f681f0757b
toby676 <tjcmui@gmail.com>
2016-05-09 10:34:16 +0100

    ch9/1 complete

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
 def ask question
-  # your code here
+  while true
+
+puts question
+
+reply = gets.chomp.downcase
+
+if reply == "yes"
+   return true
+elsif reply == "no"
+   return false
+else
+	"please answer yes or no" 
+	return
+end
+end
 end

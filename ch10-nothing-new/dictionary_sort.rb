ad6e5ffb1d85546b32f3b53f4cf700919166db98
mtaner <mervetaner88@gmail.com>
2016-05-09 10:40:21 +0000

    Ch 10: Exercises Dictionary_sort and sort solved

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,25 @@
 def dictionary_sort arr
-  # your code here
+  l = arr.length 
+  n = 0
+ 
+    while l-1 > n
+        if  arr[n].downcase > arr[n+1].downcase
+            arr << arr[n]
+            arr.delete_at(n)
+            n = 0
+        else
+            n += 1
+        end
+    end
+    arr
 end
+#Dictionary sort. Your sorting algorithm is pretty good, sure. But there was
+#always that sort of embarrassing point you were hoping I’d just sort of
+#gloss over, right? About the capital letters? Your sorting algorithm is good
+#for general-purpose sorting, but when you sort strings, you are using the
+#ordering of the characters in your fonts (called the ASCII codes) rather
+#than true dictionary ordering. In a dictionary, case (upper or lower) is
+#irrelevant to the ordering. So, make a new method to sort words (something
+#like dictionary_sort). Remember, though, that if I give your program words
+#starting with capital letters, it should return words with those same capital
+#letters, just ordered as you’d find in a dictionary.
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,27 @@
-def sort arr
+#def sort arr
   # your code here
+#end
+
+
+#def sort some_array # This "wraps" recursive_sort.
+#recursive_sort some_array, []
+#end
+#def recursive_sort unsorted_array, sorted_array
+# Your fabulous code goes here.
+#end
+
+def sort arr
+  l = arr.length 
+  n = 0
+ 
+    while l-1 > n
+        if  arr[n].downcase > arr[n+1].downcase
+            arr << arr[n]
+            arr.delete_at(n)
+            n = 0
+        else
+            n += 1
+        end
+    end
+    arr
 end

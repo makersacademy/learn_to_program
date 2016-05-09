e331c9ab56fd4fa455ffbd2bf34a2679eb8406a3
apsaxena24 <apsaxena24@gmail.com>
2016-05-09 23:45:21 +0100

    changes in ch09 and dictionary of chapter 10

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -2,11 +2,15 @@ def old_roman_numeral(num)
 	
 	values = {"M" => 1000,"D" => 500,"C" => 100,"L" => 50,"X" => 10,"V" => 5,"I" => 1}
 	romanstr = ""
-	values.each do |k,v|
-		romanstr << k*(num/v)
-		num = num %v
+	while (num != 0)
+		values.each do |k,v|
+			romanstr << k*(num/v)
+			num = num %v
+		end
 	end
 	return romanstr
 		
   
 end
+
+old_roman_numeral(120)
--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,33 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort (arr)
+	arr1 = []
+	for i in (0..arr.length-1)
+		less = arr[i]
+		pos_less = i
+		for j in (i+1..arr.length-1)
+			if (less > arr[j])
+				less = arr[j]
+				pos_less = j
+			end
+		end
+	c = arr[i]
+	arr[i] = arr[pos_less]
+	arr[pos_less] = c
+	end 
+return arr
 end
+
+
+
+# cat bat sat at
+
+# 1 8 7 5 6 3
+
+
+# 1 3 7 5 6 8
+
+# less = 1
+# arr = 1 
+
+
+# dictionary_sort(["cat", "bat", "at", "some", "look"])
+# # [1,7,6,8,2]

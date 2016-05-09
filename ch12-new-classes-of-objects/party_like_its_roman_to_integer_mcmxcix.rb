92dca7f28df9d297ca8483fad2c83129c12ee578
danielschambers <daniel.s.chambers@gmail.com>
2016-05-09 16:56:07 +0100

    Chapter 12 done

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,29 @@
-def roman_to_integer roman
+NUMERALS = {
+  "M" => 1000,
+  "D" => 500,
+  "C" => 100,
+  "L" => 50,
+  "X" => 10,
+  "V" => 5,
+  "I" => 1
+}
+
+def roman_to_integer(roman)
   # your code here
+  rom = []
+  roman = roman.downcase
+  rom << '4'   and roman.gsub!("iv", "") if roman.include?("iv")
+  rom << '9'   and roman.gsub!("ix", "") if roman.include?("ix")
+  rom << '40'  and roman.gsub!("xl", "") if roman.include?("xl")
+  rom << '90'  and roman.gsub!("xc", "") if roman.include?("xc")
+  rom << '400' and roman.gsub!("cd", "") if roman.include?("cd")
+  rom << '900' and roman.gsub!("cm", "") if roman.include?("cm")
+  roman = roman.upcase.split("")
+  roman.each do |roman|
+    rom << NUMERALS[roman]
+  end
+  rom.map(&:to_i).inject(:+)
 end
+
+
+p roman_to_integer('iv')

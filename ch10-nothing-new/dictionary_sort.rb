aaa28661e5ded955293c4d72938a5680226a472c
Elena <elena.vilimaite@gmail.com>
2016-05-09 16:33:15 +0100

    Ch10, dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,12 @@
 def dictionary_sort arr
   # your code here
+	0.upto(arr.length-2) do |i|
+		if arr[i] > arr[i+1]
+			temp = arr[i]
+			arr[i] = arr[i+1]
+			arr[i+1] = temp
+			dictionary_sort arr
+		end
+	end
+	arr
 end

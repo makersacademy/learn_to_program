9fece075cd45d5be0d0654ff75799c89ecca6911
Matt Butler <mattb-w@hotmail.co.uk>
2016-05-09 14:46:48 +0000

    Sort/Shuffle/dict/Continent Complete

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,12 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle arr, []
+  
+end
+
+def recursive_shuffle unsort_arr, sort_arr
+ x = rand(unsort_arr.size)
+ sort_arr << unsort_arr[x]
+ unsort_arr.delete_at(x)
+ return sort_arr if unsort_arr.size < 1
+ recursive_shuffle(unsort_arr,sort_arr)
 end

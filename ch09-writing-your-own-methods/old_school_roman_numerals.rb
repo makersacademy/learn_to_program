39f314d659ef6bd2a5027ae76c917b3f63a122f0
toby676 <tjcmui@gmail.com>
2016-05-09 10:35:20 +0100

    ch9/2 complete

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
 def old_roman_numeral num
-  # your code here
+hash = {
+"M" => 1000, 
+"D" => 500,
+"C" => 100, 
+"L" => 50, 
+"X" => 10, 
+"V" => 5, 
+"I" => 1, 
+}
+
+answer = ""
+
+hash.each do |letter,number|
+	answer << letter * (num / number)
+	num %= number
+end
+answer
 end

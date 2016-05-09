478e88d0e4fe82e7f224d80ca579a15e04c6619a
mtaner <mervetaner88@gmail.com>
2016-05-09 19:46:03 +0000

    Ch12 Exercise roman_to_integer revised solution

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,36 +1,28 @@
 
-
  NUMERALS = {
-    '1000' => "M",
-    '900' => "CM",
-    '500' => "D",
-    '400' => "CD",
-    '100' => "C",
-    '90' => "XC",
-    '50' => "L",
-    '40' => "XL",
-    '10' => "X",
-    '9' => "IX",
-    '5' => "V",
     '4' => "IV",
+    '9' => "IX",
+    '40' => "XL",
+    '90' => "XC",
+    '400' => "CD",
+    '900' => "CM",
+    '1000' => "M",    
+    '500' => "D",    
+    '100' => "C",    
+    '50' => "L",    
+    '10' => "X",    
+    '5' => "V",    
     '1' => "I"
   }
   
-  # XXIV = 24
-
 def roman_to_integer (str)
     arr = []
-    str = str.downcase
-    arr << '4' and str.gsub!("iv", "") if str.include?("iv") 
-    arr << '9' and str.gsub!("ix", "") if str.include?("ix")
-    arr << '40' and str.gsub!("xl", "") if str.include?("xl")
-    arr << '90' and str.gsub!("xc", "") if str.include?("xc")
-    arr << '400' and str.gsub!("cd", "") if str.include?("cd")
-    arr << '900' and str.gsub!("cm", "") if str.include?("cm")
-    str = str.upcase.split("")
-    str.each { |x|  arr << NUMERALS.invert[x] }
+    str = str.upcase
+    while /[a-zA-Z]/ === str
+    NUMERALS.invert.each { |k, v| str.sub!(k, v) and arr << v  if str.include?(k) }
+    end
     arr.map(&:to_i).reduce(:+)
 end
 
 
-roman_to_integer('mcmxcix')
+

fdae8d3bd859f529774ae417ffdc2cd51904de89
Elena <elena.vilimaite@gmail.com>
2016-05-09 13:59:43 +0100

    Ch09, old roamn

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,25 @@
 def old_roman_numeral num
-  # your code here
+	romanNums = {
+		1    => 'I',
+		5    => 'V',
+		10   => 'X',
+		40   => 'XL',
+		50   => 'L',
+		90   => 'XC',
+		100  => 'C',
+		400  => 'CD',
+		500  => 'D',
+		900  => 'CM',
+		1000 => 'M'
+	}
+
+	result = ''
+	romanNums.keys.reverse.each do |n|
+		while num >= n
+			num -= n
+			result += romanNums[n]
+		end
+	end
+	result
 end
+

13cf0ba8f07745d8fabf637bd93e20179fd00c4a
Elena <elena.vilimaite@gmail.com>
2016-05-09 16:14:43 +0100

    Ch10, sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
 def sort arr
-  # your code here
+	0.upto(arr.length-2) do |i|
+		if arr[i] > arr[i+1]
+			temp = arr[i]
+			arr[i] = arr[i+1]
+			arr[i+1] = temp
+			sort arr
+		end
+	end
+	arr
 end

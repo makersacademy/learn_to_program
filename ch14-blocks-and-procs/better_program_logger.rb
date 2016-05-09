1cdd2385c1d318b6f8f92f296efdb1e7d11d1a94
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-09 18:00:40 +0100

    added better_program_logger chapter 14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,11 +1,24 @@
-$var = "   "
+$var = ' '
+$i = 0
 
-def log(des, &block)
-    puts $var + "starting " + des
-    yield
-    puts $var + "done"
+def better_log(des, &block)
+  puts $var*$i + "Beginning \"#{des}\"...".strip
+  $i += 1
+  y = yield
+  $i -= 1
+  puts $var*$i + "...\"#{des}\" finished, returning: #{y}"
 end
 
-y = 5
-log("stupid code") { }
 
+better_log("outer block") do
+  better_log("some little block") do
+    better_log("teeny-tiny block") do
+    "lots of love"
+  end
+    "42"
+  end
+  better_log("yet another block") do
+    "I love Indian food!"
+  end
+  true
+end

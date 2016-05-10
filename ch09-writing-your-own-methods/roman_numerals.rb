12e6917bef6f57a52cf0c8c14648a8fb2d2d0887
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-10 19:47:19 +0100

    CH09 "Roman numerals" completed and verified

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,7 @@
 def roman_numeral num
-  # your code here
+  output = ""+"M"*(num/1000)
+  output += (num%1000)/100 == 9 ? "CM" : (num%1000)/100 == 4 ? "CD" : "D"*((num%1000)/500)+"C"*((num%500)/100)
+  output += (num%100)/10 == 9 ? "XC" : (num%100)/10 == 4 ? "CL" : "L"*((num%100)/50)+"X"*((num%50)/10)
+  output += num%10 == 9 ? "IX" : num%10 == 4 ? "IV" : "V"*((num%10)/5)+"I"*(num%5)
+  output
 end

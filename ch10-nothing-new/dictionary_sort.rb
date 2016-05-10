396ef6f29b3a524c87a3832266d7600657b6779f
srMarquinho <sr.marquinho@gmail.com>
2016-05-10 10:24:24 +0100

    ch10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,25 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort un_arr, so_arr 
+  if un_arr.size <= 0
+    return so_arr 
+  end
+
+  smallest = un_arr.pop
+  still_un_arr = []
+
+  un_arr.each do |word| 
+    if word.downcase < smallest.downcase
+      still_un_arr << smallest
+      smallest = word
+    else
+      still_un_arr << word
+    end 
+  end
+
+  so_arr << smallest
+  
+  recursive_sort still_un_arr, so_arr
 end
--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,21 @@
-# your code here
+require './english_number.rb'
+
+def song(int)
+	for i in (int).downto(1)
+		if i == 1
+			puts """
+			#{(english_number i).capitalize!} bottle of beer on the wall, #{(english_number i).capitalize!} bottle of beer.
+			Take one down and pass it around, no more bottles of beer on the wall.
+		"""
+			puts
+		else
+			puts """
+			#{(english_number i).capitalize!} bottles of beer on the wall, #{(english_number i).capitalize!} bottles of beer.
+			Take one down and pass it around, #{(english_number i-1).capitalize!} bottles of beer on the wall.
+			"""
+			puts
+		end
+	end
+end
+
+#puts song(999)

e8d0d85a0e05d925c313437038fd085f166b9e88
srMarquinho <sr.marquinho@gmail.com>
2016-05-10 15:34:40 +0100

    ch13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,43 @@
+class Array
+	def shuffle
+	  arr = self
+	  result = []
+	  while arr.size > 0
+		result << arr.delete_at(rand(arr.length))
+	  end
+	  result
+	end
+end
+
+
+
 class Integer
-  # your code here
+  def factorial
+  	self <= 1 ? 1 : self * (self-1).factorial
+  end
+
+  def to_roman
+  	symbols = [
+          ['M', 1000],
+          ['CM', 900],
+          ['D', 500],
+          ['CD', 400],
+          ['C', 100],
+          ['XC', 90],
+          ['L', 50],
+          ['XL', 40],
+          ['X', 10],
+          ['VIIII', 9],
+          ['V', 5],
+          ['IIII', 4],
+          ['I', 1]
+          ]
+
+  return '' if self == 0
+  symbols.each do |str, int|
+    if self >= int
+      return str + (self - int).to_roman
+    end
+  end
+  end
 end

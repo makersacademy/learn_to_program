92aeafd8b9d8407f1a9d70dd77f51413b023ae66
toby676 <tjcmui@gmail.com>
2016-05-10 11:53:48 +0100

    ch13 romanize added to integer class

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,28 @@
 class Integer
-  # your code here
+  def romanize
+num = self
+hash = {
+"M" => 1000, 
+"CM" => 900,
+"D" => 500,
+"CD" => 400,
+"C" => 100, 
+"XC" => 90,
+"L" => 50, 
+"XL" => 40,
+"X" => 10,
+"IX" => 9, 
+"V" => 5, 
+"IV" => 4,
+"I" => 1, 
+}
+
+answer = ""
+
+hash.each do |letter,number|
+	answer << letter * (num / number)
+	num %= number
+end
+answer
+end
 end

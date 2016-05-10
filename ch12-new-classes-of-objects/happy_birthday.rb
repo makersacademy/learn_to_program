a6423e4dd9ccdf43cf8e67bf016a8c2c15e2dda4
srMarquinho <sr.marquinho@gmail.com>
2016-05-10 12:41:17 +0100

    ch12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,7 @@
-# your code here
+def happy_birthday (birthday)
+	years = Time.now.year - birthday.year
+	years.times {|i| p 'SPANK!'}
+end
+
+
+#puts happy_birthday (Time.new(1983, 6, 29, 23, 00))
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,25 @@
 def roman_to_integer roman
-  # your code here
+  roman.upcase!
+  maches = [
+            ['M', 1000],
+            ['CM', 900],
+            ['D', 500],
+            ['CD', 400],
+            ['C', 100],
+            ['XC', 90],
+            ['L', 50],
+            ['XL', 40],
+            ['X', 10],
+            ['IX', 9],
+            ['V', 5],
+            ['IV', 4],
+            ['I', 1]
+            ]
+  n = 0
+  maches.each do |str, int|
+    while roman.sub!(/^#{str}/, "")
+      n += int
+    end
+  end
+  n
 end

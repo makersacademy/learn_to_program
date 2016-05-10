aa2cebb74d0206ca149a8b5675791eb525f2a717
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-10 18:22:34 +0200

    Implement recursive shuffle method

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,9 @@
 def shuffle arr
-  # your code here
+  recursive_shuffle(arr, [])
+end
+
+def recursive_shuffle(array, shuffled) 
+  return shuffled if array == []
+  shuffled << array.delete_at(array.index(array[rand(array.size)]))
+  recursive_shuffle(array, shuffled)
 end

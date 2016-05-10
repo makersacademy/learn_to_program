0c789cc4d6f000a94ab394c52c1a245183ae5b68
toby676 <tjcmui@gmail.com>
2016-05-10 11:53:23 +0100

    ch 12 birthday helper attempt #1

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,14 @@
-# your code here
+
+def birthday_helper(name)
+file = File.new("input.txt", "r")
+data = file.read
+file.close
+
+hash = {}
+
+lines = data.split("\n")
+sections = lines.map{|line| line.split(", ")}
+sections.each{|section| hash[section[0]] = section[1] + " " + section[2]}
+
+puts hash[name]
+end

75fbcb1fde3f2d4dc843ed03598557ebcada9513
paulkenrick <pkenrick@gmail.com>
2016-05-10 15:21:30 +0700

    Completed Ch09 - roman_numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,6 +1,6 @@
 def old_roman_numeral num
 
-	numeral_hash = {'M'=>1000, 'D'=>500,'C'=>100, 'L'=>50,'X'=>10, 'V'=>5,'I'=>1}
+	numeral_hash = {'M'=>1000, 'D'=>500, 'C'=>100, 'L'=>50, 'X'=>10, 'V'=>5, 'I'=>1}
 	return_string = ''
 
 	numeral_hash.each do |key, value|
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,16 @@
 def roman_numeral num
-  # your code here
+  
+  	numeral_hash = {'M'=>1000, 'CM'=>900, 'D'=>500, 'CD'=>400, 'C'=>100, 'XC'=>90, 'L'=>50, 'XL'=>40,'X'=>10, 'IX'=>9, 'V'=>5, 'IV'=>4, 'I'=>1}
+	return_string = ''
+
+	numeral_hash.each do |key, value|
+		if num >= value
+  			multiplier = num / value
+  			multiplier.times {return_string += key}
+  			num -= multiplier * value
+  		end
+	end
+
+	return_string
+	
 end

fbadbdf87409bceaf64cd46ff288e8b60e01ed4c
Van <singularity@vannio.co.uk>
2016-05-10 22:00:41 +0100

    Completed ch09 - roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,29 @@
-def roman_numeral num
-  # your code here
+Numerals = {
+	1000 => 'M',
+	900 => 'CM',
+	500 => 'D',
+	400 => 'CD',
+	100 => 'C',
+	90 => 'XC',
+	50 => 'L',
+	40 => 'XL',
+	10 => 'X',
+	9 => 'IX',
+	5 => 'V',
+	4 => 'IV',
+	1 => 'I'
+}
+
+def roman_numeral number
+	string = ''
+
+	Numerals.collect do |num, roman|
+		# How many times does the number fit?
+    string << roman * (number / num)
+
+		# Update number with remainder
+    number %= num
+  end
+
+	string
 end

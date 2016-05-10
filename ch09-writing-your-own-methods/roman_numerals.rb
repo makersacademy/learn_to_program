59e2146521c91b7d6579476293dd64c1ceb079cd
kpuwal <kpuwal@gmail.com>
2016-05-10 02:22:02 +0100

    ch09: roman_numerals solved

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,31 @@
-def roman_numeral num
-  # your code here
+def roman_numeral number
+    arr=[]
+    result= []
+    number_to_arr= number.to_s.split('')
+    printer= -> arabic_numeral {arabic_numeral.to_s}
+
+    roman_arr_tens= ["I", "X", "C", "M"]
+    roman_arr_fives= ["V", "L", "D"]
+
+    counter= -1
+    roman_counter= 0
+    
+    while counter >= -number_to_arr.length
+        arabic= number_to_arr[counter]
+        if arabic.to_i < 4
+            (arabic.to_i).times do arr<< printer[roman_arr_tens[roman_counter]] end
+           result<< arr.join
+       elsif arabic.to_i == 4
+            result<< "#{roman_arr_tens[roman_counter]}#{roman_arr_fives[roman_counter]}"
+        elsif arabic.to_i == 9
+            result<< "#{roman_arr_tens[roman_counter]}#{roman_arr_tens[roman_counter+1]}"
+        else
+            (arabic.to_i-5).times do arr<< printer[roman_arr_tens[roman_counter]] end
+           result<< "#{roman_arr_fives[roman_counter]}#{arr.join}"
+        end
+        arr=[]
+        counter-=1
+        roman_counter+=1
+    end
+    result.reverse.join
 end

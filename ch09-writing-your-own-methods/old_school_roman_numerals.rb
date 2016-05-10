cf9d5d33e83e330ddfefca044f4edcd3412d7e21
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-10 05:25:30 +0100

    Implement a solution for the old roman numerals excercise

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,11 @@
 def old_roman_numeral num
-  # your code here
+  base_5_convert(num.to_s[-4].to_i, 'M', '_') + 
+  base_5_convert(num.to_s[-3].to_i, 'C', 'D') + 
+  base_5_convert(num.to_s[-2].to_i, 'X', 'L') + 
+  base_5_convert(num.to_s[-1].to_i, 'I', 'V')
+end
+
+def base_5_convert n, unit, tenth
+  base5 = n.to_s(5)
+  (tenth * base5[-2].to_i) + (unit * base5[-1].to_i) 
 end

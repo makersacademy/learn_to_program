d4cd1c0b2f14eb3408c14e2d90e8e1a1f7f13745
Stephen Dawes <sdawes@outlook.com>
2016-05-10 18:18:19 +0100

    ch10-nothing-new/sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,7 @@
-def sort arr
-  # your code here
+def sort(unsorted_array, sorted_array = [])
+  sorted_array << unsorted_array.delete_at(unsorted_array.index(unsorted_array.min))
+  sort(unsorted_array, sorted_array) until unsorted_array.length == 0
+  sorted_array
 end
+
+puts(sort(["Stephen", "Robert", "David", "Dawes"]).join(' '))

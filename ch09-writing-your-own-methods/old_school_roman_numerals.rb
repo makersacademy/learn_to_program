53d50072230af9cd9de9e19cac7cebca6c389899
Luke <lukeaveil@yahoo.com>
2016-05-10 15:15:55 +0100

    chapter 9, second challenge

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,24 @@
 def old_roman_numeral num
-  # your code here
+  
+  numeral_hash = {
+  	'M' => 1000,
+  	'D' => 500,
+  	'C' => 100,
+  	'L' => 50,
+  	'X' => 10,
+  	'V' => 5,
+  	'I' => 1
+  }
+
+  numerals = ''
+
+  numeral_hash.each do |numeral, number|
+  	numerals << numeral * (num / number)
+
+  	num = num % number
+  	
+  end
+
+  numerals
+
 end

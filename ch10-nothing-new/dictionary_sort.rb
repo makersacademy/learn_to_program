5014f19591ba329625222c53417f0908e9ee4930
gtormiston <grahamormiston@gmail.com>
2016-05-11 17:19:46 +0100

    ch10:dictionary_sort complete

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -2,6 +2,6 @@ def dictionary_sort arr
   return [] if arr.length == 0
   original = arr
   pivot = arr.shift
-  less, more = arr.partition {|e| e < pivot }
+  less, more = arr.partition {|e| e.downcase < pivot.downcase }
   dictionary_sort(less) + [pivot] + dictionary_sort(more)
 end

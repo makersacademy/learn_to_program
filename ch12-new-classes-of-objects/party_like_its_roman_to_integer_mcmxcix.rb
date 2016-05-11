3f30f466bdeae4cb8e6cf238db866b94cf4ecbdb
Michael_Lo <Michael.Lo@live.co.uk>
2016-05-11 13:46:47 +0100

    completed roman_to_integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,27 @@
-def roman_to_integer roman
-  # your code here
+def roman_to_integer(str)
+ str = str.downcase
+  roman_mapping = {
+      1000 => "m",
+      900 => "cm",
+      500 => "d",
+      400 => "cd",
+      100 => "c",
+      90 => "xc",
+      50 => "l",
+      40 => "xl",
+      10 => "x",
+      9 => "ix",
+      5 => "v",
+      4 => "iv",
+      1 => "i"
+    }
+
+  result = 0
+  roman_mapping.values.each do |roman|
+    while str.start_with?(roman)
+      result += roman_mapping.invert[roman]
+      str = str.slice(roman.length, str.length)
+    end
+  end
+  result
 end

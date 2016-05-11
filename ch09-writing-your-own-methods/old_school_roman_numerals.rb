92857ae198ccb05c0a9e10ac862313f5ac252234
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-11 16:54:27 +0000

    Solved 2nd excerise in ch09 - roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,15 @@
 def old_roman_numeral num
   # your code here
+  
+  m = num.to_i / 1000
+  d = (num % 1000) / 500
+  c = (num % 500) / 100
+  l = (num % 100) / 50
+  x = (num % 50) / 10
+  v = (num % 10) / 5
+  i = num % 5
+  
+  roman = ("M" * m) + ("D" * d) + ("C" * c) + ("L" * l) + ("X" * x) + ("V" * v) + ("I" * i)
+  
+  
 end

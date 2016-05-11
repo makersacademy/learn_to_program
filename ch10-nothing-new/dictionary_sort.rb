10635bf3c3c7e1ee9d283b28d2b000e2b71cd0bb
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-11 15:14:14 +0200

    Refactors dictionary sort method

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,12 +1,10 @@
 def dictionary_sort arr
-  capitals = arr.select{|word| word == word.capitalize}
-  recursive_sort(arr, [], capitals) 
+  recursive_sort(arr, []) 
 end
 
-def recursive_sort some_arr, sorted_arr, capitals
-  some_arr.map!{|word| word.downcase}
-  return sorted_arr.map{|word| (capitals.include?(word.capitalize)) ? word.capitalize : word } if some_arr == [] 
-  sorted_arr.push(some_arr.min)
-  some_arr.delete_at(some_arr.index(some_arr.min))
-  recursive_sort(some_arr, sorted_arr, capitals)
+def recursive_sort some_arr, sorted_arr
+  return sorted_arr if some_arr == [] 
+  sorted_arr.push(some_arr.min_by {|w| w.downcase})
+  some_arr.delete_at(some_arr.index(some_arr.min_by{|w| w.downcase}))
+  recursive_sort(some_arr, sorted_arr)
 end

043e7a742e04706090f3361cf195a527fa4f149e
Matt Butler <mattb-w@hotmail.co.uk>
2016-05-11 02:08:26 +0000

    ch11 challenge 1/2 complete

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,18 @@
-# your code here
+def shuffle arr
+  recursive_shuffle arr, []
+  
+end
+
+def recursive_shuffle unsort_arr, sort_arr
+ x = rand(unsort_arr.size)
+ sort_arr << unsort_arr[x]
+ unsort_arr.delete_at(x)
+ return sort_arr if unsort_arr.size < 1
+ recursive_shuffle(unsort_arr,sort_arr)
+end
+File.open("playlist.m3a", "w+") do |f| 
+f.puts(Dir['/home/ubuntu/workspace/mp3/*.mp3'].shuffle) 
+
+end
+
+

3441493d2f82ce00b6ba1a64247885bd46e8e50d
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-11 14:35:40 +0100

    CH10 "sort" challenge completed and verified

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,10 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  return sorted_array if unsorted_array == []
+  sorted_array << unsorted_array.min
+  unsorted_array.delete_at(unsorted_array.index(unsorted_array.min) || unsorted_array.length)
+  recursive_sort unsorted_array, sorted_array
 end

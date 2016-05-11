b6edbc308588455f7836a365d2110daa9b5d0179
Stephen Dawes <sdawes@outlook.com>
2016-05-11 10:44:21 +0100

    ch10-nothing-new/dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,9 @@
-def dictionary_sort arr
-  # your code here
-end
+def dictionary_sort(array)
+  return array if array.length <= 1  # => false, false, false, false, true, true, true, true, true
+
+  middle = array.pop                                         # => "singing", "like", "feel", "can"
+  less = array.select { |x| x.downcase < middle.downcase }   # => ["A", "can", "can", "feel", "like"], ["A", "can", "can", "feel"], ["A", "can", "can"], ["A"]
+  more = array.select { |x| x.downcase >= middle.downcase }  # => [], [], [], ["can"]
+
+  dictionary_sort(less) + [middle] + dictionary_sort(more)  # => ["A", "can", "can"], ["A", "can", "can", "feel"], ["A", "can", "can", "feel", "like"], ["A", "can", "can", "feel", "like", "singing"]
+end                                   # => :sort

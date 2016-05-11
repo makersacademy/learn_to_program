e58dd5a16fe02adfb2b77fc23ef611d6b580d805
awye765 <awye765@googlemail.com>
2016-05-11 07:43:30 +0100

    CP CH10 - shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,9 @@
-def shuffle arr
-  # your code here
-end
+def shuffle(arr)
+  arr.shuffle(random: Random.new(1))      # => [9, 3, 7, 8, 2, 1, 5, 4, 6], [6, 7, 5, 4, 3, 9, 2, 8, 1]
+  # Shuffles the array using a Random
+  # number generator.
+end                                       # => :shuffle
+
+shuffle p(shuffle([1,2,3,4,5,6,7,8,9]))   # => [6, 7, 5, 4, 3, 9, 2, 8, 1]
+
+# >> [9, 3, 7, 8, 2, 1, 5, 4, 6]

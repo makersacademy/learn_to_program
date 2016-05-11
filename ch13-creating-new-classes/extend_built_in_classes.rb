21bad48c97f06543150cadcab920aefcb5246d5d
apsaxena24 <apsaxena24@gmail.com>
2016-05-12 00:25:23 +0100

    few changes in ch-13 1 solution

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,18 +1,19 @@
 class Integer
   def factorial
-  	if self <= 1
-  		return 1
-  	else
-  		 self * (self-1)factorial
-  	end
+    if self <= 1
+      1
+    else
+      self * (self-1).factorial
+    end
   end
   def to_roman
   	roman_map = {1000 => "M",  900 => "CM", 500 => "D",400 => "CD",100 => "C",90 => "XC",50 => "L",40 => "XL",10 => "X",9 => "IX",5 => "V",4 => "IV",1 => "I"}
    	roman_str = ""
-    while (self != 0)
+   	num = self
+    while (num != 0)
     	roman_map.each do |k,v|
     		roman_str << v*(num/k)
-    		self = self % k
+    		num = num % k
     	end
     end
 

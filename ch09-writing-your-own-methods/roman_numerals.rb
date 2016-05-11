2035fdcea937c94f01ff9f7af8a6fc51b7161544
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-11 22:30:45 +0100

    Completed ch09 - Roman Numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,18 +1,22 @@
 def roman_numeral num
-  numeral = ''
-
-	numeral << 'M' * (num / 1000)
-	numeral << 'CM' * (num % 1000 / 900)
-	numeral << 'D' * (num % 900 / 500)
-	numeral << 'CD' * (num % 500 / 400)
-	numeral << 'C' * (num % 400 / 100)
-	numeral << 'XC' * (num % 100 / 90)
-	numeral << 'L' * (num % 90 / 50)
-	numeral << 'XL' * (num % 50 / 40)
-	numeral << 'X' * (num % 40 / 10)
-	numeral << 'IX' * (num % 10 / 9)
-	numeral << 'V' * (num % 9 / 5)
-	numeral << 'IV' * (num % 5 / 4)
-	numeral << 'I' * (num % 4 / 1)
-	numeral
+    roman_nums = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90, 
+    "L" => 50, 
+    "XL" => 40, 
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+    }
+    amt = 0
+    roman_nums.map do |ltr, val|
+        amt, num = num.divmod(val)
+        ltr * amt
+    end.join
 end

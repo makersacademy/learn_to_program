b73194c68c3bbae50d49e48f6195d715dac8b8b5
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-11 21:07:00 +0100

    Completed ch09 - Old school roman numerals - improved notes

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,18 @@
 def roman_numeral num
-  # your code here
+  numeral = ''
+
+	numeral << 'M' * (num / 1000)
+	numeral << 'CM' * (num % 1000 / 900)
+	numeral << 'D' * (num % 900 / 500)
+	numeral << 'CD' * (num % 500 / 400)
+	numeral << 'C' * (num % 400 / 100)
+	numeral << 'XC' * (num % 100 / 90)
+	numeral << 'L' * (num % 90 / 50)
+	numeral << 'XL' * (num % 50 / 40)
+	numeral << 'X' * (num % 40 / 10)
+	numeral << 'IX' * (num % 10 / 9)
+	numeral << 'V' * (num % 9 / 5)
+	numeral << 'IV' * (num % 5 / 4)
+	numeral << 'I' * (num % 4 / 1)
+	numeral
 end

bac56c43153040d7ecf18526cc0526d7e9f69b71
paulkenrick <pkenrick@gmail.com>
2016-05-11 11:08:10 +0700

    Completed Ch10 - dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort unsorted_array, sorted_array = []
+
+  return sorted_array if unsorted_array.empty?
+		
+	next_word = ""
+	unsorted_array.each do |word|
+		next_word == "" ? next_word += word : (next_word = word if word.downcase < next_word.downcase)
+	end
+	
+	unsorted_array.delete_at(unsorted_array.index(next_word))
+	sorted_array << next_word
+	
+	dictionary_sort(unsorted_array, sorted_array)
+
 end
+
+p dictionary_sort(["Hello", "boy", "boy", "car", "Intermediate", "languange", "Doing", "high"])

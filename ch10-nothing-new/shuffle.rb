79278266b340bbaa4bc4905ea1ef24e6cab2a2f3
apsaxena24 <apsaxena24@gmail.com>
2016-05-11 12:54:01 +0100

    checking fail scenraion

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,13 +1,20 @@
-def shuffle(arr)
-	arr_rand = []
- 	if (arr.length == 0)
-  		return []
-  	end
-  	for i in (0..(arr.length)-1)
-  		random = rand(0..arr.length-1)
-  		arr_rand << arr[random]
-  	end
+# def shuffle(arr)
+# 	arr_rand = []
+#  	if (arr.length == 0)
+#   		return []
+#   	end
+#   	for i in (0..(arr.length)-1)
+#   		random = rand(0..arr.length-1)
+#   		arr_rand << arr[random]
+#   	end
 
-	return arr_rand
+# 	return arr_rand
+# end
+
+def shuffle arr
+	result = []
+	while arr.size > 0
+		result << arr.delete_at(rand(arr.length))
+	end
+	result
 end
-

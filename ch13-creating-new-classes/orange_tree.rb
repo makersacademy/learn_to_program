aea4055d3ae68dc9407c211544f84efa0fc7c21c
Marlon Date-Chong <mdatechong@gmail.com>
2016-05-11 15:51:23 +0100

    new orange tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,61 +7,49 @@
 
 
 class OrangeTree
-  def initialize
+ # your code here
+ def initialize
+    @age = 0
+    @oranges = 0
     @height = 0
-    @orange_count = 0
-    @alive = true
   end
-
-  def height
-    if @alive
-      @height.round(1)
-    else
-      'A dead tree is not very tall. :('
-    end
-  end
-
-  def count_the_oranges
-    if @alive
-      @orange_count
-    else
-      'A dead tree has no oranges. :('
-    end
-  end
-
   def one_year_passes
-    if @alive
-      @height = @height + 0.4
-      @orange_count = 0 # old oranges fall off
-      if @height >= 10.4 && rand(2) > 0
-        # tree dies
-        @alive = false
-        'Oh, no! The tree is too old, and has died. :('
-      elsif @height > 2
-        # new oranges grow
-        @orange_count = (@height * 15 - 25).to_i
-        "This year your tree grew to #{@height.round(1)}m tall," +
-          " and produced #{@orange_count} oranges."
-      else
-        "This year your tree grew to #{@height.round(1)}m tall," +
-          " but is still too young to bear fruit."
-      end
-    else
-      'A year later, the tree is still dead. :('
+    @age += 1
+    if @age < 26
+    @height += 0.4
+    @height = @height.round(1)
+     if @age > 4
+        @oranges = (@height * 15 - 25).to_i
+     end
+    return "This year your tree grew to #{@height}m tall, and produced #{@oranges} oranges."
+    elsif @age == 26
+    return "Oh, no! The tree is too old, and has died. :("
+      @height = 0
+      @oranges = 0
+    elsif @age > 26
+    return "A year later, the tree is still dead. :("
     end
   end
-
+  def count_the_oranges
+    if @age < 25
+    return @oranges
+    else
+    return 'A dead tree has no oranges. :('
+     end
+  end
   def pick_an_orange
-    if @alive
-      if @orange_count > 0
-        @orange_count = @orange_count - 1
-        'You pick a juicy, delicious orange!'
-      else
-        'You search every branch, but find no oranges.'
-      end
+    if @age < 25
+    @orange -= 1
+    return "you now have #{@oranges} oranges on the tree, and this one was delicious!"
     else
-      'A dead tree has nothing to pick. :('
+    return 'A dead tree has nothing to pick. :('
     end
   end
-end
-
+  def height
+    if @age < 25
+    return @height
+    else
+    return 'A dead tree is not very tall. :('
+    end
+  end
+  end

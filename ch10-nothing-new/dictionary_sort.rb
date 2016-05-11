965aa9e145b8ce73846fd37c8e39cc64c5c80772
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-11 15:20:22 +0100

    CH10 "dictionary sort" challenge completed and verified

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,12 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, arr.map(&:downcase),[]
+end
+
+def recursive_sort unsorted_array, unsorted_downcase, sorted_array
+  return sorted_array if unsorted_array == []
+  min_index = unsorted_downcase.index(unsorted_downcase.min)
+  sorted_array << unsorted_array[min_index]
+  unsorted_array.delete_at(min_index)
+  unsorted_downcase.delete_at(min_index)
+  recursive_sort unsorted_array, unsorted_downcase, sorted_array
 end

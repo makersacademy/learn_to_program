ea9f55724fa89317488ae99aff79b19b8e33eb9a
Van Le <van@niceandserious.com>
2016-05-11 15:06:11 +0100

    Completed ch10 - shuffling

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,12 @@
 def shuffle arr
-  # your code here
+  shuffled = []
+
+  arr.length.times do
+    so_Randumm = rand(arr.length)
+
+    shuffled << arr[so_Randumm]
+    arr.delete_at(so_Randumm)
+  end
+
+  shuffled
 end

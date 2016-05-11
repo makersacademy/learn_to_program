380a8a7d69f230a53e616543be08f47980b2472b
Marlon Date-Chong <mdatechong@gmail.com>
2016-05-11 14:54:29 +0100

    ch12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
 # your code here
+def happy_birthday
+
+puts "when were you born yyyy,mm,dd?"
+birth = gets.chomp.split(',')
+year = birth[0].to_i
+month = birth[1].to_i
+day = birth[2].to_i
+years_in_secs = Time.new - Time.local(year,birth,day)
+total = (years_in_secs / 31557600).to_i
+total.times {|x| print "SPANK!"}
+end
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,6 @@
 # your code here
+born = Time.local(1991,12,1,1,31,08)
+
+billion_secs_later = born + 1000000000
+
+billion_secs_later

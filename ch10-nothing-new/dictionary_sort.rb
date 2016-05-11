7e490ada2b1815eb89c2500becc1863ad4f2885b
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-11 03:41:23 +0200

    Implement dictionary_sort recursively

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,12 @@
 def dictionary_sort arr
-  # your code here
+  capitals = arr.select{|word| word == word.capitalize}
+  recursive_sort(arr, [], capitals) 
+end
+
+def recursive_sort some_arr, sorted_arr, capitals
+  some_arr.map!{|word| word.downcase}
+  return sorted_arr.map{|word| (capitals.include?(word.capitalize)) ? word.capitalize : word } if some_arr == [] 
+  sorted_arr.push(some_arr.min)
+  some_arr.delete_at(some_arr.index(some_arr.min))
+  recursive_sort(some_arr, sorted_arr, capitals)
 end

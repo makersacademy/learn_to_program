f04bce1d95135a3e83de9d59f256ccb8877ebe9e
Michael_Lo <Michael.Lo@live.co.uk>
2016-05-11 11:20:18 +0100

    completed shuffle and sort from chapter 10

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,17 @@
-def sort arr
-  # your code here
+def sort(array)
+  n = array.length
+  (n-1).times do
+  new_arr = []
+    n.times do
+      if array[1] == nil || array[0] < array[1]
+        new_arr.push << array[0]
+      array.delete_at(0)
+      else
+        new_arr << array[1]
+        array.delete_at(1)
+      end
+    end
+  array = new_arr
+  end
+  array
 end

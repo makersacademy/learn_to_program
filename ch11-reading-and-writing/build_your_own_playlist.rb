61ee1e7b1b0e96f33297e608d2b5e720409238cf
awye765 <awye765@googlemail.com>
2016-05-11 15:16:36 +0100

    CP CH11 - build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,24 @@
-# your code here
+@origin = "/Users/alistairwye/projects/music"
+@destination = "/Users/alistairwye/projects/music/playlists"
+
+Dir.chdir @destination
+# Changes the current directory to where the @destination varibale is pointed.
+playlist = Dir["#{@origin}/**/*.{mp3,m4a}"].shuffle
+# Creates an array of each 'mp3' and 'm4a' file in the origin directory,
+# shuffles it and assigns it to the variable 'playlist'.
+File.open 'playlist.m3u', 'w' do |f|
+  playlist.each do |m|
+    f.write m+"\n"
+# Creates a new file called 'playlist.m3u' and writes the file path for each
+# song in the origin directory to a new line in the 'playlist.m3u' file, which
+# looks something like this:
+#
+# /Users/alistairwye/Projects/music/The Smiths/Hatful of Hollow/07 Hand in glove.mp3
+# /Users/alistairwye/Projects/music/The Smiths/Hatful of Hollow/14 Back to the old house.mp3
+# /Users/alistairwye/Projects/music/The Smiths/Peel Session/04 Handsome Devil.mp3
+# ... and so on
+#
+  end
+end
+
+puts 'Done!'

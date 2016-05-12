eb8b65443250ca8dc773eadd416b5484581de7b1
Luke <lukeaveil@yahoo.com>
2016-05-12 15:42:21 +0100

    chapter 10, first challenge

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,9 @@
 def sort arr
-  # your code here
+  return arr if arr.length <= 1 # if the array only has one word it does not need sorting
+
+  middle = arr.pop # taking the last word in the array and assigning it to var middle
+  less = arr.select{|x| x < middle} # if word comes before the word in 'middle' we create a new array called less
+  more = arr.select{|x| x >= middle} #if word comes after the word in 'middle' we create a new array called more
+
+  sort(less) + [middle] + sort(more) # we then sort array 'less', then add it to array 'middle', then sort array 'more' and add it after array 'middle'
 end

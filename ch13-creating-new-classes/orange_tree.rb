43aaf0dbe98745c375b883f46852bbf29febd3cc
Toby Wilkins <tobywilkinsuk@hotmail.co.uk>
2016-05-12 16:24:21 +0100

    did orange trees ex

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,5 +7,62 @@
 
 
 class OrangeTree
-  # your code here
+  def initialize 
+  	@height = 0
+  	@years = 0
+  	@oranges = 0
+  	@dead = false
+  end
+
+  def height
+  	if @dead == false 
+  	return @height.round(1)
+  	else
+  	return 'A dead tree is not very tall. :('
+  	end
+  end
+
+  def one_year_passes
+  	@height += 0.4
+  	@years += 1
+  	if @dead == false
+  		if @years == 26
+  			@dead = true
+  			return 'Oh, no! The tree is too old, and has died. :('
+  		elsif @years >= 5 && @years <= 26
+  		@oranges = ((@height * 15)-25)
+  		return "This year your tree grew to #{@height.round(1)}m tall, and produced #{@oranges.to_i} oranges."
+  		else
+  		return "This year your tree grew to #{@height.round(1)}m tall, and produced #{@oranges.to_i} oranges."  			
+  		end
+  	else
+  	return 'A year later, the tree is still dead. :('
+  	end
+  end
+
+  def count_the_oranges
+  	if @dead == false 
+  		@oranges.to_i
+  	else
+  		return 'A dead tree has no oranges. :('
+  	end
+  end
+
+  def pick_an_orange
+  	if @dead == false 
+  	@oranges -= 1
+  	else
+  		return 'A dead tree has nothing to pick. :('
+  	end
+  end
 end
+
+tree = OrangeTree.new
+24.times do tree.one_year_passes end
+	puts tree.one_year_passes
+	print tree.one_year_passes
+
+
+
+
+

425e0a284036a7fb4bb2d79e2c392c31857a6d25
paulkenrick <pkenrick@gmail.com>
2016-05-12 12:44:30 +0700

    Completed Ch11 - build_your_own_playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,20 @@
-# your code here
+def shuffle arr
+  shuffled_array = []
+  while !arr.empty?
+  	random_num = rand(arr.length)
+  	shuffled_array << arr[random_num]
+  	arr.delete_at(random_num)
+  end
+  shuffled_array
+end
+
+Dir.chdir('/Users/paulkenrick/Desktop/Test_Playlist')
+all_songs = Dir['/Users/paulkenrick/Music/iTunes/iTunes Media/Music/*/*/*.m4a']
+
+song_string = shuffle(all_songs).first(10).join("\n")
+
+File.open('playlist.m3u', 'w') do |f|
+	f.write(song_string)
+end
+
+puts song_string

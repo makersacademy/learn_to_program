80b3954c661b8f718ebb0620e25af45784879269
paulkenrick <pkenrick@gmail.com>
2016-05-12 11:40:55 +0700

    Completed Ch11 - safeer_picture_downloading

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,30 @@
-# your code here
+Dir.chdir('/Users/paulkenrick/Desktop/Test_Photo_Outbox')
+pic_names = Dir['/Users/paulkenrick/Desktop/Test_Photo_Inbox/*.JPG']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+pic_names.each do |name|
+	print '.' # This is our "progress bar".
+	if pic_number < 10
+		new_name = "#{batch_name}0#{pic_number}.jpg"
+	else
+		new_name = "#{batch_name}#{pic_number}.jpg"
+	end
+
+	if File.exists?(new_name)
+		puts
+		puts "The distination folder already contains a file named #{new_name}\nThe program will stop without copying this file."
+		exit
+	else
+		File.rename(name, new_name)
+		pic_number = pic_number + 1
+	end
+end
+
+puts
+
+puts 'Transfer Complete'

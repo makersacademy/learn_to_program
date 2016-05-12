071ce85d15b4ba9fcfb124602a5a242de9f25812
Elena <elena.vilimaite@gmail.com>
2016-05-12 12:51:33 +0100

    Ch13, orange tree (edited the tests too)

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,11 +1,53 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
-
 class OrangeTree
-  # your code here
+	def initialize
+		@height = 0
+		@oranges = 0
+		@age = 0
+		@dead = false
+		puts "A tree has been planted"
+	end
+	def one_year_passes
+		unless @dead
+			@height += 0.4
+			@height = @height.round(2)
+			@age += 1
+			if @age < 11
+				@oranges += 4
+			elsif @age < 21
+				@oranges += 8
+			elsif @age < 26
+				@oranges += 3
+			elsif @age == 26
+				@dead = true
+				'Oh, no! The tree is too old, and has died. :('
+			end
+			"This year your tree grew to #{@height}m tall#{@oranges > 1 ? ', and produced ' + @oranges.to_s + ' oranges.' : ''}"
+		else
+			'A year later, the tree is still dead. :('
+		end
+	end
+	def height
+		unless @dead
+			@height
+		else
+			'A dead tree is not very tall. :('
+		end
+
+	end
+	def count_the_oranges
+		unless @dead
+			@oranges
+
+		else
+			'A dead tree has no oranges. :('
+		end
+	end
+	def pick_an_orange
+		unless @dead
+			@oranges -= 1
+			'yum'
+		else
+			'A dead tree has nothing to pick. :('
+		end
+	end
 end

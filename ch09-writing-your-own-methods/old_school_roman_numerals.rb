a259b192bf3ec7ef9cbd23964788ef55e04272ea
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-12 13:03:48 +0100

    Learn To Program: Old_school_roman_numerals.rb Complete

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,39 @@
-def old_roman_numeral num
+def old_roman_numeral number
+
+one_nums = {1000 => "M",
+	500 => "D",
+	100 => "C",
+	50 => "L",
+	10 => "X",
+	5 => "V",
+	1 => "I"
+}
+
+to_return = ""
+
+one_nums.each do |k, v|
+	to_return << v * (number / k) 
+	number = (number % k)
+end
+to_return
+
+
+	# until number - k = 0 keep minusing the number by the keys and printing the values 
+
+#until number == 0
+#	number - one_nums a really quick amount of times 
+
+
+
+
+
+
+#	number =< 1 && number > 5
+#		one_nums[1] * number
+#
+#	if number <= 5
+#		number.
   # your code here
 end
+
+

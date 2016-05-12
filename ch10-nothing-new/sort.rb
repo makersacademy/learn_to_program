16a4906737f80a6c38a5699c140f3b031059b1da
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-12 15:48:23 +0100

    Learn To Program: sort.rb Complete

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,19 @@
 def sort arr
-  # your code here
+	num = arr.length
+	loop do 
+		sorted = false 
+
+		(num-1).times do |i|
+			if arr[i] > arr[i+1]
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+			sorted = true
+		end 
+	end
+	break if not sorted 		
+	end
+	arr
 end
+
+  # your code here
+
+

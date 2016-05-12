04d736f580106a8d5345d2562e2e8efd3b71b402
Carlos <chdezmar@gmail.com>
2016-05-12 18:52:22 +0200

    Chapter 9

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,26 @@
 def roman_numeral num
-  # your code here
+  romans = {
+     1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+       90 => "XC",
+       50 => "L",
+       40 => "XL",
+       10 => "X",
+         9 => "IX",
+         5 => "V",
+         4 => "IV",
+         1 => "I",
+   }
+
+   n = num
+   roman = ""
+   romans.each do |eng, roman_letter|
+     roman << roman_letter * (n / eng)
+     n = n % eng
+   end
+   return roman
+
 end

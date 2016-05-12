7ecb7e3c2a3bbaab362c5bbe6e3b2538067e69bc
apsaxena24 <apsaxena24@gmail.com>
2016-05-12 17:24:20 +0100

    interactive_baby_dragon and orange_tree

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,27 @@
-# your code here
+puts "Please enter a name for the baby dragon"
+name = gets.chomp
+pet = Dragon.new name
+
+while true
+	puts 'commands: feed, toss, walk, rock, put to bed, exit'
+	command = gets.chomp
+
+	puts case command
+	when "feed"
+		pet.feed
+	when 'toss'
+		pet.toss
+	when "walk"
+		pet.walk
+	when "rock"
+		pet.rock
+	when "put to bed"
+		pet.put_to_bed
+	when "exit"
+		exit
+	end
+end
+
+
+
+

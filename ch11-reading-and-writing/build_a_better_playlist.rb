2ffa1e116d47e33e07a72483f7fb34c36222cc1e
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-12 12:26:59 +0200

    Implement playlist builder with better shuffle

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,35 @@
-def music_shuffle filenames
-  # your code here
+begin
+    Dir.mkdir("#{Dir.home}/temp_muzak/playlists")
+  rescue
+    puts "Folder exists"
+  ensure
+    Dir.chdir("#{Dir.home}/temp_muzak/playlists")
+end
+
+all_music = mixup_shuffle Dir["../my_music/*"]
+
+File.open("./playlist.m3u", "w")do |f|
+  all_music.each {|song| f.write(song + "\n")}
+end
+
+  #selects the next song randomly and checks the artist: if different than previous
+  #then keeps, otherwise selects a new random song, unless there are no new artists
+def mixup_shuffle arr
+  recursive_shuffle(arr, [])
+end
+
+def recursive_shuffle(array, shuffled)
+  return shuffled if array == []
+  
+  current_item = array.last
+  next_item = array[rand(array.size)]
+  artist_regex = /\/\w*--/
+  
+  while current_item.match(artist_regex)[0] != next_item.match(artist_regex)[0]  
+    next_item = array[rand(array.size)]
+    break if array.all? {|song| current_item.match(artist_regex)[0] == song }
+  end
+
+  shuffled << array.delete_at(array.index(next_item)])
+  recursive_shuffle(array, shuffled)
 end

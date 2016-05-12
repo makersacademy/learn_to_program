9874b2f996b73945444fccb7386c026e27975bd9
Stephen Dawes <sdawes@outlook.com>
2016-05-12 16:10:46 +0100

    ch12-new-classes-of-objects/happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,32 @@
+def happy_birthday
+
+  puts "What year where you born in?"
+  year = gets.chomp.to_i
+  puts "What month where you born in? (NUMBER Please)"
+  month = gets.chomp.to_i
+  puts "What day where you born in? (NUMBER Please)"
+  day = gets.chomp.to_i
+
+  birthday = Time.local(year, day, month)
+  now = Time.new
+
+  age = now.year - birthday.year
+  if now.month > birthday.month
+    age = age - 1
+  end
+
+  puts "SPANK!" + \n * 38
+
+end
+
+
+
+
+
+
+
+
+
+
+
 # your code here

ba484ea6178cfc749da504b76c1bcc7dd558e1c3
Matt Butler <mattb-w@hotmail.co.uk>
2016-05-12 19:27:37 +0000

    Ch12 Complete

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,2 @@
-# your code here
+puts "SPANK" * ((Time.gm(1993, 6, 25) - Time.new)/ 60 / 60 / 24 / 365).abs.to_i
+
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,27 @@
 def roman_to_integer roman
-  # your code here
+  cval = {
+    "i" => 1,
+    "v" => 5,
+    "x" => 10,
+    "l" => 50,
+    "c" => 100,
+    "m" => 1000,
+  }
+  
+  total = 0
+  roman.downcase!
+  roman.each_char.with_index do |char, i|
+    if i < roman.size - 1
+      if cval[char] < cval[roman[i+1]]
+       total -= cval[char]
+      else
+        total += cval[char]
+      end
+    else
+      total += cval[char]
+    end
+  end
+    p total 
 end
+
+roman_to_integer('CCCLXV')

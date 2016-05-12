31a945ef0d58ef2cf8b45fc988f51862edb681e7
Luke <lukeaveil@yahoo.com>
2016-05-12 16:24:38 +0100

    updated my notes

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -5,5 +5,5 @@ def sort arr
   less = arr.select{|x| x < middle} # if word comes before the word in 'middle' we create a new array called less
   more = arr.select{|x| x >= middle} #if word comes after the word in 'middle' we create a new array called more
 
-  sort(less) + [middle] + sort(more) # we then sort array 'less', then add it to array 'middle', then sort array 'more' and add it after array 'middle'
+  sort(less) + [middle] + sort(more) # we then add 'less', to array 'middle', then add array 'more' after array 'middle'
 end

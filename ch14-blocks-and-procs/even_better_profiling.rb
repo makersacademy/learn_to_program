162e34072b9339d9aeea4efbccb3c902156f2b88
Michael_Lo <Michael.Lo@live.co.uk>
2016-05-13 10:45:05 +0100

    ch 14 exercise

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,14 @@
 def profile block_description, &block
-  # your code here
+  exit # addition or removal of keyword exit stios or executes the program
+  start_time = Time.new
+  block.call
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+end                                                 # => :profile
+
+profile '25000 doublings' do
+  number = 1
+  25000.times do
+      number = number + number
+  end
 end

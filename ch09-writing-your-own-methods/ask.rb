94b9bb4f437a15a2d5c5998a393732fdfcd6bc56
Andy Bell <andy.bell.github@gmail.com>
2016-05-13 10:52:11 +0000

    ch09 ask & old numerals complete

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,6 @@
 def ask question
   # your code here
+	puts question
+	reply = gets.chomp.downcase
+	reply == "yes" ? return true : (reply == "no" ? return false : puts('Please answer "yes" or "no".')
 end

03a8e2dcacb8950f3e7d380feaf2b682ca7b5282
Andy Bell <andy.bell.github@gmail.com>
2016-05-13 22:09:23 +0000

    completed One billion seconds, happy birthday and roman integer, with tests passing

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
 # your code here
+
+def happy_birthday
+	puts "what year were you born in?"
+	year = gets.chomp
+	puts "what number month were you born in?"
+	month = gets.chomp
+	puts "what date were you born on?"
+	date = gets.chomp
+	
+	
+	time = Time.new
+	age = (time - Time.local(year, month, date))/(3600*24*365)
+	puts "that would make you #{age.to_i} years old, correct?"
+	answer = gets.chomp
+	age.to_i.times{puts "SPANK!"} if answer.downcase == "yes"
+end
+
+happy_birthday
+

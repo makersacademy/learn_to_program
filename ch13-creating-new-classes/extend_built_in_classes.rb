3e705b9f86fb28583c12872cbee93b0609d92362
Matt Butler <mattb-w@hotmail.co.uk>
2016-05-13 00:32:20 +0000

    ch13 2/3 Complete

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,26 @@
 class Integer
-  # your code here
+  
+  def to_roman
+    num = self
+    no_m = num / 1000
+    num = num % 1000
+    no_d = num / 500
+    num = num % 500
+    no_c = num / 100
+    num = num % 100
+    no_l = num / 50
+    num = num % 50
+    no_x = num / 10
+    num = num % 10
+    no_v = num / 5
+    num = num % 5
+    no_i = num / 1
+  
+    return ("M"*no_m) + ("D"*no_d) + ("C"*no_c) + ("L"*no_l) + ("X"*no_x) + ("V"*no_v) + ("I"*no_i)
+  end
+  
+  def factorial
+    self == 0 ? 1 : self * (self-1).factorial
+  end
 end
+

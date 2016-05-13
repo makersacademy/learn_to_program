b59a00999c95be3ae1486035b0ac72ef3b412b7a
Van Le <van@niceandserious.com>
2016-05-13 14:30:14 +0100

    Completed ch10 - english_num and 99 bottles :beer:

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,143 @@
-# your code here
+# Using english_number and your old program, write out the lyrics to this song the
+# right way this time. Punish your computer: have it start at 9999.
+
+def english_number number
+  if number < 0 # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
+  elsif number == 0
+    return 'zero'
+  end
+
+  # No more special cases! No more returns!
+  num_string = '' # This is the string we will return.
+  ones_place = ['one',       'two',      'three',
+                'four',      'five',     'six',
+                'seven',     'eight',    'nine']
+  tens_place = ['ten',       'twenty',   'thirty',
+                'forty',     'fifty',    'sixty',
+                'seventy',   'eighty',   'ninety']
+  teenagers  = ['eleven',    'twelve',   'thirteen',
+                'fourteen',  'fifteen',  'sixteen',
+                'seventeen', 'eighteen', 'nineteen']
+
+  zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
+
+  # "left" is how much of the number we still have left to write out.
+  # "write" is the part we are writing out right now.
+  # write and left...get it?  :)
+  left = number
+
+
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base # How many zillions left?
+
+    write.to_s + ", " + zil_name
+    left = left - write*zil_base # Subtract off those zillions.
+
+    if write > 0
+      # Now here's the recursion:
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+        # So we don't write 'two billionfifty-one'...
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+  write = left/100 # How many hundreds left?
+  left = left - write*100 # Subtract off those hundreds.
+
+  if write > 0
+    # Now here's the recursion:
+    hundreds = english_number write
+    num_string = num_string + hundreds + ' hundred'
+
+    if left > 0
+      #  So we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/10 # How many tens left?
+  left = left - write*10 # Subtract off those tens.
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      #  Since we can't write "tenty-two" instead of
+      #  "twelve", we have to make a special exception
+      #  for these.
+
+      num_string = num_string + teenagers[left-1]
+      # The "-1" is because teenagers[3] is
+      # 'fourteen', not 'thirteen'.
+      # Since we took care of the digit in the
+      # ones place already, we have nothing left to write.
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+      #  The "-1" is because tens_place[3] is
+      # 'forty', not 'thirty'.
+    end
+
+    if left > 0
+      # So we don't write 'sixtyfour'...
+      num_string = num_string + '-'
+    end
+
+  end
+
+  write = left # How many ones left to write out?
+  left = 0 # Subtract off those ones.
+
+  if write > 0
+    num_string = num_string + ones_place[write-1] # The "-1" is because ones_place[3] is
+  # 'four', not 'three'.
+  end
+
+  #  Now we just return "num_string"...
+  num_string
+end
+
+# Checks if 'bottle' should be plural or singular
+def pluralBottle count
+  if count == 1
+    return 'bottle'
+  else
+    return 'bottles'
+  end
+end
+
+# bottles = 5
+
+while bottles > 0
+  puts "#{(english_number bottles).capitalize} #{pluralBottle bottles} of beer on the wall, #{english_number bottles} #{pluralBottle bottles} of beer."
+  bottles -= 1
+  puts "Take one down and pass it around, #{english_number bottles} #{pluralBottle bottles} of beer on the wall."
+end

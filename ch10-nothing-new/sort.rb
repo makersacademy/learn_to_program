aee3aaedfa13276858d1386a7941f180d796ea18
Lukasz <lukasz.grychowski@gmail.com>
2016-05-13 14:57:21 +0100

    sort.rb done

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,23 @@
 def sort arr
-  # your code here
+  recursive_sort(arr, [])
+end
+
+def recursive_sort(unsorted, sorted)
+  return sorted if unsorted.empty?
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |word|
+    if word < smallest
+      still_unsorted << smallest
+      smallest = word
+    else
+      still_unsorted << word
+    end
+  end
+
+  sorted << smallest
+
+  recursive_sort(still_unsorted, sorted)
 end

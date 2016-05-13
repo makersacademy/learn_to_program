1f893f203a4bd8bc3b05c9d52b5000bc8affa0d2
Oliver Diamond <diamond.oliver@gmail.com>
2016-05-13 17:38:48 +0100

    Ch09 done

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,8 @@
 def old_roman_numeral num
-  # your code here
+	numerals = {"I" => 1,"V" => 5,"X" => 10,"L" => 50,"C" => 100,"D" => 500,"M" => 1000}
+	numerals.sort_by{|k,v| -v}.inject(""){|sum,(k,v)| 
+		i = num/v
+		num -= v*i
+		sum << k*i
+	}
 end

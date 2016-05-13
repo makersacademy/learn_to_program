a2e741d34bd28b023f44c5570508c3063cea0dff
Michael_Lo <Michael.Lo@live.co.uk>
2016-05-13 13:00:21 +0100

    ch14 program logger exercise complete

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+    puts "Beginning \"#{desc}\"..."
+    result = block.call
+    puts "...\"#{desc}\" finished, returning: #{result}"
+
 end
+
+# program_log 'outer block' do
+#   program_log 'some little block' do
+#      'hello'
+#   end
+#   program_log 'yet another block'do
+#      'goodbye'
+#   end
+#    false
+# end

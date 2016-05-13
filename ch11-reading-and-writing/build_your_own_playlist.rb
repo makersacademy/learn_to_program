4cc2defb914bf6860e1af4508e491d63cdbea74f
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-13 14:59:47 +0100

    CH11 "build your own playlist" completed

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,17 @@
-# your code here
+def shuffle arr
+  shuffled = []
+  for i in 0...arr.length do
+  	rand = rand(arr.length)
+  	shuffled << arr[rand]
+  	arr.delete_at(rand)
+  end
+  shuffled
+end
+
+song_names = shuffle(Dir['**/*.mp3'])
+
+File.open 'Playlist.m3u', 'w' do |f|
+  song_names.each do |x|
+    f.write x +"\n"
+  end
+end

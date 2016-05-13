14fbdb8fcec571ecf770d2de73a680d6baad52f4
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-13 14:32:22 +0100

    Learn To Program: ninety_nine_bottles_of_beer.rb Complete

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,26 @@
-# your code here
+require "./english_number.rb"
+
+
+
+def bottles_of_beer(bottles)
+    if bottles > 1
+      (bottles - 1).times do 
+        puts "#{english_number bottles} bottles of beer on the wall,"       ,
+        "#{english_number bottles} bottles of beer,"           ,
+        "Take one down, pass it around,"         ,
+        "#{english_number (bottles -= 1)} #{bottles == 1 ? "bottle" : "bottles"} of beer on the wall."
+      end
+      puts "One bottle of beer on the wall,"        ,
+           "One bottle of beer,"                    ,
+           "Take one down, pass it around,"                         ,
+           "Zero bottles of beer on the wall."
+    elsif @num_bottles == 1
+      puts "One bottle of beer on the wall,"        ,
+           "One bottle of beer,"                    ,
+           "Take one down, pass it around,"                         ,
+           "Zero bottles of beer on the wall."
+     else
+      puts ""
+      end
+    end
+

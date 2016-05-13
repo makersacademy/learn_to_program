c5c05a17a9f498f2eb03391c4aa42c1b527e524e
Lukasz <lukasz.grychowski@gmail.com>
2016-05-13 15:52:02 +0100

    ch10 dictionary sort done

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort(arr, [])
+end
+
+def recursive_sort(unsorted, sorted)
+  return sorted if unsorted.empty?
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |word|
+    if word.downcase < smallest.downcase
+      still_unsorted << smallest
+      smallest = word
+    else
+      still_unsorted << word
+    end
+  end
+
+  sorted << smallest
+
+  recursive_sort(still_unsorted, sorted)
 end

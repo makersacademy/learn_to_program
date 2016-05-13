71324db31c86ccd1505278e4cb525b19dabfa0a9
Luke <lukeaveil@yahoo.com>
2016-05-13 14:17:59 +0100

    chapter 11, second challenge

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,9 @@
-# your code here
+directory = '/Users/lukeaveil/projects/learn_to_program/music'
+
+playlist = Dir['#{directory}**/*.{mp3, MP3}'].shuffle
+
+File.open 'playlist.m3u' , 'w' do |x|
+	playlist.each do |song|
+		x.write song + "\n"
+	end
+end

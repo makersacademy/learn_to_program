c3f2245895d58d9c2e4eac9174688a199a8d73d8
Carlos <chdezmar@gmail.com>
2016-05-14 20:28:53 +0200

    Chapters 11 and 12

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
-# your code here
+require "./ch10-nothing-new/shuffle.rb"
+
+all_mp3 = shuffle(Dir['**/*.mp3'])
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_mp3.each do |mp3|
+    f.write mp3+"\n"
+  end
+end
+
+puts 'Done!'

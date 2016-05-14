a32366edcdf41e60b68a0db4d9f86ab56beca016
apsaxena24 <apsaxena24@gmail.com>
2016-05-14 19:41:57 +0100

    ch-13 built in classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,32 +1,21 @@
 class Integer
   def factorial
-    if self <= 1
-      1
-    else
-      self * (self-1).factorial
-    end
-  end
-  def to_roman
-  	roman = [
-          ['M', 1000],
-          ['CM', 900],
-          ['D', 500],
-          ['CD', 400],
-          ['C', 100],
-          ['XC', 90],
-          ['L', 50],
-          ['XL', 40],
-          ['X', 10],
-          ['VIIII', 9],
-          ['V', 5],
-          ['IIII', 4],
-          ['I', 1]
-          ]
+    raise "Please enter a positive number" if self < 0  
+    self <= 1 ? 1 : self * (self-1).factorial                              
+  end                                                                      
 
-  return '' if self == 0
-  roman.each do |str, int|
-    if self >= int
-      return str + (self - int).to_roman
-    end
-  end
-end
+  def to_roman
+    letters = [M,D,C,L,X,VI]            
+    values  =   [ 1000, 500, 100, 50, 10, 5, 1 ]            
+    romans = letters.zip(values)                           
+    if self.between?(1, 3000)                               
+      num = self                                            
+      c = 0                                                 
+      romans.map{|l,v| c, num = num.divmod v; l*c}.join ''  
+    end                                                     
+  end                                                       
+
+end  
+
+# 3.factorial  
+# 4.to_roman

4eec4bc39a44eaaef63d3f4c5f5796db88434ac1
Elena <elena.vilimaite@gmail.com>
2016-05-14 21:57:22 +0100

    Ch14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,11 @@
 def log desc, &block
   # your code here
 end
+
+
+# def program_log desc, &block
+# 	puts "Beginning \"#{desc}\"..."
+# 	blockReturn =  block[]
+	
+# 	puts "...\"#{desc}\" finished, returning: #{blockReturn}"
+# end
--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
 def grandfather_clock &block
-  # your code here
+	current_time = (Time.new.hour)
+	current_time = case current_time
+	when 0, 12
+		12
+	when 13 .. 23
+		current_time - 12
+	else
+		current_time
+	end
+	current_time.times(&block)
+end
+
+grandfather_clock do 
+	puts 'DONG!' 
 end

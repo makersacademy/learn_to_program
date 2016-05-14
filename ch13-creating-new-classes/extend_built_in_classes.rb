3e3000018093a6a1ebbd0260e36a26070039a9ed
apsaxena24 <apsaxena24@gmail.com>
2016-05-14 19:48:30 +0100

    few changes in factorial

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,21 +1,31 @@
 class Integer
-  def factorial
-    raise "Please enter a positive number" if self < 0  
-    self <= 1 ? 1 : self * (self-1).factorial                              
-  end                                                                      
+    
+NUMERALS = {1000 => "M",
+  500 => "D",
+  100 => "C",
+  50 => "L",
+  10 => "X",
+  5 => "V",
+  1 => "I"
+}
 
-  def to_roman
-    letters = [M,D,C,L,X,VI]            
-    values  =   [ 1000, 500, 100, 50, 10, 5, 1 ]            
-    romans = letters.zip(values)                           
-    if self.between?(1, 3000)                               
-      num = self                                            
-      c = 0                                                 
-      romans.map{|l,v| c, num = num.divmod v; l*c}.join ''  
-    end                                                     
-  end                                                       
+def factorial
+    if self == 0
+    1
+    else
+    p self*(self-1).factorial
+    end
+end
 
-end  
+def to_roman
 
-# 3.factorial  
-# 4.to_roman
+  n = self
+  numbers = ""
+  NUMERALS.each do |english, roman|
+    numbers << roman * (n / english)
+    n = n % english
+  end
+  p numbers
+end
+
+end

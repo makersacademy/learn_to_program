40b0a5de26c6152ec17055b4f8bee42f25912d4d
Luke <lukeaveil@yahoo.com>
2016-05-14 11:43:52 +0100

    chapter 12, second challenge

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+def its_your_birthday
+
+	puts "What year were you born? (yyyy) format please"
+	year = gets.chomp.to_i
+
+	puts "What month were you born? (mm) format please"
+	month = gets.chomp.to_i
+
+	puts "What day were you born? (dd) format please"
+	day = gets.chomp.to_i
+
+	age = (Time.now - Time.new(year, month, day)) / 60 / 60 / 24 / 365
+
+	birthday_spank = "SPANK!" * age
+
+	puts birthday_spank
+
+end

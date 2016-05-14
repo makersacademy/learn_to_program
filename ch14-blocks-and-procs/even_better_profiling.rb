58274d1c2b6f1481bd46b0aea3b1478d1dedf325
apsaxena24 <apsaxena24@gmail.com>
2016-05-14 13:05:22 +0100

    better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,12 @@
 def profile block_description, &block
-  # your code here
+profiling = true
+ if profiling == true
+ 	then 
+  		begin_time = Time.new
+  		block.call
+  		dur = Time.new - begin_time
+  		puts "#{block_description}: #{dur} seconds"
+ 	else
+  		block.call
+ end
 end

24a41d0633379c7d37c5059e3507b85ad35424f8
Lukasz <lukasz.grychowski@gmail.com>
2016-05-14 20:04:24 +0100

    ch12 birthday helper done

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,23 @@
-# your code here
+birthdays = {}
+
+File.read("b_days.txt").each_line do |line|
+  line = line.chomp
+  name = line[0..21].strip
+  date = line[-12..-1]
+  birthdays[name] = date
+end
+
+puts "Whose birthday would you like to know?"
+name = gets.chomp
+date = birthdays[name]
+
+if date == nil
+  puts "I don't know that one."
+else
+  puts date[0..5]
+end
+
+
+
+
+

c399d7a37e518888656c4e39f6422821a61c6e19
paulkenrick <pkenrick@gmail.com>
2016-05-14 06:52:43 +0100

    Completed Ch12 - happy_birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,14 @@
-# your code here
+print "What year were you born? "
+year = gets.chomp
+print "What month were you born? "
+month = gets.chomp
+print "What day were you born? "
+day = gets.chomp
+
+birth_date = Time.local(year, month, day, 12)
+age_secs = Time.now - birth_date
+age_years = age_secs.to_f / 60 / 60 / 24 / 365
+
+age_years.to_i.times do
+	puts "SPANK"
+end

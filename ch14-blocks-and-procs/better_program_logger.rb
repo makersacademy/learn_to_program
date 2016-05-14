4a021c9a4f67a7b40485588372471c729efe839f
apsaxena24 <apsaxena24@gmail.com>
2016-05-14 12:54:53 +0100

    changes in ch-14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,24 +1,17 @@
 $space_depth = 0
-def log desc, &block
-	prefix = ' '*$logger_depth
-	puts prefix+"Beginning #{desc.inspect}..."
-	  $logger_depth += 1
-	  result = block[]
-	  $logger_depth -= 1
-    puts prefix+"...#{desc.inspect} finished, returning: #{result}"
-  
+
+def better_log desc, &block
+  puts "#{" " * $call_depth}Beginning \"#{desc}\"..."
+  	$space_depth += 1
+  	response = block.call
+  	$space_depth -= 1
+  puts "#{" " * $space_depth}...\"#{desc}\" finished, returning: #{response}"
 end
 
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
-      'lOtS oF lOVe'.downcase
-    end
-    7 * 3 * 2
-  end
-
-  log 'yet another block' do
-    '!doof naidnI evol I'.reverse
-  end
-  '0' == "0"
+chain = Proc.new do
+  better_log('some little block'){5} 
+  better_log('yet another block'){"I love thai food!"}
+  false
 end
+
+better_log 'outer block', &chain

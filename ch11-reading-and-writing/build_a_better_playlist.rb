e410ab4c35baa933afd437756b9dc11198ec2390
paulkenrick <pkenrick@gmail.com>
2016-05-14 06:12:09 +0100

    Completed Ch11 - build_a_better_playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,31 @@
-def music_shuffle filenames
-  # your code here
+def music_shuffle arr
+  shuffled_array = []
+  while !arr.empty?
+  	random_num = rand(arr.length)
+  	if shuffled_array == []
+  		shuffled_array << arr[random_num]
+  		arr.delete_at(random_num)
+  	else
+  		if arr[random_num].split("/")[-3] != shuffled_array.last.split("/")[-3]
+  			shuffled_array << arr[random_num]
+  			arr.delete_at(random_num)
+  		end
+  	end
+  end
+  shuffled_array
 end
+
+
+Dir.chdir('/Users/paulkenrick/Desktop/Test_Playlist')
+all_songs = Dir['/Users/paulkenrick/Music/iTunes/iTunes Media/Music/*/*/*.m4a']
+
+song_string = music_shuffle(all_songs).first(10).join("\n")
+
+File.open('playlist.m3u', 'w') do |f|
+	f.write(song_string)
+end
+
+puts song_string
+
+
+

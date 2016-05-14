df22c6d65df3adb2551d573de0004cf9ecf7cc5c
Hassan Radheyyan <hassan.rad@gmail.com>
2016-05-14 06:45:25 +0000

    Exercise 10:1

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,24 @@
-def sort arr
-  # your code here
+def sort some_array # This "wraps" recursive_sort.
+recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+if unsorted_array.length <= 0
+	return sorted_array
+end
+
+temp_array = []
+smallest = unsorted_array.pop
+unsorted_array.each do |x|
+	if x < smallest
+		temp_array << smallest
+		smallest = x
+	else
+		temp_array << x
+	end
+end
+
+sorted_array << smallest 
+
+recursive_sort temp_array, sorted_array
 end

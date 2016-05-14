c219f4e3bb7ffd9df56a4d04b4dc4e3a2c7e1748
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-14 11:23:52 +0100

    Add ch11 - build_your_own_playlist - Update

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -4,7 +4,7 @@ end
 
 all_m4a = shuffle(Dir['/Users/tomharker/Projects/Playlist/*.m4a'])
 
-File.open 'playlist.txt', 'w' do |x|
+File.open 'playlist.m3u', 'w' do |x|
     all_m4a.each do |m4a|
         x.write("#{m4a}" + "\n")
     end

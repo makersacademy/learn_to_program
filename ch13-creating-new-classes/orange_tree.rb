d8572f64b77167fcb304fd0189c96b1981d01fd7
paulkenrick <pkenrick@gmail.com>
2016-05-14 11:23:49 +0100

    Completed Ch13 - orange_tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,5 +7,79 @@
 
 
 class OrangeTree
-  # your code here
+  def initialize
+  	@height = 0
+  	@age = 0
+  	@no_of_oranges = 0
+  end
+
+  def height
+  	if @age > 25
+  		"A dead tree is not very tall. :("
+  	else
+  		@height.round(1)
+  	end
+  end
+
+  def age
+  	@age
+  end
+
+  def one_year_passes
+  	@no_of_oranges = 0
+  	@age += 1
+  	if @age <= 25
+  		@height += 0.4
+  	elsif @age == 26
+  		return "Oh, no! The tree is too old, and has died. :("
+  	else
+  		return "A year later, the tree is still dead. :("
+  	end
+
+  	if @age >= 6 && @age <= 25
+  		@no_of_oranges = (@height * 15 - 25).to_i
+  	end
+  	"This year your tree grew to #{@height.round(1)}m tall, and produced #{@no_of_oranges} oranges."
+  end
+
+  def count_the_oranges
+  	if @age > 25
+  		"A dead tree has no oranges. :("
+  	else
+  		@no_of_oranges
+  	end
+  end
+
+  def pick_an_orange
+  	if @age > 25
+  		"A dead tree has nothing to pick. :("
+  	elsif @no_of_oranges > 0 
+  		@no_of_oranges -= 1
+  		"That was delicious!"
+  	else
+  		"Sorry, there are no oranges to pick."
+  	end
+  end
 end
+
+# ot = OrangeTree.new
+
+# 23.times do
+#   ot.one_year_passes
+# end
+
+# puts ot.one_year_passes #'This year your tree grew to 9.6m tall, and produced 119 oranges.'
+# puts ot.count_the_oranges #119
+# puts ot.height #9.6
+# puts ot.one_year_passes #'This year your tree grew to 10.0m tall, and produced 125 oranges.'
+# puts ot.one_year_passes #'Oh, no! The tree is too old, and has died. :('
+# puts ot.one_year_passes #'A year later, the tree is still dead. :('
+# puts ot.one_year_passes #'A year later, the tree is still dead. :('
+# puts ot.one_year_passes #'A year later, the tree is still dead. :('
+# puts ot.height #'A dead tree is not very tall. :('
+# puts ot.count_the_oranges #'A dead tree has no oranges. :('
+# puts ot.pick_an_orange #'A dead tree has nothing to pick. :('
+
+
+
+

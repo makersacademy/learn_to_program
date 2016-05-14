98ada4f2940478078320b4fe962d61ea8daf2033
Luke <lukeaveil@yahoo.com>
2016-05-14 14:07:54 +0100

    chapter 13, first challenge

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,40 @@
 class Integer
-  # your code here
+	def factorial
+		if self <= 1
+			1
+		else
+			self * (self-1).factorial
+		end
+	end
+
+	def old_roman_numeral
+
+		num = self
+
+		numeral_hash = {
+			'M' => 1000,
+			'D' => 500,
+			'C' => 100,
+			'L' => 50,
+			'X' => 10,
+			'V' => 5,
+			'I' => 1
+		}
+
+		numeral_hash.each do |numeral, number|
+			numerals << numeral * (num / number)
+
+			num = num % number
+		end
+
+		numerals
+		
+	end
+
+end
+
+class Array
+	def shuffle
+		sort_by(rand)
+	end
 end

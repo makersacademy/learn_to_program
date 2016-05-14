4beec64c5dbdd120894725c545d4fc0121e3f88b
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-14 20:05:52 +0100

    Add ch12 - happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+def many_birthdays
+
+	puts "What year (yyyy) were you born?"
+	year = gets.chomp.to_i
+
+	puts "What month (mm) were you born?"
+	month = gets.chomp.to_i
+
+	puts "What day (dd) were you born?"
+	day = gets.chomp.to_i
+
+	age = (Time.now - Time.new(year, month, day))/ 60 / 60 / 24 / 365
+
+	birthday_spank = "SPANK!\n" * age
+
+	puts birthday_spank
+
+end

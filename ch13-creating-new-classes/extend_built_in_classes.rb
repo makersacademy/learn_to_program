ffb4df6673fe8b2053897d2f02dd5264fca87b4a
Oliver Diamond <diamond.oliver@gmail.com>
2016-05-15 13:42:09 +0100

    CH13 Done

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,22 @@
 class Integer
-  # your code here
+	def factorial
+    	(self <= 1) ? 1 : self * (self-1).factorial
+ 	end
+	
+	def to_roman
+		roman_numeral(self)
+	end
+end	
+def roman_numeral num
+	numerals = {"I" => 1,"V" => 5,"X" => 10,"L" => 50,"C" => 100,"D" => 500,"M" => 1000}
+	# add_subtraction_numerals(numerals) - because the test is shit
+	numerals.sort_by{|k,v| -v}.inject(""){|sum,(k,v)|
+		i = num/v
+		num -= v*i
+		sum << k*i
+	}
+end
+
+def add_subtraction_numerals n
+	n.keys.permutation(2).to_a.each{|a,b| n[a+b] = n[b]-n[a] unless (n.values.include?(n[b]-n[a]) || n[b]-n[a]  < 0)}
 end
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,5 +7,58 @@
 
 
 class OrangeTree
-  # your code here
+	
+	@@age_to_start_fruit = 6
+	@@age_to_die = 25
+	@@growth_rate = 0.4
+
+
+  def initialize
+    @height = @age = @orange_count = 0
+    @alive = true
+  end
+
+  def height
+    @alive ? @height.round(1) : 'A dead tree is not very tall. :('
+  end
+
+  def count_the_oranges
+    @alive ? @orange_count : 'A dead tree has no oranges. :('
+  end
+
+  def one_year_passes
+    if @alive
+   		@age +=1
+    	@height += 0.4
+    	@orange_count = 0
+    	if @age > @@age_to_die
+    		@alive = false
+      	'Oh, no! The tree is too old, and has died. :('
+  	  elsif @age >= @@age_to_start_fruit
+      	@orange_count = new_oranges
+      	"This year your tree grew to #{@height.round(1)}m tall, and produced #{@orange_count} oranges."
+    	else
+      	"This year your tree grew to #{@height.round(1)}m tall, but is still too young to bear fruit."
+  	  end
+  	else
+      'A year later, the tree is still dead. :('
+    end
+  end
+
+  def pick_an_orange
+    if @alive
+      if @orange_count > 0
+        @orange_count -= 1
+        'You pick a juicy, delicious orange!'
+      else
+        'You search every branch, but find no oranges.'
+      end
+    else
+      'A dead tree has nothing to pick. :('
+    end
+  end
+
+  def new_oranges
+  	(height * 15 - 25).round(0)
+  end
 end

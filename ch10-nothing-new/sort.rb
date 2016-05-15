a433136607e5be3dff5a5a094a0b5d577c2f8be5
joestephens <joe.stephens@live.co.uk>
2016-05-15 22:44:28 +0100

    Populated sort method

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,13 @@
-def sort arr
-  # your code here
+def sort(some_array)
+  recursive_sort(some_array, [])
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+  return sorted_array if unsorted_array.length <= 0
+  sorted_array << unsorted_array.min
+  unsorted_array.delete(unsorted_array.min)
+  recursive_sort(unsorted_array, sorted_array)
+end
+
+unsorted_words = ['red', 'yellow', 'pink', 'green', 'purple', 'orange', 'blue']
+puts sort(unsorted_words)

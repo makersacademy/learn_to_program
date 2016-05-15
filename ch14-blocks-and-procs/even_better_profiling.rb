bcc23a7990e46fe8f46be4c9275ac0a2849b75a9
Oliver Diamond <diamond.oliver@gmail.com>
2016-05-15 13:43:34 +0100

    CH14 Done

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,7 @@
 def profile block_description, &block
-  # your code here
+	on_off = false
+    start_time = Time.new if on_off.freeze
+    block.call
+    duration = Time.new - start_time if on_off
+    puts "#{block_description}: #{duration} seconds" if on_off
 end

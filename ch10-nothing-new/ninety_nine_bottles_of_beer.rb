8e0b7e7d8aa6a85ac5a36180c7b35c9b5f5861f7
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-15 09:11:21 +0000

    Solved ninety_nine_bottles_of_beer.rb

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,14 @@
 # your code here
+require_relative 'english_number'
+
+def ninety_nine_song beers
+    beers.times do 
+        puts english_number(beers) + " #{beers == 1 ? "bottle" : "bottles"} of beer on the wall"
+        puts english_number(beers) + " #{beers == 1 ? "bottle" : "bottles"} of beer"
+        puts "Take one down and pass it around"
+        beers = beers - 1
+        puts english_number(beers) + " #{beers == 1 ? "bottle" : "bottles"} of beer on the wall"
+    end
+end
+
+ninety_nine_song (15)

3fb9f9796d1f101b1226c6dc1dfd4b6f5fcf02b1
joestephens <joe.stephens@live.co.uk>
2016-05-16 00:36:58 +0100

    Added spank

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What year were you born in? YYYY"
+year = gets.chomp.to_i
+puts "And the month? M"
+month = gets.chomp.to_i
+puts "And the day? D"
+day = gets.chomp.to_i
+
+age = Time.new - Time.local(year, month, day)
+
+years_old = (age / (31_557_600)).floor # seconds for 365.25 days
+
+years_old.times { puts "SPANK!" }

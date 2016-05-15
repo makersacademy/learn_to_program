ea01000347e370e96547db11d3459cf84df8ad25
joestephens <joe.stephens@live.co.uk>
2016-05-15 16:37:18 +0100

    Created beer_song method

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,12 @@
-# your code here
+require './english_number.rb'
+
+def beer_song(n)
+  puts "#{english_number(n).capitalize} #{n == 1 ? "bottle" : "bottles" } of beer on the wall,"
+  puts "#{english_number(n).capitalize} #{n == 1 ? "bottle" : "bottles" } of beer."
+  puts "Take one down, pass it around,"
+  n -= 1
+  puts "#{english_number(n).capitalize} #{n == 1 ? "bottle" : "bottles" } of beer on the wall."
+  beer_song(n) unless n == 0
+end
+
+beer_song(9999)

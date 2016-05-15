1454093020d32f6ad9d960cb0281f025c3a7dfb8
gtormiston <grahamormiston@gmail.com>
2016-05-15 20:07:44 +0100

    ch13:extend_built_in_classes complete

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,77 @@
 class Integer
-  # your code here
+  def factorial
+
+    if self == 0
+      return 1
+    else
+      split = self.downto(1).map {|x| x}
+      final = 1
+      split.each {|x| final *= x}
+      final
+    end
+
+  end
+end
+
+
+
+class Integer
+def to_roman_new
+
+  string = ""
+
+  m = (self / 1000)
+  d = ((self % 1000) / 500)
+  c = ((self % 500) / 100)
+  l = ((self % 100) / 50)
+  x = ((self % 50) / 10)
+  v = ((self % 10) / 5)
+  i = ((self % 5) / 1)
+
+  m.times {|x| string << 'M'}
+  d.times {|x| string << 'D'}
+  c.times {|x| string << 'C'}
+  l.times {|x| string << 'L'}
+  x.times {|x| string << 'X'}
+
+  if v == 1 && i == 4
+    string << 'IX'
+  elsif i == 4
+    string << 'IV'
+  else
+    v.times {|x| string << 'V'}
+    i.times {|x| string << 'I'}
+  end
+
+  string
+
+
+end
+end
+
+
+class Integer
+def to_roman
+
+  string = ""
+
+  m = (self / 1000)
+  d = ((self % 1000) / 500)
+  c = ((self % 500) / 100)
+  l = ((self % 100) / 50)
+  x = ((self % 50) / 10)
+  v = ((self % 10) / 5)
+  i = ((self % 5) / 1)
+
+  m.times {|x| string << 'M'}
+  d.times {|x| string << 'D'}
+  c.times {|x| string << 'C'}
+  l.times {|x| string << 'L'}
+  x.times {|x| string << 'X'}
+  v.times {|x| string << 'V'}
+  i.times {|x| string << 'I'}
+
+  string
+end
+
 end

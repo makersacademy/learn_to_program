ee2485485f657f79dfdf4cdcdf4817a99252fb66
Stephen Dawes <sdawes@outlook.com>
2016-05-15 18:06:54 +0100

    ch13-creating-new-classes/extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,69 +1,62 @@
+# class Array
+#   def shuffle
+#     arr = self
+#     # Now we can just copy the old shuffle method.
+#     shuf = []
+
+#     while arr.length > 0
+#       # Randomly pick one element of the array.
+#       rand_index = rand(arr.length)
+
+#       # Now go through each item in the array,
+#       # putting them all into new_arr except for
+#       # the randomly chosen one, which goes into
+#       # shuf.
+#       curr_index = 0
+#       new_arr = []
+#       arr.each do |item|
+#         if curr_index == rand_index
+#           shuf.push item
+#         else
+#           new_arr.push item
+#         end
+
+#         curr_index = curr_index + 1
+#       end
+#       # Replace the original array with the new,
+#       # smaller array.
+
+#       arr = new_arr
+#     end
+#     shuf
+#   end
+# end
+
+# note modern ruby has already has a built in shuffle method
+
 class Integer
   def factorial
-    if self < 0
-      return "You cant take the factorial of a negative number"
-    end
-
     if self <= 1
       1
     else
-      self * self(num-1)
+      self * (self-1).factorial
     end
-  end
-end
+  end                            # => :factorial
+  def to_roman
+    # I chose old-school roman numerals just to save space.
+    roman = ''
 
-class Array
-  def shuffle
-    self
+    roman = roman + 'M' * (self / 1000)
+    roman = roman + 'D' * (self % 1000 / 500)
+    roman = roman + 'C' * (self % 500 / 100)
+    roman = roman + 'L' * (self % 100 / 50)
+    roman = roman + 'X' * (self % 50 / 10)
+    roman = roman + 'V' * (self % 10 / 5)
+    roman = roman + 'I' * (self % 5 / 1)
 
-    shuf = []
-
-    while self.length > 0
-      # Randomly pick one element of the array.
-      rand_index = rand(arr.length)
-
-      # Now go through each item in the array,
-      # putting them all into new_arr except for
-      # the randomly chosen one, which goes into
-      # shuf.
-      curr_index = 0
-      new_arr = []
-      self.each do |item|
-        if curr_index == rand_index
-          shuf.push item
-        else
-          new_arr.push item
-        end
-
-        curr_index = curr_index + 1
-      end
-      # Replace the original array with the new,
-      # smaller array.
-
-      self = new_arr
-    end
-    shuf
-  end
-end
-
-class Roman
-  def old_roman_numeral
-    self
-    roman_hash = {
-      1000 => "M",
-       500 => "D",
-       100 => "C",
-        50 => "L",
-        10 => "X",
-          5 => "V",
-          1 => "I",
-      }
-
-      roman_numeral = ""
-      roman_hash.each do |value, letter|
-        roman_numeral << letter * (self / value)
-        self = self % value
-      end
-      return roman_numeral
-  end
-end
+    roman
+  end      # => :to_roman
+end        # => :to_roman
+  # puts [1,2,3,4,5].shuffle
+  # puts 7.factorial
+  # puts 73.to_roman

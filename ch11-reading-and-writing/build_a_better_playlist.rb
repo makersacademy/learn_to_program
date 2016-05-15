01611f5a0015c66d3724a58ba0c393d8edfaad50
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-15 16:48:50 +0100

    Learn To Program: Playlist Exercises Complete

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,18 @@
-def music_shuffle filenames
-  # your code here
+
+look_at_files = Dir['/Users/anthonyepstein/Desktop/SCARY2/**/*.mp3']
+
+Dir.chdir '/Users/anthonyepstein/Desktop/SCARY'
+
+filename = 'new_playlist.m3u'
+
+str = ""
+
+look_at_files.shuffle!
+
+files = look_at_files.each { |x| str += "#{x}\n" }
+
+
+
+File.open filename, 'w' do |f|
+	f.write str
 end
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,20 @@
-# your code here
+
+
+look_at_files = Dir['/Users/anthonyepstein/Desktop/SCARY2/**/*.mp3']
+
+Dir.chdir '/Users/anthonyepstein/Desktop/SCARY'
+
+filename = 'new_playlist.m3u'
+
+str = ""
+
+look_at_files.shuffle!
+
+files = look_at_files.each { |x| str += "#{x}\n" }
+
+
+
+File.open filename, 'w' do |f|
+	f.write str
+end
+

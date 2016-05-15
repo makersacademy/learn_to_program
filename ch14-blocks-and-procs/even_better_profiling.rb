c612e49d135ca70db13acc41551970088d6d35b8
gtormiston <grahamormiston@gmail.com>
2016-05-15 22:20:50 +0100

    ch14:even_better_profiling complete

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,20 @@
+# Even better profiling. After you do your profiling, see the slow parts of your
+# program, and either make them faster or learn to love them as they are,
+# you probably don’t want to see all of that profiling anymore. But (I hope)
+# you’re too lazy to go back and delete it all…especially because you might
+# want to use it again someday. Modify the profile method so you can turn
+# all profiling on and off by changing just one line of code. Just one word!
+
+$OPT_PROFILING_ON = false
+
 def profile block_description, &block
-  # your code here
+  if $OPT_PROFILING_ON
+    start_time = Time.new
+    block[]
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block[]
+  end
 end
+

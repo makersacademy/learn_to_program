3cf0683fcdce8b8dfa71f74f6d0c895204889db0
Tim <timchipperfield@gmail.com>
2016-05-15 21:15:22 +0100

    Chapter 11 from learn to program completed

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,11 @@
 def music_shuffle filenames
-  # your code here
+
+Dir.chdir '/Users/timchipp/Desktop/music'
+filename = 'new_playlist.m3u'
+str = ""
+filenames.shuffle!
+files = look_at_files.each { |x| str += "#{x}\n" }
+File.open filename, 'w' do |f|
+    f.write str
+end
 end
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,25 @@
-# your code here
+Dir.chdir '/Users/timchipp/Desktop/folder1'
+
+pic_names = Dir['/Users/timchipp/Desktop/folder2/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files: "
+pic_number = 1
+pic_names.each do |name|
+print '.' # This is our "progress bar".
+new_name = if pic_number < 10
+"#{batch_name}0#{pic_number}.jpg"
+else
+"#{batch_name}#{pic_number}.jpg"
+end
+if File.exist? new_name
+    puts 'ERROR, FILE ALREADY EXISTS! PLEASE CHANGE FILENAME'
+else
+    File.rename name, new_name
+end
+pic_number = pic_number + 1
+end
+puts # This is so we aren't on progress bar line.
+puts 'Done!'# your code here

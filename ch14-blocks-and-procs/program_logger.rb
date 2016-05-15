11c42bfe984a14f45a5372363843f19c273bc5c9
paulkenrick <pkenrick@gmail.com>
2016-05-15 19:08:15 +0100

    Completed Ch14 - program_logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  
+	puts 'Beginning "' + desc + '"...'
+	result = block.call
+	puts '..."' + desc + '" finished, returning: ' + result.to_s
+
 end
+
+# log 'outer block' do 
+# 	log 'some little block' do
+# 		2 + 3
+# 	end
+# 	log 'yet another block' do
+# 		'I like Thai food!'
+# 	end
+# 	false
+# end
+

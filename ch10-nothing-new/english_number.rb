62532e93a0414ee04ccfced9016ef27e0ba562bc
kpuwal <kpuwal@gmail.com>
2016-05-15 22:31:18 +0100

    ch10: english_number, 99_bottles; ch12: happy_bday, 1bln seconds

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,54 @@
 def english_number number
-  # your code here
+    return " please enter the number that isnt negative" if number < 0
+    return "zero" if number == 0
+
+    string_numb= ""
+    ones= ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    teens= ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineten']
+    tens= ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    big_numbers=[['hundred',2], ['thousand',3], ['million',6], ['billion',9], ['trillion',12], ['quadrillion',15], ['quintillion',18], ['sextillion',21], ['septillion',24], ['octillion', 27], ['nonillion', 30], ['decillion', 33], ['undecillion', 36], ['duodecillion', 39], ['tredecillion', 42], ['quattuordecillion', 45], ['quindecillion', 48], ['sexdecillion', 51], ['septendecillion', 54], ['octodecillion', 57], ['novemdecillion', 60], ['vigintillion', 63], ['googol', 100]]
+    
+    left= number
+
+    while big_numbers.length > 0
+        big_num_pair= big_numbers.pop
+        big_num_name= big_num_pair[0]
+        big_num_base= 10 ** big_num_pair[1]
+        write= left/big_num_base
+        left= left - write*big_num_base
+
+        if write > 0
+            prefix= english_number write
+            string_numb= string_numb + prefix + ' ' + big_num_name
+            string_numb= string_numb + ' ' if left > 0
+        end
+    end
+
+    write= left/10
+    left= left - write*10
+
+    if write > 0
+        if (write == 1) && (left > 0)
+            string_numb= string_numb + teens[left-1]
+            left= 0
+        else
+            string_numb= string_numb + tens[write-1]
+        end
+        string_numb= string_numb + '-' if left > 0
+    end
+
+    write= left
+    left= 0
+
+    string_numb= string_numb + ones[write-1] if write > 0
+    string_numb
 end
+
+num_at_start= 9999
+    num_now= num_at_start
+
+    while num_now > 2
+        puts english_number(num_now).capitalize + 'bottles of beer on the wall, ' + english_number(num_now) + ' bottles of beer!'
+        num_now= num_now - 1
+        puts 'Take one down, pass it around, ' + english_number(num_now) + ' bottles of beer on the wall!'
+    end
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+puts "What year were you born?"
+year= gets.chomp
+puts "What month?"
+month= gets.chomp
+puts "What day?"
+day= gets.chomp
+
+time= Time.mktime(year,month,day)
+puts "You were born on #{time}"
+
+current_time=Time.new
+sec_old=current_time-time
+years_old=sec_old/(60*60*24*365)
+years_old=years_old.to_i
+puts "You\'re #{years_old} years old"
+
+while years_old > 0
+    puts "SPANK! x #{years_old}"
+    years_old-= 1
+end

57d61ef64dad2ba0f8fa6084daea754c715e818c
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-15 15:28:46 +0100

    Learn To Program: Safer_picture_downloading.rb Complete

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,44 @@
-# your code here
+
+Dir.chdir '/Users/anthonyepstein/Desktop/SCARY2'
+
+pic_names = Dir['/Users/anthonyepstein/Desktop/SCARY/**/*.jpg']
+
+puts 'What would you like to call this batch?'
+
+batch_name = gets.chomp
+
+puts
+
+print "Downloading #{pic_names.length} files: "
+
+
+pic_number = 1
+
+pic_names.each do |name|
+
+print '.' # This is our "progress bar".
+
+new_name = if pic_number < 10
+
+"#{batch_name}0#{pic_number}.jpg"
+
+else
+
+"#{batch_name}#{pic_number}.jpg"
+
+end
+
+if File.exist? new_name
+	puts 'ERROR, FILE ALREADY EXISTS! PLEASE CHANGE FILENAME'
+else
+	File.rename name, new_name
+end
+
+pic_number = pic_number + 1
+
+end
+
+puts # This is so we aren't on progress bar line.
+
+puts 'Done, cutie!'
+

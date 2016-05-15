ee0fb7de039c5844c962e12f4128d482f2ae9a86
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-15 11:39:08 +0100

    Added missing method for CH13 "extend built-in classes"

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,8 +1,12 @@
 class Integer
   def factorial
-    return "No factorial of a negative number" if self < 0
+    return "No factorial of a negative selfber" if self < 0
     return 1 if self <= 1
     self * (self-1).factorial
+  end
+
+  def to_roman
+    "M"*(self/1000)+"D"*((self%1000)/500)+"C"*((self%500)/100)+"L"*((self%100)/50)+"X"*((self%50/10))+"V"*((self%10/5))+"I"*(self%5)
   end
 end
 

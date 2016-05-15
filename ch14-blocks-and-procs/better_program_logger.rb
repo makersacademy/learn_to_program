01369bef17ea104c6f5e97c1a1c1869cbea77ac6
paulkenrick <pkenrick@gmail.com>
2016-05-15 19:40:44 +0100

    Completed Ch14 - better program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,24 @@
-def log desc, &block
-  # your code here
+$indent = ''
+
+def better_log desc, &block
+	puts $indent + 'Beginning "' + desc + '"...'
+	$indent += ' '
+	result = block.call
+	$indent[-1] = ''
+	puts $indent + '..."' + desc + '" finished, returning: ' + result.to_s
 end
+
+
+# program_log 'outer block' do 
+# 	program_log 'some little block' do
+# 		program_log 'teeny tiny block' do
+# 			"hello"
+# 		end
+# 		2 + 3
+# 	end
+# 	program_log 'yet another block' do
+# 		'I like Thai food!'
+# 	end
+# 	false
+# end
+

ee364db7067575b791e02d0207a00e469ff421e4
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-15 21:16:17 +0000

    Solved: build_your_own_playlist.rb, build_a_better_playlist.rb, happy_birthday.rb, one_billion_seconds.rb, party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,19 @@
 # your code here
+puts "When where you born?"
+puts "Year:"
+year = gets.chomp.to_i
+puts "Month:"
+month = gets.chomp.to_i
+puts "Day:"
+day = gets.chomp.to_i
+
+birthday = Time.new(year, month, day)
+
+now = Time.new
+
+birthdays = ((now - birthday) / 31622400).to_i
+
+birthdays.times do
+    puts "SPANK"
+end
+

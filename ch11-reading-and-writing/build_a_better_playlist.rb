9bddfba44d2a193da07e0f8704ab752263470268
Oliver Diamond <diamond.oliver@gmail.com>
2016-05-15 01:01:10 +0100

    CH11 Done

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,15 @@
 def music_shuffle filenames
-  # your code here
+	result = []
+	files_in_folders = Hash.new{|h,k| h[k] = []}
+	filenames.each{ |e| 
+		files_in_folders[e.split("/")[-2] || :nofolder] << e
+	}
+	files_in_folders = files_in_folders.values
+	while(files_in_folders.length > 0) 
+		files_in_folders.shuffle!.delete_if{ |e| 
+			result << e.shuffle!.pop
+			e.empty?
+		}
+	end
+	result
 end

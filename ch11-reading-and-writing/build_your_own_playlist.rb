1a980a5d569a63dc711c5613567fde85b4964bac
joestephens <joe.stephens@live.co.uk>
2016-05-15 23:51:46 +0100

    Added playlist creation

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,7 @@
-# your code here
+songs = Dir["/Users/joestephens/Music/iTunes/iTunes Media/Music/**/*.m4a"]
+
+File.open("playlist.m3u", "w") do |file|
+  songs.each do |song|
+    file.write("#{song}\n")
+  end
+end

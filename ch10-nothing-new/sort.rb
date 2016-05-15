4ec3c0db954d72bf62ace847a91d4ba0caac0199
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-05-15 23:05:37 +0300

    Ch 10 ex 2 - sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,11 @@
 def sort arr
-  # your code here
-end
+  return arr if arr.length <= 1
+ middle = arr.pop
+ less = arr.select{|x| x.downcase < middle.downcase} 
+ more = arr.select{|x| x.downcase >= middle.downcase}
+
+  sort(less) + [middle] + sort(more)
+ end
+
+
+print (sort(['the' , 'bird' , 'chirps', 'tweet','twitter', 'is','dead']))

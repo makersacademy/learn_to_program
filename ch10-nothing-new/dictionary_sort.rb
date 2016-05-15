b53084ac68777d5367a9cb40fdd684e1d599de18
joestephens <joe.stephens@live.co.uk>
2016-05-15 22:40:04 +0100

    Populated dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,15 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(some_array)
+  recursive_sort(some_array, [])
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+  return sorted_array if unsorted_array.length <= 0
+  all_downcase = unsorted_array.map(&:downcase)
+  downcase_mindex = all_downcase.index(all_downcase.min)
+  sorted_array << unsorted_array[downcase_mindex]
+  unsorted_array.delete(unsorted_array[downcase_mindex])
+  recursive_sort(unsorted_array, sorted_array)
+end
+
+unsorted_words = ['Red', 'yellow', 'pink', 'Green', 'purple', 'orange', 'blue']
+puts dictionary_sort(unsorted_words)

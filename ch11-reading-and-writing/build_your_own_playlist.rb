aeb01c492fdaa7b8d7d609887e10e024ef951bd7
joestephens <joe.stephens@live.co.uk>
2016-05-16 00:15:43 +0100

    Created playlist scripts

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,4 +1,4 @@
-songs = Dir["/Users/joestephens/Music/iTunes/iTunes Media/Music/**/*.m4a"]
+songs = Dir["/Users/joestephens/Music/iTunes/iTunes Media/Music/**/*.m4a"].shuffle
 
 File.open("playlist.m3u", "w") do |file|
   songs.each do |song|

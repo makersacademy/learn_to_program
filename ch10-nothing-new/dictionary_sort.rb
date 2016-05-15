a595ea6746ae2c58178e0d289442068e371d8346
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-05-15 12:40:07 +0100

    CH10 "dictionary sort" function updated

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,12 +1,12 @@
 def dictionary_sort arr
-  recursive_sort arr, arr.map(&:downcase),[]
+  recursive_sort arr,[]
 end
 
-def recursive_sort unsorted_array, unsorted_downcase, sorted_array
+def recursive_sort unsorted_array, sorted_array
   return sorted_array if unsorted_array == []
-  min_index = unsorted_downcase.index(unsorted_downcase.min)
+  alL_downcase = unsorted_array.map(&:downcase)
+  min_index = alL_downcase.index(alL_downcase.min)
   sorted_array << unsorted_array[min_index]
   unsorted_array.delete_at(min_index)
-  unsorted_downcase.delete_at(min_index)
-  recursive_sort unsorted_array, unsorted_downcase, sorted_array
+  recursive_sort unsorted_array, sorted_array
 end

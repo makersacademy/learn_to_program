8bc664458350777d3853b52c4252e819de0a20be
kpuwal <kpuwal@gmail.com>
2016-05-15 02:58:10 +0100

    ch11: safer_picture_downloading, build_your_own_playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,20 @@
-# your code here
+def shuffle array
+    shuffled_arr= []
+
+    while array.length > 0
+        random= array.sample
+        arr= []
+        array.each {|x|
+            x == random ? shuffled_arr<< x : arr<< x
+        }
+        array= arr
+    end
+    shuffled_arr
+end
+
+all_files= shuffle(Dir['/Users/kass/Desktop/TmpMusic/**/*.mp3'])
+File.open 'shuffled_playlist.m3u', 'w' do |file|
+    all_files.each do |x|
+        f.write x+"\n"
+    end
+end
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,47 @@
-# your code here
+
+Dir.chdir '/Users/kass/Desktop/picturesInbox' # target directory
+pic_names= Dir['/Users/kass/Desktop/picturesSource/**/*.jpg']
+puts "Hello!"
+puts "How would you like to name this batch?"
+batch_name= gets.chomp
+
+pic_number= 1
+pic_names.each do |name|
+    
+    # condition needs improvement (regexp)
+    # Dir['*.jpg'].any? {|x| x =~ /#{batch_name}/}
+
+    if FileTest.exist? ("#{batch_name}0#{pic_number}.jpg") or FileTest.exist? ("#{batch_name}#{pic_number}.jpg") 
+
+        puts ""
+        puts "- - - - - - - - - - - - - - - - - - - - - "
+        puts "WARNING! this batch name already exists"
+        puts "- - - - - - - - - - - - - - - - - - - - - "
+        puts ""
+        puts "Would you like to rename this batch? y/n"
+        answer= gets.chomp
+        if answer == 'y'
+            puts 'Enter new name, please'
+            batch_name= gets.chomp
+        else
+            puts "The file will be written over"
+        end
+    end
+        
+        new_name= if pic_number < 10
+            "#{batch_name}0#{pic_number}.jpg"
+        else
+            "#{batch_name}#{pic_number}.jpg"
+        end
+
+        
+        print "Downloading #{new_name}"
+        File.rename name, new_name
+        pic_number= pic_number + 1
+    puts ""
+end
+print ''
+puts "- - - - - - - - - - - - - - - - - - - - - "
+puts "- - - - - - - - - - - - - Done! Thank you"
+puts ""
+

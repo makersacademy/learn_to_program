f496395dee88aee7e1a80aaca1652bacbe5a2301
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-15 08:15:25 +0000

    Solved dictionary_sort, shuffle and english_number

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,21 @@
 def dictionary_sort arr
   # your code here
+  return arr if arr.length <= 1
+  
+  recursive_sort( arr, [])
+  
+  
 end
+
+def recursive_sort unsorted_arr, sorted_arr
+    return sorted_arr.flatten if unsorted_arr == []
+    
+    sorted_arr << unsorted_arr.select {|a| a == unsorted_arr.min}
+    unsorted_arr.delete(unsorted_arr.min)
+    
+    recursive_sort unsorted_arr, sorted_arr
+    
+   
+    
+end
+  

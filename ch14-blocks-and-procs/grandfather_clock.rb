1c5abf464fb01cde4e61d5d67cbc315d897c3142
Luke <lukeaveil@yahoo.com>
2016-05-16 10:41:55 +0100

    Chapter 14, second challenge

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,18 @@
 def grandfather_clock &block
-  # your code here
+  
+  time = Time.new.hour
+
+  if time > 12
+  	time = time % 12
+  end
+
+  if time == 0
+  	time = 12
+  end
+
+  time.times(&block)
 end
+
+grandfather_clock{puts "DONG!"}
+
+	

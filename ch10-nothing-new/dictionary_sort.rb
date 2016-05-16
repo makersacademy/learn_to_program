19e474761ca0500da5cc491f5d48fad66e435cbf
joestephens <joe.stephens@live.co.uk>
2016-05-16 23:51:17 +0100

    Changed delete to delete_at

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -5,11 +5,8 @@ end
 def recursive_sort(unsorted_array, sorted_array)
   return sorted_array if unsorted_array.length <= 0
   all_downcase = unsorted_array.map(&:downcase)
-  downcase_mindex = all_downcase.index(all_downcase.min)
-  sorted_array << unsorted_array[downcase_mindex]
-  unsorted_array.delete(unsorted_array[downcase_mindex])
+  downcase_min_index = all_downcase.index(all_downcase.min)
+  sorted_array << unsorted_array[downcase_min_index]
+  unsorted_array.delete_at(downcase_min_index)
   recursive_sort(unsorted_array, sorted_array)
 end
-
-unsorted_words = ['Red', 'yellow', 'pink', 'Green', 'purple', 'orange', 'blue']
-puts dictionary_sort(unsorted_words)

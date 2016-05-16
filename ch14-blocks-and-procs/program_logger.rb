1285934801cf491c435000dbffeb107f0defd604
gtormiston <grahamormiston@gmail.com>
2016-05-16 07:29:58 +0100

    ch14:program_logger complete

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,39 @@
-def log desc, &block
-  # your code here
+# Program logger. Write a method called log that takes a string description
+# of a block (and, of course, a block). Similar to the method do_self_importantly,
+# it should puts a string telling you it started the block and another string
+# at the end telling you it finished and also telling you what the block
+# returned. Test your method by sending it a code block. Inside the block,
+# put another call to log, passing a block to it. In other words, your output
+# should look something like this:
+# Beginning "outer block"...
+# Beginning "some little block"...
+# ..."some little block" finished, returning:
+# 5
+# Beginning "yet another block"...
+# ..."yet another block" finished, returning:
+# I like Thai food!
+# ..."outer block" finished, returning: false
+
+
+
+def program_log desc, &block
+
+  puts "Beginning \"#{desc}\"..."
+  result = block[]
+  puts "...\"#{desc}\" finished, returning: #{result}"
+
+end
+
+program_log 'outer block' do
+
+  program_log 'some little block' do
+    1**1 + 2**2
+  end
+
+  program_log 'yet another block' do
+    '!doof iahT ekil I'.reverse
+  end
+
+  '0' == 0
+
 end

02d4addb78e461c88f6cf1f83febb9f20fd928ed
joestephens <joe.stephens@live.co.uk>
2016-05-16 01:41:26 +0100

    Extended Integer with to_rom_num

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,31 @@
 class Integer
-  # your code here
+  def to_rom_no
+    num = self
+    roman_nums = {
+      "M"   =>  1000,
+      "CM"  =>  900,
+      "D"   =>  500,
+      "CD"  =>  400,
+      "C"   =>  100,
+      "XC"  =>  90,
+      "L"   =>  50,
+      "XL"  =>  40,
+      "X"   =>  10,
+      "IX"  =>  9,
+      "V"   =>  5,
+      "IV"  =>  4,
+      "I"   =>  1
+    }
+
+    os_rom_num = ""
+
+    roman_nums.each do |k, v|
+      os_rom_num << k * (num / v)
+      num = num % v
+    end
+
+    os_rom_num
+  end
 end
+
+puts 2_999.to_rom_no

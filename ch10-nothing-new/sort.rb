7f1bb8b0546cc237d94089b244c009cc6f6aa5ca
joestephens <joe.stephens@live.co.uk>
2016-05-17 00:17:45 +0100

    Can can

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -4,10 +4,10 @@ end
 
 def recursive_sort(unsorted_array, sorted_array)
   return sorted_array if unsorted_array.length <= 0
-  sorted_array << unsorted_array.min
-  unsorted_array.delete(unsorted_array.min)
+  all_downcase = unsorted_array.map(&:downcase)
+  downcase_min_index = all_downcase.index(all_downcase.min)
+  sorted_array << unsorted_array[downcase_min_index]
+  unsorted_array.delete_at(downcase_min_index)
   recursive_sort(unsorted_array, sorted_array)
+  sorted_array
 end
-
-unsorted_words = ['red', 'yellow', 'pink', 'green', 'purple', 'orange', 'blue']
-puts sort(unsorted_words)

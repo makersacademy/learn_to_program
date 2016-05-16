eb66c8efb4fe9a3738d4ad885b205f5ef1132142
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-16 12:04:40 +0000

    Solved program_logger.rb, better_program_logger.rb and grandfather_clock.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,13 @@
-def log desc, &block
-  # your code here
+$depth = 0
+def better_log desc, &block
+  
+  
+  puts (" " * $depth) + "Beginning \"#{desc}\"..."
+  $depth += 1
+  return_value = block.call
+  $depth -= 1
+  puts (" " * $depth) + "...\"#{desc}\" finished, returning: #{return_value}"
+  
+  
+  
 end

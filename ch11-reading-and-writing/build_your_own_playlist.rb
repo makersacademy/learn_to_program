97a0c931856c289a8722e7a0229d2b44d2abd2f8
Jonathan <j_passe@live.fr>
2016-05-17 19:38:01 +0100

    ch11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,14 @@
-# your code here
+def shuffle arr
+
+arr.shuffle
+
+end
+
+songs = shuffle(Dir['**/*.mp3'])
+
+File.open 'playlist.m3u', 'w' do |f|
+ songs.each do |song|
+    f.write song
+  end
+end
+
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,34 @@
-# your code here
+Dir.chdir 'C:/Documents and Settings/Katy/PictureInbox'
+
+
+
+pic_names = Dir['F:/**/*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+
+print '.'
+
+new_name = if pic_number < 10
+
+"#{batch_name}0#{pic_number}.jpg"
+else
+"#{batch_name}#{pic_number}.jpg"
+end
+
+  if File.exist?(new_name)
+    puts 'There is already a file under this name'
+    puts 'please pick a different name'
+  new_name = gets.chomp
+  end
+
+File.rename name, new_name
+pic_number = pic_number + 1
+puts
+puts 'Done, cutie!'

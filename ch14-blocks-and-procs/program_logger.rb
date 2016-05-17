ab44cbd0aa78f765d866d638d1c0ed3cfb1c8950
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-17 13:55:26 +0100

    learn to program: program_logger.rb complete

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,6 @@
-def log desc, &block
+def program_log desc, &block
   # your code here
+  puts "Beginning \"#{desc}\"..."
+  mwahaha = block.call
+  puts "...\"#{desc}\" finished, returning: #{mwahaha}"
 end

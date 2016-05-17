2ea2632dde5b135f23e0b3c6696e6bb8dfab6ba5
Tim <timchipperfield@gmail.com>
2016-05-17 13:39:04 +0100

    Ch 14 even better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,16 @@
 def profile block_description, &block
-  # your code here
+  number = 0
+  if number == 0
+  start_time = Time.new
+  block.call
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+  else 
+    block.call
+    end
 end
+
+
+
+#it will test the time unless i change something in the code
+#2 methods one where it checks the time and one where it calls block w/o checking time

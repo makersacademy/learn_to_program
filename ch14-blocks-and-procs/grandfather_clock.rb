ba2baa6c3fc5ba4c328a87ec63cf28a7fe1579f2
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-18 17:00:26 +0100

    Add ch14 - grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
 def grandfather_clock &block
-  # your code here
+	hour = Time.new.hour
+	if hour > 12
+		hour = hour - 12
+	elsif hour == 0
+		hour = 12
+	end
+
+	hour.times {block.call}
+		
 end
+
+grandfather_clock {puts 'DONG!'}

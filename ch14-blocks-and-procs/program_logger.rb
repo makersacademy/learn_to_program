99d8ba86f36ebcd9bc86f394b4b771b750c8bee5
Hassan Radheyyan <hassan.rad@gmail.com>
2016-05-18 20:39:28 +0000

    Exercise 14:3

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,17 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+	puts 'Beginning "' + desc + '"...'
+	resulting = block.call
+	puts '..."' + desc + '" finished, returning: ' + resulting.to_s
+end
+
+program_log 'outer block' do
+	program_log 'some little block' do
+		5 * 1
+	end
+
+	program_log 'yet another block' do
+		"I like Thai food"
+	end
+
+1 == 2
 end

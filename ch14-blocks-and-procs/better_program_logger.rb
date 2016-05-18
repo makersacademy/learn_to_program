27d0110a06ffd19e6c3c99797da94e03d367ad6a
Jonathan <j_passe@live.fr>
2016-05-18 09:59:38 +0100

    ch 14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
+$start = 0
+
 def log desc, &block
-  # your code here
+  puts ' '* $start + 'Beginning "' + desc + '"...'
+
+  $start += 1
+  block.call
+  $start -= 1
+
+  puts ' '*$depth + '..."' + desc + '" finished, returning: ' + block.call.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lots of love'
+    end
+    42
+  end
+
+  log 'yet another block' do
+    'I love indian food!'
+  end
+  true
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,18 @@
 def log desc, &block
-  # your code here
+puts 'Beginning "' + desc + '"...'
+block.call
+puts '..."' + desc + '" finished, returning: ' + block.call.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    5
+  end
+end
+
+log 'yet another block' do
+  log 'some little block' do
+    'I like Thai food!'
+  end
+  false
 end

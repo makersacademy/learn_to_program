58fac95bba70f2c4b12da585eedc621edcbab1fd
Hassan Radheyyan <hassan.rad@gmail.com>
2016-05-18 20:05:44 +0000

    Challenge 14:2

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,6 +1,7 @@
 def grandfather_clock &block
   current_hour = Time.new.hour
-  if current_hour > 12 then current_hour -= 12 end
+  if current_hour > 12 then current_hour -= 12
+  elsif current_hour == 0 then current_hour = 12
   current_hour.times do
 	puts 'DONG!'
   end

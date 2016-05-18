5be242e48836a5d3bc776341d54455d6cb7e2cf3
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-18 17:25:12 +0100

    Add ch14 - blocks_and_procs.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
 def log desc, &block
-  # your code here
+	puts "Beginning #{desc.inspect}..."
+	result = block.call
+	puts "... #{desc.inspect} finished, returning: " + result.to_s
+end
+
+log "outer block" do
+	log "some little block" do	
+		5
+	end
+	log "yet another block" do
+		"I like Thai food!"
+	end
+	false
 end

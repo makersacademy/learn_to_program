6686cf29238d0712b6c71cf0f3cbb47753e51d35
Nomi Vos <nvoswork@gmail.com>
2016-06-13 07:23:20 -0500

    solution for old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,12 @@
 def old_roman_numeral num
-  # your code here
+  number = ''
+  number = number + 'M' * (num / 1000)
+  number = number + 'D' * (num % 1000 / 500)
+  number = number + 'C' * (num % 500 / 100)
+  number = number + 'L' * (num % 100 / 50)
+  number = number + 'X' * (num % 50 / 10)
+  number = number + 'V' * (num % 10 / 5)
+  number = number + 'I' * (num % 5 / 1)
 end
+
+puts (old_roman_numeral(2894))

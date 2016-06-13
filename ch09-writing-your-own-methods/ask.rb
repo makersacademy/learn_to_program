326eec8734ba989fe70f3a948d76eb690938444f
Mali Michael <malimccalla@gmail.com>
2016-06-13 11:53:35 +0100

    Complete chapter 9 ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,34 @@
-def ask question
-  # your code here
+def ask(question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == "yes" || reply == "no")
+      if reply == "yes"
+        return true
+      else
+        return false
+      end
+      break
+    else
+      puts "Please answer \"Yes\" or \"No\""
+    end
+  end
 end
+
+puts 'Hello, and thank you for taking the time to help me with this experiment. My experiment has to do with the way people feel about Mexican food. Just think about Mexican food and try to answer every question honestly, with a "yes" or a "no". My experiment has nothing to do with bed wetting.'
+
+puts
+ask('Do you like eating tacos?')
+ask('Do you like eating burritos?')
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for taking the time to help with this experiment. In fact, this experiment has nothing to do with Mexican food. It is an experiment about bed-wetting. The Mexican food was just there to catch you off guard in the hopes that you would answer more honestly.'
+puts
+
+puts wets_bed

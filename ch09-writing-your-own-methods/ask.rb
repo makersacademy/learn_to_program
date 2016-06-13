c869d0246c1cdf212e47d236ec8fbc8011cd44fa
BenWhite <benjaminwhite@live.co.uk>
2016-06-13 11:36:43 +0000

    Answers for Chapter 9

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,12 @@
 def ask question
-  # your code here
+puts question
+reply = gets.chomp.downcase
+  if reply == 'yes'
+    true
+  elsif reply == 'no'
+    false
+  else
+    puts 'Please answer "yes" or "no".'
+    ask question
+  end
 end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,27 @@
 def roman_numeral num
-  # your code here
+  	
+mapper = {
+1    => 'I',
+4    => 'IV',
+5    => 'V',
+9    => 'IX',
+10   => 'X',
+40   => 'XL',
+50   => 'L',
+90   => 'XC',
+100  => 'C',
+400  => 'CD',
+500  => 'D',
+900  => 'CM',
+1000 => 'M'
+}
+
+roman = ''
+mapper.keys.reverse.each do |value|
+while num >= value
+num -= value
+roman += mapper[value]
+end
+end
+roman
 end

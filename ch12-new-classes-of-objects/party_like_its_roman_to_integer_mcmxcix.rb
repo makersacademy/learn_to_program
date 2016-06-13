76f3afa191849aa6d2bd6079a2041ace638e18fb
BenWhite <benjaminwhite@live.co.uk>
2016-06-13 21:54:48 +0000

    Incremental answers update

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,24 @@
-def roman_to_integer roman
-  # your code here
+def roman_to_integer num
+  arr = num.split('')
+  sum = []
+  res = 0
+
+  arr.each do |v| 
+    sum << 1000 if v == 'm' || v == 'M'
+    sum << 100 if v == 'c' || v == 'C'
+    sum << 50 if v == 'l' || v == 'L'
+    sum << 10 if v == 'x' || v == 'X'
+    sum << 5 if v == 'v' || v == 'V'
+    sum << 1 if v == 'i' || v == 'I'
+  end
+
+  sum.each {|v| res += v}
+
+    res -=2 if num.match('iv')
+    res -=2 if num.match('ix')
+    res -=20 if num.match('xl')
+    res -=20 if num.match('xc')
+    res -=200 if num.match('cm')
+  
+  res
 end

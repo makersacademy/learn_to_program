3a44714b6ce6e46b317e8381f9c950a0bb223705
Laurence Kite <laurent.kitecb@gmail.com>
2016-06-13 18:47:41 +0000

    added two files old_school_roman and ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,26 @@
 def ask question
-  # your code here
+  # 
+    while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == 'yes' || reply == 'no') 
+      if reply == 'yes'
+        return true
+      else
+        return false
+     end
+     break
+  else
+    puts 'Please answer yes or no '
+    end
+  end
 end
+ask 'Do you like tacos?'
+ask 'Do you like eating buritos?'
+wets_bed = ask ('Do you wet the bed?')
+ask 'Do you like Christmas '
+puts 
+puts 'DEBRIEFING: '
+puts 'Thank for...'
+puts
+puts wets_bed
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,26 @@
 def old_roman_numeral num
   # your code here
+  numM = num / 1000
+  numMr = num % 1000
+  numd =  numMr / 500
+  numdr= numMr % 500
+  numc = numdr / 100
+  numcr = numdr % 100
+  numL = numcr / 50
+  numLr = numcr % 50
+  numx = numLr / 10
+  numxr = numLr % 10
+  numv = numxr / 5
+  numvr = numxr % 5
+  numi = numvr / 1
+  numir = numvr % 1
+  totalm = numM
+  totald = numd
+  totalc = numc
+  totalL = numL
+  totalx = numx
+  totalv = numv
+  totali = numi 
+  return 'M' * totalm.to_i + 'D'  * totald.to_i + 'C' * totalc.to_i + 'L' * totalL.to_i + 'X' * totalx.to_i + 'V' * totalv.to_i + 'I' * totali.to_i
 end
+

069cf325813e30a34792b71d51fa97ab24c58cff
JulieLev <jkgee1@yahoo.co.uk>
2016-06-13 11:03:51 +0000

    Includes lines outside method

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,33 @@
 def ask question
-  # your code here
+
+	while true
+	  puts question
+	  reply = gets.chomp.downcase
+
+	  case reply
+	    when 'yes'
+          answer = true
+		  break
+		when 'no'
+		  answer = false
+		  break
+		else
+		  puts "Please answer yes or no."
+	  end
+	
+	end
+
+return answer
+end
+
+ask "Is it Monday?"
+ask "Are you testing Github?"
+ask "Are you awake?"
+puts "Silly question!"
+confident = ask "Do you know what you're doing?"
+
+if confident == true
+puts "Yay, way to go!"
+else
+puts "You moron, concentrate more!"
 end

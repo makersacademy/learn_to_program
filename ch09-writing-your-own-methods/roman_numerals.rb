f1852c0a89ff4265980742dab85c67645dc0f199
Jack Oddy <jackoddy91@gmail.com>
2016-06-13 16:58:39 +0100

    copied previous version of old_school

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,27 @@
 def roman_numeral num
-  # your code here
+  ones = "I"
+  fives = "V"
+  tens = "X"
+  fifties = "L"
+  hundreds = "C"
+  fiveHundreds = "D"
+  thousands = "M"
+
+  numerals = ''
+
+  (num / 1000).times { numerals << thousands }
+  num = num % 1000
+  (num / 500).times { numerals << fiveHundreds }
+  num = num % 500
+  (num / 100).times { numerals << hundreds }
+  num = num % 100
+  (num / 50).times { numerals << fifties }
+  num = num % 50
+  (num / 10).times { numerals << tens }
+  num = num % 10
+  (num / 5).times { numerals << fives }
+  num = num % 5
+  num.times { numerals << ones }
+
+  numerals 
 end

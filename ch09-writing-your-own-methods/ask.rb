9a9e41b317e923f95a33479b5503d58468256e58
Jack Oddy <jackoddy91@gmail.com>
2016-06-13 13:56:21 +0100

    Refactored ask.rb with recursion

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,14 @@
 def ask question
-  # your code here
+  puts question
+  reply = gets.chomp
+
+  unless (reply.downcase == "yes") || (reply.downcase == "no")
+      puts "Please answer \"yes\" or \"no\"."
+      ask question
+  end
+
+reply.downcase == "yes" ? true : false
+
 end
+
+ask "Yes or No?"

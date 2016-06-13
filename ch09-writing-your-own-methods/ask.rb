20a718e4f80c1d877ea0bb7b08d5ba09a178f902
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-13 11:53:16 +0100

    ask.rb challenge completed

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,16 @@
 def ask question
-  # your code here
+while true
+		puts question
+		reply = gets.chomp.downcase
+		if reply == "yes"
+      return true
+		elsif reply == "no"
+      return false
+		else puts "please only answer with yes or no"
+		end
+	end
 end
+
+ask "Is Latin the coolest?"
+answer = ask "Do you like Chris Pine?"
+puts answer

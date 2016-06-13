6d43265fd863d5b6e94e5103d9fba88bb44a1a85
Jack Oddy <jackoddy91@gmail.com>
2016-06-13 21:18:31 +0100

    experimented with engnum & shuff - rspec rjects them

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,52 @@
-def english_number number
-  # your code here
-end
+def english_number n
+
+		ones = [	"one", "two", "three", "four", "five",
+					"six", "seven", "eight", "nine"]
+
+		tens = [	"Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty",
+					"Seventy", "Eighty", "Ninety"]
+
+		teens =[	"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+					"Seventeen", "Eighteen", "Nineteen"]
+
+		string = []
+
+		a = n / 100
+
+		if a > 0 #hundreds
+			string.push("#{ones[a-1]} hundred")
+		end
+
+		b = (n - a * 100) / 10
+
+		if b > 1
+			if a >= 1 then string.push " and " end
+			string.push("#{tens[b-1]} ")
+		end
+
+		c = n - ((a*100) + (b*10))
+
+		if ((c > 0) && (b == 1))
+			string.push("#{teens[c-1]}")
+		elsif ((c > 0) && (b != 1))
+			string.push("#{ones[c-1]}")
+		end
+
+
+
+
+		puts string.join.downcase
+
+	end
+
+
+
+
+english_number 347
+english_number 256
+english_number 444
+english_number 12
+english_number 100
+english_number 200
+english_number 999
+english_number 10
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,16 @@
 def shuffle arr
-  # your code here
+
+n = arr.length
+
+arr.insert(rand(0..(arr.length)), arr.pop)
+
+n = n - 1
+
+arr.shuffle
+
+puts arr.compact
+
 end
+
+
+shuffle ["1","2","3","4","5","6","7","8"]

7ea762a6d3a72f3834167fcc6a595847a6c2720d
Jack Oddy <jackoddy91@gmail.com>
2016-06-14 10:45:05 +0100

    recursive sort method

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
 def dictionary_sort arr
-arr.sort! {|a,b| a.downcase <=> b.downcase }
+
+recursive_sort arr, []
+
 end
+
+def recursive_sort unsorted_arr, sorted_arr
+
+return sorted_arr if unsorted_arr.length <= 0
+
+sml = unsorted_arr.pop
+still_unsorted = []
+
+unsorted_arr.each do |t|
+  if t.downcase < sml.downcase
+    still_unsorted.push sml
+    sml = t
+  else
+    still_unsorted.push t
+  end
+end
+
+  sorted_arr.push sml
+  recursive_sort still_unsorted, sorted_arr
+end
+
+puts dictionary_sort ["apple","banana","starfruit","watermelon","COCOCNUT","Peach","lemon","LIME"]

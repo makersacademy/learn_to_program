14e9cc2be651ddd74974471852725a75a6b9dbf6
Jonny Moore <jonnymoore121212@gmail.com>
2016-06-14 11:46:01 +0100

    Update to old_school_romans

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,18 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+  m = num / 1000
+  d = (num - 1000 * m) / 500
+  c = (num - 1000 * m - 500 * d) / 100
+  l = (num - 1000 * m - 500 * d - 100 * c) / 50
+  x = (num - 1000 * m - 500 * d - 100 * c - 50 * l) / 10
+  v = (num - 1000 * m - 500 * d - 100 * c - 50 * l - 10 * x) / 5
+  i = (num - 1000 * m - 500 * d - 100 * c - 50 * l - 10 * x - v * 5)
+  "M" * m + "D" * d + "C" * c + "L" * l + "X" * x + "V" * v + "I" * i
 end
+
+
+
+p old_roman_numeral(1987)
+p old_roman_numeral(12)
+p old_roman_numeral(58)
+p old_roman_numeral(219)
+p old_roman_numeral(2021)

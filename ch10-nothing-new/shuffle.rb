3b4b1bcb22bc39da5ad2cb90aebc487cb448a15d
Jess Astbury <jess.astbury@hotmail.com>
2016-06-14 11:31:12 +0000

    Created shuffle function

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,17 @@
 def shuffle arr
-  # your code here
+
+shuffled = []
+    
+while arr.length > 0
+    
+    index = rand(arr.length)
+    num = arr[index]
+    arr.delete(num)
+
+    shuffled.insert(rand(arr.length), num)
+    
 end
+    shuffled.compact
+end
+
+print shuffle(['apple','garden','pear','table', 'fence'])

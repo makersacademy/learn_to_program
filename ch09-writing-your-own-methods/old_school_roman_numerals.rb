04fbd4eb1a1a24dcc6a846b3962ce3044d9fb7ab
Tommy Williams <tom@teow.co.uk>
2016-06-14 14:52:24 +0100

    Old School Roman Numerals V2

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -3,6 +3,43 @@ def old_roman_numeral(num)
   # orn stands for old roman numeral
 orn = ""
 
+while num > 1000
+  orn += 'M' if num % 1000 != num
+  num = num -= 1000
+end
+while num > 500
+orn += 'D' if num % 500 != num
+num = num -= 500
+end
+while num > 100
+orn += 'C' if num % 100 != num
+num = num -= 100
+end
+while num > 50
+orn += 'L' if num % 50 != num
+num = num -= 50
+end
+while num > 10
+orn += 'X' if num % 10 != num
+num = num -= 10
+end
+while num > 5
+orn += 'V' if num % 5 != num
+num = num -= 5
+end
+while num > 1
+orn += 'I' if num % 1 != num
+num = num -= 1
+end
+orn
+end
+
+puts old_roman_numeral(2016)
+puts old_roman_numeral(113)
+
+=begin
+orn = ""
+
 orn += 'M' if num % 1000 != num
 orn += 'D' if num % 500 != num
 orn += 'C' if num % 100 != num
@@ -10,13 +47,13 @@ orn += 'L' if num % 50 != num
 orn += 'X' if num % 10 != num
 orn += 'V' if num % 5 != num
 orn += 'I' if num % 1 != num
-puts orn
+orn
 end
 
-old_roman_numeral(2016)
-old_roman_numeral(113)
+puts old_roman_numeral(2016)
+puts old_roman_numeral(113)
 
-=begin
+
 if num % 1000 != num
   orn += 'M'
 end

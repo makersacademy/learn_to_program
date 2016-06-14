0724b3a1b6947180c04cbc0c638e4a1c2ebb3502
Mali Michael <malimccalla@gmail.com>
2016-06-14 15:20:02 +0100

    Complete chapter 10 dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,34 @@
-def dictionary_sort arr
-  # your code here
+
+puts "Type some words"
+response = nil
+words = []
+while response != ""
+  response = gets.chomp
+  words << response
 end
+
+def dictionary_sort(arr)
+  rec_dict_sort(arr,[])
+end
+
+def rec_dict_sort(unsorted,sorted)
+  if unsorted.length <=0
+    return sorted
+  end
+  smallest = unsorted.pop
+  still_unsorted =[]
+
+  unsorted.each do |n|
+    if n < smallest
+      still_unsorted << smallest.downcase
+      smallest = n
+    else
+      still_unsorted << n
+    end
+  end
+
+  sorted << smallest
+  rec_dict_sort(still_unsorted,sorted)
+end
+
+puts(dictionary_sort(words))

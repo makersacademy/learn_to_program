6dac438d33e269e4f72946e2563cdfd361911aa3
Nomi Vos <nvoswork@gmail.com>
2016-06-14 06:45:56 -0500

    solution to dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,26 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  shortest = unsorted_array.pop
+  still_unsorted = []
+
+  unsorted_array.each do |word|
+    if word.downcase < shortest.downcase
+      still_unsorted.push shortest
+      shortest = word
+    else
+      still_unsorted.push word
+    end
+  end
+  sorted_array.push shortest
+
+  recursive_sort still_unsorted, sorted_array
+end
+
+puts (dictionary_sort(['hello', 'ant', 'raining', 'grass', 'A', 'dog']))

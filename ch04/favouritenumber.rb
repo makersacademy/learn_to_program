b88e7c3bc23f57230d490f194304891ce51ea5f4
Joe Sweeny <joesweeny84@hotmail.com>
2016-06-14 15:57:19 +0100

    Chapter 4 plus Makers Updates

---
+++ ch04/favouritenumber.rb
@@ -0,0 +1,4 @@
+puts "What's your favourite number?"
+number = gets.chomp
+numberBet = number.to_i + 1
+puts "#{numberBet} is a bigger and better number!"
---
+++ ch04/names.rb
@@ -0,0 +1,7 @@
+puts "What\'s your first name?"
+nameF = gets.chomp
+puts "And your middle name?"
+nameM = gets.chomp
+puts "And finally your last name?"
+nameL = gets.chomp
+puts "Hi #{nameF} #{nameM} #{nameL}, pleased to meet you!"

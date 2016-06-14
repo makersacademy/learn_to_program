cb7ba75363ad0b1e5d5fffb2b2d0765c4dea8496
Jess Astbury <jess.astbury@hotmail.com>
2016-06-14 10:15:06 +0000

    Created sorting function

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,28 @@
 def sort arr
-  # your code here
+    recursive_sort arr, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+    
+    if unsorted_array.length <= 0
+        return sorted_array
+    end
+    
+    smallest = unsorted_array.pop
+    still_unsorted = []
+    
+    unsorted_array.each do |testnum|
+        if testnum < smallest
+            still_unsorted << smallest
+            smallest = testnum
+        else
+            still_unsorted << testnum
+        end
+    end
+    sorted_array << smallest
+    
+    recursive_sort still_unsorted, sorted_array
+end
+
+
+puts sort(['table','garden','apple','pear'])

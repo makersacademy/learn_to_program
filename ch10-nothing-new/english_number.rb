d1ebe41bed51dc9f0bbe9ea850b4e5730c7ad97f
Jack Oddy <jackoddy91@gmail.com>
2016-06-14 14:23:50 +0100

    English number works up to quintillion by injecting a hash, but most numbers seem to append a ' '(space) from somewhere.

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,52 +1,80 @@
 def english_number n
 
+english = []
+
+units = {
+"quindecillion" 					=> 10 ** 48,
+"quattuordecillion"				=> 10 ** 45,
+"tredecillion"						=> 10 ** 42,
+"duodecillion"						=> 10 ** 39,
+"undecillion"							=> 10 ** 36,
+"decillion"								=> 10 ** 33,
+"nonillion"								=> 10 ** 30,
+"octillion"								=> 10 ** 27,
+"septillion"							=> 10 ** 24,
+"sextillion"							=> 10 ** 21,
+"quintillion"							=> 10 ** 18,
+"quadrillion"							=> 10 ** 15,
+"trillion"								=> 10 ** 12,
+"billion"									=> 10 ** 9,
+"million"									=> 10 ** 6,
+"thousand"								=> 10 ** 3,
+""												=> 1}
+
+units.inject(n) do |num, (name, value)|
+	unless num < value
+		english << words(num / value)
+		english << name
+	end
+	num - (value * (num / value))
+end
+
+english.compact.join(" ")
+
+end
+
+
+
+
+def words modulus
+
 		ones = [	"one", "two", "three", "four", "five",
 					"six", "seven", "eight", "nine"]
 
-		tens = [	"Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty",
-					"Seventy", "Eighty", "Ninety"]
+		tens = [	"ten", "twenty", "thirty", "fourty", "fifty", "sixty",
+					"seventy", "eighty", "ninety"]
 
-		teens =[	"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
-					"Seventeen", "Eighteen", "Nineteen"]
+		teens =[	"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+					"seventeen", "eighteen", "nineteen"]
 
-		string = []
+string = []
+a = modulus / 100
+if a > 0 #hundreds
+	string.push("#{ones[a-1]} hundred")
+end
+b = (modulus - a * 100) / 10
+if b > 1
+	string.push("#{tens[b-1]}")
+end
+c = modulus - ((a*100) + (b*10))
+if ((c > 0) && (b == 1))
+	string.push("#{teens[c-1]}")
+elsif (((c > 0) && (b != 1))) && (b > 1)
+	string[-1]<<("-#{ones[c-1]}")
+elsif (((c > 0) && (b != 1)))
+	string.push("#{ones[c-1]}")
 
-		a = n / 100
+end
 
-		if a > 0 #hundreds
-			string.push("#{ones[a-1]} hundred")
-		end
+string.join(' ')
 
-		b = (n - a * 100) / 10
+end
 
-		if b > 1
-			if a >= 1 then string.push " and " end
-			string.push("#{tens[b-1]} ")
-		end
-
-		c = n - ((a*100) + (b*10))
-
-		if ((c > 0) && (b == 1))
-			string.push("#{teens[c-1]}")
-		elsif ((c > 0) && (b != 1))
-			string.push("#{ones[c-1]}")
-		end
-
-
-
-
-		puts string.join.downcase
-
-	end
-
-
-
-
-english_number 347
+puts english_number 109238745102938560129834709285360238475982374561034
 english_number 256
 english_number 444
-english_number 12
-english_number 100
+puts english_number 3211
+puts english_number 100
 english_number 200
 english_number 999
 english_number 10

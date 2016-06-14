710839ea188b6de55fdb1161b5b0f52fbabd8dfc
Mali Michael <malimccalla@gmail.com>
2016-06-14 14:50:34 +0100

    Complete shuffle.rb in chapter 10

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,6 @@
-def shuffle arr
-  # your code here
+def shuffle(arr)
+  arr.shuffle
 end
+
+#puts shuffle([1,4,3,5,7,4,3,3,6,777,3,2,1,]).inspect
+#puts shuffle(["Hello", "How", "are", "you", "today"])

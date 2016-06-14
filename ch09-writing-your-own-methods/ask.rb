84ad407056d90b1409cc7d4ce9c85dd5220feb50
Tommy Williams <tom@teow.co.uk>
2016-06-14 12:12:15 +0100

    Ask V1

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,19 @@
-def ask question
-  # your code here
+def ask(question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply ==  "yes" || reply == "no")
+      if reply == "yes"
+        return true
+      else
+        return false
+      end
+      break
+    else
+      puts "Pease answer 'yes' or 'no'."
+    end
+  end
 end
+
+ask "Do you like Ruby?"

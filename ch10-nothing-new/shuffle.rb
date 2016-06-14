5eb82487e25f1c1077d84ea6bb1e79493db2f8fd
Terry <terry@terrytilley.com>
2016-06-14 14:49:23 +0100

    Complete shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,5 @@
-def shuffle arr
-  # your code here
+def shuffle(arr)
+  arr.shuffle
 end
+puts shuffle([1,2,3,4,5,6,7,8,9]).inspect
+puts shuffle(["terry","mali","becca","tommy","jess","tim","jack"]).inspect

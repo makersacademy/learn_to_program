143f4d8d71102e8af75dbab0497f1b3a45ad8cfe
prashantmathias <mathiasaudio@gmail.com>
2016-06-14 15:14:52 +0100

    Done with shuffle & sort exercises

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,24 @@
 def shuffle arr
-  # your code here
+  shuf = []
+
+  while arr.length > 0
+
+    rand_index = rand(arr.length)
+    curr_index = 0
+    new_arr = []
+
+    arr.each do |x|
+      if curr_index == rand_index
+        shuf << x
+      else
+        new_arr << x
+      end
+
+      curr_index += 1
+    end
+
+      arr = new_arr
+  end
+
+  shuf
 end

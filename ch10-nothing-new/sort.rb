0fb4222795161b204cb8afbe4414a9783f1fef06
Jack Oddy <jackoddy91@gmail.com>
2016-06-14 12:35:36 +0100

    confused trying to revert to file, pushing local version to github

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,27 +1,5 @@
 def sort arr
 
-recursive_sort arr, []
+arr.shuffle
 
 end
-
-def recursive_sort unsorted_arr, sorted_arr
-
-return sorted_arr if unsorted_arr.length <= 0
-
-sml = unsorted_arr.pop
-still_unsorted = []
-
-unsorted_arr.each do |t|
-  if t.downcase < sml
-    still_unsorted.push sml
-    sml = t
-  else
-    still_unsorted.push t
-  end
-end
-
-  sorted_arr.push sml
-  recursive_sort still_unsorted, sorted_arr
-end
-
-sort ["5","3","7","9","1","2","7"]

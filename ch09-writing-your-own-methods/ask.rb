f2af5e01e71849fcede503ed57b57f0196137934
Thady Condon <thadycondon@hotmail.com>
2016-06-14 20:08:10 +0100

    Chapter 09 commits, tested ask, old school roman, and roman with possitive results

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,20 @@
 def ask question
-  # your code here
+    while true
+        puts question
+        reply = gets.chomp.downcase
+        if reply == 'yes'|| reply == 'no'
+            if reply == 'yes'
+                return true
+                else
+                return false
+            end
+            break
+            else
+            puts 'Please answer "Yes" or "No".'
+        end
+    end
+    #return
+    #puts "do you like?"
 end
+
+ask 'Do you like tacos?'
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,15 @@
 def old_roman_numeral num
-  # your code here
+    if (num < 0 || num > 3000)
+       puts 'number is out of range'
+    end
+    m = 'M' * (num / 1000)
+    d = 'D' * ((num % 1000)/500)
+    c = 'C' * ((num % 500)/100)
+    l = 'L' * ((num % 100)/50)
+    x = 'X' * ((num % 50)/10)
+    v = 'V' * ((num % 10)/5)
+    i = 'I' * ((num % 5)/1)
+    return m+d+c+l+x+v+i
 end
+
+old_roman_numeral 99

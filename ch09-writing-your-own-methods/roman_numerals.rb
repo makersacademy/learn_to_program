394db3a519e3e779bf37bbfe459533fcf2a79f5a
Nomi Vos <nvoswork@gmail.com>
2016-06-14 05:22:52 -0500

    solution to roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,40 @@
 def roman_numeral num
-  # your code here
+  thousands = (num / 1000)
+  hundreds = (num % 1000 / 100)
+  tens = (num % 100 / 10)
+  ones = (num % 10 / 1)
+
+  number = 'M' * thousands
+
+  if hundreds == 9
+    number = number + 'CM'
+  elsif hundreds == 4
+    number = number + 'CD'
+  else
+    number = number + 'D' * (num % 1000 / 500)
+    number = number + 'C' * (num % 500 / 100)
+  end
+
+  if tens == 9
+    number = number + 'XC'
+  elsif tens == 4
+    number = number + 'XL'
+  else
+    number = number + 'L' * (num % 100 / 50)
+    number = number + 'X' * (num % 50 / 10)
+  end
+
+  if ones == 9
+    number = number + 'IX'
+  elsif tens == 4
+    number = number + 'IV'
+  else
+    number = number + 'V' * (num % 10 / 5)
+    number = number + 'I' * (num % 5 / 1)
+  end
+
+  number
+
 end
+
+puts (roman_numeral(1589))

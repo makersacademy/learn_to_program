d24b52c9258ba2f2d8efe52c8d270121bf09c0bf
BenWhite <benjaminwhite@live.co.uk>
2016-06-14 19:37:55 +0000

    Incremental update of answers

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
 def profile block_description, &block
-  # your code here
+  profile_on = true
+  if profile_on
+    start_time = Time.new
+    block[]
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block[]
+  end
 end

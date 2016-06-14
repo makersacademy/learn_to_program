f37b6d1f5b321f216ea88f6e925a6c9e0951b865
Joe Sweeny <joesweeny84@hotmail.com>
2016-06-14 14:45:32 +0100

    Chapter 1

---
+++ ch01/numbers.rb
@@ -0,0 +1,11 @@
+#How many hours in a year?
+puts 365 * 24
+
+#How many minutes in a decade?
+puts 60 * 24 * 365 * 10
+
+#How many seconds old am I?
+puts (60 * 60 * 24 * 365 * 32) + (60 * 60 * 24 * 94) + (60 * 60 * 2) + (60 * 26)
+
+#If I am 1246 million seconds old, how old am I?
+puts 1246000000/60/60/24/365

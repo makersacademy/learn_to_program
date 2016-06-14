58039620b3427739550293189ce4b2ae25374178
prashantmathias <mathiasaudio@gmail.com>
2016-06-14 12:49:01 +0100

    Done with DS exercise

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,9 @@
 def dictionary_sort arr
-  # your code here
+  return arr if arr.size <= 1
+
+  m = arr.pop.downcase
+  less = arr.select{|x| x.downcase < m}
+  more = arr.select{|x| x.downcase >= m}
+
+  dictionary_sort(less) + [m] + dictionary_sort(more)
 end

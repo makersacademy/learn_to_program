53e4e56eae9f8f3eeb10f5efd1ad908cbcb3ffd9
Jack Oddy <jackoddy91@gmail.com>
2016-06-14 12:21:21 +0100

    shuffle method

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,16 +1,3 @@
 def shuffle arr
-
-n = arr.length
-
-arr.insert(rand(0..(arr.length)), arr.pop)
-
-n = n - 1
-
 arr.shuffle
-
-puts arr.compact
-
 end
-
-
-shuffle ["1","2","3","4","5","6","7","8"]

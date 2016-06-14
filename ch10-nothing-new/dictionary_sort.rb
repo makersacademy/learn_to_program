722fdda71ad3cb707aa3aa387732392b3f591265
Jess Astbury <jess.astbury@hotmail.com>
2016-06-14 11:54:50 +0000

    Created the dictionary sort method

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
 def dictionary_sort arr
-  # your code here
+    recursive_sort arr, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+    
+    if unsorted_array.length <= 0
+        return sorted_array
+    end
+    
+    smallest = unsorted_array.pop
+    still_unsorted = []
+    
+    unsorted_array.each do |testnum|
+        if testnum.downcase < smallest.downcase
+            still_unsorted << smallest
+            smallest = testnum
+        else
+            still_unsorted << testnum
+        end
+    end
+    sorted_array << smallest
+    
+    recursive_sort still_unsorted, sorted_array
+end
+
+print dictionary_sort ['Table', 'apple', 'chair', 'Banana', 'Laptop']

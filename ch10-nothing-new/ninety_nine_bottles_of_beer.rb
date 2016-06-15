791f661862ef8342dca1baa95471b05c2c2dc9d1
Mali Michael <malimccalla@gmail.com>
2016-06-15 11:08:21 +0100

    Complete chapter 10 ninety nine bottles requring englisgh number

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,19 @@
-# your code here
+require_relative 'english_number'
+
+def ninety_nine_bottles_of_beer(bottles)
+  while bottles > 0
+    if bottles > 2
+      puts "#{english_number(bottles).capitalize} bottles of beer on the wall, #{english_number(bottles)} bottles of beer. Take one down, pass it around, #{english_number(bottles-1)} bottles of beer on the wall."
+      bottles -= 1
+    elsif bottles == 2
+      puts "#{english_number(bottles).capitalize} bottles of beer on the wall #{english_number(bottles)} bottles of beer. Take one down, pass it around, #{english_number(bottles-1)} bottle of beer on the wall."
+      bottles -=1
+    else
+      puts "#{english_number(bottles).capitalize} bottle of beer on the wall #{english_number(bottles)} bottle of beer. Take one down, pass it around, well... Now you're drunk arent you."
+      bottles -=1
+    end
+  end
+end
+
+#ninety_nine_bottles_of_beer(5)
+#ninety_nine_bottles_of_beer(9999)

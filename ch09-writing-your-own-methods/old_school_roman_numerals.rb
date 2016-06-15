49e27630d6659e3efd0202393c3f3177f1ab6dc7
acsauk <acsauk@gmail.com>
2016-06-15 16:25:52 +0100

    Uncompleted roman numerals - moving on to ch10

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,54 @@
 def old_roman_numeral num
-  # your code here
+  while true
+    numerals = {
+      1 => 'I',
+      5 => 'V',
+      10 => 'X',
+      50 => 'L',
+      100 => 'C',
+      500 => 'D',
+      1000 => 'M'
+    }
+
+    str = ""
+    num = gets.chomp.to_i
+
+    numerals.each do |number, numeral|
+      if num == number
+        return str + "#{numeral}"
+      elsif num / 5 == even && num % 5 == 0 # does not end in a 5
+        puts
+      elsif num / 5 == odd && num % 5 == 0 # does end in a five
+        puts
+      elsif
+    
+
+    if num % 5 != 0 && num < 5
+      puts i *num
+    end
+
+    if num % 5 == 0 && num < 10
+      puts v
+    end
+
+    if num % 5 != 0 && num > 5 && num < 10
+      puts v + (i * (num % 5))
+    end
+
+    if num % 10 == 0 && num < 50
+      puts x
+    end
+
+    if num % 10 != 0 && num % 5 == 0 && num > 10 && num < 50
+      puts x + v + (i * (num % 5))
+    end
+
+    if num % 10 != 0 && num % 5 != 0 && num > 10 && num < 50
+      puts (x * (num / 10)) + (i * (num % 5))
+    end
+  break
+  end
 end
+
+puts "Hi - please enter a number"
+old_roman_numeral 0

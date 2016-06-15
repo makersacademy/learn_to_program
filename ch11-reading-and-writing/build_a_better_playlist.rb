e619faff6ee5d070d639b792171c8ab5ef1a9295
prashantmathias <mathiasaudio@gmail.com>
2016-06-15 18:50:45 +0100

    Done with BABP Exercise

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,46 @@
 def music_shuffle filenames
-  # your code here
+
+  filenames = filenames.sort
+  len = filenames.length
+
+
+  2.times do
+    l_idx = 0
+    r_idx = len/2
+    shuf = []
+
+
+    while shuf.length < len
+      if shuf.length%2 == 0
+
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+
+    filenames = shuf
+  end
+
+  arr = []
+  cut = rand(len)
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+
+  arr
 end
+
+songs = ['/Users/prashantmathias/music/Puscifer/Conditions_of_My_Parole/01_Tiny_Monsters.m4a',
+         '/Users/prashantmathias/music/Puscifer/Conditions_of_My_Parole/02_Green_Valley.m4a',
+         '/Users/prashantmathias/music/Puscifer/Conditions_of_My_Parole/03_Monsoons.m4a',
+         '/Users/prashantmathias/music/Puscifer/Conditions_of_My_Parole/04_Telling_Ghosts.m4a',
+         '/Users/prashantmathias/music/Puscifer/Conditions_of_My_Parole/05_Horizons.m4a']
+
+puts (music_shuffle(songs))

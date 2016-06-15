dfd0f447781f61cd6c6f1ef0dc0f5873a2858ced
Luke Cartledge <luke@lukecartledge.com>
2016-06-15 21:27:47 +0100

    Cleaned code

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,15 +1,15 @@
-months = {'january'=>1,
-          'february'=>2,
-          'march'=>3,
-          'april'=>4,
-          'may'=>5,
-          'june'=>6,
-          'july'=>7,
-          'august'=>8,
-          'september'=>9,
-          'october'=>10,
-          'november'=>11,
-          'december'=>12}
+months = { 'january' => 1,
+           'february' => 2,
+           'march' => 3,
+           'april' => 4,
+           'may' => 5,
+           'june' => 6,
+           'july' => 7,
+           'august' => 8,
+           'september' => 9,
+           'october' => 10,
+           'november' => 11,
+           'december' => 12 }
 
 puts 'Hello, what year where you born in?'
 year = gets.chomp.to_i
@@ -19,7 +19,7 @@ puts 'Finally could you tell me the day of the month on which you where born?'
 day = gets.chomp.to_i
 
 dob = Time.gm(year, months[month], day)
-years = ((Time.new - dob)/31557600).floor
+years = ((Time.new - dob) / 31_557_600).floor
 puts
 puts 'Here are your spanks!'
-years.times{puts 'SPANK!'}
+years.times { puts 'SPANK!' }
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,7 +1,7 @@
 luke = Time.gm(1980, 07, 13, 2)
 
 def billion_secs(dob)
-  puts dob + 1000000000
+  puts dob + 1_000_000_000
 end
 
 billion_secs(luke)

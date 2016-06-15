cdbd7b0931157281004d1c317fa39f6a9f1851ae
Thady Condon <thadycondon@hotmail.com>
2016-06-15 21:37:04 +0100

    Chapter 10 dictionary sort is working okay, shuffle is returning an error but i can't see what it is.

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,18 @@
-def shuffle arr
-  # your code here
+def shuffle some_array
+    recursive_shuffle some_array, []
 end
+
+def recursive_shuffle unsorted_array, shuffle_array
+    y = -1
+    until shuffle_array.count == unsorted_array.count
+        x = rand(unsorted_array.count)
+        if x == y then
+            else
+            y = x
+            shuffle_array << unsorted_array[x]
+        end
+    end
+    print shuffle_array
+    end
+
+#shuffle(['pear','apple', 'banana','orange','blackberry'])

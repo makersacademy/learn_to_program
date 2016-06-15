95ff0292327b9818358be2abeb5e867f85897014
acsauk <acsauk@gmail.com>
2016-06-15 22:19:17 +0100

    Adding attempt at sort.rb - not completed :(

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,30 @@
 def sort arr
-  # your code here
-end
+
+  #puts "Enter some names:"
+
+  #arr = gets.chomp
+  names = arr
+
+  #while arr != ''
+  #  names.push arr
+  #  arr = gets.chomp
+  #end
+
+  names.each_index do |first|
+    (first + 1..names.length).each do |second|
+      if names[first] > names[second]
+        names[first], names[second] = names[second], names[first]
+        puts "[#{names.join(', ')}]"
+      end
+    end
+  end
+  puts "[#{names.join(', ')}]"
+  end
+
+sort ['Alex', 'Zara', 'Bill', 'Gerrard']
+
+  #Move the 'smallest' word to the end of sorted array
+
+  #Sort the words that are being added to the sorted array
+
+  #Print the finished sorted array

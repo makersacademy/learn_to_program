f41cbc0d33fa80bb63695bb9715f05b7d42fa244
Jack Oddy <jackoddy91@gmail.com>
2016-06-15 14:43:59 +0100

    Adds a method to check whether or not the directory exists. Will change to directory if it does and will return an error message if it does not before running the method again with new input.

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,27 @@
-# your code here
+puts "Where are the files located that you want to download?"
+
+directory = gets.chomp
+
+def change_dir dir
+
+  if File.exist?(dir)
+    Dir.chdir "#{dir}"
+    puts Dir.pwd
+  else
+    puts "Invalid Directory. Please input a valid directory"
+    change_dir gets.chomp
+  end
+
+end
+
+change_dir directory
+
+pic_names = Dir['**/*.{JPG,jpg}']
+
+puts "Where would you like to download these pictures to?"
+
+change_dir gets.chomp
+
+puts "What would you like to call this batch of photos?"
+
+batch_name = gets.chomp

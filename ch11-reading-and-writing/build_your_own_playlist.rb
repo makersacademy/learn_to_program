a670b9d9bf7b462023f35bc519ad242b07db7e2a
Luke Cartledge <luke@lukecartledge.com>
2016-06-15 15:40:04 +0100

    Partial Fix

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -7,12 +7,13 @@ def playlist_maker
   playlist_name = "#{input}.m3u"
   puts 'Would you like this playlist to be random?'
   random = gets.chomp.downcase
-  if random == 'yes'|'y'
+  if random == ('yes' || 'y')
     track_names.shuffle
   end
   File.open(playlist_name, w) do |f|
     track_names.each{|mp3| f.write mp3+"\n"}
   end
+  puts 'Done!'
 end
 
-puts 'Done!'
+playlist_maker

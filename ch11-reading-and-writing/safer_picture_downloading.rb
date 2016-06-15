41c6ee6a7e9e0243a52616b140c5f3b969d55e49
Jack Oddy <jackoddy91@gmail.com>
2016-06-15 15:59:43 +0100

    adds method to check if a batch has already been used by checking if there are already files with the batch name present. Also adds block to rename files, however only works when renaming files in current directory as it changes directory and the paths are no longer valid from the new working directory.

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -18,10 +18,42 @@ change_dir directory
 
 pic_names = Dir['**/*.{JPG,jpg}']
 
+
 puts "Where would you like to download these pictures to?"
 
 change_dir gets.chomp
 
 puts "What would you like to call this batch of photos?"
 
-batch_name = gets.chomp
+def batch_name bname
+
+  if File.exist?("#{bname}/\d+/.{JPG,jpg}")
+    puts "Error. A batch already exists with this name.\n
+    Please choose another name for this batch."
+    batch_name gets.chomp
+  else
+    bname
+  end
+
+end
+
+batch = batch_name gets.chomp
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.'
+
+  new_name = if pic_number < 10
+      "#{batch}0#{pic_number}.jpg"
+    else
+      "#{batch}#{pic_number}.jpg"
+    end
+
+    File.rename name, new_name
+
+    pic_number += 1
+  end
+
+puts #
+puts "Pictures moved to #{Dir.pwd} and named \"#{batch}\""

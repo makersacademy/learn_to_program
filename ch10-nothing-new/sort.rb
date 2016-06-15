b93366de68b9a43b31c938b66a273dccaa710f09
JulieLev <jkgee1@yahoo.co.uk>
2016-06-15 14:45:34 +0000

    Adding sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,35 @@
-def sort arr
-  # your code here
+
+def mysort arr # This "wraps" recursive_sort.
+    recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted
+
+    #when to stop...
+    if unsorted.length <= 0
+      return sorted
+    end
+
+    smallest = unsorted.pop
+    still_to_sort = []
+
+    unsorted.each do|word|
+        if word < smallest
+            still_to_sort.push smallest #chuck popped word back as it isn't smallest
+            smallest = word #this will be smallest so far 
+        else 
+            still_to_sort.push word #chuck this one back in again
+        end
+    end
+
+    sorted.push smallest #take this one 
+    
+    recursive_sort still_to_sort, sorted
+end
+
+
+arr = ['beaver', 'badger', 'bear', 'lion', 'tiger', 'panda', 'bear', 'giraffe']
+puts mysort arr
+puts arr.sort #to check it's working
+
+

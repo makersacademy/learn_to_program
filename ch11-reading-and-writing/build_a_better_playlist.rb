3973029e1e3f1e9fd398f61b93cfbb07f1962803
Jack Oddy <jackoddy91@gmail.com>
2016-06-15 19:21:04 +0100

    adds a program to allow user to create a playlist out of all music files in a specified directory and to a different one

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,27 @@
 def music_shuffle filenames
-  # your code here
+  arr = filenames.split("\n")
+
+  a = []
+  b = []
+
+  c = 1
+  arr.each do |x|
+    c % 2 == 0 ? a << x : b << x
+    c += 1
+  end
+
+shuf = []
+
+until (a == []) && (b == [])
+  shuf << a
+  shuf << b
 end
+
+
+
+
+shuf.compact
+
+end
+
+music_shuffle "this\nis\na test to\n see if this \n program\nactually\nworks"

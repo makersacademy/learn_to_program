c3304c3ba4826703244d0406b875ccb27c6b73d8
Jonny Moore <jonnymoore121212@gmail.com>
2016-06-15 13:24:27 +0100

    Added ch10/bottles solution

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,30 @@
-# your code here
+def bottles(n)
+   # Method which sings the bottles of beer song with n verses
+   while n > 0
+      if n > 1
+         puts "#{n} bottles of beer on the wall,"
+      else
+         puts "#{n} bottle of beer on the wall,"
+      end
+
+      if n > 1
+         puts "#{n} bottles of beer."
+      else
+         puts "#{n} bottle of beer."
+      end
+
+
+      puts "Take one down, pass it around,"
+      if n > 2
+         puts "#{n-1} bottles of beer on the wall."
+      elsif n == 2
+         puts "#{n-1} bottle of beer on the wall."         
+      else
+         puts "No more bottles of beer on the wall."
+      end
+      puts ""
+      n -= 1
+   end
+end
+
+bottles(5)

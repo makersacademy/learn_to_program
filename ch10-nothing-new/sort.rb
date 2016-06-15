5e50e58ff47d111d7f578789a3972101f6ea3541
samjbro <sjbroughton1@gmail.com>
2016-06-15 20:39:53 +0100

    Ch09 & Ch10 exercises completed

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,13 @@
-def sort arr
-  # your code here
+def sort unsorted
+ 	recursive_sort unsorted, []
 end
+
+def recursive_sort unsorted, sorted
+  lowest = unsorted.min
+  sorted << lowest
+  unsorted.delete_at(unsorted.index(lowest))
+  unsorted.empty? ? sorted : (recursive_sort unsorted, sorted)
+end
+
+sort([4,7,2,8,33,5,2,11,7,544,22,7,2,1,456,866,3355])
+sort(['can','feel','singing','like','a','can'])

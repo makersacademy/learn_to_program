ac37d2547e12211d3443679f4ef66e29f6c43255
hannako <jessjones1@gmail.com>
2016-06-15 19:06:56 +0100

    Both sorting programmes

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,13 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+
+  return array if array.length <= 1
+
+array.each{|x| x.downcase}
+
+  middle = array.pop
+  less = array.select{|x| x < middle}
+  more = array.select{|x| x >= middle}
+
+  return dictionary_sort(less) + [middle] + dictionary_sort(more)
+
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,9 @@
-def sort arr
-  # your code here
+def sort (array)
+return array if array.length <= 1
+
+middle = array.pop
+less = array.select{|x| x < middle}
+more = array.select{|x| x >= middle}
+
+return sort(less) + [middle] + sort(more)
 end

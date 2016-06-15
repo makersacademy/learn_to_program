a7a62ad1e5b495190e1f5d3323f695890bb67fac
jluong <jason.qh.luong@gmail.com>
2016-06-15 23:16:54 +0000

    Completed old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,30 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+    rom_num = ' '
+    while num != 0
+        if num / 1000 > 0
+            rom_num << "M" * (num / 1000)
+            num = num % 1000
+        elsif num / 500 > 0
+            rom_num << "D" * (num / 500)
+            num = num % 500
+        elsif num / 100 > 0
+            rom_num << "C" * (num / 100)
+            num = num % 100
+        elsif num / 50 > 0
+            rom_num << "L" * (num / 50)
+            num = num % 50
+        elsif num / 10 > 0
+            rom_num << "X" * (num / 10)
+            num = num % 10
+        elsif num / 5 > 0
+            rom_num << "V" * (num / 5)
+            num = num % 5
+        else
+            rom_num << "I" * num
+            num = 0
+            
+        end
+    end
+    
+    puts rom_num
 end
--- learn2program_ch09.rb
+++ learn2program_ch09.rb
@@ -1,3 +1,149 @@
-def say_moo
-    puts "moooooooooooooooooooo...."
+# 9.1: Method Parameters
+=begin
+puts "9.1: Method Parameters\n"
+
+def say_moo(number_of_moos=1)
+    puts "mooooo...." * number_of_moos
 end
+
+say_moo(3)
+
+def double_this(num)
+    num_times_2 = num * 2
+    puts num.to_s+' doubled is '+num_times_2.to_s
+end
+
+double_this(44)
+
+# 9.4: Return Values
+
+puts "\n9.4: Return Values\n"
+
+def say_moo(number_of_moos)
+    puts 'mooooo...' * number_of_moos
+    'yellow submarine'
+end
+x = say_moo(3)
+puts x.capitalize + ', dude...'
+puts x + '.'
+
+def favourite_food(name)
+    if name == 'Lister'
+        return 'vindaloo'
+    end
+    if name == 'Rimmer'
+        return 'mashed potatoes'
+    end
+    
+    'hard to say...maybe fried plantain?'
+end
+
+def favourite_drink(name)
+    if name =='Jean-Luc'
+        'tea, Earl Grey, hot'
+    elsif name == 'Kathryn'
+        'coffee, black'
+    else
+        'perhaps...horchata?'
+    end
+end
+
+puts favourite_food('Rimmer')
+puts favourite_food('Lister')
+puts favourite_food('Cher')
+puts favourite_drink('Kathryn')
+puts favourite_drink('Oprah')
+puts favourite_drink('Jean-Luc')
+
+=end
+
+=begin
+# Psychology Experiment: Refactored
+puts "\nPsychology Experiment: Refactored\n"
+
+def ask(question)
+    while true
+        puts question
+        reply = gets.chomp.downcase
+        
+        if (reply == 'yes' || reply == 'no')
+            if reply == 'yes'
+                answer = true
+            else
+                answer = false
+            end
+            break
+        else
+            puts "Please answer 'yes' or 'no'."
+        end
+    end
+    
+    answer # This is what we reutrn (true or false).
+end
+
+puts "Hello, and thank you for..."
+puts
+ask("Do you like eating tacos?")        # Ignore this return value
+ask("Do you like eating burritos?")     # And this one
+
+wets_bed = ask("Do you wet the bed?")  # Save this return value
+
+ask("Do you like eating chimichangas?")
+ask("Do you like eating sopapillas?")
+
+puts "Just a few more questions"
+
+ask("Do you like drinking horchata?")
+ask("Do you like eating flautas?")
+puts
+
+puts "DEBRIEFING:"
+puts "Thank you for..."
+puts
+puts wets_bed
+=end
+
+=begin
+# 9.5.1: Psychology Experiment Refactored: Improved ask method
+puts "\nPsychology Experiment Refactored: Improved ask method\n"
+
+def ask(question)
+    while true
+        puts question
+        reply = gets.chomp.downcase
+        
+        if (reply == 'yes' || reply == 'no')
+            if reply == 'yes'
+                return true
+            else
+                return false
+            end
+            break
+        else
+            puts "Please answer 'yes' or 'no'."
+        end
+    end
+end
+
+puts "Hello, and thank you for..."
+puts
+ask("Do you like eating tacos?")        # Ignore this return value
+ask("Do you like eating burritos?")     # And this one
+
+wets_bed = ask("Do you wet the bed?")  # Save this return value
+
+ask("Do you like eating chimichangas?")
+ask("Do you like eating sopapillas?")
+
+puts "Just a few more questions"
+
+ask("Do you like drinking horchata?")
+ask("Do you like eating flautas?")
+puts
+
+puts "DEBRIEFING:"
+puts "Thank you for..."
+puts
+puts wets_bed
+
+=end

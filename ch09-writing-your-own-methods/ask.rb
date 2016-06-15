8a9b6816c1b48b66b9e1132ce5c127d98386ff00
sus111 <sgraham535@gmail.com>
2016-06-15 21:04:49 +0100

    re-added ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -7,18 +7,3 @@ def ask question
   end
   reply == "yes" ? true : false
 end
-
-puts 'Hello, and thank you for...'
-puts
-
-ask('Do you like eating tacos?') # Ignore this return value ask 'Do you like eating burritos?' # And this one
-wets_bed = ask('Do you wet the bed?') # Save this return value ask ' Do you like eating chimichangas?'
-ask('Do you like eating sopapillas?')
-puts 'Just a few more questions...'
-ask('Do you like drinking horchata?')
-ask('Do you like eating flautas?')
-
-puts
-puts 'DEBRIEFING:'
-puts 'Thank you for...' 
-puts wets_bed

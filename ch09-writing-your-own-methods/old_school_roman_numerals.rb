d00ac0bc962859e0a389c50d7a4976a57e1683e0
richo225 <richo225@gmail.com>
2016-06-15 18:30:57 +0100

    Submit old_school_roman_numerals.rb solution

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
 def old_roman_numeral num
-  # your code here
+
+  if num >3000 || num <1
+    puts "Please enter a number in between 1 and 3000"
+  end
+
+
+rom_M = "M" * (num / 1000)
+rom_D = "D" * (num % 1000 / 500)
+rom_C = "C" * (num % 500 / 100)
+rom_L = "L" * (num % 100 / 50)
+rom_X = "X" * (num % 50 / 10)
+rom_V = "V" * (num % 10 / 5)
+rom_I = "I" * (num % 5 / 1)
+
+rom = rom_M + rom_D + rom_C + rom_L + rom_X + rom_V + rom_I
+#rom = "#{rom_M}#{rom_D}#{rom_C}#{rom_L}#{rom_X}#{rom_V}#{rom_I}"
+puts rom.to_s
 end

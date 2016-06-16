702b41cdb0b3f0046644231a5af1c1da12f714a1
Mali Michael <malimccalla@gmail.com>
2016-06-16 09:18:37 +0100

    Change File.exist? parameter from 'name' to 'new_name' for photo rename program in chapter 11

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -9,12 +9,12 @@ print "Downloading #{pic_names.length} files:  "
 pic_number = 1
 pic_names.each do |name|
   print '.'
-  new_name = if pic_number <10
+  new_name = if pic_number < 10
     "#{batch_name}0#{pic_number}.jpg"
   else
     "#{batch_name}#{pic_number}.jpg"
   end
-  if File.exists?(name) == true  # checking to see if filename already exists to prevent overwriting
+  if File.exists?(new_name) == true  # checking to see if filename already exists to prevent overwriting
     puts "Error: One or more file by this name already exists"
     exit
   else

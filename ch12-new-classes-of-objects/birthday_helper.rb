3331ddf2e2f2a2b602d7419fdcbb5045a59d9b55
Jonny Moore <jonnymoore121212@gmail.com>
2016-06-16 10:24:53 +0100

    Added ch12/bday_helper ch12/bday_helper skeleton

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,17 @@
-# your code here
+# I've completed the task here, but I only return the date of the birthday.
+# I don't calculate how old they will turn as well.
+
+birthdays = {}
+
+File.readlines('birthdays.txt').each do |line|
+   name, date, year = line.split(',')
+   birthdays[name] = date
+end
+
+p birthdays
+
+puts "Whose birthday are you after?"
+name = gets.chomp
+
+puts "No idea, but I do know LOADS of other birthdays" if name == nil
+puts "Their next birthday will be on #{birthdays[name]}"

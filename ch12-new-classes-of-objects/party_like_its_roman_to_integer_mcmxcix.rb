c80b15e34e5146e3b6bc3c850620d879b92d1a65
Jack Oddy <jackoddy91@gmail.com>
2016-06-16 12:49:47 +0100

    Adds method to convert roman numerals into integers

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,34 @@
 def roman_to_integer roman
-  # your code here
+
+  numerals_hash = { "M"=>1000,
+                    "D"=>500,
+                    "C"=>100,
+                    "L"=>50,
+                    "X"=>10,
+                    "V"=>5,
+                    "I"=>1}
+
+  total = 0
+  prev = 0
+  index = roman.length - 1
+
+  while index >= 0
+    c = roman[index].upcase
+    index -= 1
+    val = numerals_hash[c]
+
+puts "Not a valid numeral" if !val
+
+    if val < prev
+      val *= -1
+    else
+      prev = val
+    end
+
+  total = total + val
+
+end
+
+total
+
 end

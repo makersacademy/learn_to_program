f96deeb5689d5937245c2d8f568d5dda1add56b7
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-16 10:33:21 +0100

    Added ch12 one billion seconds and happy Birthday challenge

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,3 +1,7 @@
-age_in_seconds = Time.now - Time.local(1992, 12, 4)
+age_in_seconds = Time.now - Time.local(1992, 12, 4, 1, 32)
 
-puts age_in_seconds
+puts "You are #{age_in_seconds} seconds old!"
+
+billion_old = Time.local(1992, 12, 4, 1, 32) + 10**9
+
+puts "You will be a billion seconds old on #{billion_old}"

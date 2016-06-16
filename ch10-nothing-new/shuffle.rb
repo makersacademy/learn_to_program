cdf4f961c12ce7076838021099c565e03671dc8e
Thady Condon <thadycondon@hotmail.com>
2016-06-16 22:19:30 +0100

    Chap11, built playlist creater, had to update the shuffle file from Chap10, had the wrong code.

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,18 +1,18 @@
 def shuffle some_array
-    recursive_shuffle some_array, []
+    recursive_sort some_array, []
 end
 
-def recursive_shuffle unsorted_array, shuffle_array
+def recursive_sort unsorted_array, sorted_array
     y = -1
-    until shuffle_array.count == unsorted_array.count
+    until sorted_array.count == unsorted_array.count
         x = rand(unsorted_array.count)
         if x == y then
-            else
-            y = x
-            shuffle_array << unsorted_array[x]
+        else
+        y = x
+        sorted_array << unsorted_array[x]
         end
-    end
-    print shuffle_array
+        end
+   # print   sorted_array
     end
 
-#shuffle(['pear','apple', 'banana','orange','blackberry'])
+#sort(['pear','apple', 'banana','orange','blackberry'])
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,37 @@
-# your code here
+require '/Users/thadycondon/Documents/learn_to_program/ch10-nothing-new/shuffle.rb'
+
+Dir.chdir '/Users/thadycondon/Desktop/Chap11'
+
+tune_names = Dir['*.{m3u,mp3,m4a}']
+tunes = shuffle tune_names
+#puts tunes
+#exit
+puts 'What would you like to call this playlist?'
+batch_name = gets.chomp
+
+puts
+
+print "Downloading #{tune_names.length} files: "
+tune_number = 1
+
+if File.exist?("#{batch_name}0#{tune_number}.m3u") == true
+  puts 'File already exists, abandon ship!'
+  exit
+end
+
+tune_names.each do |name| print '.'
+  new_name = if tune_number < 10
+    "#{batch_name}0#{tune_number}.m3u"
+  else
+    "#{batch_name}#{tune_number}.m3u"
+  end
+  
+  File.rename name, new_name
+  tune_number = tune_number + 1
+  
+end
+
+
+
+puts
+puts 'Done'

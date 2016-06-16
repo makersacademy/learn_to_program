3d218c583ccb3baa45ae924acbc9b0bd86508b64
Jess Astbury <jess.astbury@hotmail.com>
2016-06-16 23:28:29 +0000

    Roman numeral to integer method started

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,34 @@
 def roman_to_integer roman
-  # your code here
+    
+roman_numerals = {
+    "M" => 1000,
+    "D" => 500,
+    "C" => 100,
+    "L" => 50,
+    "X" => 10,
+    "V" => 5,
+    "I" => 1,
+}
+
+integer = 0
+
+numerals = roman.upcase.split("")
+
+numerals.each do |x|
+if roman_numerals.has_key?(x) == false
+    puts "This is not a valid Roman Numeral"
+    exit
 end
+end
+
+numerals.each do |x|
+    roman_numerals.each do |key, value|
+    integer +=value if key == x
+end
+end
+
+integer
+
+end
+
+roman_to_integer("MMMDCJ")

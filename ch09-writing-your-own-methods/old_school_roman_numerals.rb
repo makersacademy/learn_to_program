87ecf207bd344d84d2575857d8cb49edfef6af44
Zeeshan <mzishtiaq@yahoo.co.uk>
2016-06-16 18:34:36 +0000

    solve old roman numeral

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,14 @@
 def old_roman_numeral num
-  # your code here
+   
+   #mapping the roman numberals in a hash
+    roman_numerals_old = {1000 => "M", 500=> "D", 100=> "C",
+                            50=> "L", 10=> "X", 5=> "V", 1=> "I"}
+    
+    #variable to include the numberals
+    roman_old = ''
+    roman_numerals_old.each do |key, value|
+      (num/key).times{roman_old << value; num -= key}
+    end
+    roman_old
+
 end

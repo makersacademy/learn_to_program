08ba7d03589e11320068968fdf3114c5000537e4
Terry <terry@terrytilley.com>
2016-06-16 12:12:41 +0100

    Complete build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,24 @@
-def music_shuffle filenames
-  # your code here
+# Not sure if this is just a shuffle or mixed up.
+def music_shuffle(filenames)
+  filenames = filenames.sort
+
+  length = filenames.length
+  half = length / 2
+  left_half = filenames[0...half]
+  right_half = filenames[half..-1]
+
+  shuff = []
+  alternate = right_half.zip(left_half).flatten.compact
+
+  while shuff.length < length
+    alternate.each_with_index do |song, index|
+      if index == rand(length)
+        shuff << song
+        alternate.delete(song)
+      end
+    end
+  end
+  shuff
 end
+
+#puts music_shuffle(["Song1", "Song2", "Song3", "Song4", "Song5", "Song6", "Song7"])

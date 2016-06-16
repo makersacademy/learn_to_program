4d4ecd6d9651e95cb16773ff1939b06da636d483
Terry <terry@terrytilley.com>
2016-06-16 10:43:01 +0100

    Complete build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,17 @@
-# your code here
+require_relative '../ch10-nothing-new/shuffle'
+
+music_path = '/Users/apple/Desktop/music/'
+Dir.chdir(music_path)
+all_music = shuffle(Dir['*.{MP3,mp3}'])
+
+puts "What would you like to call your playlist?"
+playlist_name = gets.chomp
+
+File.open(playlist_name + ".m3u", "w") do |file|
+  all_music.each do |mp3|
+    file.write(music_path + mp3 + "\n")
+  end
+end
+
+puts ""
+puts "Playlist \"#{playlist_name}\" created!"

07521fe90f0650e5bb36471af8e42fe6147a35b2
Jack Oddy <jackoddy91@gmail.com>
2016-06-16 13:34:30 +0100

    Adds program to read a text file of birthdays sorted [name].[dd/mm/yyy] and allows user to search for birthdays using the name

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,15 @@
-# your code here
+birthdates = File.read('birthdays.txt').split("\n")
+
+birthdays_hash = birthdates.inject({}) do |h, a|
+  splt = a.split(".")
+  h[splt[0]] = splt[1]
+  h
+end
+
+puts "Whose Birthday have you forgotten again?"
+name = gets.chomp
+
+bday = birthdays_hash[name]
+
+
+puts "#{name}'s Birthday is #{bday[0..4]}"

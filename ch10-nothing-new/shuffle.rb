f6d6bbb30d0dd6d2efeb29ce7b5194f9b25b9d60
Terry <terry@terrytilley.com>
2016-06-16 10:42:15 +0100

    Commented out the output

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,5 +1,5 @@
 def shuffle(arr)
   arr.shuffle
 end
-puts shuffle([1,2,3,4,5,6,7,8,9]).inspect
-puts shuffle(["terry","mali","becca","tommy","jess","tim","jack"]).inspect
+#puts shuffle([1,2,3,4,5,6,7,8,9]).inspect
+#puts shuffle(["terry","mali","becca","tommy","jess","tim","jack"]).inspect

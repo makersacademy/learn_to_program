592fb05b3d8c5a5c8ee07c98b74374195d259812
Laurence Kite <laurent.kitecb@gmail.com>
2016-06-16 15:11:44 +0000

    shuffle that work re rspec

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,28 +1,5 @@
-def recursive_shuffle unshuffled, shuffled
-  shuff_length = unshuffled.length 
-  if unshuffled == []
-    unshuffled = shuffled
-    return shuffled
-  end  
-  #generate a random number from 0 to unshuffled length
-  random_index = rand(0..shuff_length -1)
-  #iterate over array find the random item and push into shuffled until unshuffled is empty then return 
-  #set counter to zero outside loop and use it to get the random item
-  item_counter = 0
-  unshuffled.each do |item|
-    if item_counter == random_index
-       item_push = item
-       shuffled.push item_push
-       #remove item from unshuffled 
-       unshuffled.delete_at(random_index)
-    end   
-    item_counter +=1
-  end
-  recursive_shuffle unshuffled, shuffled
-end    
 def shuffle arr
-  # your code here
-  recursive_shuffle arr, []
+  # I created my own shuffle called myshuffe that works recursively but it wont be accepted by rspec
+  #So decided to submit this was as it allows it through
+  arr.sort_by{rand}
 end
-temp = shuffle ['1','2','3','4']
-puts temp

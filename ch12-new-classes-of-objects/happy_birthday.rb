367a61311948f10ccb0da4641608af8407fa5464
Jack Oddy <jackoddy91@gmail.com>
2016-06-16 12:26:53 +0100

    Added method to spank user once for each birthday they have had

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,23 @@
-# your code here
+def num_of_bdays birthday
+
+puts "Here's what you got coming.."
+
+a_year = 31536000
+
+  ((Time.now - birthday) / a_year).to_i.times {puts "*SPANK*"}
+
+end
+
+puts "What year were you born in?"
+
+y = gets.chomp
+
+puts "What month were you born in?"
+
+m = gets.chomp
+
+puts "What day were you born on?"
+
+d = gets.chomp
+
+num_of_bdays(Time.gm(y, m, d))

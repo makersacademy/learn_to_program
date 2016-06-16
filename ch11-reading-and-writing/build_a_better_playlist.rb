4b38174b153dc006d6a17d58d738a17c461a8cd6
Jack Oddy <jackoddy91@gmail.com>
2016-06-16 09:41:31 +0100

    adds a shuffle method that splits an array of file names into two arrays and then adds them into a single array at random indexes

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,17 +1,20 @@
 def music_shuffle filenames
   a = filenames.split("\n")
 
-count = 1
-b =  a.inject([]) do |result, element|
-    result << element if count % 2 == 0
-    count = count + 1
-    result
-  end
+  count = 0
 
-puts a
-puts "blank"
-puts b
+  b =  a.inject([]) do |result, element|
+      result << element if count % 2 == 0
+      count = count + 1
+      result
+    end
 
+  c = b.inject([]) {|result, element| result.insert(rand(20), element)}
+
+  a.inject(c) {|result, element| result.insert(rand(20), element)}
+
+
+  c.uniq.compact.join("\n")
 end
 
-music_shuffle "this\nis\na test to\n see if this \n program\nactually\nworks"
+puts music_shuffle "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15"

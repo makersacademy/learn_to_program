903d67fba08f27fb925a0becb5e114250787f550
Jess Astbury <jess.astbury@hotmail.com>
2016-06-16 16:41:21 +0000

    Added one billion seconds exercise

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,8 @@
-# your code here
+age = Time.now - Time.local(1992, 5, 29, 9, 25)
+
+puts "Age in seconds: #{age}"
+
+
+billionseconds = Time.local(1992, 5, 29, 9, 25) + 1000000000
+
+puts "Bilion seconds old at: #{billionseconds}"

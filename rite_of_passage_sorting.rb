92abe2e070b0bc3aaa53ee7f6274559818325d4a
Bart <kluczynskib@gmail.com>
2016-06-16 11:11:55 +0100

    Adding three exercisesfor CP's book

---
+++ rite_of_passage_sorting.rb
@@ -0,0 +1,43 @@
+def sort array
+  rec_sort array, []
+  
+end
+
+
+def rec_sort unsorted, sorted
+  
+  if unsorted.length <= 0
+    return sorted
+  
+  end
+  smallest = unsorted.pop
+  still_unsorted = []
+  
+  unsorted.each do |word|
+
+    if word < smallest 
+      still_unsorted.push smallest
+      smallest = word
+      
+    else
+      still_unsorted.push word
+    end
+  
+  end
+  
+  sorted.push smallest
+  
+  rec_sort still_unsorted, sorted
+
+  end
+
+  puts sort(['can','feel','singing','like','a','can'])
+
+
+
+
+
+
+
+
+

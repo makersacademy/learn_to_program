788d517c49d62fb8a4f10702005ce8fc25f3fe10
Zeeshan <mzishtiaq@yahoo.co.uk>
2016-06-16 18:44:42 +0000

    solve roman_numeral modern

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,15 @@
 def roman_numeral num
-  # your code here
+   
+    roman_numerals = {1000 => "M", 500=> "D", 100=> "C",
+                            50=> "L", 10=> "X", 9=>"IX", 
+                            5=> "V",4=> "IV", 1=> "I"}
+    
+    roman = ''
+    
+    roman_numerals.each do |key, value|
+      (num/key).times{roman << value; num -= key}
+    end
+    
+    roman
+
 end

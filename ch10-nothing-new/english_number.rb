a0871537381ff5ff34134f77d095cea30f21cacb
acsauk <acsauk@gmail.com>
2016-06-16 21:43:13 +0100

    added shuffle.rb and english_number.rb

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,179 @@
 def english_number number
-  # your code here
+  if number < 0 # Ensures no negative numbers
+    retuns 'Please enter a number that isn\'t negative'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+  num_string = '' # String that we will returns
+
+  ones_place = ['one', 'two', 'three',
+                'four', 'five', 'six',
+                'seven', 'eight', 'nine']
+  tens_place = ['ten', 'twenty', 'thirty',
+                'forty', 'fifty', 'sixty',
+                'seventy', 'eighty', 'ninety']
+  teenagers  = ['eleven', 'twelve', 'thirteen',
+                'fourteen', 'fifteen', 'sixteen',
+                'seventeen', 'eighteen', 'nineteen']
+
+  left  = number
+  write = left/1000000000000000
+  left  = left - write*1000000000000000
+
+  if write > 0
+    #RECURSION!
+    quadrillion  = english_number write
+    num_string = num_string + quadrillion + ' quadrillion'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/1000000000000
+  left  = left - write*1000000000000
+
+  if write > 0
+    #RECURSION!
+    trillion  = english_number write
+    num_string = num_string + trillion + ' trillion'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/1000000000
+  left  = left - write*1000000000
+
+  if write > 0
+    #RECURSION!
+    billion  = english_number write
+    num_string = num_string + billion + ' billion'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/1000000
+  left  = left - write*1000000
+
+  if write > 0
+    #RECURSION!
+    million  = english_number write
+    num_string = num_string + million + ' million'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/100000
+  left  = left - write*100000
+
+  if write > 0
+    #RECURSION!
+    hun_thousands  = english_number write
+    num_string = num_string + hun_thousands + ' hundred thousand'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/10000
+  left  = left - write*10000
+
+  if write > 0
+    #RECURSION!
+    ten_thousands  = english_number write
+    num_string = num_string + ten_thousands + ' thousand'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/1000
+  left  = left - write*1000
+
+  if write > 0
+    #RECURSION!
+    thousands  = english_number write
+    num_string = num_string + thousands + ' thousand'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/100
+  left  = left - write*100
+
+  if write > 0
+    #RECURSION!
+    hundreds  = english_number write
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      # so we don't write 'two hundredfifty-one'...
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/10         # How many tens left?
+  left = left - write*10  # Subract off tens
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      # As we cant write 'tenty-two' instead of
+      # 'twelve', we have to make a special exception
+      # for these.
+
+      num_string = num_string + teenagers[left-1]
+      # The "-1" is because teenagers[3] is
+      # 'fourteen', not 'thirteen'.
+      # Since we took care of the digit in the
+      # ones place already, we have nothing left to write.
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+      #  The "-1" is because tens_place[3] is
+      # 'forty', not 'thirty'.
+    end
+
+    if left > 0
+      # so we don't write 'sixtyfour'
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left # How many ones left to write out?
+  left  = 0    # Subtract off those ones.
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+    # The "-1" is because ones_place[3] is
+    # 'four', not 'three'.
+  end
+
+  # Now we just return "num_string"...
+  num_string
 end
+
+puts english_number(  0)
+puts english_number(  9)
+puts english_number( 10)
+puts english_number( 11)
+puts english_number( 17)
+puts english_number( 32)
+puts english_number( 88)
+puts english_number( 99)
+puts english_number(100)
+puts english_number(101)
+puts english_number(234)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,69 @@
 def shuffle arr
-  # your code here
+
+# Version based on Fisher Yates Shuffle - inside-out method
+
+  i = arr.length
+
+  while i > 1
+    i = i - 1
+    random = i*rand
+    arr[random], arr[i] = arr[i], arr[random]
+  end
+
+
+# # Version based on Fisher Yates Shuffle - original method
+#ran_arr = []
+
+#  while arr.length > 0
+#    random = arr[arr.length*rand]
+#    ran_arr.push random
+#    arr.delete_if{|i|i==random}
+#  end
+
+# arr = ran_arr
+# puts arr
+puts arr
 end
+
+shuffle ['Zebra','Lion','Aardvark','Bear','Panda']
+
+
+
+
+=begin
+# Write a shuffle method that takes an array
+# and returns a totally shuffled version
+
+# What method can we use for iterating through items in an array?
+
+# Iteraties through the index of array - value after comma is argument
+ arr.each_index {|x| print x, " and "}
+
+# Iterates through values of array - value after comma is argument
+ arr.each {|x| print x, " and "}
+
+# How do we shuffle anything in ruby? Look at rand and rand_index
+
+puts rand
+
+# Write down the numbers from 1 through N.
+arr = ['Zebra','Lion','Aardvark','Bear','Panda']
+
+# Pick a random number k between one and the number of unstruck numbers remaining (inclusive).
+
+random = arr[arr.length*rand]
+
+# Counting from the low end, strike out the kth number not yet struck out,
+# and write it down at the end of a separate list.
+
+ran_arr = []
+ran_arr.push random
+arr.delete_if{|i|i==random}
+
+# Repeat from step 2 until all the numbers have been struck out.
+
+while arr.length > 0
+end
+
+# The sequence of numbers written down in step 3 is now a random permutation of the original numbers.
+=end

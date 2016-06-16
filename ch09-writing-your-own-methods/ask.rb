aac34593899e8192746363d2022cc627dda42243
Adam <adamjbrowner@mgmail.com>
2016-06-16 21:54:42 +0100

    Completed Ask challenge

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,15 @@
-def ask question
-  # your code here
+def ask(question)
+    while true
+        puts question
+        response  = gets.chomp.downcase
+        if response == "yes"
+            return true
+        end
+        if response == "no"
+            return false
+        end
+        puts "Please respond with a 'yes' or a 'no'"
+    end
 end
+
+ask("Do you like dogs?")

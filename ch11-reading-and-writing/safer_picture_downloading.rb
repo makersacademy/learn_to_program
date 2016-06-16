4bb5da37d36365f61271ee3b4e8dea4be37d1082
Thady Condon <thadycondon@hotmail.com>
2016-06-16 20:55:40 +0100

    Chap 11, first exercise complete, safter picture download. File checks others before completion.

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,30 @@
-# your code here
+Dir.chdir '/Users/thadycondon/Desktop'
+
+pic_names = Dir[ '*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+
+puts
+
+print "Downloading #{pic_names.length} files: "
+pic_number = 1
+
+if File.exist?("#{batch_name}0#{pic_number}.jpg") == true
+  puts 'File already exists, abandon ship!'
+  exit
+end
+
+pic_names.each do |name| print '.'
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+  
+  File.rename name, new_name
+  pic_number = pic_number + 1
+  
+end
+
+puts
+puts 'Done'

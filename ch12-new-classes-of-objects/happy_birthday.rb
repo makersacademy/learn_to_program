ef75ab913c3b4a433416323234a3938e55138b67
prashantmathias <mathiasaudio@gmail.com>
2016-06-16 19:09:43 +0100

    Done with HB exercise

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,19 @@
-# your code here
+
+puts "Enter birth year"
+year = gets.chomp.to_i
+
+puts "Enter birth month (1-12)"
+month = gets.chomp.to_i
+
+puts "Enter birth day"
+day = gets.chomp.to_i
+
+b = Time.local(year, month, day)
+t = Time.new
+
+age = 1
+
+while Time.local(year + age, month, day) <= t
+  puts 'SPANK!'
+  age += 1
+end

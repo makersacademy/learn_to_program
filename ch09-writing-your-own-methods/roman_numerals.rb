6a0ee1632075acceaad366ab066a6485e7f541a2
sus111 <sgraham535@gmail.com>
2016-06-16 16:27:03 +0100

    add roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -33,32 +33,3 @@ romannums = "M" * arr[-4] if arr[-4]
   end
   romannums
 end
-
-puts roman_numeral(4)
-puts roman_numeral(19)
-puts roman_numeral(34)
-puts roman_numeral(94)
-puts roman_numeral(144)
-puts roman_numeral(294)
-puts roman_numeral(442)
-puts roman_numeral(1444)
-puts roman_numeral(2974)
-
-
-
-
-# arr[-2]
-# arr[-3]
-#else
-#romannums += "M" * (num / 1000) 
-#romannums += "C" * (num % 500 / 100) 
-#romannums += "L" * (num % 100 /50) 
-#romannums += "X" * (num % 50 /10) 
-#romannums += "V" * (num % 10 /5) 
-#romannums += "I" * (num % 5 /1) 
-#    
-
-#newnumerals = { 4 => "IV", 9 => "IX", 40 => "XL", 90 => "XC", 400 => "CD", 900 => "CM" }
-#numerals = { 1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M" }
-
-

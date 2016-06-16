214a46b29daab2d26f48819fb57872f3088882d7
liskowsky <karollebiedzinski@gmail.com>
2016-06-16 17:49:39 +0100

    CH 10

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,26 @@
 def sort arr
-  # your code here
+	sorting(arr, [])
+end
+
+def sorting(us_arr, sorted_arr)
+	
+	if us_arr.length == 0
+		return sorted_arr
+	end
+
+	still_unsorted = []
+	smallest = us_arr.pop
+
+	us_arr.each do |x|
+		if x < smallest
+			still_unsorted.push smallest
+			smallest = x
+		else
+			still_unsorted.push x
+		end
+	end
+
+	sorted_arr.push smallest
+	sorting(still_unsorted, sorted_arr)
+
 end

13562231c9fdf02ba3f836c30e1e630226fdd162
liskowsky <karollebiedzinski@gmail.com>
2016-06-17 18:39:29 +0100

    CH 11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
-# your code here
+Playlist = Dir["**/*.{mp3,MP3}"]
+Playlist.shuffle
+
+puts "What's the name of this playlist: "
+file_name = gets.chomp
+
+File.open file_name, 'w' do |n|
+	Playlist.each do |name|
+		n.write name + "\n"
+	end
+end
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,27 @@
-# your code here
+#Dir.chdir "home/Desktop/Pictures"
+
+Pictures_list = Dir["**/*.{jpg,JPG}"]
+
+puts "What's the name of this batch: "
+batch_name = gets.chomp
+
+pic_nr = 1
+
+Pictures_list.each do |name|
+	print "."
+
+	if pic_nr < 10
+		new_name = "#{batch_name}0#{pic_nr}"
+	else
+		new_name = "#{batch_name}#{pic_nr}"
+	end
+
+	if File.exist? (new_name)
+		exit
+	else
+		File.rename(name, new_name)
+	end
+
+	pic_nr += 1
+
+end

3872801b49d5f31965f921b68f475e16dcdf3186
Zeeshan <mzishtiaq@yahoo.co.uk>
2016-06-17 19:31:21 +0000

    add improve shuffle and dictionary_sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
 def dictionary_sort arr
-  # your code here
+    recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+    if unsorted.length == 0
+        sorted
+    else
+        still_unsorted = []
+        small = unsorted.pop
+        
+        unsorted.each do |word|
+            if word.downcase < small.downcase
+                still_unsorted << small
+                small = word
+            else
+                still_unsorted << word
+            end
+        end
+        sorted << small
+        recursive_sort still_unsorted, sorted
+    end
 end
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,23 +1,26 @@
 def shuffle arr
-  recursive_sort arr, []
-end
+  
+  shuf = []
 
-def recursive_sort unsorted, sorted
-    if unsorted.length == 0
-        sorted
-    else
-        still_unsorted = []
-        small = unsorted.pop
-        
-        unsorted.each do |word|
-            if word > small
-                still_unsorted << small
-                small = word
-            else
-                still_unsorted << word
-            end
-        end
-        sorted << small
-        recursive_sort still_unsorted, sorted
+  while arr.length > 0
+  
+  random_index = rand(arr.length)
+  current_index = 0
+  
+  new_array = []
+    arr.each do |item|
+      
+      if current_index == random_index
+        shuf.push item
+      else
+        new_array.push item
+      end
+    
+    current_index = current_index + 1
     end
+  
+    arr = new_array
+  end
+  
+  shuf
 end

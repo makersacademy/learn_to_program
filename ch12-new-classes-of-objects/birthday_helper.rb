7adfbfbecd3fe6b634b4a181cda229c0b76fc41d
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-17 23:39:09 +0100

    ch12 excercises

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,17 @@
-# your code here
+name_birth_dates = {}
+
+File.read('birthdays.txt').each_line do |entry|
+   name = entry.split(',').first
+   date = entry.split(',', 2).last
+   name_birth_dates[name] = date
+end
+
+puts 'Enter name please:'
+name = gets.chomp
+date = name_birth_dates[name]
+
+if date == nil
+  puts "No such name on list."
+else
+  puts "#{name}'s birthday is on #{date[1, 6]}"
+end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,28 @@
 def roman_to_integer roman
-  # your code here
+  digit_value = { "I" => 1,
+                  "V" => 5,
+                  "X" => 10,
+                  "L" => 50,
+                  "C" => 100,
+                  "D" => 500,
+                  "M" => 1000,}
+  total = 0
+  previous = 0
+  index = roman.length - 1
+  while index >= 0
+    x = roman[index].upcase
+    index -= 1
+    value = digit_value[x]
+    if !value
+      puts 'This is not a valid roman numeral!'
+      return
+    elsif value < previous
+      value *= -1
+    else
+      previous = value
+    end
+    total += value
+  end
+
+  total
 end

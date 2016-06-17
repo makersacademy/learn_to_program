40c90314c89fbc220109ec47c20448d4eab2a1ce
Robert Summers <rsummers02@gmail.com>
2016-06-17 22:34:49 +0100

    Shuffle.rb and Sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,26 @@
 def sort arr
-  # your code here
+  my_sort arr, []
 end
+
+def my_sort unsorted, sorted
+  sort_hash = Hash.new
+  if unsorted.length <= 0
+    return sorted
+  end
+  unsorted.each do |word|
+    sort_hash[word.downcase] = word
+    sorted << word.downcase
+  end
+  sorted.sort!
+  sorted.map! do |sortedword|
+    sort_hash[sortedword]
+  end
+  sorted
+end
+
+
+#my_arry = ['can', 'feel', 'singing', 'like', 'a', 'can']
+#puts my_arry.sort
+
+#my_arry2 = []
+#puts my_arry2.sort

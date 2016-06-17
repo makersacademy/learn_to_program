3cd5c4271bb07c8a735676af895b1123a648f683
Tommy Williams <tom@teow.co.uk>
2016-06-17 11:24:40 +0100

    One Billion Seconds V2

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,2 +1,6 @@
-tommy_bd = Time.gm(1988, 2, 19)
-puts tommy_bd.to_i
+# tommy_bd = Time.gm(1988, 2, 19)
+# puts tommy_bd.to_i
+
+
+tommy_billion = Time.gm(1988, 2, 19) + 1_000_000_000
+puts tommy_billion

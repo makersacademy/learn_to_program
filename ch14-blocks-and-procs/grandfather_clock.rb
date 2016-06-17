db5732a56f30a937962fd6615b6869906e255792
Nomi Vos <nvoswork@gmail.com>
2016-06-17 12:30:09 -0500

    solution to grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,19 @@
 def grandfather_clock &block
-  # your code here
+  time = Time.new.hour
+
+    if time > 12
+      time = convert_military_time
+    end
+
+  def convert_military_time
+    time = time - 12
+  end
+
+  time.times do
+    block.call
+  end
+end
+
+grandfather_clock do
+  puts 'DONG!'
 end

941f401af24311613ec85e005fe10a4181bc2f96
albie <albiebabie@gmail.com>
2016-06-17 20:15:43 +0100

    ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes'
+      return true
+    end
+    if reply == 'no'
+      return false
+    end
+# If we got this far, then we're going to loop # and ask the question again.
+    puts 'Please answer "yes" or "no".'
+  end
 end
+  likes_it = ask 'Do you like eating tacos?'
+  puts likes_it

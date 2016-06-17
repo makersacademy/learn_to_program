684e3ebc5e45e96ba501a481fee36b998ea5bd0b
alwinsolanky <alwinsolanky@gmail.com>
2016-06-17 17:26:07 +0100

    ask added again

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -2,8 +2,11 @@ def ask question
   while true
     puts question
     reply = gets.chomp.downcase
+
     return true if reply == 'yes'
+
     return false if reply == 'no'
+    
     puts 'Please answer "yes" or "no".'
   end
 end

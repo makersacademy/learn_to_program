f41d75f22958166294220652c9474d67fe4c36af
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-17 14:33:29 +0000

    Q.9.Ask

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,4 +1,13 @@
 def ask question
   # your code here
-  new
+  question = puts "please answer \"yes\" or \"no\" "
+while true 
+answer = gets.chomp
+if answer == "yes" then return true
+  elsif answer == "no" then return false
 end
+puts question
+end
+
+comp =  "do you use a PC?"
+puts comp

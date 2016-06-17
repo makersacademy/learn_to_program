a431c8f3aff96844be8810a6afcff56b98adcdf0
jluong <jason.qh.luong@gmail.com>
2016-06-17 14:18:54 +0000

    Added sort.rb solution

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,32 @@
-def sort arr
-  # your code here
+#=begin
+#10.2: Rite of Passage: Sorting
+def sort(array)
+    recursive_sort(array, [])
 end
+
+def recursive_sort(list, sorted_list)
+    if list.length <= 0
+        return sorted_list
+    end
+    
+    word = list.pop #take last word in array to use for comparison
+    half_sorted = []
+    
+    #iterate 'list', comparing each element to 'word'. If there is an element 
+    #that is lower than 'word', replace 'word' with value of element
+    list.each do |test_word|
+        if test_word < word
+            half_sorted.push word
+            word = test_word
+        else
+            half_sorted.push test_word
+        end
+    end
+    
+    sorted_list.push(word) #append list with sorted list
+    recursive_sort(half_sorted, sorted_list)
+end
+
+#list_of_words = ['hello','world','bye','go','ask','peel','apple','hello']
+#puts(sort(list_of_words))
+#=end
---
+++ learn2program_ch10.rb
@@ -0,0 +1,114 @@
+=begin
+#10.1: Recursive Psychology Program
+def ask_recursively(question)
+    puts question
+    reply = gets.chomp.downcase
+    
+    if reply == 'yes'
+        true
+    elsif reply == 'no'
+        false
+    else
+        puts "Please answer 'yes' or 'no'."
+        ask_recursively(question)
+    end
+end
+
+ask_recursively("Do you wet the bed?")
+=end
+
+=begin
+#10.1: Recursion with factorials
+def factorial(num)
+    if num  < 0
+        return "Please enter a positive number!"
+    end
+    
+    if num <= 1
+        1
+    else
+        num * factorial(num-1)
+    end
+end
+
+puts factorial(3)
+puts factorial(30)
+=end
+
+=begin
+#10.1: Recursive land
+M = 'land'
+o = 'water'
+
+world = [[o,o,o,o,o,o,o,o,o,o,o],
+         [o,o,o,o,M,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,M,M,o],
+         [o,o,o,M,o,o,o,o,o,M,o],
+         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,o,M,M,M,M,o,o,o],
+         [o,o,o,M,M,M,M,M,M,M,M],
+         [o,o,o,M,M,o,M,M,M,o,o],
+         [o,o,o,o,o,o,M,M,o,o,o],
+         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,o,o,o,o,M,o,o,o,o,o]]
+
+def continent_size(world,x,y)
+    if world[y][x] != 'land'
+        return 0
+    elsif (y > world.length) || (x > world[0].length)
+        return 0
+    elsif (y < 0) || (x < 0)
+        return 0
+    end
+    
+        size = 1
+        world[y][x] = 'counted land'
+        
+        size = size + continent_size(world, x-1, y-1)
+        size = size + continent_size(world, x, y-1)
+        size = size + continent_size(world, x+1, y-1)
+        size = size + continent_size(world, x-1, y)
+        size = size + continent_size(world, x+1, y)
+        size = size + continent_size(world, x-1, y+1)
+        size = size + continent_size(world, x, y+1)
+        size = size + continent_size(world, x+1, y+1)
+        size
+end
+
+puts continent_size(world,5,5)
+=end
+
+#=begin
+#10.2: Rite of Passage: Sorting
+list_of_words = ['hello','world','bye','go','ask','peel','apple','hello']
+
+def sort(array)
+    recursive_sort(array, [])
+end
+
+def recursive_sort(list, sorted_list)
+    if list.length <= 0
+        return sorted_list
+    end
+    
+    word = list.pop #take last word in array to use for comparison
+    half_sorted = []
+    
+    #go through array, comparing each element to 'word'. If there is an element 
+    #that is lower than 'word', replace 'word' with value of element
+    list.each do |test_word|
+        if test_word < word
+            half_sorted.push word
+            word = test_word
+        else
+            half_sorted.push test_word
+        end
+    end
+    
+    sorted_list.push(word) #append list with sorted list
+    recursive_sort(half_sorted, sorted_list)
+end
+
+puts(sort(list_of_words))
+#=end

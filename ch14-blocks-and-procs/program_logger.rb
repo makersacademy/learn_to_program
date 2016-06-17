d2d59b76d726b2ee248a21f9a9a82b3aaffb0651
Nomi Vos <nvoswork@gmail.com>
2016-06-17 15:05:51 -0500

    solution to program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,20 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+
+end
+
+program_log 'animal block' do
+  color = 'red'
+  animal = 'cow'
+
+  program_log 'math block' do
+    1+2
+  end
+
+  program_log 'annoying block' do
+    'Ding! Dong! Ding! Dong!'
+  end
+  color.length == animal.length
 end

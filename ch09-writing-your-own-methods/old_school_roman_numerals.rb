2eb7d0e8a4352c404ec605b065d4606170312aa1
alwinsolanky <alwinsolanky@gmail.com>
2016-06-17 17:27:14 +0100

    adding old school roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,27 @@
 def old_roman_numeral num
-  # your code here
+
+  if num <= 0
+    puts 'Enter a positive whole number above 0 Please'
+  end
+
+  oldrom = ''
+
+  oldrom = oldrom + 'M' * (num / 1000)
+
+  oldrom = oldrom + 'D' * (num % 1000 / 500)
+
+  oldrom = oldrom + 'C' * (num % 500 / 100)
+
+  oldrom = oldrom + 'L' * (num % 100 / 50)
+
+  oldrom = oldrom + 'X' * (num % 50 / 10)
+
+  oldrom = oldrom + 'V' * (num % 10 / 5)
+
+  oldrom = oldrom + 'I' * (num % 5 / 1)
+
+  oldrom
+
 end
+
+ #puts (old_roman_numeral (10))

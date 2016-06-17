06542f23f5cbc294e82727818a31f3e4e46bc397
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-17 20:45:11 +0000

    Q.9.old_school_roman_numerals complete.2

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,5 +1,16 @@
 def old_roman_numeral num
   # your code here
+  roman = " "
+  roman = roman + "M" * (num / 1000)
+  roman = roman + "D" * (num % 1000 / 500)
+  roman = roman + "C" * (num % 500 / 100)
+  roman = roman + "L" * (num % 100 / 50)
+  roman = roman + "X" * (num % 50 / 10)
+  roman = roman + "V" * (num % 10 / 5)
+  roman = roman + "I" * (num % 5 / 1)
+  puts roman
+end
+  # your code here
   # M = 1000
   # D = 500
   # C = 100
@@ -9,13 +20,3 @@ def old_roman_numeral num
   # I = 1
   # single line? roman = ( ) or (( ) or ( ))?
   # why modulo previous num? prevent overlap? ---------- remainder/num e.g 1500/1000= 1M... 500 remains, to get D 1500%1000=500...500/500=1..1D
-  roman = " "
-  roman =roman+ "M" *(num/1000)
-  roman =roman+ "D" *(num%1000/500)
-  roman =roman+ "C" *(num%500/100)
-  roman =roman+ "L" *(num%100/50)
-  roman =roman+ "X" *(num%50/10)
-  roman =roman+ "V" *(num%10/5)
-  roman =roman+ "I" *(num%5/1)
-  roman
-end

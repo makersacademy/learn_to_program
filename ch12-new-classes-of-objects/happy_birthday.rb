86b129876843db91d51f9b90b0adc014840963fb
Sophie Gill <sophie-gill@hotmail.com>
2016-06-17 11:58:37 +0100

    adding Happy Birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,19 @@
 # your code here
+puts "What year were you born?"
+year = gets.chomp.to_i
+puts "What number month were you born?"
+month = gets.chomp.to_i
+puts "What day were you born?"
+day = gets.chomp.to_i
+
+birthday = Time.gm(year, month, day)
+
+age = ((Time.new - birthday)/60/60/24/365).to_i
+
+puts "You are #{age} years old!"
+puts "Here are your birthday spanks! (oo eer!)"
+puts
+
+age.times do
+  puts "SPANK!"
+end

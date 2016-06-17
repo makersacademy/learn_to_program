d9c7a0481550f541dc1e23ec383c9d62d32d73fa
Jose <jcopoviking@outlook.com>
2016-06-17 14:18:24 +0100

    Failed attempt to create the sort fuction

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,8 @@
 def sort arr
-  # your code here
+  sorted_arr = Array.new(arr.count)
+  sorted_arr.insert(0,arr(1))
+   while arr =! [nil]
+
 end
+
+sort ['BP','Texaco','Shell','Morrisons','Tesco','Total','Esso']

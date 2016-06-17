abd5259cdb6677f7bf487463aedc9357fe82912e
richo225 <richo225@gmail.com>
2016-06-17 18:07:40 +0100

    ch10 Submit dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,28 @@
 def dictionary_sort arr
-  # your code here
+  rec_sort arr, [] #calls other method and passes in arr as the unsorted_arr
+                           #passes in [] as the sorted_arr
 end
+
+def rec_sort unsorted, sorted
+  if unsorted.empty?
+    return sorted   #stops recursion if all the strings have been transferred to sorted_arr
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |word|
+    if word.downcase < smallest.downcase #downcase the two compared
+      still_unsorted.push smallest
+      smallest = word
+    else
+      still_unsorted.push word
+    end
+  end
+
+  sorted.push smallest
+
+  rec_sort still_unsorted, sorted
+end
+
+dictionary_sort ["hello", "it", "is", "nice", "A", "to", "meet", "You", "you"]

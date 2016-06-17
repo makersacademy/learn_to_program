5d826845eaf64a1d04b1e30e8b1a63df1f6d5767
Jack Oddy <jackoddy91@gmail.com>
2016-06-17 17:02:49 +0100

    extends program_logger method to add indents for each nested block to log method in better_program_logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,11 @@
-def log desc, &block
-  # your code here
+  $nesting_depth = 0
+
+def better_log desc, &block
+
+indent = " " * $nesting_depth
+  puts indent + "Beginning #{desc.inspect}..."
+  $nesting_depth += 1
+  a = block[]
+  $nesting_depth -= 1
+  puts indent + "...#{desc.inspect} finished, returning: #{a}"
 end

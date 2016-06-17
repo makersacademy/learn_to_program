3d97030d6358fabbb9cd60866b9fd8aa1fab1b65
Jess Astbury <jess.astbury@hotmail.com>
2016-06-17 11:38:54 +0000

    Corrected roman to integer with modern roman numerals

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -15,20 +15,30 @@ integer = 0
 numerals = roman.upcase.split("")
 
 numerals.each do |x|
-if roman_numerals.has_key?(x) == false
+    if roman_numerals.has_key?(x) == false
     puts "This is not a valid Roman Numeral"
     exit
-end
-end
-
-numerals.each do |x|
-    roman_numerals.each do |key, value|
-    integer +=value if key == x
-end
+    end
 end
 
-integer
+
+pnum = 0
+index = roman.length - 1
+
+while index >= 0
+c = roman[index].upcase
+index = index - 1
+val = roman_numerals[c]
+    if val < pnum
+    val = val * -1
+    else
+    pnum = val
+    end
+integer += val
+end
+
+return integer
 
 end
 
-roman_to_integer("MMMDCJ")
+roman_to_integer("ccclxv")

db5a5711da2c05d11226397769c8799a7ae2985a
Thady Condon <thadycondon@hotmail.com>
2016-06-17 20:08:21 +0100

    Chap 11, didn't finish the better playlist, need to finish. Chap 12, completed 3 of 4

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,19 +1,46 @@
-def music_shuffle filenames
-      recursive_shuffle some_array, []
-  end
+def music_shuffle some_array
+    recursive_shuffle some_array, []
+end
 
-  def recursive_shuffle unsorted_array, mixed_array
-      y = -1
-      until sorted_array.count == unsorted_array.count
-          x = rand(unsorted_array.count)
-          if x == y then
-          else
-          y = x
-          mixed_array << unsorted_array[x]
-          end
-          end
-     puts mixed_array
-      end
+def recursive_shuffle unsorted_array, sorted_array
+        x = (0...(unsorted_array.count)).sort_by{rand}
+        x.each{|num| sorted_array << unsorted_array[num]}
+        return sorted_array
+end
 
   sort(['pear','apple', 'banana','orange','blackberry'])
 end
+
+require '/Users/thadycondon/Documents/learn_to_program/ch10-nothing-new/shuffle.rb'
+
+Dir.chdir '/Users/thadycondon/Desktop/Chap11'
+
+tune_names = Dir['*.{m3u,mp3,m4a}']
+tunes = shuffle tune_names
+puts 'What would you like to call this playlist?'
+batch_name = gets.chomp
+
+puts
+
+print "Downloading #{tune_names.length} files: "
+tune_number = 1
+tunes.each do |name| print '.'
+  new_name = if tune_number < 10
+    "#{batch_name}0#{tune_number}.m3u"
+  else
+    "#{batch_name}#{tune_number}.m3u"     
+  end
+    if File.exist?("#{batch_name}#{tune_number}.m3u") == true || File.exist?("#{batch_name}0#{tune_number}.m3u") == true
+      puts 'File already exists, abandon ship!'
+    exit
+  end
+
+  File.rename name, new_name
+  tune_number = tune_number + 1
+  
+end
+
+
+
+puts
+puts 'Done'
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,36 @@
 def roman_to_integer roman
   # your code here
 end
+
+numerical = {"I" => 1, "IV" => 4, "V" => 5, "IX" => 9, "X" => 10, "XL" => 40, "L" => 50, "XC" => 90, "C" => 100, "CD" => 400, "D" => 500, "CM" => 900, "M" => 1000}
+
+roman = "mcmxcix"
+roman = roman.upcase
+roman_split = []
+roman_ready = []
+roman_split = roman.split("")
+
+a = 0
+b = 1
+
+until roman_split[b] == nil
+ if numerical[roman_split[a]] < numerical[roman_split[b]]
+   roman_ready << roman_split[a] + roman_split[b]
+   roman_split.delete_at(b)
+    a += 1
+    b += 1
+  else
+  roman_ready << roman_split[a]
+    a += 1
+    b += 1
+  end
+end  
+
+mod_num = 0
+roman_ready.map(&:to_s)
+
+roman_ready.each do |num|
+  mod_num = mod_num + numerical[num]
+end
+
+puts mod_num

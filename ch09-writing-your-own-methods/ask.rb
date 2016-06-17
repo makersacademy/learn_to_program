7b325d9e80fbcd89807f3a9d84f51c175cf52b41
Sara <sarah.alalaya@gmail.com>
2016-06-18 00:56:09 +0300

    ch09 Ex.

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,28 @@
+#!/Users/sara/.rvm/rubies/ruby-2.3.0/bin/ruby
 def ask question
-  # your code here
+while true
+  puts question
+  reply = gets.chomp.downcase
+  if reply == 'yes'
+    return true
+  elsif reply == 'no'
+    return false
+  else
+    puts 'Please answer "yes" or "no"'
+  end
 end
+end
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed
--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,27 @@
+#!/Users/sara/.rvm/rubies/ruby-2.3.0/bin/ruby
 def old_roman_numeral num
-  # your code here
+thousands = num / 1000
+hundreds = (num % 1000) / 100
+tens = (num % 100) / 10
+singles = num % 10
+new_num =''
+new_hundreds=''
+new_tens=''
+new_singles=''
+
+thousands.times {new_num << 'M'}
+
+hundreds < 5 ? ((hundreds.times {new_hundreds << 'C'}) if hundreds > 0) : ((hundreds-5).times {new_hundreds <<'C'} ; new_hundreds = 'D' << new_hundreds)
+
+tens < 5 ? (tens.times {new_tens << 'X'}) : ((tens-5).times {new_tens <<'X'} ; new_tens = 'L' << tens)
+
+singles < 5 ? (singles.times {new_singles << 'I'}) :  ((singles-5).times {new_singles <<'I'} ; new_singles = 'V' << new_singles)
+
+puts
+
+
+new_num << new_hundreds << new_tens << new_singles
+
 end
+
+puts old_roman_numeral(3641)

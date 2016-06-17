b1bd02af102c528d6bf4990a323f33668a5f75bc
Sophie Gill <sophie-gill@hotmail.com>
2016-06-17 11:51:50 +0100

    billion seconds old answer

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,8 @@
 # your code here
+# My date and time of birth is 11th July 1984, 19:30:30
+
+birth_time = Time.gm(1984,07,11,19,30,30)
+
+billion_secs_old = birth_time + (10**9)
+
+puts billion_secs_old

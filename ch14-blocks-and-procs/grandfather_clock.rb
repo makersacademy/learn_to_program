876361357a5a52ed7a3900281389421f2c139965
prashantmathias <mathiasaudio@gmail.com>
2016-06-17 18:47:10 +0100

    Done with GC exercise

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,14 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour
+
+  if hour >= 13
+    hour -= 12
+  end
+
+  if hour == 0
+    hour = 12
+  end
+
+  hour.times { block.call }
 end
+grandfather_clock { puts 'DONG!' }

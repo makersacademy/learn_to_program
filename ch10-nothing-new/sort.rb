fe9372f14b8e4c7e6337efe18f328333df09dfc9
richo225 <richo225@gmail.com>
2016-06-17 16:01:50 +0100

    ch10 Submit sort.rb & sort_non_recursion.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,15 @@
 def sort arr
-  # your code here
+  recursive_sort arr, [] #calls other method and passes in arr as the unsorted_arr
+                           #passes in [] as the sorted_arr
+end
+
+def recursive_sort unsorted_arr, sorted_arr
+
+  if unsorted_arr.empty?
+    return sorted_arr   #stops recursion if all the strings have been transferred to sorted_arr
+  else
+
+    sorted_arr << unsorted_arr.sort!.slice!(0)#moves smallest string from unsorted to sorted
+    recursive_sort unsorted_arr, sorted_arr   #recursion => repeat recursive_sort method
+  end
 end

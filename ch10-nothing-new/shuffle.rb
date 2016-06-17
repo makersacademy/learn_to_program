705966f13525da74f84d39d7a249bec4a6eba67e
Titus Iudean <iudeant@gmail.com>
2016-06-17 21:54:35 +0300

    Shuffle method done by re-using previously created 'sort' method

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,35 @@
-def shuffle arr
-  # your code here
+#  SHUFFLING ORDERED ARRAY BY RE-USING 
+# PREVIOUSLY CREATED 'sort' METHOD 
+
+#  SIMILARILY COULD HAVE USED THE 
+# RECURSIVE 'sort' METHOD INSTEAD (OR 
+# THE 'sort' METHOD FROM API)
+# [ WELL, OR EVEN THE 'shuffle' method in API ]
+
+def sort(arr)  
+  i =0
+  while i < arr.length 
+    j = i + 1
+    while j < arr.length
+      if arr[i] > arr[j]
+        arr[i]  , arr[j] = arr[j], arr[i] 
+      end
+      j += 1 
+    end
+    i += 1
+ end
+ arr
+end 
+
+def sort(arr)
+  pre_sort(arr, [])
 end
+
+def shuffle(arr)
+  # your code here
+  arr.sort() {rand}
+end
+
+# TEST THIS...
+arr = [1, 2, 3, 4, 5,6, 7]
+puts shuffle(arr)

7ae4178e1d6d2e4bcbeea8a2620c9ea02e0a6fde
Nomi Vos <nvoswork@gmail.com>
2016-06-17 15:27:20 -0500

    solution to better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,31 @@
-def log desc, &block
-  # your code here
+$depth = 0
+
+def better_log desc, &block
+  indent = ' ' * $depth
+
+  puts indent + 'Beginning "' + desc + '"...'
+  $depth += 1
+  result = block.call
+
+  $depth -= 1
+  puts indent + '..."' + desc + '" finished, returning: ' + result.to_s
+
+end
+
+better_log 'animal block' do
+  color = 'red'
+  animal = 'cow'
+
+  better_log 'math block' do
+    better_log 'time block' do
+      time = Time.new
+      time
+    end
+    1+2
+  end
+
+  better_log 'annoying block' do
+    'Ding! Dong! Ding! Dong!'
+  end
+  color.length == animal.length
 end

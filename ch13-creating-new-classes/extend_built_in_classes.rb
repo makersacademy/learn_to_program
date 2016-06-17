b16535545ca73730038a4f85cec9e1c3c58877fe
Bart <kluczynskib@gmail.com>
2016-06-17 17:39:13 +0100

    solution to extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,9 +1,27 @@
-    class Array
-        
-        def shuffle
-            
-            sort_by{rand}
-            
+class Integer
+ def factorial
+        if self <= 1
+          1
+        else
+          self * (self-1).factorial
         end
-        
-    end
+  end
+
+
+  def to_roman
+        number = ''
+        number = number + 'M' * (self / 1000)
+        number = number + 'D' * (self % 1000 / 500)
+        number = number + 'C' * (self % 500 / 100)
+        number = number + 'L' * (self % 100 / 50)
+        number = number + 'X' * (self % 50 / 10)
+        number = number + 'V' * (self % 10 / 5)
+        number = number + 'I' * (self % 5 / 1)
+      end
+end
+
+end
+
+
+puts 7.factorial
+puts 73.to_roman

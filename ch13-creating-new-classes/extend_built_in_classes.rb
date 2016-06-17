e025ef548d2831133711a29f4d2350c1705efdbd
Mali Michael <malimccalla@gmail.com>
2016-06-17 16:54:52 +0100

    Complete extent build in class - Chapter 13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,52 @@
 class Integer
-  # your code here
+
+  def factorial
+    result = 1
+    self.times { |i| result *= i + 1}
+    result
+  end
+
+  def to_roman
+    num = self
+    m = 0
+    d = 0
+    c = 0
+    l = 0
+    x = 0
+    v = 0
+    i = 0
+    if num >= 1000
+      m = num / 1000
+      num = num % 1000
+    end
+    if num >= 500
+      d = num / 500
+      num = num % 500
+    end
+    if num >= 100
+      c = num / 100
+      num = num % 100
+    end
+    if num >= 50
+      l = num / 50
+      num = num % 50
+    end
+    if num >= 10
+      x = num /10
+      num = num %10
+    end
+    if num >= 5
+      v = num / 5
+      num = num % 5
+    end
+    if num < 5
+      i = num / 1
+      #num = num % 10
+    end
+    "M"*m + "D"*d + "C"*c + "L"*l + "X"*x + "V"*v + "I"*i
+  end
+
 end
+
+#puts 5.to_roman
+#puts 5.factorial

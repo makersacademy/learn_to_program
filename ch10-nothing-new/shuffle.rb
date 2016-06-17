c674eabee9ff425de7858d6a56427006051441fc
hannako <jessjones1@gmail.com>
2016-06-17 16:17:16 +0100

    Update to shuffle programme

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,11 @@
-def shuffle arr
-  # your code here
+def shuffle (array)
+  n = array.length
+  shuffled = Array.new(n)
+  shuffled.fill{|x| (array[rand(n)])}
+  short_shuffle = shuffled.uniq!
+  remainders = array - short_shuffle
+  short_shuffle << remainders
+  puts short_shuffle
 end
+
+shuffle([1,2,3,4,5,6,7,8,9])

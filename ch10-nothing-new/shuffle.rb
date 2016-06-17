c102b2a24a572c7247c7dd2d5ac408f609132ef0
Zeeshan <mzishtiaq@yahoo.co.uk>
2016-06-17 18:05:41 +0000

    add shuffle sort

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,23 @@
 def shuffle arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+    if unsorted.length == 0
+        sorted
+    else
+        still_unsorted = []
+        small = unsorted.pop
+        
+        unsorted.each do |word|
+            if word > small
+                still_unsorted << small
+                small = word
+            else
+                still_unsorted << word
+            end
+        end
+        sorted << small
+        recursive_sort still_unsorted, sorted
+    end
 end

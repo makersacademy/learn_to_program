472d7ded7bd051e46560b5adf33218bd5215d4b1
ogirginc <ogirginc@gmail.com>
2016-06-18 21:55:11 +0100

    Completed old_roman

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,9 +1,21 @@
 def old_roman_numeral num
-  num = gets 'Write a num between 1 and 3000'
-
-  if
-    puts 'Num must be 0<num && num<3000'
-  else
+  if num > 0 && num < 3000
+    roman = ''
+    roman << 'M' * (num / 1000) 
+    roman << 'D' * (num % 1000 / 500)  
+    roman << 'C' * (num %  500 / 100)
+    roman << 'L' * (num %  100 /  50)
+    roman << 'X' * (num %   50 /  10)
+    roman << 'V' * (num %   10 /   5)
+    roman << 'I' * (num %    5 /   1)
     
+    return roman
+  else
+    return 'Use a num between 0 and 3000'
   end
 end
+
+puts(old_roman_numeral(3000))
+puts(old_roman_numeral(2000))
+puts(old_roman_numeral(1999))
+puts(old_roman_numeral(5))

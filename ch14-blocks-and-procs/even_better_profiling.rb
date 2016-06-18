377924b4dc8c15779dbf72b369cc6d764a203ef5
Mali Michael <malimccalla@gmail.com>
2016-06-18 11:51:11 +0100

    Update even_better_profiling.rb to output expected string

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -5,7 +5,7 @@ def profile(block_description, &block)
     start_time = Time.new
     block.call
     duration = Time.new - start_time
-    puts "'#{block_description}' took #{duration} seconds to complete"
+    puts "#{block_description}: #{duration} seconds"
   else
     block.call
   end

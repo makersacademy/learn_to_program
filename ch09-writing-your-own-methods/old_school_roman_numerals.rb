f53979ae84baae9a05689d595015591b0c79ee29
6eff <6eff@mail.ru>
2016-06-18 19:09:37 +0300

    old school romans added

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,10 @@
 def old_roman_numeral num
-  # your code here
+letters = %w[ M     D    C    L   X   V  I ]
+values  =   [ 1000, 500, 100, 50, 10, 5, 1 ]
+lv = letters.zip values
+
+p lv
+  c=0
+  lv.map{|l,v| c, num = num.divmod v; l*c}.join ''
+
 end

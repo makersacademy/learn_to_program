50acd4e4d7edd5e71a374fbe176331968eedfcba
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-18 16:10:19 +0100

    Updated old_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -4,28 +4,28 @@ splitArray = num.split(",").map { |s| s.to_i }
 romanArray = Array.new
 
 if splitArray.length == 4
-  romanArray.push ('M' * (splitArray(1)/1000))
+  romanArray.push ('M' * (splitArray(0)/1000))
   splitArray.delete_at(0)
 
 elsif splitArray.length == 3 && splitArray(0) >=5
   romanArray.push ('D')
 
 elsif splitArray.length == 3 && splitArray(0) < 5
-  romanArray.push ('C' * (splitArray(1)/100))
+  romanArray.push ('C' * (splitArray(0)/100))
   splitArray.delete_at(0)
 
 elsif splitArray.length == 2 && splitArray(0) >=5
   romanArray.push ('L')
 
 elsif splitArray.length == 2 && splitArray(0) < 5
-  romanArray.push ('X' * (splitArray(1)/10))
+  romanArray.push ('X' * (splitArray(0)/10))
   splitArray.delete_at(0)
 
 elsif splitArray.length == 1 && splitArray(0) >=5
   romanArray.push ('V')
 
 elsif splitArray.length == 1 && splitArray(0) < 5
-  romanArray.push ('I' * (splitArray(1)/10))
+  romanArray.push ('I' * (splitArray(0)/10))
   splitArray.delete_at(0)
 end
 

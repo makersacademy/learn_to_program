a80404314598edd4c40d7c12146ca989c3185ff2
Mali Michael <malimccalla@gmail.com>
2016-06-18 14:48:08 +0100

    Complete grandfather clock - Chapter 14

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -3,7 +3,7 @@ def profile(block_description, &block)
   profiling_on = true
   if profiling_on
     start_time = Time.new
-    block.call
+    block.call  #block is executed here
     duration = Time.new - start_time
     puts "#{block_description}: #{duration} seconds"
   else

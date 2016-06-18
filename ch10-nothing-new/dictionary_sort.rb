7cbba4322ee5fd05d30971f2d6abb8dfcc07cd1d
6eff <6eff@mail.ru>
2016-06-19 01:11:07 +0300

    dictionary sort added.DOWNCASE

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,14 @@
 def dictionary_sort arr
-  # your code here
+
+  sorted = false
+  arr[0...-1].each.with_index do |x, i|
+    if x.downcase > arr[i+1].downcase
+    arr[i], arr[i+1] = arr[i+1], arr[i]
+    sorted = true
+  end
+  end
+  dictionary_sort(arr) if sorted
+  arr
+
+
 end

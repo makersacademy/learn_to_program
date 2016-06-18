efec1b7e4d3fd8aafad028f9c2979d69dee4e8ec
hannako <jessjones1@gmail.com>
2016-06-18 19:10:28 +0100

    chapter 11 exercises: playlist and picture download

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,28 @@
-# your code here
+#shuffle method:
+
+def shuffle (array)
+  n = array.length
+  shuffled = Array.new(n)
+  shuffled.fill{|x| (array[rand(n)])}
+  short_shuffle = shuffled.uniq!
+  remainders = array - short_shuffle
+  short_shuffle << remainders
+  puts short_shuffle
+end
+
+#playlist:
+
+Dir.chdir '/Users/jessicajones/music'
+my_music = Dir['**/**.m4a']
+my_music.map! do |name|
+  '/Users/jessicajones/music/' + name
+end
+
+shuffle(my_music)
+
+filename = "makers_playlist.m3u"
+File.open filename, 'w' do |f|
+  f.write my_music
+end
+
+puts my_music

7c7184d0c826fa7335c69a2e9e7f4504e9acf573
Mali Michael <malimccalla@gmail.com>
2016-06-18 11:49:18 +0100

    Complete even_better_programming - Chapter 14

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,27 @@
-def profile block_description, &block
-  # your code here
+def profile(block_description, &block)
+  # &block is being turned into a proc
+  profiling_on = true
+  if profiling_on
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "'#{block_description}' took #{duration} seconds to complete"
+  else
+    block.call
+  end
+end
+
+profile('25000 doublings') do #from here to end is &block
+  number = 1
+  25000.times do
+    number += number
+  end
+  puts "#{number.to_s.length} digits"
+end
+
+profile('count to a million') do
+  number = 0
+  1000000.times do
+    number += 1
+  end
 end

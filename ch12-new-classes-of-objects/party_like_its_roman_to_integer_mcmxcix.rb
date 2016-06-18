704bc1a4dbf86a34fa13bec587cfdc61b8c4d656
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-18 20:50:58 +0100

    roman numeral to integer task update

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,22 @@
 def roman_to_integer roman
-  # your code here
+values = {
+  "M"=> 1000,
+  "D"=>500,
+  "C"=>100,
+  "L"=>50,
+  "X"=>10,
+  "V"=>5,
+  "I"=>1
+}
+#if roman.include?"m" ||roman.include?"d" || roman.include?"c" || roman.include?"l" || roman.include?"x" || roman.include?"v"|| roman.include?"i" == false puts "sum romana! soli numeri romani conosco"
+arr = roman.upcase.chars
+num = []
+
+arr.each do |char|
+  num << values[char]
 end
+num.inject { |memo, n| memo +n}
+end
+
+puts(roman_to_integer("mcmxcix"))
+puts(roman_to_integer("CCCLXV"))

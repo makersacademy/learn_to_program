2aa9cacef9174e78741fe3cece51143b26961fae
Jose <jcopoviking@outlook.com>
2016-06-18 17:14:12 +0100

    99 bottles of beer

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,10 @@
-# your code here
+bottles_on_wall = 99
+
+ while bottles_on_wall > 0
+ 	puts bottles_on_wall.to_s + ' bottles of beer on the wall,' + bottles_on_wall.to_s + ' bottles of beer.'
+ 	bottles_on_wall = bottles_on_wall -1
+ 	puts  'Take one down and pass it around,' + bottles_on_wall.to_s + ' bottles of beer on the wall.'
+ end
+
+ puts 'No more bottles of beer on the wall, no more bottles of beer.'
+ puts 'Go down to the store, and buy some more, 99 bottles of beer on the wall'

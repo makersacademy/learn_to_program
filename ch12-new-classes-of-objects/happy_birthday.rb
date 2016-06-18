9d4dbd87e4792feda4b3ba24f0574107680cfd91
richo225 <richo225@gmail.com>
2016-06-18 19:47:30 +0100

    ch12 Submit happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,21 @@
-# your code here
+
+require 'date'
+
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "What month were you born in?"
+month_input = gets.chomp
+month = Date::MONTHNAMES.index(month_input)
+
+puts "What day were you born on?"
+day = gets.chomp.to_i
+
+birth_date = Time.local(year, month, day)
+current_date = Time.new
+
+age = current_date - birth_date
+
+years = age / 31536000
+
+puts "SPANK!\n" * years

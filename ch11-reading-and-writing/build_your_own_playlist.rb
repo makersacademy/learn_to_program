179a64dcac08b743f0a8dcf6aecd235ab2e2231c
nikklein <objcoding@gmail.com>
2016-06-18 21:36:58 +0300

    Own playlist builder

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,9 @@
-# your code here
+play_list_dir = Dir['/Users/NNS/Downloads/SomeMusic/*.{mp3,m4a,MP3}'].shuffle
+
+File.open 'playlist_new.m3u', 'w' do |f| 
+play_list_dir.each {|mp| 
+f.write mp + "\n"}
+end
+
+puts "Created!"
+

3ed47576a0673d99f4e0d75377346c78c9cd03f8
Sophie Gill <sophie-gill@hotmail.com>
2016-06-18 16:47:24 +0100

    adding orange tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -9,6 +9,58 @@
 class OrangeTree
   # your code here
 
-  
+  def initialize
+    @age = 0
+    @height = 0
+    @alive = true
+    @oranges = 0
+  end
 
+  def count_the_oranges
+    if @alive
+      @oranges.to_i
+    else
+      return "A dead tree has no oranges. :("
+    end
+  end
+
+  def height
+    if @alive
+      @height
+    else
+      return "A dead tree is not very tall. :("
+    end
+  end
+
+  def pick_an_orange
+    if @alive
+      @oranges -= 5
+    else
+      return "A dead tree has nothing to pick. :("
+    end
+  end
+
+  def one_year_passes
+    if @alive
+      if @age < 25
+        @age += 1
+        @height = (@height + 0.4).round(2)
+        orange_production
+        return "This year your tree grew to #{@height}m tall, and produced #{@oranges.to_i} oranges."
+      elsif @age == 25
+        @alive = false
+        return "Oh, no! The tree is too old, and has died. :("
+      end
+    else
+      return "A year later, the tree is still dead. :("
+    end
+  end
+
+  def orange_production
+    if @alive == true
+      if @age > 5
+        @oranges = @height * 15 - 25
+      end
+    end
+  end
 end

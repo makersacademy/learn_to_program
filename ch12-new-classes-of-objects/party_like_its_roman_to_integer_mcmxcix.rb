009322586143088f581846eadf7c7772a85e710b
liskowsky <karollebiedzinski@gmail.com>
2016-06-18 13:58:57 +0100

    CH 12_v2

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,34 @@
-def roman_to_integer roman
-  # your code here
+def roman_to_integer(roman)
+  values = { "M" => 1000,
+  	"D" => 500,
+  	"C" => 100,
+  	"L" => 50,
+  	"X" => 10,
+  	"V" => 5,
+  	"I" => 1 }
+
+  	index = roman.length - 1
+  	total = 0
+  	prev = 0
+
+  	while index >= 0
+  		x = roman[index].capitalize
+
+  		if values[x]
+  			if values[x] < prev
+  				total = total - 1
+  			else
+  				total = total + values[x]
+  			end
+  		else
+  			return "Not a valid roman nr"
+  		end
+  		index -= 1
+  		prev = values[x]
+  	end
+  	return total
 end
+
+puts roman_to_integer("MCLIX")
+puts roman_to_integer("MCLXI")
+puts roman_to_integer("MCLXIX")

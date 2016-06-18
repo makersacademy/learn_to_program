de25e61e9ca4a930dea0eddf6b69a0f6c182c87d
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-18 16:01:34 +0100

    Updated old_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -3,34 +3,28 @@ def old_roman_numeral num
 splitArray = num.split(",").map { |s| s.to_i }
 romanArray = Array.new
 
-when splitArray.length == 4
+if splitArray.length == 4
   romanArray.push ('M' * (splitArray(1)/1000))
   splitArray.delete_at(0)
-end
 
-when splitArray.length == 3 && splitArray(0) >=5
+elsif splitArray.length == 3 && splitArray(0) >=5
   romanArray.push ('D')
-end
 
-when splitArray.length == 3 && splitArray(0) < 5
+elsif splitArray.length == 3 && splitArray(0) < 5
   romanArray.push ('C' * (splitArray(1)/100))
   splitArray.delete_at(0)
-end
 
-when splitArray.length == 2 && splitArray(0) >=5
+elsif splitArray.length == 2 && splitArray(0) >=5
   romanArray.push ('L')
-end
 
-when splitArray.length == 2 && splitArray(0) < 5
+elsif splitArray.length == 2 && splitArray(0) < 5
   romanArray.push ('X' * (splitArray(1)/10))
   splitArray.delete_at(0)
-end
 
-when splitArray.length == 1 && splitArray(0) >=5
+elsif splitArray.length == 1 && splitArray(0) >=5
   romanArray.push ('V')
-end
 
-when splitArray.length == 1 && splitArray(0) < 5
+elsif splitArray.length == 1 && splitArray(0) < 5
   romanArray.push ('I' * (splitArray(1)/10))
   splitArray.delete_at(0)
 end

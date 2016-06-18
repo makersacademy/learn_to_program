c87550a50ba8793071234e97b852d3f53ab05e2b
cyberplanner <saleh.gfx@gmail.com>
2016-06-18 01:36:24 +0100

    added ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,26 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == "yes" || reply == "no"
+      if reply == "yes"
+        return true
+      elsif reply == "no"
+        return false
+      end
+        break
+      else
+        puts "Please, answer with a 'yes' or a 'no'! "
+    end
+  end
+
 end
+
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+wets_bed = ask "Do you wet the bed?"
+ask 'Do you like eating burritos?'
+ask 'Do you like eating tacos?'
+
+puts wets_bed

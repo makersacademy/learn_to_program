178c9778221916d45ac75d725960f729c82f9f5f
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-19 23:14:55 +0200

    ch14 exercises

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
+$nesting_depth = 0
 def log desc, &block
-  # your code here
+  indent = " "*$nesting_depth
+  puts indent + "Beginning \"#{desc}\"..."
+  $nesting_depth += 1
+  puts indent + "...\"#{desc}\" finished, returning: #{block.call}"
+  $nesting_depth -= 1
+end
+
+log "outer block" do
+  log "some little block" do
+    log "teeny-tiny block" do
+    "lots of love"
+    end
+  6*7
+  end
+  log "yet another block" do
+    "I love Indian food!"
+  end
+  1 == 1
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,14 @@
 def log desc, &block
-  # your code here
+  puts "Beginning #{desc}..."
+  puts "...#{desc} finished, returning: #{block.call}"
+end
+
+log "outer block" do
+  log "some little block" do
+    2 + 3
+  end
+  log "yet another block" do
+    "I like Thai food!"
+  end
+  1 == 2
 end

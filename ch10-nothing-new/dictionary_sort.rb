27b3e397fbe13a4dce6b75273baf97f4f673abd7
Titus Iudean <iudeant@gmail.com>
2016-06-19 03:36:14 +0300

    Sort method altered to sort items dictionary-style

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
+def pre_sort(unsorted, sorted)  
+  
+  return sorted if unsorted.length <= 0
+  
+  parked_arr = []
+  parked = unsorted.pop
+
+  unsorted.each do |item| 
+    if item.downcase < parked.downcase
+      parked_arr.push parked
+      parked = item
+    else
+      parked_arr.push item
+    end 
+  end
+
+  sorted << parked
+  pre_sort(parked_arr, sorted)
+end 
+
 def dictionary_sort arr
   # your code here
+  pre_sort(arr, [])
 end
+
+arr = ["titus", "Paul", "andreea", "lizzy"]
+puts dictionary_sort(arr)

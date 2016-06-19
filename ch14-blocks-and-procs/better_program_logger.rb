a9b859be1ad9c2501f1b8728b33e30f9c3158fb7
Terry <terry@terrytilley.com>
2016-06-19 19:41:23 +0100

    Complete better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
-def log desc, &block
-  # your code here
+$logger_depth = 0
+
+def better_log(desc, &block)
+  prefix = ' '*$logger_depth
+  puts "#{prefix}Beginning \"#{desc}\"..."
+  $logger_depth += 1
+  result = block.call
+  $logger_depth -= 1
+  puts "#{prefix}...\"#{desc}\" finished, returning: #{result}"
+end
+
+better_log("outer block") do
+  better_log("some little block") do
+    1**1 + 2**2
+  end
+
+  better_log("yet another block") do
+    "!doof iahT ekil I".reverse
+  end
+
+  '0' == 0
 end

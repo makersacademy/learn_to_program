cf52023d65017430725c6064e16af7ed84c616da
liskowsky <karollebiedzinski@gmail.com>
2016-06-19 16:40:11 +0100

    CH 09_v6

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,34 +1,32 @@
-def roman(num)
+def old_roman_numeral (num)
 	roman_nr = ''
 
-	how_meny = num / 1000 #how meny thousends are in num
-	roman_nr = roman_nr + ("M" * how_meny) #gives "M" for every thousend
-	num = num - how_meny * 1000 #substracts thousends
+	how_many = num / 1000 #how many thousends are in num
+	roman_nr = roman_nr + ("M" * how_many) #gives "M" for every thousend
+	num = num - how_many * 1000 #substracts thousends
 
-	how_meny = num / 500 
-	roman_nr = roman_nr + ("D" * how_meny) 
-	num = num - how_meny * 500 
+	how_many = num / 500 
+	roman_nr = roman_nr + ("D" * how_many) 
+	num = num - how_many * 500 
 
-	how_meny = num / 100 
-	roman_nr = roman_nr + ("C" * how_meny) 
-	num = num - how_meny * 100 
+	how_many = num / 100 
+	roman_nr = roman_nr + ("C" * how_many) 
+	num = num - how_many * 100 
 
-	how_meny = num / 50 
-	roman_nr = roman_nr + ("L" * how_meny) 
-	num = num - how_meny * 50
+	how_many = num / 50 
+	roman_nr = roman_nr + ("L" * how_many) 
+	num = num - how_many * 50
 
-	how_meny = num / 10 
-	roman_nr = roman_nr + ("X" * how_meny) 
-	num = num - how_meny * 10 
+	how_many = num / 10 
+	roman_nr = roman_nr + ("X" * how_many) 
+	num = num - how_many * 10 
 
-	how_meny = num / 5
-	roman_nr = roman_nr + ("V" * how_meny) 
-	num = num - how_meny * 5
+	how_many = num / 5
+	roman_nr = roman_nr + ("V" * how_many) 
+	num = num - how_many * 5
 
-	how_meny = num / 1
-	roman_nr = roman_nr + ("I" * how_meny) 
+	how_many = num / 1
+	roman_nr = roman_nr + ("I" * how_many) 
 
-	return roman_nr
+	roman_nr
 end
-
-puts roman(5673)
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,22 +1,22 @@
-def roman(num)
+def roman_numeral (num)
   roman_nr = ''
 
-  how_meny = num / 1000 #how meny thousends are in num
-  roman_nr = roman_nr + ("M" * how_meny) #gives "M" for every thousend
-  num = num - how_meny * 1000 #substracts thousends
+  how_many = num / 1000 #how meny thousends are in num
+  roman_nr = roman_nr + ("M" * how_many) #gives "M" for every thousend
+  num = num - how_many * 1000 #substracts thousends
 
   if num / 900 == 9
     roman_nr = roman_nr + "CM"
   elsif num / 400 == 4
     roman_nr = roman_nr + "CD"
   else
-    how_meny = num / 500 
-    roman_nr = roman_nr + ("D" * how_meny) 
-    num = num - how_meny * 500 
+    how_many = num / 500 
+    roman_nr = roman_nr + ("D" * how_many) 
+    num = num - how_many * 500 
 
-    how_meny = num / 100 
-    roman_nr = roman_nr + ("C" * how_meny) 
-    num = num - how_meny * 100 
+    how_many = num / 100 
+    roman_nr = roman_nr + ("C" * how_many) 
+    num = num - how_many * 100 
   end
 
   if num / 90 == 9
@@ -24,13 +24,13 @@ def roman(num)
   elsif num / 40 == 4
     roman_nr = roman_nr + "XL"
   else
-    how_meny = num / 50 
-    roman_nr = roman_nr + ("L" * how_meny) 
-    num = num - how_meny * 50 
+    how_many = num / 50 
+    roman_nr = roman_nr + ("L" * how_many) 
+    num = num - how_many * 50 
 
-    how_meny = num / 10 
-    roman_nr = roman_nr + ("X" * how_meny) 
-    num = num - how_meny * 10 
+    how_many = num / 10 
+    roman_nr = roman_nr + ("X" * how_many) 
+    num = num - how_many * 10 
   end
 
   if num == 9
@@ -38,15 +38,12 @@ def roman(num)
   elsif num == 4
     roman_nr = roman_nr + "IV"
   else
-    how_meny = num / 5
-    roman_nr = roman_nr + ("V" * how_meny) 
-    num = num - how_meny * 5
+    how_many = num / 5
+    roman_nr = roman_nr + ("V" * how_many) 
+    num = num - how_many * 5
 
-    how_meny = num / 1
-    roman_nr = roman_nr + ("I" * how_meny) 
+    how_many = num / 1
+    roman_nr = roman_nr + ("I" * how_many) 
   end 
-  return roman_nr
+  roman_nr
 end
-
-puts roman(5699)
-puts roman(1404)

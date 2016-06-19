2bda7e24f8311ef32e7ec3af312be10a507942ca
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-19 13:44:03 +0000

    Q.10.dictionary_sort.attempt.Smallest_Word?

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,18 @@
-def dictionary_sort arr
+def dictionary_sort (arr)
   # your code here
+  sorting_array (arr,[])
 end
+
+def sorting_array (sorted,unsorted)
+    if unsorted.length = 0
+        return sorted
+    end
+    
+    smallest_word = 
+    
+    unsorted.each do |x|
+        if x.downcase < 
+    
+    
+end
+

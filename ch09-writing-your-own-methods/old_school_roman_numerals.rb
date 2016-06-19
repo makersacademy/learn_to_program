d21013a22f41e2997ef785eae038aa5b09b92539
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-19 19:47:33 +0100

    Made old school roman numerals more readable (for me :) )

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -4,7 +4,9 @@ Old_Roman_Numerals = {'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50,
 def old_roman_numeral num
 	old_roman = ''
 	Old_Roman_Numerals.each do |k,v|
-		(num / v).times {old_roman += k; num = num - v}
+		(num / v).times do
+			old_roman += k; num = num - v
+		end
 	end
 	return old_roman
 end

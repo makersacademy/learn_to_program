72efd1aa9a1ddb18209d42673d41edf2e4a06f48
ogirginc <ogirginc@gmail.com>
2016-06-19 17:16:09 +0100

    Completed sort.rb challenge

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,26 @@
-def sort arr
-  # your code here
+def sort(arr)
+  sorter(arr, []) 
 end
+
+def sorter(unsorted, sorted)
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |tested_obj|
+    if tested_obj < smallest
+      still_unsorted.push smallest
+      smallest = tested_obj
+    else
+      still_unsorted.push tested_obj
+    end
+  end
+
+  sorted.push smallest
+  sorter(still_unsorted, sorted)
+end
+
+puts(sort(['can', 'feel', 'singing', 'a', 'can']))

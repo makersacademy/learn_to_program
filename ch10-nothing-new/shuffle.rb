38d88acc5e26caeef3be84cfce1a104e10cadda1
cyberplanner <saleh.gfx@gmail.com>
2016-06-19 19:51:47 +0100

    Added shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,13 @@
-def shuffle arr
-  # your code here
+
+def shuffle unshuffled
+    shuffled = unshuffled.sort_by{rand}
 end
+
+#or
+=begin
+
+
+def rec_shuffle unshuffled
+    shuffled = unshuffled.shuffle
+end
+=end

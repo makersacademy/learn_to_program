b3bf41f0ae0d4316cb0c6984e04a7feabc99a191
nikklein <objcoding@gmail.com>
2016-06-19 16:23:53 +0300

    Happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,10 @@
-# your code here
+puts "Input your year of birth:"
+b_year = gets.chomp
+puts "Input your month of birth:"
+b_month = gets.chomp
+puts "Input your day of birth:"
+b_day = gets.chomp
+puts
+puts "Thanks"
+puts
+puts ((Time.now - Time.local(b_year, b_month, b_day))/31536000).to_i.times {puts "SPANK!"}

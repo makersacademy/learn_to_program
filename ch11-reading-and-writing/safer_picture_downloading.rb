353450aa368a147f6d82b78d26b5c64043d146ed
6eff <6eff@mail.ru>
2016-06-19 13:29:21 +0300

    safer file renaming added

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,27 @@
-# your code here
+pic_names = Dir['./*.jpg']
+puts 'What would you like to call this batch?'
+batch_name = gets.chomp
+puts
+print "Downloading #{pic_names.length} files:  "
+pic_number = 1
+pic_names.each do |name|
+print '.'
+new_name = if pic_number < 10
+    if File.exist?("#{batch_name}0#{pic_number}.jpg")
+      puts "#{name} there is already a file with that name, please rename"
+      exit
+    else
+      "#{batch_name}0#{pic_number}.jpg"
+    end
+  else
+    if File.exist?("#{batch_name}#{pic_number}.jpg")
+        puts "#{name} there is already a file with that name, please rename"
+        exit
+    else
+      "#{batch_name}#{pic_number}.jpg"
+    end
+  end
+
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end

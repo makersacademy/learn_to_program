da666217a7f64eac82b831ea0d86f598635f4f56
Mali Michael <malimccalla@gmail.com>
2016-06-19 09:57:32 +0100

    Complete birthday helper -Chapter 12

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,20 @@
-# your code here
+require 'yaml'
+Dir.chdir '/Users/Mali/Desktop' #will only work if file is on my Desktop
+
+birthdays = {}
+
+File.read('birthdays.txt').each_line do |line|
+  name, date, year = line.split(',')
+  birthdays[name] = date + year
+end
+
+#still needs to be extented to calcualate age and next birthday.
+
+puts "Whose birthday would you like?"
+answer = gets.chomp
+
+if answer == nil
+  puts "Sorry i dont know that name"
+else
+  puts "#{answer}'s birthday is on #{birthdays[answer]}"
+end

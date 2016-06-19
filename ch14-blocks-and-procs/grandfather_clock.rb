874631225e0497f1c064365437c2a1f85688cac2
Bart <kluczynskib@gmail.com>
2016-06-19 23:49:03 +0100

    Solution to grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,14 @@
-def grandfather_clock &block
-  # your code here
+
+def grand_clock &some_block
+    
+    twelve_hours = Time.new.strftime("%I")
+    twelve_hours.to_i.times do
+        some_block.call
+    end
 end
+
+grand_clock do
+    puts 'DONG!'
+    
+end
+

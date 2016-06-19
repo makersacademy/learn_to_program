9dce79201575de0015806eeed66e160017ab2e97
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-19 18:42:08 +0000

    Q.12.happy_birthday. complete

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,31 @@
 # your code here
+#.local / .gm?
+#time from birth till now
+puts "Your year of birth?"
+birthday_year = gets.chomp.to_i
+
+puts "Your month of birth?"
+birthday_month = gets.chomp.to_i
+
+puts "Your date of birth?"
+birthday_date = gets.chomp.to_i
+
+birthday = Time.local(birthday_year, birthday_month, birthday_date)
+time = Time.new
+
+def birthday_boy(age_min, age_max)
+age_min = 0
+age_max = 22
+while a <= age_max
+a = 0
+a = + 1
+end 
+end
+
+age = 0
+
+while Time.local(birthday_year, birthday_month, birthday_date) <= time
+puts "BIRTHDAY TIME"
+age = age + 1
+end
+

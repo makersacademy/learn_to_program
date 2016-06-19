d926fbe488a23647f26cbc5aba4fbc4b507d0b94
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-19 17:13:37 +0100

    Roman numerals fix

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,7 +1,7 @@
-def roman_numeral num
+def solution (num)
   thousand = (num / 1000)
   hundred = (num % 1000 / 100)
-  ten = (num % 1000 / 10)
+  ten = (num % 100 / 10)
   one = (num % 10)
 
   romNum = "M" * thousand
@@ -17,7 +17,7 @@ end
 
 if ten == 9
   romNum = romNum + "XC"
-elsif hundred == 4
+elsif ten == 4
   romNum = romNum + "XL"
 else
 romNum = romNum + 'L' * (num % 100 / 50)
@@ -26,11 +26,13 @@ end
 
 if one == 9
   romNum = romNum + "IX"
-elsif hundred == 4
+elsif one == 4
   romNum = romNum + "IV"
 else
-romNum = romNum + 'V' * (num % 1000 / 500)
-romNum = romNum + 'I' * (num % 500 / 100)
+romNum = romNum + 'V' * (num % 10 / 5)
+romNum = romNum + 'I' * (num % 5 / 1)
 end
+
+return romNum
 
 end

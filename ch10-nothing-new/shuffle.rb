a71373e9fcc12f6cd4f646e4ab5e13f56a9cfbb5
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-19 19:56:06 +0000

    Q.12.all possible completes

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,35 @@
-def shuffle arr
+def shuffle (arr)
   # your code here
+  shuffled_array []
+  
+  #---random objects from array---
+  #class Array
+  #def random_element
+  # self[rand(length)]
+  #end
+  #end
+  #
+  #or
+  #
+  #rand - random generator  
+  while arr.length > 0
+  rand_arr_object = rand(arr.length)
+  
+  selected = 0
+  new_array = []
+  
+  arr.each {|x|
+  
+  if selected == rand_arr_object
+      return shuf.push(item)
+  else
+      return new_array.push(item)
+  end
+  selected = selected + 1
+      
+  }
+  
+  arr= new_array
+end
+shuffle
 end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,38 @@
-def roman_to_integer roman
+def roman_to_integer (roman)
   # your code here
+  # roman values: M = 1000
+  #               D = 500
+  #               C = 100
+  #               L = 50
+  #               X = 10
+  #               V = 5
+  #               I = 1
+  #wants integer 1999 in numerals
+  #hash to assign values to characters
+  #
+  roman.downcase
+  roman_values ={'i' =>1,'v' => 5,'x' => 10,'l' =>50,'c' =>100,'d' =>500,'m' =>1000}
+  
+  total = 0
+  prev = 0
+  index = roman.length - 1
+  while index >= 0
+    c = roman[index].downcase
+    index = index - 1
+    val = digit_vals[c]
+    if !val
+      puts 'Non-valid character present'
+      return
+    end
+
+    if val < prev
+      val = val * -1
+    else
+      prev = val
+    end
+    total = total + val
+  end
+
+  total
 end
+  

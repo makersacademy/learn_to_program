4b4381caea8674d9269ba71edcb1b29bc577cff2
Jess Astbury <jess.astbury@hotmail.com>
2016-06-19 16:34:02 +0000

    Build your own playlist added

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,16 @@
-# your code here
+def shuffle arr
+    arr.sort_by{rand}
+end
+
+music_files = shuffle(Dir['*.mp3'])
+
+File.open 'playlist.m3u', 'w' do |f|
+    music_files.each do |file|
+        f.write file"\n"
+    end
+end
+
+puts "Playlist created!"
+        
+        
+

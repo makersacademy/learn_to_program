d219b1e792ddb904a8d47baf691bedb0375b37cf
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-19 13:56:04 +0000

    Q.10.99bottlesofbeer.complete.2

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -2,12 +2,14 @@
 starting_num = 9999
 current_num = starting_num
 #loop required
-while current_num > 1
+while current_num > 2
 puts current_num.to_s + 'bottles of ber on the wall,' + current_num.to_s + 'bottles of beer!'
 current_num = current_num - 1
 puts 'take one down, pass it around,' + current_num + ' bottles of beer on the wall!'
-until current_num == 1
-puts '1 bottle of beer on the wall, 1 bottle of beer!'
+until current_num == 2
+puts "Two bottles of beer on the wall, two bottles of beer!"
+puts "Take one down, pass it around, one bottle of beer on the wall!"
+puts 'One bottle of beer on the wall, 1 bottle of beer!'
 puts 'take one down, pass it around, no more bottles of beer on the wall!'
 end
 end

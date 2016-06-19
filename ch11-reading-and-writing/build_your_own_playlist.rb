4e4526682bf57002c3b8763de5202e6bde757d41
JulieLev <jkgee1@yahoo.co.uk>
2016-06-19 12:08:45 +0000

    Adding build_your_own_playlist.rb. Works to create playlist but playback problems with file names.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,34 @@
-# your code here
+require 'yaml'
+
+def yaml_save object, filename
+    File.open filename, 'w' do |f|
+    f.write(object.to_yaml)
+    end
+end
+
+def yaml_load filename
+    yaml_string = File.read filename
+    YAML::load yaml_string
+end
+
+# Find music files to add to the playlist.
+#pic_names = Dir['F:/**/*.jpg']
+mp3tracks = Dir['C:/Users/Lenovo/Music/**/*.mp3']
+
+#Go to directory in which to place the playlist
+Dir.chdir 'C:/Users/Lenovo/Music'
+
+puts 'What would you like to call this playlist?'
+playlist = gets.chomp
+playlist = playlist + ".m3u"
+puts
+print "Creating playlist of all #{mp3tracks.length} music files: "
+
+# We save it...
+yaml_save mp3tracks, playlist
+
+# We load it...
+read_playlist = yaml_load playlist
+
+# We check it matches...
+puts(read_playlist == mp3tracks)

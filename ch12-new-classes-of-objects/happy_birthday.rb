81814e3614b22e184ea6e65b616d8d93b4695c7a
Ebere <ebere10@hotmail.com>
2016-06-19 19:52:14 +0000

    Birthday spanks calculated

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp
+puts "What month were you born? Please enter a number"
+month = gets.chomp
+puts "What day were you born?"
+day = gets.chomp
+
+birthday = Time.local(year, month, day)
+
+age = ((Time.new - birthday)/60/60/24/365).to_i
+
+puts "You are #{age} years old"
+
+puts "SPANK!" * age
+
+

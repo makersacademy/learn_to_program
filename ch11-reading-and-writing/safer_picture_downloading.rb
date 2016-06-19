61d974a4bd1a67b9001f9f6a13f82ec6ba2aff18
Luke <luke.webster86@gmail.com>
2016-06-19 19:00:28 +0000

    Safer picture downloading and playlists

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,31 @@
-# your code here
+Dir.chdir "/home/ubuntu/workspace/sample_files"
+puts Dir.pwd
+pic_names = Dir["*.jpg"]
+
+puts "What would you like to call the current batch?"
+batch_name = gets.chomp
+
+puts 
+print "Downloading #{pic_names.length} files: "
+pic_number = 1 
+
+pic_names.each do |name|
+	print ".."
+
+	new_name = if pic_number < 10 
+	"#{batch_name}0#{pic_number}.jpg"
+	else "#{batch_name}#{pic_number}.jpg"
+	end
+	
+	while File.exist?(new_name)
+	puts "Error, file #{new_name} already exists, please choose an alternative name:"
+	new_name = gets.chomp 	
+
+	end
+	
+	File.rename name, new_name
+	pic_number += 1
+end
+
+puts
+puts "Operation completed successfully!"

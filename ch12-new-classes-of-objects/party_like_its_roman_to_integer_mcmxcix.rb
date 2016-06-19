ab02501e932f5c8205edecb428684962c24bd5ac
Robert Summers <rsummers02@gmail.com>
2016-06-19 16:49:17 +0100

    Party Roman to Integer

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,27 @@
+#mcmxcix
 def roman_to_integer roman
-  # your code here
+  roman_values = {'i' => 1,
+                  'v' => 5,
+                  'x' => 10,
+                  'l' => 50,
+                  'c' => 100,
+                  'd' => 500,
+                  'm' => 1000}
+  num = 0
+  last = 0
+  roman.reverse.downcase.each_char do |r|
+    if r == nil
+      puts "Not a valid roman numeral"
+      return
+    end
+    digit = roman_values[r]
+    if digit < last
+      digit *= -1
+    end
+    num += digit
+    last = digit
+  end
+  num
 end
+
+puts roman_to_integer('MCMXCIX')

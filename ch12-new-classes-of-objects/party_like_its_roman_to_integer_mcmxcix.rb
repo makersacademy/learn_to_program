826fc3cc19b3c2d778d17c1b9081aad2c9203f51
Arunas Skirius <arukomp@gmail.com>
2016-06-19 13:59:52 +0100

    Added Romans to Integers solution

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,5 +1,25 @@
 def roman_to_integer roman
   
-  
+  roman = roman.downcase.split('')
+
+  rhash = {'i' => 1,
+           'v' => 5,
+           'x' => 10,
+           'l' => 50,
+           'c' => 100,
+           'd' => 500,
+           'm' => 1000}
+
+  # let's begin!
+  sum = 0
+  while roman.length >= 1
+    checking = roman.pop
+    if roman.length > 0 and rhash[checking] > rhash[roman.last]
+      sum = sum - rhash[roman.pop]
+    end
+    sum = sum + rhash[checking]
+  end
+
+  sum
   
 end

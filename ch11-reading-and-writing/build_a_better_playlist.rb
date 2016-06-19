31e685b2743da7690d7be2618a85b5b8ff7de67b
noby <n.fujioka@abacusmaths.info>
2016-06-19 08:10:09 +0100

    Done ch11

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,52 @@
 def music_shuffle filenames
-  # your code here
+ # this function seems to do exactly the same thing as arr.srot_by {rand}
+
+  filenames = filenames.sort
+  len = filenames.length
+
+  # Now we shuffle twice.
+  2.times do
+    l_idx = 0 # index of next card in left pile
+    r_idx = len/2 # index of next card in right pile
+    shuf = []
+    # NOTE: If we have an odd number of "cards",
+    # then the right pile will be larger.
+
+    while shuf.length < len
+      if shuf.length%2 == 0
+        # take card from right pile
+        shuf.push(filenames[r_idx])
+        r_idx = r_idx + 1
+      else
+        # take card from left pile
+        shuf.push(filenames[l_idx])
+        l_idx = l_idx + 1
+      end
+    end
+
+    filenames = shuf
+  end
+
+
+  # And cut the deck.
+  # This bit I do not understand.
+  arr = []
+  cut = rand(len) # index of card to cut at
+  idx = 0
+
+  while idx < len
+    arr.push(filenames[(idx+cut)%len])
+    idx = idx + 1
+  end
+  
+  arr
 end
+
+# using the shuffle method as defined above
+all_oggs = music_shuffle(Dir['**/*.ogg'])
+File.open 'playlistMore.m3u', 'w' do |f| 
+	all_oggs.each do |ogg|
+		f.write ogg+"\n" 
+	end
+end
+puts 'Done!'
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,12 @@
-# your code here
+def shuffle (arr)
+  arr.sort_by{rand}
+end
+
+# using the shuffle method as defined above
+all_oggs = shuffle(Dir['**/*.ogg'])
+File.open 'playlist.m3u', 'w' do |f| 
+	all_oggs.each do |ogg|
+		f.write ogg+"\n" 
+	end
+end
+puts 'Done!'

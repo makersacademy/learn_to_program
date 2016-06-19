2801102434a97afba040ea99cb315a4ad78b2cf4
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-06-19 17:20:02 +0100

    complete happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,17 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp!
+
+puts "What month were you born?"
+month = gets.chomp!
+
+puts "What day were you born"
+day = gets.chomp!
+
+time = Time.new
+birthday = Time.gm(year, month, day)
+
+years_old = (time - birthday)/(60*60*24*365)
+
+years_old.round.times do
+  puts "SPANK!"
+end

51fb4b5a0e5d2bd7b60135804467ee1f86a32cd2
Adam <adamjbrowner@mgmail.com>
2016-06-19 18:09:12 +0100

    Completed roman numerials exercise

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,14 @@
-def old_roman_numeral num
-  # your code here
+def roman_numerial(x)
+    x = x.to_i
+	numerial = " "
+	numerial = numerial + "M" * (x / 1000)
+	numerial = numerial + "D" * ((x % 1000) / 500)
+	numerial = numerial + "C" * ((x % 500) / 100)
+	numerial = numerial + "L" * ((x % 100) / 50)
+	numerial = numerial + "X" * ((x % 50) / 10)
+	numerial = numerial + "V" * ((x % 10) / 5)
+	numerial = numerial + "I" * ((x % 5) / 1)
+	puts numerial
 end
+
+roman_numerial(876)

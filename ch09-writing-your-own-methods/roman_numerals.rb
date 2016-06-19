48d4e6113226d45f527fa080c9c7385a7fcfa381
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-19 21:33:26 +0100

    Roman Numerals, working ok

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,15 @@
+ROMAN_NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
+                  100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
+                  10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+
 def roman_numeral num
-  # your code here
+  roman = ''
+  ROMAN_NUMERALS.each do |k,v|
+    (num / k).times do
+      roman += v; num = num - k
+    end
+  end
+  return roman
 end
+
+#puts roman_numeral 4

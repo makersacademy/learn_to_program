ec2df714272edfc79dbe366ebe7c13907b838975
Luke Cartledge <luke@lukecartledge.com>
2016-06-19 18:16:36 +0300

    Ch14 task 3

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
 def log desc, &block
-  # your code here
+  puts 'Beginning "' + desc + '"...'
+  output = block.call
+  puts '..."' + desc + '" finished, returning:' + output.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    (24 + 1) / 5
+  end
+  log 'yet another block' do
+    'I like' << '!doof iahT '.reverse
+  end
+  Time.new == Time.gm(2016, 5, 12)
 end

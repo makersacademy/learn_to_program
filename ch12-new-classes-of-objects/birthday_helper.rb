57e4b5292436a8517e01ea1b777d831500e6de68
richo225 <richo225@gmail.com>
2016-06-19 14:22:17 +0100

    ch12 Submit birthday_helper.rb & birthday_list.txt

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,11 @@
-# your code here
+Dir.chdir "/home/richard/Documents/learn_to_program/ch12-new-classes-of-objects/"
+
+file = File.open("birthday_list.txt", "r")
+contents = file.read
+
+arr = contents.lines
+
+puts arr
+#contents.each_line do |line|
+ #line.split(",")
+# end

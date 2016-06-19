6c1ed0db1a61d89ecf9c6a580146e8af3f274803
Zeeshan <mzishtiaq@yahoo.co.uk>
2016-06-19 12:21:07 +0000

    add birthday_helper and text file

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,16 @@
-# your code here
+birth_dates = {}
+File.read('birthday.txt').each_line do |line|
+  line = line.chomp
+  splitting = line.split(',',2)
+  birth_dates[splitting[0]] = [splitting[1..2]]
+end
+
+puts 'Whose birthday would you like to know?'
+name = gets.chomp
+date = birth_dates[name]
+
+if date == nil
+  puts "Sorry, I don't know that one..."
+else
+  puts date[0..5]
+end

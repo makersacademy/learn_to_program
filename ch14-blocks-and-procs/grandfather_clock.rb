97cffa0a608c71b741fef1608dd77bd5c9602bc2
Luke Cartledge <luke@lukecartledge.com>
2016-06-19 16:15:38 +0300

    Chapter 14 second task

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,17 @@
 def grandfather_clock &block
-  # your code here
+  current_hour = Time.new.hour
+  if current_hour > 12
+    current_hour -= 12
+  end
+  if current_hour == 0
+    current_hour == 12
+  end
+
+  current_hour.times do
+    block.call
+  end
+end
+
+grandfather_clock do
+  puts 'DING!'
 end

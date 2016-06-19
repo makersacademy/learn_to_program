2c008a4b1e268e5c625267eac41f9f1dc9843ff4
Arunas Skirius <arukomp@gmail.com>
2016-06-19 12:26:21 +0100

    Added Extend Built-In Classes solution

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,22 @@
 class Integer
-  # your code here
+
+  def factorial
+    if self <= 1
+      1
+    else
+      self * (self - 1).factorial
+    end
+  end
+
+  def to_roman
+    answer = ""
+    answer += "M" * (self / 1000)
+    answer += "D" * ((self % 1000) / 500)
+    answer += "C" * ((self % 500) / 100)
+    answer += "L" * ((self % 100) / 50)
+    answer += "X" * ((self % 50) / 10)
+    answer += "V" * ((self % 10) / 5)
+    answer += "I" * (self % 5)
+    answer
+  end
 end

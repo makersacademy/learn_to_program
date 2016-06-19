c590b213bbef6f280811175cfb4062fc92660513
Luke Cartledge <luke@lukecartledge.com>
2016-06-19 09:18:14 +0300

    Add main OrangeTree def\'s

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -15,11 +15,49 @@ class OrangeTree
   end
 
   def height
+    if @age <= 25
+      @height
+    else
+      'A dead tree is not very tall. :('
   end
 
   def one_year_passes
+    if @age <= 25
+      @height_of_tree += 0.4
+      @age += 1
+      puts "This year your tree grew to #{@height}m tall, and produced #{@oranges} oranges."
+    else
+      puts 'Oh, no! The tree is too old, and has died. :('
+    end
   end
 
-  
+  def count_the_oranges
+    if @age <= 25
+      if @oranges > 0
+        puts "There are #{@oranges} left for you to eat."
+      else
+        puts 'There are no more oranges on the tree.'
+      end
+    else
+      puts 'A dead tree has no oranges. :('
+    end
+  end
 
+  def pick_an_orange
+    if @age <= 25
+      if @oranges > 0
+        @oranges -= 1
+        puts 'That orange was absolutely delicious!'
+      else
+        puts 'Sorry, the tree has no more oranges. You will just have to wait until next year.'
+      end
+    else
+      puts 'A dead tree has nothing to pick. :('
+    end
+  end
 end
+
+
+un = OrangeTree.new
+un.one_year_passes
+un.one_year_passes

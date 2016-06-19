eff5c5e02e6303147a9d485c3ffbb1bf5dee6917
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-19 13:08:04 +0000

    Q.10.english_number.attempt.googol?

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -48,19 +48,71 @@ end
   # roman = roman + "X" * (num % 50 / 10)
   # roman = roman + "V" * (num % 10 / 5)
   # roman = roman + "I" * (num % 5 / 1)
+  
   # only 4-9, 40-90 and 400-900 only want 1000/100 ... 100/10 ... 10/1
   #***IV not VI*** Do subtractable number first followed by total being subtracted
+  
   # M = 1000
   # D = 500
   # C = 100
   # L = 50
   # X = 10
   # V = 5
+  
   # I = 1
   # want to make it so that 4 = IV (5-1) 
+  
   # possible numbers 4 and 9, 40 and 90, 400 and 900
   # possible combinations MD or MC/ CL or CX / XV or XI
+  
   # single digit, double digit, triple digit (M=1000)
   # hundreds... 900 in roman 1000-100=M-C / 400 in R 500-100 = D-C
   # tens...90 in R 100-10=C-X / 40 in R 50-10=L-X
   # single digits ... 9 = 10-1(X-I) / 4 = 5-1(V-I)
+  
+  #-----------------------------
+  
+  # what went wrong????
+  
+  #-----------------------------
+  
+  #def roman_numeral num
+  # your code here
+  
+  # four_digit = (num / 1000)
+  # three_digit = (num % 1000 / 100)
+  # two_digit = (num % 100 / 10)
+  # one_digit = (num % 10 / 1)
+  
+  # roman = 'M' * four_digit
+  
+  # if three_digit == 4 
+  #    roman = roman + 'CD'
+  # elsif three_digit == 9 
+  #    roman = roman + 'CM'
+  # else
+  #     roman = roman + 'C' * (num % 500 / 100)
+  #     roman = roman + 'D' * (num % 1000 / 500)
+  # end
+  # 
+  # if two_digit == 4
+  #     return roman = roman + 'XL'
+  # elsif two_digit == 9
+  #     return roman = roman + 'XC'
+  # else
+  #     roman = roman + 'X' * (num % 50 / 10)
+  #     roman = roman + 'L' * (num % 100 / 50)
+  # end
+  #
+  # if one_digit == 4
+  #     return roman = roman + 'IV'
+  # elsif one_digit == 9
+  #     return roman = roman + 'IX'
+  # else
+  #     roman = roman + 'V' * (num % 10 / 5)
+  #     roman = roman + 'I' * (num % 5 / 1)
+  # end
+  # roman
+  # end
+ # ERROR 1 = Method names can not begin with a number for it name
+ # ERROR 2 = Non-ASCII character use?
--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,41 @@
-def english_number number
+def english_number (number)
+
   # your code here
+
+  if number <= 0
+  return "No negative Integers, please put positive integer e.g where X is your integer; X >= 0"
+ 
+  elsif number == 0 then return "Zero"
+  
+  end 
+
 end
+
+# special number cases upto 100.. 20 onwards similar patttern of names e.g 26 and 36 = Twenty six and Thirty six
+# 10 to 19 vary
+# ones, tens, hundreds, thousands
+# number up to googol(highest known value?) 10^100
+
+num_string = ''
+
+ones = ['one', 'two','three','four','five','six','seven','eight','nine']
+tens = ['ten','eleven','twelev','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+tenss = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+hundreds = ones + "hundred"
+
+
+def thousands 
+    if number < 10000 
+        return ones + "thousand"
+        elsif number>= 10000 and number <1000000
+        tenss + "thousand"
+        end
+end
+
+big_numbers = [["hundred",2]["thousand",3]["million",6]["billion",9]["googol",100]]
+
+#left and write to fill remaining space?
+
+left = number
+write =left/(10^100)#?? googol but if less then?
+

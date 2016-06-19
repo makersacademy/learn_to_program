665b8dcb24b4061935e48f34418c10b6ac7cc2a7
nikklein <objcoding@gmail.com>
2016-06-19 22:32:55 +0300

    Extended Integer class

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,23 @@
-class Integer
-  # your code here
+class Integer 
+
+def factorial
+
+self < 0 ? 'You can\'t take the factorial of a negative number!': self <= 1 ? 1 : self * (self - 1).factorial
+
+end
+
+def to_roman
+
+roman_num = ''   
+roman_num = roman_num + 'M'*(self/1000)
+roman_num = roman_num + 'D'*(self % 1000 /500)
+roman_num = roman_num + 'C'*(self % 500 /100)
+roman_num = roman_num + 'L'*(self % 100 /50)
+roman_num = roman_num + 'X'*(self % 50 /10)
+roman_num = roman_num + 'V'*(self % 10 /5)
+roman_num = roman_num + 'I'*(self % 5)
+roman_num 
+
+end
+
 end

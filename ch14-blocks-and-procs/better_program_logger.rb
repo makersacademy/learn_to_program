4c9cefe78f734e71d3a421042c7e834671b4b05d
albie <albiebabie@gmail.com>
2016-06-20 08:23:00 +0100

    bplupdate2

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,30 +1,14 @@
-$logger_depth = 0
+$nesting_depth = 0
 
-def log desc, &block
-  prefix = ' '*$logger_depth
+def better_log desc, &block
 
-  puts prefix + 'Beginning "' + desc + '"...'
+  depth = ' ' * $nesting_depth
+  puts depth + 'Beginning "' + desc + '"...'
 
-  $logger_depth = $logger_depth + 1
+  $nesting_depth += 1
+  call = block.call
+  $nesting_depth -= 1
 
-  result = block.call
+  puts depth + '..."' +  desc + '" finished, returning: ' + call.to_s
 
-  $logger_depth = $logger_depth - 1
-  puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
-end
-
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
-      'lOtS oF lOVe'.downcase
-    end
-
-    7 * 3 * 2
-  end
-
-  log 'yet another block' do
-    '!doof naidnI evol I'.reverse
-  end
-
-  '0' == "0"
 end

51ef46d3e33a602f51f36ca382fdf331a5f0b807
Titus Iudean <iudeant@gmail.com>
2016-06-20 09:49:21 +0100

    Program logger now points out that I AM DONE WITH L2P - will revisit though

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,27 @@
+
+$logger_depth = 0
+
 def log desc, &block
-  # your code here
+   # your code here
+  prefix = ' '*$logger_depth
+  puts prefix + 'Beginning "' + desc + '"...' 
+  $logger_depth = $logger_depth + 1
+  result = block.call
+  $logger_depth = $logger_depth - 1
+
+  puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s 
+end
+
+log 'outer block' do
+  log 'inner block' do
+    log 'inner inner block' do 
+      'DONE HERE...'.downcase
+    end
+    7*3*2
+  end
+  log 'yet another block' do 
+    "now I really am done (ish) with 'learn to program' ".upcase
+end
+
+'0' == "0" 
 end

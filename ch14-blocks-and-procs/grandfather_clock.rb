70aadef15647f671ae36fe1928947d8dcb732ccf
Titus Iudean <iudeant@gmail.com>
2016-06-20 09:18:01 +0100

    Program lets us know what the time is according to Accurist ;)

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,21 @@
 def grandfather_clock &block
   # your code here
+  hour = Time.new.hour
+  if hour > 12
+    hour -= 12
+  end
+
+  if hour == 0 
+    hour = 12
+  end
+
+  hour.times do 
+    block.call
+  end 
 end
+
+puts "The time sponsored by Accurist is...:"
+grandfather_clock do 
+  puts 'DONG!'
+end
+puts "Yeah, that much."

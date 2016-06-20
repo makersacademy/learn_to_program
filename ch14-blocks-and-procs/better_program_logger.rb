5e6c2c757408805821751498f7e1d826ebb60b43
Zeeshan <mzishtiaq@yahoo.co.uk>
2016-06-20 05:11:11 +0000

    add better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,14 @@
-def log desc, &block
-  # your code here
+$nesting_depth = 0
+
+def better_log desc, &block
+  
+  depth = ' ' * $nesting_depth
+  puts depth + 'Beginning "' + desc + '"...'
+  
+  $nesting_depth += 1
+  call = block.call
+  $nesting_depth -= 1
+  
+  puts depth + '..."' +  desc + '" finished, returning: ' + call.to_s
+  
 end

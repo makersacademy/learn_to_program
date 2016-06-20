f6824c74e0f0eab2c16d4ed6a36cc84053a2c1dc
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-20 20:03:10 +0200

    building a better playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,33 @@
 def music_shuffle filenames
-  # your code here
+  filenames = filenames.sort
+  num = filenames.length
+
+  2.times do
+    left_index = 0
+    right_index = num/2
+    shuffled = []
+
+    while shuffled.length < num
+      if shuffled.length % 2 == 0
+        shuffled.push(filenames[right_index])
+        right_index += 1
+      else
+        shuffled.push(filenames[left_index])
+        left_index += 1
+      end
+    end
+
+    filenames = shuffled
+  end
+
+  array = []
+  cut = rand(num)
+  index = 0
+
+  while index < num
+    array.push(filenames[(index + cut) % num])
+    index += 1
+  end
+
+  array
 end

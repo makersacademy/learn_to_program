69fc1a12dd9ceaafe8bcf5117c5cb12f427e3154
Harry Jaggs <harryjaggs92@gmail.com>
2016-06-20 08:23:10 +0000

    shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,26 +1,3 @@
 def shuffle arr
-  
-  shuf = []
-
-  while arr.length > 0
-  
-  random_index = rand(arr.length)
-  current_index = 0
-  
-  new_array = []
-    arr.each do |item|
-      
-      if current_index == random_index
-        shuf.push item
-      else
-        new_array.push item
-      end
-    
-    current_index = current_index + 1
-    end
-  
-    arr = new_array
-  end
-  
-  shuf
+  arr.shuffle
 end

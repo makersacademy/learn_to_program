3f71f354a3a93b6bc84fdc5319c2d35aa8743382
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-20 20:38:29 +0100

    cleanup ch09 files

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,15 +1,16 @@
-ROMAN_NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
-                  100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
-                  10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+ROMAN_NUMERALS = {1000 => 'M', 900 => "CM", 500 => "D", 400 => "CD",
+                  100 => 'C', 90 => "XC", 50 => "L", 40 => "XL",
+                  10 => 'X', 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
 
-def roman_numeral num
+def roman_numeral(num)
   roman = ''
-  ROMAN_NUMERALS.each do |k,v|
+  ROMAN_NUMERALS.each do |k, v|
     (num / k).times do
-      roman += v; num = num - k
+      roman += v
+      num = num - k
     end
   end
-  return roman
+  roman
 end
 
-#puts roman_numeral 4
+# puts roman_numeral 4
--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,5 @@
 def dictionary_sort arr
-  # your code here
+  arr = arr.sort! {|a,b| a.length <=> b.length}
+  arr = arr.sort_by {|w| w.to_s.downcase}
+  return arr
 end

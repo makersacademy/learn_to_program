77fa85356f676da5e6ffaa805c22c747119baefa
Titus Iudean <iudeant@gmail.com>
2016-06-20 04:26:27 +0100

    Program converts roman numerals to equivalent integer while validating for correct numeral

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,35 @@
 def roman_to_integer roman
   # your code here
+
+  roman_numerals = {
+      "M" => 1000,
+      "D" => 500,
+      "C" => 100,
+      "L" => 50,
+      "X" => 10,
+      "V" => 5,
+      "I" => 1,
+  }
+
+  converted_numeral = 0
+
+  numerals = roman.upcase.split("")
+
+  numerals.each do |num|
+    if !roman_numerals.has_key?(num)
+      puts "This is not a valid Roman Numeral"
+      exit
+    else
+      roman_numerals.each do |key, value|
+        if key == num
+          converted_numeral += value
+        end
+      end
+    end
+  end
+
+  converted_numeral
+
 end
+
+puts roman_to_integer("MMMciid")

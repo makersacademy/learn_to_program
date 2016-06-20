9bdb64491b07a9c2a25137b672d0ece4797a0e4c
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-20 12:20:26 +0100

    Adds dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,26 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort (unsorted_arr, sorted_arr)
+
+  if unsorted_arr.length <= 0
+    return sorted_arr
+  end
+
+  smallestWord = unsorted_arr.pop
+  still_unsorted = []
+
+  unsorted_arr.each { |testedWord|
+    if testedWord.downcase < smallestWord.downcase
+      still_unsorted.push (smallestWord)
+      smallestWord = testedWord
+    else
+      still_unsorted.push (testedWord)
+    end
+  }
+
+  sorted_arr.push (smallestWord)
+
+  recursive_sort still_unsorted, sorted_arr
 end

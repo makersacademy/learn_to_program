41d14b1813362e7ef862e0f2d2d9d1698a707eca
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-20 17:12:06 +0100

    challenges for ch14 attempt

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour
+  case
+    when hour >= 13
+    hour = hour - 12
+    when hour == 0
+    hour = 12
+  end
 end
+  hour.times do
+    block.call
+  end
+
+  grandfather_clock do
+    puts 'DONG!'
+  end

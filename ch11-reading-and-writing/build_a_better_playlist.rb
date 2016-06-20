7e49a26652f5c8780bb08984a629ab6409816bb3
samjbro <sjbroughton1@gmail.com>
2016-06-20 09:47:01 +0100

    All exercises completed

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,25 @@
+require 'yaml'
+
+def yaml_save object, filename
+	File.open filename, 'w' do |f|
+		f.write(object.to_yaml)
+	end
+end
+
+def yaml_load filename
+	yaml_string = File.read filename
+	YAML::load yaml_string
+end
+
 def music_shuffle filenames
   # your code here
 end
+
+
+filename = 'playlist.txt'
+
+yaml_save Dir['/Users/sambroughton/Music/iTunes/iTunes Media/Music/**/*.mp3'], filename
+
+playlist = yaml_load filename
+p playlist
+puts music_shuffle playlist
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+def happy_birthday
+	puts "What year were you born in? (YYYY)"
+	year = gets.chomp
+	puts "What month were you born in? (MM)"
+	month = gets.chomp
+	puts "What day were you born on? (DD)"
+	day = gets.chomp
+
+	birthdate = Time.local(year,month,day)
+	age = (Time.now-birthdate)/60/60/24/365
+	age.floor.times{|y| puts "SPANK!"}
+end
+happy_birthday

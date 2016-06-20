c4a34365d4eca65a3098368ccd5752c10f255ff7
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-20 14:47:17 +0000

    Q.11 revision and solution.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,3 +1,13 @@
 # your code here
 #use shuffle.rb
-File.open 'example.music.file',
+all_oggs = shuffle(Dir['**/*.ogg'])
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_oggs.each do |ogg|
+    f.write ogg+"\n"
+  end
+end
+
+#-Dir array with ** and *.0gg ---- understand this
+
+

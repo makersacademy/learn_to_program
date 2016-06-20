87094f9e03106c8d3b4430ecb2a3174d3b8c8932
Luke Cartledge <luke@lukecartledge.com>
2016-06-20 10:44:26 +0300

    logger pass

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,23 +1,23 @@
-@nesting_depth = 0
+$nesting_depth = 0
 
-def log(desc, &block)
-  indent = '  ' * @nesting_depth
+def better_log(desc)
+  indent = ' ' * $nesting_depth
   puts indent + 'Beginning "' + desc + '"...'
-  @nesting_depth += 1
+  $nesting_depth += 1
   output = yield
-  @nesting_depth -= 1
+  $nesting_depth -= 1
   puts indent + '..."' + desc + '" finished, returning: ' + output.to_s
 end
 
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
-      'lots of love'
-    end
-    24 + 18
-  end
-  log 'yet another block' do
-    'I love' << '!doof naidnI '.reverse
-  end
-  5**5 == 25
-end
+# better_log 'outer block' do
+#   better_log 'some little block' do
+#     better_log 'teeny-tiny block' do
+#       'lots of love'
+#     end
+#     24 + 18
+#   end
+#   lbetter_log 'yet another block' do
+#     'I love' << '!doof naidnI '.reverse
+#   end
+#   5**5 == 25
+# end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,16 +1,17 @@
-def log (desc, &block)
+def program_log(desc)
   puts 'Beginning "' + desc + '"...'
   output = yield
-  puts '..."' + desc + '" finished, returning:'
-  puts output.to_s
+  puts '..."' + desc + '" finished, returning: ' + output.to_s
 end
 
-log 'outer block' do
-  log 'some little block' do
-    (24 + 1) / 5
-  end
-  log 'yet another block' do
-    'I like' << '!doof iahT '.reverse
-  end
-  Time.new == Time.gm(2016, 5, 12)
-end
+# program_log 'outer block' do
+#   program_log 'some little block' do
+#     1**1 + 2**2
+#   end
+#
+#   program_log 'yet another block' do
+#     '!doof iahT ekil I'.reverse
+#   end
+#
+#   '0' == 0
+# end

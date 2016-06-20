803b02c92f36f800398996bfc25b7d2c784122db
Harry Jaggs <harryjaggs92@gmail.com>
2016-06-20 07:58:33 +0000

    Dragon

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -3,27 +3,20 @@ class Dragon
 def initialize name
 @name = name
 @asleep = false
-@stuff_in_belly = 10 # he's full
-@stuff_in_intestine = 0 # doesn't need to go
-
+@stuff_in_belly = 10 
+@stuff_in_intestine = 0 
 puts @name + ' is born.'
 end
-
-# Feed
 def feed
 puts 'You feed ' + @name + '.'
 @stuff_in_belly = 10
 passage_of_time
 end
-
-# Walk
 def walk
 puts 'You walk ' + @name + '.'
 @stuff_in_intestine = 0
 passage_of_time
 end
-
-# Put to bed 
 def put_to_bed
 puts 'You put ' + @name + ' to bed.'
 @asleep = true
@@ -40,15 +33,11 @@ if @asleep
 puts @name + ' wakes up slowly.'
 end
 end
-
-# Toss
 def toss
 puts 'You toss ' + @name + ' up into the air.'
 puts 'He giggles, which singes your eyebrows.'
 passage_of_time
 end
-
-# Rock
 def rock
 puts 'You rock ' + @name + ' gently.'
 @asleep = true
@@ -59,25 +48,15 @@ if @asleep
 puts '...but wakes when you stop'
 end
 end
-
 private
-
-# following methods are internal
-
-# hungry?
 def hungry?
 @stuff_in_belly <= 2
 end
-
-# poopy?
 def poopy?
 @stuff_in_intestine >= 8
 end
-
-# passage of time, this is when things happen
 def passage_of_time
 if @stuff_in_belly > 0
-# move food from belly to intestine
 @stuff_in_belly = @stuff_in_belly - 1
 @stuff_in_intestine = @stuff_in_intestine + 1
 else
@@ -88,12 +67,10 @@ end
 puts @name + ' is starving! In desperation, he ate YOU!'
 exit
 end
-
 if @stuff_in_intestine >= 10
 @stuff_in_intestine = 0
 puts 'Whoops! ' + @name + ' had an accident...'
 end
-
 if hungry?
 if @asleep
 @asleep = false
@@ -101,7 +78,6 @@ puts 'He wakes up suddenly!'
 end
 puts @name + '\'s stomach grumbles...'
 end
-
 if poopy?
 if @asleep
 @asleep = false
@@ -110,13 +86,9 @@ end
 puts @name + ' does the potty dance...'
 end
 end
-
 end
-
 pet = Dragon.new 'Norbert'
-
 command = ''
-
 while command != 'exit'
 puts 'Enter a command to control your dragon:'
 command = gets.chomp

353223680eabd0facb0dd43a779684de40699945
nikklein <objcoding@gmail.com>
2016-06-20 03:20:09 +0300

    Orange Tree ver. 2.0

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -4,37 +4,53 @@ def initialize
 @height = 0
 @orange_count = 0
 @alive = true
+@age = 0
 end
 
 def height
 if @alive
 @height
 else
-"Oh, no! The tree is too old, and has died. :("
-end
-end
-
-def count_the_oranges
-if @alive
-@orange_count
+"A dead tree is not very tall. :("
 end
 end
 
 def one_year_passes
+if @alive
+@age +=1
 @height = (@height + 0.4).round(1)
-if @height > 10
+if @age > 25
 @alive = false
-"Oh, no! The tree is too old, and has died. :("
-exit
-elsif @height > 2
+'Oh, no! The tree is too old, and has died. :('
+elsif @age > 5
 @orange_count = (@height * 15 - 25).round(1).to_i
-else
-@orange_count = 0
-end
 "This year your tree grew to #{@height}m tall, and produced #{@orange_count} oranges."
+else
+"Your tree is still too young to bear fruit"
+end
+else
+'A year later, the tree is still dead. :('
+end
 end
 
 
+def count_the_oranges
+if @alive
+@orange_count
+else
+"A dead tree has no oranges. :("
+end
+end
 
+def pick_an_orange
+     if @alive && @oranges >= 1
+        @oranges -= 1
+        "You picked one orange"
+     elsif @alive
+        "No oranges to pick, I'm afraid :("
+     else
+        "A dead tree has nothing to pick. :("
+     end
+  end
 
 end

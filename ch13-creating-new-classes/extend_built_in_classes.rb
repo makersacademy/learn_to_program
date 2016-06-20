2297cde27142f333aa292a663266eefa3f69a1e7
JulieLev <jkgee1@yahoo.co.uk>
2016-06-20 22:12:09 +0000

    Adding extend_built_in_classes.rb Amazingly, it works.

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,20 @@
 class Integer
-  # your code here
+ def fact
+    num = self
+    
+    if num < 0
+        return 'You can\'t take the factorial of a negative number!'
+    elsif num == 1 
+        return 1
+    end
+        
+    num = num * (num-1).fact
+    
+    return num    
+ end
 end
+
+puts 4.fact
+puts 2.fact
+puts 20.fact
+

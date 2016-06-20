cb38139eee4db087c44b8886a9d9143ac5bee5be
Yuri Zhuravel <zhuravel@gmail.com>
2016-06-20 14:35:45 +0100

    Old roman numerals fixed

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,34 +1,15 @@
-def old_roman_numeral num
+def old_roman_numeral (num)
 
-splitArray = num.split(",").map { |s| s.to_i }
-romanArray = Array.new
+  romNum = ""
 
-if splitArray.length == 4
-  romanArray.push ('M' * (splitArray[0]/1000)) 
-  splitArray.delete_at(0)
+  romNum = romNum + 'M' * (num / 1000)
+  romNum = romNum + 'D' * (num % 1000 / 500)
+  romNum = romNum + 'C' * (num % 500 / 100)
+  romNum = romNum + 'L' * (num % 100 / 50)
+  romNum = romNum + 'X' * (num % 50 / 10)
+  romNum = romNum + 'V' * (num % 10 / 5)
+  romNum = romNum + 'I' * (num % 5 / 1)
 
-elsif splitArray.length == 3 && splitArray[0] >=5
-  romanArray.push ('D')
-
-elsif splitArray.length == 3 && splitArray[0] < 5
-  romanArray.push ('C' * (splitArray[0]/100))
-  splitArray.delete_at(0)
-
-elsif splitArray.length == 2 && splitArray[0] >=5
-  romanArray.push ('L')
-
-elsif splitArray.length == 2 && splitArray[0] < 5
-  romanArray.push ('X' * (splitArray[0]/10))
-  splitArray.delete_at(0)
-
-elsif splitArray.length == 1 && splitArray[0] >=5
-  romanArray.push ('V')
-
-elsif splitArray.length == 1 && splitArray[0] < 5
-  romanArray.push ('I' * (splitArray[0]/10))
-  splitArray.delete_at(0)
-end
-
-return romanArray.join("")
+  romNum
 
 end

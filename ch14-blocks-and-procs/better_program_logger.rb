ed859349417bb6590350dbdff6ea8229106e338c
6eff <6eff@mail.ru>
2016-06-20 08:03:10 +0300

    ch14 loggers

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
-def log desc, &block
-  # your code here
+def log str, &block
+  puts 'Beginning "' + str + '"...'
+  result = block.call
+  puts '..."' + str + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+    log 'teeny-tiny block' do
+      'lost in plov'
+    end
+   42
+  end
+
+
+  log 'yet another block' do
+    ["I", "love", "Indian", "food!"].join(" ")
+  end
+
+  "Ruby" != "Java"
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,17 @@
-def log desc, &block
-  # your code here
+def log str, &block
+  puts 'Beginning "' + str + '"...'
+  result = block.call
+  puts '..."' + str + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+  log 'some little block' do
+   5
+  end
+
+  log 'yet another block' do
+    ["I", "like", "Thai", "food!"].join(" ")
+  end
+
+  false
 end

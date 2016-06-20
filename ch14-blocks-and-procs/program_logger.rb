198d1337c442ba19a2fbf1fe633a5e7037c9bea1
albie <albiebabie@gmail.com>
2016-06-20 08:27:31 +0100

    program logger update2

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,21 +1,6 @@
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  call = block.call
+  puts '..."' +  desc + '" finished, returning: ' + call.to_s
 
-def log desc, &block
-  puts "Beginning #{desc.inspect}..."
-  # inspect is used more for debugging and
-  # to_s for end-user or display purposes.
-  result = block[] #here inserting what is returned
-  # from the block into result variable.
-  puts "...#{desc.inspect} finished, returning: #{result}"
-end
-
-log 'outer block' do
-  log 'some little block' do
-    1**1 + 2**2
-  end
-
-  log 'yet another block' do
-    '!doof iahT ekil I'.reverse
-  end
-
-  '0' == 0
 end

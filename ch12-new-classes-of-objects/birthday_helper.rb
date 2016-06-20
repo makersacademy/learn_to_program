c9e417185702281431295861118d629a6af00d36
Titus Iudean <iudeant@gmail.com>
2016-06-20 06:21:26 +0100

    Program reads file and based on user input a name is selected and date of birth displayed

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,18 @@
 # your code here
+birthdays = {}
+
+File.read('birthdays.txt').each_line do |line|
+  line = line.chomp
+  details = line.split(", ")
+
+  #puts details
+  birthdays[details[0]] = details[2]
+  puts birthdays
+end
+
+puts "Whose birthday do you want to know?"
+name = gets.chomp.to_s
+
+birthdays.each do |list_name, date|
+  puts "#{list_name}, #{date}" if list_name == name
+end 

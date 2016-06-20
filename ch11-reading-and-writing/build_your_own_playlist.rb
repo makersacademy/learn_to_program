d0884edc8234aa502fe332ff94a351da630ea9af
Titus Iudean <iudeant@gmail.com>
2016-06-20 01:54:02 +0100

    Program takes a list of music files and creates a shuffled playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,34 @@
 # your code here
+def sort(arr)  
+  i =0
+  while i < arr.length 
+    j = i + 1
+    while j < arr.length
+      if arr[i] > arr[j]
+        arr[i]  , arr[j] = arr[j], arr[i] 
+      end
+      j += 1 
+    end
+    i += 1
+ end
+ arr
+end 
+
+def sort(arr)
+  pre_sort(arr, [])
+end
+
+def shuffle(arr)
+  arr.sort() {rand}
+end
+
+# give you music a good shuffle
+music_mix = shuffle(Dir['/Users/AppsDJ/Music/*.{MP3, mp3}'])
+
+# now shove it into a new list
+File.open 'stanton_lanier.m3u', 'w' do |f| 
+  music_mix.each do |song|
+    f.write song + "\n" 
+  end
+end
+puts 'New playlist created successfuly!'

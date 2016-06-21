11fd16205bd4a12f6fc9fc569ce45eac57237d2e
Mikey <mjosephmiller.uk@gmail.com>
2016-06-21 14:55:20 +0100

    Working on a Ruby recursive sorting method. Not yet working

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,30 @@
 def sort arr
-  # your code here
+  rec_sort arr, []
 end
+
+def rec_sort unsorted, all_sorted
+  if unsorted.length <= 0
+    return all_sorted
+  end
+
+all_sorted = []
+still_unsorted = []
+
+smallest = unsorted.min
+
+unsorted.each do |tested_string|
+  if tested_string == smallest
+    all_sorted << smallest
+    unsorted.delete(smallest)
+  else
+    still_unsorted << smallest
+  end
+end
+
+
+rec_sort still_unsorted, all_sorted
+end
+
+puts sort (['baby', 'baby', 'fred', 'car', 'ham', 'alien'])
+
+#doesn't work. Puts only 'alien'

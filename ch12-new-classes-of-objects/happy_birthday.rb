37485816bdaaf1a504353e46b6dd1fa27e0328f1
Mikey <mjosephmiller.uk@gmail.com>
2016-06-21 17:09:49 +0100

    A Ruby age calculator

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+puts 'What year were you born?'
+year = gets.chomp.to_i
+puts 'What month? (1-12)'
+month = gets.chomp.to_i
+puts 'Day? Digits only please!'
+day = gets.chomp.to_i
+
+seconds_old = Time.new - Time.local(year, month, day)
+minutes_old = seconds_old / 60
+hours_old = minutes_old / 60
+days_old = hours_old / 24
+years_old = days_old / 365
+years_old_int = years_old.to_i
+puts 'WOW! You\'re ' + years_old_int.to_s + ' years old!' #this answer is wrong
+puts 'SPANK! ' * years_old_int

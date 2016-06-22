e9d8e6ae22066d8260a06d833e44e2f6abfc2615
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-06-22 16:11:51 +0100

    refactor old roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,30 +1,10 @@
 def old_roman_numeral(num)
+  numeral = {1000 => 0, 500 => 0, 100 => 0, 50 => 0, 10 => 0, 5 => 0, 1 => 0}
 
-  length = { :m_length => 0, :d_length => 0, :c_length => 0, :l_length => 0,
-                             :x_length => 0, :v_length => 0, :i_length => 0 }
-
-  length[:m_length] = num/1000 if num >= 1000
-  num = num%1000 if num >=1000
-
-  length[:d_length] = num/500 if num >= 500
-  num = num%500 if num >= 500
-
-  length[:c_length] = num/100 if num >= 100
-  num = num%100 if num >= 100
-
-  length[:l_length] = num/50 if num >= 50
-  num = num%50 if num >= 50
-
-  length[:x_length] = num/10 if num >= 10
-  num = num%10 if num >= 10
-
-  length[:v_length] = num/5 if num >= 5
-  num = num%5 if num >= 5
-
-  length[:i_length] = num/1 if num < 5
-  num = num%10 if num < 5
-
-  "M"*length[:m_length] + "D"*length[:d_length] + "C"*length[:c_length] +
-       "L"*length[:l_length] + "X"*length[:x_length] +
-       "V"*length[:v_length] + "I"*length[:i_length]
+  numeral.each do |k, v|
+    numeral[k] = num/k if num >= k
+    num = num%k        if num >= k
+  end
+     'M'*numeral[1000] + 'D'*numeral[500] + 'C'*numeral[100]  +
+     'L'*numeral[50] + 'X'*numeral[10] +'V'*numeral[5] + 'I'*numeral[1]
 end

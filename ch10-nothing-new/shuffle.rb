6d573df47ef8aef2e5131ad42db507c5475a18f9
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-06-22 12:32:47 +0100

    shuffle update

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,25 +1,3 @@
 def shuffle arr
-  shuffled = []
-
-  while !arr.empty?
-    random = rand(arr.length) # picking random element using length of array
-    index_no = 0
-    new_arr = []
-
-    arr.each do |element|
-      if index_no == random
-        shuffled.push element # if index number matches random number add to shuffled array
-      else
-        new_arr.push element # otherwise add to new array
-      end
-
-      index_no += 1
-    end
-
-  arr = new_arr #new array smaller, loops until arr is empty
-  end
-
-  shuffled # returns all elements in shuffled array in random order
+  arr.shuffle
 end
-
-puts(shuffle([1,2,3,4,5,6,7,8,9]))

dc003dc4f03065efeab2e2366c06da7f6f06d3e2
acsauk <acsauk@gmail.com>
2016-06-23 21:02:48 +0100

    Updates roman_numerals and old_school_roman_numerals with soltions

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,54 +1,12 @@
 def old_roman_numeral num
-  while true
-    numerals = {
-      1 => 'I',
-      5 => 'V',
-      10 => 'X',
-      50 => 'L',
-      100 => 'C',
-      500 => 'D',
-      1000 => 'M'
-    }
+  roman = ""
 
-    str = ""
-    num = gets.chomp.to_i
-
-    numerals.each do |number, numeral|
-      if num == number
-        return str + "#{numeral}"
-      elsif num / 5 == even && num % 5 == 0 # does not end in a 5
-        puts
-      elsif num / 5 == odd && num % 5 == 0 # does end in a five
-        puts
-      elsif
-
-
-    if num % 5 != 0 && num < 5
-      puts i *num
-    end
-
-    if num % 5 == 0 && num < 10
-      puts v
-    end
-
-    if num % 5 != 0 && num > 5 && num < 10
-      puts v + (i * (num % 5))
-    end
-
-    if num % 10 == 0 && num < 50
-      puts x
-    end
-
-    if num % 10 != 0 && num % 5 == 0 && num > 10 && num < 50
-      puts x + v + (i * (num % 5))
-    end
-
-    if num % 10 != 0 && num % 5 != 0 && num > 10 && num < 50
-      puts (x * (num / 10)) + (i * (num % 5))
-    end
-  break
-  end
+￼￼roman = roman + 'M' * (num / 1000)
+  roman = roman + 'D' * (num % 1000 / 500)
+  roman = roman + 'C' * (num % 500 / 100)
+  roman = roman + 'L' * (num % 100 / 50)
+  roman = roman + 'X' * (num % 50 / 10)
+  roman = roman + 'V' * (num % 10 / 5)
+  roman = roman + 'I' * (num % 5 / 1)
+  roman
 end
-
-puts "Hi - please enter a number"
-old_roman_numeral 0

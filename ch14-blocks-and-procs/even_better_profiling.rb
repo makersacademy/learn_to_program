4643920bca7dbf0c59e92239e87b5f136a6b25c8
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-23 21:17:28 +0100

    Grandfather clock using Block

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,14 @@
+$OPT_PROFILING_ON = true
+
 def profile block_description, &block
-  # your code here
+  if $OPT_PROFILING_ON
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end
+
+# profile ('test') { 2+3 }
--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
 def grandfather_clock &block
-  # your code here
+  curr_hour = Time.new.hour
+  if curr_hour >= 13
+    curr_hour -= 12
+  end
+
+  if curr_hour == 0
+    curr_hour = 12
+  end
+  curr_hour.times do
+    block.call
+  end
 end

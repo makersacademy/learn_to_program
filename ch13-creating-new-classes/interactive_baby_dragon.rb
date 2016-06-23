9291ffda0f67a3f3407e76b6eb563e654a32d921
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-23 18:26:00 +0100

    Interactive Dragon added

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,24 @@
-# your code here
+require_relative '/Users/newuser/Desktop/Makers/precourse/week2/' +
+                'learn_to_program/ch13-creating-new-classes/dragon.rb'
+class Dragon
+  def input_command command
+    if command.downcase == 'walk'
+      self.walk
+    elsif command.downcase == 'feed'
+      self.feed
+    elsif command.downcase == 'put to bed'
+      self.put_to_bed
+    elsif command.downcase == 'toss'
+      self.toss
+    elsif command.downcase == 'rock'
+      self.rock
+    else
+      puts "Sorry unknown command entered... #{@name} looks confused.."
+    end
+  end
+end
+
+sparky = Dragon.new('Sparky')
+puts "Enter a command to control your dragon"
+command = gets.chomp
+sparky.input_command(command)

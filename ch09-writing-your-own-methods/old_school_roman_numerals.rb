a1a879aadbc086f91c58d4b3343dcfd774f52cf6
acsauk <acsauk@gmail.com>
2016-06-23 22:07:16 +0100

    Updated further exercises with solutions from solutions folder...

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,7 +1,7 @@
 def old_roman_numeral num
   roman = ''
 
-￼￼roman = roman + 'M' * (num / 1000)
+  ￼￼roman = roman + 'M' * (num / 1000)
   roman = roman + 'D' * (num % 1000 / 500)
   roman = roman + 'C' * (num % 500 / 100)
   roman = roman + 'L' * (num % 100 / 50)
@@ -10,3 +10,6 @@ def old_roman_numeral num
   roman = roman + 'I' * (num % 5 / 1)
   roman
 end
+
+puts old_roman_numeral 1000
+puts old_roman_numeral 999

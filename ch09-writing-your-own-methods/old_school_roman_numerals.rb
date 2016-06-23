59b3e0626d0c79d992d304c490a11d88ca2bddf5
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-23 19:41:04 +0100

    Changed Constant variable to all upper case

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,9 +1,9 @@
-Old_Roman_Numerals = {'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50,
+OLD_ROMAN_NUMERALS = {'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50,
 											'X' => 10, 'V' => 5, 'I' => 1}
 
 def old_roman_numeral num
 	old_roman = ''
-	Old_Roman_Numerals.each do |k,v|
+	OLD_ROMAN_NUMERALS.each do |k,v|
 		(num / v).times do
 			old_roman += k; num = num - v
 		end
@@ -11,4 +11,4 @@ def old_roman_numeral num
 	return old_roman
 end
 
-puts old_roman_numeral gets.chomp.to_i
+# puts old_roman_numeral gets.chomp.to_i

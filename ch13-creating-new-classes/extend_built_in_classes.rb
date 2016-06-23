b878c3eba98d4c4051f8f34f5636e6ddebc4a6fd
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-06-23 12:04:40 +0100

    Extended Built in Integer class to include factorial and old school roman numeral conversion

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,22 @@
 class Integer
-  # your code here
+  def factorial
+    factorial = (1..self).inject(:*)
+  end
+
+  def to_roman
+    r_numerals = {'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50,
+    											'X' => 10, 'V' => 5, 'I' => 1}
+    roman = ''
+    num = self
+    r_numerals.each do |k, v|
+      (num / v).times do
+        roman += k
+        num = num - v
+      end
+    end
+    roman
+  end
 end
+
+#puts 73.factorial
+#puts 1004.to_roman

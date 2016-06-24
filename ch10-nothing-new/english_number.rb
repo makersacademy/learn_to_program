180ed9440501b24caf3c3a1b852b8060a92334d9
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-06-24 20:16:07 +0100

    added english number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,78 +1,59 @@
-def english_number number
+def english_number(number)
   if number < 0
-    return 'Please enter a number that isn\'t negative.'
+    return "Please enter a positive number"
   end
   if number == 0
-    return 'zero'
+    return "zero"
   end
+  num_string = ""
+  ones_place = ["one",      "two",      "three",
+                "four",     "five",     "six",
+                "seven",    "eight",    "nine"]
+  tens_place = ["ten",      "twenty",   "thirty",
+                "forty",    "fifty",    "sixty",
+                "seventy",   "eighty",   "ninety"]
+  teenagers  = ["eleven",   "twelve",   "thirteen",
+                "frouteen", "fifteen",  "sixteen",
+                "seventeen", "eighteen",  "ninteen"]
+  zillions  = [["hundred",2],
+               ["thousand",3],
+               ["million", 6],
+               ["billion", 9],
+               ["trillion", 12],
+               ["quadrillion",15],
+               ["quintillion", 18],
+               ["sextillion", 21],
+               ["septillion", 24],
+               ['octillion', 27],
+               ['nonillion', 30],
+               ['decillion', 33],
+               ['undecillion', 36],
+               ['duodecillion', 39],
+               ['tredecillion', 42],
+               ['quattuordecillion', 45],
+               ['quindecillion', 48],
+               ['sexdecillion', 51],
+               ['septendecillion', 54],
+               ['octodecillion', 57],
+               ['novemdecillion', 60],
+               ['vigintillion', 63],
+               ['googol', 100]]
+  left = number
 
-  num_string = ''
-  ones_place = ['one', 'two', 'three', 'four', 'five',
-                'six', 'seven', 'eight', 'nine']
-  tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty',
-                'sixty','seventy', 'eighty', 'ninety']
-  teenagers =  ['eleven', 'twelve', 'thirteen','fourteen', 'fifteen',
-                'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base 
 
-  left  = number
-  write = left/1000000000000
-  left  = left - write*1000000000000
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
 
-  if write > 0
-    trillions = english_number write
-    num_string = num_string + trillions + ' trillion'
-
-    if left > 0
-      num_string = num_string + ' '
-    end
-  end
-
-  write = left/1000000000
-  left = left - write*1000000000
-
-  if write > 0
-    billions = english_number write
-    num_string = num_string + billions + ' billion'
-
-    if left > 0
-      num_string = num_string + ' '
-    end
-  end
-
-  write = left/1000000
-  left = left - write*1000000
-
-  if write > 0
-    millions = english_number write
-    num_string = num_string + millions + ' million'
-
-    if left > 0
-      num_string = num_string + ' '
-    end
-  end
-
-  write = left/1000
-  left = left - write*1000
-
-  if write > 0
-    thousands = english_number write
-    num_string = num_string + thousands + ' thousand'
-
-    if left > 0
-      num_string = num_string + ' '
-    end
-  end
-
-  write = left/100
-  left = left - write*100
-
-  if write > 0
-
-    hundreds = english_number write
-    num_string = num_string + hundreds + ' hundred'
-
-    if left > 0
-      num_string = num_string + ' '
+      if left > 0
+        num_string = num_string + ' '
+      end
     end
   end
 
@@ -80,15 +61,12 @@ def english_number number
   left = left - write*10
 
   if write > 0
-    if ((write == 1) and (left > 0))
-
+    if ((write == 1) && (left > 0))
       num_string = num_string + teenagers[left-1]
-
       left = 0
     else
       num_string = num_string + tens_place[write-1]
     end
-
     if left > 0
       num_string = num_string + '-'
     end
@@ -100,6 +78,5 @@ def english_number number
   if write > 0
     num_string = num_string + ones_place[write-1]
   end
-
   num_string
 end

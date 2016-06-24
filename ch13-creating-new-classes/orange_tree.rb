2e904ac5a41138bb9c29812948da4c4ae9a60207
hannako <jessjones1@gmail.com>
2016-06-24 14:44:38 +0100

    orange tree program tweaked to pass rspec

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -11,59 +11,60 @@ class OrangeTree
     @height = 0
     @orange_count = 0
     @alive = true
-"Your orange tree has just been born."
+    return "Your orange tree has just been born."
   end
 
 def height
 
   if @alive
-      "Your tree is #{@height.round(2)} metres tall."
+      return @height.round(2)
   else
-         "A dead tree is not very tall. :("
+      return "A dead tree is not very tall. :("
   end
 end
 
 def one_year_passes
-  if @alive
-        @age = @age + 1
-        @height = @height + 0.4
-        "This year your tree grew to #{@height.round(2)}m tall, and produced #{@orange_count.round(0)} oranges."
-          if @age < 6
-                @orange_count = 0
-            else
-              @orange_count = ((@height + 0.4) * 15 - 25)
-          end
+    @height = @height + 0.4
+    @age = @age + 1
+    @orange_count = (@height * 15 - 25).to_i
+    if @age > 26
+      @alive = false
+      return "A year later, the tree is still dead. :("
+    end
 
-          if @age > 24
-          @alive = false
-           "Oh, no! The tree is too old, and has died. :("
-          end
-    else
-        "A year later the tree is still dead"
-      end
+    if @age == 26
+      return "Oh, no! The tree is too old, and has died. :("
+    end
+
+  if @alive
+    if @age > 5
+      return "This year your tree grew to #{@height.round(2)}m tall, and produced #{@orange_count} oranges."
+    end
   end
+end
+
 
 def count_the_oranges
-if @alive
-"Your tree has  #{@orange_count.round(0)} oranges."
-else
-  "A dead tree has no oranges. :("
+  if @alive
+    return @orange_count.to_i
+  else
+    return "A dead tree has no oranges. :("
+  end
 end
 
 def pick_an_orange
-if @alive
-    if @age > 5
-     "My what a delicious orange that was!"
-      @orange_count = @orange_count - 1
-    end
-    if @age < 5
-      @orange_count = 0
-       "Your tree isn't old enough to have oranges yet"
-    end
-else
- 'A dead tree has nothing to pick. :('
+  if @alive
+      if @age >= 5
+        return "My what a delicious orange that was!"
+        @orange_count = @orange_count - 1
+      else
+        @orange_count = 0
+        return "Your tree isn't old enough to have oranges yet"
+      end
+  else
+   return "A dead tree has nothing to pick. :("
+  end
 
 end
-end
-end
+
 end

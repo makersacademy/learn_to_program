b5de5cf01db9ab72f2329339c1d6e21f9ecaffcc
Rylan <rylangooch@gmail.com>
2016-06-24 21:35:36 +0000

    Even better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,13 +1,30 @@
-def profile block_description, &block
-    profiling_on = false
-  
-    if profiling_on
-        start_time = Time.new
-        block.call
+$logger_depth = 0
 
-        duration = Time.new - start_time
-        puts "#{block_description}: #{duration} seconds"
-    else
-        block.call
+def log desc, &block
+    prefix = ' '*$logger_depth
+
+    puts prefix + 'Beginning "' + desc + '"...'
+
+    $logger_depth = $logger_depth + 1
+
+    result = block.call
+
+    $logger_depth = $logger_depth - 1
+    puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+log 'outer block' do
+    log 'some little block' do
+        log 'teeny-tiny block' do
+            'lOtS oF lOVe'.downcase
+        end
+
+        7 * 3 * 2
     end
+
+    log 'yet another block' do
+        '!doof naidnI evol I'.reverse
+    end
+
+    '0' == "0"
 end

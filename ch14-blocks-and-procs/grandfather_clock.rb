4d977cd1f2cf1a6073fc76dbfe848b6d0e29f5f3
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-06-24 14:47:43 +0100

    complete grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,7 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock(&block)
+  hours = Time.new.hour
+  hours = hours - 12 if hours > 12
+  hours.times { block.call }
 end
+
+grandfather_clock { puts "Dong!" }

a0a3d6241879f88b3dbbf073aafb119e7f620a61
Nick J Bowman <nicholas.j.bowman@hotmail.com>
2016-06-25 08:12:33 +0000

    Q11 + 12 complete

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,5 +1,31 @@
 # your code here
-#use shuffle.rb
+def shuffle arr
+  # your code here
+  shuffled_array = []
+  
+ 
+  while arr.length > 0
+  rand_arr_object = rand(arr.length)
+  
+  selected = 0
+  new_array = []
+  
+  arr.each {|x|
+  
+  if selected == rand_arr_object
+      shuffled_array.push x
+  else
+      new_array.push x
+  end
+  selected +=1
+      
+  }
+  
+  arr= new_array
+end
+shuffled_array
+end
+
 all_oggs = shuffle(Dir['**/*.ogg'])
 
 File.open 'playlist.m3u', 'w' do |f|
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,17 +1,11 @@
 def roman_to_integer (roman)
-  # your code here
-  # roman values: M = 1000
-  #               D = 500
-  #               C = 100
-  #               L = 50
-  #               X = 10
-  #               V = 5
-  #               I = 1
-  #wants integer 1999 in numerals
-  #hash to assign values to characters
-  #
-  roman.downcase
-  roman_values ={'i' =>1,'v' => 5,'x' => 10,'l' =>50,'c' =>100,'d' =>500,'m' =>1000}
+  digit_vals ={'i' =>1,
+  'v' => 5,
+  'x' => 10,
+  'l' =>50,
+  'c' =>100,
+  'd' =>500,
+  'm' =>1000}
   
   total = 0
   prev = 0
@@ -38,3 +32,14 @@ end
   
   #-------------understand-------------
   #why val = val * -1? negative integers
+  #  # your code here
+  # roman values: M = 1000
+  #               D = 500
+  #               C = 100
+  #               L = 50
+  #               X = 10
+  #               V = 5
+  #               I = 1
+  #wants integer 1999 in numerals
+  #hash to assign values to characters
+  

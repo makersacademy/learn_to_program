ffb79fb7a582a11c4b68d939165260f53bdddcf5
Adam <adamjbrowner@mgmail.com>
2016-06-26 21:27:21 +0100

    Slightly edited sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,4 +1,4 @@
-def sort arr
+def sort(arr)
     rec_sort arr, [] #Gives the method rec_sort two arguments, one is the same argument given to "sort" (the array to sort), the other is an empty array in which to place the sorted array. These correspond to the arguments defined in the rec_sort method below, unsorted and sorted.
 end
 
@@ -7,20 +7,22 @@ def rec_sort unsorted, sorted
         return sorted
     end
     #if there is nothing in the unsorted array (nothing to sort) then return the sorted argument.
-    
-    smallest = unsorted.pop #the variable "smallest" contains the last element from the unsorted array. 
+
+    smallest = unsorted.pop #the variable "smallest" contains the last element from the unsorted array.
     still_unsorted = []
-    
+
     unsorted.each do |word|
         if word < smallest
             still_unsorted.push smallest
-            smallest = word 
+            smallest = word
         else
             still_unsorted.push word
         end
     end
-    
-    sorted.push smallest #push the smallest variable into the sorted array. 
-    
+
+    sorted.push smallest #push the smallest variable into the sorted array.
+
     rec_sort still_unsorted, sorted
 end
+
+puts sort([2, 5, 7, 3, 8])

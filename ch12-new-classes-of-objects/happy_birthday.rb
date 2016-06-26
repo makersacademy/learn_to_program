e233a9ee3c9fb3482e931bb929e33517ff2eb644
Frunez <francesco.nunez@gmail.com>
2016-06-26 19:23:03 +0100

    a spank a year

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,4 +1,12 @@
 # your code here
-diff =  Time.new - Time.local(1993, 12, 11)
-age = diff/60/60/24/365
-puts age
+puts 'what year were you born?'
+year = gets.chomp.to_i
+puts 'what month?'
+month = gets.chomp.to_i
+puts 'and the day?'
+day = gets.chomp.to_i
+birthday = Time.local(year, month, day)
+diff =  Time.new - birthday
+age = diff.to_i/60/60/24/365
+puts "you are #{age} years old"
+age.to_i.times {|s| puts 'SPANK!'}

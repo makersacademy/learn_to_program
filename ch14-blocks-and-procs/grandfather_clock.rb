b4ca7b4b5d3f65cf17a64ae7e5b957517654b9dc
Frunez <francesco.nunez@gmail.com>
2016-06-27 04:04:42 +0100

    finished finally

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,20 @@
 def grandfather_clock &block
   # your code here
-end
+  time = Time.new.hour
+  
+  if time > 12
+  	time -= 12
+  elsif time == 0
+  	time += 12
+  end
+  time.times {block.call}
+end 
+
+grandfather_clock {puts 'DONG!'}
+
+puts "  _   _"
+puts '_/0\_/0\_'
+puts ' \_/ \_/ '
+puts '   ___   '
+puts '  /___\  '
+puts '   ===   '

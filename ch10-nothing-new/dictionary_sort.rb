71d90ca77cdee01dce0072b52a7fdd43f90db42d
Zak Richardson <Zak@Zaks-MacBook-Pro.local>
2016-06-28 10:49:41 +0100

    Add more completed programs

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,40 @@
 def dictionary_sort arr
-  # your code here
+    #
+    #I copied over my sort algorithm, only change needed would be to
+    #compare downcase versions of the elements to ensure that capital
+    #letters are not affecting the sort
+    #also you would need to recursively call THIS method
+    #
+    #so the aim of this seems to be to make a sorting algorithm (without just .sort)
+    # and i guess to use some recursion
+    # dont need to sort an array with 1 element
+    if arr.length <= 1
+      return arr
+    else
+      #since its bigger than one, time to get sorting
+      #step 1 pick a pivot
+      piv = arr.sample
+      #.sample == Choose a random element or n random elements from the array.
+      # ruby docs OP
+      arr.delete_at(arr.index(piv)) #delete pivot from array
+      less = []
+      greater = []
+      #loop through arr
+      #assign each value of arr to less or greater arrays
+      #depending on relation to the pivot
+      arr.each do |x|
+        if x.downcase <= piv.downcase
+          less << x
+        else
+          greater << x
+        end
+      end
+      #recursion!
+      sorted_array = []
+      sorted_array << self.dictionary_sort(less)
+      sorted_array << pivot
+      sorted_array << self.dictionary_sort(greater)
+
+      sorted_array.flatten!
+
 end
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+#first prompt user for inputs and store them
+puts "What year were you born?"
+user_year = gets.chomp.to_i
+puts "What month were you born?"
+user_month = gets.chomp.to_i
+puts "What day of the month were you born?"
+user_day = gets.chomp.to_i
+
+user_birth = Time.gm(user_year, user_month, user_day)
+age_diff = Time.new - user_birth
+age = (age_diff / 31536000).floor
+puts "#{age}!\n"
+puts "SPANK!\n" * age

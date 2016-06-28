79c05a832d2479093c769664c8e512792192749d
6eff <6eff@mail.ru>
2016-06-28 16:32:52 +0300

    spec file for ch10 shuffle missing require 'set'

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,17 +1,17 @@
 #my original code, failed CI
-# def shuffle arr
-#   arr.sort{|x, y| rand<=>rand}
-# end
+def shuffle arr
+  arr.sort{|x, y| rand<=>rand}
+end
 # def shuffle arr
 #   arr.shuffle
 # end
-def shuffle(arr)
-  random_shuffle arr, []
-end
-
-def random_shuffle(unshuffled_arr, shuffled_arr)
-  unshuffled_arr.count.downto(1) do |n|
-    shuffled_arr.push unshuffled_arr.delete_at(rand(n))
-  end
-  return shuffled_arr
-end
+# def shuffle(arr)
+#   random_shuffle arr, []
+# end
+#
+# def random_shuffle(unshuffled_arr, shuffled_arr)
+#   unshuffled_arr.count.downto(1) do |n|
+#     shuffled_arr.push unshuffled_arr.delete_at(rand(n))
+#   end
+#   return shuffled_arr
+# end

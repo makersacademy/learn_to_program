a7ddaa552f9b257506fd09cdc4432f0c44d69280
6eff <6eff@mail.ru>
2016-06-28 16:12:00 +0300

    shuffle again, to pass CI

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -2,6 +2,16 @@
 # def shuffle arr
 #   arr.sort{|x, y| rand<=>rand}
 # end
-def shuffle arr
-  arr.shuffle
+# def shuffle arr
+#   arr.shuffle
+# end
+def shuffle(arr)
+  random_shuffle arr, []
+end
+
+def random_shuffle(unshuffled_arr, shuffled_arr)
+  unshuffled_arr.count.downto(1) do |n|
+    shuffled_arr.push unshuffled_arr.delete_at(rand(n))
+  end
+  return shuffled_arr
 end

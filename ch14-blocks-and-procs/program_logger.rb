e18d9d5cb1b6d9f54eec649e45fd84bb338d557f
JulieLev <jkgee1@yahoo.co.uk>
2016-06-29 22:02:18 +0100

    Added program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,17 @@
 def log desc, &block
-  # your code here
+  puts "The block #{desc} is starting now..."
+    returned = block.call
+    puts "Block #{desc} has run, returning: #{returned.to_s}"
+    false ? "False" : "True"
+  end
+
+log 'envelope block' do
+
+  log 'square 7' do
+    7 * 7
+  end
+
+  log 'hour in France' do
+    "The hour in France now is #{Time.new.hour + 1} in 24 hour clock time."
+  end
 end

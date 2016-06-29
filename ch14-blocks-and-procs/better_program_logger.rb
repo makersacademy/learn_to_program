f3bbc82d461a06b7223148116a6b25c6ef86dc93
JulieLev <jkgee1@yahoo.co.uk>
2016-06-29 22:20:22 +0100

    Adding better_program_logger.rb, has counter but not formatted output yet

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
+$log_counter = 0
+
 def log desc, &block
-  # your code here
+  puts "The block #{desc} is starting now..."
+    returned = block.call
+    puts "Block #{desc} has run, returning: #{returned.to_s}"
+    $log_counter += 1
+    puts "$log_counter = #{$log_counter}"
+    false ? "False" : "True"
+  end
+
+log 'envelope block' do
+
+  log 'square 7' do
+    7 * 7
+  end
+
+  log 'hour in France' do
+    "The hour in France now is #{Time.new.hour + 1} in 24 hour clock time."
+  end
 end

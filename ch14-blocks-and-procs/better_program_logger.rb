a48cb0d0e13513af8a9112e5c593ffa127f6ce07
Luke <luke.webster86@gmail.com>
2016-06-30 13:26:42 +0000

    Better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,13 @@
-def log desc, &block
-  # your code here
+$nested_level = 0
+
+def better_log desc, &block
+  prefix = " " * $nested_level
+  
+  $nested_level += 1
+  puts prefix  +  "Beginning \"#{desc}\"..."
+
+  result = block.call
+
+  $nested_level -= 1
+  puts prefix  + "...\"#{desc}\" finished, returning: #{result}"
 end

239a4d78371a49ce235a20030922e5569beb8059
JulieLev <jkgee1@yahoo.co.uk>
2016-07-01 12:43:17 +0100

    extend_built_in_classes passed to fix (used factorial instead of fact)

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,20 +1,31 @@
 class Integer
- def fact
-    num = self
+ 
+ def factorial
     
-    if num < 0
+    if self < 0
         return 'You can\'t take the factorial of a negative number!'
-    elsif num == 1 
+    elsif self == 1 
         return 1
     end
         
-    num = num * (num-1).fact
+    self * (self-1).factorial
     
-    return num    
  end
-end
 
-puts 4.fact
-puts 2.fact
-puts 20.fact
+
+def to_roman
+    # I chose old-school roman numerals just to save space.
+    roman = ''
+
+    roman = roman + 'M' * (self / 1000)
+    roman = roman + 'D' * (self % 1000 / 500)
+    roman = roman + 'C' * (self % 500 / 100)
+    roman = roman + 'L' * (self % 100 / 50)
+    roman = roman + 'X' * (self % 50 / 10)
+    roman = roman + 'V' * (self % 10 / 5)
+    roman = roman + 'I' * (self % 5 / 1)
+
+    roman
+  end
+end
 

1021d03ab6b01500f184f95b37edbda5b5bf59ef
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-02 20:05:20 +0200

    Exercise 9,2 corrected

--- chapter_9,2.rb
+++ chapter_9,2.rb
@@ -1,92 +1,19 @@
-def old_school_numeral (x)
-	if x < 0
-		return "Please enter a number greater than zero"
-	end 
-	if x > 30000
-		"Please enter a number smaller than 3000"
-	end 
-	roman_numeral = ""
-	rest = x 
-	write = rest/1000
-	rest = rest%1000
-	if write > 0 
-		if write == 1 
-			roman_numeral = roman_numeral + "M"
-		elsif write == 2
-			roman_numeral = roman_numeral + "MM"
-		elsif write == 3 
-			roman_numeral = roman_numeral + "MMM"
-		end 
-	end 
-	write = rest/500
-	rest = rest%500
-	if write > 0 
-		roman_numeral = roman_numeral + "D"
-	end 
-	write = rest/100
-	rest = rest%50
-	if write > 0 
-		if write == 1 
-			roman_numeral = roman_numeral + "C"
-		elsif write == 2
-			roman_numeral = roman_numeral + "CC"
-		elsif write == 3
-			roman_numeral = roman_numeral + "CCC"
-		elsif write == 4
-			roman_numeral = roman_numeral + "CCCC"
-		end 
-	
-	else 
-		roman_numeral = roman_numeral +""
-	end 
-	write = rest/50
-	rest = rest%50
-	if write > 0 
-		roman_numeral = roman_numeral + "L"
-	
-	else
-		roman_numeral = roman_numeral +""
-	end 
-	write = rest/10 
-	rest = rest%10
-	if write > 0 
-		if write == 1 
-			roman_numeral = roman_numeral + "X"
-		elsif write == 2
-			roman_numeral = roman_numeral + "XX"
-		elsif write == 3
-			roman_numeral = roman_numeral + "XXX"
-		elsif write == 4
-			roman_numeral = roman_numeral + "XXXX"
-		end 
-	
-	else 
-		roman_numeral = roman_numeral + ""
-	end 
-	write = rest/5
-	rest = rest%5
-	if write > 0 
-		roman_numeral = roman_numeral + "V"
-	
-	else 
-		roman_numeral = roman_numeral +""
-	end 
-	write = rest 
-	if write > 0 
-		if write == 1 
-			roman_numeral = roman_numeral + "I"
-		elsif write == 2
-			roman_numeral = roman_numeral + "II"
-		elsif write == 3
-			roman_numeral = roman_numeral + "III"
-		elsif write == 4
-			roman_numeral = roman_numeral + "IIII"
-		end 
-	else 
-		roman_numeral = roman_numeral +""
-	end 
-end 
-puts old_school_numeral(781)
+def old_roman_numeral (num )
+  roman = ""
+
+  roman = roman + "M" * (num / 1000) #how many M's
+  roman = roman + "D" * (num % 1000 / 500) #how many D's remain after M's done 
+  roman = roman + "C" *(num % 500 / 100) # how many C's after D's done etc
+  roman = roman + "L" *(num % 100 / 50) 
+  roman = roman + "X" *(num % 50 / 10) 
+  roman = roman + "V" *(num % 10 / 5) 
+  roman = roman + "I" *(num % 5 / 1) 
+  roman
+  # you can multiply a string with an integer n . To repeat the string n times.
+
+end
+puts old_roman_numeral(1481)
+
 
 
 

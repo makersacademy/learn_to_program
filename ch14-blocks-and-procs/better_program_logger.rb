cd6d1ea187b7977f83b9180c43741f6a11f965d3
Adam <adamjbrowner@mgmail.com>
2016-07-03 21:40:58 +0100

    Edited

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,9 +1,9 @@
 $logger_depth = 0
 
-def log desc, &block
+def better_log desc, &block
   prefix = " "*$logger_depth
 
-  puts prefix + "Beginning \"#{desc}\" ..."
+  puts prefix + "Beginning \"#{desc}\"..."
 
   $logger_depth += 1
 
@@ -12,16 +12,16 @@ def log desc, &block
   puts prefix + "...\"#{desc}\" finished, returning: " + result.to_s
 end
 
-log "outer block" do
-  log "some little block" do
-    log "teeny-tiny block" do
+better_log "outer block" do
+  better_log "some little block" do
+    better_log "teeny-tiny block" do
       "lOtS oF LoVE".downcase
     end
 
     7 * 3 * 2
   end
 
-  log "yet another block" do
+  better_log "yet another block" do
     "!doof naidnI evol I".reverse
   end
 

9caa0c10258cf44271349efb9bc36b1e6919ac57
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-04 00:20:05 +0200

    old_school_roman_numerals.rb final

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,24 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral (num )
+  roman = ""
+
+  roman = roman + "M" * (num / 1000) #how many M's
+  roman = roman + "D" * (num % 1000 / 500) #how many D's remain after M's done 
+  roman = roman + "C" *(num % 500 / 100) # how many C's after D's done etc
+  roman = roman + "L" *(num % 100 / 50) 
+  roman = roman + "X" *(num % 50 / 10) 
+  roman = roman + "V" *(num % 10 / 5) 
+  roman = roman + "I" *(num % 5 / 1) 
+  roman
+  # you can multiply a string with an integer n . To repeat the string n times.
+
 end
+puts old_roman_numeral(1481)
+
+
+
+
+
+
+
+			
+

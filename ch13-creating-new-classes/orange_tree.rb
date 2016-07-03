9711c9105a9e031bb1a61c5c5fdc6efdcf43b9c9
cyberplanner <saleh.gfx@gmail.com>
2016-07-03 22:19:27 +0100

    Fixed orange_tree.rb

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -33,17 +33,17 @@ class OrangeTree
     if @alive
       @height = @height + 0.4
       @orange_count = 0 # old oranges fall off
-      if @height > 10 && rand(2) > 0
+      if @height >= 10.4
         # tree dies
         @alive = false
         'Oh, no! The tree is too old, and has died. :('
       elsif @height > 2
         # new oranges grow
         @orange_count = (@height * 15 - 25).to_i
-        "This year your tree grew to #{@height.round(1)}m tall," +
+        "This year your tree grew to #{@height.round(1)}" + "m tall," +
           " and produced #{@orange_count} oranges."
       else
-        "This year your tree grew to #{@height.round(1)}m tall," +
+        "This year your tree grew to #{@height.round(1)}" + "m tall," +
           " but is still too young to bear fruit."
       end
     else
@@ -65,19 +65,22 @@ class OrangeTree
   end
 end
 
-# ot = OrangeTree.new
-# 23.times do
-#   ot.one_year_passes
-# end
+=begin
 
-# puts(ot.one_year_passes)
-# puts(ot.count_the_oranges)
-# puts(ot.height)
-# puts(ot.one_year_passes)
-# puts(ot.one_year_passes)
-# puts(ot.one_year_passes)
-# puts(ot.one_year_passes)
-# puts(ot.one_year_passes)
-# puts(ot.height)
-# puts(ot.count_the_oranges)
-# puts(ot.pick_an_orange)
+ ot = OrangeTree.new
+ 23.times do
+   ot.one_year_passes
+ end
+
+ puts(ot.one_year_passes)
+ puts(ot.count_the_oranges)
+ puts(ot.height)
+ puts(ot.one_year_passes)
+ puts(ot.one_year_passes)
+ puts(ot.one_year_passes)
+ puts(ot.one_year_passes)
+ puts(ot.one_year_passes)
+ puts(ot.height)
+ puts(ot.count_the_oranges)
+ puts(ot.pick_an_orange)
+=end

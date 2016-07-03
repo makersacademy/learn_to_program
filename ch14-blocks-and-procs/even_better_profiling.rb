ae0a32d6c6c5a5e7d5fc276779ea469328583e0a
cyberplanner <saleh.gfx@gmail.com>
2016-07-03 22:58:28 +0100

    More typos in even_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,14 +1,15 @@
 def profile block_description, &block
   def profile block_description, &block
-  # To turn profiling on/off, set this
-  # to true/false.
-  profiling_on = false
-  if profiling_on
-    start_time = Time.new
-    block.call
-    duration = Time.new - start_time
-    puts "#{block_description}: #{duration} seconds"
-  else
-    block.call
+    # To turn profiling on/off, set this
+    # to true/false.
+    profiling_on = false
+    if profiling_on
+      start_time = Time.new
+      block.call
+      duration = Time.new - start_time
+      puts "#{block_description}: #{duration} seconds"
+    else
+      block.call
+    end
   end
 end

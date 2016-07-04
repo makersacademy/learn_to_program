7dfb4fc7826ed694a5d7937ae0c2bcf4c1226b5a
Sara <sarah.alalaya@gmail.com>
2016-07-04 10:19:31 +0300

    ch14 Ex.

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,10 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour
+
+  hour -= 12 if hour >= 13
+  hour  = 12 if hour == 0
+
+  hour.times {block.call}
 end
+
+  grandfather_clock {puts "Dong!"}

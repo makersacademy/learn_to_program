51f6a93c10c4d74df45d7788db3b91b2250eba23
Tom Stuart <thomascstuart@gmail.com>
2016-07-07 18:18:41 +0100

    Completed Grandfather Clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,6 @@
 def grandfather_clock &block
-  # your code here
+
+	(Time.new.hour%12).times{yield}			#for each hour of the current time (Time.new.hour) expressed in 12h blocks yield to the block
+
+
 end

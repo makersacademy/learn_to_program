fcffd94d1664a37486b75755ae85a10cff130f5b
Tom Stuart <thomascstuart@gmail.com>
2016-07-07 17:29:57 +0100

    Completed Orange Tree Class

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,5 +7,53 @@
 
 
 class OrangeTree
-  # your code here
+
+def initialize 
+	@height = 0.0
+	@fruit = 0
+	@age = 0
+	@alive = true
 end
+
+def one_year_passes
+	if @age <= 5
+		@age+=1
+		@height+=0.4
+	
+	elsif @age == 25
+		@alive=false
+		@age+=1
+		'Oh, no! The tree is too old, and has died. :('
+		
+	elsif @alive == false
+		'A year later, the tree is still dead. :('
+	
+	else
+	 	@age+=1
+		@height+=0.4
+		@fruit=(@height * 15 - 25).floor
+		return "This year your tree grew to #{@height.round(2)}m tall, and produced #{@fruit.round(0)} oranges."
+	end
+end
+	
+def height
+	@alive == false ? 'A dead tree is not very tall. :(' : @height.round(2)
+end
+
+def pick_an_orange
+	if @alive == false 
+		'A dead tree has nothing to pick. :('
+	elsif @fruit > 1 	
+		@fruit-=1
+		' You pick a juicy orange'
+	else
+		' There is no fruit left'
+	end
+end
+
+def count_the_oranges
+	@alive == false ? 'A dead tree has no oranges. :(' : @fruit
+end
+
+end
+

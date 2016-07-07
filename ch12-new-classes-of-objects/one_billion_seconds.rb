304c8e6a6aa2cc318f0dd27af38552a4f24dabf1
Tom Stuart <thomascstuart@gmail.com>
2016-07-07 16:00:25 +0100

    Completed One Billion Seconds

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,4 @@
-# your code here
+Born=DateTime.new(1992,8,2,6,34,59)
+Billion=Born + Rational(1000000000, 86400)
+
+puts "I will be one billion seconds old on the #{Billion.to_date.to_s.chomp} at #{Billion.to_time.strftime "%M:%S"}"

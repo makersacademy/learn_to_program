aba58b7a6d27e54ba3df1d1104ba4cf663748acb
Tom Stuart <thomascstuart@gmail.com>
2016-07-07 16:00:06 +0100

    Completed Happy Birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,9 @@
-# your code here
+puts "Enter your birthday in the form dd-mm-yyyy"
+input=gets.chomp
+
+arr=input.split("-")
+born=Date.new(arr[2],arr[1],arr[0])
+age=(Date.today.mjd - born.mjd)/365
+
+age.times {Puts "Happy Birthday!"}
+

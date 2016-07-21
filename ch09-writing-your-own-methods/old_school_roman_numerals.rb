5046ec4a9df68cde83926a92024d829ac948c2a8
Matthew Perkins <mfperkins@gmail.com>
2016-07-21 14:03:16 +0100

    Updated Old School code

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,17 @@
+#!/usr/bin/env ruby
+
 def old_roman_numeral num
-  # your code here
+
+  results = ''
+  results = results + 'M' * (num / 1000)
+  results = results + 'D' * (num % 1000 / 500)
+  results = results + 'C' * (num % 500 / 100)
+  results = results + 'L' * (num % 100 / 50)
+  results = results + 'X' * (num % 50 / 10)
+  results = results + 'V' * (num % 10 / 5)
+  results = results + 'I' * (num % 5)
+  results
+
 end
+
+puts(old_roman_numeral(1999))

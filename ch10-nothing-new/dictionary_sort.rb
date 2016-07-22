4dcf649974163414242db2bd1c89e1ce5b92dc5f
Matthew Perkins <mfperkins@gmail.com>
2016-07-22 11:22:26 +0100

    CH10 Ex3

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,30 @@
 def dictionary_sort arr
-  # your code here
+  recursiveSort arr, []
 end
+
+def recursiveSort unsortedArr, sortedArr
+
+  if unsortedArr.length <= 0
+    return sortedArr
+  end
+
+  item = unsortedArr.pop
+  toSort = []
+
+  unsortedArr.each do |x|
+    if x.downcase < item.downcase
+      toSort.push item
+      item = x
+    else
+      toSort.push x
+    end
+  end
+  sortedArr.push item
+
+  recursiveSort toSort, sortedArr
+
+end
+
+myArr = ["hello","goodbye", "dog", "xenophobic", "Ruby","London", "magic"]
+
+puts(dictionary_sort(myArr))

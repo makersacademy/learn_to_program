59ae7e08ffb57d197a3e46d9bf7a59556af85c58
lsewilson <laurawilson@hotmail.co.uk>
2016-07-23 18:26:28 +0100

    Ch9 Roman Numerals submission

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,26 @@
 def roman_numeral num
-  # your code here
+  numerals = {
+    1    => "I",
+    4    => "IV",
+    5    => "V",
+    9    => "IX",
+    10   => "X",
+    40   => "XL",
+    50   => "L",
+    90   => "XC",
+    100  => "C",
+    400  => "CD",
+    500  => "D",
+    900  => "CM",
+    1000 => "M"
+  }
+
+    roman_num = ''
+      numerals.reverse_each do |k, v|
+        while num >= k
+          num -= k
+          roman_num += v
+        end
+      end
+    roman_num
 end

f2c60052d0a68d6d45c11175863f13f04b5bde80
Peter Miklos <pt.miklos@gmail.com>
2016-07-23 10:56:55 +0100

    Add Roman numerals related files

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+  num_roman =""
+  roman_a = [[1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]
+  roman_a.each { |elem| num_roman, num = num_roman + elem[1] * (num / elem[0]), num % elem[0] }
+  num_roman
 end
+
+print old_roman_numeral(2999)
+puts ""
+print old_roman_numeral(1765)
+puts ""
+print old_roman_numeral(333)
+puts ""
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,14 @@
 def roman_numeral num
-  # your code here
+  num_roman =""
+  roman_a = [[1000, "M"], [900, "CM"], [500, "D"], [100, "C"], [90, "XC"],
+  [50, "L"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+  roman_a.each { |elem| num_roman, num = num_roman + elem[1] * (num / elem[0]), num % elem[0] }
+  num_roman
 end
+
+print roman_numeral(2999)
+puts ""
+print roman_numeral(1765)
+puts ""
+print roman_numeral(333)
+puts ""

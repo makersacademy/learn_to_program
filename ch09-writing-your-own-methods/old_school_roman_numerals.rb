9c53c0dddbd73ace75def1987078bec247b07436
lsewilson <laurawilson@hotmail.co.uk>
2016-07-23 18:25:17 +0100

    Ch 9 old school roman numerals submission

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
-def old_roman_numeral num
-  # your code here
+def roman_numeral num
+  numerals = {
+    1    => "I",
+    5    => "V",
+    10   => "X",
+    50   => "L",
+    100  => "C",
+    500  => "D",
+    1000 => "M"
+  }
+    roman_num = ""
+      numerals.reverse_each do |k, v|
+        while num >= k
+          num -= k
+          roman_num += v
+        end
+      end
+    roman_num
 end

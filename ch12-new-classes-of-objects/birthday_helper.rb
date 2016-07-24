f16dffdb4f6d2550fb9ce5411319cb14f2ba1755
Peter Miklos <pt.miklos@gmail.com>
2016-07-24 22:07:07 +0100

    Add completed exercises of chapter 12 and changes to 99 bottles exercise

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,28 @@
-# your code here
+def birtdate(name)
+  filename = 'birthdate_input.txt'; birth_dates = Hash.new #store name and birthdate
+  File.open filename, 'r' do |f| #open the input File
+    f.each_line do |line| #read all lines from file
+      line_a = line.split(",") #line is splitted at colons
+      #elem at position 1 needs to be splitted to day and month
+      line_a.push(line_a[1].split[0], line_a[1].split[1]); line_a.delete_at(1)
+      birth_name = line_a[0].downcase
+      birthdate = Time.new(line_a[1].to_i, line_a[2], line_a[3].to_i)
+      birth_dates[birth_name] = birthdate #name and birthdate are added to the hash
+    end
+  end
+  birth_dates[name]
+end
+
+loop do
+  print "Please add a name to check (exit to quit): "
+  name_to_check = gets.chomp #prompt user to add a name
+  break if name_to_check == "exit" #if exit is written
+  #birtdate received from the defined method
+  b_day = birtdate(name_to_check.downcase)
+  if b_day == nil then puts "#{name_to_check} not found! Try again."
+  else
+    puts "#{name_to_check}'s date of birth is #{b_day.day}-#{b_day.month}-" +
+    "#{b_day.year} and he/she is #{Time.new.year - b_day.year} year(s) old."
+    break
+  end
+end
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,18 @@
 def roman_to_integer roman
-  # your code here
+  return 0 if roman.empty?
+  #set a hash for storing values of roman numbers
+  rh = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000}
+  #create array with reversed roman numbers in uppercase
+  roman_a = roman.upcase.each_char.to_a.reverse
+  pos, final_number = 0, 0
+  roman_a.each do |elem|
+    if pos == 0 then final_number = rh[roman_a[pos]] #store value of first elem
+    else
+      #if value of previous elem > value of current elem, the current elem needs
+      #to be deducted from sum, otherwise value of current elem needs to be added to sum
+      rh[roman_a[pos - 1]] > rh[elem] ? final_number -= rh[elem] : final_number += rh[elem]
+    end
+    pos += 1
+  end
+  final_number
 end

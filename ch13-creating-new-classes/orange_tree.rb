d2bb004ef44086a754c07fb739dca10e920df0ae
Matthew Perkins <mfperkins@gmail.com>
2016-07-24 17:39:47 +0100

    Ch13 Ex2

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,11 +1,55 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
-
 class OrangeTree
-  # your code here
+
+  def initialize
+    @age = 0
+    @growth_rate = 0.4
+    @maturity = 5
+    @death = 26
+    @height = 0
+    @orange_count = 0
+  end
+
+  def height
+    if @age < @death
+      return @height.round(2)
+    else
+      return "A dead tree is not very tall. :("
+    end
+  end
+
+  def one_year_passes  # when you call it, the tree ages by one year
+    @age += 1
+    @height += @growth_rate
+    if @age < 5
+      return "This year your tree grew to #{@height.round(2)}, but your tree isn't #{@maturity.to_i} years old yet, so there aren't any fruit! Your tree will have fruit in #{@maturity - @age} years."
+    elsif @age == @death
+      return "Oh, no! The tree is too old, and has died. :("
+    elsif @age > @death
+      return "A year later, the tree is still dead. :("
+    else
+      @orange_count = @height * 15 - 25
+      return "This year your tree grew to #{@height.round(2)}m tall, and produced #{@orange_count.to_i} oranges."
+    end
+  end
+
+  def count_the_oranges
+    if @age < @death
+      return @orange_count.to_i
+    else
+      return "A dead tree has no oranges. :("
+    end
+  end
+
+  def pick_an_orange
+    # reduces @orange_count by 1
+    if (@age < @death) & (@orange_count > 1)
+      @orange_count -= 1
+      return "Mmmmhhh, that was delicious. You have #{@orange_count.to_i} oranges left." # returns string telling you how the orange was
+    elsif (@age < @death) & (@orange_count < 1) # else tells you that there are no more oranges to pick this year
+      return "Your tree is not yet #{@maturity}, so there aren't any fruit! Come back soon."
+    else
+      return "A dead tree has nothing to pick. :("
+    end
+  end
+
 end

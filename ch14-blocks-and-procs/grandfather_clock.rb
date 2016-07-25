d430d43e2b044ef855ce839249060eb3c45ddee0
Matthew Perkins <mfperkins@gmail.com>
2016-07-25 12:58:36 +0100

    Ch14 Ex03 - works from command line. will go back and fix for RSPEC in due course.

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,23 @@
 def grandfather_clock &block
-  # your code here
+
+  start = Time.new.hour
+
+  if start == 0
+    12.times do
+      block.call
+    end
+  elsif start > 12
+    (start-12).times do
+      block.call
+    end
+  else
+    start.times do
+      block.call
+    end
+  end
+
+end
+
+grandfather_clock do
+  puts "DONG!"
 end

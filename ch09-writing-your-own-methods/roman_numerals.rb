ceba5cdf8fb4c6f1d3a9ce80357914686b93d828
Ewan Sheldon <ewansheldon@gmail.com>
2016-07-25 15:33:06 +0100

    ch09 ex3 completed

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,35 @@
 def roman_numeral num
-  # your code here
+  if num <= 0
+    puts 'Please enter a positive integer'
+  elsif num >= 4000
+    puts 'Please enter a positive integer less than 4000'
+  else
+    roman = ""
+    roman << "M" * (num / 1000)
+    if num % 1000 / 100 == 9
+      roman << "CM"
+    elsif num % 1000 / 100 == 4
+      roman << "CD"
+    else
+    roman << "D" * (num % 1000 / 500)
+    roman << "C" * (num % 500 / 100)
+    end
+    if num % 100 / 10 == 9
+      roman << "XC"
+    elsif num % 100 / 10 == 4
+      roman << "XL"
+    else
+    roman << "L" * (num % 100 / 50)
+    roman << "X" * (num % 50 / 10)
+    end
+    if num % 10 == 9
+      roman << "IX"
+    elsif num % 10 == 4
+      roman << "IV"
+    else
+    roman << "V" * (num % 10 / 5)
+    roman << "I" * (num % 5)
+    end
+    return roman
+  end
 end

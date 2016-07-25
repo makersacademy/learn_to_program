9c2fee467df089c566c916a35c1f37917b1f3416
Ewan Sheldon <ewansheldon@gmail.com>
2016-07-25 15:01:03 +0100

    ch09 ex1 recompleted with corrected errors

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,14 +1,17 @@
 def old_roman_numeral num
-  puts 'Please enter a positive integer' if num <= 0
-  puts 'Please enter a positive integer less than 5000' if num >= 5000
-  roman = ""
-  roman << "M" * (num / 1000)
-  roman << "D" * (num % 1000 / 500)
-  roman << "C" * (num % 500 / 100)
-  roman << "L" * (num % 100 / 50)
-  roman << "X" * (num % 50 / 10)
-  roman << "V" * (num % 10 / 5)
-  roman << "I" * (num % 5)
-
-  puts roman
+  if num <= 0
+    puts 'Please enter a positive integer'
+  elsif num >= 5000
+    puts 'Please enter a positive integer less than 5000'
+  else
+    roman = ""
+    roman << "M" * (num / 1000)
+    roman << "D" * (num % 1000 / 500)
+    roman << "C" * (num % 500 / 100)
+    roman << "L" * (num % 100 / 50)
+    roman << "X" * (num % 50 / 10)
+    roman << "V" * (num % 10 / 5)
+    roman << "I" * (num % 5)
+    puts roman
+  end
 end

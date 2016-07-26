789255b388178e03132bbc13120b10c1284d98ae
Tam Boine <tamborinehands@gmail.com>
2016-07-26 16:57:28 +0000

    hopefully these work

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
 def shuffle arr
-  # your code here
+	#This function should take an array and randomly rearrange the items 
+	#we need a way to move based on random ideas
+	new_Arr = []
+	until arr.length == 0
+		t = arr.length
+		r = rand(t)
+		n = arr.pop(arr[r])
+		new_Arr.push(n)
+	end
+puts new_Arr
 end
+
+shuffle [1,2,3,4,5,6,7,8,9]

5e104f4111a727ba5f218570206943a8928bd80b
Abigail <abigail.mcphillips@gmail.com>
2016-07-26 12:41:03 +0100

    Solution to Ch10 Ex2

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,14 @@
-def shuffle arr
-  # your code here
+def shuffle array
+  rec_shuffle array, []
 end
+
+def rec_shuffle unshuffled, shuffled
+  while unshuffled.length != shuffled.length
+    random_item = unshuffled.sample
+    unless shuffled.include?(random_item) then shuffled << random_item end
+    rec_shuffle unshuffled, shuffled
+  end
+  shuffled
+end
+
+shuffle [1,5,4,78]

e97520a90abb4bc628d16660ffd22134d8886651
MrJons <shad_stick@hotmail.co.uk>
2016-07-26 21:57:50 +0100

    altered asl.rb second entry.

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -3,16 +3,14 @@ def ask question
     puts question
     reply = gets.chomp.downcase
 
-    if (reply == "yes" || reply == "no")
-      if (reply == "yes")
-        answer = true
-      else
-        answer = false
-      end
-      break
-    else
-      puts "Please asnwer 'yes' or 'no'."
+    if reply == "yes"
+      answer = true
+    elsif reply == "no"
+      answer = false
     end
+    break
+
+    puts "Please answer with a 'yes' or 'no'."
   end
   answer
 end

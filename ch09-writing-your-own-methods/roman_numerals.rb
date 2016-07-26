1d5ddaebdb3e5bbb4efbf1718f1558ef5a52a7c3
Tam Boine <tamborinehands@gmail.com>
2016-07-26 10:41:49 +0000

    fixed roman_numerals for all exceptions

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,7 +1,7 @@
 #!/usr/local/rvm/rubies/ruby-2.3.0/bin/ruby
 def roman_numeral n
 	#this function should take an int n and convert it to a new roman numeral
-	nums = {'M'=>1000, 'D'=>500, 'C'=>100, 'L'=>50, 'X'=>10, 'V'=>5, 'I'=>1} #a hash of the building blocks for roman numerals
+	nums = {'M'=>1000, 'CM'=>900, 'D'=>500, 'CD'=>400, 'C'=>100, 'XC'=>90, 'L'=>50, 'XL'=>40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1 } #a hash of the building blocks for roman numerals
 	
 	answer = [] #initialize an empty array we'll push to later
 
@@ -13,12 +13,10 @@ def roman_numeral n
 
 	}
 ans = answer.join("") #converting array to one string
-ans = ans.sub(/VIIII/, "IX")#if ans has VIIII excahnge for IX 
-ans = ans.sub(/IIII/, "IV")# if ans has III exchange for IV 
 puts ans
 end 
 
 roman_numeral 6444 #test
-roman_numeral 1719 #test
+roman_numeral 999 #test
 
 

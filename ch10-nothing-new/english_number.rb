c68fd71e3fdc1f3bcaec9be5d6056f052761377e
Abigail <abigail.mcphillips@gmail.com>
2016-07-26 14:59:43 +0100

    Solution to Ch10 Ex4

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,75 @@
 def english_number number
-  # your code here
+  if number < 0
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+  num_string = ''
+  ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  million_plus = {6 => 'million', 9 => 'billion', 12 => 'trillion'}
+
+  left = number
+
+  while left > 999999
+  exp = (left.to_s.length-1) - (left.to_s.length-1)%3 # finding exponent of 10
+  write = left/10**exp # How many 10^exp left?
+  left = left - write*(10**exp) # Subtract those millions/billions/trillions
+    if write > 0
+      big_num = english_number write
+      num_string = num_string + big_num + " #{million_plus[exp]}"
+      if left > 0
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+  write = left/1000 # How many thousands left?
+  left = left - write*1000 # Subtract off those thousands.
+
+  if write > 0
+    thousands = english_number write
+    num_string = num_string + thousands + ' thousand'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+
+  write = left/100 # How many hundreds left?
+  left = left - write*100 # Subtract off those hundreds.
+
+  if write > 0
+    hundreds = english_number write
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      num_string = num_string + ' and '
+    end
+  end
+
+  write = left/10 # How many tens left?
+  left = left - write*10 # Subtract off those tens.
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
+
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left # How many ones left to write out?
+  left = 0 # Subtract off those ones.
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+  num_string
 end

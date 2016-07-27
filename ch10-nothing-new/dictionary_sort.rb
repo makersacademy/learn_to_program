952b121efa51d01960e5e1ef46697d6e71b8dc9f
Tim3tang <tim3tang@gmail.com>
2016-07-27 15:47:30 +0000

    Ch10 Learn to Program (but mostly copy and paste)

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,17 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+  (n-1).times do |i|
+    if array[i].downcase > array[i+1].downcase
+      array[i], array[i+1] = array[i+1], array[i]
+      swapped = true
+    end
+  end
+    break if not swapped
+  end
+  array
 end
+
+puts(dictionary_sort(['can','feel','singing.','like','A','can']))

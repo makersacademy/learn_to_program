f818f0c5d8a861c46246e41efa553b576fa5f09d
MrJons <shad_stick@hotmail.co.uk>
2016-07-27 20:55:16 +0100

    Old school numerals challenge.

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+  numerals = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1}
+  roman = ""
+
+    until num == 0 do
+      numerals.each do |key,value|
+        roman << (key * (num / value))
+        num = num % value
+      end
+    end
+
+  roman
 end

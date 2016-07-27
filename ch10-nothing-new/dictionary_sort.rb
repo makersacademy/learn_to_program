bc2017dc475c538688a4fcc9be224694cf3e957c
Stéphan Gaud <stephgoeseast@gmail.com>
2016-07-27 18:55:22 +0100

    chap 10 ex 3 (real one) - Dictionnary sorting

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,24 @@
 def dictionary_sort arr
-  # your code here
+  recursive arr, []
 end
+
+def recursive unsorted, sorted
+return sorted if unsorted == []
+
+  temp = unsorted.shift
+  transition = []
+
+  unsorted.each do |word|
+    if temp.downcase < word.downcase
+      transition << word
+    else
+      transition << temp
+      temp = word
+    end
+  end
+  sorted << temp
+  recursive transition, sorted
+end
+
+words = [ "Hello", "You", "zzz", "how", "are", "maison", "House" ]
+puts dictionary_sort words

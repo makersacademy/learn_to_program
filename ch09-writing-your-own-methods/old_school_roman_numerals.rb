da06e4f9a1bd7a18e0ec8a2a2e5f2ff9015698ee
Tim3tang <tim3tang@gmail.com>
2016-07-27 15:32:03 +0000

    merge changes again

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def old_roman_numeral num_roman
   letter_num = [["M", 1000], ["D", 500], ["C", 100], ["L", 50], ["X", 10], ["V", 5], ["I", 1]]
   out = ""
@@ -13,56 +12,3 @@ def old_roman_numeral num_roman
 end
 
 puts old_roman_numeral(2900)
-=======
-def roman number
-
-m_length = 0
-d_length = 0
-c_length = 0
-l_length = 0
-x_length = 0
-v_length = 0
-i_length = 0
-
-if number >= 1000
-m_length = number/1000
-number = number%1000
-end
-
-if number >= 500
-d_length = number/500
-number = number%500
-end
-
-if number >= 100
-c_length = number/100
-number = number%100
-end
-
-if number >= 50
-l_length = number/50
-number = number%50
-end
-
-if number >= 10
-x_length = number/10
-number = number%10
-end
-
-if number >= 5
-v_length = number/5
-number = number%5
-end
-
-if number < 5
-i_length = number/1
-number = number%10
-end
-
-puts 'M'*m_length + 'D'*d_length + 'C'*c_length + 'L'*l_length + 'X'*x_length +
-'V'*v_length + 'I'*i_length 
-
-end
-
-roman 2999
->>>>>>> 11673945d6bf849a1c90999e1382e105a8fe4577

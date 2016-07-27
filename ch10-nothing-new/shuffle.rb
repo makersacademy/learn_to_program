2bbb2c85ce7f02a2497082c35c0265428b4298f9
Tam Boine <tamborinehands@gmail.com>
2016-07-27 13:54:40 +0000

    only the english no. + reliants to do now, l8r

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,14 +1,4 @@
 def shuffle arr
-	#This function should take an array and randomly rearrange the items 
-	#we need a way to move based on random ideas
-	new_Arr = []
-	until arr.length == 0
-		t = arr.length
-		r = rand(t)
-		n = arr.pop(arr[r])
-		new_Arr.push(n)
-	end
-puts new_Arr
+	arr.sort_by{rand}
 end
-
-shuffle [1,2,3,4,5,6,7,8,9]
+puts(shuffle([1,2,3,4,5,6]))

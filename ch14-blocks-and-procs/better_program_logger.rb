ea0fe2319aa339ac28b8b44ec7b67303af8543cf
Peter Miklos <pt.miklos@gmail.com>
2016-07-28 13:34:44 +0100

    Update program_logger.rb and better_program_logger.rb in chapter 14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,21 @@
-def log desc, &block
-  # your code here
+$l_depth = 0
+
+def better_log desc, &block
+  tab = ' ' * $l_depth
+  puts "#{tab}Beginning \"#{desc}\"..."
+  $l_depth += 1 #add 1 to increase the tab space
+  target_v = block[] #call the received block
+  $l_depth -= 1 #deduct 1 to shorten the tab space
+  puts "#{tab}...\"#{desc}\" finished, returning: #{target_v.to_s}"
+end
+
+better_log("outer block") do
+  better_log("some little block") do
+    better_log("teeny-tiny block") { 'lOtS oF LOVE'.downcase }
+    7 * 3 * 2
+  end
+
+  better_log("yet another block") { "I love Indian food!" }
+
+  true
 end

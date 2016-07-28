0a2cf74d8e221defecc5f2877420a8ce4aea724f
Rosie Allott <rosie.allott@gmail.com>
2016-07-28 19:51:28 +0100

    Ch11 Build Playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "going to the destination of your music"
+Dir.chdir '/Users/rosieallott/Music/test'
+
+music_files = Dir['/Users/rosieallott/Music/test/**/music*.mp3']
+
+def shuffle(arr)
+  arr.sort_by{rand}
+end
+
+music_files = shuffle(music_files)
+
+filename = 'playlist.m3u'
+
+File.open filename, 'w' do |file|
+  file.write music_files
+end

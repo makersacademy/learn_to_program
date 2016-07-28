80a14b8761dff91311da642d0f19b0d6b6c8b52c
Rosie Allott <rosie.allott@gmail.com>
2016-07-28 19:31:48 +0100

    Ch11 Pic Downloads

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,35 @@
-# your code here
+# go into end destination of pictures
+puts "searching for the destination of your pictures"
+Dir.chdir '/Users/rosieallott/Pictures/NewDir'
+# find the pictures to be moved - effectively changes directory too?
+pic_names = Dir['/Users/rosieallott/Pictures/SourceDir/**/*.jpg']
+
+puts "What would you like to call this batch?"
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files: "
+# set and define counter
+pic_number = 1
+
+pic_names.each do |name|
+  print "." # progress bar
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  if File.exist? new_name
+    puts "ERROR ERROR - already exists"
+    exit
+  end
+
+  # renames and moves pictures (and deletes original apparently)
+  File.rename name, new_name
+  # increment counter
+  pic_number += 1
+end
+
+puts # so as not to be on progress bar line
+puts "Done"

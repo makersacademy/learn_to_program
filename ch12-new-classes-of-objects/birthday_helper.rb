35cb15b1bd55a932e0f0ee82bd9592c53daff356
James Stonehill <jamesstonehill@gmail.com>
2016-07-28 11:32:32 +0100

    done with more stuff

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,25 @@
-# your code here
+my_birthday = Time.new(1993,7,27)
+my_name = "James Stonehill"
+name_dob = {}
+
+def age(dob)
+    now = Time.now.utc
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+end
+
+File.read('birthdays.txt').each_line do |line|
+  line = line.chomp
+
+  name, date, year = line.split(',')
+
+  day_of_birth = /\d+/.match(date)
+  month_of_birth = /[a-zA-Z]+/.match(date)
+  year_of_birth = /\d{4}/.match(year)
+
+  day_of_birth = day_of_birth.to_s.to_i
+  month_of_birth = month_of_birth.to_s.to_i
+  year_of_birth = year_of_birth.to_s.to_i
+
+  dob2 = Time.new(year_of_birth, month_of_birth, day_of_birth)
+  puts dob2
+end
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,23 @@
-# your code here
+puts "What year were you born in?"
+birth_year = gets.chomp.to_i
+puts "What numer month were you born in? (e.x. 1st of April = 4 or 04)"
+birth_month = gets.chomp.to_i
+puts "What day were you born on (e.x. 1st of April = '1' or '01')"
+birth_day = gets.chomp.to_i
+
+birthday = Time.new(birth_year, birth_month, birth_day)
+
+puts birthday.strftime("You were born on %d/%m/%y")
+
+def age(dob)
+    now = Time.now.utc
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+end
+
+puts "that means that you are " + age(birthday).to_s + " years old."
+
+puts "Here are #{age(birthday)} spanks:"
+
+age(birthday).times do |x|
+  puts "SPANK!"
+end

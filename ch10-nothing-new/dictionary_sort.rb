c079ff8a70484dcd4863be9ba0e017b0eed2aad5
Rosie Allott <rosie.allott@gmail.com>
2016-07-28 16:22:34 +0100

    Ch10 Dictionary Sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,25 @@
 def dictionary_sort arr
-  # your code here
+  rec_dic_sort arr, []
 end
+
+def rec_dic_sort unsorted, sorted
+  return sorted if unsorted.length <=0
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |object|
+    if object.downcase < smallest.downcase
+      still_unsorted << smallest
+      smallest = object
+    else
+      still_unsorted << object
+    end
+  end
+
+  sorted << smallest
+  rec_dic_sort still_unsorted, sorted
+
+end
+
+puts dictionary_sort ["Tar", "rat", "Art"]

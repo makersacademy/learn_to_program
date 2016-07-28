3915a11d8092440acf93ef1058723a6a592022b2
Peter Miklos <pt.miklos@gmail.com>
2016-07-28 17:49:03 +0100

    Update 2 files in chapter 11: build_your_own_playlist.rb build_a_better_playlist.rb

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,22 +1,12 @@
+require_relative '../ch10-nothing-new/shuffle.rb'
+
 def better_playlist(filename)
   orig_mp3_files = Dir['**/*.mp3']
-  #shuffle the music files twice
-  mp3_files = music_shuffle(music_shuffle(orig_mp3_files))
+  #shuffle the music files twice by using the already existing shuffle method
+  mp3_files = shuffle(shuffle(orig_mp3_files))
   #open file and add the names of mp3 files one-by-one
   File.open filename, 'w' do |f|
     mp3_files.each { |track| f.puts track}
   end
 end
-
-def music_shuffle filenames
-  shuffled_array = []; n = filenames.length
-  n.times do
-    #randomly define a postion of an item in the array
-    position = rand(filenames.length)
-    #remove the item at a random position and put it into the shuffled_array
-    shuffled_array.push(filenames.delete_at(position))
-  end
-  shuffled_array
-end
-
 better_playlist('better_playlist.m3u')
--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,13 +1,9 @@
+require_relative '../ch10-nothing-new/shuffle.rb'
+
 def playlist(filename)
   orig_mp3_files = Dir['**/*.mp3']
-  mp3_files = []; n = orig_mp3_files.length
-  n.times do
-    #randomly define a postion of an item in the array
-    position = rand(orig_mp3_files.length)
-    #remove the item at a random position and put it into the shuffled_array
-    mp3_files.push(orig_mp3_files.delete_at(position))
-  end
-  #open file and add the names of mp3 files one-by-one
+  #shuffle the music files once by using the already existing shuffle method
+  mp3_files = shuffle(orig_mp3_files)
   File.open filename, 'w' do |f|
     mp3_files.each { |track| f.puts track}
   end

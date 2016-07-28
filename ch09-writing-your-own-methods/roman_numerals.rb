b8906ff631c9e4afa23524e546c10fb940825ec9
MrJons <shad_stick@hotmail.co.uk>
2016-07-28 22:06:08 +0100

    roman numerals untested

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,20 @@
 def roman_numeral num
-  # your code here
+  numerals = {"M" => 1000, "D" => 500, "C" => 100, "L" => 50,
+              "X" => 10, "V" => 5, "I" => 1}
+
+  replacements = {"DCCCC" => "CM", "LXXXX" => "XC", "VIIII" => "IX",
+                  "CCCC" => "CD", "XXXX" => "XL", "IIII" => "IV"}
+
+  roman = ""
+
+    until num == 0 do
+      numerals.each do |key,value|
+        letters = (key * (num / value))
+        replacements.each{|k,v| letters == k ? letters = v : letters}
+        roman << letters
+        num = num % value
+      end
+    end
+
+  roman
 end

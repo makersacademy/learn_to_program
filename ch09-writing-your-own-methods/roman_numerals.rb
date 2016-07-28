c6a3d9b6941e02f907963d143a52ec1895f722e2
Anna Beddow <anna.beddow@gmail.com>
2016-07-28 15:54:12 +0100

    roman_numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,35 @@
-def roman_numeral num
-  # your code here
+def roman_numeral number
+	thousand = (num /1000)
+	hundred = (num % 1000 /100)
+	tens = (num % 100 /10)
+	ones = (num % 10)
+	
+	roman = 'M' * thousand
+	if hunds == 9
+		roman = roman +'CM'
+	elsif hunds == 4
+		roman = roman + 'CD'
+	else 
+		roman = roman + 'D' * (num % 1000 / 500)
+		roman = roman + 'D' * (num % 500 / 100)
+	end
+		if tens == 9
+		roman = roman +'XC'
+	elsif tens == 4
+		roman = roman + 'XL'
+	else 
+		roman = roman + 'L' * (num % 100 / 50)
+		roman = roman + 'X' * (num % 50 / 10)
+	end
+			if ones == 9
+		roman = roman +'IX'
+	elsif ones == 4
+		roman = roman + 'IV'
+	else 
+		roman = roman + 'V' * (num % 100 / 50)
+		roman = roman + 'I' * (num % 50 / 10)
+	end
+	
+	roman
 end
+puts (roman_numeral(769))

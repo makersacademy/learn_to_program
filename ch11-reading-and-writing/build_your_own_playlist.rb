8a6a7237742083bcc2e3c9b4f48b93ebdadae9c9
lsewilson <laurawilson@hotmail.co.uk>
2016-07-29 18:08:06 +0100

    Chapter 11 build your own playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,20 @@
-# your code here
+#Dir.chdir '/Users/laurawilson/newpics' #file where playlist will be stored
+def shuffle arr
+  return arr if arr.length <= 1
+  x = arr.sample
+  arr.delete(x)
+  [x] + shuffle(arr)
+end
+
+songlist = shuffle(Dir['/Users/laurawilson/Music/**/*.{mp3,MP3,m4a,M4A,wma,WMA}'])
+
+playlist = "Playlist.m3u"
+numberofsongs = 10
+
+songs = songlist.sample(numberofsongs)
+
+File.open playlist, 'w' do |f|
+  songs.each do |song|
+    f.puts(song)
+  end
+end

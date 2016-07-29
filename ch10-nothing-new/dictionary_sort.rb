2d94d6923a15a4ab7604d9c56d8e6361937ba5f9
Thao Vo <ttthao.vo@gmail.com>
2016-07-29 20:37:04 +0000

    ch10 dictionary sorting solution

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,20 @@
 def dictionary_sort arr
-  # your code here
+   has_changed = false   
+    
+    for i in 0...arr.length-1
+        if arr[i].downcase > arr[i+1].downcase
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+            has_changed = true
+        end            
+    end
+    
+    if has_changed == true
+        dictionary_sort arr
+    else
+        return arr
+    end
 end
+
+puts(dictionary_sort(["Beta", "alpha", "Telephone", "playground"]))

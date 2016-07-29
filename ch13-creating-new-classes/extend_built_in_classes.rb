dae88b482baf01c9c63545fe6dc13fc547b8c81d
Rosie Allott <rosie.allott@gmail.com>
2016-07-29 14:40:43 +0100

    Ch13 Extend Classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,10 @@
 class Integer
   # your code here
+  def factorial
+    return "needs to be positive" if self < 0
+    self <= 1 ? 1 : self * (self-1).factorial
+  end 
 end
+
+puts 5.factorial
+puts 20.factorial

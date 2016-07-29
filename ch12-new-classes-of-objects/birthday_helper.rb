392e9e90ffeb13b0bd0b4cc4d8ba3785049f952b
Rosie Allott <rosie.allott@gmail.com>
2016-07-29 14:18:07 +0100

    Ch12 Birthday Helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,13 @@
-# your code here
+hash = {}
+
+File.open 'names_dates.txt', 'r' do |f|
+  f.each_line do |line|
+    name, dob = line.chomp.split(",")
+    hash[name] = dob
+  end
+end
+
+puts "What name would you like"
+input_name = gets.chomp
+
+puts hash[input_name]

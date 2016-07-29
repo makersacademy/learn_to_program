b505b317be503a711ff78a5793cc1fbdacaa1397
Thao Vo <ttthao.vo@gmail.com>
2016-07-29 21:22:27 +0000

    modification to ch09 sort algorithm

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -3,25 +3,13 @@ def sort some_array
 end
 
 def recursive_sort unsorted_array, sorted_array
-   has_changed = false   # means nothing has changed after iteration
-    
-    for i in 0...unsorted_array.length-1
-        if unsorted_array[i].downcase > unsorted_array[i+1].downcase
-            temp = unsorted_array[i]
-            unsorted_array[i] = unsorted_array[i+1]
-            unsorted_array[i+1] = temp
-            has_changed = true
-        end            
-    end
-    
-    if has_changed == true
+    if unsorted_array.length > 0
+        smallest = unsorted_array.min
+        unsorted_array.delete(smallest)
+        sorted_array << smallest
         recursive_sort unsorted_array, sorted_array
-    else
-        sorted_array = unsorted_array
-        return sorted_array
     end
+    return sorted_array
 end
-
-
 
 puts(sort(["laugh", "Apple", "sail", "Pear"]))

9d68273d0f49da042b1de265003049750972c322
Rosie Allott <rosie.allott@gmail.com>
2016-07-29 08:21:55 +0100

    Ch11 Better Playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,8 @@
 def music_shuffle filenames
-  # your code here
+  Dir.chdir '/Users/rosieallott/Music/test'
+  filenames = Dir['/Users/rosieallott/Music/test/**/music*.mp3']
+  music_files = filenames.map do |s|
+    [s,s.split('/')]
+  end
+  music_files = music_files.shuffle
 end

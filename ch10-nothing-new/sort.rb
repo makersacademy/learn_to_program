fe8608d889c2b4d8c4b22be372680d41a7e50034
Thao Vo <ttthao.vo@gmail.com>
2016-07-29 20:10:26 +0000

    ch10 sort algorithm recursion

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,27 @@
-def sort arr
-  # your code here
+def sort some_array 
+    recursive_sort some_array, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+   has_changed = false   # means nothing has changed after iteration
+    
+    for i in 0...unsorted_array.length-1
+        if unsorted_array[i].downcase > unsorted_array[i+1].downcase
+            temp = unsorted_array[i]
+            unsorted_array[i] = unsorted_array[i+1]
+            unsorted_array[i+1] = temp
+            has_changed = true
+        end            
+    end
+    
+    if has_changed == true
+        recursive_sort unsorted_array, sorted_array
+    else
+        sorted_array = unsorted_array
+        return sorted_array
+    end
+end
+
+
+
+puts(sort(["laugh", "Apple", "sail", "Pear"]))

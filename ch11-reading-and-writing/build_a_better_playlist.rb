c45dfc2a66f2f0c7738fc7224512acbc8159b1dc
Stéphan Gaud <stephgoeseast@gmail.com>
2016-07-29 21:19:54 +0100

    solution chap 11 ex 3 - Super Shuffle Playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,24 @@
-def music_shuffle filenames
-  # your code here
+# Using the shuffle method created in chap 10
+require_relative '../ch10-nothing-new/shuffle.rb'
+
+
+Dir.chdir "./music"
+music = Dir['../music/**/*.{mp3}']
+puts music
+
+shuffled_music = shuffle(music)
+super_shuffle = shuffle(shuffled_music)
+
+filename = "playlist.m3u"
+
+# Creating file
+File.open filename, "w" do |f|
+  # writing down tracks one by one
+  super_shuffle.each do |track|
+    f.puts track
+  end
 end
+
+read_string = File.read filename
+puts
+puts read_string

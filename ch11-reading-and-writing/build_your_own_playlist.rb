3210d0cd4592e18fe543ced2ce8f80298894ce58
Thao Vo <ttthao.vo@gmail.com>
2016-07-30 16:16:19 +0000

    ch11 my_playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,12 @@
-# your code here
+Dir.chdir 'D:/Pottomke/Music_repo'
+
+filename = "MyPlaylist.m3u"
+
+song_names = Dir['D:/Pottomke/Music_repo/*.{mp3}']
+
+File.open filename, 'w' do |f|
+	 song_names.each do
+		|song| f.write song
+		f.write "\n"
+	 end
+end

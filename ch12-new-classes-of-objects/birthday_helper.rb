7d97e850b2cb96f1f00205fc6a0b5965fc2538a6
Stéphan Gaud <stephgoeseast@gmail.com>
2016-07-30 18:05:16 +0100

    chap 12 ex 4 - Solution to Bday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,30 @@
-# your code here
+birthdays = {}
+
+ File.open('bday.txt').each_line do |line|
+ name, date, year = line.split", "
+ birthdays[name] = Time.gm(year,*(date.split' '))
+ end
+
+ puts "Please enter a name"
+ name = gets.chomp
+
+ if birthdays[name] == nil
+   puts "Sorry, no one has this name in our records"
+   exit
+ end
+
+ now = Time.new
+ birthday = birthdays[name]
+
+ if (now.month < birthday.month) || (now.month == birthday.month && now.day < birthday.day)
+   next_bday_year = now.year
+  age = now.year - birthday.year
+elsif (now.month == birthday.month) && (now.day == birthday.day)
+   puts "#{name} has her/his bday today!"
+   exit
+ else
+   next_bday_year = today.year + 1
+   age = today.year - birthday.year + 1
+ end
+
+ puts "#{name} will be #{age} on #{birthday.day} #{birthday.month} #{next_bday_year}"

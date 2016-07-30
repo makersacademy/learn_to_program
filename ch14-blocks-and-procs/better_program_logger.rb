1b31e964a6346a477762172d22b6c1cef595be3f
Ewan Sheldon <ewansheldon@gmail.com>
2016-07-30 22:49:10 +0100

    ch14 completed

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,29 @@
+$nesting_depth = 0
+
 def log desc, &block
-  # your code here
+  indent = ' ' * $nesting_depth
+  puts indent + 'Beginning "' + desc + '"...'
+  $nesting_depth += 1
+  result = block.call
+  $nesting_depth -= 1
+  puts indent + '..."' + desc + '" finished, returning: '
+  puts indent + result.to_s
+end
+
+log 'outer block' do
+
+  log 'some little block' do
+
+    log 'teeny-tiny block' do
+    "lots of love"
+  end
+  42
+end
+
+  log 'yet another block' do
+    "I love Indian food!"
+  end
+
+  true
+
 end

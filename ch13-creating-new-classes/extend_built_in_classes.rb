e4dd5847451a5f2696643f14c7a91bfe31545fa9
lsewilson <laurawilson@hotmail.co.uk>
2016-07-30 13:01:15 +0100

    Completed Ch13 extend built in classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,47 @@
 class Integer
-  # your code here
+
+  def factorial
+    return "You can't take the factorial of a negative number" if self < 0
+
+    if self <= 1
+      1
+    else self > 1
+      self * (self-1).factorial
+    end
+  end
+
+  def to_roman
+    numerals = {
+      1    => "I",
+      5    => "V",
+      10   => "X",
+      50   => "L",
+      100  => "C",
+      500  => "D",
+      1000 => "M"
+    }
+
+    num = self
+    roman_num = ''
+      numerals.reverse_each do |k, v|
+        while num >= k
+          num -= k
+          roman_num += v
+        end
+      end
+    roman_num
+  end
+
+end
+
+class Array
+
+  def shuffle
+    return self if self.length <= 1
+    arr = self
+    x = arr.sample
+    arr.delete(x)
+    [x] + arr.shuffle
+  end
+
 end

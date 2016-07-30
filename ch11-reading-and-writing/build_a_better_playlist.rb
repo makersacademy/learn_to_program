d0ad26821d70d3da07194a3d9442eee13ec5342a
Johnny Dworzynski <j.dworzynski@me.com>
2016-07-30 12:38:11 +0100

    completed better playlist shuffler

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,21 @@
 def music_shuffle filenames
-  # your code here
+
+  filenames.sort_by!{rand}
+
+  #added extra shuffling bit ??
+  array_1 = []
+  array_2 = []
+
+    while filenames.size > 0
+      array_1 << filenames.pop
+      array_2 << filenames.pop
+    end
+
+  array_1.concat(array_2)
+  array_1.compact!
+
 end
+
+songs = ['aa/bbb',   'aa/ccc',   'aa/ddd',
+         'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+puts(music_shuffle(songs))

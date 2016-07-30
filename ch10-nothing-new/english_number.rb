6566e6ac51f9897cc51b24ad9243f319c7080dfe
MrJons <shad_stick@hotmail.co.uk>
2016-07-30 17:15:42 +0100

    English_number attempt and Shuffle.

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,52 @@
 def english_number number
-  # your code here
+  1 = ['one', 'two', 'three',
+      'four', 'five', 'six',
+      'seven', 'eight', 'nine']]
+
+  2 = ['ten', 'twenty', 'thirty',
+      'forty', 'fifty', 'sixty',
+      'seventy', 'eighty', 'ninety']]
+
+  teens = ['eleven', 'twelve', 'thirteen',
+           'fourteen', 'fifteen', 'sixteen',
+          'seventeen', 'eighteen', 'nineteen']
+
+  3 = ['one hundred', 'two hundred', 'three hundred',
+      'four hundred', 'five hundred', 'six hundred',
+      'seven hundred', 'eight hundred', 'nine hundred']
+
+  if number < 0
+    puts "Please use a positive integer."
+  end
+
+  if number == 0
+    puts "Zero"
+  end
+
+  written_num = []
+  num_array = number.to_a
+  size = left.length
+  num_rev = number.reverse.to_s
+  left = number
+
+  num_rev.each do |x|
+    ind = num_rev.index(x)
+    written_num << ind[x.to_i - 1]
+  end
+
+  written_num.reverse
+
+  if number.length == 3
+    written_num[1] = "and"
+  end
+
+  if written_num[-1] == 1 && written_num[written_num.length -1] != 0
+    y = written number.last
+    written_num.last(2).delete
+    written_num << teens[y-1]
+  end
+
+  written_num.join(" ")
 end
+
+english_number(51)

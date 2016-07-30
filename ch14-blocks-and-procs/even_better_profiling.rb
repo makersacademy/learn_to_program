e8d61a45e07b88be13b81c2f59a27f8af8ea28b6
Ewan Sheldon <ewansheldon@gmail.com>
2016-07-30 16:17:13 +0100

    ch14 ex1 completed

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,11 @@
 def profile block_description, &block
-  # your code here
+  on_off_switch = true
+  if on_off_switch
+  start_time = Time.new
+  block.call
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+  else
+  block.call
+  end
 end

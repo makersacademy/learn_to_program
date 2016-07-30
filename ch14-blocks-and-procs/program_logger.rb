4827bd9529908a599e01fca476460ed3e910e42b
Tam Boine <tamborinehands@gmail.com>
2016-07-30 19:42:46 +0000

    nearly done

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,17 @@
-def log desc, &block
-  # your code here
+
+def log block_description, &block
+	puts "Beginning #{block_description.inspect}.."
+	l = block.call
+	puts "#{block_description.inspect}.. finished, returning: #{l}"
+end
+
+log "outer block" do 
+	log "some little block" do
+		5
+	end 
+
+	log "yet another block" do 
+		'I like Thai food!'
+	end
+'0' == 0
 end

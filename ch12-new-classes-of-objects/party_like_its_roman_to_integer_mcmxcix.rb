eadea8baa230f1e96e5a2221110224223de67fe2
lsewilson <laurawilson@hotmail.co.uk>
2016-07-30 11:04:05 +0100

    Completed Ch12 party like its roman_to_integer mcmxcix

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,40 @@
 def roman_to_integer roman
-  # your code here
+  unless roman.is_a? String
+    return "Please enter a valid Roman numeral"
+  end
+
+  roman.upcase!
+
+  numerals = {
+    1    => "I",
+    4    => "IV",
+    5    => "V",
+    9    => "IX",
+    10   => "X",
+    40   => "XL",
+    50   => "L",
+    90   => "XC",
+    100  => "C",
+    400  => "CD",
+    500  => "D",
+    900  => "CM",
+    1000 => "M"
+  }
+
+  result = 0
+  numerals.reverse_each do |num, rom|
+    while roman.index(rom) == 0
+      result += num
+      roman.slice!(rom)
+    end
+  end
+
+  if roman != ''
+    return "Please enter a valid Roman numeral"
+  else
+    result
+  end
 end
+
+puts roman_to_integer("miiix")
+puts roman_to_integer(0)

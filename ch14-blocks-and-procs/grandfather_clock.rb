80f95ea72630cbfb00ff12aca36993de11c312b2
Ewan Sheldon <ewansheldon@gmail.com>
2016-07-30 16:24:32 +0100

    ch14 ex2 completed

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,12 @@
 def grandfather_clock &block
-  # your code here
+  current_hour = Time.new.hour
+  if current_hour > 12
+    (current_hour - 12).times do
+      block.call
+    end
+  else
+    current_hour.times do
+      block.call
+    end
+  end
 end

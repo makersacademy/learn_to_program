ea3238c82f8f315feb487e0288450736cfaa1df3
lsewilson <laurawilson@hotmail.co.uk>
2016-07-30 12:11:36 +0100

    Completed Ch12 birthday helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,23 @@
-# your code here
+filename = "birthdays.txt"
+
+bday_hash = {}
+age_hash = {}
+now = Time.new
+
+File.open(filename).each_line do |line|
+  name, date, year = line.split(",")
+  bday_hash[name] = date
+  bday_time = Time.mktime(year, *(date.split))
+  age = now.year - bday_time.year
+  if bday_time.month < now.month || bday_time.month == now.month && bday_time.day <= now.day
+    age_hash[name] = age + 1
+  else
+    age_hash[name] = age
+  end
+ end
+
+puts "Whose birthday would you like to know?"
+request = gets.chomp
+
+puts "#{request}'s birthday is on #{bday_hash[request].strip}"
+puts "#{request} will be #{age_hash[request]}"

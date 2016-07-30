22569ce24f33929e7cd8e9bf7948d67e96b46c6f
Tam Boine <tamborinehands@gmail.com>
2016-07-30 15:41:49 +0000

    done some in chapters 12 and 13

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,19 @@
 class Integer
-  # your code here
+	def roman_numeral
+	#this function should take an int n and convert it to a new roman numeral
+	nums = {'M'=>1000, 'CM'=>900, 'D'=>500, 'CD'=>400, 'C'=>100, 'XC'=>90, 'L'=>50, 'XL'=>40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1 } #a hash of the building blocks
+	n = self
+	answer = [] #initialize an empty array we'll push to later
+
+	nums.each { |i,j| #iterate through nums hash (where i is key, j is value)
+		q = n/j #where j is the value in nums
+		r = n%j #get the remainder
+		answer.push i*q #push the multiples of j to answer in numeral form (i)
+		n = r #redefine n - what was pushed already for next iteratio
+	}
+	ans = answer.join("") #converting array to one string
+	puts ans
+	end 
 end
+
+puts 7.roman_numeral
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,11 +1,58 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
-
 class OrangeTree
-  # your code here
+
+	def initialize
+	@oranges = 0
+	@height = 0
+	@age = 0
+	end
+
+	def height
+		if alive?
+			@height
+		else
+			puts 'A dead tree is not very tall. :('
+		end
+
+	end
+
+	def count_the_oranges
+		if alive?
+			@oranges
+		else
+			'A dead tree has no oranges. :('
+		end
+	end
+
+	def pick_an_orange
+		if @oranges <= 0 
+			"Sorry there are no oranges to pick this year"
+		elsif alive? == false
+			puts 'A dead tree has nothing to pick. :('
+		else
+			@oranges -= 1
+			puts "What a juicy tasty fleshy citrus!"
+		end
+	end
+
+	def one_year_passes
+		@oranges = 0
+		@height += 0.4
+		if @age > 5
+			@oranges += (@height * 15 - 25)
+		end
+		@age += 1
+		if !alive?
+		puts "Oh, no! The tree is too old, and has died. :("
+		elsif @age > 25
+		puts "A year later, the tree is still dead. :("
+		end
+
+		puts "This year your tree grew to #{@height}m tall, and produced #{@oranges} oranges."
+	end
+
+	private
+
+	def alive?
+		@age < 25
+	end
 end

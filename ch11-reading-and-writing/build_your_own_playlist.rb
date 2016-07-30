7758af6061f7ad6088ce5aeacbe7482b1da48673
Johnny Dworzynski <j.dworzynski@me.com>
2016-07-30 11:38:28 +0100

    playlist builder completed

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "Name your playlist"
+playlist_name = gets.chomp
+playlist_songs = Dir['**/*.mp3']
+
+puts "Building a playlist of #{playlist_songs.length} songs..."
+
+playlist_songs.sort_by!{rand}
+
+filename = playlist_name + ".m3u"
+
+  File.open filename, 'w' do |f|
+    playlist_songs.each do |song|
+      f.write song + "\n"
+    end
+  end
+puts "Done!"

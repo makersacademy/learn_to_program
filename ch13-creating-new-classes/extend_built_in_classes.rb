548ee78b681eab9fefd96410a2a2d157ab57d20b
Stéphan Gaud <stephgoeseast@gmail.com>
2016-07-31 11:36:53 +0100

    chap 13 ex 3 Baby Dragon solution and updated extented classes wioth roman numerals

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -7,4 +7,24 @@ class Integer
     end
     fac
   end
+
+  def to_roman
+    str = ""
+    m = self/1000
+    r = self%1000
+    d = r/500
+    r = r%500
+    c = r/100
+    r = r%100
+    l= r/50
+    r = r%50
+    x = r/10
+    r = r%10
+    v = r/5
+    r = r%5
+    i = r/1
+
+    str << "M"*m + "D"*d + "C"*c + "L"*l + "X"*x + "V"*v +"I"*i
+    str
+  end
 end

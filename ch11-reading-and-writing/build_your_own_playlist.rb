c94da19e6becbd8fc9b05c06f2f8f049a6c3ee4d
Tim3tang <tim3tang@gmail.com>
2016-07-31 18:20:47 +0000

    ch11 learn to program (more copy paste)

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,25 @@
-# your code here
+def shuffle arr
+  x = arr.length
+  new_arr = []
+  while x != 0
+    randpos = rand(x)
+    x = x-1
+    item = arr[randpos]
+    new_arr.push item
+    arr[randpos] = arr[x]
+    arr.pop
+  end
+
+  new_arr
+end
+
+Dir.chdir 'C:\Users\tim3t\Music'
+ 
+song_names = shuffle(Dir['*.mp3'])
+filename = 'playlist.m3u'
+ 
+File.open filename, 'w' do |f|
+song_names.each do |song|
+    f.write song+"\n"
+ end
+end

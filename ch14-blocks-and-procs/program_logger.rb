0b9545227b819df145feb985dc011739ae9e6f0b
Johnny Dworzynski <j.dworzynski@me.com>
2016-07-31 23:36:12 +0200

    14.4 program logger minor amends

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,20 +1,20 @@
-def log desc, &block
+def program_log desc, &block
   # your code here
-  puts "Beginning #{desc}..."
+  puts "Beginning \"#{desc}\"..."
   result = block.call
-  puts "... #{desc} finished, returning: " + result.to_s
+  puts "...\"#{desc}\" finished, returning: " + result.to_s
 end
 
-log "outer block" do
+program_log "outer block" do
 
-    log "some little block" do
+    program_log "some little block" do
       1**1 + 2**2
     end
 
-    log "yet another block" do
+    program_log "yet another block" do
       '!doof iahT ekil I'.reverse
     end
 
     '0' == 0
-    
+
 end

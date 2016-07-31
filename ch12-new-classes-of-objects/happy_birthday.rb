4d648f2f98ee665fba4b8478d1c544717a2b8693
Tim3tang <tim3tang@gmail.com>
2016-07-31 20:53:29 +0000

    Ch12 learn to program (birthday_helper not working)

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+def age_calculator (year, month, day)
+  age = (Time.new - Time.mktime(year, month, day)) /
+    (365 * 24 * 60 * 60)
+  age.to_i
+end
+
+print "year of birth (yyyy): "; year = gets.chomp.to_i
+
+print "month of birth (mm): "; month = gets.chomp.to_i
+
+print "day of birth (dd): "; day = gets.chomp.to_i
+
+age = age_calculator(year, month, day)
+
+print "Your age is #{age}.\n" + "SPANK!\n" * age
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,28 @@
 def roman_to_integer roman
-  # your code here
+  hash = {"I" => 1, "IV" => 4, "V" => 5, "IX" => 9, "X" => 10, "XL" => 40, "L" => 50, "XC" => 90, "C" => 100, "CD" => 400, "D" => 500, "CM" => 900, "M" => 1000}
+
+  total = 0
+    prev = 0
+    index = roman.length - 1
+    while index >= 0
+      c = roman[index].upcase
+      index = index - 1
+      val = hash[c]
+      if !val
+        puts 'Please enter a valid roman numeral'
+        return
+      end
+
+      if val < prev
+        val = val * -1
+      else
+        prev = val
+      end
+      total = total + val
+    end
+
+    total
 end
+
+  puts(roman_to_integer('mcmxcix'))
+  puts(roman_to_integer('CCCLXV'))

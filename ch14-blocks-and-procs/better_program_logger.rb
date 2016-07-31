256324d86708475eacad2a4a72b5e404947120b6
Tam Boine <tamborinehands@gmail.com>
2016-07-31 10:20:21 +0000

    ch14 done

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,28 @@
-def log desc, &block
-  # your code here
+
+
+$nest = 0 #nest will track how many times we should indent our putsing, which is the times that log calls another block
+def log block_description, &block
+	
+	prefix = '	'*$nest
+	puts prefix + "Beginning #{block_description.inspect}.."
+	$nest += 1
+	l = block.call
+	$nest -= 1
+	puts prefix + "#{block_description.inspect}.. finished, returning: #{l}"
 end
+
+log "outer block" do 
+	log "some little block" do		
+		log "teeny-tiny block" do
+			#$nest = 3
+			'lots of love'
+		end
+		5
+	end 
+	log "yet another block" do 
+		'I like Thai food!'
+	end
+'0' == 0
+end
+
+

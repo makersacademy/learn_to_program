37feea774df7b036c0eaea4e6e56288ebfec2b48
Thao Vo <ttthao.vo@gmail.com>
2016-07-31 21:42:03 +0000

    ch12 birthday helper files

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,18 @@
-# your code here
+def birthday_helper
+    
+    birthdates = Hash.new
+    
+    file = File.readlines("ch12_birthday_list.txt") 
+    
+    file.each do |f|
+        temp = f.split(',')
+        birthdates[temp[0]] = "#{temp[1]}, #{temp[2]}"
+    end
+    
+    puts "Whose birthday are you looking for?"    
+    name_of_person = gets.chomp
+    
+    return birthdates[name_of_person]
+end
+
+puts(birthday_helper())

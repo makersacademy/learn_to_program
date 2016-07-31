813328acdd7737818559b09edb87fa070e0b61b6
MrJons <shad_stick@hotmail.co.uk>
2016-07-31 19:15:10 +0100

    Happy birthday & 1 billion seconds.

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,12 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "What month (number) were you born in?"
+month = gets.chomp
+
+puts "What day were you born on?"
+day = gets.chomp
+
+age = ((Time.new - Time.new(year,month,day)) / 31536000).to_int
+
+print "SPANK " * age

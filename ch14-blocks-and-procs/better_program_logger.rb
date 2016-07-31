8572248db3151ee3526292f6ff39c9b84539fdcf
Rosie Allott <rosie.allott@gmail.com>
2016-07-31 14:41:44 +0100

    Ch 14 - Better Programme Logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,22 @@
+$indentation = 0
+
 def log desc, &block
-  # your code here
+  puts "  "*$indentation + "Beginning #{$desc}..."
+  $indentation += 1
+  result = block.call
+  $indentation -= 1
+  puts " "*$indentation + "...#{$desc} finished, returning: #{result}"
+end
+
+log "outer block" do
+  log 'some little block' do
+    log "teeny-tiny block" do
+      "lots of love"
+    end
+    42
+  end
+  log 'yet another block' do
+    "I love Indian food"
+  end
+  true
 end

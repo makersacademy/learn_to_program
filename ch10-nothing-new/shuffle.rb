70a23fa6c5d7d5393502d4738512e221dace4dfe
Anna Beddow <anna.beddow@gmail.com>
2016-07-31 15:47:25 +0100

    ch10_shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,20 @@
 def shuffle arr
-  # your code here
+ shuff []
+
+	while arr.length > 0
+		random_index = rand(arr.length)
+		current_index = 0
+		new_array = []
+		arr.each do |item|
+			if current_index == random_index
+				shuf.push item
+			else 
+				new_array.push item
+			end
+			current_index = current_index +1
+		end
+		arr = new_array
+	end
+	shuf
 end
+p(shuffle([1,2,3,4,5,6,7,8,9]))

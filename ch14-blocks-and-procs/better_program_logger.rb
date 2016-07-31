3da5c4e0cbc2732f6bc41a040190124ddfca3db8
Stéphan Gaud <stephgoeseast@gmail.com>
2016-07-31 16:28:57 +0100

    chap 14 ex 4 - A better program logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
+$just = 0
+
 def log desc, &block
-  # your code here
+  prefix = ' ' * $just
+  puts prefix + "Beginning #{desc}"
+  $just = $just + 1
+  result = block.call
+  $just = $just - 1
+  str = "#{desc} finished, returning #{result}"
+  puts prefix + str
+end
+
+log 'outer block' do
+  log 'First block inside outer block ' do
+    x = 5
+    x
+  end
+
+  log 'Second block inside outer block' do
+    x = "I like Thai food"
+    x
+  end
+
+  x = "This is the result from outer block itself"
 end

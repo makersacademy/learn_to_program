7cc0231ce6412d56b055bc92334a8e30dcd9fa77
jameshamann <jameshamann@me.com>
2016-08-01 11:59:38 +0100

    Updated built-in extended classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,96 +1,50 @@
+class Array
+   def shuffle
+     arr = self
+      shuf = []
+      while arr.length > 0
+      rand_index = rand(arr.length)
+        curr_index = 0
+        new_arr = []
+          arr.each do |item|
+            if curr_index == rand_index
+              shuf.push item
+            else
+              new_arr.push item
+            end
+
+            curr_index = curr_index + 1
+          end
+
+          arr = new_arr
+        end
+        shuf
+      end
+    end
+
 class Integer
   def factorial
     if self <= 1
       1
     else
-      self*(self-1).factorial
+      self * (self-1).factorial
     end
   end
-  def old_roman_numeral
-    num = self
-    i_length = 0
-    v_length = 0
-    x_length = 0
-    l_length = 0
-    c_length = 0
-    d_length = 0
-    m_length = 0
+  def to_roman
+    roman = ''
 
-    if num >= 1000
-      m_length = num / 1000
-      num = num % 1000
-    end
+    roman = roman + 'M' * (self / 1000)
+    roman = roman + 'D' * (self % 1000 / 500)
+    roman = roman + 'C' * (self % 500 / 100)
+    roman = roman + 'L' * (self % 100 / 50)
+    roman = roman + 'X' * (self % 50 / 10)
+    roman = roman + 'V' * (self % 10 / 5)
+    roman = roman + 'I' * (self % 5 / 1)
 
-    if num >= 500
-      d_length = num / 500
-      num = num % 500
-    end
-
-    if num >= 100
-      c_length = num / 100
-      num = num % 100
-    end
-
-    if num >= 50
-      l_length = num / 50
-      num = num % 50
-    end
-
-    if num >= 10
-      x_length = num / 10
-      num =  num % 10
-    end
-
-    if num >= 5
-      v_length = num / 5
-      num = num % 5
-    end
-
-    if num >= 1
-      i_length = num / 1
-      num = num % 1
-    end
-  puts 'M'*m_length + 'D'*d_length + 'C'*c_length + 'L'*l_length + 'X'*x_length + 'V'*v_length + 'I'*i_length
-end
-end
-
-puts 'Factorial Method:'
-puts 5.factorial
-puts 20.factorial
-puts 1.factorial
-puts
-puts 'Old Roman Numeral Method:'
-puts 100.old_roman_numeral
-puts 5.old_roman_numeral
-puts 10.old_roman_numeral
-puts 75.old_roman_numeral
-
-
-class Array
-  def shuffle
-    arr = self
-    shuff = []
-
-  while arr.length > 0
-    rand_index = rand(arr.length)
-    curr_index = 0
-    new_arr = []
-
-      arr.each do |item|
-        if curr_index == rand_index
-          shuff.push item
-        else
-          new_arr.push item
-        end
-
-        curr_index = curr_index + 1
-      end
-      arr = new_arr
-    end
-    shuff
+    roman
   end
 end
 
-
-puts 'Shuffle Method:'
-puts ['50','20','30','10','60'].shuffle
+puts [1,2,3,4,5].shuffle
+puts 7.factorial
+puts 73.to_roman

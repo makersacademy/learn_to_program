cd309ea65f7014367b300d6010ef0f4acccfc8bc
Tam Boine <tamborinehands@gmail.com>
2016-08-01 09:19:31 +0000

    done

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,19 +1,30 @@
 class Integer
-	def roman_numeral
-	#this function should take an int n and convert it to a new roman numeral
-	nums = {'M'=>1000, 'CM'=>900, 'D'=>500, 'CD'=>400, 'C'=>100, 'XC'=>90, 'L'=>50, 'XL'=>40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1 } #a hash of the building blocks
+def factorial
 	n = self
+	if n == 0
+		1
+	elsif n >= 1
+		n* (n-1).factorial
+	else 
+	nil
+	end	
+end
+def to_roman
+	n = self
+	#this function should take an int n and convert it to old school roman numeral
+	nums = {'M'=>1000, 'D'=>500, 'C'=>100, 'L'=>50, 'X'=>10, 'V'=>5, 'I'=>1} #a hash of the building blocks for roman numerals
+	
 	answer = [] #initialize an empty array we'll push to later
 
 	nums.each { |i,j| #iterate through nums hash (where i is key, j is value)
 		q = n/j #where j is the value in nums
 		r = n%j #get the remainder
 		answer.push i*q #push the multiples of j to answer in numeral form (i)
-		n = r #redefine n - what was pushed already for next iteratio
-	}
-	ans = answer.join("") #converting array to one string
-	puts ans
-	end 
-end
+		n = r #redefine n - what was pushed already for next iteration
 
-puts 7.roman_numeral
+	}
+answer.join("") #converting array to one string
+end 
+
+end 
+

8b731fd5f04ffd0b779d28e125073eded31a7483
MrJons <shad_stick@hotmail.co.uk>
2016-08-01 14:09:50 +0100

    party like its roman to integer challenge

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,16 @@
-def roman_to_integer roman
-  # your code here
+def roman_to_integer(roman)
+  numerals = {"M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90,
+              "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1}
+
+  int_num = 0
+
+  roman.chars do |x|
+    if numerals[x] == nil
+      puts "This is not a valid integer."
+    else
+      int_num = int_num + numerals[x]
+    end
+  end
+
+  int_num
 end

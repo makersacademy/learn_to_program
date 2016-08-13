cd8edf24c6f4a18a22e369c666b17d9b61154563
Ben Ross <benjaminjackross@gmail.com>
2016-08-13 22:10:08 +0000

    added final solutions

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,2 @@
-# your code here
+my_birthday = Time.local(1992, 07, 19, 06, 30, 00)
+puts my_birthday + 1000000000
--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,37 @@
 class Integer
-  # your code here
+    def factorial
+        if self < 0
+            return 'You can\'t take the factorial of a negative number!'
+        end
+        if self <= 1
+            1
+        else
+            self * (self-1).factorial
+        end
+    end
+    
+    def factorial
+    if self <= 1
+      1
+    else
+      self * (self-1).factorial
+    end
+  end
+  def to_roman
+    # I chose old-school roman numerals just to save space.
+    roman = ''
+
+    roman = roman + 'M' * (self / 1000)
+    roman = roman + 'D' * (self % 1000 / 500)
+    roman = roman + 'C' * (self % 500 / 100)
+    roman = roman + 'L' * (self % 100 / 50)
+    roman = roman + 'X' * (self % 50 / 10)
+    roman = roman + 'V' * (self % 10 / 5)
+    roman = roman + 'I' * (self % 5 / 1)
+
+    roman
+  end
 end
+
+# puts 8.factorial
+# puts 49.to_roman

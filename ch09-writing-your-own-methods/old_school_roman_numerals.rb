fc446a4c5c86cc3f7b5e97f67f95e7b90d20bbcd
mr_christoff <chris.lawrence@gmx.com>
2016-09-01 14:14:23 +0000

    O_S_R_Numerals partial attempt - barking up the wrong tree??

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,35 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numerals(num)
+
+	$res_arr = []
+
+	def zero_to_four(sin)
+		sin.times do
+		$res_arr << "I"
+		end
+	end	
+	
+	def fives(fiv)
+		$res_arr << "V"
+		zero_to_four(fiv % 5)
+	end
+
+	def tens(tn)
+		(tn/10).times do 
+			$res_arr << "X"
+		end
+		zero_to_four(tn % 10)
+	end
+
+	if num < 5
+		zero_to_four(num)
+
+	elsif (5..9).include? num
+		fives(num)
+	elsif (10..49)
+		tens(num)
+
+	end
+
+
+p $res_arr.join
 end

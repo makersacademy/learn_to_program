361571f8bc5feb101b6c1649f6ad40bedcb71fcc
Darrenf <darrenf54321@gmail.com>
2016-09-03 15:56:36 +0100

    bubble sort coding

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,21 @@
-def sort arr
-  # your code here
+#I'm not going to pretend that I didn't utilise Google for this!....
+
+def sort(array)
+
+  num = array.length
+
+  loop do
+    changes = false
+
+    (num-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        changes = true
+      end
+    end
+
+    break if not changes #I need to revisit this wording
+  end
+
+  array
 end

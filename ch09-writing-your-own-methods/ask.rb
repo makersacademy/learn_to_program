827ab87dbf571836f27ae2edbd2ad1aea3ddb573
mr_christoff <chris.lawrence@gmx.com>
2016-09-04 19:30:34 +0000

    Working ask.rb, but failing rspec

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,20 @@
-def ask question
-  # your code here
+class String
+        def ask
+            while true 
+            puts self
+            reply = gets.chomp.downcase
+            if (reply == 'yes' || reply == 'no')
+                if reply == 'yes'
+                    answer = true
+                else
+                    answer = false
+                end
+                break
+            else
+                puts "Please answer \"yes\" or \"no\"."
+            end
+        end
+    end
 end
+
+

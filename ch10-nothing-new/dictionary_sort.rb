94685a9f6f6f2024463955afb20127856ff5cd56
James <James.Steel@me.com>
2016-09-05 15:30:00 +0100

    some sort methods completed

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
-def dictionary_sort arr
-  # your code here
+def sort arr
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+  #when unsorted empty returns sorted array
+  first = unsorted.pop
+  still_to_sort = []
+  #pulled the current last element in order to compare
+  unsorted.each { |compare|
+   if compare.downcase < first.downcase
+   unsorted.push first
+   first = compare
+   else
+   still_to_sort.push compare
+   end }
+   sorted.push first
+   recursive_sort still_to_sort, sorted
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,22 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+  #when unsorted empty returns sorted array
+  first = unsorted.pop
+  still_to_sort = []
+  #pulled the current last element in order to compare
+  unsorted.each { |compare|
+   if compare < first
+   unsorted.push first
+   first = compare
+   else
+   still_to_sort.push compare
+   end }
+   sorted.push first
+   recursive_sort still_to_sort, sorted
 end

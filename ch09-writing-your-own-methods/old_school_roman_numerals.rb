ef5cf71bb935911e024f2dc87bcf4f8ce71bf6c7
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-05 22:07:14 +0200

    Second update of old_school_roman_numerals.rb - used ternary operator

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -2,14 +2,11 @@ def old_roman_numeral num
   array = num.to_s.chars.map(&:to_i)
   a_length = array.length
     if a_length == 1
-      if array[0] < 5
-        puts "I" * array[0]
-      else 
-        puts "V" + ("I" * (array[0] - 5))
-      end
+      puts array[0] < 5 ? "I" * array[0] : "V" + "I" * (array[0] - 5)
     elsif a_length == 2
     elsif a_length == 3
     elsif a_length == 4
     end
 end
 
+old_roman_numeral 9

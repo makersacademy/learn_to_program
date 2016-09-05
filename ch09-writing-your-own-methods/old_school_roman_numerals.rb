d50212b1737b05b5ae539adccfec91ce93a5feb6
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-05 22:35:56 +0200

    Third update of old_school_roman_numerals.rb - working on array with 3 objects

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -4,9 +4,16 @@ def old_roman_numeral num
     if a_length == 1
       puts array[0] < 5 ? "I" * array[0] : "V" + "I" * (array[0] - 5)
     elsif a_length == 2
+      if num < 50
+        puts "X" * array[0] + (array[1] < 5 ? "I" * array[1] : "V" + "I" * (array[1] - 5))
+      else
+        puts "L" + ("X" * (array[0] - 5)) + (array[1] < 5 ? "I" * array[1] : "V" + "I" * (array[1] - 5))
+      end
     elsif a_length == 3
+      if num < 500
+        puts "C" * array[0]
+      else
+      end
     elsif a_length == 4
     end
 end
-
-old_roman_numeral 9

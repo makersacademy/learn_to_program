d783c544cb59bea85f302706a44a2412e1428565
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-05 14:38:41 +0100

    completed old school roman numerals exercise

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,11 @@
 def old_roman_numeral num
-  # your code here
+	roman_numeral = ''
+	letters_and_numbers = {'M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I' => 1}
+	letters_and_numbers.each do |letter,number|
+		roman_numeral += letter * (num / number)
+		num = num % number
+	end
+	return roman_numeral
 end
+
+

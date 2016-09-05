9a1c6d7b262a274f55fb65143fbf4ca1e1b3ba6c
crispinandrews <crispin.andrews@gmail.com>
2016-09-05 21:52:29 +0100

    Old school solution

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,19 @@
 def roman_numeral num
-  # your code here
+  roman = ''
+
+roman = roman + 'M' * (num/1000)
+
+
+
+hundreds = (num % 1000 / 100)
+if hundreds == 9
+  roman = roman + 'CM'
+elsif hundreds == 4
+  roman = roman + 'CD'
+else
+  roman = roman + 'D' * (num % 1000 / 500)
+  roman = roman + 'C' * (num % 500 / 100)
 end
+end
+
+puts roman_numeral(300)

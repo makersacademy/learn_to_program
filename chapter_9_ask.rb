664e302c29f8829b8bfac5a830c4f580a9ee3506
Laura Weston <laura.weston@outlook.com>
2016-09-05 13:22:47 +0100

    Chapter 9 improved ask method

---
+++ chapter_9_ask.rb
@@ -0,0 +1,24 @@
+def ask(question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    return true if reply == "yes"
+    return false if reply == "no"
+    puts "Please answer 'yes' or 'no'."
+  end
+end
+
+puts 'Hello, and thank you for...'
+puts
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for...'
+puts
+puts wets_bed

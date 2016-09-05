3784859cbf3d913f9e8707f01353bb22b1938a5e
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-05 21:54:48 +0200

    First update of old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,9 +1,15 @@
 def old_roman_numeral num
-  if num.to_i < 5
-    puts "I"*num
-  end
+  array = num.to_s.chars.map(&:to_i)
+  a_length = array.length
+    if a_length == 1
+      if array[0] < 5
+        puts "I" * array[0]
+      else 
+        puts "V" + ("I" * (array[0] - 5))
+      end
+    elsif a_length == 2
+    elsif a_length == 3
+    elsif a_length == 4
+    end
 end
 
-old_roman_numeral 6
-
-old_roman_numeral 3

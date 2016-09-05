3d62a59b3eae99d56253c82e65e7a86377aff80a
Frankie Bell <fbell.pro@gmail.com>
2016-09-05 22:32:25 +0100

    I started off with them ordered upside down, didn't realise until I added the code for L - I thought it was working in the modern way. My maths was then completly wrong and it took me a while to figure out how to line up the modulus correctly. Practiced in a separate file and transferred across, had to alter number to num so that it matched up

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,28 @@
 def old_roman_numeral num
-  # your code here
-end
+    old = ''
+
+    old = old + 'M' * (num % 10000 / 1000)
+    old = old + 'D' * (num % 1000 / 500)
+    old = old + 'C' * (num % 500 / 100)
+    old = old + 'L' * (num % 100 / 50)
+    old = old + 'X' * (num % 50 / 10)
+    old = old + 'V' * (num % 10 / 5)
+    old = old + 'I' * (num % 5 / 1)
+  end
+
+  puts old_roman_numeral(9)
+  puts old_roman_numeral(6)
+  puts old_roman_numeral(16)
+  puts old_roman_numeral(1110)
+  puts old_roman_numeral(2458)
+  puts old_roman_numeral(3001)
+
+"""
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+"""

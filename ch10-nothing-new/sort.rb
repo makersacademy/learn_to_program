059640b8af4ba22a531df5da91b44e2235356600
Malinna Leach <malinnab@yahoo.com>
2016-09-05 12:02:24 +0100

    sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,23 @@
 def sort arr
-  # your code here
+  rec_sort arr, []
+end
+
+def rec_sort arr, sorted
+  smallest = ""
+  arr.each do |x|
+    if (smallest == "" || x < smallest)
+      smallest = x
+    end
+  end
+  arr.each do |y|
+    if y == smallest
+      sorted << smallest
+    end
+  end
+  arr.delete(smallest)
+  if arr.length > 0
+    rec_sort arr, sorted
+  else
+    return sorted
+  end
 end

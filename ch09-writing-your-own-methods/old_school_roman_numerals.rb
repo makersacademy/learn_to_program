d1549ee61e6b59ffc6689bebad8ce6c5a49d1e16
Laura Weston <laura.weston@outlook.com>
2016-09-05 19:42:28 +0100

    Complete ch09 old_roman_numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(num)
+  numerals = [[1000, "M"],
+              [500, "D"],
+              [100, "C"],
+              [50, "L"],
+              [10, "X"],
+              [5, "V"],
+              [1, "I"]]
+
+  roman = ""
+  return "nulla" if num == 0
+  numerals.each do |n|
+    while num >= n[0]
+      roman += n[1]
+      num -= n[0]
+    end
+  end
+  roman
 end

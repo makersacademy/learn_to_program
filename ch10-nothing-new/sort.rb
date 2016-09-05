78850cb9c2fb5f99df79a3e37c0536c3fb94b9c9
mr_christoff <chris.lawrence@gmx.com>
2016-09-05 20:29:57 +0000

    Sort.rb WIP

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,10 @@
-def sort arr
-  # your code here
+def sort(some_array)
+	recursive_sort(some_array, [])
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+	sorted_array << unsorted_array.min
+	unsorted_array.delete(unsorted_array.min)
+end
+
+p sort(["b", "s", "e", "t", "a"])

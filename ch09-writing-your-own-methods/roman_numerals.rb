574d8e4a08c8f71ce98b7768a8bc40b055e363de
Stephen Gregory <stejgregory@gmail.com>
2016-09-06 18:53:23 +0100

    End of ch09, that is a head fart

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,3 +1,45 @@
 def roman_numeral num
-  # your code here
+    thous = (num / 1000)
+    hunds = (num % 1000 / 100)
+    tens  = (num % 100 / 10)
+    ones  = (num % 10)
+
+    roman = 'M' * thous
+
+    if hunds == 9
+      roman = roman + 'CM'
+    elsif hunds == 4
+      roman = roman + 'CD'
+    else
+      roman = roman + 'D' * (num % 1000 / 500)
+      roman = roman + 'C' * (num % 500 / 100)
+    end
+
+    if tens == 9
+      roman = roman + 'XC'
+    elsif tens == 4
+      romans = romans + 'XL'
+    else
+      roman = roman + 'L' * (num % 100 / 50)
+      roman = roman + 'X' * (num % 50 / 10)
+    end
+
+    if ones == 9
+      roman = roman + "IX"
+    elsif roman == 4
+      roman = roman + "IV"
+    else
+      roman = roman + 'V' * (num % 10 / 5)
+      roman = roman + 'I' * (num % 5 / 1)
+    end
+    roman
+end
+
+puts 'Give me a number between 1 and 3000 to convert it into a roman numeral:'
+reply = gets.chomp.to_i
+
+if reply <= 3000
+    puts roman_numeral reply
+  else
+    puts "That isn't between 1 and 3000! Maybe next time."
 end

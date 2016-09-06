0a44f3dd1da5848f7da0d18998f476679f9823b8
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-06 13:59:29 +0100

    completed dictionary_sort method

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,8 @@
 def dictionary_sort arr
-  # your code here
+	sorted_words = []
+  arr.each {|item| arr[arr.index(item)] = item.downcase}
+  until arr.length == 0 do sorted_words << arr.delete(arr.min)end
+  print sorted_words
 end
+
+dictionary_sort ["xczcz","Gdfhui","qwruqio","Fbbfak"]

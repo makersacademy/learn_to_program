f70566a00425819d949123b653a5cbb5143cb732
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-06 13:39:09 +0100

    Completed exercise 2 of chapter 9

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,62 @@
 def old_roman_numeral num
-  # your code here
+
+    number = String.new
+
+    number_array = num.to_s.split('')
+
+
+    thousands = number_array[0..-4].join('').to_i
+    thousands.times do |n|
+      number << "M"
+    end
+
+    hundereds = number_array[-3].to_i * 100
+
+    if hundereds >= 500
+      number << "D"
+
+      until hundereds == 500
+        number << "C"
+        hundereds -= 100
+      end
+
+    else
+      simple = hundereds / 100
+      simple.times do |n|
+        number << "C"
+      end
+    end
+
+    tens = number_array[-2].to_i * 10
+
+    if tens >= 50
+      number << "L"
+
+      until tens == 50
+        number << "X"
+        tens -= 10
+      end
+
+    else
+        simply = tens / 10
+        simply.times do |n|
+          number << "X"
+        end
+    end
+
+
+    singles = number_array[-1].to_i
+    if singles >= 5
+      number << "V"
+      until singles == 5
+        number << "I"
+        singles -= 1
+      end
+
+    else
+      singles.times do |n|
+        number << "I"
+      end
+    end
+    return number
 end

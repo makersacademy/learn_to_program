e03729c97b15fb2a1a4447cec5ed94cfc1f07624
jinimcoroneo <jinim1983@gmail.com>
2016-09-06 20:53:47 +0100

    ask.rb exercise chapter 9

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,20 @@
-def ask question
-  testing
+def ask(question)
+  while true
+  puts question
+
+  reply = gets.chomp.downcase
+    if reply == "yes"
+      return true
+
+    elsif reply == "no"
+      return false 
+
+    else
+      puts "Please answer 'yes' or 'no'."
+    end
+  end
+  answer 
 end
+
+ruby = ask("Do you like learning ruby?")
+puts ruby

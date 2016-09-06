af4faf75bb6e2a375d37f4a52d53074d38769ee6
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-06 14:48:11 +0100

    sorted with caps

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,26 @@
-def dictionary_sort arr
-  # your code here
+
+
+def dict_sort some_array
+  recursive_sort some_array, []
 end
+
+def recursive_sort unsorted_array, sorted_array
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+  smallest = unsorted_array.pop
+  stillunsorted = []
+
+  unsorted_array.each do |x|
+    if x.downcase < smallest.downcase
+    stillunsorted.push smallest
+    smallest = x
+   else
+    stillunsorted.push x
+  end
+  end
+    sorted_array.push smallest
+    recursive_sort stillunsorted, sorted_array
+end
+
+puts(dict_sort(['can', 'Life', 'moment', 'fun' ]))

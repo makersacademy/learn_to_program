42fff352052e9f2bbd7893bb40d732e80a9e5749
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-09-06 15:23:59 +0100

    sorting challenges sorted.

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,30 +1,30 @@
 def dictionary_sort arr
-rec_dictionary_sort arr, []
+  rec_dict_sort arr, []
 end
 
-def rec_dictionary_sort unsorted, sorted
-  if unsorted.length <= 0
-    return sorted
-  end
+def rec_dict_sort unsorted, sorted
+if unsorted.length <= 0
+  return sorted
+end
+
+# So if we got here, then it means we still # have work to do.
 
 smallest = unsorted.pop
 still_unsorted = []
 
-# If the one we're testing's smaller than the smallest,
-# the smallest gets pushed back in the still_unsorted array...
-# and the tested_object becomes the smallest!
-
 unsorted.each do |tested_object|
-  if tested_object.downcase < smallest.downcase
-    still_unsorted.push smallest
-    smallest = tested_object
-  else still_unsorted.push tested_object
+if tested_object.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = tested_object
+else
+      still_unsorted.push tested_object
 end
 end
 
-sorted.push smallest
-rec_dictionary_sort still_unsorted, sorted
-end
+# Now "smallest" really does point to the
+# smallest element that "unsorted" contained,
+# and all the rest of it is in "still_unsorted". sorted.push smallest
 
-# Let's roll it out!
-puts(dictionary_sort (['huge', 'angry', 'big', 'words', 'forever', 'on', 'screens']))
+  rec_dict_sort still_unsorted, sorted
+end
+puts(dictionary_sort(['can','feel','singing.','like','A','can']))

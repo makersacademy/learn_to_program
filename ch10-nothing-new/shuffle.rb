8e4c53e7df7916c6e97d6a2c8c485b0456017892
Laurent Bo <misterbl@hotmail.com>
2016-09-06 19:40:44 +0000

    def shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,20 @@
 def shuffle arr
-  # your code here
+	final_array = []
+	while arr.length > 0
+		rand_index = rand(arr.length)
+		intial_index = 0
+		array = []
+		arr.each do |w|
+				if intial_index == rand_index
+					final_array.push(w)
+				else
+					array.push(w)
+				end
+
+				intial_index = intial_index + 1
+				end
+	arr = array
+	end
+
+return final_array
 end

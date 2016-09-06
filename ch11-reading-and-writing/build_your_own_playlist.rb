002d717c14607d632239a16ff4c2eb67a5112c87
James <James.Steel@me.com>
2016-09-06 17:05:39 +0100

    playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,17 @@
-# your code here
+Dir.chdir '/Users/jamessteel/Music/iTunes/iTunes Media/Apple Music'
+def shuffle arr
+ arr.sort_by{rand}
+end
+
+
+puts "building your nice playlist"
+puts "What shall we call this one"
+name = gets.chomp
+
+music = shuffle(Dir['**/*.{m4p}'])
+File.open "#{name.strip}.m3u", 'w' do |m|
+ music.each do |f|
+   m.write f + "\n"
+ end
+ end
+ puts "Done"

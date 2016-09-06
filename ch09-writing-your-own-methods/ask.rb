7fa6983c5656ad9f29ef6b529131dd894d50e5c1
iaink <iainkinnish@googlemail.com>
2016-09-06 18:29:27 +0000

    origin

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -3,7 +3,7 @@ def ask question
     puts question
     reply = gets.chomp.downcase
     
-    if (reply == "yes"} || reply == "no")
+    if (reply == "yes" || reply == "no")
         if reply == "yes"
              return true
         else
@@ -13,8 +13,8 @@ def ask question
     else
         puts "Please answer \"yes\" or \"no\"."
     end
+  end
 end
-
 puts "Hello, and thank you for participating in this survey."
 puts
 

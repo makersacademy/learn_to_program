1d14477a4be2c8fc516455a2a9290a32744f55e1
your name <akram.rasikh@gmail.com>
2016-09-06 16:09:25 +0100

    Shuffle method

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,9 @@
+#unsorted_array = ['something', 'cry', 'history', 'phone']
+#dont allow them to be in the same place as before
+
+
 def shuffle arr
-  # your code here
+  puts arr.sort_by { rand }
 end
+
+shuffle(['something', 'cry', 'history', 'phone'])

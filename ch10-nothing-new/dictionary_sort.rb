183086b87642108359affac45d3f42cd55c38a98
B-raw <michaelbarry1989@hotmail.com>
2016-09-06 15:08:21 +0100

    fixed dictionary_sort.rb to function

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -8,8 +8,8 @@
 
 #Partitioning: reorder the array so that all elements with values less than the pivot come before the pivot, while all elements with values greater than the pivot come after it (equal values can go either way). After this partitioning, the pivot is in its final position. This is called the partition operation.
 
-    less = arr.select {|x| x <  pivot}
-    more = arr.select {|x| x >= pivot}
+    less = arr.select {|x| x.upcase <  pivot.upcase}
+    more = arr.select {|x| x.upcase >= pivot.upcase}
 
 #Recursively apply the above steps to the sub-array of elements with smaller values and separately to the sub-array of elements with greater values.
     dictionary_sort(less) + [pivot] + dictionary_sort(more)

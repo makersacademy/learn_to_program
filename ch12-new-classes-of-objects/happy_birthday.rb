2a86705dfab3e0a2c1fdfa11aa0f5fe11e0d8a07
Malinna Leach <malinnab@yahoo.com>
2016-09-06 21:40:28 +0100

    party like it's your birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,11 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp
+puts "What month were you born in?  Please input a number between 1 and 12"
+month = gets.chomp
+puts "Which day of the month were you born on? Please input a number between 1 and 31"
+day = gets.chomp
+birthday = Time.local(year, month, day)
+years = ((Time.new - birthday) / 60 / 60 / 24 / 365).to_i
+puts "You're " + years.to_s + "!"
+print "SPANK! " * years
+puts

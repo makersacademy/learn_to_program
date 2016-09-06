344119b2360e88ff85e80d7381e78a244514cd7e
Malinna Leach <malinnab@yahoo.com>
2016-09-06 10:50:07 +0100

    dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
 def dictionary_sort arr
-  # your code here
+  rec_dic_sort arr, []
+end
+
+def rec_dic_sort arr, sorted
+  smallest = ""
+  arr.each do |x|
+    if (smallest == "" || x.downcase < smallest.downcase)
+      smallest = x
+    end
+  end
+  arr.each do |y|
+    if y == smallest
+      sorted << smallest
+    end
+  end
+  arr.delete(smallest)
+  if arr.length > 0
+    rec_dic_sort arr, sorted
+  else
+    return sorted
+  end
 end

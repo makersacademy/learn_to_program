ec86d05d046defbc6c4b298b1899d597ff50e411
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-09-06 15:14:09 +0100

    finished the dictionary sorting program

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,30 @@
 def dictionary_sort arr
-  # your code here
+rec_dictionary_sort arr, []
 end
+
+def rec_dictionary_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+smallest = unsorted.pop
+still_unsorted = []
+
+# If the one we're testing's smaller than the smallest,
+# the smallest gets pushed back in the still_unsorted array...
+# and the tested_object becomes the smallest!
+
+unsorted.each do |tested_object|
+  if tested_object.downcase < smallest.downcase
+    still_unsorted.push smallest
+    smallest = tested_object
+  else still_unsorted.push tested_object
+end
+end
+
+sorted.push smallest
+rec_dictionary_sort still_unsorted, sorted
+end
+
+# Let's roll it out!
+puts(dictionary_sort (['huge', 'angry', 'big', 'words', 'forever', 'on', 'screens']))

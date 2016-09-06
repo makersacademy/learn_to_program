99c2209d03d568d226ed866e34846cb2ce1f8cc8
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-06 10:14:04 +0100

    Finished first exercise of Chapter 9

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,27 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == "yes" || reply == "no")
+      if reply == "yes"
+        return true
+      else
+        return false
+      end
+    else
+      puts 'Please answer "yes" or "no"'
+    end
+  end
 end
+
+  ask "Do you like eating tacos?"
+  ask "Do you like eating burritos?"
+  wets_bed = ask "Do you wet the bed?"
+
+
+  puts
+  puts 'DEBRIEFING'
+  puts "Thank you for..."
+  puts
+  puts wets_bed

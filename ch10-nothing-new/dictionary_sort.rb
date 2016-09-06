59704c3508d3cbbd7481549a0ec09e02c8bed8c6
mr_christoff <chris.lawrence@gmx.com>
2016-09-06 14:12:23 +0000

    english_number.rb formatted but un-expanded

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,18 @@
-def dictionary_sort arr
-  # your code here
+#not working - return if have time
+def dictionary_sort(some_array)
+    recursive_sort(some_array, [])
+end
+
+def recursive_sort(unsorted_array, sorted_array)
+    min_word =[]
+    
+    if unsorted_array.length == 0
+        return sorted_array
+    else
+        min_word << (unsorted_array.map(&:downcase)).min
+        sorted_array << (unsorted_array.map(&:downcase)).min
+        min_word.each do |word| 
+            unsorted_array.delete_at((unsorted_array.map(&:downcase)).index(word)) end
+        recursive_sort(unsorted_array, sorted_array)
+    end
 end

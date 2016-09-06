6e31430bd2929de29eb9b83a6ebe63deaed2fe4b
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-06 15:43:31 +0100

    Added more questions to the ask exercise

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -18,7 +18,11 @@ end
   ask "Do you like eating tacos?"
   ask "Do you like eating burritos?"
   wets_bed = ask "Do you wet the bed?"
-
+  ask "Do you like eating chimichangas?"
+  ask "Do you like eating sopapillas?"
+  puts "Just a few more questions..."
+  ask "Do you like drinking horchata?"
+  ask "Do you like eating flautas?"
 
   puts
   puts 'DEBRIEFING'

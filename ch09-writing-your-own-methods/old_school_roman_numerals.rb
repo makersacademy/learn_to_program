b7c2ffad8cc4bc536f2445610844133c635aaa6a
jinimcoroneo <jinim1983@gmail.com>
2016-09-07 21:43:09 +0100

    pushed wrong file, this is updated version

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,20 +1,7 @@
 def old_roman_numeral(n)
   roman = ""
 
-  if n(1..3)
-    roman = roman + "I" * ( n % 5 / 1) 
-  if n
-
-
-
-
-
-
-  thousand = roman + "M" * ( n / 1000 )
-
-  if n(900..999)
-    roman = "DM" + thousand
-
+  roman = roman + "M" * (n / 1000)
   roman = roman + "D" * ( n % 1000 / 500 ) 
   roman = roman + "C" * ( n % 500 / 100 )
   roman = roman + "L" * ( n % 100 / 50 )

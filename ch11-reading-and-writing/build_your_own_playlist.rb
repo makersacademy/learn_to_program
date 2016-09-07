e0d20a2d3e7d77e0de92455e7e5a28982470aa26
Felix Harrison <fmlharrison@gmail.com>
2016-09-07 23:41:50 +0100

    playlist1

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,9 @@
-# your code here
+music = shuffle(Dir['/Users/felixharrison/music/**/*.mp3'])
+
+File.open 'Playlist.m3u', 'w' do |y|
+  music.each do |mp3|
+    y.write mp3 + "\n"
+  end
+end
+
+puts 'Done!'

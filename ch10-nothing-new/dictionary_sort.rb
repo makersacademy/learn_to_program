8e9cafbc0b3c174477cfc3395b3cd420b03ea38c
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-09-07 12:43:07 +0100

    dictionary sort completed

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+def recursive_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  small = unsorted.pop
+
+  remain_unsorted = []
+
+  unsorted.each do | x |
+    if x.downcase < small.downcase
+      remain_unsorted.push small
+      small = x
+    else
+      remain_unsorted.push x
+    end
+  end
+
+  sorted.push small
+  recursive_sort remain_unsorted, sorted
+end
+
+puts(dictionary_sort(["beans","Eggs","Brunch","apple","Acorn","badger"]))

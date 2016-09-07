3339c7c1ab5db4116fce4954724249ae1dca3c20
B-raw <michaelbarry1989@hotmail.com>
2016-09-07 18:21:20 +0100

    finished one billion seconds

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,5 @@
-# your code here
+my_birthday = Time.local(1989,06,26, 10, 05)
+
+one_billion_seconds_old = my_birthday + 1000000000
+
+puts "You will be one billion seconds old on #{one_billion_seconds_old}."

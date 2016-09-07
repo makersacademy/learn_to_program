12166bbcc909e0a686c535f8777ab39a55b665b4
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-07 12:55:18 +0100

    Completed dictionary sort exercise of chapter 10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,24 @@
 def dictionary_sort arr
-  # your code here
+  recursive_dictionary_sort arr, []
+end
+
+def recursive_dictionary_sort unsorted, sorted
+
+  return sorted if unsorted.length <= 0
+
+  still_unsorted = Array.new
+  smallest = unsorted.pop
+
+  unsorted.each do |tested_object|
+    if tested_object.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = tested_object
+    else
+      still_unsorted.push tested_object
+    end
+  end
+
+  sorted.push smallest
+
+  recursive_dictionary_sort still_unsorted, sorted
 end

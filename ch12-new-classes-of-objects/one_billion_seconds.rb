ec8afd44bcef0ed33c9cfbd69eb0ba2934242e9d
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-09-07 20:50:10 +0100

    Chapter 12: One billion seconds

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,14 @@
-# your code here
+def billionise(time)
+  time + 10 ** 9
+end
+
+puts "Please enter the year you were born (eg. 1980):"
+year = gets.chomp.to_i
+puts "Please enter the month you were born (eg. 9):"
+month = gets.chomp.to_i
+puts "Please enter the day you were born (eg. 4):"
+day = gets.chomp.to_i
+
+birth_date = Time.gm(year, month, day)
+
+puts "I was one billion seconds old on #{billionise(birth_date)}."

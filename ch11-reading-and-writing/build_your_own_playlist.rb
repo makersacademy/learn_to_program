ec4aaf6f382502413b05a4ac2943818d094f68af
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-09-07 17:27:55 +0100

    Build playlist completed

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,36 @@
-# your code here
+# Shuffle method:
+def shuffle arr
+  shuffled = []
+
+  while arr.length > 0
+    rand_index = rand(arr.length)
+    index = 0
+    new_arr = []
+
+    arr.each do |x|
+      if index == rand_index
+        shuffled.push x
+      else
+        new_arr.push x
+      end
+
+      index = index + 1
+    end
+
+    arr = new_arr
+  end
+
+  shuffled
+
+end
+
+# Playlist:
+all_mp3s = shuffle(Dir['**/*.mp3'])
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_mp3s.each do |mp3|
+    f.write mp3+"\n"
+  end
+end
+
+puts "Complete!"

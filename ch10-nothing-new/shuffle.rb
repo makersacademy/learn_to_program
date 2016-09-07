8b11da0d98a91f7199019025b1e599d83764fad5
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-07 16:55:09 +0000

    Chapter 10: Shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,13 @@
-def shuffle arr
-  # your code here
+def shuffle (array)
+  shuffled = []
+  if array.length == 0
+    return array
+  else
+    while array.length > 0
+      n = rand(array.length - 1)
+      shuffled << array[n]
+      array.delete_at(n)
+    end
+  end
+    shuffled
 end

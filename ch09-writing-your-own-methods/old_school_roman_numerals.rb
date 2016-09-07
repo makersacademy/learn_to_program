47dad0064f9a0b0527ede2ccbf81758e760d7087
jinimcoroneo <jinim1983@gmail.com>
2016-09-07 19:47:53 +0100

    updated version of old school roman numerals problem

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,7 +1,20 @@
 def old_roman_numeral(n)
   roman = ""
 
-  roman = roman + "M" * ( n / 1000 )
+  if n(1..3)
+    roman = roman + "I" * ( n % 5 / 1) 
+  if n
+
+
+
+
+
+
+  thousand = roman + "M" * ( n / 1000 )
+
+  if n(900..999)
+    roman = "DM" + thousand
+
   roman = roman + "D" * ( n % 1000 / 500 ) 
   roman = roman + "C" * ( n % 500 / 100 )
   roman = roman + "L" * ( n % 100 / 50 )
@@ -13,4 +26,5 @@ def old_roman_numeral(n)
 
 end
 
-old_roman_numeral(4)
+
+

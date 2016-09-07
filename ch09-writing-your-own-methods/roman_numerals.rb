dafbe024713f53deeb7055cca6151a664e82a290
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-07 10:12:38 +0200

    initial commit of roman_numerals.rb

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -2,6 +2,9 @@ def roman_numeral num
   array = num.to_s.chars.map(&:to_i)
   a_length = array.length
     if a_length == 1
-      array[0] < 5 ? "I" * array[0] : "V" + "I"  * (array[0] - 5)
+      array[0] < 5 ? "I" * array[0] : (array[0] == 9 ? "IX" : "V" + "I"  * (array[0] - 5))
     end
 end
+
+puts (roman_numeral 8)
+puts (roman_numeral 9)

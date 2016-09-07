767db7b3ec218b85438a8af814b0ba4fe11bc41d
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-07 13:26:09 +0100

    completed build your own playlist exercise

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,10 @@
-# your code here
+Dir.chdir("/home/ants/music") do
+	File.new "playlist.m3u" 
+	tracks = Dir["*.mp3"]
+	File.open "playlist.m3u", "w" do |f|
+		tracks.each do |track|
+			f.write(track)
+			f.puts('')
+		end
+	end
+end

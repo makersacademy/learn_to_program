f791700abd79a731adf33c40b879124dc95eff19
Laura Weston <laura.weston@outlook.com>
2016-09-07 22:31:24 +0100

    Ch12 birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,19 @@
 # your code here
+birthdays = {}
+
+
+text = File.read("birthdays.txt")
+
+text.each_line do |line|
+  name, date, year = line.chomp.split(",")
+  birthdays[name] = date
+end
+
+puts "Enter a name"
+name = gets.chomp
+
+if birthdays[name].nil?
+  return "#{name}'s birthday is not listed"
+else
+  puts "#{name}'s birthday is on #{birthdays[name]}"
+end

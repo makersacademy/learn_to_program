ffbedc0f18197431ce7da0e4e61d7c5ee6fe19ba
Rosie <r.gollancz@gmail.com>
2016-09-07 17:13:03 +0100

    Adds answer to Chapter 12 happy birthday exercise

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,28 @@
-# your code here
+puts "In what year were you born (e.g. 1987)?"
+birth_year = gets.chomp
+
+puts "In what number month were you born (e.g. March)?"
+birth_month = gets.chomp
+
+puts "Which day of the month were you born on (e.g. 18)?"
+birth_day = gets.chomp
+
+months = { "January" => 1,
+		   "February" => 2,
+		   "March" => 3,
+		   "April" => 4,
+		   "May" => 5,
+		   "June" => 6,
+		   "July" => 7,
+		   "August" => 8,
+		   "September" => 9,
+		   "October" => 10,
+		   "November" => 11,
+		   "Decembder" => 12 }
+
+
+seconds_in_a_year = (Time.gm(1987) - Time.gm(1986))
+time_of_birth = Time.local("#{birth_year}".to_i, months.key("#{birth_month}"), "#{birth_day}".to_i)
+time_now = Time.new
+birthdays = ((time_now - time_of_birth)/seconds_in_a_year).floor
+puts "SPANK!\n" * birthdays.to_i

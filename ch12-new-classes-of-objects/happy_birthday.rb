da47ba6d4406efec09c806d07497896b6b89094d
Laura Weston <laura.weston@outlook.com>
2016-09-07 20:37:42 +0100

    ch11 & ch 12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp
+
+puts "What month were you born in?"
+month = gets.chomp
+
+puts "What day of the month were you born on?"
+day = gets.chomp
+
+age_in_secs = Time.now - Time.local(year, month, day)
+birthdays = (age_in_secs / 60 / 60 / 24 / 365.25).floor
+
+birthdays.times { puts "SPANK!" }
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,5 @@
-# your code here
+time_of_birth = Time.local(1984, 5, 24, 15, 10)
+
+billion_seconds_old = time_of_birth + 1000000000
+
+puts billion_seconds_old

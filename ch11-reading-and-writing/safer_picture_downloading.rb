57cb3d62dbc51198bf5ee62fb742ed63d086ebb0
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-07 13:07:46 +0100

    completed safer picture downlaoding exercise

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,13 @@
-# your code here
+Dir.chdir("/home/ants/Pictures") do
+	pictures =  Dir["*.jpg"]
+	puts "what would you like to rename this batch?"
+	batch_name = gets.chomp
+	pictures.each_with_index do |picture,index|
+		new_name = "#{batch_name}#{index}.jpg"
+		if File.exist? new_name
+			puts "ERROR: There is already a file called #{new_name}"
+		else
+			File.rename picture, new_name
+		end
+	end
+end

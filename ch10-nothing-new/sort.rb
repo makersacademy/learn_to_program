577a7c18a2076d4bf080411856177b66cc4c5d50
LMCMLJ <mjcalvey2@gmail.com>
2016-09-07 17:58:05 +0100

    Added dictionary sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,15 @@
-def sort arr
-  # your code here
+some_array = ['apple', 'banana', 'tomato', 'bread', 'wine', 'beer']
+sorted_array = []
+
+def sort some_array, sorted_array
+  if some_array =! []
+    first = some_array[0]
+    some_array.select{|x| x <}
+        sort some_array, sorted_array
+      else
+        sort some_array, sorted_array
+      end
+  else
+    return sorted_array
+  end
 end

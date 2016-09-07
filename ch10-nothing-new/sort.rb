bff5b35335f84e159f2ee9387f64298449cbb5cc
hanrattyjen <hanratty.jen@gmail.com>
2016-09-07 12:37:02 +0100

    Chapter 10 exercises

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,15 @@
 def sort arr
-  # your code here
+	recursive_sort(arr, [])
 end
+
+def recursive_sort unsorted_array, sorted_array
+	until unsorted_array.length < 1
+		sorted_array << unsorted_array.delete(unsorted_array.min)
+	end
+	print sorted_array
+end
+
+# sort(['testing', 'with', 'duplicate', 'duplicate', 'words', 'alright'])
+# sort(['f', 'v', 'e', 'd', 'j', 'f'])
+
+# HAVING REAL PROBLEMS GETTING DUPLICATED ELEMENTS INTO THE NEW ARRY

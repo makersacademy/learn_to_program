7c1c9d6b85a80fac7269ef7e630712c065a471e9
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-07 16:34:24 +0000

    Chapter 10: Sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,29 @@
 def sort arr
-  # your code here
+  rec_sort arr, []
 end
+
+def rec_sort unsorted, sorted
+
+  if unsorted.length < 1
+    return sorted
+  end
+
+  still_unsorted = []
+  smallest = unsorted.pop  # smallest placeholder item
+
+  unsorted.each do |i|
+    if i < smallest
+      still_unsorted << smallest
+      smallest = i
+    else
+      still_unsorted << i
+    end
+  end
+  
+  # this returns smallest item,store it in sorted and use recursion to find the next smallest item. 
+  
+  sorted << smallest
+  rec_sort still_unsorted, sorted
+
+end
+

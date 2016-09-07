e52089040f7f298d7a32681ddfddae76be232e9c
Tom <tom.w.page@gmail.com>
2016-09-07 09:41:36 +0000

    improved ask method

---
+++ improvedAskMethod.rb
@@ -0,0 +1,32 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' || reply == 'no')
+      return true if reply == 'yes';
+      return false if reply == 'no';
+      break
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+  end
+
+
+puts "Hello and thank you for ..."
+puts
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you loke eating sopapillas?'
+puts 'Just a few more questions....'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+
+puts
+puts 'DEBRIEFING'
+puts 'Thank you for...'
+puts
+puts wets_bed

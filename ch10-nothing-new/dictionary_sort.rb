2ed5e90b563aadfbd83ef2d6a5d6e446e5fa7ac9
manuciao <mn.sabatino@gmail.com>
2016-09-07 13:45:41 +0100

    Added Ch.10 exercises

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,20 @@
-def dictionary_sort arr
-  # your code here
+def sort(some_array)
+	recursive_sort(some_array, [])
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+	#loop through unsorted array to find smallest word
+	#push to sorted array
+	smallest = unsorted_array.reduce { |memo, word|  memo.downcase < word.downcase ? memo : word}
+	unsorted_array.delete_at(unsorted_array.index(smallest))
+	sorted_array << smallest
+	#repeat (recursion)
+	#if unsorted array length > 0, repeat, else return sorted array
+	if unsorted_array.length > 0
+		recursive_sort(unsorted_array, sorted_array)
+	else
+		sorted_array
+	end
+end
+
+puts sort(["Graig", "kiwi", "Australia", "dawn", "sea"])
--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,21 @@
-def shuffle arr
-  # your code here
+def shuffle(array)
+	rec_shuffle(array, [])
 end
+
+def rec_shuffle(initial_array, shuffled_array)
+	#pick element from array at random
+	rand_index = rand(initial_array.length)
+	element = initial_array[rand_index]
+	#add to shuffled array
+	shuffled_array << element
+	#remove from initial array
+	initial_array.delete_at(rand_index)
+	#repeat 
+	if initial_array.length > 0
+		rec_shuffle(initial_array, shuffled_array) 
+	else
+		shuffled_array
+	end
+end
+
+puts shuffle(["house", "family", "mother", "son", "car", "horse"])

da5e225e151855423b195fc52b0423d66aec9529
gabriel-l-e <gcbassoricci@gmail.com>
2016-09-07 12:49:17 +0100

    Ch9 exercise 1: Improved ask method

---
+++ ch09-writing-your-own-methods/ch9_improvedAskMethod.rb
@@ -0,0 +1,17 @@
+def ask question
+	while true
+		puts question
+		reply = gets.chomp.downcase
+
+		if (reply == "yes" || reply == "no")
+			if reply == "yes"
+				return true
+			else
+				return false
+			end
+			break
+		else
+			puts "Please answer 'yes' or 'no'."
+		end
+	end
+end

0e7d15d1e70818bcff144d66a0348e1ce8e2fe72
manuciao <mn.sabatino@gmail.com>
2016-09-07 17:57:29 +0100

    Added Ch.12 ruby scripts + The Birthday_helper.txt file

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,29 @@
-# your code here
+def birthday
+	birth_dates = Hash.new
+  
+  File.read('Birthday_helper.txt').each_line do |line|
+    line = line.chomp
+    
+    first_comma = 0
+    while line[first_comma] != ',' &&
+      first_comma < line.length
+      first_comma = first_comma + 1
+	  end
+    name = line[0..(first_comma - 1)]
+    date = line[-12..-1]  
+    birth_dates[name] = date
+  end
+  
+  puts "Type the name of the person you want to know his/her birthday!"
+	name = gets.chomp
+
+  date = birth_dates[name]
+  if date == nil
+    puts "Try again.. The name is incorrect!"
+  else
+    puts date[0..-1]
+  end
+	
+end
+
+birthday()
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,32 @@
 def roman_to_integer roman
-  # your code here
+	numerals = {'i' => 1,
+				'v' => 5,
+				'x' => 10,
+				'l' => 50,
+				'c' => 100,
+				'd' => 500,
+				'm' => 1000}
+
+	value = 0
+	index = roman.length - 1
+
+	while index >= 0
+		cur = roman[index].downcase
+		if index == 0
+			value += numerals[cur]
+			break
+		end
+		value += numerals[cur]
+		prev = roman[index - 1].downcase
+		if numerals[cur] > numerals[prev]
+			value -= numerals[prev]
+			index -= 1
+		end
+		index -= 1
+	end
+	
+	return value
 end
+
+puts(roman_to_integer('mcmxcix'))
+puts(roman_to_integer('CCCLXV'))

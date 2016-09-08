bb67619151afe89e713bc3d7d994c7feaf7dd5b6
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-08 17:41:20 +0100

    CHapter 9 - Old Roman Numeral Converter Created

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,12 @@
 def old_roman_numeral num
-  # your code here
+  roman_numeral = ""
+  roman_numeral = roman_numeral + "M" * (num / 1000)
+  roman_numeral = roman_numeral + "D" * (num % 1000 / 500)
+  roman_numeral = roman_numeral + "C" * (num % 500 / 100)
+  roman_numeral = roman_numeral + "L" * (num % 100 /50)
+  roman_numeral = roman_numeral + "X" * (num % 50 / 10)
+  roman_numeral = roman_numeral + "V" * (num % 10 / 5)
+  roman_numeral = roman_numeral + "I" * (num % 5 / 1)
 end
+
+puts old_roman_numeral(2456)

4401c1386b91f969136c7487224fcd38325932e2
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-08 14:42:07 +0100

    Finished playlist task in chapter 11

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,22 @@
-# your code here
+def shuffle arr
+  second_array = Array.new
+  n = 0
+  alength = arr.length
+
+  alength.times do
+    random_number = rand(0...alength)
+    second_array[n] = arr[random_number]
+    arr.delete(arr[random_number])
+    alength -=  1
+    n += 1
+  end
+  return second_array
+
+end
+
+files = shuffle(Dir['**/".mp3"'])
+File.open 'playlist.m3u', 'w' do |f|
+  files.each do |file|
+    f.write file + '\n'
+  end
+end

271a0a3fd0470cc5383ca2557ded927ae20d0792
Malinna Leach <malinnab@yahoo.com>
2016-09-08 20:35:24 +0100

    granddaddy clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,14 @@
 def grandfather_clock &block
-  # your code here
+  if Time.new.hour > 12
+    count = Time.new.hour-12
+  elsif Time.new.hour == 0
+    count = 12
+  else
+    count = Time.new.hour
+  end
+  count.times &block
+end
+
+grandfather_clock do
+  puts "DONG!"
 end

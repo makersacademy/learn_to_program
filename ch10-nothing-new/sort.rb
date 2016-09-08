ad957b151aadb07e6c9123a837f9a660d5fa08fb
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-08 19:35:10 +0300

    Chapter 10. recursive sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,19 @@
 def sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  smallest_id = 0
+  
+  (unsorted_array.length-1).times { |i|
+    smallest_id = i+1 if unsorted_array[smallest_id] > unsorted_array[i+1]
+  }
+  
+  sorted_array.push(unsorted_array.delete_at(smallest_id))
+    
+  if unsorted_array.length > 0
+    recursive_sort unsorted_array, sorted_array
+  else
+    return sorted_array
+  end
 end

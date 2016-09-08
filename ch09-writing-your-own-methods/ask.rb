bf9a2beef10965008b0b08929785f7690fc35214
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-08 09:22:53 +0000

    added ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,21 @@
 def ask question
-  # your code here
+    while true
+    puts question
+    reply = gets.chomp.downcase
+    
+    if (reply == 'yes' || reply == 'no')
+        if reply == 'yes'
+            return true
+        else
+            return false
+        end
+    else
+        puts 'Please answer "Yes" or "No".'
+    end
+    end
 end
+
+ask "question 1"
+ask "question 2"
+ruby= ask "do you like ruby?"
+puts "User likes Ruby: #{ruby}."

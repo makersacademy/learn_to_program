77da74e10a15f0ff545881e9a658923e9368570a
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-08 19:49:21 +0100

    CHapter 10 - Shuffle exercise complete (with help from the solutions)

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,25 @@
-def shuffle arr
-  # your code here
+def shuffle array
+  shuffled = []
+
+  while array.length > 0
+
+    random_index = rand(array.length)
+
+    current_index = 0
+    new_array = []
+
+    array.each do |index|
+      if current_index == random_index
+        shuffled.push index
+      else
+        new_array.push index
+      end
+      current_index = current_index + 1
+    end
+
+    array = new_array
+  end
+  shuffled
 end
+
+puts(shuffle(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]))

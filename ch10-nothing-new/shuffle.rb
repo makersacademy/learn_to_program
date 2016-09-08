6749ebdaf9b56398ce2b914c439f32de5d9fc71e
hanrattyjen <hanratty.jen@gmail.com>
2016-09-08 14:54:39 +0100

    Chap 10 exercises

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -4,11 +4,11 @@ end
 
 def recursive_shuffle old_array, new_array
 	until old_array.length < 1
-	new_array << old_array.delete_at(rand(old_array.length))
+		new_array << old_array.delete_at(rand(old_array.length))
 	end
 	new_array
 end
 
-# shuffle([1, 4, 5, 10, 2])
-# shuffle(['testing', 'with', 'words'])
-# shuffle(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+shuffle([1, 4, 5, 10, 2, 2])
+shuffle(['testing', 'with', 'words'])
+shuffle(['a', 'b', 'c', 'd', 'e', 'f', 'g'])

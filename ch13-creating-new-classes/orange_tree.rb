38d34eddb1398ecf80236be82a9989bec3b95f49
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-09-08 16:55:51 +0100

    Chapter 13: Orange tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,11 +1,45 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
-
 class OrangeTree
-  # your code here
+  def initialize
+    @age = 0
+    @height = 0
+    @orange_count = 0
+  end
+
+  def count_the_oranges
+    if @age > 25
+      'A dead tree has no oranges. :('
+    else
+      @orange_count.round(0)
+    end
+  end
+
+  def pick_an_orange
+    if @age > 25
+      'A dead tree has nothing to pick. :('
+    elsif @age >= 6
+      @orange_count = [@orange_count - 1, 0].max
+    end
+  end
+
+  def height
+    if @age > 25
+      'A dead tree is not very tall. :('
+    else
+      @height.round(1)
+    end
+  end
+
+  def one_year_passes
+    @age = @age + 1
+    @height = @height + 0.4
+    @orange_count = (@height * 15 - 25).round(1)
+
+    if @age > 26
+      'A year later, the tree is still dead. :('
+    elsif @age > 25
+      'Oh, no! The tree is too old, and has died. :('
+    elsif @age >= 6
+      "This year your tree grew to #{self.height}m tall, and produced #{self.count_the_oranges} oranges."
+    end
+  end
 end

0574f6304687b04c537e155608a4cf1ac5890c77
Laura Weston <laura.weston@outlook.com>
2016-09-08 11:32:16 +0100

    Update ch12 birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,12 +1,12 @@
-# your code here
+birthdays_file = Dir["birthdays.txt"]
+
 birthdays = {}
 
-
-text = File.read("birthdays.txt")
+text = File.read(birthdays_file[0])
 
 text.each_line do |line|
   name, date, year = line.chomp.split(",")
-  birthdays[name] = date
+  birthdays[name] = Time.gm(year, *date.split)
 end
 
 puts "Enter a name"
@@ -15,5 +15,7 @@ name = gets.chomp
 if birthdays[name].nil?
   return "#{name}'s birthday is not listed"
 else
-  puts "#{name}'s birthday is on #{birthdays[name]}"
+  next_birthday = "#{birthdays[name].day}/#{birthdays[name].month}"
+  age = (Time.now - birthdays[name]) / 60 / 60 / 24 / 365.25
+  puts "#{name} will be #{age.ceil} on #{next_birthday}"
 end

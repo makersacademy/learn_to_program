96a9ba3ae756f6509bded7d289421ff9d4a83bc1
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-08 21:34:24 +0100

    Finished happy birthday exercise in chapter 12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+puts "What year were you born in?"
+year = gets.chomp.to_i
+puts "and what month were you born in?"
+month = gets.chomp.to_i
+puts "ok...and what day?"
+day = gets.chomp.to_i
+
+smacks = 1
+now = Time.now
+
+
+while Time.new(year + smacks, month, day) <=  now
+  puts "SMACK"
+  smacks += 1
+end

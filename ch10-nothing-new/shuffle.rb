b6ca4e758987d80f38d8fc061c43d58e2a5c8cf9
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-08 13:50:54 +0200

    Initial and final commit of shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,17 @@
-def shuffle arr
-  # your code here
+def shuffle(arr)
+  rec_unsort(arr, [])
 end
+
+def rec_unsort(sorted, shuffeled)
+  if sorted.length == 0
+    return shuffeled
+  end
+
+  object = sorted[rand(0..(sorted.length-1))]
+  shuffeled.push object
+  sorted.delete(object)
+
+  rec_unsort(sorted, shuffeled)
+end
+
+puts(shuffle(['Guess', 'it\'s', 'true', 'I\'m', 'not', 'good', 'at', 'a', 'one-night', 'stand']))

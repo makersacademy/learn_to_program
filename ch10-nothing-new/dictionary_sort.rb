f793330acac80688e72326bc55cef555df3e3b02
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-08 20:03:33 +0100

    Chapter 10 - Dictionary Sort complete (without aid from solutions, using previous sort.rb)

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,23 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort array
+  recursive_dictionary_sort array, []
 end
+
+def recursive_dictionary_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+  smallest = unsorted.pop
+  still_unsorted = []
+  unsorted.each do |test_object|
+    if test_object.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = test_object
+    else
+      still_unsorted.push test_object
+    end
+  end
+  sorted.push smallest
+  recursive_dictionary_sort still_unsorted, sorted
+end
+
+puts(dictionary_sort(['can','feel','singing','Like','A','can']))

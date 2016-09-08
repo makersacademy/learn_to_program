e956e91d789b65e6c7de8f32fad73dbdd274b209
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-09-08 11:21:25 +0100

    Chapter 13: Creating new classes/extend built in classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,41 @@
+def fact(num)
+  if num <= 1
+    1
+  else
+    num * fact(num - 1)
+  end
+end
+
+def old_roman_numeral(num)
+  left = num
+  m = left / 1000
+  left = left % 1000
+  d = left / 500
+  left = left % 500
+  c = left / 100
+  left = left % 100
+  l = left / 50
+  left = left % 50
+  x = left / 10
+  left = left % 10
+  v = left / 5
+  left = left % 5
+  i = left / 1
+  left = left % 1
+
+  ("M" * m) + ("D" * d) + ("C" * c) + ("L" * l) +
+  ("X" * x) + ("V" * v) + ("I" * i)
+end
+
+
 class Integer
-  # your code here
+  def factorial
+    fact(self)
+  end
+
+
+
+  def to_roman
+    old_roman_numeral(self)
+  end
 end

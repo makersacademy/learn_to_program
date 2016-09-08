394822ea88a2648b14d29f9c1cd1a0d7b2e56a1d
Malinna Leach <malinnab@yahoo.com>
2016-09-08 21:37:10 +0100

    better logging

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,25 @@
-def log desc, &block
-  # your code here
+$count = 0
+
+def better_log desc, &block
+  puts (" " * $count) + "Beginning \"#{desc}\"..."
+  end_count = $count
+  $count += 1
+  output = block.call
+  puts (" " * end_count) + "...\"#{desc}\" finished, returning: #{output}"
+  $count -= 1
 end
+
+#program_log 'outer block' do
+#      program_log 'some little block' do
+#        program_log "teeny" do
+#          "lots of love"
+#        end
+#        1**1 + 2**2
+#      end
+#
+#      program_log 'yet another block' do
+#        '!doof iahT ekil I'.reverse
+#      end
+#
+#      '0' == 0
+#end

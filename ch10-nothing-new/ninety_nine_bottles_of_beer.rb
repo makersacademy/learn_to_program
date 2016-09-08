7f4942e59d85bc722c28798c2e1b10ed669fa951
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-08 22:43:04 +0200

    Commit beer bottle song...

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,19 @@
-# your code here
+load 'english_number.rb'
+
+def beer
+
+  num_at_start = 9999
+  num_now = num_at_start
+  while num_now > 2
+    puts english_number(num_now).capitalize + ' bottles of beer on the wall, ' + english_number(num_now) + ' bottles of beer!' 
+    num_now = num_now - 1
+    puts 'Take one down, pass it around, ' + english_number(num_now) + ' bottles of beer on the wall!' 
+  end
+
+
+puts "Two bottles of beer on the wall, two bottles of beer!"
+puts "Take one down, pass it around, one bottle of beer on the wall!"
+puts "One bottle of beer on the wall, one bottle of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall!"
+
+end

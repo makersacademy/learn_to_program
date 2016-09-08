a87d1c747a74dafd1d4c098aa62a158f1d5ad291
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-08 11:27:14 +0300

    Chapter 10. Dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+  smallest_id = 0
+  
+  (unsorted_array.length-1).times { |i|
+    if unsorted_array[smallest_id].downcase > unsorted_array[i+1].downcase
+      smallest_id = i+1
+    end
+  }
+  
+  sorted_array.push(unsorted_array[smallest_id])
+  unsorted_array.delete_at(smallest_id)
+  
+  if unsorted_array.length > 0
+    recursive_sort unsorted_array, sorted_array
+  else
+    return sorted_array
+  end
 end

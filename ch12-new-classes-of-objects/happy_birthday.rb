a8a99a7cc1de19ec05806a9def6e90c4bf0674ad
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-08 13:17:45 +0100

    completed party like its roman to integer exercise

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+puts "What year were you born?:"
+year = gets.chomp
+puts "What month?:"
+month = gets.chomp
+puts "What day?"
+day = gets.chomp
+
+input_time = Time.new(year,month,day)
+time_now = Time.new
+
+difference = time_now - input_time
+seconds_in_a_year = 60 * 60 * 24 * 365
+
+n = (difference / seconds_in_a_year).to_i
+n.times { puts "SPANK!" }

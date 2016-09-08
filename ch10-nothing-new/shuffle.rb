3ad9190acbebf1258c27fe5c26d33039d903f28e
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-08 20:57:28 +0000

    Shuffling

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,21 @@
 def shuffle arr
-  # your code here
+
+    shuffled_array = []
+    
+    while arr.length > 0
+    index = 0
+    not_shuffled = []
+    random_index = rand(arr.length)
+    
+      arr.each do |tested_object|
+        if index == random_index
+          shuffled_array.push tested_object
+        else 
+          not_shuffled.push tested_object
+        end
+        index += 1
+      end
+      arr = not_shuffled
+    end
+    shuffled_array
 end

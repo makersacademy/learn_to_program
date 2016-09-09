8c9f281fdb68724fd23f83ffb5c444caf545eb6f
amidabrian51 <briankaty1@me.com>
2016-09-09 23:08:00 +0100

    This is changes to english number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,78 +1,127 @@
-def english_number(number)
-	if number < 0
-		return "Please enter a number that isn't negative."
-	end
+def english_number number
+if number < 0 # no neg numbers
+return 'Please enter a number that isn\'t negative'
+end
+if number == 0
+return 'zero'
+end
 
-	if number == 0
-		return "zero"
-	end
+# no more special cases, no more returns
 
-	num_string = ""
+num_string = '' # This is the string we will return, define it an start 'empty'
 
-	ones_place = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-	tens_place = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-	teenagers = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+ones_place = [ 'one', 'two', 'three',
+'four', 'five', 'six',
+'seven', 'eight', 'nine']
 
-	left = number
+tens_place = [ 'ten', 'twenty', 'thirty',
+'forty', 'fifty', 'sixty',
+'seventy', 'eighty', 'ninty']
 
-	write = left / 1000000
-	left = left - write * 1000000
+teenagers = [ 'eleven', 'twelve', 'thirteen',
+'fourteen', 'fifteen', 'sixteen',
+'seventeen', 'eighteen', 'nineteen']
 
-	if write > 0
-		millions = english_number write
-		num_string = num_string + millions + " million"
 
-		if left > 0
-			num_string = num_string + " "
-		end
-	end
+# "left" is how much of the number we still have to write
+# "write" is which part we are writing out at the time
 
-	write = left / 1000
-	left = left - write * 1000
+left = number
 
-	if write > 0
-		thousands = english_number write
-		num_string = num_string + thousands + " thousand"
+# trillions
+write = left/1000000000000
+left = left - write*1000000000000
 
-		if left > 0
-			num_string = num_string + " "
-		end
-	end
+if write > 0
+trillions = english_number write
+num_string = num_string + trillions + ' trillion'
 
-	write = left / 100
-	left = left - write * 100
+if left > 0
+num_string = num_string + ' '
+end
+end
 
-	if write > 0
-		hundreds = english_number write
-		num_string = num_string + hundreds + " hundred"
+# billions
+write = left/1000000000
+left = left - write*1000000000
 
-		if left > 0
-			num_string = num_string + " "
-		end
-	end
+if write > 0
+billions = english_number write
+num_string = num_string + billions + ' billion'
 
-	write = left / 10
-	left = left - write * 10
+if left > 0
+num_string = num_string + ' '
+end
+end
 
-	if write > 0
-		if ((write == 1) and (left > 0))
-			num_string = num_string + teenagers[left - 1]
-			left = 0
-		else
-			num_string = num_string + tens_place[write - 1]
-		end
+# millions
+write = left/1000000
+left = left - write*1000000
 
-		if left > 0
-			num_string = num_string + " "
-		end
-	end
+if write > 0
+millions = english_number write
+num_string = num_string + millions + ' million'
 
-	write = left
-	left = 0
+if left > 0
+num_string = num_string + ' '
+end
+end
 
-	if write > 0
-		num_string = num_string + ones_place[write - 1]
-	end
+# thousands
+write = left/1000
+left = left - write*1000
 
-	num_string
+if write > 0
+thousands = english_number write
+num_string = num_string + thousands + ' thousand'
+
+if left > 0
+num_string = num_string + ' '
+end
+end
+
+write = left/100 # how many hundreds left
+left = left - write*100 # subtract off those hundreds
+
+if write > 0
+# recursion
+hundreds = english_number write
+num_string = num_string + hundreds + ' hundred'
+
+if left > 0
+# adds a space
+num_string = num_string + ' '
+end
+end
+
+write = left/10 # how many tens left
+left = left - write*10 # subtract off those tens
+
+if write > 0
+if ((write == 1) and (left > 0))
+# exception for teenagers
+num_string = num_string + teenagers[left-1]
+# -1 because of the array ordering [0] is eleven etc.
+# with teenagers the digit in the ones place is taken care of so nothing left
+left = 0
+else
+num_string = num_string + tens_place[write-1]
+end
+
+if left > 0
+# add a space
+num_string = num_string + '-'
+end
+end
+
+write = left # how many ones left
+left = 0 # subtract off those ones
+
+if write > 0
+num_string = num_string + ones_place[write-1]
+end
+
+# now we just return the num_string
+
+num_string
 end
--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,85 +1,153 @@
 # your code here
 def english_number number
-  # your code here
-  if number < 0
-    return 'Please enter a number that isn\'t negative'
-  end
-  if number == 0
-    return 'zero'
-  end
-  num_string = ''
-  ones_place = ['one', 'two', 'three',
-                  'four', 'five', 'six', 'seven',
-                'eight', 'nine',]
-  tens_place = ['ten', 'twenty', 'thirty',
-                  'forty', 'fifty', 'sixty', 'seventy',
-                'eighty', 'ninety']
-  teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-                'seventy', 'eighteen', 'nineteen']
-  zillions = [[ 'hundred',    2],
-              [ 'thousand',    3],
-              [ 'million',    6],
-              [ 'billion',    9],
-              [ 'trillion',    12],
-              [ 'quadrillion',    15],
-              [ 'quintillion',    18],
-              [ 'sextillion',    21],
-              [ 'septillion',    24],
-              [ 'octillion',    27],
-              [ 'nonillion',    30],
-              [ 'decillion',    33],
-              [ 'undecillion',    36],
-              [ 'duodecillion',    39],
-              [ 'tredicillion',    42],
-              [ 'quattuordecillion',    45],
-              [ 'quindecillion',    48],
-              [ 'sexdecillion',    51],
-              [ 'septendecillion',    54],
-              [ 'octodecillion',    57],
-              [ 'novemdecillion',    60],
-              [ 'vigintillion',    63],
-              [ 'googol',    100],]
-  left = number
+if number < 0 # no neg numbers
+return 'Please enter a number that isn\'t negative'
+end
+if number == 0
+return 'zero'
+end
 
-  while zillions.length > 0
-      zil_pair = zillions.pop
-      zil_name =          zil_pair[0]
-      zil_base = 10 ** zil_pair[1]
-      write = left/zil_base
-      left = left - write* zil_base
+# no more special cases, no more returns
+
+num_string = '' # This is the string we will return, define it an start 'empty'
+
+ones_place = [ 'one', 'two', 'three',
+'four', 'five', 'six',
+'seven', 'eight', 'nine']
+
+tens_place = [ 'ten', 'twenty', 'thirty',
+'forty', 'fifty', 'sixty',
+'seventy', 'eighty', 'ninty']
+
+teenagers = [ 'eleven', 'twelve', 'thirteen',
+'fourteen', 'fifteen', 'sixteen',
+'seventeen', 'eighteen', 'nineteen']
+
+
+# "left" is how much of the number we still have to write
+# "write" is which part we are writing out at the time
+
+left = number
+
+# trillions
+write = left/1000000000000
+left = left - write*1000000000000
 
 if write > 0
-  prefix = english_number write
-  num_string = num_string + prefix + '' + zil_name
-  if left > 0
-    num_string = num_string + ''
-  end
-end
-end
-write = left/10
-left = left - write*10
-if write > 0
-  if ((write == 1 ) and (left > 0))
-    num_string= num_string + teenagers[left-1]
-    left = 0
-  else
-    num_string = num_string + tens_place[write-1]
-  end
+trillions = english_number write
+num_string = num_string + trillions + ' trillion'
+
 if left > 0
-  num_string = num_string + '-'
+num_string = num_string + ' '
 end
 end
-write = left
-left = 0
+
+# billions
+write = left/1000000000
+left = left - write*1000000000
+
 if write > 0
-  num_string = num_string + ones_place[write-1]
+billions = english_number write
+num_string = num_string + billions + ' billion'
+
+if left > 0
+num_string = num_string + ' '
 end
+end
+
+# millions
+write = left/1000000
+left = left - write*1000000
+
+if write > 0
+millions = english_number write
+num_string = num_string + millions + ' million'
+
+if left > 0
+num_string = num_string + ' '
+end
+end
+
+# thousands
+write = left/1000
+left = left - write*1000
+
+if write > 0
+thousands = english_number write
+num_string = num_string + thousands + ' thousand'
+
+if left > 0
+num_string = num_string + ' '
+end
+end
+
+write = left/100 # how many hundreds left
+left = left - write*100 # subtract off those hundreds
+
+if write > 0
+# recursion
+hundreds = english_number write
+num_string = num_string + hundreds + ' hundred'
+
+if left > 0
+# adds a space
+num_string = num_string + ' '
+end
+end
+
+write = left/10 # how many tens left
+left = left - write*10 # subtract off those tens
+
+if write > 0
+if ((write == 1) and (left > 0))
+# exception for teenagers
+num_string = num_string + teenagers[left-1]
+# -1 because of the array ordering [0] is eleven etc.
+# with teenagers the digit in the ones place is taken care of so nothing left
+left = 0
+else
+num_string = num_string + tens_place[write-1]
+end
+
+if left > 0
+# add a space
+num_string = num_string + '-'
+end
+end
+
+write = left # how many ones left
+left = 0 # subtract off those ones
+
+if write > 0
+num_string = num_string + ones_place[write-1]
+end
+
+# now we just return the num_string
+
 num_string
 end
 
-num_at_start = 5
-num_bot = proc { |n| "#{english_number n} bottle#{n == 1 ? '' : 's'}"}
-num_at_start.downto(2) do |num|
-  puts "#{num_bot[num]} of beer on the wall , #{num_bot[num]} of beer!".capitalize
-  puts "Take one down, pass it around.#{num_bot[num-1]} of beer on the wall!"
+bottles = 999
+while bottles != 0
+puts english_number(bottles).to_s + ' bottles of beer on the wall'
+puts english_number(bottles).to_s + ' bottles of beer'
+bottles = bottles - 1
+puts 'take one down, pass it around'
+
+if bottles == 1
+puts english_number(bottles).to_s + ' bottle of beer on the wall'
+else
+puts english_number(bottles).to_s + ' bottles of beer on the wall'
+end
+
+puts ''
+
+if bottles == 1
+puts english_number(bottles).to_s + ' bottle of beer on the wall'
+puts english_number(bottles).to_s + ' bottle of beer'
+bottles = bottles - 1
+puts 'take one down, pass it around'
+puts english_number(bottles).to_s + ' bottles of beer on the wall'
+end
+
 end

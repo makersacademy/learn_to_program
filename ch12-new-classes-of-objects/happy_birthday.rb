92ce87a0e0974b939fff474f0ad7e220bd04805c
jinimcoroneo <jinim1983@gmail.com>
2016-09-09 17:38:47 +0100

    method for finding someone's age

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,15 @@
-# your code here
+
+def age(year, month, date)
+  seconds = (60 * 60 * 24 * 7 * 52.1429)
+  birthday = Time.gm(year, month, date)
+  today = Time.now
+
+  age = (today - birthday) / seconds
+  return age
+
+end
+
+years = age(1980, 1, 6)
+puts "you get #{years.floor} spankings!"
+
+

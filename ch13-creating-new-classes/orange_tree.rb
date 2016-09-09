7ca293d4731b81d615d507b80daa33c45e51731d
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-09 14:21:26 +0000

    Chapter 13: Orange Tree

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -5,7 +5,50 @@
 # have the tree die after 25 years.
 # check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
 
-
 class OrangeTree
-  # your code here
+  def initialize
+    @age = 0
+    @height = 0
+    @oranges = 0
+  end
+
+  def height
+    if @age <= 25
+      @height
+    else
+      "A dead tree is not very tall. :("
+    end
+  end
+
+  def one_year_passes
+    @age += 1
+    @height = (@age * 0.4).round(1)
+    if @age <= 5
+      @oranges = 0
+    elsif @age <= 25
+      @oranges = (@height * 15 - 25)
+        "This year your tree grew to #{@height}m tall, and produced #{@oranges.round} oranges."
+    elsif @age == 26
+        "Oh, no! The tree is too old, and has died. :("
+    else
+        "A year later, the tree is still dead. :("
+    end
+  end
+
+  def count_the_oranges
+    if @age <= 25
+       @oranges.round
+    else
+       "A dead tree has no oranges. :("
+    end
+  end
+
+  def pick_an_orange
+    if @age <=25
+       @oranges -= 1
+    else
+       "A dead tree has nothing to pick. :("
+    end
+  end
 end
+

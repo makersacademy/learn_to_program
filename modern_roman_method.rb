7fc3909cfea8bc39bf8fd59071c7ee0c34246f91
Tom <tom.w.page@gmail.com>
2016-09-09 12:53:27 +0000

    Modern roman  .method

---
+++ modern_roman_method.rb
@@ -0,0 +1,47 @@
+class Integer
+  def old_roman_numeral
+    num = self
+    numeral = ('')
+    ((num / 1000).to_i).times {numeral << 'M'} #adds number of M´s needed for 1000
+    num = num % 1000
+
+    if num >= 500
+      numeral << 'D'
+      ((num / 100).to_i - 5).times {numeral << 'C'}
+    elsif num == 400
+      numeral << 'C' + 'D'
+    else
+      ((num / 100).to_i).times {numeral << 'C'}
+    end
+
+    num = num % 100
+    if num >= 50
+      numeral << 'L'
+      ((num / 10).to_i - 5).times {numeral << 'X'}
+    elsif num == 40
+      numeral << 'X' + 'L'
+
+    else
+      ((num / 10).to_i).times {numeral << 'X'}
+    end
+
+    num = num % 10
+    if num > 5
+      numeral << 'V' + ('I' * (num - 5))
+      ((num / 1).to_i - 5).times {numeral << 'I'}
+    elsif num == 5
+      numeral << 'V'
+    elsif num == 4
+      numeral << 'IV'
+    else
+      ((num / 1).to_i).times {numeral << 'I'}
+    end
+
+    puts numeral
+  end
+
+  #old_roman_numeral
+
+end
+
+30.old_roman_numeral 

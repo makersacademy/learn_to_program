6d361a0ae4504d17937a6469e5894f46339b735f
Frankie Bell <fbell.pro@gmail.com>
2016-09-09 09:45:12 +0100

    struggled with this one

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,31 @@
-# your code here
+STDOUT.sync = true #immediately see the results on screen rather that internally
+thread.abort_on_exception = true #stops the script running if there is an issue
+
+Dir.chdir '/Users/Fbell/Projects/week_2/learn_to_program/ch11-reading-and-writing'
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts 'What would you like to call these pictures?'
+batch_name = gets.chomp
+
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.'
+
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end
+
+puts
+puts 'Done, kapow!'

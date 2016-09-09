10f9b8c49d7abf87602f30dfce396a76991b7363
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-09 11:06:42 +0000

    modified dictionary_sort.rb and sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,31 @@
 def sort arr
-  # your code here
+  unsorted=[]
+  sorted=[]
+  
+  
+  puts "Type the words that you want me to alphabetise"
+  #can I delete this next line? does the while loop handle this?
+  user_input=gets.chomp.downcase
+  
+  while user_input !=''
+    unsorted_words.push(user_input)
+    user_input=gets.chomp.downcase
+  end
+  
+  #each iterates over unsorted and compares them, finding the smallest
+  unsorted.each do |x,y|
+      if x < y
+          
+      end
+  end
+  
+  
+  #the smallest word is pushed to sorted
+  
+  #this is done recursively until unsorted is empty, at which point the program
+  #probably stops itself
+  
+ 
+  puts sorted
+  
 end

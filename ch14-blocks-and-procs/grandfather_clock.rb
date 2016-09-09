a3632b3a8de1ba1fb2ee921b1043a558126a12d4
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-09 13:27:39 +0100

    completed grandfather clock exercise

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,13 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour
+  if hour >= 13
+  	hour = hour - 12
+  end
+  hour.times {
+  	block.call
+  }
+end
+
+log "dong" do
+	puts "DONG!"
 end

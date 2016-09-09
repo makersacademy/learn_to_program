6ef4c9d53822de97efaad31d200393c2a4075611
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-09 19:15:18 +0000

    Chapter 14: Grandfather Clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,8 @@
 def grandfather_clock &block
-  # your code here
+  hour = (Time.new.hour + 11) % 12 + 1 # if Time.new.hour % 12 is coded only, hour at 12 and 24 will == 0
+  hour.times {block.call}
+end
+
+grandfather_clock do
+  puts "DONG!"
 end

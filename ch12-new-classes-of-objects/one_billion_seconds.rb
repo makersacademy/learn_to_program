c8699eb14c0bca425db55e4d24b4e646f608761f
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-09 16:50:06 +0200

    Another final commit of one_billion_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -3,7 +3,7 @@ def the_date_I_turn_a_billion
     year = 1985
     month = 10
     day = 15
-    puts "The day Christopher Sanchez turns a billion seconds old, given that he was born at Greenwich in 1985-10-15,"
+    puts "The day Christopher Sanchez turns a billion seconds old, given that he was born at Greenwich in 1985-10- 15,"
     puts "the date when he becomes really old would be on #{Time.gm(year, month, day) + (10**12)}"
 end
 

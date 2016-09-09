a1465ac6706dc62d178b31b9986cd9c7a7344014
jinimcoroneo <jinim1983@gmail.com>
2016-09-09 11:21:45 +0100

    Initial sorting problem

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,44 @@
-def sort arr
-  # your code here
+def sort(array)
+
+  recursive_sort(array, [])
+
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  smallest_word = unsorted_array.pop
+  still_unsorted_array = []
+
+  unsorted_array.each {|array_element| 
+    if array_element < smallest_word
+      still_unsorted_array << smallest_word
+      smallest_word = array_element
+
+    else
+      still_unsorted_array << array_element
+
+    end
+  }
+
+  sorted_array.push(smallest_word)
+
+  recursive_sort(still_unsorted_array, sorted_array)
+
+end
+
+
+
+names =["Jini", "John", "Alex", "Andrew", "Josh"]
+puts sort(names)
+
+
+
+
+
+
+
+
+

087c6215f91cff2fe4efe775f904f372d14a3c33
michkles <michkles@gmail.com>
2016-09-09 15:20:49 +0100

    added 4 files from ch10

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,22 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arg)
+  rec_dict(arg, [])
+end
+
+def rec_dict(unsorted, sorted)
+  if unsorted.length <= 0
+    return sorted
+  end
+  smallest = unsorted.pop #removes the last element! [b, c, d] => [b, c]
+  still_unsorted = []
+
+  unsorted.each do |array_item|
+    if array_item.downcase < smallest.downcase
+      still_unsorted.push(smallest) # adds an item to the end of an array
+      smallest = array_item
+    else
+      still_unsorted.push(array_item)
+    end
+  end
+  sorted.push(smallest)
+  rec_dict(still_unsorted, sorted)
 end

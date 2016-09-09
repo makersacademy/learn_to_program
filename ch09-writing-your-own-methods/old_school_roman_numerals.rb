6d9c246f4d49cda64bf803f24e7378c5d68dba53
Royston <rlqhenson@hotmail.com>
2016-09-09 15:43:37 +0100

    added three more files

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
-def old_roman_numeral num
-  # your code here
+def old_roman_numeral(number)
+  num = ""
+  num = num + "M" * (number / 1000)
+  num = num + "D" * (number % 1000 / 500)
+  num = num + "C" * (number % 500 / 100)
+  num = num + "L" * (number % 100 / 50)
+  num = num + "X" * (number % 50 / 10)
+  num = num + "V" * (number % 10 / 5)
+  num = num + "I" * (number % 5 / 1)
 end
+
+puts old_roman_numeral(4)
+puts old_roman_numeral(5)
+puts old_roman_numeral(10)
+puts old_roman_numeral(19)
+puts old_roman_numeral(22)
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,2 @@
-# your code here
+age = Time.now - Time.local(1985, 6, 5, 14, 00)
+puts "I am #{age} seconds old!"

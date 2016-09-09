f66992ca4570ab78b19aebcaf63de2169ad58503
Laurent Bo <misterbl@hotmail.com>
2016-09-09 08:22:36 +0000

    final happy_birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,3 +1,8 @@
-age = (Time.new - Time.local(1947, 3, 30)) / 31449600
-puts age
-puts "SPANK!" * age
+years = ((Time.new - Time.local(1947, 3, 30)) / 31449600).floor
+months = ((years - years.floor) * 12).floor
+if months >= 1
+puts "#{years}years and #{months}months"
+else
+puts "#{years}years"
+end
+puts "SPANK!" * years

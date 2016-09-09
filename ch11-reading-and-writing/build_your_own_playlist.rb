ff1e96d065aec655778c8ce254fbaef864fb167a
Frankie Bell <fbell.pro@gmail.com>
2016-09-09 10:33:03 +0100

    tried to overcomplicate things again, revisit and refine is the technique

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,12 @@
-# your code here
+# Music directory is /Users/Fbell/Music/iTunes/iTunes_Media/Music
+# Playlist is in  /Users/Fbell/Music/iTunes/iTunes_Media/playlist.m3u
+
+all_mp3s = shuffle(Dir['/**/*.mp3'])
+
+File.open 'playlist.m3u' , 'w' do |x|
+  all_mp3s.each do |mp3|
+    x.write mp3+"\n"
+  end
+end
+
+puts 'Done'

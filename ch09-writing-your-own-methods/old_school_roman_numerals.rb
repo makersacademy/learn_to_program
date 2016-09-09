4fa9da7fb0193a250ebe0cf6fa6cd67269d025ef
Anna Lucking <annavlucking@hotmail.co.uk>
2016-09-09 10:03:15 +0100

    old school roman modified

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+
+oldrom = ""
+
+oldrom = oldrom + 'M' * (num / 1000)
+oldrom = oldrom + 'D' * (num % 1000 / 500)
+oldrom = oldrom + 'C' * (num % 500 / 100)
+oldrom = oldrom + 'L' * (num % 100 / 50)
+oldrom = oldrom +'X' * (num % 50 / 10)
+oldrom = oldrom + 'V' * (num % 10 / 5)
+oldrom = oldrom + 'I' * (num % 5 / 1)
+oldrom
 end

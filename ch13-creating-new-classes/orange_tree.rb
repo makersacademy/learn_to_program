fdfd82936db64c6f0c2b3535c78e1646b59dc28b
Rosie <r.gollancz@gmail.com>
2016-09-09 12:32:21 +0100

    Adds answer to Chapter 13 orange tree exercise

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,11 +1,59 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
-
 class OrangeTree
-  # your code here
+  def initialize
+    @height = 0
+    @age = 0
+    @alive = true
+    @orange_count = 0
+  end
+
+  def height
+ 	if @alive
+ 		return @height
+  	else
+  		return "A dead tree is not very tall. :("
+  	end
+  end
+
+  def one_year_passes
+    @orange_count = 0
+  	if @alive
+  		if @age <= 5
+  			@age = @age + 1
+  			height_under_5 = @height + 0.4
+  			@height = height_under_5.round(1)
+			return "This year your tree grew to #{@height}m tall, but it is still too young to produce oranges."
+		end
+
+  		if @age > 5 && @age < 25
+  			@age = @age + 1
+  			height_over_5 = @height + 0.4
+  			@height = height_over_5.round(1)
+  			@orange_count =  (@height * 15 - 25).to_i
+  			return "This year your tree grew to #{@height}m tall, and produced #{@orange_count} oranges."
+  		end
+
+  		if @age >= 25
+  			@alive = false
+  			return "Oh, no! The tree is too old, and has died. :("
+  		end
+  	else
+  		return "A year later, the tree is still dead. :("
+  	end
+  end
+
+  def count_the_oranges
+  	if @alive
+	  	return @orange_count
+	else
+		return "A dead tree has no oranges. :("
+	end
+  end
+
+  def pick_an_orange
+  	if @alive
+  		@orange_count = @orange_count - 1
+  	else
+  		return "A dead tree has nothing to pick. :("
+  	end
+  end
 end

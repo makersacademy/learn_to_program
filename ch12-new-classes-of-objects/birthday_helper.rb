6b721230a40e5fb858d044ccf77f297abfbaa9dc
Laurent Bo <misterbl@hotmail.com>
2016-09-09 13:17:56 +0000

    birthdate & birthday_helper

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,14 @@
-# your code here
+birth_dates = {}
+File.read('birthdates.txt').each_line do |line|
+  line = line.chomp
+  comma = line.index(',') 
+$name = line[0..(comma - 1)] 
+$dob = line[-12..-1]
+ birth_dates[$name] = $dob
+end
+ puts "Enter a name"
+ name = gets.chomp
+  puts (birth_dates[name])[0..5] 
+
+
+

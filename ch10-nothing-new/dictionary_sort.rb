4ddb14a8336445c6476772f1da18e1328cc40fbf
jinimcoroneo <jinim1983@gmail.com>
2016-09-09 14:44:08 +0100

    YES I fully understand dictionary sorting

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,33 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(array)
+
+  recursive_sort(array, [])
+
 end
+
+def recursive_sort(unsorted_array, sorted_array)
+  if unsorted_array.length <= 0
+    return sorted_array
+  end
+
+  smallest_word = unsorted_array.pop
+  still_unsorted_array = []
+
+  unsorted_array.each {|array_element| 
+    if array_element.downcase <= smallest_word.downcase
+      still_unsorted_array << smallest_word
+      smallest_word = array_element
+
+    else
+      still_unsorted_array << array_element
+
+    end
+  }
+
+  sorted_array.push(smallest_word)
+
+  recursive_sort(still_unsorted_array, sorted_array)
+
+end
+
+
+

9784d48e62a6059005eb8b59dbdec1d1beb88cd3
Frankie Bell <fbell.pro@gmail.com>
2016-09-09 13:09:50 +0100

    looking at the solution shows how much simpler it is than what I am making it to be, remembering everything is key

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,18 @@
-# your code here
+puts "What year were you born?"
+b_year = gets.chomp.to_i
+
+puts "What month were you born? (number)"
+b_month = gets.chomp.to_i
+
+puts "What day were you born?"
+b_day = gets.chomp.to_i
+
+b = Time.local(b_year, b_month, b_day)
+t = Time.now
+
+age = 1
+
+while Time.local(b_year + age, b_month, b_day) <= t
+  puts "Spank!"
+  age = age + 1
+end

1e238ed1b0e1533d7e01e80c1445acda596090e8
hanrattyjen <hanratty.jen@gmail.com>
2016-09-09 12:35:19 +0100

    Chapter 12 exercises

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,23 @@
-# your code here
+require 'date'
+require 'time_difference'
+
+# Today's date
+current_date = Time.new
+# ask what year a person was born
+puts "What year were you born?"
+year = gets.chomp
+# then the month
+puts "What month were you born?"
+month = gets.chomp
+# then the day
+puts "What date were you born?"
+date = gets.chomp
+
+# figure out how old they are 
+age = Time.mktime(year, month, date)
+diff_in_years = TimeDifference.between(age, current_date).in_years.to_i
+# give them a big SPANK for each birthday they have had.
+puts "Here are #{diff_in_years} spanks for your birthday!"
+diff_in_years.times do |spank|
+	print "SPANK! "
+end

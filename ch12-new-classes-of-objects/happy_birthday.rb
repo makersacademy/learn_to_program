6b1e109dcec68f5da09d13f9353c0fd0635b9060
Rachael Mahon <rachmahon@gmail.com>
2016-09-11 00:48:30 +0100

    Plea enter the commit message for your changes. Lines starting

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp
+
+puts "And month"
+month = gets.chomp
+
+puts "And the date?"
+birthday = gets.chomp
+
+birthday_string = "#{year}, #{month}, #{birthday}"
+
+age = Time.new - Time.local(birthday_string)
+
+puts age
+
+age_in_years = age/31536000
+
+puts "You are #{age_in_years.to_i} years old!"
+
+age_in_years.to_i.times { puts "SPANK!"}

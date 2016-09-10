8367450eb3d38edddab13e67247cece3eef0e55d
crispinandrews <crispin.andrews@gmail.com>
2016-09-11 00:02:45 +0100

    Chap11 Solutions

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,18 @@
-# your code here
+def shuffle arr
+  shuffled = []
+  until arr.length == 0
+      input = arr.sample
+      shuffled.push input
+      arr.delete(input)
+    end
+    shuffled
+  end
+
+songs = Dir['/Users/crispinandrews/Music/LTPSongs/*.m4a']
+playlist = shuffle(songs)
+
+File.open 'playlist.m3u', 'w' do |file|
+  playlist.each do |song|
+    file.write song
+  end
+end

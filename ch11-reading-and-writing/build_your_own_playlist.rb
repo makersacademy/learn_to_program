3c35082fb58b35aec18b3c620d9c57b1a25f101c
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-10 11:13:13 +0000

    added build_your_own_playlist.rb

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,14 @@
-# your code here
+Dir.chdir '/home/ubuntu/Projects/learn_to_program/ch11-reading-and-writing/Music'
+
+music_files= Dir['*.mp3']
+puts music_files
+
+playlist_name=File.new("rory_playlist.m3u", "w")
+
+def write_files object, filename
+   File.open filename, 'w' do |f|
+       f.write object.join("\n")
+    end
+end
+
+write_files(music_files, playlist_name)

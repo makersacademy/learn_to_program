c5216cf2a5e5f51f83067ff967035dea927bb9af
Laurent Bo <misterbl@hotmail.com>
2016-09-10 19:52:11 +0000

    program logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,16 +1,17 @@
-def program_log desc, &block
+def log desc, &block
   puts 'Beginning "' + desc + '"...'
   result = block.call
   puts '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
-program_log "outer block" do                  
-  program_log "some little block" do
-    10 / 2
+log 'outer block' do
+  log 'some little block' do
+    1**1 + 2**2
   end
 
-program_log "yet another block" do
-    "I like Thai food!"
+  log 'yet another block' do
+    '!doof iahT ekil I'.reverse
   end
-  1 != 1
+
+  '0' == 0
 end

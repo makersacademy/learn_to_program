3c8c6233bb74fa209b9f94d215f0d3b807868cb6
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-10 12:02:33 +0000

    added one_billion_seconds.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,7 @@
-# your code here
+rory_birth_minute=Time.local(1986, 03, 07, 18, 15)
+rbm_integer=rory_birth_minute.to_i
+
+puts rory_birth_minute
+puts rbm_integer
+puts rory_birth_minute+1000000000
+

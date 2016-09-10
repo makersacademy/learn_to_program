91a69993b3af2c77e1d4d0f2241f4dc73bfb91de
Laurent Bo <misterbl@hotmail.com>
2016-09-10 13:18:03 +0000

    final happy_birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,8 +1,23 @@
-years = ((Time.new - Time.local(1947, 3, 30)) / 31449600).floor
-months = ((years - years.floor) * 12).floor
-if months >= 1
-puts "#{years}years and #{months}months"
-else
-puts "#{years}years"
+puts "Enter your birth year in the following format: YYYY"
+year = gets.chomp.to_i
+puts "Enter your birth month in the following format: MM"
+month = gets.chomp.to_i
+puts "Enter your birth day in the following format: DD"
+day = gets.chomp.to_i
+
+y = Time.new.year - year
+m = Time.new.month - month
+d = Time.new.day - day
+ if m - month > 0 
+puts "You are #{y}years old"
+puts "SPANK!" * y
+elsif m -month == 0 and d - day >= 0
+puts "You are #{y}years old"
+puts "SPANK!" * y
+elsif m -month == 0 and d - day < 0
+puts "You are #{y-1}years old"
+puts "SPANK!" * (y-1)
+elsif m-month < 0
+puts "You are #{y-1}years old"
+puts "SPANK!" * (y-1)
 end
-puts "SPANK!" * years

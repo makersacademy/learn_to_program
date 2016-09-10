f4256e3addcec9ac5313beffce673332bccf5a91
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-09-10 17:46:09 +0100

    ch12

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+def birthday
+
+    puts "What year were you born?"
+    year= gets.chomp.to_i
+    puts "month"
+    month = gets.chomp.to_i
+    puts "day"
+    day = gets.chomp.to_i
+
+   
+    age = 1
+    now = Time.new
+    
+    while  Time.local(year+age , month, day) <= now
+    puts "SPANK"
+    age = age+1 
+    end
+end
+
+birthday
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,3 @@
-# your code here
+born =  Time.local(1991, 03, 20, 02,15)
+puts born
+puts born + 100000000 

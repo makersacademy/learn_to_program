f69de54f50927b526ecc5020fc61fdc01bb1830b
crispinandrews <crispin.andrews@gmail.com>
2016-09-10 13:46:37 +0100

    Shuff Solution

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,11 @@
 def shuffle arr
-  # your code here
-end
+  shuffled = []
+  until arr.length == 0
+      input = arr.sample
+      shuffled.push input
+      arr.delete(input)
+    end
+    shuffled
+  end
+
+puts (shuffle([1,2,3,4,5,6,7,8,9]))

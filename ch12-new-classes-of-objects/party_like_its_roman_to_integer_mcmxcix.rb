e715df81f7bb1da2629206cbdaed61b38120d844
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-10 14:31:30 +0100

    Chapter 12 - Party Roman Numerals complete. Solution used and annotated

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,41 @@
 def roman_to_integer roman
-  # your code here
+  digit_vals = {
+  'i' => 1,
+  'v' => 5,
+  'x' => 10,
+  'l' => 50,
+  'c' => 100,
+  'd' => 500,
+  'm' => 1000}
+  #setting the roman numeral letters to values utilising a hash
+  total = 0
+  prev = 0
+  #setting up the initial conditions
+  index = roman.length - 1
+  #starting with the initial index position
+  while index >= 0
+    c = roman[index].downcase
+    #ensuring all the roman numerals are in lowercase so don't have to work with both cases
+    index = index - 1
+    val = digit_vals[c]
+    #moving the index along the numeral and comparing to the hash to ensure they are all roman numerals
+    if !val
+      puts 'This is not a valid roman numeral!'
+      return
+      #ensuring a valid roman numeral was entered
+    end
+    if val < prev
+      val = val * -1
+      #ensuring it is a positive value
+    else
+      prev = val
+    end
+    total = total + val
+    #calculating the value
+ end
+total
+#output total value
 end
+
+puts(roman_to_integer('mcmxcix'))
+puts(roman_to_integer('CCCLXV'))

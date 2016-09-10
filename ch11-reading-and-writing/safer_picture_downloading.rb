130fa0e73a372b47fe5be9d23c96f2e59fca3372
JulioGT <julio.gteran@gmail.com>
2016-09-10 13:30:17 +0100

    Chapter 11.a

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,23 @@
-# your code here
+pic_names = Dir["**/*.{JPG,jpg}"]
+puts "How would you want to call this batch?"
+batch_name = gets.chomp
+puts
+puts "Downloading #{pic_names.length} files"
+pic_number = 1
+pic_names.each do |name|
+    print "."
+    new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+    else
+    "#{batch_name}#{pic_number}.jpg"
+end
+if File.exist?(new_name) == true
+    puts "Sorry, but you would be overwriting a picture!"
+    exit
+    else
+    File.rename name, new_name
+    pic_number = pic_number + 1
+end
+end
+
+puts "Your batch have been saved with the following name: #{batch_name}!"

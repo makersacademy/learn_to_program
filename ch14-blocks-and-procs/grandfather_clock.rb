adb1cd7ed79ef48f006a128752156eb225da7e39
Dionysis <Dionysis.k22@gmail.com>
2016-09-10 16:19:26 +0000

    grandfather clock rspec success

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,20 @@
 def grandfather_clock &block
-  # your code here
+	hour = Time.new.hour # current hour 
+
+	#convert hour format to 12 hours
+	if hour >= 13
+		hour = hour - 12
+	end
+
+	if hour == 0 || hour == 24 # at 0 and 24, the hour is 12
+		hour = 12
+	end
+
+	hour.times do # how many times the clock should sound
+		block.call 
+	end
+end
+
+grandfather_clock do 
+	puts "DONG!!"
 end

711e01ed0a62b1a9831f418228f9bc80a7d02e94
mr_christoff <chris.lawrence@gmx.com>
2016-09-10 16:05:01 +0000

    grandfather clock is donging.

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,19 @@
+# def grandfather_clock &block
+#   # your code here
+# end
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour
+  
+  if hour == 0
+      number = 12
+      elsif hour > 12
+      number = hour - 12
+      else
+          number = hour
+      end
+          
+
+  number.times do 
+    block.call
+  end
 end

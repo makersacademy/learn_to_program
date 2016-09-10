c6a0c1e194aee0bc3b8aacff71899fc42f776318
LMCMLJ <mjcalvey2@gmail.com>
2016-09-10 12:40:16 +0100

    Added shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,16 @@
 def shuffle arr
-  # your code here
+  random = Random.new.rand(1..3)
+  operand = arr.pop
+  c = 100
+  while c > 0
+    if random > 2
+      operand.prepend arr
+    else
+      operand << arr
+    end
+    c = c - 1
+  end
+  return arr
 end
+
+shuffle(['hi', 'bye', 'yes', 'no'])

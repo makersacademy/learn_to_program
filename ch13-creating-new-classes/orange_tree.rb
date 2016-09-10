6dc94d5abc7e0e3128c5853e38c42fc0feed40a5
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-10 18:57:48 +0100

    Chapter 13 - Orange Tree complete. Solution used to help with the alive functionality.

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -7,5 +7,59 @@
 
 
 class OrangeTree
-  # your code here
+  def initialize
+    @height = 0
+    @orange_count = 0
+    @alive = true
+  end
+# Initializing the starting values and local variables
+  def height
+    if @alive
+      @height.round(1)
+    else
+      'A dead tree is not very tall. :('
+    end
+  end
+# Checking if still alive and calling upon then height variable
+  def count_the_oranges
+    if @alive
+      @orange_count
+    else
+      'A dead tree has no oranges. :('
+    end
+  end
+# Checking if alive and calling on the orange count
+  def one_year_passes
+    if @alive
+      @height = @height + 0.4 # Incrementing the height of the tree
+      @orange_count = 0 # old oranges fall off
+      if @height > 10 && rand(2) > 0
+        # tree dies
+        @alive = false
+        'Oh, no! The tree is too old, and has died. :('
+      elsif @height > 2
+        @orange_count = (@height * 15 - 25).to_i # Calculating how many new oranges grow
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " and produced #{@orange_count} oranges."
+      else
+        "This year your tree grew to #{@height.round(1)}m tall," +
+          " but is still too young to bear fruit."
+      end
+    else
+      'A year later, the tree is still dead. :('
+    end
+  end
+
+  def pick_an_orange
+    if @alive
+      if @orange_count > 0
+        @orange_count = @orange_count - 1 # Decreasing the local variable for orange count
+        'You pick a juicy, delicious orange!'
+      else
+        'You search every branch, but find no oranges.'
+      end
+    else
+      'A dead tree has nothing to pick. :('
+    end
+  end
 end

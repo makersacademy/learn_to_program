d1baef0d6edf73c1fece19e7d2ae4d796da5f199
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-10 15:19:43 +0100

    Ch10 - Sort

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,17 @@
 def sort arr
-  # your code here
+  num = arr.length
+
+  loop do
+
+    sorted = false
+
+    (num-1).times do |i|
+      if arr[i] > arr [i +1]
+        arr[i], arr[i +1] = arr[i+1], arr[1]
+        sorted = true
+      end
+    end
+
+    break if not sorted
+  end
 end

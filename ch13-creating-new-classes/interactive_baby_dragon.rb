0c399853c8119c536b2fc0e857a3f72d7df46598
Laurent Bo <misterbl@hotmail.com>
2016-09-10 14:28:43 +0000

    interactive baby dragon

--- ch13-creating-new-classes/interactive_baby_dragon.rb
+++ ch13-creating-new-classes/interactive_baby_dragon.rb
@@ -1,1 +1,23 @@
-# your code here
+ 
+puts "How do you want to call your dragon?"
+name = gets.chomp
+dragon_name = Dragon.new name
+
+while true 
+puts "What do you want to do to your dragon? feed? walk? put to bed? toss? rock?"
+answer = gets.chomp.downcase
+
+if answer == "feed"
+	feed
+elsif answer == "walk"
+	walk
+elsif answer == "put to bed"
+	put_to_bed
+elsif answer == "toss"
+	toss
+elsif answer == "rock"
+	rock
+else
+    puts "This is not a valid answer. Please enter feed or walk or put to bed or toss or rock"
+end    
+end

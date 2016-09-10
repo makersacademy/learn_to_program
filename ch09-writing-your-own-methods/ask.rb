ca7f4d96cca6d38f3aa84d356781faade73e2ec9
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-10 15:00:53 +0100

    Ch09 - Ask task

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
 def ask question
-  # your code here
-end
+    while true
+      puts question
+      reply = gets.chomp.downcase
+
+      if (reply == 'yes' || reply == 'no')
+        if reply == 'yes'
+          return true
+        else
+          return false
+        end
+        break
+      else
+        puts 'Please only answer "yes" or "no".'
+      end
+    end
+    

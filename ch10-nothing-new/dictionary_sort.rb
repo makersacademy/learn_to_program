a1cb4684b663796e58844d5c3b6c3485f88c043d
crispinandrews <crispin.andrews@gmail.com>
2016-09-10 17:38:55 +0100

    Ch10 Solutions

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,27 @@
 def dictionary_sort arr
-  # your code here
+  rec_dict_sort arr, []
 end
+def rec_dict_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |word|
+    if word.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = word
+    else
+      still_unsorted.push word
+    end
+  end
+
+    sorted.push smallest
+
+    rec_dict_sort still_unsorted, sorted
+
+  end
+
+  puts (dictionary_sort([ 'Dog' , 'cat' , 'Horse' , 'Owl' , 'wolf' ]))

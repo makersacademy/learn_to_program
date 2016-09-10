7753d3a910ead4ca90f7748253811b03d6308c60
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-10 23:31:09 +0000

    ninety_nine_bottles_of_beer.rb

--- ch10-nothing-new/ninety_nine_bottles_of_beer.rb
+++ ch10-nothing-new/ninety_nine_bottles_of_beer.rb
@@ -1,1 +1,14 @@
-# your code here
+require_relative 'english_number.rb'
+
+def plurals(num, word)
+num == "one" ? num + " " + word : num + " " + word + "s"
+end
+count = 999
+while count > 0
+  number_bottles = english_number(count)
+  bottles = plurals(number_bottles, "bottle")
+  puts "#{bottles.capitalize} of beers on the wall, #{bottles} of beer."
+  count -= 1
+  bottles = plurals(english_number(count), "bottle")
+  puts "Take one down and pass it around, #{bottles} of beer on the wall."
+end

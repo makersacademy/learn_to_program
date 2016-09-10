edf91c93b0ec6f88efc1a32bbe4041fb2eeeae7d
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-10 15:24:02 +0100

    Ch10 - Dictionary sort

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,10 @@
 def dictionary_sort arr
-  # your code here
+  return arr if arr.length <= 1
+
+  mid = arr.pop
+  less = arr.select{|x| x.downcase < mid.downcase}
+  more = arr.select{|x| x.downcase >= mid.downcase}
+
+  dictionary_sort(less) + [middle] + dictionary_sort(more)
+
 end

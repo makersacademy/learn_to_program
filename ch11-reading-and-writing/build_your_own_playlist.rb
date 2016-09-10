4950eb5ca8a2346ad7ba046ed5a1e01d8fb1d80c
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-09-10 13:23:21 +0100

    playlist

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,11 @@
-# your code here
+music = Dir["/users/elizabethwicks/**/*.mp3"]
+
+puts "Name of playlist?"
+title = gets.chomp
+
+File.open (title + ".m3u"), 'w' do |f|
+    f.write music.shuffle
+end
+
+puts "Done"
+

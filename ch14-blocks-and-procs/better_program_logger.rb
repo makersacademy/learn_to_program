2c6cd4b34e2671b0fabad0b6377768092dbe38a3
Laurent Bo <misterbl@hotmail.com>
2016-09-10 21:00:47 +0000

    real final better logger

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,30 +1,31 @@
-$logger_depth = 0
+@logger_depth = 0
 
-def log desc, &block
-  prefix = ' '*$logger_depth
-
-  puts prefix + 'Beginning "' + desc + '"...'
-
-  $logger_depth = $logger_depth + 1
+def log desc, &blosk
+    prefix= ' '*$logger_depth
+    
+    puts prefix + 'Beginning"' + desc + '"...'
+    
+    $logger_depth = $logger_depth + 1
+    
+    result = block.call
+    
+    $logger_depth = $logger_depth - 1
+    puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
+  end
   
-  result = block.call
-
-  $logger_depth = $logger_depth - 1
-  puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
-end
-
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
-      'lOtS oF lOVe'.downcase
+  log 'outer block' do
+      log 'some little block' do
+        log 'teeny-tiny block' do
+          'lOtS oF lOVe'.downcase
+        end  
+        
+        7 * 3 * 2
+      end
+      
+      log 'yet another block' do
+        '!doof naidnI evol I'.reverse
+      end
+      
+      '0' == "0"
     end
-
-    7 * 3 * 2
-  end
-
-  log 'yet another block' do
-    '!doof naidnI evol I'.reverse
-  end
-
-  '0' == "0"
-end
+    

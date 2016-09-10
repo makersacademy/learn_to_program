d1797b209b9784bf31a6143a6dcf10af7d2f7d51
Anna Lucking <annavlucking@hotmail.co.uk>
2016-09-10 17:21:46 +0100

    build_a_better_playlist

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,36 @@
 def music_shuffle filenames
-  # your code here
-end
+  filenames = filenames.sort
+  len = filenames.length
+
+  2.times do
+    l_index = 0
+    r_index = len/2
+    shuf = []
+
+    while shuf.length < len
+      if shuf.length%2 == 0
+        shuf.push(filenames[r_index])
+        r_index = r_index + 1
+      else
+        shuf.push(filenames[l_index])
+        l_index = l_index + 1
+      end
+    end
+
+    filenames = shuf
+  end
+
+  arr = []
+  cut = rand(len)
+  index = 0
+
+  while index < len
+    arr.push(filenames[(index+cut)%len])
+    index = index + 1
+  end
+
+  arr
+  end
+  songs = ['RayCharles/HitTheRoadJack', 'RayCharles/GeorgiaOnMyMind', 'RayCharles/IGotAWoman', 'ArethaFranklin/Respect', 'ArethaFranklin/NaturalWoman', 'ArethaFranklin/ISayALittlePrayer']
+
+  puts(music_shuffle(songs))

cc116a5ae2ee5c5f39494077f4d65ccdb914857f
Rachael Mahon <rachmahon@gmail.com>
2016-09-10 21:05:03 +0100

    Chapter 11 exercises

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,42 @@
 def music_shuffle filenames
-  # your code here
+ filenames = filenames.sort
+ len = filenames.length
+
+2.times do
+  l_index = 0
+  r_index = len/2
+  shuf = []
+
+
+  while shuf.length < len
+    if shuf.length%2 == 0
+      shuf.push(filenames[r_index])
+      r_index = r_index + 1
+    else
+      shuf.push(filenames[l_index])
+      l_index = l_index + 1
+    end
+  end
+
+  filenames = shuf
+
 end
+
+arr = []
+cut = rand(len)
+idx = 0
+
+
+while idx < len
+  arr.push(filenames[(idx+cut)%len])
+  idx = idx + 1
+end
+
+
+arr
+
+end
+
+songs = ['aa/bbb', 'aa/ccc', 'aa/ddd',
+       'AAA/xxxx', 'AAA/yyyy', 'AAA/zzzz', 'foo/bar']
+puts (music_shuffle(songs))
--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,26 @@
-# your code here
+Dir.chdir '/Users/rachaelmahon/desktop/MA Pre-Course/MA W2/Rough_work'
+
+pic_names = Dir['F:/**/*.jpg']
+
+puts
+print "Downloading #{pic_names.length} files: "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.'
+
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+
+  while File.exists?(new_name)
+    pic_number += 1
+  end
+
+  File.rename name, new_name
+
+  pic_number = pic_number + 1
+  puts 'Done!'

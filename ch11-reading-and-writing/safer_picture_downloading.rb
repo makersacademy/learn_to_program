512976fc71473223495bbd3d76315c33bb620f45
LMCMLJ <mjcalvey2@gmail.com>
2016-09-10 18:06:01 +0100

    Safer picture downloading added

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,29 @@
-# your code here
+Dir.chdir '/users/michaelcalvey/Desktop/Music/'
+
+pic_names = Dir['F:/**/*.jpg']
+puts "What would you like to call this batch?"
+
+batch_name = gets.chomp
+
+puts
+print "Downloading #{pic_names.length} files:  "
+
+pic_number = 1
+
+pic_names.each do |name|
+  print '.'
+
+  if File.exists?("#{batch_name}#{pic_number}.jpg") == true
+    puts "File already exists, skipping..."
+    exit
+  else
+    new_name = if pic_number < 10
+      "#{batch_name}#{pic_number}.jpg"
+    else
+      "#{batch_name}#{pic_number}.jpg"
+    end
+  end
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end
+puts

623157ba8347fc9ef05d0b44977da9d2fd607aac
Amaal Ali <amq.ali@gmail.com>
2016-09-10 18:49:38 -0400

    Wrote method that returns old format of Roman numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,27 @@
 def old_roman_numeral num
-  # your code here
+
+  thousands = num / 1000 #det value in thousands place
+  num -= thousands * 1000
+
+  five_hundreds = num / 500 #det value in hundreds place
+  num -= five_hundreds * 500
+
+  hundreds = num / 100 #det value in hundreds place
+  num -= hundreds * 100
+
+  fiftys = num / 50 #det value in hundreds place
+  num -= fiftys * 50
+
+  tens = num / 10 #det value in tens place
+  num -= tens * 10
+
+  fives = num / 5 #det num of fives
+  num -= fives * 5
+
+  return 'M'*thousands + 'D'*five_hundreds + 'C'*hundreds + 'L'*fiftys + 'X'*tens + 'V'*fives + 'I'*num
+  puts 'M'*thousands + 'D'*five_hundreds + 'C'*hundreds + 'L'*fiftys + 'X'*tens + 'V'*fives + 'I'*num
 end
+
+#puts 'enter number'
+#num = gets.chomp.to_i
+#puts (old_roman_numeral num)

f6e6bb35c81de573a2a42398eb94329991361786
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-10 19:00:54 +0000

    Chapter 14: Program Logger

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+program_log "outer block" do                  # no string required because it's defined in log , only write code to return false
+  program_log "some little block" do
+    1 + 4
+  end
+
+program_log "yet another block" do
+    "I like Thai food!"
+  end
+  1 != 1
 end

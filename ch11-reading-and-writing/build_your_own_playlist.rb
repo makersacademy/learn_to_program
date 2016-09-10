089a326bc1c0908b228624643205d7e61ec1d2ef
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-10 12:20:23 +0100

    Chapter 11 - Built a PLaylist. Completed with aid from the solutions.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,33 @@
-# your code here
+def shuffle array
+  shuffled = []
+
+  while array.length > 0
+
+    random_index = rand(array.length)
+
+    current_index = 0
+    new_array = []
+
+    array.each do |index|
+      if current_index == random_index
+        shuffled.push index
+      else
+        new_array.push index
+      end
+      current_index = current_index + 1
+    end
+
+    array = new_array
+  end
+  shuffled
+end
+
+music_files = shuffle(Dir["**/*.mp3"])
+
+File.open "new_playlist.m3u", 'w' do |add|
+  music_files.each do |mp3|
+    add.write mp3 + "\n"
+  end
+end
+
+puts "Complete."

caaad5b903687c10a7d2b756e6eed546af4c3cc7
Rachael Mahon <rachmahon@gmail.com>
2016-09-10 18:14:57 +0100

    Chapter 10 exercises

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,27 @@
 def shuffle arr
-  # your code here
+  to_be_shuffled = []
+
+
+while arr.length > 0
+  rand_index = rand(arr.length)
+
+  current_index = 0
+  new_array = []
+
+  arr.each do |x|
+    if current_index == rand_index
+      to_be_shuffled.push x
+    else
+      new_array.push x
+    end
+
+    current_index = current_index + 1
+  end
+
+  arr = new_array
+
+end
+
+to_be_shuffled
+
 end
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,26 @@
 def sort arr
-  # your code here
+ recursive_sorted arr, []
 end
+
+def recursive_sorted unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+
+  unsorted.each do | x |
+      if x < smallest
+        still_unsorted.push smallest
+        smallest = x
+      else
+        still_unsorted.push x
+      end
+    end
+
+    sorted.push smallest
+
+    recursive_sorted still_unsorted, sorted
+  end

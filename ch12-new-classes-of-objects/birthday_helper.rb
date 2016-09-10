795075e4123642fc0e59c23fc97587db03c0c82d
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-10 16:09:14 +0000

    birthday helper solved

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,16 @@
-# your code here
+birth_dates = {}
+File.read('birthdates.txt').each_line do |lines|#iterate through each line of the input file
+    name, date, year = lines.split(',') # using split to separate the lines with a comma delimiter, and store them 3 variable
+    birth_dates[name] = date # I'm not using the year just name and date
+    
+end
+
+puts "Whose birthday would you like to know?"
+name = gets.chomp
+date = birth_dates[name]
+if date == nil
+    puts "I don't have that one"
+else
+puts "#{name} was born on  + #{date}"
+end
+

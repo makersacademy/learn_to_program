9ac41882775706130652b70f2a7b18384b5a139f
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-10 16:24:36 +0200

    Chapter 12. Happy BDAY, SPANK

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,20 @@
-# your code here
+while true
+	puts "Enter your birthday in format: DD/MM/YYYY"
+	bday_arr = gets.chomp.strip.split('/')
+	day = bday_arr[0].to_i
+	mon = bday_arr[1].to_i
+	yea = bday_arr[2].to_i
+	if (1..31).include?(day) && (1..12).include?(mon) && (1900..Time.now.year).include?(yea)
+		bday = Time.gm(yea, mon, day)
+		break
+	else
+		puts "ERROR: Your entered date is wrong"
+	end
+end
+
+age = Time.now.year - bday.year
+age -= 1 if Time.now < bday + age
+
+puts "-" * 20
+puts "Your age is: #{age}"
+puts "SPANK! " * age

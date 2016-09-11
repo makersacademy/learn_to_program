34853dbc49f1132ebdec172d26238fee9f1539b5
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-11 12:33:05 +0000

    added ch10 sort.rb, ch12 party_like...rb, notes_on_birthday_helper and roman_to_integer_notes

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,31 +1,45 @@
+# This is a wrapper method that means we don't have to remember to open an empty
+# array each time we call rec_sort. We can just call sort and pass it the 
+# unsorted array as a parameter.
 def sort arr
-  unsorted=[]
-  sorted=[]
-  
-  
-  puts "Type the words that you want me to alphabetise"
-  #can I delete this next line? does the while loop handle this?
-  user_input=gets.chomp.downcase
-  
-  while user_input !=''
-    unsorted_words.push(user_input)
-    user_input=gets.chomp.downcase
-  end
-  
-  #each iterates over unsorted and compares them, finding the smallest
-  unsorted.each do |x,y|
-      if x < y
-          
-      end
-  end
-  
-  
-  #the smallest word is pushed to sorted
-  
-  #this is done recursively until unsorted is empty, at which point the program
-  #probably stops itself
-  
- 
-  puts sorted
-  
+  rec_sort arr, []
 end
+
+def rec_sort unsorted, sorted
+  # This condition is met when all items have been taken from unsorted, sorted,
+  # and put into sorted.
+  if unsorted.length <=0
+    return sorted
+  end
+  
+  # This takes the last item in the unsorted array and assigns it to sorted
+  smallest=unsorted.pop
+  # This is the 3rd array in the program.
+  still_unsorted=[]
+  
+  unsorted.each do |tested_object|
+  # At this point, smallest is the last object in the array, which was removed
+  # from the array by .pop
+    if tested_object < smallest
+      still_unsorted.push smallest
+  # If any object in the array is smaller than the string popped from the end
+  # then smallest is put into still_unsorted, to be dealt with later.
+      smallest=tested_object
+  # The string that was being tested against the original smallest is now
+  # assigned to smallest, becoming the next string to be tested against the
+  # contents of unsorted. This sets the variable outside this .each iterator.
+  # This iterator continues until each string has been tested against the other
+  # until unsorted is empty. This iterator ends and smallest is pushed to the
+  # sorted array. The program then recursively calls itself, taking 
+  # still_unsorted and sorted as parameters this time. The first time round it
+  # took unsorted and an empty array.
+    else
+      still_unsorted.push tested_object
+    end
+  end
+  sorted.push smallest
+  
+  rec_sort still_unsorted, sorted
+end
+
+puts (sort(['can','help me','oh god, help','keys','hmm']))
--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,49 @@
+#This code doesn't work. I moved on to the next task.
+
 def roman_to_integer roman
-  # your code here
+  roman_hash = {
+                 "M" => 1000,
+                 "D" => 500,
+                 "C" => 100,
+                 "L" => 50,
+                 "X" => 10,
+                 "V" => 5,
+                 "I" => 1
+                }
+                
+    valid_characters=["M","D","C","L","X","V","I"]
+    
+    user_numeral_arr=[]
+    user_numeral_arr.push(user_input.split(""))
+    puts user_numeral_arr
+    
+    user_numeral_arr.each do |x|
+       if valid_characters.include?(x)
+           calculate_integer
+       else
+           abort("Invalid character used, restart program")
+       end
+    end
+    
+    
+  def calculate_integer
+      array_position=0
+    user_numeral_arr.each do |x,y|
+        x=user_numeral_arr[array_position]
+        y=user_numeral_arr[array_position+1]
+        if x>=y
+           int_output=int_output+roman_hash[x]
+          elsif
+           y>x
+           int_output=int_output+roman_hash[y-x]
+        end
+    end
+  end
+    
+                
 end
+
+puts "Type a roman numeral to be converted to arabic numbers: "
+    user_input=gets.chomp.upcase
+    int_output=0
+    puts "Party like it's "+int_output.to_s

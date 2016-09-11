6c4286448a12cb7a0f40a827f807c65652a9ddf9
James Gardiner <jamesgardiner075@gmail.com>
2016-09-12 00:44:31 +0100

    Add invalid check to party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -19,19 +19,21 @@ def roman_to_integer roman
   number = 0
   count = 0
 
-  roman.length.times {
+  until count == roman.length
     current_letter = roman[count]
     double_check = roman[count].to_s + roman[count+1].to_s
-    if double_check.length == 2 && roman_numerals[double_check].to_i > 1
+    if roman_numerals.include?(current_letter) == false
+      return "Please enter valid roman numerals"
+    elsif double_check.length == 2 && roman_numerals[double_check].to_i > 1
       number = number + roman_numerals[double_check].to_i
       count += 1
     else
       number = number + roman_numerals[current_letter].to_i
     end
     count += 1
-  }
+  end
 
   number
 end
 
-puts roman_to_integer "iv"
+puts roman_to_integer "IVCDXDD"

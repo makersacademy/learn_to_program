46d6656fc258c1adfda9083fa915c8cf6cbf45c0
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-11 19:05:45 +0100

    Ch14 - Better prog log

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,28 @@
+$logger_depth = 0
+
 def log desc, &block
-  # your code here
+  prefix = ' '*$logger_depth
+
+  puts prefix + 'Beginning "' + desc + '"...'
+
+  $logger_depth = $logger_depth + 1
+	result = block.call
+  $logger_depth = $logger_depth - 1
+
+	puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
 end
+
+ log 'outer block' do
+ 	log 'some little block' do
+    log 'teeny-tiny block' do
+      'lots of love'
+    end
+    40 + 2
+ 	end
+
+ 	log 'yet another block' do
+ 		'I love indian food'
+ 	end
+
+ 	'0' == "0"
+ end

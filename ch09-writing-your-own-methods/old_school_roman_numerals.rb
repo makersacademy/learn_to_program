e9bb2b53229cb7eec36641ea0176ef76b4b740e6
Julio <julio.gteran@gmail.com>
2016-09-11 21:57:19 +0100

    Update old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,16 +1,13 @@
 def old_roman_numeral num
-    # your code here
-    roman = ""
-    roman = roman + "M" * (num / 1000)
-    roman = roman + "D" * ((num % 1000) / 500)
-    roman = roman + "C" * ((num % 500) / 100)
-    roman = roman + "L" * ((num % 100) / 50)
-    roman = roman + "X" * ((num % 50) / 10)
-    roman = roman + "V" * ((num % 10) / 5)
-    roman = roman + "I" * ((num % 5) / 1)
-    
-    roman
-    
-end
+  puts "Oops! Try using a positive integer!" if num <= 0
+    roman = ''
+      roman << 'M' * (num / 1000)
+      roman << 'D' * (num % 1000 / 500)
+      roman << 'C' * (num % 500 / 100)
+      roman << 'L' * (num % 100 / 50)
+      roman << 'X' * (num % 50 / 10)
+      roman << 'V' * (num % 10 / 5)
+      roman << 'I' * (num % 5 / 1)
 
-puts(old_roman_numeral(2498))
+  return roman
+end

14d52ef7411af7e8b081bb0d74fa88f685e17f0b
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-11 13:56:59 +0000

    grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,16 @@
 def grandfather_clock &block
-  # your code here
+    hour = Time.new.hour #take a time reading
+    if hour > 12 # check if its am or pm
+       hour = hour - 12 #if pm than deduct 12
+    else
+        hour # if am return the hour
+    end
+    hour.times do #call the method hour times.
+    block.call
+    end
 end
+=begin
+grandfather_clock do 
+    puts "DONG!"
+end
+=end

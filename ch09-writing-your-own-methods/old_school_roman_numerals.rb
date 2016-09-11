f147a99b77f9a8c3f60a785801296acc67d8028c
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-11 19:56:04 +0100

    Ch09 - old school - rspec pass

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,12 +1,11 @@
 def old_roman_numeral num
-  puts "Positive numbers only please!" if num <= 0
- +    roman = ''
- +      roman << 'M' * (num / 1000)
- +      roman << 'D' * (num % 1000 / 500)
- +      roman << 'C' * (num % 500 / 100)
- +      roman << 'L' * (num % 100 / 50)
- +      roman << 'X' * (num % 50 / 10)
- +      roman << 'V' * (num % 10 / 5)
- +      roman << 'I' * (num % 5 / 1)
- +
- +  return roman
+  roman = ''
+  roman << 'M' * (num / 1000)
+  roman << 'D' * (num % 1000 / 500)
+  roman << 'C' * (num % 500 / 100)
+  roman << 'L' * (num % 100 / 50)
+  roman << 'X' * (num % 50 / 10)
+  roman << 'V' * (num % 10 / 5)
+  roman << 'I' * (num % 5 / 1)
+  roman
+end

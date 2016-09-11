7a2be79dff9b5a30ecc0fae16b47c4b15b7c7dae
James Gardiner <jamesgardiner075@gmail.com>
2016-09-11 22:11:38 +0100

    Complete happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+puts "Hey, what year were you born?"
+year = gets.chomp
+puts "What month were you born?"
+month = gets.chomp
+puts "What day were you born?"
+day = gets.chomp
+
+age = (Time.new - Time.mktime(year, month, day)) / 60 / 60 / 24 / 365
+puts age
+puts
+age.to_i.times {
+	puts "SPANK!"
+}

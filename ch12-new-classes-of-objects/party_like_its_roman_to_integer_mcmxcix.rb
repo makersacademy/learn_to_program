ca714d224ce3ae9e5262f546901e05c16b80f14a
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-11 20:03:59 +0100

    Chapter 12. Party like its roman integerrr

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,21 @@
 def roman_to_integer roman
-  # your code here
+	@romans = {	'I' => 1, 		
+							'V' => 5, 	
+							'X' => 10, 
+							'L' => 50,		
+							'C' => 100, 
+							'D' => 500, 
+							'M' => 1000 }
+
+	str = roman.upcase.reverse		# we will go backwards
+	last_value = @romans[str[0]]  # last digit's value
+	sum = last_value
+
+	(1..str.length-1).each do |i|	
+		next_value = @romans[str[i]]
+		sum += (next_value < last_value ? -1 : 1) * next_value # only two cases for -1, such as IV or IX..
+		last_value = next_value
+	end
+
+	sum
 end

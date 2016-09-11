b66061d18d077d2aac6fb9ea2190a752d7d57a22
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-11 21:24:06 +0100

    Ch13 - orange is the new...

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,10 +1,3 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
 class OrangeTree
 
   def initialize
@@ -17,7 +10,7 @@ class OrangeTree
     if @alive
       @height.round(1)
     else
-      "A dead tree is not very tall. :("
+      'A dead tree is not very tall. :('
     end
   end
 
@@ -25,7 +18,7 @@ class OrangeTree
     if @alive
       @orange_count
     else
-      "A dead tree has no oranges :("
+      'A dead tree has no oranges. :('
     end
   end
 
@@ -33,12 +26,11 @@ class OrangeTree
     if @alive
       @height = @height + 0.4
       @orange_count = 0
-
-      if @height > 10 && rand(2) > 0
+      if @height > 10.1
         @alive = false
-        "Oh, no! The tree is too old, and has died. :("
+        'Oh, no! The tree is too old, and has died. :('
       elsif @height > 2
-        @orange_count = (@height.round(1) * 15 - 25).to_i
+        @orange_count = (@height * 15 - 25).to_i
         "This year your tree grew to #{@height.round(1)}m tall," +
         " and produced #{@orange_count} oranges."
       else
@@ -46,7 +38,7 @@ class OrangeTree
         " but is still too young to bear fruit."
       end
     else
-      "A year later, the tree is still dead. :("
+      'A year later, the tree is still dead. :('
     end
   end
 
@@ -54,12 +46,12 @@ class OrangeTree
     if @alive
       if @orange_count > 0
         @orange_count = @orange_count - 1
-        "You pick a juicy, delicious orange!"
+        'You pick a juicy, delicious orange!'
       else
-        "You search every branch, but find no oranges."
+        'You search every branch, but find no oranges.'
       end
     else
-      "A dead tree has nothing to pick. :("
+      'A dead tree has nothing to pick. :('
     end
   end
 end

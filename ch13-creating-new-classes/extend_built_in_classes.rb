d5b7322d76b69374ab27aed5224da9e5d50df6bd
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-11 19:32:42 +0100

    Completed extend_built_in_classes

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,10 @@
-class Integer
-  # your code here
+class Integer # in the class Integer add a new method
+	def factorial # factorial method
+		if self <= 1 # self refers to the object. Which is in this case an integer
+				1
+			else
+				self * (self-1).factorial # it then takes the object and multiplies it by
+		end # by it's self-1 and then calls the function again repeating the process with
+	end
 end
+

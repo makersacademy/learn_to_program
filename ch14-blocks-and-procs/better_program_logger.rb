02e22b14f87211be7f922c582f6844977ccb9931
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-11 20:56:34 +0100

    Ch14 - better log - rspec pass

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,6 +1,6 @@
 $logger_depth = 0
 
-def log desc, &block
+def better_log desc, &block
   prefix = ' '*$logger_depth
 
   puts prefix + 'Beginning "' + desc + '"...'
@@ -12,15 +12,15 @@ def log desc, &block
 	puts prefix + '..."' + desc + '" finished, returning: ' + result.to_s
 end
 
- log 'outer block' do
- 	log 'some little block' do
-    log 'teeny-tiny block' do
+ better_log 'outer block' do
+ 	better_log 'some little block' do
+    better_log 'teeny-tiny block' do
       'lots of love'
     end
     40 + 2
  	end
 
- 	log 'yet another block' do
+ 	better_log 'yet another block' do
  		'I love indian food'
  	end
 

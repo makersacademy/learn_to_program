609bf406bf5bfe332f76dc7b20aa1ffde18813b4
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-11 17:19:08 +0100

    Finished program logger exercise in chapter 14

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts "Beginning \"#{desc}\"..."
+  result = block.call
+  puts "...\"#{desc}\" finished, returning: #{result.to_s}"
+end
+
+program_log "outer block" do
+  program_log "little block," do
+    5
+  end
+
+  program_log "yet another " do
+    "I like thai food"
+  end
 end

6c566efd2dcb76bea57caa7a28ca6aadef7ab6e5
Royston <rlqhenson@hotmail.com>
2016-09-11 21:48:07 +0100

    added birthday_helper.rb

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,21 @@
-# your code here
+birth_dates = {}
+File.read("birthdates.txt").each_line do |line|
+    line = line.chomp
+    first_comma = 0
+    while line[first_comma] != "," &&
+        first_comma < line.length
+        first_comma += 1
+    end
+    name = line[0..(first_comma - 1)]
+    date = line[-12..-1]
+    birth_dates[name] = date
+  end
+
+puts "Who's birthday would you like to know?"
+name = $stdin.gets.chomp
+date = birth_dates[name]
+if date == nil
+  puts "Sorry, I don't know that one."
+else
+  puts date[0..5]
+end

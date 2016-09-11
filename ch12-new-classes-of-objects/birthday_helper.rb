90c392034d6ad001723c3ffe43dbe49408cb2ef8
Liam Taylor <liam.taylor888@googlemail.com>
2016-09-11 18:38:38 +0100

    Ch12 exercises added.

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,26 @@
-# your code here
+Dir.chdir '/Users/liam.taylor/Projects/learn_to_program/ch12-new-classes-of-objects'
+birthday_array = []
+list_of_birthdays = File.read 'birthdays.txt'
+birthday_hash = {}
+lookup_value = 0
+lookup_value_cells = 0
+list_of_birthdays.each_line do |line|
+birthday_array.push line
+end
+birthday_array.each do |push|
+string_to_search = push
+  while lookup_value < 50
+    comma_perhaps = string_to_search[lookup_value]
+    if comma_perhaps == ","
+      name_to_push = string_to_search[0..lookup_value - 1]
+      birthday_to_push = string_to_search[lookup_value + 2..-1]
+      birthday_hash[name_to_push] = birthday_to_push
+      lookup_value = lookup_value + 1
+      break
+    else lookup_value = lookup_value + 1
+    end
+  end
+end
+puts 'Whose birthday? Do you want to know?'
+sought_person = gets.chomp
+puts "So, you want to know #{sought_person}'s birthday? Well it's #{birthday_hash[sought_person]}."
--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,16 @@
-# your code here
+puts "What year were you born in?"
+year_of_birth = gets.chomp
+puts "What month were you born in?"
+month_of_birth = gets.chomp
+puts "What day were you born on?"
+day_of_birth = gets.chomp
+puts Time.local(year_of_birth, month_of_birth, day_of_birth)
+birthday = Time.local(year_of_birth, month_of_birth, day_of_birth)
+age_in_seconds = Time.new - birthday
+age_in_years = age_in_seconds / 60 / 60 / 24 /365
+age = age_in_years.floor
+puts "Then you're #{age} years old."
+while age > 0
+  puts "SPANK!"
+  age = age - 1
+end

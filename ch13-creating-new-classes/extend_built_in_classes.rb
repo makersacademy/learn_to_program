031be966c2ad27652b44e3f36455d035d9809492
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-11 22:09:02 +0100

    Update extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,10 +1,27 @@
-class Integer # in the class Integer add a new method
-	def factorial # factorial method
-		if self <= 1 # self refers to the object. Which is in this case an integer
-				1
-			else
-				self * (self-1).factorial # it then takes the object and multiplies it by
-		end # by it's self-1 and then calls the function again repeating the process with
-	end
+class Integer
+  
+  def factorial
+    
+    raise 'Must not use negative integer' if self < 0
+    
+    (self <= 1) ? 1 : self * (self-1).factorial
+  end
+  
+  
+  def to_roman
+    
+    raise 'Must use positive integer' if self <= 0
+   
+    roman = ''
+   
+    roman << 'M' * (self / 1000)
+    roman << 'D' * (self % 1000 / 500)
+    roman << 'C' * (self % 500 / 100)
+    roman << 'L' * (self % 100 / 50)
+    roman << 'X' * (self % 50 / 10)
+    roman << 'V' * (self % 10 / 5)
+    roman << 'I' * (self % 5 / 1)
+    
+    roman
+  end
 end
-

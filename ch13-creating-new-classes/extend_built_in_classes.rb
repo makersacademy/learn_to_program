012ee2d656ed263f38c544a98d072a2c661cbe06
Julio <julio.gteran@gmail.com>
2016-09-11 21:58:23 +0100

    Update extend_built_in_classes.rb

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,45 +1,27 @@
 class Integer
-  def factorial
-    if self <= 1
-      1
-    else
-      self * (self - 1).factorial
-    end
+  
+def factorial
+
+    raise 'Must not use negative integer' if self < 0
+
+    (self <= 1) ? 1 : self * (self-1).factorial
   end
 
-  def roman
-    roman = ""
-    roman = roman + "M" * (self / 1000)
-    roman = roman + "D" * (self % 1000 / 500)
-    roman = roman + "C" * (self % 500 / 100)
-    roman = roman + "L" * (self % 100 / 50)
-    roman = roman + "X" * (self % 50 / 10)
-    roman = roman + "V" * (self % 10 / 5)
-    roman = roman + "D" * (self % 5 / 1)
+
+  def to_roman
+
+    raise 'Must use positive integer' if self <= 0
+
+    roman = ''
+
+    roman << 'M' * (self / 1000)
+    roman << 'D' * (self % 1000 / 500)
+    roman << 'C' * (self % 500 / 100)
+    roman << 'L' * (self % 100 / 50)
+    roman << 'X' * (self % 50 / 10)
+    roman << 'V' * (self % 10 / 5)
+    roman << 'I' * (self % 5 / 1)
+
+    roman
   end
-end
-
-class Array
-  def shuffle
-    arr = self
-    shuffled = []
-    while arr.length > 0
-        rand_index = rand(arr.length)
-
-        curr_index = 0
-        new_array =[]
-
-        arr.each do |item|
-            if curr_index == rand_index
-                shuffled.push item
-                else
-                new_array.push item
-            end
-
-            curr_index = curr_index + 1
-        end
-
-        arr = new_array
-    end
-    shuffled
 end

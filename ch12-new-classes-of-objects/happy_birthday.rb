f15b31ba435c2869e5a05c4c5fa67da2ddbf4368
LMCMLJ <mjcalvey2@gmail.com>
2016-09-11 13:45:48 +0100

    Added happy_birthday.rb:

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,39 @@
-# your code here
+puts "What year were you born?"
+year = gets.chomp.to_i
+puts "What month were you born"
+month_str = gets.chomp.downcase
+if month_str == "january"
+  month = 1
+elsif month_str == "february"
+  month = 2
+elsif month_str == "march"
+  month = 3
+elsif month_str == "april"
+  month = 4
+elsif month_str == "may"
+  month = 5
+elsif month_str == "june"
+  month = 6
+elsif month_str == "july"
+  month = 7
+elsif month_str == "august"
+  month = 8
+elsif month_str == "september"
+  month = 9
+elsif month_str == "october"
+  month = 10
+elsif month_str == "november"
+  month = 11
+elsif month_str == "december"
+  month = 12
+elsif month_str.to_1 < 13
+else
+  puts "you did not enter a valid month, application failed."
+end
+
+puts "What day were you born?"
+day = gets.chomp.to_i
+
+birthday = Time.local(year, month, day)
+
+puts ("Spank!" * ((Time.now - birthday) / 31536000))

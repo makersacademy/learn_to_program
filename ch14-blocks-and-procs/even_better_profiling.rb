59009bd5ab05f18179d379d487e0489409bb0e31
amidabrian51 <briankaty1@me.com>
2016-09-11 19:44:16 +0100

    Changes to even better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,13 +1,14 @@
 def profile block_description, &block
-  # your code here
-profiling_on = false
-if profiling_on
-  start_time = Time.new
-  block.call
+  # To turn profiling on/off, set this
+  # to true/false.
+  profiling_on = false
+  if profiling_on
+    start_time = Time.new
+    block.call
 
-  duration = Time.new - start_time
-  puts "#{block_description}: #{duration} seconds"
-else
-  block.call
-  
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end

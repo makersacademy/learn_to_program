eab4e195f8d09f77569b076ee0086d3dee9af49a
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-11 15:06:58 +0000

    added dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,18 @@
 def dictionary_sort arr
   
 end
+
+# Ideas for how to solve dictionary_sort:
+
+# create a hash where each word is assigned to a symbol key. key name is generated
+# by downcasing the string, value retains capitalisation.
+# run sort on the keys
+# output sorted keys to an array
+# puts the values of the symbol key
+
+# __________________________________________________________
+
+# Having looked at the solution I realise I inefficiently over engineered the
+# solution to this one. The only change that is required  to the sort.rb program
+# is to downcase the elements as they are being tested. The downcasing doesn't
+# affect the element as it creates a copy to work with.

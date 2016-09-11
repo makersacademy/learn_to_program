c719d29e40ed70153623aa04e2afe428799f08ac
Felix Harrison <fmlharrison@gmail.com>
2016-09-11 18:11:40 +0100

    Profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,14 @@
 def profile block_description, &block
-  # your code here
+  #this method take a code a sees how long it takes to run.. in seconds
+  #the block_description is the name of the code
+  #&block is the code's block truned into a proc.
+  profiling = true
+  if profiling
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
 end

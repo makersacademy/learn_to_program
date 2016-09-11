087c9f71b3b9674d7fba991034cc36d25dada9ad
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-11 15:40:02 +0100

    log

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,15 @@
-def log desc, &block
-  # your code here
+def program_log desc, &block
+  puts 'Beginning "' + desc + '"...'
+  result = block.call
+    puts '..."' + desc + '" finished, returning: ' + result.to_s
+end
+
+program_log 'outer block' do
+  program_log 'some little block' do
+    6 -1
+end
+program_log "yet anothe block" do
+  "I like thai food"
+end
+false == true
 end

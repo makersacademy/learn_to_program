c1bbc6379b6e00c4d3f8ebb5fc9bd7179046433b
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-11 22:02:55 +0100

    Updated English number

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,208 +1,87 @@
 
 def english_number number
+  if number < 0
+      return "Oops! Please use a positive integer!"
+  end
+  if number == 0
+      return "zero"
+  end
+  
+  num_string = ''
+  
+    ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-# We accept numbers from 0 to 100.
+    tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
-if number < 0
-	return 'Please enter a number zero or greater.'
+    teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+    zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
+              
+    left = number
+    
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base 
+    left = left - write*zil_base 
+    
+    if write > 0
+      # Now here's the recursion:
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+      
+    end
+
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+
+  num_string
 end
-
-if number > 100
-	return 'Please enter a number 100 or less.'
-end
-
-num_string = '' # This is the string we will return.
-
-# "left" is how much of the number
-
-# we still have left to write out.
-
-# "write" is the part we are
-
-# writing out right now.
-
-# write and left... get it? :)
-
-left = number
-
-write = left/100 # How many hundreds left?
-
-left = left - write*100 # Subtract off those hundreds.
-
-if write > 0
-	return 'one hundred'
-end
-
-write = left/10 # How many tens left?
-
-left = left - write*10 # Subtract off those tens.
-
-if write > 0
-
-if write == 1 # Uh-oh...
-
-# Since we can't write "tenty-two"
-
-# instead of "twelve", we have to
-
-# make a special exception for these.
-
-if left == 0
-	num_string = num_string + 'ten'
-elsif left == 1
-	num_string = num_string + 'eleven'
-elsif left == 2
-	num_string = num_string + 'twelve'
-elsif left == 3
-	num_string = num_string + 'thirteen'
-elsif left == 4
-	num_string = num_string + 'fourteen'
-elsif left == 5
-	num_string = num_string + 'fifteen'
-elsif left == 6
-	num_string = num_string + 'sixteen'
-elsif left == 7
-	num_string = num_string + 'seventeen'
-elsif left == 8
-
-num_string = num_string + 'eighteen'
-
-elsif left == 9
-
-num_string = num_string + 'nineteen'
-
-end
-
-# Since we took care of the digit in the
-
-# ones place already, we have nothing left to write.
-
-left = 0
-
-elsif write == 2
-
-num_string = num_string + 'twenty'
-
-elsif write == 3
-
-num_string = num_string + 'thirty'
-
-elsif write == 4
-
-num_string = num_string + 'forty'
-
-elsif write == 5
-
-num_string = num_string + 'fifty'
-
-elsif write == 6
-
-num_string = num_string + 'sixty'
-
-elsif write == 7
-
-num_string = num_string + 'seventy'
-
-elsif write == 8
-
-num_string = num_string + 'eighty'
-
-elsif write == 9
-
-num_string = num_string + 'ninety'
-
-end
-
-if left > 0
-
-num_string = num_string + '-'
-
-end
-
-end
-
-write = left # How many ones left to write out?
-
-left = 0 # Subtract off those ones.
-
-if write > 0
-
-if write == 1
-
-num_string = num_string + 'one'
-
-elsif write == 2
-
-num_string = num_string + 'two'
-
-
-
-elsif write == 3
-
-num_string = num_string + 'three'
-
-elsif write == 4
-
-num_string = num_string + 'four'
-
-elsif write == 5
-
-num_string = num_string + 'five'
-
-elsif write == 6
-
-num_string = num_string + 'six'
-
-elsif write == 7
-
-num_string = num_string + 'seven'
-
-elsif write == 8
-
-num_string = num_string + 'eight'
-
-elsif write == 9
-
-num_string = num_string + 'nine'
-
-end
-
-end
-
-if num_string == ''
-
-# The only way "num_string" could be empty
-
-# is if "number" is 0.
-
-return 'zero'
-
-end
-
-# If we got this far, then we had a number
-
-# somewhere in between 0 and 100, so we need
-
-# to return "num_string".
-
-num_string
-
-end
-
-puts english_number( 0)
-
-puts english_number( 9)
-
-puts english_number( 10)
-
-puts english_number( 11)
-
-puts english_number( 17)
-
-puts english_number( 32)
-
-puts english_number( 88)
-
-puts english_number( 99)
-
-puts english_number(100)

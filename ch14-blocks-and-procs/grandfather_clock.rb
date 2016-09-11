ec16b3b98f6a1b01fdc281671c0b2e835f6ab857
Felix Harrison <fmlharrison@gmail.com>
2016-09-11 23:44:28 +0100

    final week 2 commit

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,17 @@
 def grandfather_clock &block
-  # your code here
+  hour = Time.new.hour #gives us the current time in hours (24hr clock)
+  if hour >= 13 #takes the 24hr clock and makes it in a 12hr clock
+    hour = hour - 12
+  end
+
+  if hour == 0
+    hour = 12
+  end
+
+  hour.times do
+    block.call #this is the 'DONG' &block.
+  end
+end
+grandfather_clock do
+  puts 'DONG!'
 end

d7a0b34ae129b8d7ac49f018951b3c0f653b942c
andy jenkins <andygjenkins@gmail.com>
2016-09-11 11:02:56 +0200

    Complete happy birthday

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,17 @@
-# your code here
+puts "Hello! What year were you born?"
+year = gets.chomp
+puts "What Month?"
+month = gets.chomp
+puts "What day?"
+day = gets.chomp
+birthday = Time.mktime(year.to_i,month.to_i,day.to_i)
+
+current_date = Time.new
+
+puts "Here's a spank for every birthday you've had!"
+seconds_age = current_date - birthday
+years = ((((seconds_age / 60) / 60) / 24) /365)
+
+(years.floor).times do
+	puts "Spank!"
+end

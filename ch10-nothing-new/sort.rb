ab485515bfba6363e02eaec4b2772958052d7a8c
James Gardiner <jamesgardiner075@gmail.com>
2016-09-11 10:36:40 +0100

    Complete sort.rb

--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,36 @@
-def sort arr
-  # your code here
+def sort my_array
+  recursive_sort my_array, []
 end
+
+def recursive_sort word_list, sorted_array
+
+  if word_list == []
+    return sorted_array
+  else
+
+    smallest = "not found"
+    challenger = 0
+    opponent = 0
+    wins = 0
+
+    until smallest == "found"
+      if word_list[challenger] <= word_list[opponent]
+        opponent += 1
+        wins += 1
+      else
+        challenger += 1
+        opponent = 0
+        wins = 0
+      end
+
+      if wins == word_list.length
+        smallest = "found"
+        sorted_array.push word_list[challenger]
+        word_list.delete_at(challenger)
+      end
+    end
+    recursive_sort(word_list, sorted_array)
+  end
+end
+
+puts sort(['ant','bee','fly','cat','ant','duck','zebra'])

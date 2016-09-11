40b6702884da60a4fe512020dd91ca7c3ceb4af6
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-11 12:29:10 +0000

    ask.rb exercise

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,17 @@
 def ask question
-  # your code here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    
+    return true if reply == 'yes'
+    return false if reply =='no'
+     
+    puts 'Please answer yes or no.'
+  end
+  answer
 end
+  
+choc = ask 'Do you like chocolate?'
+puts choc
+
+

81fe2af39d6346f1bc4835dddbd7e97b3df1cf14
James Gardiner <jamesgardiner075@gmail.com>
2016-09-12 00:01:03 +0100

    Complete party_like_its_roman_to_integer_mcmxcix.rb

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,37 @@
 def roman_to_integer roman
-  # your code here
+  roman = roman.upcase
+  roman_numerals = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+  }
+
+  number = 0
+  count = 0
+
+  roman.length.times {
+    current_letter = roman[count]
+    double_check = roman[count].to_s + roman[count+1].to_s
+    if double_check.length == 2 && roman_numerals[double_check].to_i > 1
+      number = number + roman_numerals[double_check].to_i
+      count += 1
+    else
+      number = number + roman_numerals[current_letter].to_i
+    end
+    count += 1
+  }
+
+  number
 end
+
+puts roman_to_integer "iv"

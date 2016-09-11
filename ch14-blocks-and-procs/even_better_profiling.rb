b43b29f6e4be149f2d73edec93184095886d99e6
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-11 13:33:32 +0000

    profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,13 @@
 def profile block_description, &block
-  # your code here
+  profiling_on = true  #toogle profiling on / off by setting it true or false
+  
+  if profiling_on #if true
+      start_time = Time.new #get the time and store it in start_time
+      block.call #call the passed in block, the one I want to time
+      
+      duration = Time.new - start_time #calculate the time difference
+      puts "#{block_description}: #{duration} seconds" #write on the screen the result
+  else
+      block.call # if condition false, just run the code without timing.
+  end
 end

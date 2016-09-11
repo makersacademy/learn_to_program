d1505dac365e256b4ba07589d6db15567f645975
Royston <rlqhenson@hotmail.com>
2016-09-11 21:44:08 +0100

    added shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,28 @@
-def shuffle arr
-  # your code here
+# long way
+def shuffle(arr)
+  shuff = []
+  while arr.length > 0
+    rand_index = rand(arr.length)  # Randomly pick an element
+    curr_index = 0
+    new_arr = []
+    arr.each do |item|
+      if curr_index == rand_index
+        shuff.push item
+      else
+        new_arr.push item
+      end
+      curr_index += 1
+    end
+    arr = new_arr
+  end
+  shuff
 end
+
+# short way
+=begin
+def shuffle(arr)
+  arr.sort_by{rand}
+end
+=end
+animals = ["zebra", "donkey", "monkey", "tarsier", "platypus", "anteater", "koala"]
+puts shuffle(animals)

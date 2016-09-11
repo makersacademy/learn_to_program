ab43023e8f3bfcab29bab02d59c423e0512cddec
ManuCiao <mn.sabatino@gmail.com>
2016-09-12 00:12:19 +0100

    4th attempts of updating program_* in Ch14 folder

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -4,12 +4,12 @@ def log desc, &block
   puts "...#{desc.inspect} finished, returning: #{result}"
 end
 
-program_log 'outer block' do
-  program_log 'some little block' do
+log 'outer block' do
+  log 'some little block' do
     1**1 + 2**2
   end
 
-  program_log 'yet another block' do
+  log 'yet another block' do
     '!doof iahT ekil I'.reverse
   end
   

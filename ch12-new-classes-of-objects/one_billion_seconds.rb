f6deb3d899605648a8bd6d02a7d7293c76aaf2a8
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-11 11:05:29 +0000

    happy_birthday.rb

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,7 @@
-# your code here
+michelle_birth_minute=Time.local(1991, 01, 30, 10, 19)
+mbm_integer=michelle_birth_minute.to_i
+ 
+ puts michelle_birth_minute
+ puts mbm_integer
+ puts michelle_birth_minute+1000000000
+ 

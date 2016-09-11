4dd4f26a49748d159b4f949abded186f80c3ac7c
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-09-11 10:42:28 +0200

    Commit event_better_profiling.rb

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,19 @@
 def profile block_description, &block
-  # your code here
+  profiling_on = true #changed from false to true to activate time profiling
+    if profiling_on
+      start_time = Time.new
+      block.call
+      duration = Time.new - start_time
+      puts "#{block_description}: #{duration} seconds"
+    else
+      block.call
+    end
+end
+
+profile '25000 doublings' do 
+    number = 1
+
+    25000.times do
+        number = number + number
+    end
 end

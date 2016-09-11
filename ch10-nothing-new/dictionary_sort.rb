ede6aff24bc89e506dcb8983bd5e074a7b08c41d
Kath Pobee-Norris <kathwath@hotmail.com>
2016-09-11 20:03:51 +0100

    Ch10 - dictionary sort - rspec pass

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,9 +1,9 @@
 def dictionary_sort arr
   return arr if arr.length <= 1
 
-  mid = arr.pop
-  less = arr.select{|x| x.downcase < mid.downcase}
-  more = arr.select{|x| x.downcase >= mid.downcase}
+  middle = arr.pop
+  less = arr.select{|x| x.downcase < middle.downcase}
+  more = arr.select{|x| x.downcase >= middle.downcase}
 
   dictionary_sort(less) + [middle] + dictionary_sort(more)
 

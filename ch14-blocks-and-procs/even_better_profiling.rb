1754339af09593bfb2c5fcc0c44004dd8374d38a
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-11 15:54:40 +0100

    change better profiling

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -9,18 +9,3 @@ else
     block.call
   end
 end
-
-profile '25000 doubling' do
-  number = 1
-  25000.times do
-    number = number + number
-  end
-  puts "#{number.to_s.length} digits"
-end
-
-profile 'count to a million' do
-  number = 0
-  1000000.times do
-    number = number + 1
-  end
-end

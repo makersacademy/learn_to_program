c54495adf3784f5ae82c8c32b25b8e5b14d78c73
Rachael Mahon <rachmahon@gmail.com>
2016-09-11 22:25:52 +0100

    I can't believe it worked

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -4,11 +4,14 @@ def program_log desc, &block
     puts '..."' + desc + '" finished, returning: ' + result.to_s
   end
 
- program_log 'outer block' do
-   program_log 'some little block' do
+  program_log 'outer block' do
+    program_log 'some little block' do
       1**1 + 2**2
     end
 
-   program_log 'yet another block' do
+    program_log 'yet another block' do
       '!doof iahT ekil I'.reverse
     end
+
+    '0' == 0
+  end

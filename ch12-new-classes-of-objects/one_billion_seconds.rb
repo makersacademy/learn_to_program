c176ced3675c1d5d18ddf5a051414036abee3262
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-11 17:45:46 +0100

    Completed one_billion_seconds

--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,15 @@
-# your code here
+=begin
+
+Set use Time.local to set my date of birth and add 1 billion from answer.
+	
+=end
+
+def one_billion_seconds()
+
+	time_one_billion_seconds_from_your_birth = Time.local(1980, 1, 6, 13, 30) + 1000000000
+
+	print "One billion seconds from the moment your are born will be on the #{time_one_billion_seconds_from_your_birth}\n"
+
+end
+
+one_billion_seconds

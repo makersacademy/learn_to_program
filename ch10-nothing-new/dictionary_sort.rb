17cbfedc2914e671f0b9abbf14ed4794dea0e564
JulioGT <julio.gteran@gmail.com>
2016-09-11 19:05:42 +0100

    Chapter 10.b

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,21 @@
 def dictionary_sort arr
-  # your code here
+  rec_dict_sort arr, []
+end
+def rec_dict_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |tested|
+    if tested.downcase < smallest.downcase
+      still_unsorted.push smallest
+      smallest = tested
+    else
+      still_unsorted.push tested
+    end
+  end
+  sorted.push smallest
+  rec_dict_sort still_unsorted, sorted
 end

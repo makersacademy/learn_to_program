b3a27689de7ac5d8f2d5abda53e13ae4f60723b5
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-11 16:00:29 +0100

    new improved roman numerals

--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,35 +1,15 @@
-def numeral num
-  thousand = num / 1000
-  hundret = (num % 1000 /100)
-  ten = (num % 100/ 10)
-  one = (num % 10)
-
-  rn = 'M' * thousand
-  if hundret == 9
-  rn = rn + 'CM'
-  elsif hundret == 4
-  rn = rn + "CD"
-  else
-    rn = rn + 'D' * (num % 1000/ 500)
-    rn = rn + 'C' * (num % 500/ 100)
+def roman_numeral(number)
+  result, numerals = "",[
+    [1000, "M"], [900, "CM"], [500, "D"], [400, "CD"],
+    [100,  "C"], [90,  "XC"], [50,  "L"], [40,  "XL"],
+    [10,   "X"], [9,   "IX"], [5,   "V"], [4,   "IV"],
+    [1,    "I"]
+  ]
+  numerals.each do |order, roman|
+    result << roman * (number / order)
+    number %= order
   end
-  if ten == 9
-    rn = rn + 'XC'
-  elsif ten == 4
-    rn = rn + "XL"
-  else
-    rn = rn + 'L' * (num % 100/ 50)
-    rn = rn + 'X' * (num % 50/ 10)
-  end
-  if one == 9
-    rn = rn + 'IX'
-  elsif one == 4
-     rn = rn + 'IV'
-  else
-    rn = rn + 'V' * (num % 10/ 5)
-    rn = rn + 'I' * (num % 5/ 1)
-  end
-  rn
+  result
 end
 
-puts(numeral(1994))
+puts(roman_numeral(1999))

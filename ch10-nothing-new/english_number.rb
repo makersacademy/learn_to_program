ec813793f0797291e1ab7bb1615e01f73462da73
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-11 21:37:17 +0000

    english_number.rb

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -1,3 +1,96 @@
 def english_number number
-  # your code here
+	if number < 0
+		return 'Please enter a number that isn\'t negative.'
+	end
+	if number == 0
+		return 'zero'
+	end
+
+	numString = '' 
+
+	onesPlace = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+	tensPlace = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+	teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen','sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+	zillions = [['hundred', 2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdecillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novemdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
+
+  left = number
+
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base
+
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0
+        # So we don't write 'two billionfifty-one'...
+        num_string = num_string + ' '
+      end
+    end
+  end
+
+	write = left/10
+	left = left-write*10 
+
+	if write > 0
+		if ((write == 1) and (left == 0))
+			numString = numString + teenagers[left-1]
+			left = 0
+		else
+			numString = numString + tensPlace[write-1]
+		end
+		if left > 0
+			numString = numString + ' '
+		end
+	end	
+		
+
+	write = left
+	left = 0
+
+	if write > 0
+		numString = numString + onesPlace[write-1]
+	end
+
+	numString
 end
+
+
+puts english_number (12)
+puts english_number (1)
+puts english_number (0)
+puts english_number (27)
+puts english_number (100)
+puts english_number (512)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)
+puts english_number(109238745102938560129834709285360238475982374561034)
+

654f1b6a68e967da1f8de376e3c3b2eccf45bdbf
Tadas Majeris <tadas.majeris@gmail.com>
2016-09-12 11:03:01 +0100

    Chapter 14. Better program loggerrr

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,9 @@
-def log desc, &block
-  # your code here
+$nesting_depth = 0
+
+def better_log desc, &block
+	puts ' ' * $nesting_depth + "Beginning \"#{desc}\"..."
+  $nesting_depth += 1
+  result = block.call
+	$nesting_depth -= 1
+	puts ' ' * $nesting_depth + "...\"#{desc}\" finished, returning: #{result}"
 end

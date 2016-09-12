c34da983bc4667f1f15a3cec6dbdd71516157260
Liam Taylor <liam.taylor888@googlemail.com>
2016-09-12 08:53:23 +0100

    Penultimate(?) commit...

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -17,56 +17,19 @@ class Array
 end
 
 class Integer
+
 def to_roman
-  thousands_to_convert = (self / 1000)
-  hundreds_to_convert = (self % 1000 / 100)
-  tens_to_convert = (self % 100 / 10)
-  units_to_convert = self % 10
-
-if thousands_to_convert <= 3
-  converted_thous = "M" * thousands_to_convert
+  roman = ""
+  roman = roman + "M" * (self / 1000)
+  roman = roman + "D" * (self % 1000 / 500)
+  roman = roman + "C" * (self % 500/ 100)
+  roman = roman + "L" * (self % 100 / 50)
+  roman = roman + "X" * (self % 50 / 10)
+  roman = roman + "V" * (self% 10 / 5)
+  roman = roman + "I" * (self % 5 )
+  roman
 end
 
-if hundreds_to_convert <= 3
-  converted_huns = "C" * hundreds_to_convert
-elsif hundreds_to_convert == 4
-  converted_huns = 'C' + 'D'
-elsif hundreds_to_convert == 5
-  converted_huns = 'D'
-elsif hundreds_to_convert > 5 && hundreds_to_convert < 9
-  converted_huns = 'D' + "C" * (hundreds_to_convert - 5)
-elsif hundreds_to_convert == 9
-  converted_huns = 'C' + "M"
-end
-
-if tens_to_convert <= 3
-  converted_tens = "X" * tens_to_convert
-elsif tens_to_convert == 4
-  converted_tens = 'X' + 'L'
-elsif tens_to_convert == 5
-  converted_tens = 'L'
-elsif tens_to_convert > 5 && tens_to_convert < 9
-  converted_tens = 'L' + "X" * (tens_to_convert - 5)
-elsif tens_to_convert == 9
-  converted_tens = 'X' + "C"
-end
-
-if units_to_convert <= 3
-  converted_units = "I" * units_to_convert
-elsif units_to_convert == 4
-  converted_units = 'I' + 'V'
-elsif units_to_convert == 5
-  converted_units = 'V'
-elsif units_to_convert > 5 && units_to_convert < 9
-  converted_units = 'V' + "I" * (units_to_convert - 5)
-elsif units_to_convert == 9
-  converted_units = 'I' + "X"
-end
-
-converted_num = converted_thous + converted_huns + converted_tens + converted_units
-puts converted_num
-converted_num
-end
 
 def factorial
 if self < 0
@@ -79,9 +42,4 @@ else
   self * (self-1).factorial
 end
 end
-
 end
-
-
-114.to_roman
-puts 114.factorial
--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -1,10 +1,3 @@
-# in order to pass the rspec please follow the below rates of growth, orange production and age of death.
-# have your OrangeTree grow by 0.4 per year.
-# have it produce no oranges in its first 5 years
-# starting in its sixth year have it produce oranges at a rate of (height * 15 - 25) per year.
-# have the tree die after 25 years.
-# check out the rspec spec/ch13/orange_tree_spec.rb to see what strings we're looking for in the responses.
-
 
 class OrangeTree
 
@@ -18,9 +11,9 @@ end
 
 def height
   if @life_status == true
-    puts "The tree is #{@height} metres tall"
+    return @height
   else
-    puts "A dead tree is not very tall. :("
+    return "A dead tree is not very tall. :("
   end
 end
 
@@ -29,48 +22,49 @@ def one_year_passes
     @tree_age = @tree_age + 1
     @oranges_on_tree = 0
     @height = (@height + @growth_per_year).round(1)
-    if @tree_age >=6
+    if @tree_age > 25
+      @life_status = false
+      return 'Oh, no! The tree is too old, and has died. :('
+    elsif @tree_age >=5
       @oranges_on_tree = (@oranges_on_tree + (@height * 15 - 25)).round(0)
-      puts "This year your tree grew to #{@height}m tall, and produced #{@oranges_on_tree} oranges."
-    else
-    puts "The tree is now #{@tree_age} years old. This year your tree grew to #{@height}m tall but isn't old enough to bear fruit."
+      return "This year your tree grew to #{@height}m tall, and produced #{@oranges_on_tree} oranges."
+    else @tree_age <5
+    return "This year your tree grew to #{@height}m tall but is still too young to bear fruit."
   end
   else
-    puts "The tree is still dead..."
+    return "A year later, the tree is still dead. :("
   end
-  if @tree_age == 25
-    @life_status = false
-    puts 'Oh, no! The tree is too old, and has died. :('
-  end
+
 end
 
 def count_the_oranges
   if @life_status == true
     if @oranges_on_tree > 0
-      puts "There are #{@oranges_on_tree.to_i} oranges on the tree"
+      return @oranges_on_tree
     else
-      puts "There are no oranges on the tree."
+      return "There are no oranges on the tree."
     end
   else
-    puts "A dead tree has no oranges. :("
+    return "A dead tree has no oranges. :("
   end
 end
 
-def pick_orange
+def pick_an_orange
   if @life_status == true
     if @oranges_on_tree > 0
       @oranges_on_tree = @oranges_on_tree - 1
-      puts "Mmmm, tasty!"
-    else puts "There are no oranges to pick!"
+      return "Mmmm, tasty!"
+    else
+      return "There are no oranges to pick!"
     end
   else
-    puts "A dead tree has nothing to pick. :("
+    return "A dead tree has nothing to pick. :("
   end
 end
 end
 
-clurr = OrangeTree.new
+ot = OrangeTree.new
 
 23.times do
-  clurr.one_year_passes
+  ot.one_year_passes
 end

96c4814192514b4fb0b5f741f1a61cea6f5f715f
Nicole Shasha <nicoleshasha@hotmail.co.uk>
2016-09-12 04:45:58 +0100

    Learn to program exercises

---
+++ ask.rb
@@ -0,0 +1,13 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if reply == 'yes'
+      return true
+    end
+    if reply == 'no'
+      return false
+    end
+end
+likes_it = ask 'Do you like eating tacos?'
+puts likes_it
--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,5 @@
 def grandfather_clock &block
-  # your code here
+  hour = (Time.new.hour + 11)%12 + 1
+  hour.times(&block)
 end
+grandfather_clock { puts 'DONG!' }

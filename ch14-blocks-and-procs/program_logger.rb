a81077610a99a602038e10d9321cfe1b142b42a5
Liam Taylor <liam.taylor888@googlemail.com>
2016-09-12 07:52:02 +0100

    Last minute tweaks in relation to rspec error messages

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,16 +1,14 @@
 def program_log desc, &block
   puts 'Beginning "' + desc + '"...'
-  output = block.call
-  puts '..."' + desc + '" finished, returning: ' + output.to_s
+  result = block.call
+  puts '..."' + desc + '" finished, returning: ' + result.to_s
 end
-
 program_log 'outer block' do
   program_log 'some little block' do
-    5
+    1**1 + 2**2
   end
-
-  log 'yet another block' do
-    "I like Thai food!"
+  program_log 'yet another block' do
+    '!doof iahT ekil I'.reverse
   end
-  16 == 8*2
+'0' == 0
 end

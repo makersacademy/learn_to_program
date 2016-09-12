9b62ba6d82cea2318b4009900069ccd288bc6e40
Frankie Bell <fbell.pro@gmail.com>
2016-09-12 17:28:22 +0100

    Logical addition, didn't over complicate this one

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,21 @@
 def profile block_description, &block
-  # your code here
+
+profiling_on = true
+
+if profiling_on
+  start_time = Time.new
+  block.call
+
+  duration = Time.new - start_time
+  puts "#{block_description}: #{duration} seconds"
+else
+  block.call
+end
+end
+
+profile 'test to work' do
+  number = 0
+  1000000.times do
+    number = number + 1
+  end
 end

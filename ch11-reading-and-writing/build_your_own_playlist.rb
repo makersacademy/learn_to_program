0eb9cc561122de6c0a0a7031c85d3a1a5cec8677
Glenn <glynester@hotmail.com>
2016-09-12 20:19:50 +0000

    build_your_own_playlist.rb Not working. Ref = 'https://github.com/makersacademy/pre_course/issues/639'

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,24 @@
-# your code here
+#I can't get this to work
+#ref = https://github.com/makersacademy/pre_course/issues/639
+
+# using the shuffle method as defined above
+
+all_mp3s = (Dir["c:/**/*.mp3"]).shuffle
+
+#all_mp3s = Dir.entries("C:/Users/g/Desktop/Zim photos - move!/Johnny Clegg & Savuka").select {|f| !File.directory? f}
+
+=begin
+all_mp3s = 
+"C:\Users\g\Desktop\Zim photos - move!\Johnny Clegg & Savuka\In My African Dream\06 Dela.mp3
+C:\Users\g\Desktop\Zim photos - move!\Johnny Clegg & Savuka\In My African Dream\07 Siyayilanda.mp3"
+=end
+
+puts all_mp3s
+
+File.open 'playlist.m3u', 'w' do |f|
+  all_mp3s.each do |mp3|
+    f.write mp3+"\n"
+  end
+end
+
+puts 'Done!'

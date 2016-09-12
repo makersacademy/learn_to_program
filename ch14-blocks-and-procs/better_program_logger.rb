72c33a5495d593862370ecd97fd75f34878b37ac
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-09-12 08:27:00 +0100

    method names altered to suit rspec

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,6 +1,6 @@
 $nesting_depth = 0
 
-def log desc, &block
+def better_log desc, &block
 
   indent = ' ' * $nesting_depth
 
@@ -10,18 +10,18 @@ def log desc, &block
   x = block.call
 
   $nesting_depth = $nesting_depth - 1
-  puts '..."' + desc + '" finished, returning: ' + x.to_s
+  puts indent + '..."' + desc + '" finished, returning: ' + x.to_s
 end
 
-log 'outer block' do
-  log 'some little block' do
-    log 'teeny-tiny block' do
+better_log 'outer block' do
+  better_log 'some little block' do
+    better_log 'teeny-tiny block' do
       'lots of love'
   end
   42
 end
 
-  log 'yet another block' do
+  better_log 'yet another block' do
     'I love Indian food!'
   end
   false
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,14 +1,14 @@
-def log desc, &block
+def program_log desc, &block
   puts 'Beginning "' + desc + '"...'
   x = block.call
   puts '..."' + desc + '" finished, returning: ' + x.to_s
 end
 
-log 'outer block' do
-  log 'some little block' do
+program_log 'outer block' do
+  program_log 'some little block' do
     5
   end
-  log 'yet another block' do
+  program_log 'yet another block' do
     'I like Thai food!'
   end
   false

46e4585b1fffebb45a04d86303884aa6ed00bdb4
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-12 06:43:41 +0000

    added grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,24 @@
-def grandfather_clock &block
-  # your code here
+def grandfather_clock block
+    
+  hours_today=Time.new.hour
+  
+  if hours_today>12
+      hours_today=hours_today/2
+  end
+  
+  (hours_today).times do
+     block.call
+  end
 end
+
+dong = Proc.new do
+    puts "DONG"
+end
+
+puts grandfather_clock dong
+
+songbird = Proc.new do
+   puts "TWEET TWEET!" 
+end
+
+puts grandfather_clock songbird

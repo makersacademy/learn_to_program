31bf17ea8ed62f3c63948acbc6f4ef4cca602ffe
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-12 07:20:43 +0000

    added program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,22 @@
 def log desc, &block
-  # your code here
+  puts "Started running #{desc}..."
+  result=block.call
+  puts "Finished running #{desc}, returning: #{result}"
 end
+
+log "time putter" do
+    
+    log "inner calc" do
+     1999
+    end
+    
+    log "Roman Numeral!" do
+       "CXXXVII" 
+    end
+    
+Time.new
+end
+
+# log "Rory putter" do
+#   puts "Rory was here" 
+# end

88918addc34ac64c609b8c6ca1908b99acd1d968
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-09-12 06:53:21 +0100

    grandfather clock

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,9 @@
 def grandfather_clock &block
-  # your code here
+  hr = (Time.new.hour + 11) % 12 + 1
+  hr.times(&block)
 end
+
+grandfather_clock { puts 'DONG!' }
+grandfather_clock { puts 'BEEP!'}
+grandfather_clock { puts 'AH-HA!' }
+grandfather_clock { puts 'BOOM!' }

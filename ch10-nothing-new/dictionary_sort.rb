b73819c6d1077bc2fbdeadc69e4a9cac0cbfba60
Dagmara Szkurlat <daga.sz@hotmail.com>
2016-09-13 21:09:34 +0000

    sorting

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,35 @@
 def dictionary_sort arr
-  # your code here
+  recursive_sort arr, []
 end
+
+#the method that actually does sorting
+def recursive_sort unsorted_array, sorted_array
+    j = 1
+    
+    smallest = unsorted_array[0].to_s.downcase
+    
+    until unsorted_array.length == 0
+        
+        smallest_index = 0
+        
+        # find smallest in unsorted_array
+        while j < unsorted_array.length
+            if unsorted_array[j].to_s.downcase < smallest
+                smallest = unsorted_array[j].to_s.downcase
+                smallest_index = j
+            end
+            j += 1
+        end
+        
+        # move it from the unsorted array to the end of already_sorted
+        sorted_array << unsorted_array[smallest_index]
+        unsorted_array.delete_at(smallest_index)
+        
+        # find next smallest with recursive call
+        recursive_sort unsorted_array, sorted_array
+    end
+    
+    return sorted_array
+end
+
+puts dictionary_sort ["avada", "Kedavra", "black", "SirIUs", "harry"]
--- ch10-nothing-new/sort.rb
+++ ch10-nothing-new/sort.rb
@@ -1,3 +1,30 @@
 def sort arr
-  # your code here
+# wrapper method; pass given array for sorting, along with an empty array
+  recursive_sort arr, []
 end
+
+#the method that actually does sorting
+def recursive_sort unsorted_array, sorted_array
+    j = 1
+    
+    smallest = unsorted_array[0]
+    
+    until unsorted_array.length == 0
+        # find smallest in unsorted_array
+        while j < unsorted_array.length
+            smallest = unsorted_array[j] if unsorted_array[j] < smallest
+            j += 1
+        end
+        
+        # move it from the unsorted array to the end of already_sorted
+        unsorted_array.delete(smallest)
+        sorted_array << smallest
+        
+        # find next smallest with recursive call
+        recursive_sort unsorted_array, sorted_array
+    end
+    
+    return sorted_array
+end
+
+puts sort ["avada", "kedavra", "black", "sirius", "harry"]

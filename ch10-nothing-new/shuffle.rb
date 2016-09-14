1d6ba8284a23d0e990bbc71c67c9d3ac6f057342
Tom Yates <tomy8s@hotmail.com>
2016-09-14 21:00:41 +0100

    c10 pt2

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,9 @@
-def shuffle arr
-  # your code here
+def shuffle arr, shuffled=[]
+  r = rand(arr.length)
+  shuffled << arr[r]
+  arr.delete_at(r)
+  if arr.length > 0
+    shuffle arr, shuffled
+  end
+  shuffled
 end

729c88d64ac7201121806a1b45ceeeccc8266ed4
Tom Yates <tomy8s@hotmail.com>
2016-09-14 20:46:16 +0100

    c10 pt1

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,14 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort arr, sorted=[]
+  a = arr[0]
+  arr.each do |i|
+    if i <= a
+      a = i
+    end
+  end
+  arr.delete_at(arr.index(a))
+  sorted << a
+  if arr.length > 0
+    dictionary_sort arr, sorted
+  end
+  sorted
 end

dd44275a9e274b8e8151b43222753bd2a97e5971
Frances Maxwell <fmaxwell@gmail.com>
2016-09-14 23:06:29 +0100

    Adding english_number.rb for the second time.

--- ch10-nothing-new/english_number.rb
+++ ch10-nothing-new/english_number.rb
@@ -12,61 +12,45 @@ def english_number number
   ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
   tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  zillions = [['hundred',2],
+              ['thousand', 3],
+              ['million', 6],
+              ['billion', 9],
+              ['trillion', 12],
+              ['quadrillion', 15],
+              ['quintillion', 18],
+              ['sextillion', 21],
+              ['septillion', 24],
+              ['octillion', 27],
+              ['nonillion', 30],
+              ['decillion', 33],
+              ['undecillion', 36],
+              ['duodecillion', 39],
+              ['tredecillion', 42],
+              ['quattuordecillion', 45],
+              ['quindecillion', 48],
+              ['sexdicillion', 51],
+              ['septendecillion', 54],
+              ['octodecillion', 57],
+              ['novembdecillion', 60],
+              ['vigintillion', 63],
+              ['googol', 100]]
 
   left = number
 
-  write = left/1000000000000
-  left = left - write*1000000000000
+  while zillions.length > 0
+    zil_pair = zillions.pop
+    zil_name = zil_pair[0]
+    zil_base = 10 ** zil_pair[1]
+    write = left/zil_base
+    left = left - write*zil_base
 
-  if write > 0
-    trillions = english_number write
-    num_string = num_string + trillions + " trillion"
-    if left > 0
-      num_string = num_string + " "
-    end
-  end
-
-  write = left/1000000000
-  left = left - write*1000000000
-
-  if write > 0
-    billions = english_number write
-    num_string = num_string + billions + " billion"
-    if left > 0
-      num_string = num_string + " "
-    end
-  end
-
-  write = left/1000000
-  left = left - write*1000000
-
-  if write > 0
-    millions = english_number write
-    num_string = num_string + millions + " million"
-    if left > 0
-      num_string = num_string + " "
-    end
-  end
-
-  write = left/1000
-  left = left - write*1000
-
-  if write > 0
-    thousands = english_number write
-    num_string = num_string + thousands + " thousand"
-    if left > 0
-      num_string = num_string + " "
-    end
-  end
-
-  write = left/100
-  left = left - write*100
-
-  if write > 0
-    hundreds = english_number write
-    num_string = num_string + hundreds + " hundred"
-    if left > 0
-      num_string = num_string + " "
+    if write > 0
+      prefix = english_number write
+      num_string = num_string + prefix + " " + zil_name
+      if left > 0
+        num_string = num_string + " "
+      end
     end
   end
 
@@ -96,3 +80,10 @@ def english_number number
   num_string
 
 end
+
+# puts english_number 937234827364876238423423487686234
+# puts english_number 234987234982398798423
+# puts english_number 23432
+# puts english_number 234
+# puts english_number 3487876879872340983495768762340834876827634234328745768762348768736458983745435
+# puts english_number 9898176572634765762999987987

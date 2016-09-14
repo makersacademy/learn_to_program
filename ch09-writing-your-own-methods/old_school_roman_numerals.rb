acb8ab0fed203367b2ec2b894896b214577704ad
Tom Yates <tomy8s@hotmail.com>
2016-09-14 18:54:40 +0100

    c9 pt2

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,19 @@
 def old_roman_numeral num
-  # your code here
+  r = ''
+  r += 'M' * (num / 1000)
+  num = num % 1000
+  r += 'D' * (num / 500)
+  num = num % 500 
+  r += 'C' * (num / 100)
+  num = num % 100
+  r += 'L' * (num / 50)
+  num = num % 50
+  r += 'X' * (num / 10)
+  num = num % 10
+  r += 'V' * (num / 5)
+  num = num % 5
+  r += 'I' * num
+  r
 end
+
+puts old_roman_numeral 1549

e7b0c1029e037ead75fd11c8df8763d99a736432
Tom Yates <tomy8s@hotmail.com>
2016-09-15 19:50:37 +0100

    c11 pt2

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,13 +1,21 @@
 tracks = Dir['C:/Users/Tom/Test/**/*.mp3']
 Dir.chdir 'C:/Users/Tom/Test/'
-content =  tracks.each do |t|  
-        "#{t}/n"
-    end
-
 tracklist = 'playlist.m3u'
 
+def shuffle arr, shuffled=[]
+  r = rand(arr.length)
+  shuffled << arr[r]
+  arr.delete_at(r)
+  if arr.length > 0
+    shuffle arr, shuffled
+  end
+  shuffled
+end
+
+shuffled = shuffle(tracks)
+
 File.open tracklist, 'w' do |f|
-    tracks.each do |t|
+    shuffled.each do |t|
         f.write(t + "\n")
     end
 end

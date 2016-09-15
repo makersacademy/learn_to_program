e239a2adde65ac8c954c304bf24e35b77cafa9cb
Tom Yates <tomy8s@hotmail.com>
2016-09-15 20:47:57 +0100

    c12 pt2

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,13 @@
-# your code here
+puts 'Enter birth year:'
+y = gets.chomp.to_i
+puts 'Enter birth month (1-12):'
+m = gets.chomp.to_i
+puts 'Enter birth day:'
+d = gets.chomp.to_i
+
+bday = Time.local(y, m, d)
+today = Time.new
+
+age = today - bday
+
+(age / (60 * 60 * 24 * 365.25)).to_i.times{puts 'SPANK!'}

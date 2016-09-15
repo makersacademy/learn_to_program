31c9a1642b38adc9dcc2379216bbf06b9620342d
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-15 16:12:46 +0000

    dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,24 +1,12 @@
-def dictionary_sort arr
-  rec_dict_sort arr, []
-end
-def rec_dict_sort unsorted, sorted
-    if unsorted.length <= 0
-        return sortedend
-        
-        smallest = unsorted.pop
-        still_unsorted = []
-        
-        unsorted.each do |tested_object|
-            if tested_object.downcase < smallest.downcase
-                still_unsorted.push smallest
-                smallest = tested_object
-            else
-                still_unsorted.push tested_object
-            end 
-        end 
-        sorted.push smallest
-        
-        rec_dict_sort still_unsorted, sorted
-    end 
+def dictionary_sort arr 
+    return arr if arr.length <= 1
     
-    puts(dictionary_sort(['can', 'feel', 'singing.', 'like', 'A', 'can']))
+    middle = arr.pop
+    less = arr.select{|x| x.downcase < middle.downcase}
+    morr = arr.select{|x| x.downcase >= middle.downcase}
+    
+    dictionary_sort(less) + [middle] + dictionary_sort(more)
+end
+
+words = ['can', 'feel', ' singing', 'lobster', 'banana']
+puts(dictionary_sort(words).join(''))

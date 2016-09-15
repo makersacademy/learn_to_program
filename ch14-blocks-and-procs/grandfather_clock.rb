e61f6b22b3964ced9611facbf0aa7b48e1eaefec
Dagmara Szkurlat <daga.sz@hotmail.com>
2016-09-15 18:28:14 +0000

    Clock and logger

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,11 @@
 def grandfather_clock &block
-  # your code here
+  hours = Time.now.hour + 1
+  hours -= 12 if hours > 12
+  hours.times do
+      block.call
+  end
+end
+
+grandfather_clock do
+    puts 'DONG!'
 end
--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,12 @@
 def log desc, &block
-  # your code here
+ puts "started block #{desc}"
+ puts "ended block #{desc}, result is: #{block.call}"
+end
+
+log 'passing1' do 
+    log 'passing2' do
+        puts "I'm bock 2"
+        3*4
+    end
+    10+10
 end

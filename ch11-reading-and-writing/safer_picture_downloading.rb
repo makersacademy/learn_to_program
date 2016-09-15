e496c250c9db5b00431489c57adf5a7ce0239e2d
Tom Yates <tomy8s@hotmail.com>
2016-09-15 19:14:23 +0100

    c11 pt1

--- ch11-reading-and-writing/safer_picture_downloading.rb
+++ ch11-reading-and-writing/safer_picture_downloading.rb
@@ -1,1 +1,30 @@
-# your code here
+Dir.chdir 'C:/Users/Tom/Photos'
+
+pic_names = Dir['C:/Users/Tom/Test/*.jpg']
+
+batch_name = '.'
+
+while File.exists?(batch_name)
+    print 'Desired batch name: '
+    batch_name = gets.chomp
+    puts
+    if File.exists?(batch_name)
+        puts "#{batch_name} already exists. Think of a new batch name."
+    end
+end
+print "Copying #{pic_names.length} files: "
+
+pic_num = 1
+
+pic_names.each do |name|
+    print '.'
+    new_name = if pic_num < 10 and File.exists?("#{batch_name}0#{pic_num}.jpg") == false
+        "#{batch_name}0#{pic_num}.jpg"
+    elsif File.exists?("#{batch_name}#{pic_num}.jpg") == false
+        "#{batch_name}#{pic_num}.jpg"
+    else
+        exit
+    end
+    File.rename name, new_name
+    pic_num += 1
+end

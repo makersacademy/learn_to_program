78ae3df1e405816a92f748032c35355a7c796852
Tom Yates <tomy8s@hotmail.com>
2016-09-15 21:42:11 +0100

    c12 pt3

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,34 @@
 def roman_to_integer roman
-  # your code here
+  roman.upcase!
+  num = 0
+  roman.each_char do |l|
+    case l
+    when 'M'
+      num += 1000
+    when 'D'
+      num += 500
+    when 'C'
+      num += 100
+    when 'L'
+      num += 50
+    when 'X'
+      num += 10
+    when 'V'
+      num += 5
+    when 'I'
+      num += 1
+    end
+  end
+  if roman.include?('CM') or roman.include?('CD')
+    num -= 200
+  end
+  if roman.include?('XC') or roman.include?('XL')
+    num -= 20
+  end
+  if roman.include?('IX') or roman.include?('IV')
+    num -= 2
+  end
+  num
 end
+
+['i','iii','iv','v','vii','ix','x'].each{ |n| roman_to_integer n}

8955a3655397b7bdcd1d4ac1b12ad79a7e7e6ad9
Tom Yates <tomy8s@hotmail.com>
2016-09-15 20:26:02 +0100

    c11 pt3

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,3 +1,20 @@
-def music_shuffle filenames
-  # your code here
+def music_shuffle arr, shuffled=[]
+  r = rand(arr.length)
+  shuffled << arr[r]
+  arr.delete_at(r)
+  if arr.length > 0
+    shuffle arr, shuffled
+  end
+  if shuffled.length > 2 #this algorithm needs at least 3 elements to work
+    n = 2 #start at index 2, compare with index 1 and if they're both in 
+    #the same directory switch switsh index 0 with index 1 
+    #(this may cause new adjacencies to be set up and won't crash if (almost) 
+    #all files come from the same directory')
+    (shuffled.length - 2).times do
+      if shuffled[n].split('/')[-2] == shuffled[n - 1].split('/')[-2]
+        temp = shuffled[n - 2]
+        shuffled[n - 2] = shuffled[n - 1]
+        shuffled[n - 1] = temp
+      end
+    end
 end

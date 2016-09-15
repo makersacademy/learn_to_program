3274c2ef3bcd97412b05f8c5c44f8e84e5340a66
Tom Yates <tomy8s@hotmail.com>
2016-09-15 19:42:55 +0100

    c11 pt2

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,13 @@
-# your code here
+tracks = Dir['C:/Users/Tom/Test/**/*.mp3']
+Dir.chdir 'C:/Users/Tom/Test/'
+content =  tracks.each do |t|  
+        "#{t}/n"
+    end
+
+tracklist = 'playlist.m3u'
+
+File.open tracklist, 'w' do |f|
+    tracks.each do |t|
+        f.write(t + "\n")
+    end
+end

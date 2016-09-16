c80fa555fa9d1f47a90be2952ad7fd9a1e9163cc
Tom Yates <tomy8s@hotmail.com>
2016-09-16 17:30:04 +0100

    c13 pt1

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,3 +1,13 @@
 class Integer
-  # your code here
+  def to_roman
+    r = ''
+    r += 'M' * (self / 1000)
+    r += self % 1000 > 899 ? 'CL' : 'D' * (self % 1000 / 500)
+    r += self % 1000 > 899 ? '' : self % 500 > 449 ? 'CD' : 'C' * (self % 500/ 100)
+    r += self % 100 > 89 ? 'XC' : 'L' * (self % 100 / 50)
+    r += self % 100 > 89 ? '' : self % 50 > 39 ? 'XL' : 'X' * (self % 50 / 10)
+    r += self % 10 > 8 ? 'IX' : 'V' * (self % 10 / 5)
+    r += self % 10 > 8 ? '' : self % 5 > 3 ? 'IV' : 'I' * (self % 5)
+    r
+  end
 end

6661a7a3e6bb573a0681b55319dbe21101e81a40
Tom Yates <tomy8s@hotmail.com>
2016-09-16 21:54:50 +0100

    fixed factorial

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,6 +1,6 @@
 class Integer
   def factorial
-    self == 0 ? 1 : self * factorial(self - 1)
+    self == 0 ? 1 : self * (self - 1).factorial
   end
   def to_roman
     'M' * (self/1000) + 'D' * (self % 1000 / 500) + 'C' * (self % 500 / 100) + 'L' * (self % 100 / 50) + 'X' * (self % 50 / 10) + 'V' * (self % 10 / 5) + 'I' * (self % 5)

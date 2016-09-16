7c31f1753e5fac94742e2f70a5c78962015c1cec
Tom Yates <tomy8s@hotmail.com>
2016-09-16 17:19:22 +0100

    c12 pt4

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,1 +1,16 @@
-# your code here
+raw = File.read 'ch12-new-classes-of-objects/birthdays.txt'
+
+hash = {}
+
+raw.each_line do |l|
+    hash[l.split(',')[0]] = l.split(',')[1]
+end
+
+puts hash
+
+puts 'Whose birthday are you looking for?'
+bday_biyl = gets.chomp
+
+bday_day = hash[bday_biyl]
+
+puts "#{bday_biyl}'s birthday is on#{bday_day}."

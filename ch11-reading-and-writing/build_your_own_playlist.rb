c81f971f57c77d4375de2ff42b845e4cfffe7eba
Frances Maxwell <fmaxwell@gmail.com>
2016-09-16 15:09:26 +0100

    Adding playlist file.

--- ch11-reading-and-writing/build_your_own_playlist.rb
+++ ch11-reading-and-writing/build_your_own_playlist.rb
@@ -1,1 +1,36 @@
-# your code here
+# Search for music files
+# Build you a playlist, shuffling the items
+# Save it as an m3u file
+
+def shuffle arr
+  recursive_shuffle arr, []
+end
+
+def recursive_shuffle original_array, shuffled_array
+
+  if original_array.length <= 0
+    return shuffled_array
+  end
+
+  # Remove the last item from the original array
+  random_element = original_array.pop
+
+  # Insert it in a random place in the shuffled array
+  shuffled_array.insert(rand(shuffled_array.length+1), random_element)
+
+  # Call the method again until all the elements have been shuffled
+  recursive_shuffle original_array, shuffled_array
+end
+
+audio_files = Dir["../**/*.{MP3,mp3}"] # Search in the parent directory and all its subdirectories
+
+shuffled_files = []
+shuffled_files = shuffle audio_files
+
+playlist = ""
+
+shuffled_files.each do |filename|
+  playlist += filename + "\n"
+end
+
+puts playlist

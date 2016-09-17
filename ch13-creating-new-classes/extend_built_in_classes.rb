fd87546286c85d82275e7024d678ed3b5ed4bacb
Frances Maxwell <fmaxwell@gmail.com>
2016-09-17 21:46:31 +0100

    Added the roman method extension to the Integer class.

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -6,4 +6,17 @@ class Integer
       self * (self-1).factorial
     end
   end
+
+  def to_roman
+    numberMs = "M" * (self / 1000)
+    numberDs = "D" * ((self % 1000) / 500)
+    numberCs = "C" * ((self % 500) / 100)
+    numberLs = "L" * ((self % 100) / 50)
+    numberXs = "X" * ((self % 50) / 10)
+    numberVs = "V" * ((self % 10) / 5)
+    numberIs = "I" * ((self % 5) / 1)
+
+    return numberMs + numberDs + numberCs + numberLs + numberXs + numberVs + numberIs
+
+  end
 end

b941456247ad2812f9a313c4a09a407d5c685e87
Amaal Ali <amq.ali@gmail.com>
2016-09-19 06:31:34 +0100

    Commiting uncompleted work

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,23 @@
 def roman_to_integer roman
-  # your code here
+  rsym = Hash.new
+  rsym['M'] = 1000
+  rsym['D'] = 500
+  rsym['C'] = 100
+  rsym['L'] = 50
+  rsym['X'] = 10
+  rsym['V'] = 5
+  rsym['I'] = 1
+
+  symb = roman.split(//)
+
+  
+
+
+
+
+
+
+
+
+
 end

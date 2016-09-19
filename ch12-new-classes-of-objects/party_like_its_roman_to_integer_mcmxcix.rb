7a69ff935304b5ed9ca3add784242bf72265c760
Amaal Ali <amq.ali@gmail.com>
2016-09-19 20:34:48 +0100

    Roman > Int converter working

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,23 +1,43 @@
 def roman_to_integer roman
-  rsym = Hash.new
-  rsym['M'] = 1000
-  rsym['D'] = 500
-  rsym['C'] = 100
-  rsym['L'] = 50
-  rsym['X'] = 10
-  rsym['V'] = 5
-  rsym['I'] = 1
+  roman_lookup = Hash.new
+  roman_lookup['M'] = 1000
+  roman_lookup['D'] = 500
+  roman_lookup['C'] = 100
+  roman_lookup['L'] = 50
+  roman_lookup['X'] = 10
+  roman_lookup['V'] = 5
+  roman_lookup['I'] = 1
 
-  symb = roman.split(//)
+  num_val = Array.new
+  to_convert = roman.split(//)
 
-  
-
-
-
-
-
-
-
-
-
+  #converting to numberical values
+  to_convert.each do |sym|
+    if num_val.length == 0
+      num_val.push(roman_lookup[sym])
+    elsif num_val.last.to_i < roman_lookup[sym].to_i
+      new_num_val_last = -1 * num_val.pop
+      num_val.push(new_num_val_last)
+      num_val.push(roman_lookup[sym])
+    else
+      num_val.push(roman_lookup[sym])
+    end
+  end
+num_val.reduce(:+)
 end
+
+#puts 'Simple'
+#puts
+#puts roman_to_integer('I')
+#puts roman_to_integer('V')
+#puts roman_to_integer('VI')
+#puts roman_to_integer('CXI')
+#puts roman_to_integer('DLXIII')
+#puts
+#puts 'Harder'
+#puts roman_to_integer('IV')
+#puts roman_to_integer('IX')
+#puts roman_to_integer('XIV')
+#puts roman_to_integer('XL')
+#puts roman_to_integer('XCIV')
+#puts roman_to_integer('MMCMXCIX')

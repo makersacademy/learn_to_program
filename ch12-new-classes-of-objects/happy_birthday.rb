c65e629fee4ef9fd888c2c0d9fa43fa5ceaa90b2
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-20 20:38:28 +0000

    Complete happy_birthday.rb exercise

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,23 @@
-# your code here
+puts "What year were you born? (e.g. 1982)"
+year = gets.chomp
+puts "What month were you born? (e.g. if you were born in February, write '2')"
+month = gets.chomp
+puts "What day were you born?(e.g. if you were born on 19th of month, write '19')"
+day = gets.chomp
+
+birthday = Time.gm(year, month, day)
+
+age_seconds = Time.new - birthday
+
+age_years = age_seconds/60/60/24/365
+
+spank = age_years.to_i
+
+puts "Happy birthday! You are #{spank} years old!"
+
+spank.times do
+   puts "SPANK!"
+end
+
+#puts birthday
+#puts Time.new

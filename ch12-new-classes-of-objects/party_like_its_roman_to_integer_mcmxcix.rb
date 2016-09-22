13c4e88dcf4a2a903536bfd05aa0396d4f2bf21d
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-22 15:09:42 +0000

    Write program that converts roman numerals to integers

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,30 @@
 def roman_to_integer roman
-  # your code here
+  roman.upcase!
+  digits = {'M' => 1000,
+  'D' => 500,
+  'C' => 100,
+  'L' => 50,
+  'X' => 10,
+  'V' => 5,
+  'I' => 1}
+  
+  total = 0
+  prev = 0
+  roman.reverse.each_char do |char|
+      if !digits.has_key?(char)
+          puts "Please enter a valid roman numeral"
+      else
+          val = digits[char]
+          if val < prev
+            val *= -1
+          else
+            prev = val
+          end
+      end
+      total += val
+  end
+  total
 end
+
+puts roman_to_integer("mcmxcix")
+puts roman_to_integer('MMXiv')

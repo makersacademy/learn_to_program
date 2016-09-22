5f4ced6e0e22f959e6821f101f9a438176416279
Glenn <glynester@hotmail.com>
2016-09-22 20:24:46 +0000

    birthday_helper.rb calculates next birthday and age on next birthday.

--- ch12-new-classes-of-objects/birthday_helper.rb
+++ ch12-new-classes-of-objects/birthday_helper.rb
@@ -1,13 +1,12 @@
-# your code here
-# You’ll probably want to break each line up and put it in a hash, using the name as your key 
-#and the date as your value. In other words: words: birth_dates['The King of Spain'] = 'Jan 5, 1938'
-
-#[{:name=>"Christopher Alexander", :month_day=>"Oct 4", :year=>"1936"}, 
-#{:name=>"Christopher Lambert", :month_day=>"Mar 29", :year=>"1957"},  etc.]
+require 'date'
 birth_dates=[]
 file = File.open("names.txt", "r")
 file.readlines.each{|person|
 name,month_day,year = person.chomp.split(",")
+if name.nil?
+    puts "Error: Blank lines in the file." 
+    exit
+end    
 name.strip!; month_day.strip!; year.strip!
     birth_dates << {name: name,month_day: month_day,year: year} 
 }
@@ -17,9 +16,23 @@ year_get = ""
 puts "Enter a name."
 name_get = gets.chomp
 person = birth_dates.select {|person| person[:name]  == name_get }
+if person == []
+    puts "Error: That person does not exist in the file." 
+    exit
+end  
 month_day_get = person[0]["month_day".to_sym]
-year_get = person[0][:year]
+year_get = person[0][:year].to_i
+month_get = month_day_get[0..2]
+day_get = month_day_get.delete("^0-9")  
+year_now = Time.now.year
+puts year_now
+date_now = Time.now
+if date_now > Time.new(year_now,month_get,day_get)
+    year_now+=1
+end    
+puts "Hi #{name_get}! You were born on #{month_day_get} in #{year_get}."
+puts "Your next birthday is on #{day_get} #{month_get} in #{year_now}."
+puts "On your next birthday you will be #{year_now-year_get} years old."
 
-puts "Hi #{name_get}! You were born on #{month_day_get} in #{year_get}"
-
-    
+#[{:name=>"Christopher Alexander", :month_day=>"Oct 4", :year=>"1936"}, 
+#{:name=>"Christopher Lambert", :month_day=>"Mar 29", :year=>"1957"},  etc.]    

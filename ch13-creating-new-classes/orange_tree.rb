3712fe1752db599d48ac96daa0261f3ecff98b0a
Glenn <glynester@hotmail.com>
2016-09-23 16:45:13 +0000

    orange tree. Now the tree can sponteously die after the age of 20.

--- ch13-creating-new-classes/orange_tree.rb
+++ ch13-creating-new-classes/orange_tree.rb
@@ -20,6 +20,11 @@ class OrangeTree
       @height
   end
   
+  def count_the_oranges
+      @orange_count = 0 if !@alive || @age <= 5
+      @orange_count
+  end
+  
   def pick_an_orange(picked = 1)
       if @alive && @oranges
           print "You picked #{picked} juicy orange"
@@ -39,22 +44,37 @@ class OrangeTree
       end
   end
   
+  def possible_spontaneous_death_by_nematode_infestation
+      if @age >= 20 && rand(3) == 1 && @alive 
+          puts "Unfortunately the tree just died. There was a 66.66% chance it would survive but sometimes bad stuff beats the odds!"
+          @alive = false
+          @orange_count = 0
+      elsif @age < 20 && @alive 
+          puts "The tree is young enough to fight off the nematode infection."      
+      elsif !@alive
+          puts "This tree is already dead. Nematodes do not affect a dead tree!"
+      else      
+          puts "The tree successfully fought off the nematode infection."
+      end      
+  end
+  
   
   def one_year_passes
       @growth += 0.4
       @age += 1
       @orange_count = ((@growth * 15) - 25).round             #Resets each year.
       @oranges = true if @age > 5 && @alive == true
-      if @age <= 25
+      if @age <= 25 && @alive
           
           puts "Your orange tree '#{@name}' is now #{@age} years old." 
           puts "It is #{@growth.round(2)}m high."
           
           puts "Your orange tree '#{@name}' is now old enough to produce oranges." if @age == 6
           puts "Your tree has produced #{((@growth * 15) - 25).round} oranges this year!" if @age >= 6
-      elsif @age > 25
+      elsif @age > 25 || !@alive
           puts "Your orange tree '#{@name}' has died."
           @alive = false
+          @orange_count = 0
           @oranges = false
       end
   end
@@ -65,14 +85,23 @@ ot = OrangeTree.new "JuicyLucy"
 ot.one_year_passes
 ot.pick_an_orange
 ot.one_year_passes
+puts ot.count_the_oranges
+ot.possible_spontaneous_death_by_nematode_infestation
 ot.one_year_passes
 ot.one_year_passes
+puts ot.count_the_oranges
 ot.one_year_passes
 ot.one_year_passes
 ot.pick_an_orange 7
+puts ot.count_the_oranges
 ot.pick_an_orange 2
+ot.pick_an_orange 3
+puts ot.count_the_oranges
 ot.one_year_passes
+puts ot.count_the_oranges
+ot.pick_an_orange 17
 ot.one_year_passes
+ot.possible_spontaneous_death_by_nematode_infestation
 ot.one_year_passes
 ot.one_year_passes
 ot.one_year_passes
@@ -80,19 +109,29 @@ ot.pick_an_orange(5)
 ot.one_year_passes
 ot.one_year_passes
 ot.one_year_passes
+ot.pick_an_orange(100)
+ot.one_year_passes
+ot.one_year_passes
+ot.one_year_passes
+puts ot.count_the_oranges
 ot.one_year_passes
 ot.one_year_passes
 ot.one_year_passes
 ot.one_year_passes
-ot.one_year_passes
-ot.one_year_passes
-ot.one_year_passes
+ot.possible_spontaneous_death_by_nematode_infestation
 ot.one_year_passes
 ot.one_year_passes
 ot.pick_an_orange (9)
+ot.possible_spontaneous_death_by_nematode_infestation
 ot.one_year_passes
+ot.possible_spontaneous_death_by_nematode_infestation
 ot.one_year_passes
+ot.possible_spontaneous_death_by_nematode_infestation
+puts ot.count_the_oranges
 ot.one_year_passes
+ot.possible_spontaneous_death_by_nematode_infestation
+ot.one_year_passes
+puts ot.count_the_oranges
 ot.one_year_passes
 ot.pick_an_orange 21
 ot.one_year_passes

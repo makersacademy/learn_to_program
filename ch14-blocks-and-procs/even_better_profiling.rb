ea2261b2da2c9284429bf11369e78374895088fc
Glenn <glynester@hotmail.com>
2016-09-23 09:48:09 +0000

    Profiling can be turned on and off now.

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,20 @@
 def profile block_description, &block
-  # your code here
+  do_profile = false
+    if do_profile
+      start_time = Time.new
+      block.call
+      duration = Time.new - start_time
+      puts "'#{block_description}' took #{duration} seconds to run."
+    else
+      block.call
+    end  
 end
+
+profile 'Count to a high number' do
+    num = 0
+    1000000.times do
+        num += 1
+    end
+    puts num
+end
+

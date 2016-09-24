b6649a538374243cd3a859ec745cf5b321d81119
Glenn <glynester@hotmail.com>
2016-09-24 12:07:52 +0000

    Shuffle method completed.

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,13 +1,37 @@
 def music_shuffle filenames
-	#print filenames; puts
+	randomized_files = []
 	length = filenames.length
 	indexes = (0...length).to_a
-	#print indexes; puts
 	indexes.shuffle!
-	# print indexes; puts	"KKKKKKKK"
-	# puts indexes[1]; puts	"GGGGGGGGG"
-	# puts filenames[indexes[1]]; puts	"BBBBBBBBB"
-	randomized_files = []
+	temp1 = []; temp2 = []; temp3 = []
+	indexes.each{
+		loop do		
+			item = indexes[rand(length)]
+			if !temp1.include? item
+				temp1 << item 
+				break
+			end
+		end
+	}
+	temp1.each{
+		loop do		
+			item = temp1[rand(length)]
+			if !temp2.include? item
+				temp2 << item 
+				break
+			end
+		end
+	}
+	temp2.each{
+		loop do		
+			item = temp2[rand(length)]
+			if !temp3.include? item
+				temp3 << item 
+				break
+			end
+		end
+	}
+	indexes = temp3.shuffle!
 	for count in 0...length
 		randomized_files << filenames[indexes[count]]
 	end
@@ -19,7 +43,7 @@ output_dir = 'D:/RUBY_TEST_DELETE/'
 Dir.chdir output_dir	#Working directory where the m3u file will be writtten to.
 										#If this is not specified the m3u file will be written to the directory where the program was run from.
 search_dir = "D:/RUBY_TEST_DELETE/**/*.mp3" #Where the mp3 files should be searched for.
-all_mp3s = (Dir[search_dir])						#.shuffle
+all_mp3s = (Dir[search_dir])						
 
 all_mp3s = music_shuffle all_mp3s
 puts all_mp3s

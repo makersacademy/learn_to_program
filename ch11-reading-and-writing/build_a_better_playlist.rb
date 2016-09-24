c22099ac1eb61b1b23220f1f1d0ce1a631f12551
Glenn <glynester@hotmail.com>
2016-09-24 10:56:23 +0000

    Interim dev on shuffle method. It works but need to make it more random now.

--- ch11-reading-and-writing/build_a_better_playlist.rb
+++ ch11-reading-and-writing/build_a_better_playlist.rb
@@ -1,23 +1,27 @@
 def music_shuffle filenames
-  # your code here
+	#print filenames; puts
+	length = filenames.length
+	indexes = (0...length).to_a
+	#print indexes; puts
+	indexes.shuffle!
+	# print indexes; puts	"KKKKKKKK"
+	# puts indexes[1]; puts	"GGGGGGGGG"
+	# puts filenames[indexes[1]]; puts	"BBBBBBBBB"
+	randomized_files = []
+	for count in 0...length
+		randomized_files << filenames[indexes[count]]
+	end
+	randomized_files		#return filenames to music_shuffle.
 end
 
 #Copy of "build_your_own_playlist.rb" below.
-#I can't get this to work
-#ref = https://github.com/makersacademy/pre_course/issues/639
+output_dir = 'D:/RUBY_TEST_DELETE/'
+Dir.chdir output_dir	#Working directory where the m3u file will be writtten to.
+										#If this is not specified the m3u file will be written to the directory where the program was run from.
+search_dir = "D:/RUBY_TEST_DELETE/**/*.mp3" #Where the mp3 files should be searched for.
+all_mp3s = (Dir[search_dir])						#.shuffle
 
-# using the shuffle method as defined above
-
-all_mp3s = (Dir["c://**/*.mp3"]).shuffle
-
-#all_mp3s = Dir.entries("C:/Users/g/Desktop/Zim photos - move!/Johnny Clegg & Savuka").select {|f| !File.directory? f}
-
-=begin
-all_mp3s = 
-"C:\Users\g\Desktop\Zim photos - move!\Johnny Clegg & Savuka\In My African Dream\06 Dela.mp3
-C:\Users\g\Desktop\Zim photos - move!\Johnny Clegg & Savuka\In My African Dream\07 Siyayilanda.mp3"
-=end
-
+all_mp3s = music_shuffle all_mp3s
 puts all_mp3s
 
 File.open 'playlist.m3u', 'w' do |f|
@@ -25,6 +29,13 @@ File.open 'playlist.m3u', 'w' do |f|
     f.write mp3+"\n"
   end
 end
-
-puts 'Done!'
-
+puts
+puts "A randomized list of songs found in \"#{search_dir}\" has been created in \"#{output_dir}\"."
+ 
+ 
+=begin
+ Instead of using your old shuffle, write a new music_shuffle method. It should take an array of filenames 
+ (like those listed previously) and mix them up good and proper. You’ll probably need to use the split method for strings. 
+ It returns an array of chopped-up pieces of the original string, split where you specify, like this: 
+	awooga = 'this/is/not/a/daffodil'.split '/' 
+=end

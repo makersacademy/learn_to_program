86bf93cbef1343321bc010107bf643447392bad4
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-25 14:41:04 +0000

    better_program_logger.rb

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,3 +1,24 @@
+$log_depth = 0
+
 def log desc, &block
-  # your code here
+    prefix = ' ' * $log_depth
+    puts prefix + "Beginning '#{desc}'..."
+    $log_depth = $log_depth + 1
+    output = block.call
+    $log_depth = $log_depth - 1
+    puts prefix + "...'#{desc}' finished, returning: #{output}"
+  
+end
+
+log "outer block" do
+    log "some little block" do
+        log "teeny-tiny block" do
+            "lots of love"
+        end
+        42
+    end
+    log "yet another block" do
+        "I like Thai food!"
+    end
+    false
 end

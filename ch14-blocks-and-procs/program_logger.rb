56d8c30acc097e135f5a40715c561c40ff8b8699
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-25 14:27:55 +0000

    program_logger.rb

--- ch14-blocks-and-procs/program_logger.rb
+++ ch14-blocks-and-procs/program_logger.rb
@@ -1,3 +1,16 @@
 def log desc, &block
-  # your code here
+    puts "Beginning '#{desc}'"
+    output = block.call
+    puts "...'#{desc}' finished, returning: \n#{output}"
+  
+end
+
+log "outer block" do
+    log "some little block" do
+        5
+    end
+    log "yet another block" do
+        "I like Thai food!"
+    end
+    false
 end

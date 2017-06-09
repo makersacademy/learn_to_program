3544e789bf40c09dd0a7ab7432765aa039ff7858
daviryan <daviryan@aol.com>
2017-06-09 22:03:06 +0000

    ch9 roman numerals programs

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,20 @@
 def old_roman_numeral num
   # your code here
+  num = num.to_i
+    (puts "Invalid input. Choose a number between 1 and 3000"; return) if ((num.to_i < 1) || (num.to_i > 3000))
+  
+    numerals = {1000=>"M", 500=>"D", 100=>"C", 50=>"L", 10=>"X", 5=>"V", 1=>"1" }
+    roman = ""
+  
+    numerals.keys.each do |k|
+        n = num/k
+        if n > 0
+            roman += numerals[k]*n
+            num = num % k
+        end
+    end
+    
+  puts roman
 end
+
+old_roman_numeral (gets.chomp)
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -3,7 +3,7 @@ def roman_numeral num
     num = num.to_i
     (puts "Invalid input. Choose a number between 1 and 3000"; return) if ((num.to_i < 1) || (num.to_i > 3000))
   
-    numerals = {1000=>"M", 500=>"D", 100=>"C", 50=>"L", 10=>"X", 5=>"V", 1=>"1" }
+    numerals = {1000=>"M", 500=>"D", 400=>"CD,", 100=>"C", 90=>"XC", 50=>"L", 40=>"XL", 10=>"X", 5=>"V", 4=>"IV", 1=>"1" }
     roman = ""
   
     numerals.keys.each do |k|

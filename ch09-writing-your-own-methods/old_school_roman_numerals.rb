2cefdb81391bcc55712c44d9404fb5cb5aa6c38e
daviryan <daviryan@aol.com>
2017-06-09 22:40:41 +0000

    updated roman numerals to return string

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,14 +1,15 @@
 def old_roman_numeral num
+    # your code here
     num = num.to_i
     (puts "Invalid input. Choose a number between 1 and 3000"; return) if ((num.to_i < 1) || (num.to_i > 3000))
   
-    numerals = {1000=>"M", 500=>"D", 100=>"C", 50=>"L", 10=>"X", 5=>"V", 1=>"1" }
+    numerals = {1000=>"M", 500=>"D", 100=>"C", 50=>"L", 10=>"X", 5=>"V", 1=>"I" }
     roman = ""
   
     numerals.keys.each do |k|
         n = num/k
         if n > 0
-            roman += numerals[k]*n
+            roman += (numerals[k]*n)
             num = num % k
         end
     end
--- ch09-writing-your-own-methods/roman_numerals.rb
+++ ch09-writing-your-own-methods/roman_numerals.rb
@@ -1,8 +1,9 @@
 def roman_numeral num
+    # your code here
     num = num.to_i
     (puts "Invalid input. Choose a number between 1 and 3000"; return) if ((num.to_i < 1) || (num.to_i > 3000))
   
-    numerals = {1000=>"M", 500=>"D", 400=>"CD,", 100=>"C", 90=>"XC", 50=>"L", 40=>"XL", 10=>"X", 5=>"V", 4=>"IV", 1=>"1" }
+    numerals = {1000=>"M", 500=>"D", 400=>"CD,", 100=>"C", 90=>"XC", 50=>"L", 40=>"XL", 10=>"X", 5=>"V", 4=>"IV", 1=>"I" }
     roman = ""
   
     numerals.keys.each do |k|

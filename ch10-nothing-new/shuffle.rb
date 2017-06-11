55c5394f0991f71756d7274e84349443db1fc7c9
daviryan <daviryan@aol.com>
2017-06-11 18:19:23 +0000

    shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,6 +1,7 @@
 def shuffle arr
   # your code here
-  arr.size.downto(1) do |n|
+  size = arr.length
+  (size).downto(1) do |n|
        index=rand(n)
        # swap elements at index and the end
        arr[index], arr[size-1] = arr[size-1],arr[index]

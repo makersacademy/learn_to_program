18d864efeab47c2c51c72b374195fb913d96efa9
daviryan <daviryan@aol.com>
2017-06-12 17:34:00 +0000

    roman to int

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,5 +1,6 @@
 def roman_to_integer roman
   # your code here
+  roman.upcase!
   numerals = {"M"=>1000, "D"=>500, "CD"=>400, "C"=>100, "XC"=>90, "L"=>50, "XL"=>40, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1 }
     integer = 0
     i=0

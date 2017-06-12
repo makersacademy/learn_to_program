1d93ae5701a7ca94af4bbb7ac8de0b53690ad8fc
daviryan <daviryan@aol.com>
2017-06-12 18:01:39 +0000

    extend integer

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -10,7 +10,7 @@ class Integer
     num = self
     (puts "Invalid input. Choose a number between 1 and 3000"; return) if ((num.to_i < 1) || (num.to_i > 3000))
   
-    numerals = {1000=>"M", 500=>"D", 400=>"CD", 100=>"C", 90=>"XC", 50=>"L", 40=>"XL", 10=>"X", 9=>"IX", 5=>"V", 4=>"IV", 1=>"I" }
+    numerals = {1000=>"M", 500=>"D", 100=>"C", 50=>"L", 10=>"X", 5=>"V", 1=>"I" }
     roman = ""
   
     numerals.keys.each do |k|

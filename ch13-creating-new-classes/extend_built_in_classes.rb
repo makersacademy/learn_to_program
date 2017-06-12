2c058421411983c656f26b25f26ccb16e0bcb7d2
daviryan <daviryan@aol.com>
2017-06-12 17:53:08 +0000

    extend integer

--- ch13-creating-new-classes/extend_built_in_classes.rb
+++ ch13-creating-new-classes/extend_built_in_classes.rb
@@ -1,12 +1,13 @@
 class Integer
   # your code here
   def factorial 
-      return "Number needs to be more than 0" if self <= 0
+      return "Number needs to be more than 0" if self < 0
+      return 1 if self <= 1
       self * (self-1).factorial
   end
   
   def to_roman
-      num = self
+    num = self
     (puts "Invalid input. Choose a number between 1 and 3000"; return) if ((num.to_i < 1) || (num.to_i > 3000))
   
     numerals = {1000=>"M", 500=>"D", 400=>"CD", 100=>"C", 90=>"XC", 50=>"L", 40=>"XL", 10=>"X", 9=>"IX", 5=>"V", 4=>"IV", 1=>"I" }

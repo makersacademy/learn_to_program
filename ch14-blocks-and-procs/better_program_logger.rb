fae49534b4f9d98c70059df895f22e54f2984db7
daviryan <daviryan@aol.com>
2017-06-13 16:16:27 +0000

    ch14

--- ch14-blocks-and-procs/better_program_logger.rb
+++ ch14-blocks-and-procs/better_program_logger.rb
@@ -1,9 +1,9 @@
 $depth = 0
 def better_log desc, &block
   # your code here
-  puts "Beginning \"#{desc}\"..."
+  puts "#{"\t"*$depth}Beginning \"#{desc}\"..."
   $depth +=1
   result = block.call
   $depth -=1
-  puts "...\"#{desc}\" finished, returning: #{ result.to_s}"
+  puts "#{"\t"*$depth}...\"#{desc}\" finished, returning: #{ result.to_s}"
 end

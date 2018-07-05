21fa8d764c666121c2a7dcb76fad9df59420bcde
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-05 15:52:38 +0100

    Ch 9 Q2 Solution - Old Roman Numerals

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,13 @@
 def old_roman_numeral num
-  # your code here
+
+  roman_array = []
+  roman_array << 'M' * (num / 1000)
+  roman_array << 'D' * (num % 1000 / 500)
+  roman_array << 'C' * (num % 500 / 100)
+  roman_array << 'L' * (num % 100 / 50)
+  roman_array << 'X' * (num % 50 / 10)
+  roman_array << 'V' * (num % 10 / 5)
+  roman_array << 'I' * (num % 5 / 1)
+  return roman_array.join
+
 end
